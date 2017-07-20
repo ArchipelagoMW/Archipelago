037c643aab105ae05e2464f780b38d73445bf320
LLCoolDave <llcooldave@gmail.com>
2017-07-20 11:22:35 +0200

    Remove pre-filling of gtower with trash items to match vt25 implementation.

--- Rom.py
+++ Rom.py
@@ -358,7 +358,7 @@ def patch_rom(world, rom, hashtable, quickswap=False, beep='normal', sprite=None
 
     # set rom name
     # 21 bytes
-    rom.write_bytes(0x7FC0, bytearray('ER_043_%09d_' % world.seed, 'utf8') + world.option_identifier.to_bytes(4, 'big'))
+    rom.write_bytes(0x7FC0, bytearray('ER_044_%09d_' % world.seed, 'utf8') + world.option_identifier.to_bytes(4, 'big'))
 
     # set heart beep rate
     rom.write_byte(0x180033, {'off': 0x00, 'half': 0x40, 'quarter': 0x80, 'normal': 0x20}[beep])
