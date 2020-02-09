bc081ded9d1cc5b9cc5923ae3150d21f9ccdfbc4
Fabian Dill <fabian.dill@web.de>
2020-02-09 21:32:29 +0100

    add host.yaml to compiled program

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ def installfile(path):
         print('Warning,', path, 'not found')
 
 
-extra_data = ["LICENSE", "data", "EnemizerCLI"]
+extra_data = ["LICENSE", "data", "EnemizerCLI", "host.yaml"]
 
 for data in extra_data:
     installfile(Path(data))
