b7814ca05090b77316e87081c1447f08bee1754f
Fabian Dill <fabian.dill@web.de>
2020-03-07 17:08:50 +0100

    remove deprecated code

--- MultiClient.py
+++ MultiClient.py
@@ -1003,7 +1003,6 @@ if __name__ == '__main__':
     colorama.init()
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-    loop.run_until_complete(asyncio.gather(*asyncio.Task.all_tasks()))
     loop.close()
     colorama.deinit()
     atexit.unregister(exit_func)
--- MultiServer.py
+++ MultiServer.py
@@ -671,5 +671,4 @@ async def main():
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-    loop.run_until_complete(asyncio.gather(*asyncio.Task.all_tasks()))
     loop.close()
