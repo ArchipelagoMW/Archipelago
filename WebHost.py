74cbfe07a3630ac6607874069c4375da4a28a393
Fabian Dill <fabian.dill@web.de>
2020-06-27 18:17:36 +0200

    fix tracker auto update

--- WebHost.py
+++ WebHost.py
@@ -10,6 +10,25 @@ from WebHost.models import db
 DEBUG = False
 port = 80
 
+
+def autohost(config: dict):
+    from WebHost.models import *
+    import time
+    from datetime import timedelta, datetime
+
+    def keep_running():
+        db.bind(**config["pony"])
+        db.generate_mapping(check_tables=False)
+        while 1:
+            time.sleep(3)
+            with db_session:
+                select(
+                    room for room in Room if (datetime.utcnow() - room.last_activity) < timedelta(hours=room.timeout))
+
+    import threading
+    threading.Thread(target=keep_running).start()
+
+
 if __name__ == "__main__":
     multiprocessing.freeze_support()
     multiprocessing.set_start_method('spawn')
@@ -19,7 +38,6 @@ if __name__ == "__main__":
 
     if os.path.exists(configpath):
         import yaml
-
         with open(configpath) as c:
             app.config.update(yaml.safe_load(c))
 
@@ -27,6 +45,7 @@ if __name__ == "__main__":
     db.bind(**app.config["PONY"])
     db.generate_mapping(create_tables=True)
     if DEBUG:
+        autohost(config)
         app.run(debug=True, port=port)
     else:
         serve(app, port=port, threads=app.config["WAITRESS_THREADS"])
