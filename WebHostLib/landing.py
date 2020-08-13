2a2452e30f20944185ce83eac1c57c85ccfaf251
Fabian Dill <fabian.dill@web.de>
2020-08-13 09:23:52 +0200

    update landing with dynamic links and counter for Seeds

--- WebHostLib/landing.py
+++ WebHostLib/landing.py
@@ -4,7 +4,8 @@ from .models import *
 from datetime import timedelta
 
 @app.route('/', methods=['GET', 'POST'])
-@cache.cached(timeout=300) # cache has to appear under app route for caching to work
+@cache.cached(timeout=300)  # cache has to appear under app route for caching to work
 def landing():
-    multiworlds = count(room for room in Room if room.creation_time >= datetime.utcnow() - timedelta(days=7))
-    return render_template("landing.html", multiworlds=multiworlds)
+    rooms = count(room for room in Room if room.creation_time >= datetime.utcnow() - timedelta(days=7))
+    seeds = count(seed for seed in Seed if seed.creation_time >= datetime.utcnow() - timedelta(days=7))
+    return render_template("landing.html", rooms=rooms, seeds=seeds)
