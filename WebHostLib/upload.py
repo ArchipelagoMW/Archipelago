74179308ec9d90cfe4694170029fa40e88b045fc
Fabian Dill <fabian.dill@web.de>
2020-08-15 15:17:31 +0200

    apply P number targeting fix to uploads

--- WebHostLib/upload.py
+++ WebHostLib/upload.py
@@ -39,7 +39,7 @@ def uploads():
                             if file.filename.endswith(banned_zip_contents):
                                 return "Uploaded data contained a rom file, which is likely to contain copyrighted material. Your file was deleted."
                             elif file.filename.endswith(".bmbp"):
-                                player = int(file.filename.split("P")[-1].split(".")[0].split("_")[0])
+                                player = int(file.filename.split("P")[1].split(".")[0].split("_")[0])
                                 patches.add(Patch(data=zfile.open(file, "r").read(), player=player))
                             elif file.filename.endswith(".txt"):
                                 spoiler = zfile.open(file, "rt").read().decode("utf-8-sig")
