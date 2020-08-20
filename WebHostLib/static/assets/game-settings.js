79170f8195397ef27d9716ee487f1af2be9e4811
Chris Wilson <chris@legendserver.info>
2020-08-19 21:51:59 -0400

    Solid progress on Game Settings

--- WebHostLib/static/assets/game-settings.js
+++ WebHostLib/static/assets/game-settings.js
@@ -18,12 +18,15 @@ window.addEventListener('load', () => {
 
         // Update localStorage with three settings objects. Preserve original objects if present.
         for (let i=1; i<=3; i++) {
-            const localSettings = localStorage.getItem(`gameSettings${i}`);
-            const updatedObj = localSettings ? Object.assign(sourceData, JSON.parse(localSettings)) : sourceData;
+            const localSettings = JSON.parse(localStorage.getItem(`gameSettings${i}`));
+            const updatedObj = localSettings ? Object.assign(sourceData, localSettings) : sourceData;
             localStorage.setItem(`gameSettings${i}`, JSON.stringify(updatedObj));
         }
 
-        console.info(sourceData);
+        populateSettings();
+        document.getElementById('preset-number').addEventListener('change', populateSettings);
+        gameSettings.addEventListener('change', handleOptionChange);
+        gameSettings.addEventListener('keyup', handleOptionChange);
     }).catch((error) => {
         gameSettings.innerHTML = `
         <h2>Something went wrong while loading your game settings page.</h2>
@@ -32,3 +35,67 @@ window.addEventListener('load', () => {
         `
     });
 });
+
+const handleOptionChange = (event) => {
+    if(!event.target.matches('.setting')) { return; }
+    const presetNumber = document.getElementById('preset-number').value;
+    const settings = JSON.parse(localStorage.getItem(`gameSettings${presetNumber}`))
+    const settingString = event.target.getAttribute('data-setting');
+    document.getElementById(settingString).innerText = event.target.value;
+    if(getSettingValue(settings, settingString) !== false){
+        const keys = settingString.split('.');
+        switch (keys.length) {
+            case 1:
+                settings[keys[0]] = event.target.value;
+                break;
+            case 2:
+                settings[keys[0]][keys[1]] = event.target.value;
+                break;
+            case 3:
+                settings[keys[0]][keys[1]][keys[2]] = event.target.value;
+                break;
+            default:
+                console.warn(`Unknown setting string received: ${settingString}`)
+                return;
+        }
+
+        // Save the updated settings object bask to localStorage
+        localStorage.setItem(`gameSettings${presetNumber}`, JSON.stringify(settings));
+    }else{
+        console.warn(`Unknown setting string received: ${settingString}`)
+    }
+};
+
+const populateSettings = () => {
+    const presetNumber = document.getElementById('preset-number').value;
+    const settings = JSON.parse(localStorage.getItem(`gameSettings${presetNumber}`))
+    const settingsInputs = Array.from(document.querySelectorAll('.setting'));
+    settingsInputs.forEach((input) => {
+        const settingString = input.getAttribute('data-setting');
+        const settingValue = getSettingValue(settings, settingString);
+        console.info(`${settingString}: ${settingValue}`);
+        if(settingValue !== false){
+            input.value = settingValue;
+            document.getElementById(settingString).innerText = settingValue;
+        }
+    });
+};
+
+/**
+ * Returns the value of the settings object, or false if the settings object does not exist
+ * @param settings
+ * @param keyString
+ * @returns {string} | bool
+ */
+const getSettingValue = (settings, keyString) => {
+    const keys = keyString.split('.');
+    let currentVal = settings;
+    keys.forEach((key) => {
+        if(typeof(key) === 'string' && currentVal.hasOwnProperty(key)){
+            currentVal = currentVal[key];
+        }else{
+            return false;
+        }
+    });
+    return currentVal;
+};
