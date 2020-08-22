c57374a5ff70fe984e38a060e948e51e727507be
Chris Wilson <chris@legendserver.info>
2020-08-22 19:03:09 -0400

    Add JSON schema for player settings, which playerSettings.yaml (formerly easy.yaml) will need to conform to when parsed to JSON.

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ for data in extra_data:
     installfile(Path(data))
 
 os.makedirs(buildfolder / "Players", exist_ok=True)
-shutil.copyfile("easy.yaml", buildfolder / "Players" / "easy.yaml")
+shutil.copyfile("playerSettings.yaml", buildfolder / "Players" / "playerSettings.yaml")
 
 qusb2sneslog = buildfolder / "QUsb2Snes" / "log.txt"
 if os.path.exists(qusb2sneslog):
