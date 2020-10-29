c5f3b4732793a9948b1dc18dba8a58c242c03fa2
Fabian Dill <fabian.dill@web.de>
2020-10-29 01:59:44 +0100

    url_for expects names, not actual functions

--- WebHostLib/generate.py
+++ WebHostLib/generate.py
@@ -87,8 +87,8 @@ def generate_api():
             return {"text": f"Generation of seed {gen.id} started successfully.",
                     "detail": gen.id,
                     "encoded": app.url_map.converters["suuid"].to_url(None, gen.id),
-                    "wait_api_url": url_for(wait_seed_api, seed=gen.id),
-                    "url": url_for(wait_seed, seed=gen.id)}, 201
+                    "wait_api_url": url_for("wait_seed_api", seed=gen.id),
+                    "url": url_for("wait_seed", seed=gen.id)}, 201
     except Exception as e:
         return {"text": "Uncaught Exception:" + str(e)}, 500
 
