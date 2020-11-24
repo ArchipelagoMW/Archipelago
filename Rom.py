6642769caf09937fb423426a48921e67858850a8
pepperpow <pepe.tjg@gmail.com>
2020-11-23 20:17:03 -0600

    Added missing player byte from table

--- Rom.py
+++ Rom.py
@@ -1516,7 +1516,7 @@ def write_custom_shops(rom, world, player):
             item_data = [shop_id, ItemFactory(item['item'], player).code] + int16_as_bytes(item['price']) + [
                 item['max'],
                 ItemFactory(item['replacement'], player).code if item['replacement'] else 0xFF] + int16_as_bytes(
-                item['replacement_price'])
+                item['replacement_price']) + [item['player']]
             items_data.extend(item_data)
 
     rom.write_bytes(0x184800, shop_data)
