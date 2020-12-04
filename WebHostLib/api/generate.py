73013395a73fbec627056a112ad08c9d13c1b78b
Chris Wilson <chris@legendserver.info>
2020-12-04 02:59:23 -0500

    Fix bug in api wait_seed, fix styles on seed gen pages

--- WebHostLib/api/generate.py
+++ WebHostLib/api/generate.py
@@ -52,7 +52,7 @@ def generate_api():
             return {"text": f"Generation of seed {gen.id} started successfully.",
                     "detail": gen.id,
                     "encoded": app.url_map.converters["suuid"].to_url(None, gen.id),
-                    "wait_api_url": url_for("wait_seed_api", seed=gen.id, _external=True),
+                    "wait_api_url": url_for("api.wait_seed_api", seed=gen.id, _external=True),
                     "url": url_for("wait_seed", seed=gen.id, _external=True)}, 201
     except Exception as e:
         return {"text": "Uncaught Exception:" + str(e)}, 500
