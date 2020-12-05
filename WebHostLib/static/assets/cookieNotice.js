1a62772825a6db7ef4c8684ff5a226e858a428bd
Chris Wilson <chris@legendserver.info>
2020-12-05 15:37:31 -0500

    Slew of minor style adjustments
    - Added padding to bottom of /setup-guide to accomodate iPhone users with curved screen bottoms
    - Added screen orientation detection to /player-settings to force select inputs into a single column on mobile devices
    - Added an X to the cookie notice to indicate it can be closed
    - Changed "Generate Game" to "Start Game" so it matches the header

--- WebHostLib/static/assets/cookieNotice.js
+++ WebHostLib/static/assets/cookieNotice.js
@@ -5,6 +5,10 @@ window.addEventListener('load', () => {
     const cookieNotice = document.createElement('div');
     cookieNotice.innerText = "This website uses cookies to store information about the games you play.";
     cookieNotice.setAttribute('id', 'cookie-notice');
+    const closeButton = document.createElement('span');
+    closeButton.setAttribute('id', 'close-button');
+    closeButton.innerText = 'X';
+    cookieNotice.appendChild(closeButton);
     document.body.appendChild(cookieNotice);
     cookieNotice.addEventListener('click', () => {
         localStorage.setItem('cookieNotice', "1");
