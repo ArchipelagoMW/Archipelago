4b2a9b3e87b15dd3077724762b0f43948c421d38
Fabian Dill <fabian.dill@web.de>
2021-01-20 03:17:51 +0100

    unroll unneeded inner generator expression

--- Shops.py
+++ Shops.py
@@ -287,8 +287,7 @@ total_dynamic_shop_slots = sum(3 for shopname, data in shop_table.items() if not
 
 SHOP_ID_START = 0x400000
 shop_table_by_location_id = {cnt: s for cnt, s in enumerate(
-    (item for sublist in (f"{name} Slot {num}" for num in range(1, 4) for name in shop_table)
-     for item in sublist), start=SHOP_ID_START)}
+    (f"{name} Slot {num}" for num in range(1, 4) for name in shop_table), start=SHOP_ID_START)}
 shop_table_by_location_id[(SHOP_ID_START + total_shop_slots)] = "Old Man Sword Cave"
 shop_table_by_location_id[(SHOP_ID_START + total_shop_slots + 1)] = "Take-Any #1"
 shop_table_by_location_id[(SHOP_ID_START + total_shop_slots + 2)] = "Take-Any #2"
