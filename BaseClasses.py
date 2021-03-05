d0d995b3a4f9f6e41bf60ed0e8b02172cc714eee
CaitSith2 <d_good@caitsith2.com>
2021-03-05 11:05:57 -0800

    Use the more convenient _asdict() function of named tuples.

--- BaseClasses.py
+++ BaseClasses.py
@@ -1509,11 +1509,7 @@ class PlandoItem(NamedTuple):
             self.warn(warning)
 
     def to_dict(self):
-        return {"item": self.item,
-                "location": self.location,
-                "world": self.world,
-                "from_pool": self.from_pool,
-                "force": self.force}
+        return self._asdict()
 
 
 class PlandoConnection(NamedTuple):
@@ -1522,6 +1518,4 @@ class PlandoConnection(NamedTuple):
     direction: str  # entrance, exit or both
 
     def to_dict(self):
-        return {"entrance": self.entrance,
-                "exit": self.exit,
-                "direction": self.direction}
+        return self._asdict()
