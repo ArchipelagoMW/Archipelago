ce3804038ec730ef93cafbf70498b2cb8df024e7
Fabian Dill <fabian.dill@web.de>
2021-03-21 05:21:57 +0100

    Focus cursed-only location is now removed

--- test/hollow_knight/TestBasic.py
+++ test/hollow_knight/TestBasic.py
@@ -5,7 +5,6 @@ class TestBasic(TestVanilla):
 
     def testSimple(self):
         self.run_location_tests([
-            ["Focus", True, []],
             ["200_Geo-False_Knight_Chest", True, [], []],
             ["380_Geo-Soul_Master_Chest", False, [], ["Mantis_Claw"]],
         ])
