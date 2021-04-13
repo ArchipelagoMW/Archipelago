8879c27de56d1271fddf68a4784d1d7f3f653195
achuang <alexander.w.chuang@gmail.com>
2021-04-13 17:13:50 -0500

    bump network_data_package version

--- worlds/__init__.py
+++ worlds/__init__.py
@@ -27,7 +27,7 @@ lookup_any_location_name_to_id = {name: id for id, name in lookup_any_location_i
 
 network_data_package = {"lookup_any_location_id_to_name": lookup_any_location_id_to_name,
                         "lookup_any_item_id_to_name": lookup_any_item_id_to_name,
-                        "version": 4}
+                        "version": 5}
 
 
 @enum.unique
