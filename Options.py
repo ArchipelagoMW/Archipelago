a6aaabd0c12351e0294aefa80534bd7f891d4c6d
espeon65536 <espeon65536@gmail.com>
2021-04-28 21:02:34 -0500

    formally added combat_difficulty as an option

--- Options.py
+++ Options.py
@@ -301,11 +301,18 @@ class AdvancementGoal(Choice):
     option_many = 2
     default = 1
 
+class CombatDifficulty(Choice): 
+    option_easy = 0
+    option_normal = 1
+    option_hard = 2
+    default = 1
+
 minecraft_options: typing.Dict[str, type(Option)] = {
+    "advancement_goal": AdvancementGoal, 
+    "combat_difficulty": CombatDifficulty,
     "include_hard_advancements": Toggle,
     "include_insane_advancements": Toggle,
     "include_postgame_advancements": Toggle,
-    "advancement_goal": AdvancementGoal, 
     "shuffle_structures": Toggle
 }
 
