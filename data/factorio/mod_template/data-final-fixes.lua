878ab3303976fcdde030bb30f224ba588c2fadb4
Fabian Dill <fabian.dill@web.de>
2021-06-25 22:09:09 +0200

    Factorio: fix incomplete crafting category copy

--- data/factorio/mod_template/data-final-fixes.lua
+++ data/factorio/mod_template/data-final-fixes.lua
@@ -57,7 +57,10 @@ function adjust_energy(recipe_name, factor)
     data.raw.recipe[recipe_name].energy_required = energy * factor
 end
 
-table.insert(data.raw["assembling-machine"]["assembling-machine-1"].crafting_categories, "crafting-with-fluid")
+data.raw["assembling-machine"]["assembling-machine-1"].crafting_categories = table.deepcopy(data.raw["assembling-machine"]["assembling-machine-3"].crafting_categories)
+data.raw["assembling-machine"]["assembling-machine-2"].crafting_categories = table.deepcopy(data.raw["assembling-machine"]["assembling-machine-3"].crafting_categories)
+data.raw["assembling-machine"]["assembling-machine-1"].fluid_boxes = table.deepcopy(data.raw["assembling-machine"]["assembling-machine-2"].fluid_boxes)
+
 
 {# each randomized tech gets set to be invisible, with new nodes added that trigger those #}
 {%- for original_tech_name, item_name, receiving_player, advancement in locations %}
