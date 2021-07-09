bcb6d79a93f0bf34646c5c28fc10a84c64b606bf
espeon65536 <espeon65536@gmail.com>
2021-07-08 20:14:55 -0500

    add a bunch of utility functions to prepare for rom patching

--- worlds/oot/Utils.py
+++ worlds/oot/Utils.py
@@ -1,8 +1,9 @@
 import io, re, json
-from Utils import local_path
+from Utils import local_path as ap_local_path
+from .version import __version__
 
 def data_path(*args): 
-    return local_path('data', 'oot', *args)
+    return ap_local_path('data', 'oot', *args)
 
 def read_json(file_path):
     json_string = ""
@@ -16,3 +17,81 @@ def read_json(file_path):
         raise Exception("JSON parse error around text:\n" + \
                         json_string[error.pos-35:error.pos+35] + "\n" + \
                         "                                   ^^\n")
+
+def is_bundled():
+    return getattr(sys, 'frozen', False)
+
+# From the pyinstaller Wiki: https://github.com/pyinstaller/pyinstaller/wiki/Recipe-subprocess
+# Create a set of arguments which make a ``subprocess.Popen`` (and
+# variants) call work with or without Pyinstaller, ``--noconsole`` or
+# not, on Windows and Linux. Typical use::
+#   subprocess.call(['program_to_run', 'arg_1'], **subprocess_args())
+def subprocess_args(include_stdout=True):
+    # The following is true only on Windows.
+    if hasattr(subprocess, 'STARTUPINFO'):
+        # On Windows, subprocess calls will pop up a command window by default
+        # when run from Pyinstaller with the ``--noconsole`` option. Avoid this
+        # distraction.
+        si = subprocess.STARTUPINFO()
+        si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        # Windows doesn't search the path by default. Pass it an environment so
+        # it will.
+        env = os.environ
+    else:
+        si = None
+        env = None
+
+    # ``subprocess.check_output`` doesn't allow specifying ``stdout``::
+    # So, add it only if it's needed.
+    if include_stdout:
+        ret = {'stdout': subprocess.PIPE}
+    else:
+        ret = {}
+
+    # On Windows, running this from the binary produced by Pyinstaller
+    # with the ``--noconsole`` option requires redirecting everything
+    # (stdin, stdout, stderr) to avoid an OSError exception
+    # "[Error 6] the handle is invalid."
+    ret.update({'stdin': subprocess.PIPE,
+                'stderr': subprocess.PIPE,
+                'startupinfo': si,
+                'env': env })
+    return ret
+
+def local_path(path=''):
+    if local_path.cached_path is not None:
+        return os.path.join(local_path.cached_path, path)
+
+    if is_bundled():
+        # we are running in a bundle
+        local_path.cached_path = os.path.dirname(os.path.realpath(sys.executable))
+    else:
+        # we are running in a normal Python environment
+        local_path.cached_path = os.path.dirname(os.path.realpath(__file__))
+
+    return os.path.join(local_path.cached_path, path)
+
+local_path.cached_path = None
+
+def get_version_bytes(a):
+    version_bytes = [0x00, 0x00, 0x00]
+    if not a:
+        return version_bytes;
+    sa = a.replace('v', '').replace(' ', '.').split('.')
+
+    for i in range(0,3):
+        try:
+            version_byte = int(sa[i])
+        except ValueError:
+            break
+        version_bytes[i] = version_byte
+
+    return version_bytes
+
+def default_output_path(path):
+    if path == '':
+        path = local_path('output')
+
+    if not os.path.exists(path):
+        os.mkdir(path)
+    return path
