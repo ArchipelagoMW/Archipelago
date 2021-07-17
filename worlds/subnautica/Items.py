d406e4c3d9ff26e9b51dc01448227e613fd1c6e5
Daivuk <daivuk@gmail.com>
2021-07-17 12:07:45 -0400

    Added Subnautica Support

---
+++ worlds/subnautica/Items.py
@@ -0,0 +1,14 @@
+import json
+
+with open('worlds/subnautica/items.json', 'r') as file:
+    item_table = json.loads(file.read())
+
+lookup_id_to_name = {}
+lookup_name_to_item = {}
+for item in item_table:
+    lookup_id_to_name[item["id"]] = item["name"]
+    lookup_name_to_item[item["name"]] = item
+
+lookup_id_to_name[None] = "Victory"
+
+lookup_name_to_id = {name: id for id, name in lookup_id_to_name.items()}
