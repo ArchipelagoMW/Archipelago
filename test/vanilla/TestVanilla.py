df9bad75ea510d4025d6d071aa786a0897661f3f
Fabian Dill <fabian.dill@web.de>
2021-07-22 15:59:37 +0200

    fix unittests

--- test/vanilla/TestVanilla.py
+++ test/vanilla/TestVanilla.py
@@ -25,11 +25,11 @@ class TestVanilla(TestBase):
         create_dungeons(self.world, 1)
         create_shops(self.world, 1)
         link_entrances(self.world, 1)
-        generate_itempool(self.world, 1)
+        self.world.worlds[1].create_items()
         self.world.required_medallions[1] = ['Ether', 'Quake']
         self.world.itempool.extend(get_dungeon_item_pool(self.world))
         self.world.itempool.extend(ItemFactory(['Green Pendant', 'Red Pendant', 'Blue Pendant', 'Beat Agahnim 1', 'Beat Agahnim 2', 'Crystal 1', 'Crystal 2', 'Crystal 3', 'Crystal 4', 'Crystal 5', 'Crystal 6', 'Crystal 7'], 1))
         self.world.get_location('Agahnim 1', 1).item = None
         self.world.get_location('Agahnim 2', 1).item = None
         mark_dark_world_regions(self.world, 1)
-        set_rules(self.world, 1)
+        self.world.worlds[1].set_rules()
