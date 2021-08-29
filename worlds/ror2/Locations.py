944347a2b3e917d558dfea3e4bc591131424821f
Hussein Farran <hmfarran@gmail.com>
2021-08-29 14:02:02 -0400

    Risk of Rain 2 implementation

---
+++ worlds/ror2/Locations.py
@@ -0,0 +1,23 @@
+from BaseClasses import Location
+import typing
+
+class RiskOfRainLocation(Location):
+    game: str = "Risk of Rain 2"
+
+# 37000 - 38000
+base_location_table = {
+    "Victory": 37999,
+    "Level One": 37001,
+    "Level Two": 37002,
+    "Level Three": 37003,
+    "Level Four": 37004,
+    "Level Five": 37005
+}
+
+item_pickups = {
+    f"ItemPickup{i}": 37005+i for i in range(1, 51)
+}
+
+location_table = {**base_location_table, **item_pickups}
+
+lookup_id_to_name: typing.Dict[int, str] = {id: name for name, id in location_table.items()}
---
+++ worlds/ror2/Options.py
@@ -0,0 +1,42 @@
+import typing
+from Options import Option, Toggle, Range
+
+
+class TotalLocations(Range):
+    """Number of location checks which are added to the Risk of Rain playthrough."""
+    displayname = "Total Locations"
+    range_start = 10
+    range_end = 50
+    default = 15
+
+
+class TotalRevivals(Range):
+    """Number of `Dio's Best Friend` item put in the item pool."""
+    displayname = "Total Revivals Available"
+    range_start = 0
+    range_end = 10
+    default = 4
+
+
+class ItemPickupStep(Range):
+    """Number of items to pick up before an AP Check is completed.
+    Setting to 1 means every other pickup.
+    Setting to 2 means every third pickup. So on..."""
+    displayname = "Item Pickup Step"
+    range_start = 0
+    range_end = 5
+    default = 1
+
+
+class StartWithRevive(Toggle):
+    """Start the game with a `Dio's Best Friend` item."""
+    displayname = "Start with a Revive"
+    default = True
+
+
+ror2_options: typing.Dict[str, type(Option)] = {
+    "total_locations":      TotalLocations,
+    "total_revivals":       TotalRevivals,
+    "start_with_revive":    StartWithRevive,
+    "item_pickup_step":     ItemPickupStep
+}
