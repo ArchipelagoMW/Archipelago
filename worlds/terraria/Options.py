48dd1a1aa696a15d8f96973c4efe0a4e35869ea7
SolventMercury <antspiral3220@yahoo.com>
2021-09-08 07:02:12 -0700

    Revert "Add Terraria Support"
    This reverts commit 3aacaffe6bef0522dcc56df82b34f0a0ffe3a559.

--- worlds/terraria/Options.py
+++
@@ -1,8 +0,0 @@
-import typing
-from Options import Choice, Option, Toggle, Range
-
-terraria_options: typing.Dict[str, type(Option)] = {
-    "include_hardmode_achievements": Toggle,
-    "include_insane_achievements": Toggle,
-    "include_postgame_achievements": Toggle,
-}
