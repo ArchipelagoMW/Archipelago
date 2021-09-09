92fdef99850a9d6efce22460d7ec5e2982097a3e
Sandra <gjgfujb@gmail.com>
2021-09-09 23:37:04 +1000

    Pysol Logic

--- worlds/pysol/Options.py
+++ worlds/pysol/Options.py
@@ -1,36 +1,5 @@
-import typing
-from Options import Choice, Option, Range, Toggle
+from Options import Choice, OptionDict, Option, DefaultOnToggle, Range
 
-
-class Character(Choice):
-    """Pick What Character you wish to play with."""
-    display_name = "Character"
-    option_ironclad = 0
-    option_silent = 1
-    option_defect = 2
-    option_watcher = 3
-    default = 0
-
-
-class Ascension(Range):
-    """What Ascension do you wish to play with."""
-    display_name = "Ascension"
-    range_start = 0
-    range_end = 20
-    default = 0
-
-
-class HeartRun(Toggle):
-    """Whether or not you will need to collect they 3 keys to unlock the final act
-     and beat the heart to finish the game."""
-    display_name = "Heart Run"
-    option_true = 1
-    option_false = 0
-    default = 0
-
-
-spire_options: typing.Dict[str, type(Option)] = {
-    "character": Character,
-    "ascension": Ascension,
-    "heart_run": HeartRun
-}
+class NumGames(Range):
+    range_end = 1100
+options = {"initial_games": NumGames,"victory_number": NumGames}
--- worlds/pysol/__init__.py
+++ worlds/pysol/__init__.py
@@ -1,78 +1,53 @@
-import logging
-from typing import Set
-
-logger = logging.getLogger("PySolFC")
-
-from .Locations import lookup_name_to_id as locations_lookup_name_to_id
-from .Items import lookup_name_to_item
-from .Items import lookup_name_to_id as items_lookup_name_to_id
-
-from .Regions import create_regions
-from .Rules import set_rules
-
-from BaseClasses import Region, Entrance, Location, MultiWorld, Item
+import os
+import json
 from ..AutoWorld import World
+from ..generic.Rules import set_rule
+from BaseClasses import Region, Item, Location, RegionType, Entrance
+from .Options import options
 
 
-class PySolFCWorld(World):
-    game: str = "PySolFC"
-
-    item_name_to_id = items_lookup_name_to_id
-    location_name_to_id = locations_lookup_name_to_id
-
+class PysolFCWorld(World):
+    game: str = "PysolFC"
+    game_table = {}
+    with open(os.path.join(os.path.dirname(__file__), 'games.json'), 'r') as file:
+        game_table = json.loads(file.read())
+    item_name_to_id = {}
+    options = options
+    id = 626001
+    for solgame in game_table:
+        #lookup_id_to_name[id] = game["name"]
+        #lookup_name_to_item[game["name"]] = {"id": id,"name": game["name"],"progression": True,"game": game}
+        item_name_to_id[solgame["name"]] = id
+        id += 1
+        
+    location_name_to_id = item_name_to_id
     def generate_basic(self):
-        # Link regions
-        self.world.get_entrance('Startup', self.player).connect(self.world.get_region('Games Menu', self.player))
-
-        # Generate item pool
-        pool = []
-        for item in lookup_name_to_item:
-            for i in range(item["count"]):
-                pysol_item = PySolFCItem(item["name"], item["progression"], item["id"], player = self.player)
-                pool.append(pysol_item)
-        self.world.itempool += pool
-
-    def set_rules(self):
-        set_rules(self.world, self.player)
-
-
+        for solgame in self.game_table:
+            self.world.itempool.append(Item(solgame['name'],True,self.item_name_to_id[solgame['name']],self.player))
+        for i in range(self.world.initial_games[self.player]):
+            self.world.push_precollected(self.world.random.choice(self.world.itempool))
     def create_regions(self):
-        create_regions(self.world, self.player)
-
-
-    def generate_output(self, output_directory: str):
-        pass
-
-
-    def fill_slot_data(self): 
-        slot_data = {}
-        return slot_data
-
-
-def create_region(world: MultiWorld, player: int, name: str, locations=None, exits=None):
-    ret = Region(name, None, name, player)
-    ret.world = world
-    if locations:
-        for location in locations:
-            loc_id = locations_lookup_name_to_id.get(location, 0)
-            location = PySolFCLocation(player, location, loc_id, ret)
-            ret.locations.append(location)
-    if exits:
-        for exit in exits:
-            ret.exits.append(Entrance(player, exit, ret))
-
-    return ret
-
-
-class PySolFCLocation(Location):
-    game: str = "PySolFC"
-
-    def __init__(self, player: int, name: str, address=None, parent=None):
-        super(PySolFCLocation, self).__init__(player, name, address, parent)
-
-
-class PySolFCItem(Item):
-    game = "PySolFC"
-
-    def __init__(self, name, advancement, code, player: int = None):
-        super(PySolFCItem, self).__init__(name, advancement, code, player)
+        world = self.world
+        menuregion = Region("Menu",RegionType.Generic,"Menu",self.player,world)
+        world.regions.append(menuregion)
+        start = Entrance(self.player,"Start Game",menuregion)
+        menuregion.exits.append(start)
+        region = Region("PysolFC",RegionType.Generic,"PysolFC",self.player,world)
+        self.region = region
+        world.regions.append(region)
+        start.connect(region)
+        for location in self.location_name_to_id:
+            region.locations.append(Location(self.player, location, self.location_name_to_id[location], region))
+        region.world = world
+    def set_rules(self):
+        for location in self.region.locations:
+            try:
+                if self.item_name_to_id[location.name]:
+                    set_rule(location,lambda state,name=location.name: 
+                        state.has(name,self.player)
+                    )
+            except KeyError:
+                pass
+        self.world.worlds[self.player].item_name_groups["completed_games"] = self.item_name_to_id
+        self.world.completion_condition[self.player] = lambda state: state.has_group("completed_games",self.player,self.world.victory_number[self.player])
+        
