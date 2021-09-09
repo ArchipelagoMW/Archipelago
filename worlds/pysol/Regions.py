398fa5f1c3e557f041955ffc65f71bc186d092c8
Sandra <gjgfujb@gmail.com>
2021-09-09 23:37:04 +1000

    Pysol Logic

--- worlds/pysol/Regions.py
+++
@@ -1,8 +0,0 @@
-def create_regions(world, player: int):
-    from . import create_region
-    from .Locations import lookup_name_to_id as location_lookup_name_to_id
-
-    world.regions += [
-        create_region(world, player, 'Startup', None, ['Games Menu']),
-        create_region(world, player, 'Solitaire', [location for location in location_lookup_name_to_id])
-    ]
