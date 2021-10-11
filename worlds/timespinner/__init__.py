065931cae77ceeceed099b14f229bd1ddd19efc7
Jarno Westhof <jarno.westhof@wolterskluwer.com>
2021-10-11 11:41:45 +0200

    Greatly reduced number of items marked as never_excluded due to the performance implications it brings

--- worlds/timespinner/__init__.py
+++ worlds/timespinner/__init__.py
@@ -144,9 +144,7 @@ def create_item_with_correct_settings(world: MultiWorld, player: int, name: str)
     data = item_table[name]
 
     item = Item(name, data.progression, data.code, player)
-
-    if world.exclude_locations[player]: # Doubles performance to not set item exclusion when its not required
-        item.never_exclude = data.never_exclude
+    item.never_exclude = data.never_exclude
 
     if not item.advancement:
         return item
