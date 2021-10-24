82aca3bce4d4644ce8f9996fbab3c3e5f09fe2be
Kyle Franz <san.francisco.engineer@gmail.com>
2021-10-23 23:52:58 -0700

    Fix TR small key getting shuffled away

--- worlds/alttp/Rules.py
+++ worlds/alttp/Rules.py
@@ -936,8 +936,9 @@ def set_trock_key_rules(world, player):
                     # A key is required in the Big Key Chest to prevent a possible softlock.  Place an extra key to ensure 100% locations still works
                     item = ItemFactory('Small Key (Turtle Rock)', player)
                     item.world = world
-                    world.push_item(world.get_location('Turtle Rock - Big Key Chest', player), item, False)
-                    world.get_location('Turtle Rock - Big Key Chest', player).event = True
+                    location = world.get_location('Turtle Rock - Big Key Chest', player)
+                    location.place_locked_item(item)
+                    location.event = True
                     toss_junk_item(world, player)
 
     if world.accessibility[player] != 'locations':
