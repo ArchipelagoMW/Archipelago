e4413c0e07af4d2c4356a528114192a3fd74b1ad
Jarno Westhof <jarnowesthof@gmail.com>
2021-11-14 13:19:53 +0100

    Fixed some routing logic + make two checks more easily available

--- worlds/timespinner/LogicMixin.py
+++ worlds/timespinner/LogicMixin.py
@@ -15,6 +15,9 @@ class TimespinnerLogic(LogicMixin):
     def _timespinner_has_doublejump_of_npc(self, world: MultiWorld, player: int) -> bool:
         return self._timespinner_has_upwarddash(world, player) or (self.has('Timespinner Wheel', player) and self._timespinner_has_doublejump(world, player))
 
+    def _timespinner_has_fastjump_on_npc(self, world: MultiWorld, player: int) -> bool:
+        return self.has_all(['Timespinner Wheel', 'Talaria Attachment'], player)
+
     def _timespinner_has_multiple_small_jumps_of_npc(self, world: MultiWorld, player: int) -> bool:
         return self.has('Timespinner Wheel', player) or self._timespinner_has_upwarddash(world, player)
 
