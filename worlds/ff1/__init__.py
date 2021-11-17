9e224d94ad6c16c4ab1d77f00a1defb7cabbf493
jtoyoda <jamietoyoda@gmail.com>
2021-11-17 15:13:22 -0700

    Changing data version to 0 while it is still in beta

--- worlds/ff1/__init__.py
+++ worlds/ff1/__init__.py
@@ -19,7 +19,7 @@ class FF1World(World):
     game = "Final Fantasy"
     topology_present = False
     remote_items = True
-    data_version = 1
+    data_version = 0
     remote_start_inventory = True
 
     ff1_items = FF1Items()
