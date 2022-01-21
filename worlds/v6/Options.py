344f4afdbdfa4c7a5f462e9a8e919aea81b7f708
Yussur Mustafa Oraji <N00byKing@hotmail.de>
2022-01-21 22:42:11 +0100

    Add VVVVVV to Archipelago (#178)

---
+++ worlds/v6/Options.py
@@ -0,0 +1,20 @@
+import typing
+from Options import Option, DeathLink, Range
+
+class DoorCost(Range):
+    """Amount of Trinkets required to enter Areas. Set to 0 to disable artificial locks."""
+    range_start = 0
+    range_end = 3
+    default = 3
+
+class DeathLinkAmnesty(Range):
+    """Amount of Deaths to take before sending a DeathLink signal, for balancing difficulty"""
+    range_start = 0
+    range_end = 30
+    default = 15
+
+v6_options: typing.Dict[str,type(Option)] = {
+    "DoorCost": DoorCost,
+    "DeathLink": DeathLink,
+    "DeathLinkAmnesty": DeathLinkAmnesty
+}
