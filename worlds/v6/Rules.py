f7eed8d827e2c018d20b5c9f33e6bac9f3c1f2e9
Yussur Mustafa Oraji <N00byKing@hotmail.de>
2022-01-23 12:23:10 +0100

    Update Rules.py

--- worlds/v6/Rules.py
+++ worlds/v6/Rules.py
@@ -9,8 +9,8 @@ def _has_trinket_range(state,player,start,end):
     return True
 
 def create_npctrinket_rules(world,location,player):
-    add_rule(location, lambda state: state.can_reach(world.get_region("Laboratory",player),'Region',player) or 
-                                     state.can_reach(world.get_region("Space Station 2",player),'Region',player))
+    add_rule(location, lambda state: state.can_reach("Laboratory",'Region',player) or 
+                                     state.can_reach("Space Station 2",'Region',player))
 
 def set_rules(world,player):
     if (world.DoorCost[player].value == 0): pass
@@ -31,4 +31,4 @@ def set_rules(world,player):
     connect_regions(world, player,  "The Final Level", "Menu", lambda state: True)
 
     create_npctrinket_rules(world,world.get_location("NPC Trinket",player),player)
-    world.completion_condition[player] = lambda state: state.can_reach(world.get_region("The Final Level",player),'Region',player)
+    world.completion_condition[player] = lambda state: state.can_reach("The Final Level",'Region',player)
