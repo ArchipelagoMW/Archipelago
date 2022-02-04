8e04182b3f4c80e6967ed16a53045934d2375af0
Yussur Mustafa Oraji <N00byKing@hotmail.de>
2022-02-04 21:22:26 +0100

    v6: Add Area Randomizer (#249)
    * v6: Add Area Randomizer

--- worlds/v6/Rules.py
+++ worlds/v6/Rules.py
@@ -1,6 +1,8 @@
 import typing
 from ..generic.Rules import add_rule
-from .Regions import connect_regions
+from .Regions import connect_regions, v6areas
+
+area_connections = {}
 
 def _has_trinket_range(state,player,start,end) -> bool:
     for i in range(start,end):
@@ -8,11 +10,17 @@ def _has_trinket_range(state,player,start,end) -> bool:
             return False
     return True
 
-def set_rules(world,player):
-    connect_regions(world, player, "Menu", "Laboratory",      lambda state: _has_trinket_range(state,player,0,world.DoorCost[player].value))
-    connect_regions(world, player, "Menu", "The Tower",       lambda state: _has_trinket_range(state,player,world.DoorCost[player].value,world.DoorCost[player].value*2))
-    connect_regions(world, player, "Menu", "Space Station 2", lambda state: _has_trinket_range(state,player,world.DoorCost[player].value*2,world.DoorCost[player].value*3))
-    connect_regions(world, player, "Menu", "Warp Zone",       lambda state: _has_trinket_range(state,player,world.DoorCost[player].value*3,world.DoorCost[player].value*4))
+def set_rules(world,player,area_connections):
+    areashuffle = list(range(len(v6areas)))
+    if (world.AreaRandomizer[player].value):
+        world.random.shuffle(areashuffle)
+    area_connections.update({(index+1): (value+1) for index, value in enumerate(areashuffle)})
+    area_connections.update({0:0})
+
+    connect_regions(world, player, "Menu", v6areas[area_connections[1]-1], lambda state: _has_trinket_range(state,player,0,world.DoorCost[player].value))
+    connect_regions(world, player, "Menu", v6areas[area_connections[2]-1], lambda state: _has_trinket_range(state,player,world.DoorCost[player].value,world.DoorCost[player].value*2))
+    connect_regions(world, player, "Menu", v6areas[area_connections[3]-1], lambda state: _has_trinket_range(state,player,world.DoorCost[player].value*2,world.DoorCost[player].value*3))
+    connect_regions(world, player, "Menu", v6areas[area_connections[4]-1], lambda state: _has_trinket_range(state,player,world.DoorCost[player].value*3,world.DoorCost[player].value*4))
 
     #Special Rule for V
     add_rule(world.get_location("V",player), lambda state : state.can_reach("Laboratory",'Region',player) and
@@ -23,10 +31,5 @@ def set_rules(world,player):
     #Special Rule for NPC Trinket
     add_rule(world.get_location("NPC Trinket",player), lambda state: state.can_reach("Laboratory",'Region',player) or 
                                                                      state.can_reach("Space Station 2",'Region',player))
-
-    connect_regions(world, player,  "Laboratory", "Menu", lambda state: True)
-    connect_regions(world, player,  "The Tower", "Menu", lambda state: True)
-    connect_regions(world, player,  "Space Station 2", "Menu", lambda state: True)
-    connect_regions(world, player,  "Warp Zone", "Menu", lambda state: True)
 
     world.completion_condition[player] = lambda state: state.can_reach("V",'Location',player)
--- worlds/v6/__init__.py
+++ worlds/v6/__init__.py
@@ -2,7 +2,7 @@ import string
 from .Items import item_table, V6Item
 from .Locations import location_table, V6Location
 from .Options import v6_options
-from .Rules import set_rules
+from .Rules import set_rules, area_connections
 from .Regions import create_regions
 from BaseClasses import Region, RegionType, Entrance, Item, MultiWorld
 from ..AutoWorld import World
@@ -23,13 +23,16 @@ class V6World(World):
     data_version = 1
     forced_auto_forfeit = False
 
+    area_connections = {}
+
     options = v6_options
 
     def create_regions(self):
         create_regions(self.world,self.player)
 
     def set_rules(self):
-        set_rules(self.world,self.player)
+        self.area_connections = {}
+        set_rules(self.world,self.player,self.area_connections)
 
     def create_item(self, name: str) -> Item:
         return V6Item(name, True, item_table[name], self.player)
@@ -39,6 +42,7 @@ class V6World(World):
 
     def fill_slot_data(self):
         return {
+            "AreaRando": self.area_connections,
             "DoorCost": self.world.DoorCost[self.player].value,
             "DeathLink": self.world.DeathLink[self.player].value,
             "DeathLink_Amnesty": self.world.DeathLinkAmnesty[self.player].value
