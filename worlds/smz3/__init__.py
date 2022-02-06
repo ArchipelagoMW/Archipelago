5373aaefad015f32c2ad9e99c39ba4b8c6627e72
lordlou <lordlou0@hotmail.com>
2022-02-05 19:24:46 -0500

    fixed broken local_items functionnality

--- worlds/smz3/__init__.py
+++ worlds/smz3/__init__.py
@@ -124,10 +124,11 @@ class SMZ3World(World):
                     l.always_allow = lambda state, item, loc=loc: \
                         item.game == "SMZ3" and \
                         loc.alwaysAllow(TotalSMZ3Item.Item(TotalSMZ3Item.ItemType[item.name], self.smz3World), state.smz3state[self.player])
-                l.item_rule = lambda item, loc=loc, region=region: \
+                old_rule = l.item_rule
+                l.item_rule = lambda item, loc=loc, region=region: (\
                     item.game != "SMZ3" or \
                     loc.allow(TotalSMZ3Item.Item(TotalSMZ3Item.ItemType[item.name], self.smz3World), None) and \
-                        region.CanFill(TotalSMZ3Item.Item(TotalSMZ3Item.ItemType[item.name], self.smz3World))
+                        region.CanFill(TotalSMZ3Item.Item(TotalSMZ3Item.ItemType[item.name], self.smz3World))) and old_rule(item)
                 set_rule(l, lambda state, loc=loc: loc.Available(state.smz3state[self.player]))
 
     def create_regions(self):
