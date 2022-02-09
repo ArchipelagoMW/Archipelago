602f46368f99e298ed9e979cf1e17970a69ba85d
Felix <felicitusneko@vivaldi.net>
2022-02-09 18:55:21 -0400

    meritous: implement prefill logic

--- worlds/meritous/__init__.py
+++ worlds/meritous/__init__.py
@@ -5,13 +5,15 @@
 
 import typing
 
+from BaseClasses import Region, RegionType, Entrance, Item, MultiWorld
+from Fill import fill_restrictive
+from ..AutoWorld import World
+
 from .Items import item_table, MeritousItem
 from .Locations import location_table, MeritousLocation
 from .Options import meritous_options
 from .Rules import set_rules
 from .Regions import create_regions
-from BaseClasses import Region, RegionType, Entrance, Item, MultiWorld
-from ..AutoWorld import World
 
 client_version = 1
 
@@ -19,7 +21,7 @@ client_version = 1
 class MeritousWorld(World):
     """Insert some sort of description here"""
 
-    game: str = "Mertious"
+    game: str = "Meritous"
     topology_present: False
 
     item_name_to_id = item_table
@@ -49,10 +51,35 @@ class MeritousWorld(World):
             return self.create_item("Crystals x2000")
 
     def generate_basic(self):
-        frequencies = [0, 25, 23, 22, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3]
+        frequencies = [0, 25, 23, 22, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3]
         item_count = 0
         location_count = len(location_table)
         item_pool = []
+
+        self.world.get_location("Cursed Seal", self.player).place_locked_item(
+            self.create_item("Cursed Seal"))
+        self.world.get_location("Agate Knife", self.player).place_locked_item(
+            self.create_item("Agate Knife"))
+
+        if not self.world.include_psi_keys[self.player]:
+            location_count -= 3
+            psi_keys = []
+            psi_key_storage = []
+            for i in range(0, 3):
+                frequencies[i + 12] = 0
+                psi_keys += self.create_item(f"PSI Key {i + 1}")
+                psi_key_storage += self.world.get_location(
+                    f"PSI Key Storage {i + 1}", self.player)
+
+            fill_restrictive(self.world, self.world.get_all_state(
+                False), psi_key_storage, psi_keys)
+
+        if not self.world.include_evo_traps[self.player]:
+            frequencies[17] = 0
+            location_count -= 3
+            for boss in ["Meridian", "Ataraxia", "Merodach"]:
+                self.world.get_location(boss, self.player).place_locked_item(
+                    self.create_item("Evolution Trap"))
 
         for i, name in enumerate(item_table):
             if (i < len(frequencies)):
