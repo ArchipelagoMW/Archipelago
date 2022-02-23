e1e9f9393b6c6ecf9c88dd38922c2196759c3fbb
Felix <felicitusneko@vivaldi.net>
2022-02-23 18:03:41 -0400

    meritous: prevent PSI Keys from being on any boss

--- worlds/meritous/Rules.py
+++ worlds/meritous/Rules.py
@@ -7,11 +7,11 @@ from ..generic.Rules import forbid_item, exclusion_rules
 
 
 def set_rules(world, player):
-    # Prevent PSI keys from showing up in respective boss' room
-    # Should be prevented by region generation but let's be sure
-    forbid_item(world.get_location("Meridian", player), "PSI Key 1", player)
-    forbid_item(world.get_location("Ataraxia", player), "PSI Key 2", player)
-    forbid_item(world.get_location("Merodach", player), "PSI Key 3", player)
+    # Prevent PSI keys from showing up in any boss' room
+    # This is to prevent softlock from ending up having to fight a boss in the wrong boss room
+    for boss in ["Meridian", "Ataraxia", "Merodach"]:
+        for key in range(1, 4):
+            forbid_item(world.get_location(boss, player), f"PSI Key {key}", player)
 
     # Prevent progression from showing up in last six checks per store
     # This is to prevent softlock from high prices or low chest drop
