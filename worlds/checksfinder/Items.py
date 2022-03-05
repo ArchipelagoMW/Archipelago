cd327776c95af61252a19f99775d8b6f01c59ae3
jonloveslegos <jonloveslegos@gmail.com>
2022-03-05 14:48:57 -0500

    fixed some bugs, but it still wont send items

--- worlds/checksfinder/Items.py
+++ worlds/checksfinder/Items.py
@@ -13,8 +13,8 @@ class ChecksFinderItem(Item):
 
 item_table = {
     "Map Width": ItemData(80000, True),
-    "Map Height": ItemData(80000, True),
-    "Map Bombs": ItemData(80000, True),
+    "Map Height": ItemData(80001, True),
+    "Map Bombs": ItemData(80002, True),
 }
 
 # 33 required items
@@ -22,7 +22,7 @@ required_items = {
 }
 
 item_frequencies = {
-    
+
 }
 
 lookup_id_to_name: typing.Dict[int, str] = {data.code: item_name for item_name, data in item_table.items() if data.code}
--- worlds/checksfinder/Locations.py
+++ worlds/checksfinder/Locations.py
@@ -16,31 +16,31 @@ class ChecksFinderAdvancement(Location):
 
 
 advancement_table = {
-    "Tile 1": AdvData(80000, 'Overworld'),
-    "Tile 2": AdvData(80001, 'Overworld'),
-    "Tile 3": AdvData(80002, 'Overworld'),
-    "Tile 4": AdvData(80003, 'Overworld'),
-    "Tile 5": AdvData(80004, 'Overworld'),
-    "Tile 6": AdvData(80005, 'Overworld'),
-    "Tile 7": AdvData(80006, 'Overworld'),
-    "Tile 8": AdvData(80007, 'Overworld'),
-    "Tile 9": AdvData(80008, 'Overworld'),
-    "Tile 10": AdvData(80009, 'Overworld'),
-    "Tile 11": AdvData(80010, 'Overworld'),
-    "Tile 12": AdvData(80011, 'Overworld'),
-    "Tile 13": AdvData(80012, 'Overworld'),
-    "Tile 14": AdvData(80013, 'Overworld'),
-    "Tile 15": AdvData(80014, 'Overworld'),
-    "Tile 16": AdvData(80015, 'Overworld'),
-    "Tile 17": AdvData(80016, 'Overworld'),
-    "Tile 18": AdvData(80017, 'Overworld'),
-    "Tile 19": AdvData(80018, 'Overworld'),
-    "Tile 20": AdvData(80019, 'Overworld'),
-    "Tile 21": AdvData(80020, 'Overworld'),
-    "Tile 22": AdvData(80021, 'Overworld'),
-    "Tile 23": AdvData(80022, 'Overworld'),
-    "Tile 24": AdvData(80023, 'Overworld'),
-    "Tile 25": AdvData(80024, 'Overworld'),
+    "Tile 1": AdvData(81000, 'Overworld'),
+    "Tile 2": AdvData(81001, 'Overworld'),
+    "Tile 3": AdvData(81002, 'Overworld'),
+    "Tile 4": AdvData(81003, 'Overworld'),
+    "Tile 5": AdvData(81004, 'Overworld'),
+    "Tile 6": AdvData(81005, 'Overworld'),
+    "Tile 7": AdvData(81006, 'Overworld'),
+    "Tile 8": AdvData(81007, 'Overworld'),
+    "Tile 9": AdvData(81008, 'Overworld'),
+    "Tile 10": AdvData(81009, 'Overworld'),
+    "Tile 11": AdvData(81010, 'Overworld'),
+    "Tile 12": AdvData(81011, 'Overworld'),
+    "Tile 13": AdvData(81012, 'Overworld'),
+    "Tile 14": AdvData(81013, 'Overworld'),
+    "Tile 15": AdvData(81014, 'Overworld'),
+    "Tile 16": AdvData(81015, 'Overworld'),
+    "Tile 17": AdvData(81016, 'Overworld'),
+    "Tile 18": AdvData(81017, 'Overworld'),
+    "Tile 19": AdvData(81018, 'Overworld'),
+    "Tile 20": AdvData(81019, 'Overworld'),
+    "Tile 21": AdvData(81020, 'Overworld'),
+    "Tile 22": AdvData(81021, 'Overworld'),
+    "Tile 23": AdvData(81022, 'Overworld'),
+    "Tile 24": AdvData(81023, 'Overworld'),
+    "Tile 25": AdvData(81024, 'Overworld'),
 }
 
 exclusion_table = {
@@ -48,3 +48,5 @@ exclusion_table = {
 
 events_table = {
 }
+
+lookup_id_to_name: typing.Dict[int, str] = {data.id: item_name for item_name, data in advancement_table.items() if data.id}
