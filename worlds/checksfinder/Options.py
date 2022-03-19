765e558c4e5dfe406aa5883b6bf27458f263a372
jonloveslegos <jonloveslegos@gmail.com>
2022-03-19 08:58:35 -0400

    Made the requested change

--- worlds/checksfinder/Options.py
+++ worlds/checksfinder/Options.py
@@ -1,34 +1,5 @@
 import typing
-from Options import Choice, Option, Toggle, Range, OptionList, DeathLink
-
-
-class MaxWidth(Range):
-    """The goal width of the board."""
-    display_name = "Goal Width"
-    range_start = 10
-    range_end = 25
-    default = 10
-
-
-class MaxHeight(Range):
-    """The goal height of the board."""
-    display_name = "Goal Height"
-    range_start = 10
-    range_end = 25
-    default = 10
-
-
-class MaxBombs(Range):
-    """The goal amount of bombs on the board."""
-    display_name = "Goal Bombs"
-    range_start = 10
-    range_end = 40
-    default = 20
-
-
-class noneOption(Toggle):
-    """There are no options."""
-    display_name = "None"
+from Options import Option
 
 
 checksfinder_options: typing.Dict[str, type(Option)] = {
