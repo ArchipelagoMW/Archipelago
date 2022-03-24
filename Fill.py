cb024b00d90c0fd6078d8116615385fb9efd15aa
Fabian Dill <Berserker66@users.noreply.github.com>
2022-03-24 17:47:20 +0100

    Fill: don't crash before debug output in case of unfilled locations (#342)

--- Fill.py
+++ Fill.py
@@ -224,7 +224,7 @@ def distribute_items_restrictive(world: MultiWorld):
     if unplaced or unfilled:
         logging.warning(
             f'Unplaced items({len(unplaced)}): {unplaced} - Unfilled Locations({len(unfilled)}): {unfilled}')
-        items_counter = Counter([location.item.player for location in world.get_locations()])
+        items_counter = Counter([location.item.player for location in world.get_locations() if location.item])
         locations_counter = Counter([location.player for location in world.get_locations()])
         items_counter.update([item.player for item in unplaced])
         locations_counter.update([location.player for location in unfilled])
