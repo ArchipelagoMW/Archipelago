73aded6adc019800f176020979ca5944185bc00b
Chris Wilson <chris@legendserver.info>
2022-03-26 17:25:36 -0400

    [WebHost] Update background images, waiting on jungle.png, added partyTime theme

--- worlds/AutoWorld.py
+++ worlds/AutoWorld.py
@@ -79,7 +79,7 @@ class WebWorld:
     settings_page: Union[bool, str] = True
 
     # Choose a theme for your /game/* pages
-    # Available: dirt, grass, grassFlowers, ice, jungle, ocean
+    # Available: dirt, grass, grassFlowers, ice, jungle, ocean, partyTime
     theme = "grass"
 
 
