9493bb0f8400ad38dfd71edf3c87ba2e8ec3141e
Fabian Dill <fabian.dill@web.de>
2022-04-04 00:48:33 +0200

    Core: update version

--- Utils.py
+++ Utils.py
@@ -25,7 +25,7 @@ class Version(typing.NamedTuple):
     build: int
 
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 version_tuple = tuplize_version(__version__)
 
 from yaml import load, dump, SafeLoader
