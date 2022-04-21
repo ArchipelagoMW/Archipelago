a697d97a1c005234bf1dc19f20e3d42f02457c27
RaspberrySpaceJam <tyler.summers@gmail.com>
2022-04-20 21:55:47 -0700

    In progress work on gate emblem restriction logic

--- worlds/sa2b/__init__.py
+++ worlds/sa2b/__init__.py
@@ -6,7 +6,7 @@ from BaseClasses import Item, MultiWorld
 from .Items import SA2BItem, ItemData, item_table, upgrades_table
 from .Locations import SA2BLocation, all_locations, setup_locations
 from .Options import sa2b_options
-from .Regions import create_regions, shuffleable_regions, connect_regions, LevelGate
+from .Regions import create_regions, shuffleable_regions, connect_regions, LevelGate, gate_0_whitelist_regions, gate_0_blacklist_regions
 from .Rules import set_rules
 from .Names import ItemName
 from ..AutoWorld import World
@@ -60,6 +60,23 @@ class SA2BWorld(World):
 
         return slot_data
 
+    def check_for_impossible_shuffle(self, shuffled_levels: typing.List[int], gate_0_range: int, world: MultiWorld):
+        blacklist_level_count = 0
+
+        for i in range(gate_0_range):
+            if shuffled_levels[i] in gate_0_blacklist_regions:
+                blacklist_level_count += 1
+
+        if blacklist_level_count == gate_0_range:
+            index_to_swap = world.random.randint(0, gate_0_range)
+            for i in range(len(shuffled_levels)):
+                if shuffled_levels[i] in gate_0_whitelist_regions:
+                    shuffled_levels[i], shuffled_levels[index_to_swap] = shuffled_levels[index_to_swap], shuffled_levels[i]
+                    break
+
+    def get_levels_per_gate(self, world: MultiWorld, player) -> list:
+        return list()
+
     def generate_basic(self):
         itempool: typing.List[SA2BItem] = []
 
@@ -88,21 +105,26 @@ class SA2BWorld(World):
         emblem_requirement_list = list()
         self.world.random.shuffle(shuffled_region_list)
         levels_per_gate = 30 / (self.world.NumberOfLevelGates[self.player] + 1)
-        levels_added = 0
+
+        self.check_for_impossible_shuffle(shuffled_region_list, math.ceil(levels_per_gate), self.world)
+        levels_added_to_gate = 0
+        total_levels_added = 0
         current_gate = 0
         gates = list()
         gates.append(LevelGate(0))
         for i in range(30):
             gates[current_gate].gate_levels.append(shuffled_region_list[i])
             emblem_requirement_list.append(current_gate)
-            levels_added += 1
-            if levels_added >= levels_per_gate:
+            levels_added_to_gate += 1
+            total_levels_added += 1
+            if levels_added_to_gate >= levels_per_gate:
                 current_gate += 1
-                if current_gate > 5:
-                    current_gate = 5
+                if current_gate > self.world.NumberOfLevelGates[self.player]:
+                    current_gate = self.world.NumberOfLevelGates[self.player];
                 else:
-                    gates.append(LevelGate(current_gate))
-                levels_added = 0
+                    print(total_emblem_count * math.pow(total_levels_added / 30.0, 2.0))
+                    gates.append(LevelGate(max(math.floor(total_emblem_count * math.pow(total_levels_added / 30.0, 2.0)), current_gate)))
+                levels_added_to_gate = 0
 
         self.region_emblem_map = dict(zip(shuffled_region_list, emblem_requirement_list))
 
@@ -127,7 +149,10 @@ class SA2BWorld(World):
 
     def create_item(self, name: str) -> Item:
         data = item_table[name]
-        return SA2BItem(name, data.progression, data.code, self.player)
+        created_item = SA2BItem(name, data.progression, data.code, self.player)
+        if name == ItemName.emblem:
+            created_item.skip_in_prog_balancing = True
+        return created_item
 
     def set_rules(self):
         set_rules(self.world, self.player)
