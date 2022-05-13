89b4060a060af7deaa8b2b9a6a5ac1f0a8fbb65a
CaitSith2 <d_good@caitsith2.com>
2022-05-13 08:19:12 -0700

    Fix Plando options pickling error

--- WebHostLib/api/generate.py
+++ WebHostLib/api/generate.py
@@ -50,6 +50,7 @@ def generate_api():
             return {"text": str(results),
                     "detail": results}, 400
         else:
+            meta["plando_options"] = list(meta.get("plando_options", []))
             gen = Generation(
                 options=pickle.dumps({name: vars(options) for name, options in gen_options.items()}),
                 # convert to json compatible
