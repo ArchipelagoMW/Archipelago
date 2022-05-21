e1926c973e478639431239edcaf4a73916c00170
Alchav <59858495+Alchav@users.noreply.github.com>
2022-05-21 14:52:00 -0400

    [SC2] Item name groups and item game name fix (#555)

--- worlds/sc2wol/Items.py
+++ worlds/sc2wol/Items.py
@@ -1,6 +1,7 @@
 from BaseClasses import Item
 import typing
 
+
 class ItemData(typing.NamedTuple):
     code: typing.Optional[int]
     type: typing.Optional[str]
@@ -11,7 +12,7 @@ class ItemData(typing.NamedTuple):
 
 
 class StarcraftWoLItem(Item):
-    game: str = "Starcraft2WoL"
+    game: str = "Starcraft 2 Wings of Liberty"
 
     def __init__(self, name, advancement: bool = False, code: int = None, player: int = None):
         super(StarcraftWoLItem, self).__init__(name, advancement, code, player)
@@ -153,13 +154,15 @@ basic_unit: typing.Tuple[str, ...] = (
 )
 
 
-item_name_groups = {"Missions":
-                        {"Beat Liberation Day", "Beat The Outlaws", "Beat Zero Hour", "Beat Evacuation",
+item_name_groups = {}
+for item, data in item_table.items():
+    item_name_groups.setdefault(data.type, []).append(item)
+item_name_groups["Missions"] = ["Beat Liberation Day", "Beat The Outlaws", "Beat Zero Hour", "Beat Evacuation",
                          "None Outbreak", "Beat Safe Haven", "Beat Haven's Fall", "Beat Smash and Grab", "Beat The Dig",
                          "Beat The Moebius Factor", "Beat Supernova", "Beat Maw of the Void", "Beat Devil's Playground",
                          "Beat Welcome to the Jungle", "Beat Breakout", "Beat Ghost of a Chance",
                          "Beat The Great Train Robbery", "Beat Cutthroat", "Beat Engine of Destruction",
-                         "Beat Media Blitz", "Beat Piercing the Shroud"}}
+                         "Beat Media Blitz", "Beat Piercing the Shroud"]
 
 filler_items: typing.Tuple[str, ...] = (
     '+5 Starting Minerals',
