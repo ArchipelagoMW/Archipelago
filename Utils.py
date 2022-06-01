66c096ee211a2674647871751cc623537345f26b
strotlog <49286967+strotlog@users.noreply.github.com>
2022-06-01 23:49:57 +0000

    config: Use valid default enemizer_path on Linux (and Windows)

--- Utils.py
+++ Utils.py
@@ -255,7 +255,7 @@ def get_default_options() -> dict:
         },
         "generator": {
             "teams": 1,
-            "enemizer_path": os.path.join("EnemizerCLI", "EnemizerCLI.Core.exe"),
+            "enemizer_path": os.path.join("EnemizerCLI", "EnemizerCLI.Core"),
             "player_files_path": "Players",
             "players": 0,
             "weights_file_path": "weights.yaml",
