08d51a422d8bcb53dfae05c15a8f0add29832f86
alwaysintreble <mmmcheese158@gmail.com>
2022-06-09 12:47:51 -0500

    tutorials: ignore hidden worlds unless it's 'Archipelago'

--- WebHost.py
+++ WebHost.py
@@ -46,9 +46,7 @@ def create_ordered_tutorials_file() -> typing.List[typing.Dict[str, typing.Any]]
     worlds = {}
     data = []
     for game, world in AutoWorldRegister.world_types.items():
-        if hasattr(world.web, 'tutorials'):
-            if game == 'ArchipIDLE' and world.hidden:
-                continue
+        if hasattr(world.web, 'tutorials') and not world.hidden or game == 'Archipelago':
             worlds[game] = world
     for game, world in worlds.items():
         # copy files from world's docs folder to the generated folder
