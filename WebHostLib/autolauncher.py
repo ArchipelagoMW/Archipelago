89e097c543cfa11338ee0c339f8c108b9bbf5007
Fabian Dill <fabian.dill@web.de>
2022-06-09 12:07:22 +0200

    WebHostLib: wrap resource import into try to restore windows compatibility

--- WebHostLib/autolauncher.py
+++ WebHostLib/autolauncher.py
@@ -210,11 +210,15 @@ def run_guardian():
     global multiworlds
     with guardian_lock:
         if not guardian:
-            import resource
-            # Each Server is another file handle, so request as many as we can from the system
-            file_limit = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
-            # set soft limit to hard limit
-            resource.setrlimit(resource.RLIMIT_NOFILE, (file_limit, file_limit))
+            try:
+                import resource
+            except ModuleNotFoundError:
+                pass  # unix only module
+            else:
+                # Each Server is another file handle, so request as many as we can from the system
+                file_limit = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+                # set soft limit to hard limit
+                resource.setrlimit(resource.RLIMIT_NOFILE, (file_limit, file_limit))
 
             def guard():
                 while 1:
