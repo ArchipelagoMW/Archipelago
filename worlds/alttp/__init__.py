9d8756b0695e71ba64149b5969f0fd8e5b626b78
Fabian Dill <fabian.dill@web.de>
2022-06-14 05:06:40 +0200

    LttP: fix Ganon's Tower trash prefill ignoring item_rules

--- worlds/alttp/__init__.py
+++ worlds/alttp/__init__.py
@@ -472,13 +472,14 @@ class ALTTPWorld(World):
                 while gtower_locations and gt_item_pool and trash_count > 0:
                     spot_to_fill = gtower_locations.pop()
                     item_to_place = gt_item_pool.pop()
-                    if item_to_place in localrest:
-                        localrest.remove(item_to_place)
-                    else:
-                        restitempool.remove(item_to_place)
-                    world.push_item(spot_to_fill, item_to_place, False)
-                    fill_locations.remove(spot_to_fill)  # very slow, unfortunately
-                    trash_count -= 1
+                    if spot_to_fill.can_fill(world.state, item_to_place, False):
+                        if item_to_place in localrest:
+                            localrest.remove(item_to_place)
+                        else:
+                            restitempool.remove(item_to_place)
+                        world.push_item(spot_to_fill, item_to_place, False)
+                        fill_locations.remove(spot_to_fill)  # very slow, unfortunately
+                        trash_count -= 1
 
 
     def get_filler_item_name(self) -> str:
