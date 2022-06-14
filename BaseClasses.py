bad2a2b1aba4cba8bbd2091a2004d56819d7aa01
Fabian Dill <fabian.dill@web.de>
2022-06-15 00:48:32 +0200

    Core: move ItemClassification to Flag from Enum

--- BaseClasses.py
+++ BaseClasses.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
 import copy
-import warnings
-from enum import unique, IntEnum
+from enum import unique, IntEnum, IntFlag
 import logging
 import json
 import functools
@@ -1139,14 +1138,15 @@ class Location:
         return "at " + self.name.replace("_", " ").replace("-", " ")
 
 
-class ItemClassification(IntEnum):
+class ItemClassification(IntFlag):
     filler = 0b0000  # aka trash, as in filler items like ammo, currency etc,
-    trap = 0b0100  # detrimental or entirely useless (nothing) item
-    useful = 0b0010  # Item that is generally quite useful, but not required for anything logical
     progression = 0b0001  # Item that is logically relevant
+    useful = 0b0010  # Item that is generally quite useful, but not required for anything logical
+    trap = 0b0100  # detrimental or entirely useless (nothing) item
+    skip_balancing = 0b1000  # should technically never occur on its own
     # Item that is logically relevant, but progression balancing should not touch.
     # Typically currency or other counted items.
-    progression_skip_balancing = 0b1001
+    progression_skip_balancing = 0b1001  # only progression gets balanced
 
     def as_flag(self) -> int:
         """As Network API flag int."""
