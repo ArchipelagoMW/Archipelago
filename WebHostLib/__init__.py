5c05663c1ed59fd734c29099527666f195bc933d
alwaysintreble <mmmcheese158@gmail.com>
2022-06-19 18:35:29 -0500

    docs: add common terms documentation to website

--- WebHostLib/__init__.py
+++ WebHostLib/__init__.py
@@ -141,6 +141,11 @@ def faq(lang):
     return render_template("faq.html", lang=lang)
 
 
+@app.route('/common-terms/<string:lang>/')
+def terms(lang):
+    return render_template("commonterms.html", lang=lang)
+
+
 @app.route('/seed/<suuid:seed>')
 def view_seed(seed: UUID):
     seed = Seed.get(id=seed)
