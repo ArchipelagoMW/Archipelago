95c1d6b6bf1ca36367ced8a824e3b916cd7bb892
Fabian Dill <Berserker66@users.noreply.github.com>
2022-06-21 20:41:34 +0200

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -506,6 +506,7 @@ def chaining_prefix(index: int, labels: typing.Tuple[str]) -> str:
 
 # noinspection PyPep8Naming
 def format_SI_prefix(value, power=1000, power_labels=('', 'k', 'M', 'G', 'T', "P", "E", "Z", "Y")) -> str:
+    """Formats a value into a value + metric/si prefix. More info at https://en.wikipedia.org/wiki/Metric_prefix"""
     n = 0
     value = decimal.Decimal(value)
     while value >= power:
