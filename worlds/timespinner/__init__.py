753e2a4ea1ab49120df041213632c34968753734
TriumphantBass <32756996+TriumphantBass@users.noreply.github.com>
2022-06-23 17:20:47 -0700

    [Timespinner] Rename flag and add tiered loot settings

--- worlds/timespinner/__init__.py
+++ worlds/timespinner/__init__.py
@@ -228,7 +228,7 @@ def create_item_with_correct_settings(world: MultiWorld, player: int, name: str)
 
     if (name == 'Tablet' or name == 'Library Keycard V') and not is_option_enabled(world, player, "DownloadableItems"):
         item.classification = ItemClassification.filler
-    elif name == 'Oculus Ring' and not is_option_enabled(world, player, "FacebookMode"):
+    elif name == 'Oculus Ring' and not is_option_enabled(world, player, "EyeSpy"):
         item.classification = ItemClassification.filler
     elif (name == 'Kobo' or name == 'Merchant Crow') and not is_option_enabled(world, player, "GyreArchives"):
         item.classification = ItemClassification.filler
