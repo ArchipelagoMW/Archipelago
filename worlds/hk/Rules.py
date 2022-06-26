267cfd6d4aa2e35a89be8ac320a0767536a37f4c
Daniel Grace <thisgenericname@gmail.com>
2022-06-25 22:12:02 -0700

    HK: Costsanity!
    When Costsanity is enabled, ALL locations that have a cost are subject to
    a randomization pass.  This means stag stations might require grubs
    and other shenanigans.
    
    Grubfather, Seer and Egg_Shop cannot have geo costs.
    
    Which cost types are chosen is based off of weighted YAML settings.

--- worlds/hk/Rules.py
+++ worlds/hk/Rules.py
@@ -10,14 +10,16 @@ class CostTerm(NamedTuple):
     option: str
     singular: str
     plural: str
+    weight: int  # CostSanity
+    sort: int
 
 
 cost_terms = {x.term: x for x in (
-    CostTerm("RANCIDEGGS", "Egg", "Rancid Egg", "Rancid Eggs"),
-    CostTerm("GRUBS", "Grub", "Grub", "Grubs"),
-    CostTerm("ESSENCE", "Essence", "Essence", "Essence"),
-    CostTerm("CHARMS", "Charm", "Charm", "Charms"),
-    CostTerm("GEO", "Geo", "Geo", "Geo"),
+    CostTerm("RANCIDEGGS", "Egg", "Rancid Egg", "Rancid Eggs", 1, 3),
+    CostTerm("GRUBS", "Grub", "Grub", "Grubs", 1, 2),
+    CostTerm("ESSENCE", "Essence", "Essence", "Essence", 1, 4),
+    CostTerm("CHARMS", "Charm", "Charm", "Charms", 1, 1),
+    CostTerm("GEO", "Geo", "Geo", "Geo", 8, 9999),
 )}
 
 
