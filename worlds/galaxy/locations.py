029f2ebc1997ebc22c667f8971ee47c6f8136575
Joethepic <60947591+Joethepic@users.noreply.github.com>
2022-07-02 09:16:16 -0500

    galaxy basic files and GE locations

---
+++ worlds/galaxy/locations.py
@@ -0,0 +1,13 @@
+from BaseClasses import Location
+
+class SMGLocation(Location):
+    game: str = "Super Mario Galaxy"
+
+locGE_table = {
+"GE: Dino Piranha": 170000000,
+"GE: A Snack of Cosmic Proportions": 170000001,
+"GE: King Kaliente's Battle Fleet": 170000002,
+"GE: Dino Piranha Speed Run": 170000003,
+"GE: Purple Coin Omelet": 170000004,
+"GE: Luigi on the Roof": 170000005,
+}
