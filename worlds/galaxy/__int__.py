6de6fc21eebe2921ba252b917d04feb3d29d69d0
Joethepic <60947591+Joethepic@users.noreply.github.com>
2022-07-05 10:36:32 -0500

    add int.py

---
+++ worlds/galaxy/__int__.py
@@ -0,0 +1,11 @@
+import os
+import json
+from base64 import b64encode, b64decode
+from math import ceil
+from .Items import genric_item_table
+from .Locations import locHH_table, locspecialstages_table, locGE_table, locbosses_table, locSJ_table, locBR_table
+from .Regions import 
+from .Rules import 
+from BaseClasses import Region, Entrance, Item, Tutorial, ItemClassification
+from .Options import 
+from ..AutoWorld import World, WebWorld
