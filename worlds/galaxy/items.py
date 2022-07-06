31d716c3ff1f2bac6f40ce1447bb445008952136
Joethepic <60947591+Joethepic@users.noreply.github.com>
2022-07-06 13:00:38 -0500

    finish items.py

--- worlds/galaxy/items.py
+++ worlds/galaxy/items.py
@@ -8,4 +8,8 @@ class SMGItem(Item):
 genric_item_table = {
   "Power Star": 8010,
   "Grand Star Terrace": 8011,
+  "Grand Star Fountain": 8012,
+  "Grand Star Kitchen": 8013,
+  "Grand Star Bedroom": 8014,
+  "Grand Star Engine Room": 8015,
 }
