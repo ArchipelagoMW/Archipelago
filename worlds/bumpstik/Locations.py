3e6bdeeb00961699e0aa1a155c3fc67d0e67b8d0
Felix R <felicitusneko@duck.com>
2022-07-20 21:53:48 -0300

    bumpstik: add more score-based locations

--- worlds/bumpstik/Locations.py
+++ worlds/bumpstik/Locations.py
@@ -13,10 +13,11 @@ class BumpStikLocation(Location):
 offset = 595_000
 
 location_table = {
-    **{f"{(i + 1) * 500} Points": offset + i for i in range(0, 8)},
-    **{f"Combo Clear {i + 4}": offset + 8 + i for i in range(0, 3)},
-    **{f"Chain x{i + 2}": offset + 11 + i for i in range(0, 2)},
-    "All Clear": offset + 13,
-    **{f"Booster Bumper {i + 1}": offset + 14 + i for i in range(0, 5)},
-    "Cleared All Hazards": offset + 19
+    **{f"{(i * 500) + 250} Points": offset + (i * 2) for i in range(0, 8)},
+    **{f"{(i + 1) * 500} Points": offset + (i * 2) + 1 for i in range(0, 8)},
+    **{f"Combo Clear {i + 4}": offset + 16 + i for i in range(0, 3)},
+    **{f"Chain x{i + 2}": offset + 19 + i for i in range(0, 2)},
+    "All Clear": offset + 21,
+    **{f"Booster Bumper {i + 1}": offset + 22 + i for i in range(0, 5)},
+    "Cleared All Hazards": offset + 27
 }
