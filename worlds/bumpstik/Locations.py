2efcbe31e93c04f0c646ff4b9a967a776a6f336e
Felix R <felicitusneko@duck.com>
2022-07-24 17:25:43 -0300

    bumpstik: treasure bumper locations

--- worlds/bumpstik/Locations.py
+++ worlds/bumpstik/Locations.py
@@ -13,11 +13,12 @@ class BumpStikLocation(Location):
 offset = 595_000
 
 location_table = {
-    **{f"{(i * 500) + 250} Points": offset + (i * 2) for i in range(0, 8)},
-    **{f"{(i + 1) * 500} Points": offset + (i * 2) + 1 for i in range(0, 8)},
-    **{f"Combo Clear {i + 4}": offset + 16 + i for i in range(0, 3)},
-    **{f"Chain x{i + 2}": offset + 19 + i for i in range(0, 2)},
+    **{f"{(i * 500) + 250} Points": offset + (i * 2) for i in range(8)},
+    **{f"{(i + 1) * 500} Points": offset + (i * 2) + 1 for i in range(8)},
+    **{f"Combo Clear {i + 4}": offset + 16 + i for i in range(3)},
+    **{f"Chain x{i + 2}": offset + 19 + i for i in range(2)},
     "All Clear": offset + 21,
-    **{f"Booster Bumper {i + 1}": offset + 22 + i for i in range(0, 5)},
-    "Cleared All Hazards": offset + 27
+    **{f"Booster Bumper {i + 1}": offset + 22 + i for i in range(5)},
+    "Cleared All Hazards": offset + 27,
+    **{f"Treasure Bumper {i + 1}": offset + 28 + i for i in range(8)},
 }
--- worlds/bumpstik/Regions.py
+++ worlds/bumpstik/Regions.py
@@ -13,10 +13,22 @@ def _generate_entrances(player: int, entrance_list: [str], parent: Region):
 
 def create_regions(world: MultiWorld, player: int):
     region_map = {
-        "Menu": ["250 Points", "500 Points", "750 Points", "1000 Points", "Booster Bumper 1", "Booster Bumper 2"],
-        "Level 1": ["1250 Points", "1500 Points", "1750 Points", "2000 Points", "Combo Clear 4", "Chain x2", "Booster Bumper 3"],
-        "Level 2": ["2250 Points", "2500 Points", "2750 Points", "3000 Points", "Combo Clear 5", "All Clear", "Booster Bumper 4"],
-        "Level 3": ["3250 Points", "3500 Points", "3750 Points", "4000 Points", "Combo Clear 6", "Chain x3", "Booster Bumper 5"],
+        "Menu": [
+            "250 Points", "500 Points", "750 Points", "1000 Points", "Booster Bumper 1", "Booster Bumper 2",
+            "Treasure Bumper 1", "Treasure Bumper 2"
+            ],
+        "Level 1": [
+            "1250 Points", "1500 Points", "1750 Points", "2000 Points", "Combo Clear 4", "Chain x2",
+            "Booster Bumper 3", "Treasure Bumper 3", "Treasure Bumper 4"
+            ],
+        "Level 2": [
+            "2250 Points", "2500 Points", "2750 Points", "3000 Points", "Combo Clear 5", "All Clear",
+            "Booster Bumper 4", "Treasure Bumper 5", "Treasure Bumper 6"
+            ],
+        "Level 3": [
+            "3250 Points", "3500 Points", "3750 Points", "4000 Points", "Combo Clear 6", "Chain x3",
+            "Booster Bumper 5", "Treasure Bumper 7", "Treasure Bumper 8"
+            ],
         "Level 4": ["Cleared All Hazards"]
     }
 
