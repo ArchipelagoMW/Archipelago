23777811e344231cdcd2c6ed20924001f8b05d24
alwaysintreble <mmmcheese158@gmail.com>
2022-07-28 16:39:50 -0500

    rip out old boss shuffle spoiler code

--- BaseClasses.py
+++ BaseClasses.py
@@ -1435,7 +1435,6 @@ class Spoiler():
                                                "f" in self.world.shop_shuffle[player]))
                     outfile.write('Custom Potion Shop:              %s\n' %
                                   bool_to_text("w" in self.world.shop_shuffle[player]))
-                    outfile.write('Boss shuffle:                    %s\n' % self.world.boss_shuffle[player])
                     outfile.write('Enemy health:                    %s\n' % self.world.enemy_health[player])
                     outfile.write('Enemy damage:                    %s\n' % self.world.enemy_damage[player])
                     outfile.write('Prize shuffle                    %s\n' %
