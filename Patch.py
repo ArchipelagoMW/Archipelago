daeba66ea0ab07289a9071a5fda9e0e77fa3602d
Alchav <59858495+Alchav@users.noreply.github.com>
2022-08-24 22:36:41 -0400

    Save patch file extension in archipelago.json

--- Patch.py
+++ Patch.py
@@ -128,6 +128,7 @@ class APDeltaPatch(APContainer, metaclass=AutoPatchRegister):
         manifest = super(APDeltaPatch, self).get_manifest()
         manifest["base_checksum"] = self.hash
         manifest["result_file_ending"] = self.result_file_ending
+        manifest["patch_file_ending"] = self.patch_file_ending
         return manifest
 
     @classmethod
