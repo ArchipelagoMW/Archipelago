03f66a922d42f492ff751fede981d94e14dd4ee4
Yussur Mustafa Oraji <N00byKing@hotmail.de>
2022-09-01 21:21:53 +0200

    sm64ex: Fix a Location (#979)

--- worlds/sm64ex/__init__.py
+++ worlds/sm64ex/__init__.py
@@ -34,7 +34,7 @@ class SM64World(World):
     item_name_to_id = item_table
     location_name_to_id = location_table
 
-    data_version = 7
+    data_version = 8
     required_client_version = (0, 3, 0)
 
     area_connections: typing.Dict[int, int]
