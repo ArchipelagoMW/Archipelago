8e211c4fbadb8e47c42978ea4718430d726c6345
toasterparty <toasterparty@derpymail.org>
2022-09-03 13:12:24 -0700

    Update test/general/TestItems.py
    Co-authored-by: alwaysintreble <mmmcheese158@gmail.com>

--- test/general/TestItems.py
+++ test/general/TestItems.py
@@ -35,7 +35,6 @@ class TestBase(unittest.TestCase):
         for game_name, world_type in AutoWorldRegister.world_types.items():
 
             if game_name not in {"Archipelago", "Final Fantasy"}:
-                continue
 
             with self.subTest("Game", game=game_name):
                 world = setup_default_world(world_type)
