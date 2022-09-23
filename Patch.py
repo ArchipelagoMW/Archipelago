c6fc6a0345b7a03b5990cc0733613ef99617486d
PoryGone <porygoneplays@gmail.com>
2022-09-23 16:42:00 -0400

    Remove legacy patching from PR

--- Patch.py
+++ Patch.py
@@ -169,15 +169,14 @@ GAME_SOE = "Secret of Evermore"
 GAME_SMZ3 = "SMZ3"
 GAME_DKC3 = "Donkey Kong Country 3"
 GAME_SMW = "Super Mario World"
-supported_games = {"A Link to the Past", "Super Metroid", "Secret of Evermore", "SMZ3", "Donkey Kong Country 3", "Super Mario World"}
+supported_games = {"A Link to the Past", "Super Metroid", "Secret of Evermore", "SMZ3", "Donkey Kong Country 3"}
 
 preferred_endings = {
     GAME_ALTTP: "apbp",
     GAME_SM: "apm3",
     GAME_SOE: "apsoe",
     GAME_SMZ3: "apsmz",
-    GAME_DKC3: "apdkc3",
-    GAME_SMW: "apsmw"
+    GAME_DKC3: "apdkc3"
 }
 
 
@@ -194,8 +193,6 @@ def generate_yaml(patch: bytes, metadata: Optional[dict] = None, game: str = GAM
         HASH = ALTTPHASH + SMHASH
     elif game == GAME_DKC3:
         from worlds.dkc3.Rom import USHASH as HASH
-    elif game == GAME_SMW:
-        from worlds.smw.Rom import USHASH as HASH
     else:
         raise RuntimeError(f"Selected game {game} for base rom not found.")
 
@@ -228,7 +225,6 @@ def create_patch_file(rom_file_to_patch: str, server: str = "", destination: str
         ".apbp" if game == GAME_ALTTP
         else ".apsmz" if game == GAME_SMZ3
         else ".apdkc3" if game == GAME_DKC3
-        else ".apsmw" if game == GAME_SMW
         else ".apm3")
     write_lzma(bytes, target)
     return target
@@ -260,8 +256,6 @@ def get_base_rom_data(game: str):
         from worlds.smz3.Rom import get_base_rom_bytes
     elif game == GAME_DKC3:
         from worlds.dkc3.Rom import get_base_rom_bytes
-    elif game == GAME_SMW:
-        from worlds.smw.Rom import get_base_rom_bytes
     else:
         raise RuntimeError("Selected game for base rom not found.")
     return get_base_rom_bytes()
@@ -413,13 +407,6 @@ if __name__ == "__main__":
                     if 'server' in data:
                         Utils.persistent_store("servers", data['hash'], data['server'])
                         print(f"Host is {data['server']}")
-                elif rom.endswith(".apsmw"):
-                    print(f"Applying patch {rom}")
-                    data, target = create_rom_file(rom)
-                    print(f"Created rom {target}.")
-                    if 'server' in data:
-                        Utils.persistent_store("servers", data['hash'], data['server'])
-                        print(f"Host is {data['server']}")
 
                 elif rom.endswith(".zip"):
                     print(f"Updating host in patch files contained in {rom}")
@@ -429,8 +416,7 @@ if __name__ == "__main__":
                         data = zfr.read(zfinfo)
                         if zfinfo.filename.endswith(".apbp") or \
                            zfinfo.filename.endswith(".apm3") or \
-                           zfinfo.filename.endswith(".apdkc3") or \
-                           zfinfo.filename.endswith(".apsmw"):
+                           zfinfo.filename.endswith(".apdkc3"):
                             data = update_patch_data(data, server)
                         with ziplock:
                             zfw.writestr(zfinfo, data)
