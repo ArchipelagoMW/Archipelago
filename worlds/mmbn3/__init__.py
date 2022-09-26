a669083b5afc64503a4304b1a73bc850be99e62a
digiholic <digikun@gmail.com>
2022-09-25 18:40:45 -0600

    Adds regions and item collection rules

--- worlds/mmbn3/__init__.py
+++ worlds/mmbn3/__init__.py
@@ -4,11 +4,14 @@ import math
 import threading
 
 
-from BaseClasses import Item, MultiWorld, Tutorial, ItemClassification, Region, RegionType
+from BaseClasses import Item, MultiWorld, Tutorial, ItemClassification, Region, RegionType, Entrance
 from ..AutoWorld import WebWorld, World
 from .Items import MMBN3Item, ItemData, item_table, all_items
-from .Locations import MMBN3Location, all_locations, setup_locations
+from .Locations import MMBN3Location, all_locations, setup_locations, location_table
 from .Options import MMBN3Options
+from .Regions import regions
+from .Names import ItemName, LocationName
+from worlds.generic.Rules import add_rule, set_rule
 
 class MMBN3Web(WebWorld):
     theme = "ice"
@@ -65,9 +68,32 @@ class MMBN3World(World):
 
     def create_regions(self) -> None:
         """
-        called to place player's regions into the MultiWorld's regions list. If it's hard to separate, this can be done during generate_early or basic as well.
+        called to place player's regions into the MultiWorld's regions list. If it's hard to separate, this can be done
+        during generate_early or basic as well.
         """
-        pass
+        name_to_region = {}
+        for region_info in regions:
+            region = Region(region_info.name, RegionType.Generic, region_info.name, self.player, self.world)
+            name_to_region[region_info.name] = region
+            for location in region_info.locations:
+                region.locations.append(MMBN3Location(self.player, location,
+                                                      self.location_name_to_id[location], region))
+            self.world.regions.append(region)
+        for region_info in regions:
+            region = name_to_region[region_info.name]
+            for connection in region_info.connections:
+                connection_region = name_to_region[connection]
+                entrance = Entrance(self.player, connection, region)
+                entrance.connect(connection_region)
+                if connection == "Undernet":
+                    entrance.access_rule = lambda state: state.has(ItemName.HPMemory, self.player, 5)
+                if connection == "Secret Area":
+                    entrance.access_rule = lambda state: state.has(ItemName.HPMemory, self.player, 15)
+                if connection == "WWW Island":
+                    entrance.access_rule = lambda state:\
+                        state.has(ItemName.Progressive_Undernet_Rank, self.player, 8) and\
+                        state.has(ItemName.HPMemory, self.player, 10)
+                region.exits.append(entrance)
 
     def create_items(self) -> None:
         # First add in all progression and useful items
@@ -85,7 +111,29 @@ class MMBN3World(World):
         """
         called to set access and item rules on locations and entrances.
         """
-        pass
+        # Set WWW ID requirements
+        has_www_id = lambda state: state.has(ItemName.WWW_ID, self.player)
+        set_rule(self.world.get_location(LocationName.ACDC_1_PMD, self.player),
+                 has_www_id)
+        set_rule(self.world.get_location(LocationName.SciLab_1_WWW_BMD),
+                 has_www_id)
+        set_rule(self.world.get_location(LocationName.Yoka_1_WWW_BMD),
+                 has_www_id)
+        set_rule(self.world.get_location(LocationName.Undernet_1_WWW_BMD),
+                 has_www_id)
+
+        # Set Job additional area access
+        # TODO: Once we find a way to restrict overworld access
+
+        # Set Trade quests
+        set_rule(self.world.get_location(LocationName.Hades_GrabBack_K_Trade, self.player),
+                 lambda state: state.has(ItemName.GrabBack_K, self.player))
+        set_rule(self.world.get_location(LocationName.DNN_WideSwrd_C_Trade, self.player),
+                 lambda state: state.has(ItemName.WideSwrd_C, self.player))
+        set_rule(self.world.get_location(LocationName.Hospital_DynaWav_V_Trade, self.player),
+                 lambda state: state.has(ItemName.DynaWave_V, self.player))
+        set_rule(self.world.get_location(LocationName.ACDC_Recov120_S_Trade, self.player),
+                 lambda state: state.has(ItemName.Recov120_S, self.player))
 
     def generate_basic(self) -> None:
         """
