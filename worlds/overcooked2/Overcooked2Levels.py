56a26b18bdce0a2383bd448197b5757d939bc5ba
toasterparty <toasterparty@derpymail.org>
2022-09-27 16:51:30 -0700

    moon 1-1 is a prep level for 1P

--- worlds/overcooked2/Overcooked2Levels.py
+++ worlds/overcooked2/Overcooked2Levels.py
@@ -82,7 +82,8 @@ class Overcooked2Dlc(Enum):
         if self == Overcooked2Dlc.CARNIVAL_OF_CHAOS:
             return [0, 1, 3, 4, 5]
         if self == Overcooked2Dlc.SEASONAL:
-            return [0, 1, 5, 6, 12, 14, 16, 17, 18, 22, 23, 24, 29]
+            # moon 1-1 is a prep level for 1P only, but we can't make that assumption here
+            return [0, 1, 5, 6, 12, 14, 16, 17, 18, 22, 23, 24, 27, 29]
 
         return []
 
