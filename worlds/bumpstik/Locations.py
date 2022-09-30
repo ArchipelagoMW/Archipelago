0ea2419e85f0b62275f59fc0483fd9dafe4a7809
Felix R <felicitusneko@duck.com>
2022-09-30 18:56:54 -0300

    bumpstik: level 3 and 4 locations

--- worlds/bumpstik/Locations.py
+++ worlds/bumpstik/Locations.py
@@ -13,18 +13,23 @@ class BumpStikLocation(Location):
 offset = 595_000
 
 level1_locs = [f"{(i + 1) * 250} Points" for i in range(4)] + \
-    [f"{(i + 1) * 500} Total Points" for i in range(4)] + \
+    [f"{(i + 1) * 500} Level Points" for i in range(4)] + \
     ["Combo 5"]
 
 level2_locs = [f"{(i + 1) * 500} Points" for i in range(4)] + \
-    [f"{(i + 3) * 1000} Total Points" for i in range(4)] + \
+    [f"{(i + 1) * 1000} Level Points" for i in range(4)] + \
     ["Combo 5"] + ["Chain x2"]
 
-level3_locs = ["99999 Points (dummy goal)"]
+level3_locs = [f"{(i + 1) * 800} Points" for i in range(4)] + \
+    [f"{(i + 1) * 2000} Level Points" for i in range(4)] + \
+    ["Combo 5", "Combo 7"] + ["Chain x2"] + \
+    ["All Clear, 3 colors"]
 
-level4_locs = ["99999 Points (dummy goal)"]
+level4_locs = [f"{(i + 1) * 1500} Points" for i in range(4)] + \
+    [f"{(i + 1) * 4000} Level Points" for i in range(4)] + \
+    ["Combo 5", "Combo 7"] + ["Chain x2", "Chain x3"]
 
-level5_locs = ["Cleared all Hazards"]
+level5_locs = ["100,000 Total Points", "Cleared all Hazards"]
 
 for x, list in enumerate([level1_locs, level2_locs, level3_locs, level4_locs, level5_locs]):
     for y, loc in enumerate(list):
