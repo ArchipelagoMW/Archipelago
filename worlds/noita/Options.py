4867af6c0601db8c2894b6cc87c700692fafca58
Adam Heinermann <aheinerm@gmail.com>
2022-10-07 16:45:51 -0700

    Added deathlink

--- worlds/noita/Options.py
+++ worlds/noita/Options.py
@@ -1,5 +1,5 @@
 from typing import Dict
-from Options import Option, DefaultOnToggle, Range
+from Options import Option, DeathLink, DefaultOnToggle, Range
 
 class TotalLocations(Range):
     """Number of location checks which are added to the playthrough."""
@@ -12,8 +12,8 @@ class BadEffects(DefaultOnToggle):
     """Negative effects on the Noita world are added to the item pool."""
     display_name = "Bad Times"
 
-
 noita_options: Dict[str, type(Option)] = {
     "total_locations":      TotalLocations,
-    "bad_effects":          BadEffects
+    "bad_effects":          BadEffects,
+    "death_link":           DeathLink
 }
