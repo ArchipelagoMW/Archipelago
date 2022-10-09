85b49ac21cd4fc215600fa4599128da1fbc2b241
Alchav <59858495+Alchav@users.noreply.github.com>
2022-10-09 12:20:29 -0400

    Early Items option

--- Main.py
+++ Main.py
@@ -16,7 +16,7 @@ from worlds.alttp.Regions import is_main_entrance
 from Fill import distribute_items_restrictive, flood_items, balance_multiworld_progression, distribute_planned
 from worlds.alttp.Shops import SHOP_ID_START, total_shop_slots, FillDisabledShopSlots
 from Utils import output_path, get_options, __version__, version_tuple
-from worlds.generic.Rules import locality_rules, exclusion_rules, group_locality_rules
+from worlds.generic.Rules import locality_rules, exclusion_rules, group_locality_rules, early_item_rules
 from worlds import AutoWorld
 
 ordered_areas = (
@@ -133,6 +133,7 @@ def main(args, seed=None, baked_server_options: Optional[Dict[str, object]] = No
 
     for player in world.player_ids:
         exclusion_rules(world, player, world.exclude_locations[player].value)
+        early_item_rules(world, player, world.early_items[player].value)
         world.priority_locations[player].value -= world.exclude_locations[player].value
         for location_name in world.priority_locations[player].value:
             world.get_location(location_name, player).progress_type = LocationProgressType.PRIORITY
