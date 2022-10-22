724590a7dc3a01f40f4d86f909e185871859286d
beauxq <beauxq@yahoo.com>
2022-10-21 17:09:04 -0700

    better type narrowing

--- test/worlds/zillion/__init__.py
+++ test/worlds/zillion/__init__.py
@@ -1,4 +1,3 @@
-from typing import cast
 from test.worlds.test_base import WorldTestBase
 from worlds.zillion.region import ZillionLocation
 
@@ -10,6 +9,5 @@ class ZillionTestBase(WorldTestBase):
         super().world_setup()
         # make sure game requires gun 3 for tests
         for location in self.world.get_locations():
-            if location.name.startswith("O-7"):  # O-7 door is required
-                location = cast(ZillionLocation, location)
+            if isinstance(location, ZillionLocation) and location.name.startswith("O-7"):
                 location.zz_loc.req.gun = 3
