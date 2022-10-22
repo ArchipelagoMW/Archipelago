b41ea7af851e1a9381cfaf3377b2125d7d7345aa
beauxq <beauxq@yahoo.com>
2022-10-21 17:04:39 -0700

    tests: fix random failures on Zillion tests
    Normally there's a low probably that the game doesn't require a power-up that it usually requires.
    This makes sure it always has that requirement for tests.

--- test/worlds/zillion/__init__.py
+++ test/worlds/zillion/__init__.py
@@ -1,5 +1,15 @@
+from typing import cast
 from test.worlds.test_base import WorldTestBase
+from worlds.zillion.region import ZillionLocation
 
 
 class ZillionTestBase(WorldTestBase):
     game = "Zillion"
+
+    def world_setup(self) -> None:
+        super().world_setup()
+        # make sure game requires gun 3 for tests
+        for location in self.world.get_locations():
+            if location.name.startswith("O-7"):  # O-7 door is required
+                location = cast(ZillionLocation, location)
+                location.zz_loc.req.gun = 3
