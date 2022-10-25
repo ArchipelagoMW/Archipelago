d5efc713444dec62a5d398561303ff3edbf0db6f
PoryGone <98504756+PoryGone@users.noreply.github.com>
2022-10-25 13:54:43 -0400

    Core: SNI Client Refactor (#1083)
    * First Pass removal of game-specific code
    
    * SMW, DKC3, and SM hooked into AutoClient
    
    * All SNES autoclients functional
    
    * Fix ALttP Deathlink
    
    * Don't default to being ALttP, and properly error check ctx.game
    
    * Adjust variable naming
    
    * In response to:
    > we should probably document usage somewhere. I'm open to suggestions of where this should be documented.
    
    I think the most valuable documentation for APIs is docstrings and full typing.
    
    about websockets change in imports - from websockets documentation:
    > For convenience, many public APIs can be imported from the websockets package. However, this feature is incompatible with static code analysis. It breaks autocompletion in an IDE or type checking with mypy. If you’re using such tools, use the real import paths.
    
    * todo note for python 3.11
    typing.NotRequired
    
    * missed staging in previous commit
    
    * added missing death Game States for DeathLink
    
    Co-authored-by: beauxq <beauxq@users.noreply.github.com>
    Co-authored-by: lordlou <87331798+lordlou@users.noreply.github.com>

--- CommonClient.py
+++ CommonClient.py
@@ -91,12 +91,18 @@ class ClientCommandProcessor(CommandProcessor):
 
     def _cmd_items(self):
         """List all item names for the currently running game."""
+        if not self.ctx.game:
+            self.output("No game set, cannot determine existing items.")
+            return False
         self.output(f"Item Names for {self.ctx.game}")
         for item_name in AutoWorldRegister.world_types[self.ctx.game].item_name_to_id:
             self.output(item_name)
 
     def _cmd_locations(self):
         """List all location names for the currently running game."""
+        if not self.ctx.game:
+            self.output("No game set, cannot determine existing locations.")
+            return False
         self.output(f"Location Names for {self.ctx.game}")
         for location_name in AutoWorldRegister.world_types[self.ctx.game].location_name_to_id:
             self.output(location_name)
