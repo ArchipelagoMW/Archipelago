fe0540ed2a7938e0f4aa4d5b62649af6f9a8d3be
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2022-12-01 00:27:12 +0100

    Logic fix and datapackage ver

--- worlds/witness/__init__.py
+++ worlds/witness/__init__.py
@@ -38,7 +38,7 @@ class WitnessWorld(World):
     """
     game = "The Witness"
     topology_present = False
-    data_version = 9
+    data_version = 10
 
     static_logic = StaticWitnessLogic()
     static_locat = StaticWitnessLocations()
