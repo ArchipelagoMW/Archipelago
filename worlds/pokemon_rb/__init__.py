2cdd03f78624e07f2a943381500f93e8c3b5cb23
Fabian Dill <Berserker66@users.noreply.github.com>
2022-12-11 02:59:17 +0100

    Network: implement 0.4 marked compatibility removals (#757)
    * world remote items handling
    * players list when connecting

--- worlds/pokemon_rb/__init__.py
+++ worlds/pokemon_rb/__init__.py
@@ -38,9 +38,10 @@ class PokemonRedBlueWorld(World):
     # -MuffinJets#4559
     game = "Pokemon Red and Blue"
     option_definitions = pokemon_rb_options
-    remote_items = False
+
     data_version = 3
     required_client_version = (0, 3, 7)
+
     topology_present = False
 
 
