61d39e3a6838004f2237c806310c56c27d2a1088
Brooty Johnson <83629348+Br00ty@users.noreply.github.com>
2022-12-14 20:43:33 -0500

    Update locations_data.py
    add "Titanite Chunk" and "Twinkling Titanite" to progressive_locations_3

--- worlds/dark_souls_3/data/locations_data.py
+++ worlds/dark_souls_3/data/locations_data.py
@@ -584,6 +584,8 @@ progressive_locations_3 = {
     **{"Duel Charm #" + str(i): 0x40000130 for i in range(1, 3)},
     **{"Rusted Coin #" + str(i): 0x400001C7 for i in range(1, 3)},
     **{"Rusted Gold Coin #" + str(i): 0x400001C9 for i in range(1, 4)},
+    **{"Titanite Chunk #"+str(i): 0x400003EA for i in range(1, 17)},
+    **{"Twinkling Titanite #"+str(i): 0x40000406 for i in range(1, 8)}
 }
 
 
