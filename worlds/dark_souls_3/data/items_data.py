d2edb42210e1b10da9fa7763abbacc1791cb681b
Brooty Johnson <83629348+Br00ty@users.noreply.github.com>
2022-12-14 20:05:42 -0500

    Update items_data.py
    added "Contaption Key" and "Small Envoy Banner" to key_items_list

--- worlds/dark_souls_3/data/items_data.py
+++ worlds/dark_souls_3/data/items_data.py
@@ -570,7 +570,9 @@ key_items_list = {
     "Jailbreaker's Key",
     "Prisoner Chief's Ashes",
     "Old Cell Key",
-    "Jailer's Key Ring"
+    "Jailer's Key Ring",
+    "Contraption Key", # May not need
+    "Small Envoy Banner" # May not need
 }
 
 item_tables = [weapons_upgrade_5_table, weapons_upgrade_10_table, shields_table,
