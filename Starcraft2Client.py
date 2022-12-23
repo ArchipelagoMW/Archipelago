1753a8051e02fd7019d230f9726a522bac6afba9
SoldierofOrder <107806872+SoldierofOrder@users.noreply.github.com>
2022-12-23 14:11:57 -0500

    SC2Client: Fixed a missing space.

--- Starcraft2Client.py
+++ Starcraft2Client.py
@@ -202,7 +202,7 @@ class SC2Context(CommonContext):
                 if is_mod_update_available("TheCondor07", "Starcraft2ArchipelagoData", current_ver):
                     sc2_logger.info("NOTICE: Update for required files found. Run /download_data to install.")
             elif maps_present:
-                sc2_logger.info("NOTICE: Your map files may be outdated (version number not found). Run /download_data"
+                sc2_logger.info("NOTICE: Your map files may be outdated (version number not found). Run /download_data "
                                 "to update them.")
 
 
