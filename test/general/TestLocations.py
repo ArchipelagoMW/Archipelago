289341a161e66e060cb2a196c6775e4f87270104
alwaysintreble <mmmcheese158@gmail.com>
2023-01-08 10:53:50 -0600

    remove the first, less useful test

--- test/general/TestLocations.py
+++ test/general/TestLocations.py
@@ -10,8 +10,6 @@ class TestBase(unittest.TestCase):
                 continue
             multiworld = setup_default_world(world_type)
             locations = [location for location in multiworld.get_locations() if location.address is not None]
-            self.assertLessEqual(len(locations), len(list(world_type.location_name_to_id)),
-                                 f"{world_type.game} has more locations than registered")
             for location_name in world_type.location_name_to_id:
                 self.assertLessEqual(len([location for location in locations if location.name == location_name]), 1,
                                      f"{world_type.game} has duplicate of location {location_name}")
