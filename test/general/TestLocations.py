c2bbb37a1d67a00066ec5a9f7e4664799be0808f
alwaysintreble <mmmcheese158@gmail.com>
2023-01-08 10:50:45 -0600

    Tests: write a test to check for duplicate locations

---
+++ test/general/TestLocations.py
@@ -0,0 +1,17 @@
+import unittest
+from worlds.AutoWorld import AutoWorldRegister
+from . import setup_default_world
+
+
+class TestBase(unittest.TestCase):
+    def testCreateDuplicateLocations(self):
+        for game_name, world_type in AutoWorldRegister.world_types.items():
+            if game_name in {"Final Fantasy"}:
+                continue
+            multiworld = setup_default_world(world_type)
+            locations = [location for location in multiworld.get_locations() if location.address is not None]
+            self.assertLessEqual(len(locations), len(list(world_type.location_name_to_id)),
+                                 f"{world_type.game} has more locations than registered")
+            for location_name in world_type.location_name_to_id:
+                self.assertLessEqual(len([location for location in locations if location.name == location_name]), 1,
+                                     f"{world_type.game} has duplicate of location {location_name}")
