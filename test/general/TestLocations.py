e6efdcdf5101ef4e6d0919ae981c898715f2b2e1
alwaysintreble <mmmcheese158@gmail.com>
2023-01-12 19:19:50 -0600

    use counter

--- test/general/TestLocations.py
+++ test/general/TestLocations.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import Counter
 from worlds.AutoWorld import AutoWorldRegister
 from . import setup_default_world
 
@@ -9,7 +10,7 @@ class TestBase(unittest.TestCase):
             if game_name in {"Final Fantasy"}:
                 continue
             multiworld = setup_default_world(world_type)
-            locations = [location for location in multiworld.get_locations() if location.address is not None]
-            for location_name in world_type.location_name_to_id:
-                self.assertLessEqual(len([location for location in locations if location.name == location_name]), 1,
-                                     f"{world_type.game} has duplicate of location {location_name}")
+            locations = Counter([location for location in multiworld.get_locations()])
+            if len(locations) >= 1:
+                self.assertLessEqual(locations.most_common(1)[0][1], 1,
+                                     f"{world_type.game} has duplicate of location {locations.most_common(1)}")
