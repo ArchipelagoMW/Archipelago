8d7828c9cf49ad9566d9cdedefef48102a2f4369
Jarno Westhof <jarnowesthof@gmail.com>
2023-01-23 20:40:26 +0100

    More refactorings

--- worlds/timespinner/LogicExtensions.py
+++ worlds/timespinner/LogicExtensions.py
@@ -2,12 +2,16 @@ from BaseClasses import MultiWorld, CollectionState
 from .Options import is_option_enabled
 
 class TimespinnerLogic():
-    world: MultiWorld
     player: int
 
+    flag_unchained_keys: bool
+    flag_specific_keycards: bool
+
     def __init__(self, world: MultiWorld, player: int):
-        self.world = world
         self.player = player
+
+        self.flag_specific_keycards = is_option_enabled(world, player, "SpecificKeycards")
+        self.flag_unchained_keys = is_option_enabled(world, player, "UnchainedKeys")
 
     def has_timestop(self, state: CollectionState) -> bool:
         return state.has_any({'Timespinner Wheel', 'Succubus Hairpin', 'Lightwall', 'Celestial Sash'}, self.player)
@@ -42,28 +46,31 @@ class TimespinnerLogic():
         return state.has('Security Keycard A', self.player)
 
     def has_keycard_B(self, state: CollectionState) -> bool:
-        if is_option_enabled(self.world, self.player, "SpecificKeycards"):
+        if self.flag_specific_keycards:
             return state.has('Security Keycard B', self.player)
         else:
             return state.has_any({'Security Keycard A', 'Security Keycard B'}, self.player)
 
     def has_keycard_C(self, state: CollectionState) -> bool:
-        if is_option_enabled(self.world, self.player, "SpecificKeycards"):
+        if self.flag_specific_keycards:
             return state.has('Security Keycard C', self.player)
         else:
             return state.has_any({'Security Keycard A', 'Security Keycard B', 'Security Keycard C'}, self.player)
 
     def has_keycard_D(self, state: CollectionState) -> bool:
-        if is_option_enabled(self.world, self.player, "SpecificKeycards"):
+        if self.flag_specific_keycards:
             return state.has('Security Keycard D', self.player)
         else:
             return state.has_any({'Security Keycard A', 'Security Keycard B', 'Security Keycard C', 'Security Keycard D'}, self.player)
 
     def can_break_walls(self, state: CollectionState) -> bool:
-        if is_option_enabled(self.world, self.player, "EyeSpy"):
+        if self.flag_specific_keycards:
             return state.has('Oculus Ring', self.player)
         else:
             return True
 
     def can_kill_all_3_bosses(self, state: CollectionState) -> bool:
         return state.has_all({'Killed Maw', 'Killed Twins', 'Killed Aelana'}, self.player)
+
+    def has_teleport(self, state: CollectionState) -> bool:
+        return self.flag_unchained_keys or state.has('Twin Pyramid Key', self.player)
