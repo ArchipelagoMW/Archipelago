bb2fd8ece3ae5d99de4e4088810fd7fbb46ff4cc
JaredWeakStrike <jddoc31@gmail.com>
2023-01-25 20:34:16 -0500

    fixed the try

--- KH2Client.py
+++ KH2Client.py
@@ -220,40 +220,40 @@ class KH2Context(CommonContext):
         while self.kh2connected==False:
             await asyncio.sleep(1)
         itemMemory=0
-        #try:
-        while int.from_bytes(self.kh2.read_bytes(self.kh2.base_address+0x0714DB8,1), "big")==255:
-            await asyncio.sleep(0.5)
-        while int.from_bytes(self.kh2.read_bytes(self.kh2.base_address+0x8E9DA3,1), "big")>0 or int.from_bytes(self.kh2.read_bytes(self.kh2.base_address+0xAB8BC7,1), "big")>0:
-            await asyncio.sleep(0.5)
-        if itemcode.ability:
-            if char=="Donald":
-                self.kh2.write_short(self.kh2.base_address + self.Save+self.donaldbackofinventory, itemcode.memaddr)
-                self.donaldbackofinventory-=2
-            elif char=="Goofy":
-                self.kh2.write_short(self.kh2.base_address + self.Save+self.goofybackofinventory, itemcode.memaddr)
-                self.goofybackofinventory-=2
-            else:
-                if itemcode.memaddr in {0x05E,0x062,0x066,0x06A,0x234}:
-                    self.give_growth(itemcode)
+        try:
+            while int.from_bytes(self.kh2.read_bytes(self.kh2.base_address+0x0714DB8,1), "big")==255:
+                await asyncio.sleep(0.5)
+            while int.from_bytes(self.kh2.read_bytes(self.kh2.base_address+0x8E9DA3,1), "big")>0 or int.from_bytes(self.kh2.read_bytes(self.kh2.base_address+0xAB8BC7,1), "big")>0:
+                await asyncio.sleep(0.5)
+            if itemcode.ability:
+                if char=="Donald":
+                    self.kh2.write_short(self.kh2.base_address + self.Save+self.donaldbackofinventory, itemcode.memaddr)
+                    self.donaldbackofinventory-=2
+                elif char=="Goofy":
+                    self.kh2.write_short(self.kh2.base_address + self.Save+self.goofybackofinventory, itemcode.memaddr)
+                    self.goofybackofinventory-=2
                 else:
-                    #cannot give stuff past this point or the game will crash
-                    if self.backofinventory==0x2544:
-                        return
-                    self.kh2.write_short(self.kh2.base_address + self.Save+self.backofinventory, itemcode.memaddr)
-                    self.backofinventory-=2
-        elif itemcode.bitmask>0:
-            itemMemory=int.from_bytes(self.kh2.read_bytes(self.kh2.base_address+self.Save+itemcode.memaddr,1), "big")
+                    if itemcode.memaddr in {0x05E,0x062,0x066,0x06A,0x234}:
+                        self.give_growth(itemcode)
+                    else:
+                        #cannot give stuff past this point or the game will crash
+                        if self.backofinventory==0x2544:
+                            return
+                        self.kh2.write_short(self.kh2.base_address + self.Save+self.backofinventory, itemcode.memaddr)
+                        self.backofinventory-=2
+            elif itemcode.bitmask>0:
+                itemMemory=int.from_bytes(self.kh2.read_bytes(self.kh2.base_address+self.Save+itemcode.memaddr,1), "big")
 
-            self.kh2.write_bytes(self.kh2.base_address+self.Save+itemcode.memaddr,(itemMemory|0x01<<itemcode.bitmask).to_bytes(1,'big'),1)
-        else:
+                self.kh2.write_bytes(self.kh2.base_address+self.Save+itemcode.memaddr,(itemMemory|0x01<<itemcode.bitmask).to_bytes(1,'big'),1)
+            else:
 
-            amount=int.from_bytes(self.kh2.read_bytes(self.kh2.base_address+self.Save+itemcode.memaddr,1), "big")
-            self.kh2.write_bytes(self.kh2.base_address + self.Save+itemcode.memaddr,(amount+1).to_bytes(1,'big'),1)
-        #except:
-        #    if self.kh2connected:
-        #        logger.info("Connection Lost, Please /autotrack")
-        #        self.kh2connected = False
-        #        return
+                amount=int.from_bytes(self.kh2.read_bytes(self.kh2.base_address+self.Save+itemcode.memaddr,1), "big")
+                self.kh2.write_bytes(self.kh2.base_address + self.Save+itemcode.memaddr,(amount+1).to_bytes(1,'big'),1)
+        except:
+            if self.kh2connected:
+                logger.info("Connection Lost, Please /autotrack")
+                self.kh2connected = False
+                return
     async def ItemSafe(self,args,svestate):
         try: 
             await self.roomSave(args,svestate)
