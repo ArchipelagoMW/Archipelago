e66a33dbe2b064f6b147308ba40df28e8c9a441c
BootsinSoots <theron.swanson@gmail.com>
2023-01-31 09:52:58 -0500

    Corrections to Options

--- worlds/luigimansion/Options.py
+++ worlds/luigimansion/Options.py
@@ -2,12 +2,14 @@ from typing import Dict, Union
 from BaseClasses import MultiWorld
 from Options import Toggle, DefaultOnToggle, DeathLink, Choice, Range, Option, OptionDict
 
-# Will look into feasability of options later.
+
+# Will look into feasibility of options later.
 
 
 class StartWithBetterVacuum(Toggle):
     "Start with Poltergust 4000"
     display_name = "Better Vacuum"
+
 
 # These might end up being the same
 class StartHiddenMansion(Toggle):
@@ -15,16 +17,17 @@ class StartHiddenMansion(Toggle):
     display_name = "Hidden Mansion"
 
 
-class MoneyGhosts(Toggle):
+class SpeedySpirits(Toggle):
     "Adds Blue Ghosts and Gold Mice to location pool"
-    display_name = "Money Ghosts"
+    display_name = "Speedy Spirits"
 
 
 class StartWithBooRadar(Toggle):
     "Start with Boo Radar"
     display_name = "Boo Radar"
 
-#class DoorRando(Toggle):
+
+# class DoorRando(Toggle):
 #   "Keys wil open different doors than normal, and doors may require elements instead of keys"
 #   display_name = "Door Randomization"
 # Heavy logic editing required
@@ -33,13 +36,16 @@ class Toadsanity(Toggle):
     "Adds Toads to location pool"
     display_name = "Toadsanity"
 
+
 class Plants(Toggle):
     "Adds all plants to location pool"
     display_name = "Plantsanity"
 
+
 class Interactables(Toggle):
     "Adds every interactable, such a dressers and light fixtures, to the location pool"
     display_name = "Interactables"
+
 
 class MarioItems(Range):
     "How many Mario Items it takes to capture the Fortune-Teller. 0 = Starts Capturable"
@@ -48,12 +54,14 @@ class MarioItems(Range):
     range_end = 5
     default = 5
 
+
 class WashroomBooCount(Range):
     "Set the number of Boos required to reach the 1F Washroom. 0 = Starts Open"
     display_name = "Washroom Boo Count"
     range_start = 0
     range_end = 50
     default = 5
+
 
 class BalconyBooCount(Range):
     "Set the number of Boos required to reach the Balcony. 0 = Starts Open"
@@ -62,6 +70,7 @@ class BalconyBooCount(Range):
     range_end = 50
     default = 20
 
+
 class FinalBooCount(Range):
     "Set the number of Boos required to reach the Secret Altar. 0 = Starts Open"
     display_name = "Altar Boo Count"
@@ -69,13 +78,16 @@ class FinalBooCount(Range):
     range_end = 50
     default = 40
 
+
 class Boosanity(Toggle):
     "Turns Boos into Items and Locations"
     display_name = "Boosanity"
 
+
 class PortraitGhosts(Toggle):
     "Turn Portrait Ghosts into checks in addition to their clear chests"
     display_name = "Portrait Ghosts"
+
 
 # Old Timespinner Options
 class Enemizer(Toggle):
