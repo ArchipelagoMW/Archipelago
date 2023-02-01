7835b1d3b992f3dd64195b064b1a8f7f1c38c75a
JaredWeakStrike <jddoc31@gmail.com>
2023-01-31 23:29:29 -0500

    fixed file name for the seed

--- WebHostLib/downloads.py
+++ WebHostLib/downloads.py
@@ -89,7 +89,7 @@ def download_slot_file(room_id, player_id: int):
         elif slot_data.game == "Dark Souls III":
             fname = f"AP_{app.jinja_env.filters['suuid'](room_id)}.json"
         elif slot_data.game == "Kingdom Hearts 2":
-            fname = f"AP_{app.jinja_env.filters['suuid'](room_id)}.zip"
+            fname = f"AP_{app.jinja_env.filters['suuid'](room_id)}_P{slot_data.player_id}_{slot_data.player_name}.zip"
         else:
             return "Game download not supported."
         return send_file(io.BytesIO(slot_data.data), as_attachment=True, download_name=fname)
