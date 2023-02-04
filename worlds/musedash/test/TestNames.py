91c0f318b1d2687b1079a66aa58f4dafd68c184d
Justus Lind <DeamonHunter@users.noreply.github.com>
2023-02-04 14:26:33 +1000

    Add a test to ensure the item names are within the normal ascii range.

---
+++ worlds/musedash/test/TestNames.py
@@ -0,0 +1,20 @@
+import unittest
+import logging
+
+
+from ..MuseDashCollection import MuseDashCollections
+
+class NamesTest(unittest.TestCase):
+
+    def test_all_names_are_ascii(self) -> None:
+        bad_names = list()
+        collection = MuseDashCollections(0, 1)
+        for name in collection.SongItems.keys():
+            for c in name:
+                if (0 <= ord(c) <= 127):
+                    continue
+
+                bad_names.append(name)
+                break
+
+        assert len(bad_names) == 0, f"Muse Dash has {len(bad_names)} songs with non-ASCII characters.\n{bad_names}"
---
+++ worlds/musedash/test/__init__.py
@@ -0,0 +1,6 @@
+from typing import cast
+from test.TestBase import WorldTestBase
+from worlds.musedash import MuseDashWorld
+
+class MuseDashTestBase(WorldTestBase):
+    game = "Muse Dash"
