e98a0b19114243193adf429c10b59dd335371e66
alwaysintreble <mmmcheese158@gmail.com>
2023-02-12 14:05:59 -0600

    Tests: test that exits to `Regions` are the parents of the `Entrance`

--- test/general/TestImplemented.py
+++ test/general/TestImplemented.py
@@ -10,5 +10,15 @@ class TestImplemented(unittest.TestCase):
         for gamename, world_type in AutoWorldRegister.world_types.items():
             if not world_type.hidden and gamename not in {"ArchipIDLE", "Final Fantasy", "Sudoku"}:
                 with self.subTest(gamename):
-                    world = setup_default_world(world_type)
-                    self.assertFalse(world.completion_condition[1](world.state))
+                    multiworld = setup_default_world(world_type)
+                    self.assertFalse(multiworld.completion_condition[1](multiworld.state))
+
+    def testEntranceParents(self):
+        """Tests that the parents of created Entrances match the exiting Region."""
+        for game_name, world_type in AutoWorldRegister.world_types.items():
+            if not world_type.hidden and game_name not in {"ArchipIDLE", "Final Fantasy", "Sudoku"}:
+                with self.subTest(game_name):
+                    multiworld = setup_default_world(world_type)
+                    for region in multiworld.regions:
+                        for exit in region.exits:
+                            self.assertEqual(exit.parent_region, region)
