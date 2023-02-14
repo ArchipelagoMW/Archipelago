8960e068cb1ecc15fe2538ebae3121e717d39234
trevorleslie <80716066+TRPG0@users.noreply.github.com>
2023-02-13 18:02:57 -0700

    Blasphemous: Fix regions

--- worlds/blasphemous/__init__.py
+++ worlds/blasphemous/__init__.py
@@ -1,5 +1,5 @@
 from typing import Dict, Set, Any
-from BaseClasses import Region, Entrance, Location, Item, Tutorial, ItemClassification, RegionType
+from BaseClasses import Region, Entrance, Location, Item, Tutorial, ItemClassification
 from ..AutoWorld import World, WebWorld
 from .Items import item_table, group_table, tears_set, reliquary_set, skill_set
 from .Locations import location_table, shop_set
@@ -270,71 +270,40 @@ class BlasphemousWorld(World):
 
     def create_regions(self) -> None:
         
-        type = RegionType.Generic
         player = self.player
         world = self.multiworld
 
         region_table: Dict[str, Region] = {
-            "menu"    : Region("Menu", type, 
-                            "Menu", player, world),
-            "albero"  : Region("Albero", type, 
-                            "Albero", player, world),
-            "attots"  : Region("All the Tears of the Sea", type, 
-                            "All the Tears of the Sea", player, world),
-            "ar"      : Region("Archcathedral Rooftops", type, 
-                            "Archcathedral Rooftops", player, world),
-            "bottc"   : Region("Bridge of the Three Cavalries", type, 
-                            "Bridge of the Three Cavalries", player, world),
-            "botss"   : Region("Brotherhood of the Silent Sorrow", type, 
-                            "Brotherhood of the Silent Sorrow", player, world),
-            "coolotcv": Region("Convent of Our Lady of the Charred Visage", type, 
-                            "Convent of Our Lady of the Charred Visage", player, world),
-            "dohh"    : Region("Deambulatory of His Holiness", type, 
-                            "Deambulatory of His Holiness", player, world),
-            "dc"      : Region("Desecrated Cistern", type, 
-                            "Desecrated Cistern", player, world),
-            "eos"     : Region("Echoes of Salt", type, 
-                            "Echoes of Salt", player, world),
-            "ft"      : Region("Ferrous Tree", type,
-                            "Ferrous Tree", player, world),
-            "gotp"    : Region("Graveyard of the Peaks", type, 
-                            "Graveyard of the Peaks", player, world),
-            "ga"      : Region("Grievance Ascends", type, 
-                            "Grievance Ascends", player, world),
-            "hotd"    : Region("Hall of the Dawning", type, 
-                            "Hall of the Dawning", player, world),
-            "jondo"   : Region("Jondo", type, 
-                            "Jondo", player, world),
-            "kottw"   : Region("Knot of the Three Words", type, 
-                            "Knot of the Three Words", player, world),
-            "lotnw"   : Region("Library of the Negated Words", type, 
-                            "Library of the Negated Words", player, world),
-            "md"      : Region("Mercy Dreams", type, 
-                            "Mercy Dreams", player, world),
-            "mom"     : Region("Mother of Mothers", type, 
-                            "Mother of Mothers", player, world),
-            "moted"   : Region("Mountains of the Endless Dusk", type, 
-                            "Mountains of the Endless Dusk", player, world),
-            "mah"     : Region("Mourning and Havoc", type, 
-                            "Mourning and Havoc", player, world),
-            "potss"   : Region("Patio of the Silent Steps", type, 
-                            "Patio of the Silent Steps", player, world),
-            "petrous" : Region("Petrous", type, 
-                            "Petrous", player, world),
-            "thl"     : Region("The Holy Line", type, 
-                            "The Holy Line", player, world),
-            "trpots"  : Region("The Resting Place of the Sister", type, 
-                            "The Resting Place of the Sister", player, world),
-            "tsc"     : Region("The Sleeping Canvases", type, 
-                            "The Sleeping Canvases", player, world),
-            "wothp"   : Region("Wall of the Holy Prohibitions", type, 
-                            "Wall of the Holy Prohibitions", player, world),
-            "wotbc"   : Region("Wasteland of the Buried Churches", type, 
-                            "Wasteland of the Buried Churches", player, world),
-            "wotw"    : Region("Where Olive Trees Wither", type, 
-                            "Where Olive Trees Wither", player, world),
-            "dungeon" : Region("Dungeons", type,
-                            "Dungeons", player, world)
+            "menu"    : Region("Menu", player, world),
+            "albero"  : Region("Albero", player, world),
+            "attots"  : Region("All the Tears of the Sea", player, world),
+            "ar"      : Region("Archcathedral Rooftops", player, world),
+            "bottc"   : Region("Bridge of the Three Cavalries", player, world),
+            "botss"   : Region("Brotherhood of the Silent Sorrow", player, world),
+            "coolotcv": Region("Convent of Our Lady of the Charred Visage", player, world),
+            "dohh"    : Region("Deambulatory of His Holiness", player, world),
+            "dc"      : Region("Desecrated Cistern", player, world),
+            "eos"     : Region("Echoes of Salt", player, world),
+            "ft"      : Region("Ferrous Tree", player, world),
+            "gotp"    : Region("Graveyard of the Peaks", player, world),
+            "ga"      : Region("Grievance Ascends", player, world),
+            "hotd"    : Region("Hall of the Dawning", player, world),
+            "jondo"   : Region("Jondo", player, world),
+            "kottw"   : Region("Knot of the Three Words", player, world),
+            "lotnw"   : Region("Library of the Negated Words", player, world),
+            "md"      : Region("Mercy Dreams", player, world),
+            "mom"     : Region("Mother of Mothers", player, world),
+            "moted"   : Region("Mountains of the Endless Dusk", player, world),
+            "mah"     : Region("Mourning and Havoc", player, world),
+            "potss"   : Region("Patio of the Silent Steps", player, world),
+            "petrous" : Region("Petrous", player, world),
+            "thl"     : Region("The Holy Line", player, world),
+            "trpots"  : Region("The Resting Place of the Sister", player, world),
+            "tsc"     : Region("The Sleeping Canvases", player, world),
+            "wothp"   : Region("Wall of the Holy Prohibitions", player, world),
+            "wotbc"   : Region("Wasteland of the Buried Churches", player, world),
+            "wotw"    : Region("Where Olive Trees Wither", player, world),
+            "dungeon" : Region("Dungeons", player, world)
         }
 
         for rname, reg in region_table.items():
