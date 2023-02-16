b235df8f051313402ab1637465b71cb5d025f6f5
PoryGone <porygoneplays@gmail.com>
2023-02-16 01:53:09 -0500

    Send Kart Race setting in slot data

--- worlds/sa2b/__init__.py
+++ worlds/sa2b/__init__.py
@@ -84,6 +84,7 @@ class SA2BWorld(World):
             "Whistlesanity": self.multiworld.whistlesanity[self.player].value,
             "GoldBeetles": self.multiworld.beetlesanity[self.player].value,
             "OmochaoChecks": self.multiworld.omosanity[self.player].value,
+            "KartRaceChecks": self.multiworld.kart_race_checks[self.player].value,
             "ChaoRaceChecks": self.multiworld.chao_race_checks[self.player].value,
             "ChaoGardenDifficulty": self.multiworld.chao_garden_difficulty[self.player].value,
             "DeathLink": self.multiworld.death_link[self.player].value,
