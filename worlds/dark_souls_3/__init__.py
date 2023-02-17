b99d9ded15d31955abb7a2e1d37e23f94f0ab7e0
Br00ty <aribant92@gmail.com>
2023-02-17 02:03:49 -0500

    Update __init__.py
    deleted RegionType

--- worlds/dark_souls_3/__init__.py
+++ worlds/dark_souls_3/__init__.py
@@ -14,7 +14,7 @@ from .data.locations_data import location_dictionary, fire_link_shrine_table, \
     untended_graves_table, archdragon_peak_table, firelink_shrine_bell_tower_table, progressive_locations, \
     progressive_locations_2, progressive_locations_3, painted_world_table, dreg_heap_table, ringed_city_table, dlc_progressive_locations
 from ..AutoWorld import World, WebWorld
-from BaseClasses import MultiWorld, Region, Item, RegionType, Entrance, Tutorial, ItemClassification
+from BaseClasses import MultiWorld, Region, Item, Entrance, Tutorial, ItemClassification
 from ..generic.Rules import set_rule, add_item_rule
 
 
