ba4ca8f87fc7a729a1ed22bc1baf82d0a5a3ee1b
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2023-02-19 02:50:38 +0100

    Fixed missing Obelisk Side

--- worlds/witness/__init__.py
+++ worlds/witness/__init__.py
@@ -38,7 +38,7 @@ class WitnessWorld(World):
     """
     game = "The Witness"
     topology_present = False
-    data_version = 12
+    data_version = 13
 
     static_logic = StaticWitnessLogic()
     static_locat = StaticWitnessLocations()
