95c54531010e9c1b386819ae2c2de844a6491bbf
Zunawe <gyroscope15@gmail.com>
2023-02-20 20:05:08 -0800

    Refactor region creation to manually defined regions

--- worlds/pokemon_emerald/Data.py
+++ worlds/pokemon_emerald/Data.py
@@ -3,62 +3,23 @@ import json
 import os
 from typing import List, Optional, Dict, MutableSet, NamedTuple
 from BaseClasses import ItemClassification
+    
 
-class LocationData:
-    name: str
-    map_name: str
-    default_item: int
-    ram_address: Optional[int]
-    rom_address: Optional[int]
-    flag: int
-    tags: MutableSet[str]
-
-    def __init__(self, name: str, map_name: str, default_item: int, ram_address: Optional[int], rom_address: Optional[int], flag: int):
-        self.name = name
-        self.map_name = map_name
-        self.default_item = default_item
-        self.ram_address = ram_address
-        self.rom_address = rom_address
-        self.flag = flag
-        self.tags = set()
+def load_json(filename):
+    json_string = ""
+    with open(os.path.join(os.path.dirname(__file__), f"data/{filename}"), "r") as infile:
+        for line in infile.readlines():
+            json_string += line
+    return json.loads(json_string)
 
 
-class EncounterType(Enum):
-    LAND = 1
-    WATER = 2
-    FISHING = 3
-    # STATIC = 4
-
-
-class EncounterTableData:
-    encounter_type: EncounterType
-    map_name: str
-    encounter_slots: List[int]
-    ram_address: int
-    rom_address: int
-
-    def __init__(self, encounter_type: EncounterType, map_name: str, encounter_slots: List[int], ram_address: int, rom_address: int):
-        self.encounter_type = encounter_type
-        self.map_name = map_name
-        self.encounter_slots = encounter_slots
-        self.ram_address = ram_address
-        self.rom_address = rom_address
-
-
-class MapData:
-    name: str
-    connections: List[str]
-    warps: List[str]
-    locations: List[LocationData]
-    land_encounters: Optional[EncounterTableData]
-    water_encounters: Optional[EncounterTableData]
-    fishing_encounters: Optional[EncounterTableData]
-
-    def __init__(self, name: str):
-        self.name = name
-        self.connections = []
-        self.warps = []
-        self.locations = []
+data_json = None
+def get_data_json():
+    global data_json
+    if (data_json == None):
+        data_json = load_json("data.json")
+    
+    return data_json
 
 
 class ItemAttributes(NamedTuple):
@@ -90,132 +51,184 @@ def str_to_item_classification(string):
         return ItemClassification.filler
     elif (string == "TRAP"):
         return ItemClassification.trap
-    
-
-def load_json(filename):
-    json_string = ""
-    with open(os.path.join(os.path.dirname(__file__), f"data/{filename}"), "r") as infile:
-        for line in infile.readlines():
-            json_string += line
-    return json.loads(json_string)
 
 
-data_json = None
-def get_data_json():
-    global data_json
-    if (data_json == None):
-        data_json = load_json("data.json")
-    
-    return data_json
+class LocationData:
+    name: str
+    region_name: str
+    default_item: int
+    ram_address: Optional[int]
+    rom_address: Optional[int]
+    flag: int
+    tags: MutableSet[str]
+
+    def __init__(self, name: str, region_name: str, default_item: int, ram_address: Optional[int], rom_address: Optional[int], flag: int):
+        self.name = name
+        self.region_name = region_name
+        self.default_item = default_item
+        self.ram_address = ram_address
+        self.rom_address = rom_address
+        self.flag = flag
+        self.tags = set()
 
 
-data = None
-def get_data():
-    global data
-    if (data == None):
-        data = create_maps_from_json()
-    return data
+class WarpData:
+    region_name: str
+    encoded_string: str
+
+    def __init__(self, encoded_string: str, region_name: str):
+        self.encoded_string = encoded_string
+        self.region_name = region_name
 
 
-remapped_locations = {
-    "RECEIVED_HM01": "MAP_RUSTBORO_CITY_CUTTERS_HOUSE",
-    "RECEIVED_WAILMER_PAIL": "MAP_ROUTE104_PRETTY_PETAL_FLOWER_SHOP",
-    "ITEM_BADGE_1": "MAP_RUSTBORO_CITY_GYM",
-    "ITEM_BADGE_2": "MAP_DEWFORD_TOWN_GYM",
-    "ITEM_BADGE_3": "MAP_MAUVILLE_CITY_GYM",
-    "ITEM_BADGE_4": "MAP_LAVARIDGE_TOWN_GYM_1F",
-    "ITEM_BADGE_5": "MAP_PETALBURG_CITY_GYM",
-    "ITEM_BADGE_6": "MAP_FORTREE_CITY_GYM",
-    "ITEM_BADGE_7": "MAP_MOSSDEEP_CITY_GYM",
-    "ITEM_BADGE_8": "MAP_SOOTOPOLIS_CITY_GYM_1F",
-}
-def get_region_name_for(location_name: str, default_value: str):
-    global remapped_locations
-    if (location_name in remapped_locations):
-        return remapped_locations[location_name]
-    
-    return default_value
+class RegionData:
+    name: str
+    exits: List[str]
+    warps: List[WarpData]
+    locations: List[LocationData]
+
+    def __init__(self, name: str):
+        self.name = name
+        self.exits = []
+        self.warps = []
+        self.locations = []
 
 
-def create_maps_from_json() -> Dict[str, MapData]:
+regions_data = None
+def get_regions_data():
+    global regions_data
+    if (regions_data == None):
+        regions_data = create_regions_from_json()
+
+    return regions_data
+
+
+def create_regions_from_json() -> Dict[str, RegionData]:
     data_json = get_data_json()
 
-    maps = {}
-    # TODO: Map connections/warps currently only have info for their destination.
-    # Some maps (especially caves) have two warps leading to the same destination map.
-    # The source code specifies a destination ID to distinguish these warps.
-    for map_json in data_json["maps"]:
-        new_map = MapData(map_json["name"])
-        new_map.connections = [connection for connection in map_json["connections"]]
-        # MAP_DYNAMIC is mostly used for secret bases and online rooms
-        # With the exceptions of Terra Cave, Marine Cave, and the Dept. Store elevator
-        # These shouldn't affect logic for now
-        # TODO: Handle MAP_DYNAMIC edge cases
-        new_map.warps = [warp for warp in map_json["warps"] if (not warp == "MAP_DYNAMIC")]
-        maps[map_json["name"]] = new_map
-    
-    # -------------------------------------------------------------------------
-    # Ground/Hidden Item Locations
-    # -------------------------------------------------------------------------
+    location_to_region_map = {}
+
+    regions_json = load_json("regions.json")
+    regions = {}
+    for region_name, region_json in regions_json.items():
+        new_region = RegionData(region_name)
+        for location_name in region_json["locations"]:
+            location_to_region_map[location_name] = region_name
+        for exit in region_json["exits"]:
+            new_region.exits.append(exit)
+        for encoded_warp in region_json["warps"]:
+            new_region.warps.append(WarpData(encoded_warp, region_name))
+
+        regions[region_name] = new_region
+
     # Ground Items
     for item_json in data_json["ball_items"]:
         new_location = LocationData(
             item_json["name"],
-            item_json["map_name"],
+            location_to_region_map[item_json["name"]],
             item_json["default_item"],
             None,
             item_json["rom_address"],
             item_json["flag"]
         )
         new_location.tags.add("GroundItem")
-        maps[new_location.map_name].locations.append(new_location)
+        regions[new_location.region_name].locations.append(new_location)
 
     # Hidden Items
     for item_json in data_json["hidden_items"]:
         new_location = LocationData(
             item_json["name"],
-            item_json["map_name"],
+            location_to_region_map[item_json["name"]],
             item_json["default_item"],
             None,
             item_json["rom_address"],
             item_json["flag"]
         )
         new_location.tags.add("HiddenItem")
-        maps[new_location.map_name].locations.append(new_location)
+        regions[new_location.region_name].locations.append(new_location)
 
     # NPC Gifts
     for item_json in data_json["npc_gifts"]:
-        map_name = get_region_name_for(item_json["name"], None)
         new_location = LocationData(
             item_json["name"],
-            map_name,
+            location_to_region_map[item_json["name"]],
             item_json["default_item"],
             None,
             item_json["rom_address"],
             item_json["flag"]
         )
         new_location.tags.add("NpcGift")
-        maps[new_location.map_name].locations.append(new_location)
+        regions[new_location.region_name].locations.append(new_location)
 
     # Badges
     for i in range(0, 8):
         item_name = f"ITEM_BADGE_{i + 1}"
         item_code = data_json["constants"]["items"][item_name]
-        map_name = get_region_name_for(item_name, None)
         new_location = LocationData(
             item_name,
-            map_name,
+            location_to_region_map[item_json["name"]],
             item_code,
             None,
             data_json["misc_rom_addresses"]["gGymBadgeItems"] + (i * 2),
             data_json["constants"]["flags"][f"FLAG_BADGE0{i + 1}_GET"]
         )
         new_location.tags.add("Badge")
-        maps[new_location.map_name].locations.append(new_location)
+        regions[new_location.region_name].locations.append(new_location)
+    
+    return regions
 
-    # -------------------------------------------------------------------------
-    # Encounter Tables
-    # -------------------------------------------------------------------------
+
+def get_warp_destination_region_name(warp: WarpData):
+    regions_data = get_regions_data()
+    
+    source, dest = warp.encoded_string.split("/")
+    dest_map_name, dest_id = dest.split(':')
+
+    for region in regions_data.values():
+        for region_warp in region.warps:
+            region_warp_source, region_warp_dest = region_warp.encoded_string.split("/")
+            region_warp_source_map_name, region_warp_source_id_string = region_warp_source.split(':')
+            source_ids = region_warp_source_id_string.split(",")
+
+            if (
+                dest_map_name == region_warp_source_map_name and
+                dest_id in source_ids
+            ):
+                return region_warp.region_name
+
+    return None
+
+
+class EncounterType(Enum):
+    LAND = 1
+    WATER = 2
+    FISHING = 3
+    # STATIC = 4
+
+
+class EncounterTableData:
+    encounter_type: EncounterType
+    map_name: str
+    encounter_slots: List[int]
+    ram_address: int
+    rom_address: int
+
+    def __init__(self, encounter_type: EncounterType, map_name: str, encounter_slots: List[int], ram_address: int, rom_address: int):
+        self.encounter_type = encounter_type
+        self.map_name = map_name
+        self.encounter_slots = encounter_slots
+        self.ram_address = ram_address
+        self.rom_address = rom_address
+
+
+class EncounterData:
+    land_encounters: Optional[EncounterTableData]
+    water_encounters: Optional[EncounterTableData]
+    fishing_encounters: Optional[EncounterTableData]
+
+
+def create_encounters_from_json():
+    encounters = {}
     for map_name, encounter_table in data_json["encounter_tables"].items():
         if ("land_encounters" in encounter_table):
             land_encounters_json = encounter_table["land_encounters"]
@@ -227,7 +240,7 @@ def create_maps_from_json() -> Dict[str, MapData]:
                 land_encounters_json["ram_address"],
                 land_encounters_json["rom_address"]
             )
-            maps[map_name].land_encounters = land_encounters
+            encounters[map_name].land_encounters = land_encounters
 
         if ("water_encounters" in encounter_table):
             water_encounters_json = encounter_table["water_encounters"]
@@ -239,7 +252,7 @@ def create_maps_from_json() -> Dict[str, MapData]:
                 water_encounters_json["ram_address"],
                 water_encounters_json["rom_address"]
             )
-            maps[map_name].water_encounters = water_encounters
+            encounters[map_name].water_encounters = water_encounters
 
         if ("fishing_encounters" in encounter_table):
             fishing_encounters_json = encounter_table["fishing_encounters"]
@@ -251,6 +264,4 @@ def create_maps_from_json() -> Dict[str, MapData]:
                 fishing_encounters_json["ram_address"],
                 fishing_encounters_json["rom_address"]
             )
-            maps[map_name].fishing_encounters = fishing_encounters
-    
-    return maps
+            encounters[map_name].fishing_encounters = fishing_encounters
