f70c2055fcd4f7532f516856d0a17f3053b2a9b4
Justus Lind <DeamonHunter@users.noreply.github.com>
2023-02-25 14:54:24 +1000

    Some Oops...

--- worlds/musedash/__init__.py
+++ worlds/musedash/__init__.py
@@ -1,6 +1,6 @@
 from worlds.AutoWorld import World, WebWorld
 from worlds.generic.Rules import set_rule
-from BaseClasses import Region, RegionType, Item, ItemClassification, MultiWorld, Entrance, Tutorial
+from BaseClasses import Region, Item, ItemClassification, MultiWorld, Entrance, Tutorial
 
 from math import floor
 
@@ -13,7 +13,7 @@ client_version = 1
 
 
 class MuseDashWebWorld(WebWorld):
-    theme: "partyTime"
+    theme = "partyTime"
 
     bug_report_page = "https://github.com/DeamonHunter/ArchipelagoMuseDash/issues"
     setup_en = Tutorial(
@@ -159,8 +159,8 @@ class MuseDashWorld(World):
 
     def create_regions(self) -> None:
         # Basic Region Setup: Menu -> Song Select -> Songs
-        mainMenu = Region("Menu", "Menu", self.player, self.multiworld)
-        songSelect = Region("Song Select", "Song Select", self.player, self.multiworld)
+        mainMenu = Region("Menu", self.player, self.multiworld)
+        songSelect = Region("Song Select", self.player, self.multiworld)
 
         songSelectExit = Entrance(self.player, "Song Select Entrance", mainMenu)
 
@@ -183,7 +183,7 @@ class MuseDashWorld(World):
         # Make a region per song/album, then adds 1-2 item locations to them
         for i in range(0, len(all_selected_locations)):
             name = all_selected_locations[i]
-            region = Region(name, name, self.player, self.multiworld)
+            region = Region(name, self.player, self.multiworld)
 
             # 2 Locations are defined per song
             location_name = name + "-0"
