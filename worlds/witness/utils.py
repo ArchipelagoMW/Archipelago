df89f6838b2327eeb9aadb9ff246903cc71358b9
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2023-02-28 00:15:52 +0100

    Improved make_warning_string function signature
    Co-authored-by: black-sliver <59490463+black-sliver@users.noreply.github.com>

--- worlds/witness/utils.py
+++ worlds/witness/utils.py
@@ -5,7 +5,7 @@ from typing import *
 from fractions import Fraction
 
 
-def make_warning_string(any_j : bool, any_u : bool, any_d : bool, all_j : bool, all_u : bool, all_d : bool ):
+def make_warning_string(any_j: bool, any_u: bool, any_d: bool, all_j: bool, all_u: bool, all_d: bool) -> str:
     warning_string = ""
 
     if any_j:
