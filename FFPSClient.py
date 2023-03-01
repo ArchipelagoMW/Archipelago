7ebcd47d0d589fc471f1b3ecc5ea9d8ca1cecbaa
jonloveslegos <jonloveslegos@gmail.com>
2023-03-01 05:38:34 -0500

    Fixed the client not settings starting save file if it is missing lines

--- FFPSClient.py
+++ FFPSClient.py
@@ -186,6 +186,25 @@ async def game_watcher(ctx: FFPSContext):
                 sync_msg.append({"cmd": "LocationChecks", "locations": list(ctx.locations_checked)})
             await ctx.send_msgs(sync_msg)
             ctx.syncing = False
+
+        if os.path.exists(os.path.expandvars("%appdata%/MMFApplications/FNAF6")):
+            try:
+                with open(os.path.expandvars("%appdata%/MMFApplications/FNAF6"), 'r+') as f:
+                    lines = f.read()
+                    if not lines.__contains__("[FNAF6]"):
+                        f.write("[FNAF6]\n")
+                    if not lines.__contains__("stage="):
+                        f.write("stage=0\n")
+                    if not lines.__contains__("cups="):
+                        f.write("cups=0\n")
+                    if not lines.__contains__("speakers="):
+                        f.write("speakers=0\n")
+                    if not lines.__contains__("money="):
+                        f.write("money=200\n")
+                    f.close()
+                break
+            except PermissionError:
+                continue
         path = os.path.expandvars("%appdata%/MMFApplications/FNAF6BOUGHT")
         sending = []
         victory = False
