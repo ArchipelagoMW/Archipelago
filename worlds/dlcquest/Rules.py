0e37370e5f16e33896c32fd124c2c3f63459257d
axe-y <hamel.antoine@gmail.com>
2023-03-03 02:17:26 -0500

    Start of Rules
    Start of Rules and added Entrance to region

--- worlds/dlcquest/Rules.py
+++ worlds/dlcquest/Rules.py
@@ -2,3 +2,20 @@ import collections
 import typing
 
 from BaseClasses import LocationProgressType, MultiWorld
+from ..generic.Rules import add_rule, set_rule
+from .Regions import DLCquestRegion
+
+def set_rules(world, player)
+    set_rule(world.get_entrance("mouving", player),
+             lambda state: state.has("Movement Pack", player))
+    set_rule(world.get_entrance("tree", player),
+             lambda state: state.has("Time is Money Pack", player))
+    set_rule(world.get_entrance("Cloud", player),
+             lambda state: state.has("Psychological Warfare Pack", player))
+    set_rule(world.get_entrance("behind tree double jump", player),
+             lambda state: state.has("Double jump Pack", player))
+    set_rule(world.get_entrance("Forest Entrance", player),
+             lambda state: state.has("Map Pack", player))
+    set_rule(world.get_entrance("Psychological warfare double jump", player),
+             lambda state: state.has("Double jump Pack", player))
+
