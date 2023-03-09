c8aab62f9a3da9777f166ad8d753cfcf0383261c
Zunawe <gyroscope15@gmail.com>
2023-03-09 13:51:14 -0800

    Pokemon Emerald: Fix birch pokemon setting wrong number of bytes

--- worlds/pokemon_emerald/Rom.py
+++ worlds/pokemon_emerald/Rom.py
@@ -85,8 +85,7 @@ def generate_output(multiworld: MultiWorld, player: int, output_directory: str):
     _set_bytes_little_endian(patched_rom, options_address + 4, 2, 100)
 
     # Set Birch pokemon
-    # TODO: Sometimes crashes game
-    # _set_bytes_little_endian(patched_rom, options_address + 6, 1, get_random_species(multiworld.per_slot_randoms[player]).id)
+    _set_bytes_little_endian(patched_rom, options_address + 6, 2, get_random_species(multiworld.per_slot_randoms[player]).id)
 
     # Write Output
     outfile_player_name = f"_P{player}"
