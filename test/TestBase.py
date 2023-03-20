2a63ec95ea139b527a7dcf796fabcfa1796d420b
alwaysintreble <mmmcheese158@gmail.com>
2023-03-20 12:00:32 -0500

    I like generators ok
    Co-authored-by: Fabian Dill <Berserker66@users.noreply.github.com>

--- test/TestBase.py
+++ test/TestBase.py
@@ -206,7 +206,7 @@ class WorldTestBase(unittest.TestCase):
             items = self.collect_by_name(item_names)
             for location in locations:
                 self.assertTrue(self.can_reach_location(location),
-                                f"{location} not reachable with {[item for item in item_names]}")
+                                f"{location} not reachable with {item_names}")
             self.remove(items)
 
     def assertBeatable(self, beatable: bool):
