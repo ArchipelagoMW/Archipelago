48535cbc622c691ff20607baebc577620bbb675e
PoryGone <porygoneplays@gmail.com>
2023-04-07 03:19:51 -0400

    Support Voice Shuffle

--- worlds/sa2b/Options.py
+++ worlds/sa2b/Options.py
@@ -567,7 +567,7 @@ class SADXMusic(Choice):
 class MusicShuffle(Choice):
     """
     What type of Music Shuffle is used
-    Off: No music is shuffled.
+    None: No music is shuffled.
     Levels: Level music is shuffled.
     Full: Level, Menu, and Additional music is shuffled.
     Singularity: Level, Menu, and Additional music is all replaced with a single random song.
@@ -576,6 +576,22 @@ class MusicShuffle(Choice):
     option_none = 0
     option_levels = 1
     option_full = 2
+    option_singularity = 3
+    default = 0
+
+
+class VoiceShuffle(Choice):
+    """
+    What type of Voice Shuffle is used
+    None: No voices are shuffled.
+    Shuffled: Voices are shuffled.
+    Rude: Voices are shuffled, but some are replaced with rude words.
+    Singularity: All voices are replaced with a single random voice.
+    """
+    display_name = "Voice Shuffle Type"
+    option_none = 0
+    option_shuffled = 1
+    option_rude = 2
     option_singularity = 3
     default = 0
 
@@ -647,6 +663,7 @@ sa2b_options: typing.Dict[str, type(Option)] = {
     "ring_loss": RingLoss,
     "sadx_music": SADXMusic,
     "music_shuffle": MusicShuffle,
+    "voice_shuffle": VoiceShuffle,
     "narrator": Narrator,
     "logic_difficulty": LogicDifficulty,
     "speed_mission_count": SpeedMissionCount,
