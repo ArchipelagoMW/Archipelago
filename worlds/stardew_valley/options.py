408e60db8a71a8154646038f141f38720719ebf1
Alex Gilbert <alexgilbert@yahoo.com>
2023-04-06 23:10:26 -0400

    - Normalized documentation on many options

--- worlds/stardew_valley/options.py
+++ worlds/stardew_valley/options.py
@@ -22,16 +22,16 @@ class StardewOptions:
 
 class Goal(Choice):
     """What's your goal with this play-through?
-    With Community Center, the world will be completed once you complete the Community Center.
-    With Grandpa's Evaluation, the world will be completed once 4 candles are lit at Grandpa's Shrine.
-    With Bottom of the Mines, the world will be completed once you reach level 120 in the mineshaft.
-    With Cryptic Note, the world will be completed once you complete the quest "Cryptic Note" where Mr Qi asks you to
+    Community Center: the world will be completed once you complete the Community Center.
+    Grandpa's Evaluation: the world will be completed once 4 candles are lit at Grandpa's Shrine.
+    Bottom of the Mines: the world will be completed once you reach level 120 in the mineshaft.
+    Cryptic Note: the world will be completed once you complete the quest "Cryptic Note" where Mr Qi asks you to
         reach floor 100 in the Skull Cavern.
-    With Master Angler, the world will be completed once you have caught every fish in the game. Pairs well with
+    Master Angler: the world will be completed once you have caught every fish in the game. Pairs well with
         Fishsanity.
-    With Complete Collection, the world will be completed once you have completed the museum by donating every possible
+    Complete Collection: the world will be completed once you have completed the museum by donating every possible
         item. Pairs well with Museumsanity.
-    With Full House, you must get married and have two kids. Pairs well with Friendsanity.
+    Full House: you must get married and have two kids. Pairs well with Friendsanity.
     """
     internal_name = "goal"
     display_name = "Goal"
@@ -91,9 +91,9 @@ class ResourcePackMultiplier(SpecialRange):
 
 class BundleRandomization(Choice):
     """What items are needed for the community center bundles?
-    With Vanilla, you get the standard bundles from the game
-    With Thematic, every bundle will require random items within their original category
-    With Shuffled, every bundle will require random items without logic"""
+    Vanilla: you get the standard bundles from the game
+    Thematic: every bundle will require random items within their original category
+    Shuffled: every bundle will require random items without logic"""
     internal_name = "bundle_randomization"
     display_name = "Bundle Randomization"
     default = 1
@@ -104,10 +104,10 @@ class BundleRandomization(Choice):
 
 class BundlePrice(Choice):
     """How many items are needed for the community center bundles?
-    With Very Cheap, every bundle will require two items fewer than usual
-    With Cheap, every bundle will require 1 item fewer than usual
-    With Normal, every bundle will require the vanilla number of items
-    With Expensive, every bundle will require 1 extra item"""
+    Very Cheap: every bundle will require two items fewer than usual
+    Cheap: every bundle will require 1 item fewer than usual
+    Normal: every bundle will require the vanilla number of items
+    Expensive: every bundle will require 1 extra item"""
     internal_name = "bundle_price"
     display_name = "Bundle Price"
     default = 2
@@ -119,16 +119,16 @@ class BundlePrice(Choice):
 
 class EntranceRandomization(Choice):
     """Should area entrances be randomized?
-    With Disabled, no entrance randomization is done
-    With Pelican Town, only buildings in the main town area are randomized with each other
-    With Non Progression, only buildings that are always available are randomized with each other
+    Disabled: no entrance randomization is done
+    Pelican Town: only buildings in the main town area are randomized with each other
+    Non Progression: only buildings that are always available are randomized with each other
     """
-    # With Buildings, All buildings in the world are randomized with each other
-    # With Everything, All buildings and areas are randomized with each other
-    # With Chaos, same as everything, but the buildings are shuffled again every in-game day. You can't learn it!
-    # With Buildings One-way, Entrance pairs are disconnected, they aren't two-way!
-    # With Everything One-way, Entrance pairs are disconnected, and every entrance is in the shuffle
-    # With Chaos One-way, Entrance pairs are disconnected, and they change every day!
+    # Buildings: All buildings in the world are randomized with each other
+    # Everything: All buildings and areas are randomized with each other
+    # Chaos, same as everything: but the buildings are shuffled again every in-game day. You can't learn it!
+    # Buildings One-way: Entrance pairs are disconnected, they aren't two-way!
+    # Everything One-way: Entrance pairs are disconnected, and every entrance is in the shuffle
+    # Chaos One-way: Entrance pairs are disconnected, and they change every day!
 
     internal_name = "entrance_randomization"
     display_name = "Entrance Randomization"
@@ -147,10 +147,10 @@ class EntranceRandomization(Choice):
 class SeasonRandomization(Choice):
     """Should seasons be randomized?
     All settings allow you to choose which season you want to play next (from those unlocked) at the end of a season.
-    With Disabled, you will start in Spring with all seasons unlocked.
-    With Randomized, the seasons will be unlocked randomly through Archipelago items.
-    With Randomized Not Winter, the seasons are randomized, but you're guaranteed not to start with winter.
-    With Progressive, you will unlock the seasons in order.
+    Disabled: you will start in Spring with all seasons unlocked.
+    Randomized: the seasons will be unlocked randomly through Archipelago items.
+    Randomized Not Winter: the seasons are randomized, but you're guaranteed not to start with winter.
+    Progressive: you will unlock the seasons in order.
     """
     internal_name = "season_randomization"
     display_name = "Season Randomization"
@@ -165,8 +165,8 @@ class SeedShuffle(Choice):
     """Should seeds be randomized?
     Pierre now sells a random amount of seasonal seeds and Joja sells them without season requirements, but only in
         huge packs.
-    With Disabled, all the seeds will be unlocked from the start.
-    With Randomized, the seeds will be unlocked as Archipelago items
+    Disabled: all the seeds will be unlocked from the start.
+    Randomized: the seeds will be unlocked as Archipelago items
     """
     internal_name = "seed_shuffle"
     display_name = "Seed Shuffle"
@@ -177,9 +177,9 @@ class SeedShuffle(Choice):
 
 class BackpackProgression(Choice):
     """How is the backpack progression handled?
-    With Vanilla, you can buy them at Pierre's General Store.
-    With Progressive, you will randomly find Progressive Backpack upgrades.
-    With Early Progressive, you can expect your first Backpack in sphere 1.
+    Vanilla: you can buy them at Pierre's General Store.
+    Progressive: you will randomly find Progressive Backpack upgrades.
+    Early Progressive: you can expect your first Backpack in sphere 1.
     """
     internal_name = "backpack_progression"
     display_name = "Backpack Progression"
@@ -191,8 +191,8 @@ class BackpackProgression(Choice):
 
 class ToolProgression(Choice):
     """How is the tool progression handled?
-    With Vanilla, Clint will upgrade your tools with ore.
-    With Progressive, you will randomly find Progressive Tool upgrades."""
+    Vanilla: Clint will upgrade your tools with ore.
+    Progressive: you will randomly find Progressive Tool upgrades."""
     internal_name = "tool_progression"
     display_name = "Tool Progression"
     default = 1
@@ -202,10 +202,10 @@ class ToolProgression(Choice):
 
 class TheMinesElevatorsProgression(Choice):
     """How is The Mines' Elevator progression handled?
-    With Vanilla, you will unlock a new elevator floor every 5 floor in the mine.
-    With Progressive, you will randomly find Progressive Mine Elevator to go deeper. Location are sent for reaching
+    Vanilla: you will unlock a new elevator floor every 5 floor in the mine.
+    Progressive: you will randomly find Progressive Mine Elevator to go deeper. Location are sent for reaching
         every level multiple of 5.
-    With Progressive from previous floor, Locations are sent for taking the ladder or stairs to every 5
+    Progressive from previous floor: Locations are sent for taking the ladder or stairs to every 5
         levels, taking the elevator does not count."""
     internal_name = "elevator_progression"
     display_name = "Elevator Progression"
@@ -217,8 +217,8 @@ class TheMinesElevatorsProgression(Choice):
 
 class SkillProgression(Choice):
     """How is the skill progression handled?
-    With Vanilla, you will level up and get the normal reward at each level.
-    With Progressive, the xp will be counted internally, locations will be sent when you earn a level. Your real
+    Vanilla: you will level up and get the normal reward at each level.
+    Progressive: the xp will be counted internally, locations will be sent when you earn a level. Your real
         levels will be scattered around the multiworld."""
     internal_name = "skill_progression"
     display_name = "Skill Progression"
@@ -229,10 +229,10 @@ class SkillProgression(Choice):
 
 class BuildingProgression(Choice):
     """How is the building progression handled?
-    With Vanilla, you will buy each building normally.
-    With Progressive, you will receive the buildings and will be able to build the first one of each type for free,
+    Vanilla: you will buy each building normally.
+    Progressive: you will receive the buildings and will be able to build the first one of each type for free,
         once it is received. If you want more of the same building, it will cost the vanilla price.
-    With Progressive early shipping bin, you can expect your shipping bin in sphere 1.
+    Progressive early shipping bin: you can expect your shipping bin in sphere 1.
     """
     internal_name = "building_progression"
     display_name = "Building Progression"
@@ -244,11 +244,11 @@ class BuildingProgression(Choice):
 
 class ArcadeMachineLocations(Choice):
     """How are the Arcade Machines handled?
-    With Vanilla, the arcade machines are not included in the Archipelago shuffling.
-    With Victories, each Arcade Machine will contain one check on victory
-    With Victories Easy, the arcade machines are both made considerably easier to be more accessible for the average
+    Vanilla: the arcade machines are not included in the Archipelago shuffling.
+    Victories: each Arcade Machine will contain one check on victory
+    Victories Easy: the arcade machines are both made considerably easier to be more accessible for the average
         player.
-    With Full Shuffling, the arcade machines will contain multiple checks each, and different buffs that make the game
+    Full Shuffling: the arcade machines will contain multiple checks each, and different buffs that make the game
         easier are in the item pool. Junimo Kart has one check at the end of each level.
         Journey of the Prairie King has one check after each boss, plus one check for each vendor equipment.
     """
@@ -283,11 +283,11 @@ class HelpWantedLocations(SpecialRange):
 
 class Fishsanity(Choice):
     """Locations for catching fish?
-    With None, there are no locations for catching fish
-    With Legendaries, each of the 5 legendary fish are checks
-    With Special, a curated selection of strong fish are checks
-    With Random, a random selection of fish are checks
-    With All, every single fish in the game is a location that contains an item. Pairs well with the Master Angler Goal
+    None: there are no locations for catching fish
+    Legendaries: each of the 5 legendary fish are checks
+    Special: a curated selection of strong fish are checks
+    Randomized: a random selection of fish are checks
+    All: every single fish in the game is a location that contains an item. Pairs well with the Master Angler Goal
     """
     internal_name = "fishsanity"
     display_name = "Fishsanity"
@@ -301,10 +301,10 @@ class Fishsanity(Choice):
 
 class Museumsanity(Choice):
     """Locations for museum donations?
-    With None, there are no locations for donating artifacts and minerals to the museum
-    With Milestones, the donation milestones from the vanilla game are checks
-    With Random, a random selection of minerals and artifacts are checks
-    With All, every single donation will be a check
+    None: there are no locations for donating artifacts and minerals to the museum
+    Milestones: the donation milestones from the vanilla game are checks
+    Randomized: a random selection of minerals and artifacts are checks
+    All: every single donation will be a check
     """
     internal_name = "museumsanity"
     display_name = "Museumsanity"
@@ -317,11 +317,11 @@ class Museumsanity(Choice):
 
 class Friendsanity(Choice):
     """Locations for friendships?
-    With None, there are no checks for befriending villagers
-    With Bachelors, each heart of a bachelor is a check
-    With Starting NPCs, each heart for npcs that are immediately available is a check
-    With All, every heart with every NPC is a check, including Leo, Kent, Sandy, etc
-    With All With Marriage, marriage candidates must also be dated, married, and raised up to 14 hearts.
+    None: there are no checks for befriending villagers
+    Bachelors: each heart of a bachelor is a check
+    Starting NPCs: each heart for npcs that are immediately available is a check
+    All: every heart with every NPC is a check, including Leo, Kent, Sandy, etc
+    All With Marriage: marriage candidates must also be dated, married, and raised up to 14 hearts.
     """
     internal_name = "friendsanity"
     display_name = "Friendsanity"
@@ -410,11 +410,11 @@ class FriendshipMultiplier(SpecialRange):
 
 class DebrisMultiplier(Choice):
     """How much debris spawn on the player's farm?
-    With Vanilla, debris spawns normally
-    With Half, debris will spawn at half the normal rate
-    With Quarter, debris will spawn at one quarter of the normal rate
-    With None, No debris will spawn on the farm, ever
-    With Start Clear, debris will spawn at the normal rate, but the farm will be completely clear when starting the game
+    Vanilla: debris spawns normally
+    Half: debris will spawn at half the normal rate
+    Quarter: debris will spawn at one quarter of the normal rate
+    None: No debris will spawn on the farm, ever
+    Start Clear: debris will spawn at the normal rate, but the farm will be completely clear when starting the game
     """
     internal_name = "debris_multiplier"
     display_name = "Debris Multiplier"
