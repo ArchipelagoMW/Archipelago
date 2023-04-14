e78cae10b4ccdc4abafb3a9aaad84a6c84de21aa
Zunawe <gyroscope15@gmail.com>
2023-04-14 15:05:35 -0700

    Pokemon Emerald: Fix surf test
    Getting to the gym entrance requires the devon scope; I'm unsure how this ever passed

--- worlds/pokemon_emerald/test/TestOverworld.py
+++ worlds/pokemon_emerald/test/TestOverworld.py
@@ -13,11 +13,11 @@ class TestOverworld(WorldTestBase):
         self.assertFalse(self.can_reach_location(location_name_to_label("ITEM_LILYCOVE_CITY_MAX_REPEL")))
         self.assertFalse(self.can_reach_entrance("REGION_ROUTE118/WATER -> REGION_ROUTE118/EAST"))
         self.assertFalse(self.can_reach_entrance("REGION_ROUTE119/UPPER -> REGION_FORTREE_CITY/MAIN"))
-        self.assertFalse(self.can_reach_entrance("MAP_FORTREE_CITY:2/MAP_FORTREE_CITY_GYM:0"))
+        self.assertFalse(self.can_reach_entrance("MAP_FORTREE_CITY:3/MAP_FORTREE_CITY_MART:0"))
 
         self.collect_by_name(["HM03 Surf", "Balance Badge", "HM06 Rock Smash", "Dynamo Badge"])
         self.assertTrue(self.can_reach_location(location_name_to_label("ITEM_PETALBURG_CITY_ETHER")))
         self.assertTrue(self.can_reach_location(location_name_to_label("ITEM_LILYCOVE_CITY_MAX_REPEL")))
         self.assertTrue(self.can_reach_entrance("REGION_ROUTE118/WATER -> REGION_ROUTE118/EAST"))
         self.assertTrue(self.can_reach_entrance("REGION_ROUTE119/UPPER -> REGION_FORTREE_CITY/MAIN"))
-        self.assertTrue(self.can_reach_entrance("MAP_FORTREE_CITY:2/MAP_FORTREE_CITY_GYM:0"))
+        self.assertTrue(self.can_reach_entrance("MAP_FORTREE_CITY:3/MAP_FORTREE_CITY_MART:0"))
