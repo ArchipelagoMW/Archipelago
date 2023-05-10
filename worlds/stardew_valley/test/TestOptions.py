b7dc0dbf4272b4906559a31d910d71397bbdacc2
Alex Gilbert <alexgilbert@yahoo.com>
2023-05-10 00:28:29 -0400

    - fixed wrong method

--- worlds/stardew_valley/test/TestOptions.py
+++ worlds/stardew_valley/test/TestOptions.py
@@ -51,7 +51,7 @@ class TestGenerateDynamicOptions(SVTestBase):
             if not issubclass(option, SpecialRange):
                 continue
             for value in option.special_range_names:
-                with self.subTest(f"{option.internal_name}: {value} [Seed: {seed}]"):
+                with self.subTest(f"{option.internal_name}: {value}"):
                     choices = {option.internal_name: option.special_range_names[value]}
                     multiworld = setup_solo_multiworld(choices)
                     basic_checks(self, multiworld)
@@ -62,7 +62,7 @@ class TestGenerateDynamicOptions(SVTestBase):
             if not option.options:
                 continue
             for value in option.options:
-                with self.subTest(f"{option.internal_name}: {value}"):
+                with self.subTest(f"{option.internal_name}: {value} [Seed: {seed}]"):
                     choices = {option.internal_name: option.options[value]}
                     multiworld = setup_solo_multiworld(choices, seed)
                     basic_checks(self, multiworld)
