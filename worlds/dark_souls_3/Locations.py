bdd585c07e312e36492b0de05cbf23d4e5cf7f09
Br00ty <aribant92@gmail.com>
2023-05-21 04:25:48 -0400

    added remaining weapons, shields, armors, rings, spells, dlc equivalents | added remaining dlc ring locations (2 in dreg heap, 5 in ringed city)

--- worlds/dark_souls_3/Locations.py
+++ worlds/dark_souls_3/Locations.py
@@ -516,6 +516,8 @@ location_tables = {
         DS3LocationData("DH: Flame Fan",                           "Flame Fan",                               DS3LocationCategory.SPELL),
         DS3LocationData("DH: Soul of the Demon Prince",            "Soul of the Demon Prince",                DS3LocationCategory.MISC),
         DS3LocationData("DH: Small Envoy Banner",                  "Small Envoy Banner",                      DS3LocationCategory.KEY),
+        DS3LocationData("DH: Ring of Favor+3",                    "Ring of Favor+3",                        DS3LocationCategory.RING),
+        DS3LocationData("DH: Covetous Silver Serpent Ring+3",     "Covetous Silver Serpent Ring+3",         DS3LocationCategory.RING),
     ],
     "Ringed City": [
         DS3LocationData("RC: Ruin Sentinel Helm",                  "Ruin Sentinel Helm",                      DS3LocationCategory.ARMOR),
@@ -553,7 +555,12 @@ location_tables = {
         DS3LocationData("RC: Violet Wrappings",                    "Violet Wrappings",                        DS3LocationCategory.ARMOR),
         DS3LocationData("RC: Soul of Darkeater Midir",             "Soul of Darkeater Midir",                 DS3LocationCategory.MISC),
         DS3LocationData("RC: Soul of Slave Knight Gael",           "Soul of Slave Knight Gael",               DS3LocationCategory.MISC),
-        DS3LocationData("RC: Blood of the Dark Soul",              "Blood of the Dark Soul",                  DS3LocationCategory.KEY),
+        DS3LocationData("RC: Blood of the Dark Souls",             "Blood of the Dark Soul",                  DS3LocationCategory.KEY),
+        DS3LocationData("RC: Chloranthy Ring+3",                  "Chloranthy Ring+3",                      DS3LocationCategory.RING),
+        DS3LocationData("RC: Ring of Steel Protection+3",         "Ring of Steel Protection+3",             DS3LocationCategory.RING),
+        DS3LocationData("RC: Covetous Gold Serpent Ring+3",       "Covetous Gold Serpent Ring+3",           DS3LocationCategory.RING),
+        DS3LocationData("RC: Ring of the Evil Eye+3",             "Ring of the Evil Eye+3",                 DS3LocationCategory.RING),
+        DS3LocationData("RC: Wolf Ring+3",                        "Wolf Ring+3",                            DS3LocationCategory.RING),
     ],
 
     # Progressive
