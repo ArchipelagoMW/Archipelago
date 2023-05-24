ee15886d50706e19a93bfb446f7b888b1b8414d6
Justus Lind <DeamonHunter@users.noreply.github.com>
2023-05-24 17:52:31 +1000

    Correct some names.

--- worlds/musedash/__init__.py
+++ worlds/musedash/__init__.py
@@ -39,7 +39,7 @@ class MuseDashWorld(World):
     game: str = "Muse Dash"
     option_definitions = musedash_options
     topology_present = False
-    data_version = 3
+    data_version = 4
     web = MuseDashWebWorld()
 
     music_sheet_name: str = "Music Sheet"
