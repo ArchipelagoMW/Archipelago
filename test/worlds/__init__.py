5088dad281c6820dad7959334c2b7022e2ac5cf3
Fabian Dill <fabian.dill@web.de>
2023-05-31 20:10:42 +0200

    Tests: comprehend harder

--- test/worlds/__init__.py
+++ test/worlds/__init__.py
@@ -8,13 +8,13 @@ def load_tests(loader, standard_tests, pattern):
     suite.addTests(standard_tests)
     folders = [os.path.join(os.path.split(world.__file__)[0], "test")
                for world in AutoWorldRegister.world_types.values()]
-    all_tests = []
-    for folder in folders:
-        if os.path.exists(folder):
-            for test_collection in loader.discover(folder, top_level_dir=file_path):
-                for test_suite in test_collection:
-                    for test_case in test_suite:
-                        all_tests.append(test_case)
+
+    all_tests = [
+        test_case for folder in folders if os.path.exists(folder)
+        for test_collection in loader.discover(folder, top_level_dir=file_path)
+        for test_suite in test_collection
+        for test_case in test_suite
+    ]
 
     suite.addTests(sorted(all_tests, key=lambda test: test.__module__))
     return suite
