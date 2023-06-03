0ee9897036eb71b02b0e3259dfdf4b5106540229
el-u <109771707+el-u@users.noreply.github.com>
2023-06-03 06:36:19 +0200

    Core: update version to 0.4.2

--- Utils.py
+++ Utils.py
@@ -42,7 +42,7 @@ class Version(typing.NamedTuple):
         return ".".join(str(item) for item in self)
 
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 version_tuple = tuplize_version(__version__)
 
 is_linux = sys.platform.startswith("linux")
