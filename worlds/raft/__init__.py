3ed138eb974b0f204b5a84f9a85b87b63e9b5211
SunnyBat <Sunnybat@yahoo.com>
2023-06-10 22:31:00 -0700

    Use create_items instead of generate_basic

--- worlds/raft/__init__.py
+++ worlds/raft/__init__.py
@@ -43,7 +43,7 @@ class RaftWorld(World):
     data_version = 2
     required_client_version = (0, 3, 4)
 
-    def generate_basic(self):
+    def create_items(self):
         minRPSpecified = self.multiworld.minimum_resource_pack_amount[self.player].value
         maxRPSpecified = self.multiworld.maximum_resource_pack_amount[self.player].value
         minimumResourcePackAmount = min(minRPSpecified, maxRPSpecified)
