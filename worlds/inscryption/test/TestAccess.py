8131c3530ddc02b56e79d71334b6f66ca0f25c99
Benjamin Gregoire <benjamingregoire@outlook.com>
2023-07-08 18:01:44 -0400

    Added test for bridge requirement and added a dash to locationfor clarity

--- worlds/inscryption/test/TestAccess.py
+++ worlds/inscryption/test/TestAccess.py
@@ -4,17 +4,32 @@ from . import InscryptionTestBase
 class AccessTest(InscryptionTestBase):
 
     def testDagger(self) -> None:
-        self.assertAccessDependency(["Act 1 Magnificus Eye"], [["Dagger"]])
+        self.assertAccessDependency(["Act 1 - Magnificus Eye"], [["Dagger"]])
 
     def testCagedWolf(self) -> None:
-        self.assertAccessDependency(["Act 1 Dagger"], [["Caged Wolf Card"]])
+        self.assertAccessDependency(["Act 1 - Dagger"], [["Caged Wolf Card"]])
 
     def testMagnificusEye(self) -> None:
-        self.assertAccessDependency(["Act 1 Clock Main Compartment"], [["Magnificus Eye"]])
+        self.assertAccessDependency(["Act 1 - Clock Main Compartment"], [["Magnificus Eye"]])
 
     def testWardrobeKey(self) -> None:
         self.assertAccessDependency(
-            ["Act 1 Wardrobe Drawer 1", "Act 1 Wardrobe Drawer 2",
-             "Act 1 Wardrobe Drawer 3", "Act 1 Wardrobe Drawer 4"],
+            ["Act 1 - Wardrobe Drawer 1", "Act 1 - Wardrobe Drawer 2",
+             "Act 1 - Wardrobe Drawer 3", "Act 1 - Wardrobe Drawer 4"],
             [["Wardrobe Key"]]
         )
+
+    def testBridgeRequirement(self) -> None:
+        self.assertAccessDependency(
+            ["Act 2 - Battle Pike Mage", "Act 2 - Battle Goobert", "Act 2 - Battle Lonely Wizard",
+             "Act 2 - Battle Inspector", "Act 2 - Battle Melter", "Act 2 - Battle Dredger",
+             "Act 2 - Tower Chest 1", "Act 2 - Tower Chest 2", "Act 2 - Tower Chest 3",
+             "Act 2 - Tower Bath", "Act 2 - Factory Trash Can", "Act 2 - Factory Drawer 1",
+             "Act 2 - Factory Drawer 2", "Act 2 - Factory Chest 1", "Act 2 - Factory Chest 2",
+             "Act 2 - Factory Chest 3", "Act 2 - Factory Chest 4", "Act 2 - Monocle"],
+            [["Epitaph Piece 1", "Epitaph Piece 2", "Epitaph Piece 3", "Epitaph Piece 4",
+              "Epitaph Piece 5", "Epitaph Piece 6", "Epitaph Piece 7", "Epitaph Piece 8",
+              "Epitaph Piece 9", "Camera Replica", "Pile Of Meat"]]
+        )
+
+
