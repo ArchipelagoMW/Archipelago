cf2cb78ce38208cefff7de7a5c2a14361358096c
Alex Gilbert <alexgilbert@yahoo.com>
2023-07-13 16:21:51 -0400

    - Only 1000 worlds on the random worlds test

--- worlds/stardew_valley/test/long/TestRandomWorlds.py
+++ worlds/stardew_valley/test/long/TestRandomWorlds.py
@@ -88,6 +88,6 @@ class TestGenerateManyWorlds(SVTestBase):
     def test_generate_many_worlds_then_check_results(self):
         # if self.skip_long_tests:
         #     return
-        number_worlds = 5000
+        number_worlds = 1000
         multiworlds = generate_many_worlds(number_worlds)
         check_every_multiworld_is_valid(self, multiworlds)
