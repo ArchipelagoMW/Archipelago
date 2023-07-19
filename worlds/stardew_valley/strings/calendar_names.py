62657df3fb081482bdc89022b102794f24ba3992
agilbert1412 <alexgilbert@yahoo.com>
2023-07-19 14:26:38 -0400

    Stardew Valley: 4.x.x - The Ginger Update (#1931)
    ## What is this fixing or adding?
    Major content update for Stardew Valley
    
    ## How was this tested?
    One large-scale public Beta on the archipelago server, plus several smaller private asyncs and test runs
    
    You can go to https://github.com/agilbert1412/StardewArchipelago/releases to grab the mod (latest 4.x.x version), the supported mods and the apworld, to test this PR
    
    ## New Features:
    - Festival Checks [Easy mode or Hard Mode]
    - Special Orders [Both Board and Qi]
    - Willy's Boat
    - Ginger Island Parrots
    - TV Channels
    - Trap Items [Available in various difficulty levels]
    - Entrance Randomizer: Buildings and Chaos
    - New Fishsanity options: Exclude Legendaries, Exclude Hard fish, Only easy fish
    - Resource Pack overhaul [Resource packs are now more enjoyable and varied]
    - Goal: Greatest Walnut Hunter [Find every single Golden Walnut]
    - Goal: Perfection [Achieve Perfection]
    - Option: Profit Margin [Multiplier over all earnings]
    - Option: Friendsanity Heart Size [Reduce clutter from friendsanity hearts]
    - Option: Exclude Ginger Island - will exclude many locations and items to generate a playthrough that does not go to the island
    - Mod Support [Curated list of mods]
    
    ## New Contributors:
    @Witchybun for the mod support
    
    ---------
    
    Co-authored-by: Witchybun <embenham05@gmail.com>
    Co-authored-by: Exempt-Medic <60412657+Exempt-Medic@users.noreply.github.com>
    Co-authored-by: Fabian Dill <Berserker66@users.noreply.github.com>

---
+++ worlds/stardew_valley/strings/calendar_names.py
@@ -0,0 +1,11 @@
+class Weekday:
+    sunday = "Sunday"
+    monday = "Monday"
+    tuesday = "Tuesday"
+    wednesday = "Wednesday"
+    thursday = "Thursday"
+    friday = "Friday"
+    saturday = "Saturday"
+    all_days = [sunday, monday, tuesday, wednesday, thursday, friday, saturday]
+
+
--- worlds/stardew_valley/test/TestRegions.py
+++ worlds/stardew_valley/test/TestRegions.py
@@ -3,44 +3,82 @@ import sys
 import unittest
 
 from .. import StardewOptions, options
-from ..regions import stardew_valley_regions, mandatory_connections, randomize_connections, RandomizationFlag
+from ..regions import vanilla_regions, vanilla_connections, randomize_connections, RandomizationFlag
 
-connections_by_name = {connection.name for connection in mandatory_connections}
-regions_by_name = {region.name for region in stardew_valley_regions}
+connections_by_name = {connection.name for connection in vanilla_connections}
+regions_by_name = {region.name for region in vanilla_regions}
 
 
 class TestRegions(unittest.TestCase):
     def test_region_exits_lead_somewhere(self):
-        for region in stardew_valley_regions:
+        for region in vanilla_regions:
             with self.subTest(region=region):
                 for exit in region.exits:
-                    assert exit in connections_by_name, f"{region.name} is leading to {exit} but it does not exist."
+                    self.assertIn(exit, connections_by_name,
+                                  f"{region.name} is leading to {exit} but it does not exist.")
 
     def test_connection_lead_somewhere(self):
-        for connection in mandatory_connections:
+        for connection in vanilla_connections:
             with self.subTest(connection=connection):
-                assert connection.destination in regions_by_name, \
-                    f"{connection.name} is leading to {connection.destination} but it does not exist."
+                self.assertIn(connection.destination, regions_by_name,
+                              f"{connection.name} is leading to {connection.destination} but it does not exist.")
 
 
 class TestEntranceRando(unittest.TestCase):
 
-    def test_pelican_town_entrance_randomization(self):
+    def test_entrance_randomization(self):
         for option, flag in [(options.EntranceRandomization.option_pelican_town, RandomizationFlag.PELICAN_TOWN),
-                             (options.EntranceRandomization.option_non_progression, RandomizationFlag.NON_PROGRESSION)]:
+                             (options.EntranceRandomization.option_non_progression, RandomizationFlag.NON_PROGRESSION),
+                             (options.EntranceRandomization.option_buildings, RandomizationFlag.BUILDINGS)]:
+            # option = options.EntranceRandomization.option_buildings
+            # flag = RandomizationFlag.BUILDINGS
+            # for i in range(0, 100000):
+            seed = random.randrange(sys.maxsize)
+            with self.subTest(flag=flag, msg=f"Seed: {seed}"):
+                rand = random.Random(seed)
+                world_options = StardewOptions({options.EntranceRandomization.internal_name: option,
+                                                options.ExcludeGingerIsland.internal_name: options.ExcludeGingerIsland.option_false})
+                regions_by_name = {region.name: region for region in vanilla_regions}
+
+                _, randomized_connections = randomize_connections(rand, world_options, regions_by_name)
+
+                for connection in vanilla_connections:
+                    if flag in connection.flag:
+                        connection_in_randomized = connection.name in randomized_connections
+                        reverse_in_randomized = connection.reverse in randomized_connections
+                        self.assertTrue(connection_in_randomized,
+                                      f"Connection {connection.name} should be randomized but it is not in the output. Seed = {seed}")
+                        self.assertTrue(reverse_in_randomized,
+                                      f"Connection {connection.reverse} should be randomized but it is not in the output. Seed = {seed}")
+
+                self.assertEqual(len(set(randomized_connections.values())), len(randomized_connections.values()),
+                                 f"Connections are duplicated in randomization. Seed = {seed}")
+
+    def test_entrance_randomization_without_island(self):
+        for option, flag in [(options.EntranceRandomization.option_pelican_town, RandomizationFlag.PELICAN_TOWN),
+                             (options.EntranceRandomization.option_non_progression, RandomizationFlag.NON_PROGRESSION),
+                             (options.EntranceRandomization.option_buildings, RandomizationFlag.BUILDINGS)]:
             with self.subTest(option=option, flag=flag):
                 seed = random.randrange(sys.maxsize)
                 rand = random.Random(seed)
-                world_options = StardewOptions({options.EntranceRandomization.internal_name: option})
+                world_options = StardewOptions({options.EntranceRandomization.internal_name: option,
+                                                options.ExcludeGingerIsland.internal_name: options.ExcludeGingerIsland.option_true})
+                regions_by_name = {region.name: region for region in vanilla_regions}
 
-                _, randomized_connections = randomize_connections(rand, world_options)
+                _, randomized_connections = randomize_connections(rand, world_options, regions_by_name)
 
-                for connection in mandatory_connections:
+                for connection in vanilla_connections:
                     if flag in connection.flag:
-                        assert connection.name in randomized_connections, \
-                            f"Connection {connection.name} should be randomized but it is not in the output. Seed = {seed}"
-                        assert connection.reverse in randomized_connections, \
-                            f"Connection {connection.reverse} should be randomized but it is not in the output. Seed = {seed}"
+                        if RandomizationFlag.GINGER_ISLAND in connection.flag:
+                            self.assertNotIn(connection.name, randomized_connections,
+                                          f"Connection {connection.name} should not be randomized but it is in the output. Seed = {seed}")
+                            self.assertNotIn(connection.reverse, randomized_connections,
+                                          f"Connection {connection.reverse} should not be randomized but it is in the output. Seed = {seed}")
+                        else:
+                            self.assertIn(connection.name, randomized_connections,
+                                          f"Connection {connection.name} should be randomized but it is not in the output. Seed = {seed}")
+                            self.assertIn(connection.reverse, randomized_connections,
+                                          f"Connection {connection.reverse} should be randomized but it is not in the output. Seed = {seed}")
 
-                assert len(set(randomized_connections.values())) == len(
-                    randomized_connections.values()), f"Connections are duplicated in randomization. Seed = {seed}"
+                self.assertEqual(len(set(randomized_connections.values())), len(randomized_connections.values()),
+                                 f"Connections are duplicated in randomization. Seed = {seed}")
