266acd2ee42cd3fa98b100da2a7ffb3d99326eb9
alwaysintreble <mmmcheese158@gmail.com>
2023-07-18 22:39:44 -0500

    LTTP: set random seed in tests

--- worlds/alttp/test/__init__.py
+++ worlds/alttp/test/__init__.py
@@ -9,6 +9,7 @@ class LTTPTestBase(unittest.TestCase):
     def world_setup(self):
         self.multiworld = MultiWorld(1)
         self.multiworld.state = CollectionState(self.multiworld)
+        self.multiworld.set_seed(None)
         args = Namespace()
         for name, option in AutoWorldRegister.world_types["A Link to the Past"].options_dataclass.type_hints.items():
             setattr(args, name, {1: option.from_any(getattr(option, "default"))})
