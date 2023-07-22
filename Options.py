ecb1e0b74b78fc25cc90a476b83528e3b0d234dc
Zach Parks <zach@alliware.com>
2023-07-21 19:31:23 -0500

    Core: Add display name for `item_links` Option. (#1952)

--- Options.py
+++ Options.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
+
 import abc
 import logging
-from copy import deepcopy
 import math
 import numbers
-import typing
 import random
+import typing
+from copy import deepcopy
 
-from schema import Schema, And, Or, Optional
+from schema import And, Optional, Or, Schema
+
 from Utils import get_fuzzy_results
 
 if typing.TYPE_CHECKING:
@@ -949,6 +951,7 @@ class DeathLink(Toggle):
 
 class ItemLinks(OptionList):
     """Share part of your item pool with other players."""
+    display_name = "Item Links"
     default = []
     schema = Schema([
         {
