c08038eca75096767532d562114a8ed0a7f377cc
Mewlif <68133186+jonloveslegos@users.noreply.github.com>
2023-07-24 23:21:57 -0400

    Update worlds/undertale/__init__.py
    Co-authored-by: Aaron Wagener <mmmcheese158@gmail.com>

--- worlds/undertale/__init__.py
+++ worlds/undertale/__init__.py
@@ -77,7 +77,7 @@ class UndertaleWorld(World):
 
     def pre_fill(self):
         if not self.multiworld.get_location("Starting Key", self.player).item:
-            chosen_key_start = self.multiworld.per_slot_randoms[self.player].choice(["Ruins Key", "Snowdin Key", "Waterfall Key", "Hotland Key"])
+            chosen_key_start = self.random.choice(["Ruins Key", "Snowdin Key", "Waterfall Key", "Hotland Key"])
             self.multiworld.get_location("Starting Key", self.player).place_locked_item(self.create_item(chosen_key_start))
             self.multiworld.itempool.remove(self.create_item(chosen_key_start))
 
