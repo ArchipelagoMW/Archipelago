c6415556958cea1af1aeda328641599cfd4c684c
Benjamin Gregoire <benjamingregoire@outlook.com>
2023-08-03 05:23:29 -0400

    Add an option for the optional death card feature

--- worlds/inscryption/Options.py
+++ worlds/inscryption/Options.py
@@ -22,9 +22,29 @@ class RandomizeDeck(Toggle):
     display_name = "Randomize deck"
 
 
+class OptionalDeathCard(Choice):
+    """Add a Moment after death where you can decide to create a death card or not.
+    Disable: Disable the feature.
+    Always on: The feature is always on.
+    DeathLink Only: The feature only happens during a DeathLink death."""
+    auto_display_name = True
+    display_name = "Optional Death Card"
+    option_disable = 0
+    option_always_on = 1
+    option_deathlink_only = 2
+
+    def get_event_name(self) -> str:
+        return {
+            self.option_disable: "Disable",
+            self.option_always_on: "Always on",
+            self.option_deathlink_only: "DeathLink Only",
+        }[self.value]
+
+
 inscryption_options: typing.Dict[str, type(Option)] = {
     "deathlink": DeathLink,
     "trap": Trap,
     "randomize_codes": RandomizeCodes,
     "randomize_deck": RandomizeDeck,
+    "optional_death_card": OptionalDeathCard,
 }
