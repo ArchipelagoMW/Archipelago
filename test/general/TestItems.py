c7875c0322dc99080375964cdcb2066a749fc2f2
alwaysintreble <mmmcheese158@gmail.com>
2023-08-04 18:06:47 -0500

    add a unit test for item links

--- test/general/TestItems.py
+++ test/general/TestItems.py
@@ -1,5 +1,11 @@
 import unittest
-from worlds.AutoWorld import AutoWorldRegister
+from argparse import Namespace
+from typing import Type
+
+from BaseClasses import CollectionState, MultiWorld, Region
+from Fill import distribute_items_restrictive
+from Options import ItemLinks
+from worlds.AutoWorld import AutoWorldRegister, World, call_all
 from . import setup_solo_multiworld
 
 
@@ -58,3 +64,44 @@ class TestBase(unittest.TestCase):
                 multiworld = setup_solo_multiworld(world_type)
                 for item in multiworld.itempool:
                     self.assertIn(item.name, world_type.item_name_to_id)
+    
+    def testItemLinks(self) -> None:
+        """
+        Tests item link creation by creating a multiworld of 2 worlds for every game and linking their items together.
+        """
+        def setup_link_multiworld(world_type: Type[World]) -> MultiWorld:
+            multiworld = MultiWorld(2)
+            multiworld.game = {1: world_type.game, 2: world_type.game}
+            multiworld.player_name = {1: "Linker 1", 2: "Linker 2"}
+            multiworld.set_seed()
+            item_link_group = [{
+                "name": "ItemLinkTest",
+                "item_pool": ["Everything"],
+                "link_replacement": True,
+                "replacement_item": None,
+            }]
+            args = Namespace()
+            for name, option in world_type.option_definitions.items():
+                setattr(args, name, {1: option.from_any(option.default), 2: option.from_any(option.default)})
+            multiworld.set_options(args)
+            multiworld.set_default_common_options()
+            setattr(multiworld, "item_links",
+                    {1: ItemLinks.from_any(item_link_group), 2: ItemLinks.from_any(item_link_group)})
+            multiworld.set_item_links()
+            # groups get added to state during its constructor so this has to be after item links are set
+            multiworld.state = CollectionState(multiworld)
+            gen_steps = ("generate_early", "create_regions", "create_items", "set_rules", "generate_basic")
+            for step in gen_steps:
+                call_all(multiworld, step)
+            return multiworld
+        
+        for game_name, world_type in AutoWorldRegister.world_types.items():
+            with self.subTest("Game", game=game_name):
+                multiworld = setup_link_multiworld(world_type)
+                # link the items together and attempt to fill
+                multiworld.link_items()
+                multiworld._recache()
+                multiworld._all_state = None
+                call_all(multiworld, "pre_fill")
+                distribute_items_restrictive(multiworld)
+                call_all(multiworld, "post_fill")
