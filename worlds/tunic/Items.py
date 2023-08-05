2a043d0a4f2b2184bf481e45a21b7a3f9689ec60
Scipio Wright <scipiowright@gmail.com>
2023-08-05 13:05:46 -0400

    CSV usage fix for apworld compatibility

--- worlds/tunic/Items.py
+++ worlds/tunic/Items.py
@@ -1,8 +1,7 @@
-from pathlib import Path
 from typing import Dict, List
-from BaseClasses import ItemClassification, MultiWorld
+from BaseClasses import ItemClassification
+from . import data
 import csv
-
 
 
 class TunicItemData:
@@ -13,13 +12,12 @@ class TunicItemData:
 
 
 class TunicItems:
-
     items: List[TunicItemData] = []
     items_lookup: Dict[str, TunicItemData] = {}
 
     def populate_items(self):
-        item_file_path = (Path(__file__).parent / "data/Items.csv").resolve()
-        with open(item_file_path) as item_file:
+        from importlib.resources import files
+        with files(data).joinpath("Items.csv").open() as item_file:
             csv_file = csv.reader(item_file)
             csv_file.__next__()
             for line in csv_file:
@@ -31,5 +29,6 @@ class TunicItems:
                 item.quantity_in_item_pool = int(line[4])
                 self.items.append(item)
                 self.items_lookup[item.name] = item
+
 
 filler_items = []
--- worlds/tunic/Locations.py
+++ worlds/tunic/Locations.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 from typing import Dict, List
+from . import data
 import csv
 
 
@@ -13,8 +13,8 @@ class TunicLocations:
     locations_lookup: Dict[str, TunicLocationData] = {}
 
     def populate_locations(self):
-        locations_file_path = (Path(__file__).parent / "data/Locations.csv").resolve()
-        with open(locations_file_path) as location_file:
+        from importlib.resources import files
+        with files(data).joinpath("Locations.csv").open() as location_file:
             csv_file = csv.reader(location_file)
             csv_file.__next__()
             for line in csv_file:
