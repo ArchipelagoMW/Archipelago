754c946f0fd1b25ef5fffc7dc9cc9da591ba09fa
alwaysintreble <mmmcheese158@gmail.com>
2023-08-08 11:37:27 -0500

    plando items is default off so force the block in and check the locations

--- worlds/ladx/test/testShop.py
+++ worlds/ladx/test/testShop.py
@@ -1,17 +1,27 @@
+from typing import Optional
+
+from Fill import distribute_planned
 from . import LADXTestBase
 
 
 class PlandoTest(LADXTestBase):
-    """Tests plandoing swords in the shop."""
     options = {
         "plando_items": [{
             "items": {
                 "Progressive Sword": 2
             },
             "locations": [
-                "Shop 10 Item (Mabe Village)",
                 "Shop 200 Item (Mabe Village)",
                 "Shop 980 Item (Mabe Village)"
             ]
         }]
     }
+        
+    def test_planned(self):
+        """Tests plandoing swords in the shop."""
+        self.multiworld.plando_items[1] = self.options["plando_items"]
+        distribute_planned(self.multiworld)
+        self.assertEqual("Progressive Sword",
+                         self.multiworld.get_location("Shop 200 Item (Mabe Village)", 1).item.name)
+        self.assertEqual("Progressive Sword",
+                         self.multiworld.get_location("Shop 980 Item (Mabe Village)", 1).item.name)
