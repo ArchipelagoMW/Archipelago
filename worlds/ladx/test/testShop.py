90de610f96984aae65045a3413fb0a6eb5bd8cbf
alwaysintreble <mmmcheese158@gmail.com>
2023-08-08 22:04:23 -0500

    cleanup the unit test

--- worlds/ladx/test/testShop.py
+++ worlds/ladx/test/testShop.py
@@ -1,7 +1,10 @@
 from typing import Optional
 
 from Fill import distribute_planned
+from test.general import setup_solo_multiworld
+from worlds.AutoWorld import call_all
 from . import LADXTestBase
+from .. import LinksAwakeningWorld
 
 
 class PlandoTest(LADXTestBase):
@@ -13,15 +16,23 @@ class PlandoTest(LADXTestBase):
             "locations": [
                 "Shop 200 Item (Mabe Village)",
                 "Shop 980 Item (Mabe Village)"
-            ]
-        }]
+            ],
+        }],
     }
+    
+    def world_setup(self, seed: Optional[int] = None) -> None:
+        self.multiworld = setup_solo_multiworld(
+            LinksAwakeningWorld,
+            ("generate_early", "create_regions", "create_items", "set_rules", "generate_basic")
+        )
+        self.multiworld.plando_items[1] = self.options["plando_items"]
+        distribute_planned(self.multiworld)
+        call_all(self.multiworld, "pre_fill")
         
     def test_planned(self):
         """Tests plandoing swords in the shop."""
-        self.multiworld.plando_items[1] = self.options["plando_items"]
-        distribute_planned(self.multiworld)
-        self.assertEqual("Progressive Sword",
-                         self.multiworld.get_location("Shop 200 Item (Mabe Village)", 1).item.name)
-        self.assertEqual("Progressive Sword",
-                         self.multiworld.get_location("Shop 980 Item (Mabe Village)", 1).item.name)
+        location_names = ["Shop 200 Item (Mabe Village)", "Shop 980 Item (Mabe Village)"]
+        locations = [self.multiworld.get_location(loc, 1) for loc in location_names]
+        for loc in locations:
+            self.assertEqual("Progressive Sword", loc.item.name)
+            self.assertFalse(loc.can_reach(self.multiworld.state))
