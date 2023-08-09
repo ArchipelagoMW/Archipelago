3d8d98c169ac61f9f99709c45cfb92fa8a38bcb5
Alex Gilbert <alexgilbert@yahoo.com>
2023-08-09 17:08:51 -0400

    - Should exclude weapons but not footwear from the progression tests

--- worlds/stardew_valley/test/TestGeneration.py
+++ worlds/stardew_valley/test/TestGeneration.py
@@ -31,10 +31,10 @@ class TestBaseItemGeneration(SVTestBase):
         items_to_ignore = [event.name for event in items.events]
         items_to_ignore.extend(item.name for item in items.all_items if item.mod_name is not None)
         items_to_ignore.extend(season.name for season in items.items_by_group[Group.SEASON])
+        items_to_ignore.extend(weapon.name for weapon in items.items_by_group[Group.WEAPON])
         items_to_ignore.extend(baby.name for baby in items.items_by_group[Group.BABY])
         items_to_ignore.extend(resource_pack.name for resource_pack in items.items_by_group[Group.RESOURCE_PACK])
-        progression_items = [item for item in items.all_items if item.classification is ItemClassification.progression
-                             and item.name not in items_to_ignore]
+        progression_items = [item for item in items.all_items if item.classification is ItemClassification.progression and item.name not in items_to_ignore]
         for progression_item in progression_items:
             with self.subTest(f"{progression_item.name}"):
                 self.assertIn(progression_item.name, all_created_items)
@@ -80,8 +80,7 @@ class TestNoGingerIslandItemGeneration(SVTestBase):
         items_to_ignore.extend(season.name for season in items.items_by_group[Group.SEASON])
         items_to_ignore.extend(season.name for season in items.items_by_group[Group.WEAPON])
         items_to_ignore.extend(baby.name for baby in items.items_by_group[Group.BABY])
-        progression_items = [item for item in items.all_items if item.classification is ItemClassification.progression
-                             and item.name not in items_to_ignore]
+        progression_items = [item for item in items.all_items if item.classification is ItemClassification.progression and item.name not in items_to_ignore]
         for progression_item in progression_items:
             with self.subTest(f"{progression_item.name}"):
                 if Group.GINGER_ISLAND in progression_item.groups:
--- worlds/stardew_valley/test/mods/TestMods.py
+++ worlds/stardew_valley/test/mods/TestMods.py
@@ -61,7 +61,6 @@ class TestBaseItemGeneration(SVTestBase):
         items_to_ignore = [event.name for event in items.events]
         items_to_ignore.extend(season.name for season in items.items_by_group[Group.SEASON])
         items_to_ignore.extend(weapon.name for weapon in items.items_by_group[Group.WEAPON])
-        items_to_ignore.extend(footwear.name for footwear in items.items_by_group[Group.FOOTWEAR])
         items_to_ignore.extend(baby.name for baby in items.items_by_group[Group.BABY])
         items_to_ignore.extend(resource_pack.name for resource_pack in items.items_by_group[Group.RESOURCE_PACK])
         progression_items = [item for item in items.all_items if item.classification is ItemClassification.progression
@@ -85,7 +84,6 @@ class TestNoGingerIslandModItemGeneration(SVTestBase):
         items_to_ignore = [event.name for event in items.events]
         items_to_ignore.extend(season.name for season in items.items_by_group[Group.SEASON])
         items_to_ignore.extend(weapon.name for weapon in items.items_by_group[Group.WEAPON])
-        items_to_ignore.extend(footwear.name for footwear in items.items_by_group[Group.FOOTWEAR])
         items_to_ignore.extend(baby.name for baby in items.items_by_group[Group.BABY])
         items_to_ignore.extend(resource_pack.name for resource_pack in items.items_by_group[Group.RESOURCE_PACK])
         progression_items = [item for item in items.all_items if item.classification is ItemClassification.progression
