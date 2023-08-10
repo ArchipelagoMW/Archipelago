b0e642e9a3c8aa1e05eb19f016527ae499658e17
chandler05 <66492208+chandler05@users.noreply.github.com>
2023-08-10 17:41:39 -0500

    Add location rules and update item categories

--- worlds/shorthike/Rules.py
+++ worlds/shorthike/Rules.py
@@ -11,10 +11,22 @@ def create_rules(self, location_table):
             forbid_item(world.get_location(loc["name"], player), "Shovel", player)
             forbid_item(world.get_location(loc["name"], player), "Toy Shovel", player)
             forbid_items_for_player(world.get_location(loc["name"], player), self.item_name_groups['Maps'], player)
+            set_rule(world.get_location(loc["name"], self.player),
+                lambda state: state.has("Shovel", self.player))
         if loc["purchase"]:
             forbid_items_for_player(world.get_location(loc["name"], player), self.item_name_groups['Coins'], player)
     forbid_item(world.get_location("Catch All Fish Reward", player), "Fishing Journal", player)
+    set_rule(world.get_location("Catch All Fish Reward", player),
+        lambda state: state.has("Fishing Journal", self.player))
     forbid_item(world.get_location("Boat Challenge Reward", player), "Motorboat Key", player)
+    set_rule(world.get_location("Boat Challenge Reward", player),
+        lambda state: state.has("Motorboat Key", self.player))
     forbid_item(world.get_location("Collect 15 Seashells", player), "Seashell", player)
+    set_rule(world.get_location("Collect 15 Seashells", player),
+        lambda state: state.has("Seashell", self.player, 15))
     forbid_item(world.get_location("Wristwatch Trade", player), "Wristwatch", player)
+    set_rule(world.get_location("Wristwatch Trade", player),
+        lambda state: state.has("Wristwatch", self.player))
     forbid_item(world.get_location("Sue the Rabbit Shoes Reward", player), "Headband", player)
+    set_rule(world.get_location("Sue the Rabbit Shoes Reward", player),
+        lambda state: state.has("Headband", self.player))
