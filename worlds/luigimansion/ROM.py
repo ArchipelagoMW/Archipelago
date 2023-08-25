89073c7099b7682890efeaf5d6a4b3922ce32255
BootsinSoots <theron.swanson@gmail.com>
2023-08-25 00:08:18 -0400

    More notes

--- worlds/luigimansion/ROM.py
+++ worlds/luigimansion/ROM.py
@@ -7,7 +7,10 @@
 # For chest items, edit treasuretable with either money amounts or specific key/mario item/element in other slot, by room. Only one true chest per room
 # Must expand iteminfotable for use with treasuretable. treasuretable references iteminfotable
 # Portrait Ghosts, toads and Boos unsure
-# Blue Ghost and Gold Mice also use iteminfortable. Must add "other" field to jmp table
+# Blue Ghost and Gold Mice also use iteminfortable. Must add "other" field to jmp iyapootable. (iyapoo are the money ghosts)
 # need to add AP type checks to table somehow and trigger the item send
-#
-#
+# Need way to unpack and then edit jump tables automatically.
+# Need a way to add a custom item type that, when acquired, triggers the client to send an item
+# Need to adjust location names and items names to match code in game.
+# Need methods to give items to Luigi in game
+# How are keys/items/money stored once received??? How do they get committed to memory?
