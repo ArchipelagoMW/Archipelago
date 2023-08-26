0fe7bba62a34b3493c6e14bea53008285806b81b
Justus Lind <DeamonHunter@users.noreply.github.com>
2023-08-26 11:45:57 +1000

    Oops include starting songs in the search.

--- worlds/musedash/test/TestIncludedSongs.py
+++ worlds/musedash/test/TestIncludedSongs.py
@@ -10,6 +10,7 @@ class TestIncludedSongs(MuseDashTestBase):
     def test_free_dlc_included_in_base_songs(self) -> None:
         muse_dash_world = self.multiworld.worlds[1]
         songs = muse_dash_world.included_songs.copy()
+        songs.extend(muse_dash_world.starting_songs)
         songs.append(muse_dash_world.victory_song_name)
 
         assert "Glimmer" in songs, "Budget Is Burning Vol.1 is not being included in base songs"
