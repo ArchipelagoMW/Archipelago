e4823d3414a373c2c5aa9c2867fbddec0760a09c
JaredWeakStrike <jddoc31@gmail.com>
2023-08-28 15:29:04 -0400

    forgot forms in itempool option

--- worlds/kh2/Items.py
+++ worlds/kh2/Items.py
@@ -627,7 +627,7 @@ exclusion_item_table = {
 }
 
 default_itempool_option = {
-    item_name: ItemData.quantity for dic in [Magic_Table, Progression_Table, Summon_Table, Movement_Table] for item_name, ItemData in dic.items()
+    item_name: ItemData.quantity for dic in [Magic_Table, Progression_Table, Summon_Table, Movement_Table, Forms_Table] for item_name, ItemData in dic.items()
 }
 item_dictionary_table = {
     **Reports_Table,
