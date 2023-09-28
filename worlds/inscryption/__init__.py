a787133eb4ac806bd01587612466246484ca9111
DrBibop <olipoulihot@hotmail.com>
2023-09-28 02:41:01 -0400

    Fixed locations and items for act 1 goal

--- worlds/inscryption/__init__.py
+++ worlds/inscryption/__init__.py
@@ -1,10 +1,10 @@
 from .Options import inscryption_options
 from .Items import act1_items, act2_items, act3_items, filler_items, base_id, InscryptionItem, ItemDict
-from .Locations import location_table, LocDict
+from .Locations import act1_locations, act2_locations, act3_locations, regions_to_locations
 from .Regions import inscryption_regions_all, inscryption_regions_act_1
 from typing import Dict, Any
 from . import Rules
-from BaseClasses import Region, Item, Tutorial, ItemClassification
+from BaseClasses import Region, Item, Tutorial
 from ..AutoWorld import World, WebWorld
 
 
@@ -43,7 +43,8 @@ class InscryptionWorld(World):
     option_definitions = inscryption_options
     all_items = act1_items + act2_items + act3_items + filler_items
     item_name_to_id = {item['name']: i + base_id for i, item in enumerate(all_items)}
-    location_name_to_id = {location['name']: i + base_id for i, location in enumerate(location_table)}
+    all_locations = act1_locations + act2_locations + act3_locations
+    location_name_to_id = {location: i + base_id for i, location in enumerate(all_locations)}
 
     def create_item(self, name: str) -> Item:
         item_id = self.item_name_to_id[name]
@@ -59,7 +60,9 @@ class InscryptionWorld(World):
                 self.multiworld.itempool.append(new_item)
                 nb_items_added += 1
 
-        filler_count = len(location_table) - nb_items_added
+        filler_count = len(self.all_locations if self.multiworld.goal[self.player] <= 1 else act1_locations)
+        filler_count -= nb_items_added
+
         for i in range(filler_count):
             index = i % len(filler_items)
             filler_item = filler_items[index]
@@ -75,8 +78,7 @@ class InscryptionWorld(World):
             region = self.multiworld.get_region(region_name, self.player)
             region.add_exits(region_connections)
             region.add_locations({
-                location['name']: self.location_name_to_id[location['name']] for location in location_table
-                if location['region'] == region_name
+                location: self.location_name_to_id[location] for location in regions_to_locations[region_name]
             })
 
     def set_rules(self) -> None:
