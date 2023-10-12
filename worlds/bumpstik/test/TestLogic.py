994c16c2eddcd50938ca331c2bc00c432b32c5be
Felix R <felicitusneko@duck.com>
2023-10-12 12:15:01 -0300

    bumpstik: holy heck it works now

--- worlds/bumpstik/test/TestLogic.py
+++ worlds/bumpstik/test/TestLogic.py
@@ -9,9 +9,14 @@ class TestRuleLogic(BumpStikTestBase):
 
             self.collect(self.get_item_by_name("Treasure Bumper"))
             if x % 8 == 0:
-                if x < 32:
+                bb_count = round(x / 8)
+
+                if bb_count < 4:
                     self.assertFalse(self.can_reach_location(f"Treasure Bumper {x + 1}"))
-                for y in range(self.count("Booster Bumper"), round(x / 8) + 1):
+                elif bb_count == 4:
+                    bb_count += 1
+
+                for y in range(self.count("Booster Bumper"), bb_count):
                     self.assertTrue(self.can_reach_location(f"Bonus Booster {y + 1}"),
                                     f"BB {y + 1} check not reachable with {self.count('Booster Bumper')} BBs")
                     if y < 4:
@@ -21,6 +26,9 @@ class TestRuleLogic(BumpStikTestBase):
 
             if x < 31:
                 self.assertFalse(self.can_reach_location(f"Treasure Bumper {x + 2}"))
+            elif x == 31:
+                self.assertFalse(self.can_reach_location("Level 5 - 50,000+ Total Points"))
+
             if x < 32:
                 self.assertTrue(self.can_reach_location(f"Treasure Bumper {x + 1}"),
                                 f"TB {x + 1} check not reachable with {self.count('Treasure Bumper')} TBs")
