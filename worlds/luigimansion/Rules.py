b018f42ee3fb59382c702ce0ea39c436aed3838a
BootsinSoots <102177943+BootsinSoots@users.noreply.github.com>
2023-10-15 18:21:22 -0400

    Delete worlds/luigimansion directory

--- worlds/luigimansion/Rules.py
+++
@@ -1,9 +0,0 @@
-from BaseClasses import Location, MultiWorld
-from worlds.generic.Rules import add_rule
-
-
-# Assign ghost_type to region list, access that attribute to set rules during connect function
-def set_ghost_type(multiworld: MultiWorld, ghost_list: dict):
-    for region_name in ghost_list:
-        ghost_type = multiworld.random.choice(["Fire", "Water", "Ice", "No Element"])
-        ghost_list.update({region_name: ghost_type})
