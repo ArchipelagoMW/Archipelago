5fc07f87c2aaa8c6ea7ebc7f65075fee68f1a841
alwaysintreble <mmmcheese158@gmail.com>
2023-10-19 15:22:17 -0500

    Tests: Add a unit test for slot_data

--- test/general/TestImplemented.py
+++ test/general/TestImplemented.py
@@ -1,6 +1,8 @@
+import json
 import unittest
-from worlds.AutoWorld import AutoWorldRegister
 
+from Fill import distribute_items_restrictive
+from worlds.AutoWorld import AutoWorldRegister, call_all
 from . import setup_solo_multiworld
 
 
@@ -31,3 +33,18 @@ class TestImplemented(unittest.TestCase):
                     for method in ("assert_generate",):
                         self.assertFalse(hasattr(world_type, method),
                                          f"{method} must be implemented as a @classmethod named stage_{method}.")
+
+    def testSlotData(self):
+        """Tests that if a world creates slot data, it's json serializable."""
+        for game_name, world_type in AutoWorldRegister.world_types.items():
+            # has an await for generate_output which isn't being called
+            if game_name in {"Ocarina of Time", "Zillion"}:
+                continue
+            with self.subTest(game_name):
+                print(game_name)
+                multiworld = setup_solo_multiworld(world_type)
+                distribute_items_restrictive(multiworld)
+                call_all(multiworld, "post_fill")
+                slot_data = multiworld.worlds[1].fill_slot_data()
+                if slot_data:
+                    self.assertIsInstance(json.dumps(slot_data), str, slot_data)
