d71257ddf096a24cad4bafb4d338717156aad38f
Matthew <matthew.marinets@gmail.com>
2023-10-18 23:24:13 -0700

    sc2: Small update to test_Regions

--- worlds/sc2/test/test_Regions.py
+++ worlds/sc2/test/test_Regions.py
@@ -5,18 +5,22 @@ from .. import Options, MissionTables
 
 class TestGridsizes(unittest.TestCase):
     def test_grid_sizes_meet_specs(self):
-        self.assertEqual((2, 4, 1), Regions.get_grid_dimensions(7))
-        self.assertEqual((2, 4, 0), Regions.get_grid_dimensions(8))
-        self.assertEqual((3, 3, 0), Regions.get_grid_dimensions(9))
-        self.assertEqual((2, 5, 0), Regions.get_grid_dimensions(10))
-        self.assertEqual((3, 4, 1), Regions.get_grid_dimensions(11))
-        self.assertEqual((3, 4, 0), Regions.get_grid_dimensions(12))
-        self.assertEqual((3, 5, 0), Regions.get_grid_dimensions(15))
-        self.assertEqual((4, 4, 0), Regions.get_grid_dimensions(16))
-        self.assertEqual((4, 6, 0), Regions.get_grid_dimensions(24))
-        self.assertEqual((5, 5, 0), Regions.get_grid_dimensions(25))
-        self.assertEqual((5, 6, 1), Regions.get_grid_dimensions(29))
-        self.assertEqual((5, 7, 2), Regions.get_grid_dimensions(33))
+        self.assertTupleEqual((1, 2, 0), Regions.get_grid_dimensions(2))
+        self.assertTupleEqual((1, 3, 0), Regions.get_grid_dimensions(3))
+        self.assertTupleEqual((2, 2, 0), Regions.get_grid_dimensions(4))
+        self.assertTupleEqual((2, 3, 1), Regions.get_grid_dimensions(5))
+        self.assertTupleEqual((2, 4, 1), Regions.get_grid_dimensions(7))
+        self.assertTupleEqual((2, 4, 0), Regions.get_grid_dimensions(8))
+        self.assertTupleEqual((3, 3, 0), Regions.get_grid_dimensions(9))
+        self.assertTupleEqual((2, 5, 0), Regions.get_grid_dimensions(10))
+        self.assertTupleEqual((3, 4, 1), Regions.get_grid_dimensions(11))
+        self.assertTupleEqual((3, 4, 0), Regions.get_grid_dimensions(12))
+        self.assertTupleEqual((3, 5, 0), Regions.get_grid_dimensions(15))
+        self.assertTupleEqual((4, 4, 0), Regions.get_grid_dimensions(16))
+        self.assertTupleEqual((4, 6, 0), Regions.get_grid_dimensions(24))
+        self.assertTupleEqual((5, 5, 0), Regions.get_grid_dimensions(25))
+        self.assertTupleEqual((5, 6, 1), Regions.get_grid_dimensions(29))
+        self.assertTupleEqual((5, 7, 2), Regions.get_grid_dimensions(33))
 
 
 class TestGridGeneration(Sc2TestBase):
