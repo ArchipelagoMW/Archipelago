316fc4e4e1599b58fc98331e6ad9e96736616e54
Fabian Dill <Berserker66@users.noreply.github.com>
2023-10-25 21:23:52 +0200

    Core/LttP: remove initialize_regions (#2362)

--- worlds/alttp/ItemPool.py
+++ worlds/alttp/ItemPool.py
@@ -535,8 +535,6 @@ def set_up_take_anys(world, player):
         take_any.shop.add_inventory(0, 'Blue Potion', 0, 0)
         take_any.shop.add_inventory(1, 'Boss Heart Container', 0, 0, create_location=True)
 
-    world.initialize_regions()
-
 
 def get_pool_core(world, player: int):
     shuffle = world.shuffle[player]
