cbb5b5d48ffe947439c44cd42e89558064b58e50
black-sliver <59490463+black-sliver@users.noreply.github.com>
2023-10-27 18:58:21 +0200

    Stardew Valley: Test: Use subtests and inheritance for backpacks

--- worlds/stardew_valley/test/TestBackpack.py
+++ worlds/stardew_valley/test/TestBackpack.py
@@ -6,31 +6,31 @@ class TestBackpackVanilla(SVTestBase):
     options = {options.BackpackProgression.internal_name: options.BackpackProgression.option_vanilla}
 
     def test_no_backpack(self):
-        # no items
-        item_names = {item.name for item in self.multiworld.get_items()}
-        self.assertNotIn("Progressive Backpack", item_names)
+        with self.subTest("no items"):
+            item_names = {item.name for item in self.multiworld.get_items()}
+            self.assertNotIn("Progressive Backpack", item_names)
 
-        # no locations
-        location_names = {location.name for location in self.multiworld.get_locations()}
-        self.assertNotIn("Large Pack", location_names)
-        self.assertNotIn("Deluxe Pack", location_names)
+        with self.subTest("no locations"):
+            location_names = {location.name for location in self.multiworld.get_locations()}
+            self.assertNotIn("Large Pack", location_names)
+            self.assertNotIn("Deluxe Pack", location_names)
 
 
 class TestBackpackProgressive(SVTestBase):
     options = {options.BackpackProgression.internal_name: options.BackpackProgression.option_progressive}
 
     def test_backpack(self):
-        # 2 items
-        item_names = [item.name for item in self.multiworld.get_items()]
-        self.assertEqual(item_names.count("Progressive Backpack"), 2)
+        with self.subTest(check="has items"):
+            item_names = [item.name for item in self.multiworld.get_items()]
+            self.assertEqual(item_names.count("Progressive Backpack"), 2)
 
-        # 2 locations
-        location_names = {location.name for location in self.multiworld.get_locations()}
-        self.assertIn("Large Pack", location_names)
-        self.assertIn("Deluxe Pack", location_names)
+        with self.subTest(check="has locations"):
+            location_names = {location.name for location in self.multiworld.get_locations()}
+            self.assertIn("Large Pack", location_names)
+            self.assertIn("Deluxe Pack", location_names)
 
 
-class TestBackpackEarlyProgressive(SVTestBase):
+class TestBackpackEarlyProgressive(TestBackpackProgressive):
     options = {options.BackpackProgression.internal_name: options.BackpackProgression.option_early_progressive}
 
     @property
@@ -39,14 +39,7 @@ class TestBackpackEarlyProgressive(SVTestBase):
         return False
 
     def test_backpack(self):
-        # 2 items
-        item_names = [item.name for item in self.multiworld.get_items()]
-        self.assertEqual(item_names.count("Progressive Backpack"), 2)
+        super().test_backpack()
 
-        # 2 locations
-        location_names = {location.name for location in self.multiworld.get_locations()}
-        self.assertIn("Large Pack", location_names)
-        self.assertIn("Deluxe Pack", location_names)
-
-        # early item
-        self.assertIn("Progressive Backpack", self.multiworld.early_items[1])
+        with self.subTest(check="is early"):
+            self.assertIn("Progressive Backpack", self.multiworld.early_items[1])
--- worlds/stardew_valley/test/mods/TestBiggerBackpack.py
+++ worlds/stardew_valley/test/mods/TestBiggerBackpack.py
@@ -8,15 +8,15 @@ class TestBiggerBackpackVanilla(SVTestBase):
                options.Mods.internal_name: ModNames.big_backpack}
 
     def test_no_backpack(self):
-        # no items
-        item_names = {item.name for item in self.multiworld.get_items()}
-        self.assertNotIn("Progressive Backpack", item_names)
+        with self.subTest(check="no items"):
+            item_names = {item.name for item in self.multiworld.get_items()}
+            self.assertNotIn("Progressive Backpack", item_names)
 
-        # no locations
-        location_names = {location.name for location in self.multiworld.get_locations()}
-        self.assertNotIn("Large Pack", location_names)
-        self.assertNotIn("Deluxe Pack", location_names)
-        self.assertNotIn("Premium Pack", location_names)
+        with self.subTest(check="no locations"):
+            location_names = {location.name for location in self.multiworld.get_locations()}
+            self.assertNotIn("Large Pack", location_names)
+            self.assertNotIn("Deluxe Pack", location_names)
+            self.assertNotIn("Premium Pack", location_names)
 
 
 class TestBiggerBackpackProgressive(SVTestBase):
@@ -24,31 +24,23 @@ class TestBiggerBackpackProgressive(SVTestBase):
                options.Mods.internal_name: ModNames.big_backpack}
 
     def test_backpack(self):
-        # 3 items
-        item_names = [item.name for item in self.multiworld.get_items()]
-        self.assertEqual(item_names.count("Progressive Backpack"), 3)
+        with self.subTest(check="has items"):
+            item_names = [item.name for item in self.multiworld.get_items()]
+            self.assertEqual(item_names.count("Progressive Backpack"), 3)
 
-        # 3 locations
-        location_names = {location.name for location in self.multiworld.get_locations()}
-        self.assertIn("Large Pack", location_names)
-        self.assertIn("Deluxe Pack", location_names)
-        self.assertIn("Premium Pack", location_names)
+        with self.subTest(check="has locations"):
+            location_names = {location.name for location in self.multiworld.get_locations()}
+            self.assertIn("Large Pack", location_names)
+            self.assertIn("Deluxe Pack", location_names)
+            self.assertIn("Premium Pack", location_names)
 
 
-class TestBiggerBackpackEarlyProgressive(SVTestBase):
+class TestBiggerBackpackEarlyProgressive(TestBiggerBackpackProgressive):
     options = {options.BackpackProgression.internal_name: options.BackpackProgression.option_early_progressive,
                options.Mods.internal_name: ModNames.big_backpack}
 
     def test_backpack(self):
-        # 3 items
-        item_names = [item.name for item in self.multiworld.get_items()]
-        self.assertEqual(item_names.count("Progressive Backpack"), 3)
+        super().test_backpack()
 
-        # 3 locations
-        location_names = {location.name for location in self.multiworld.get_locations()}
-        self.assertIn("Large Pack", location_names)
-        self.assertIn("Deluxe Pack", location_names)
-        self.assertIn("Premium Pack", location_names)
-
-        # is early
-        self.assertIn("Progressive Backpack", self.multiworld.early_items[1])
+        with self.subTest(check="is early"):
+            self.assertIn("Progressive Backpack", self.multiworld.early_items[1])
