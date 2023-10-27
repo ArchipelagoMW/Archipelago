4e053626f460cf2e189e1de627f8bd328b6bb40c
alwaysintreble <mmmcheese158@gmail.com>
2023-10-27 15:02:43 -0500

    Tests: add test for all games multiworld and test for two player multiworld per game

--- test/general/__init__.py
+++ test/general/__init__.py
@@ -1,8 +1,8 @@
 from argparse import Namespace
-from typing import Type, Tuple
+from typing import List, Tuple, Type
 
-from BaseClasses import MultiWorld, CollectionState
-from worlds.AutoWorld import call_all, World
+from BaseClasses import CollectionState, MultiWorld
+from worlds.AutoWorld import World, call_all
 
 gen_steps = ("generate_early", "create_regions", "create_items", "set_rules", "generate_basic", "pre_fill")
 
@@ -23,6 +23,32 @@ def setup_solo_multiworld(world_type: Type[World], steps: Tuple[str, ...] = gen_
     args = Namespace()
     for name, option in world_type.options_dataclass.type_hints.items():
         setattr(args, name, {1: option.from_any(option.default)})
+    multiworld.set_options(args)
+    for step in steps:
+        call_all(multiworld, step)
+    return multiworld
+
+
+def setup_multiworld(worlds: List[Type[World]], steps: Tuple[str, ...] = gen_steps) -> MultiWorld:
+    """
+    Creates a multiworld with a player for each provided world, allowing duplicates, setting default options, and
+    calling the provided gen steps.
+    
+    :param worlds: list of the types of worlds to generate a multiworld for
+    :param steps: gen steps that should be called before returning. Default calls through pre_fill
+    """
+    players = len(worlds)
+    multiworld = MultiWorld(players)
+    multiworld.game = {player: world_type.game for player, world_type in enumerate(worlds, 1)}
+    multiworld.player_name = {player: f"Tester{player}" for player in range(1, players + 1)}
+    multiworld.set_seed()
+    multiworld.state = CollectionState(multiworld)
+    args = Namespace()
+    for player, world_type in enumerate(worlds, 1):
+        for key, option in world_type.options_dataclass.type_hints.items():
+            updated_options = getattr(args, key, {})
+            updated_options[player] = option.from_any(option.default)
+            setattr(args, key, updated_options)
     multiworld.set_options(args)
     for step in steps:
         call_all(multiworld, step)
---
+++ test/multiworld/test_multiworlds.py
@@ -0,0 +1,68 @@
+from typing import List, Tuple
+from unittest import TestCase
+
+from BaseClasses import CollectionState, Location, MultiWorld
+from Fill import distribute_items_restrictive
+from worlds.AutoWorld import AutoWorldRegister, call_all, call_single
+from worlds.alttp import ALTTPWorld
+from ..general import gen_steps, setup_multiworld
+
+
+class MultiworldTestBase(TestCase):
+    multiworld: MultiWorld
+
+    # similar to the implementation in WorldTestBase.test_fill
+    # but for multiple players and doesn't allow minimal accessibility
+    def fulfills_accessibility(self) -> bool:
+        locations = self.multiworld.get_locations().copy()
+        state = CollectionState(self.multiworld)
+        while locations:
+            sphere: List[Location] = []
+            for n in range(len(locations) - 1, -1, -1):
+                if locations[n].can_reach(state):
+                    sphere.append(locations.pop(n))
+            self.assertTrue(sphere, f"Unreachable locations: {locations}")
+            if not sphere:
+                return False
+            for location in sphere:
+                if location.item:
+                    state.collect(location.item, True, location)
+        return self.multiworld.has_beaten_game(state, 1)
+
+    def assertSteps(self, steps: Tuple[str, ...]):
+        for step in steps:
+            for player, world in self.multiworld.worlds.items():
+                with self.subTest(f"stage_{step}"):
+                    stage_callable = getattr(world.__class__, f"stage_{step}", None)
+                    if stage_callable:
+                        stage_callable(self.multiworld)
+                with self.subTest(step):
+                    call_single(self.multiworld, step, player)
+
+
+class TestAllGamesMultiworld(MultiworldTestBase):
+    multiworld: MultiWorld
+
+    def test_fills(self) -> None:
+        all_worlds = list(AutoWorldRegister.world_types.values())
+        all_worlds.remove(ALTTPWorld)
+        self.multiworld = setup_multiworld(all_worlds, ())
+        self.assertSteps(gen_steps)
+        with self.subTest("filling multiworld", seed=self.multiworld.seed):
+            distribute_items_restrictive(self.multiworld)
+            call_all(self.multiworld, "post_fill")
+            self.assertTrue(self.fulfills_accessibility(), "Collected all locations, but can't beat the game")
+
+
+class TestTwoPlayerMulti(MultiworldTestBase):
+    def test_two_player_single_game_fills(self) -> None:
+        for game, world in AutoWorldRegister.world_types.items():
+            if world is ALTTPWorld:
+                continue
+            worlds = [world] * 2
+            self.multiworld = setup_multiworld(worlds, ())
+            self.assertSteps(gen_steps)
+        with self.subTest("filling multiworld", seed=self.multiworld.seed):
+            distribute_items_restrictive(self.multiworld)
+            call_all(self.multiworld, "post_fill")
+            self.assertTrue(self.fulfills_accessibility(), "Collected all locations, but can't beat the game")
