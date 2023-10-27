c5652f6f1dcfc132a6a3955a3494d8bb38cf408b
alwaysintreble <mmmcheese158@gmail.com>
2023-10-26 21:14:38 -0500

    properly assign values to the dict and don't assign to the same dict multiple times for duplicate world types

--- BaseClasses.py
+++ BaseClasses.py
@@ -233,9 +233,10 @@ class MultiWorld():
             for option_key in world_type.options_dataclass.type_hints:
                 option_values = getattr(args, option_key, {})
                 if getattr(self, option_key, None) is None:
-                    self.option_key = Utils.DeprecateDict(f"Getting options from multiworld is now deprecated. "
-                                                          f"Please use `self.options.{option_key}` instead.")
-                setattr(self, option_key, option_values)
+                    option = Utils.DeprecateDict(f"Getting options from multiworld is now deprecated. "
+                                                 f"Please use `self.options.{option_key}` instead.")
+                    option.update(option_values)
+                    setattr(self, option_key, option)
             self.worlds[player] = world_type(self, player)
             self.worlds[player].random = self.per_slot_randoms[player]
             options_dataclass: typing.Type[Options.PerGameCommonOptions] = self.worlds[player].options_dataclass
