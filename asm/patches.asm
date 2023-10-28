4e80ef189359b02062222c4639d079afaa0e6b27
lil David <1337lilDavid@gmail.com>
2023-10-28 10:57:01 -0500

    WL4: Skip post-boss cutscenes

--- asm/patches.asm
+++ asm/patches.asm
@@ -77,7 +77,33 @@
 .org 0x8080FA8  ; DoraGetItemHantei()
     nop
 
+; Boss defeat cutscene
+.org 0x8056AA8       ;  EntityAI_0xCF_boss_jewel()
+.area 0x8056AD8 - .  ; Treasure coming out of wall and rising offscreen
+    ; This ends the cutscene with the fade out before it gets anywhere,
+    ; incidentally freezing the smoke from the boss dying in the process.
+    ; The animation could be made more elegant, but this will do.
+        ldr r1, =sGameSeq
+        mov r0, #6
+        strh r0, [r1]
+        ldr r0, =0x3000021  ; ucGmapSubSeq
+        mov r2, #0
+        strb r2, [r0]
+        ldr r1, =0x3000048  ; ucSTEndType
+        mov r0, #5
+        strb r0, [r1]
+
+        mov r3, #1
+        b @@End
+    .pool
+    @@End:
+.endarea
+
+.org 0x80004C2  ; MainGameLoop(): Treasure going to pyramid
+    mov r0, #0x1D
+
 ; Post-boss cutscenes
 .org 0x8079FDC  ; MainGameLoop(): Pyramid appears
     mov r0, #1
-.org 0x807A030  ; MainGameLoop(): Pyramid opens
+.org 0x807A034  ; MainGameLoop(): Pyramid opens
+    nop
