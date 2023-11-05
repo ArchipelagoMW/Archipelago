4594b5fc22b8fe34b671fa2d9435ac62d2bb327e
alwaysintreble <mmmcheese158@gmail.com>
2023-11-05 01:58:27 -0500

    Tests: have WorldTestBase use the new PlayerState

--- test/bases.py
+++ test/bases.py
@@ -151,7 +151,7 @@ class WorldTestBase(unittest.TestCase):
         self.multiworld.game[1] = self.game
         self.multiworld.player_name = {1: "Tester"}
         self.multiworld.set_seed(seed)
-        self.multiworld.state = CollectionState(self.multiworld)
+        self.multiworld.state = CollectionState(self.multiworld, 1)
         args = Namespace()
         for name, option in AutoWorld.AutoWorldRegister.world_types[self.game].options_dataclass.type_hints.items():
             setattr(args, name, {
@@ -238,7 +238,7 @@ class WorldTestBase(unittest.TestCase):
          one of the provided combinations"""
         all_items = [item_name for item_names in possible_items for item_name in item_names]
 
-        state = CollectionState(self.multiworld)
+        state = CollectionState(self.multiworld, 1)
         self.collect_all_but(all_items, state)
         if only_check_listed:
             for location in locations:
@@ -282,7 +282,7 @@ class WorldTestBase(unittest.TestCase):
             return
         with self.subTest("Game", game=self.game):
             excluded = self.multiworld.exclude_locations[1].value
-            state = self.multiworld.get_all_state(False)
+            state = self.multiworld.get_all_state(False, 1)
             for location in self.multiworld.get_locations():
                 if location.name not in excluded:
                     with self.subTest("Location should be reached", location=location):
@@ -297,7 +297,7 @@ class WorldTestBase(unittest.TestCase):
         if not (self.run_default_tests and self.constructed):
             return
         with self.subTest("Game", game=self.game):
-            state = CollectionState(self.multiworld)
+            state = CollectionState(self.multiworld, 1)
             locations = self.multiworld.get_reachable_locations(state, 1)
             self.assertGreater(len(locations), 0,
                                "Need to be able to reach at least one location to get started.")
@@ -311,7 +311,7 @@ class WorldTestBase(unittest.TestCase):
         # basically a shortened reimplementation of this method from core, in order to force the check is done
         def fulfills_accessibility() -> bool:
             locations = list(self.multiworld.get_locations(1))
-            state = CollectionState(self.multiworld)
+            state = CollectionState(self.multiworld, 1)
             while locations:
                 sphere: typing.List[Location] = []
                 for n in range(len(locations) - 1, -1, -1):
