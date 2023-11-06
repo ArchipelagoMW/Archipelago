f6321689e70181f23ff401b47f8f5a870ffd9b02
Magnemania <magnemight@gmail.com>
2023-11-06 12:36:25 -0500

    Removed EarlyUnit import from GUI

--- worlds/sc2/ClientGui.py
+++ worlds/sc2/ClientGui.py
@@ -17,7 +17,7 @@ from CommonClient import CommonContext
 from worlds.sc2.Client import SC2Context, calc_unfinished_missions, parse_unlock
 from worlds.sc2.MissionTables import lookup_id_to_mission, lookup_name_to_mission, SC2Mission, MissionInfo
 from worlds.sc2.Locations import LocationType, lookup_location_id_to_type
-from worlds.sc2.Options import LocationInclusion, EarlyUnit
+from worlds.sc2.Options import LocationInclusion
 from worlds.sc2 import SC2World, get_first_mission, get_early_unit_location_name
 
 
