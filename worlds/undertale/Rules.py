c587b2a298a082dd7a5313649068621e70a527c7
jonloveslegos <jonloveslegos@gmail.com>
2023-11-07 15:49:51 -0500

    Alphys Date is not a Region

--- worlds/undertale/Rules.py
+++ worlds/undertale/Rules.py
@@ -101,7 +101,7 @@ def set_rules(multiworld: MultiWorld, player: int):
         set_rule(multiworld.get_location("True Lab Plot", player),
                  lambda state: state.can_reach("New Home", "Region", player)
                                and state.can_reach("Letter Quest", "Location", player)
-                               and state.can_reach("Alphys Date", "Region", player))
+                               and state.can_reach("Alphys Date", "Location", player))
         set_rule(multiworld.get_location("Chisps Machine", player),
                  lambda state: state.can_reach("True Lab", "Region", player))
         set_rule(multiworld.get_location("Dog Sale 1", player),
