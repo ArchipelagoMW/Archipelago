72cb8b7d6080a088a3f129f1f3e7cf09e4949daf
Fabian Dill <Berserker66@users.noreply.github.com>
2023-11-07 21:02:28 +0100

    Factorio: inflate location pool (#2422)

--- worlds/factorio/Locations.py
+++ worlds/factorio/Locations.py
@@ -3,18 +3,13 @@ from typing import Dict, List
 from .Technologies import factorio_base_id
 from .Options import MaxSciencePack
 
-boundary: int = 0xff
-total_locations: int = 0xff
-
-assert total_locations <= boundary
-
 
 def make_pools() -> Dict[str, List[str]]:
     pools: Dict[str, List[str]] = {}
     for i, pack in enumerate(MaxSciencePack.get_ordered_science_packs(), start=1):
-        max_needed: int = 0xff
+        max_needed: int = 999
         prefix: str = f"AP-{i}-"
-        pools[pack] = [prefix + hex(x)[2:].upper().zfill(2) for x in range(1, max_needed + 1)]
+        pools[pack] = [prefix + str(x).upper().zfill(3) for x in range(1, max_needed + 1)]
     return pools
 
 
