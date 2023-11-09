91317a306db8f3edc8ce703dc50f8257b4cbf806
alwaysintreble <mmmcheese158@gmail.com>
2023-11-09 13:17:06 -0600

    add a comment for copy pasting

--- worlds/messenger/__init__.py
+++ worlds/messenger/__init__.py
@@ -82,6 +82,7 @@ class MessengerWorld(World):
         self.shop_prices, self.figurine_prices = shuffle_shop_prices(self)
 
     def create_regions(self) -> None:
+        # MessengerRegion adds itself to the multiworld
         for region in [MessengerRegion(reg_name, self) for reg_name in REGIONS]:
             if region.name in REGION_CONNECTIONS:
                 region.add_exits(REGION_CONNECTIONS[region.name])
