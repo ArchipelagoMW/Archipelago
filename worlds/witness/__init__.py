03a51e4c2984cabf6085c8bc20b503b04929e567
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2023-11-15 17:14:04 +0100

    These need to be in here as bools

--- worlds/witness/__init__.py
+++ worlds/witness/__init__.py
@@ -5,7 +5,7 @@ import dataclasses
 from typing import Dict, Optional
 
 from BaseClasses import Region, Location, MultiWorld, Item, Entrance, Tutorial, CollectionState
-from Options import PerGameCommonOptions
+from Options import PerGameCommonOptions, DefaultOnToggle, Toggle
 from .hints import get_always_hint_locations, get_always_hint_items, get_priority_hint_locations, \
     get_priority_hint_items, make_hints, generate_joke_hints
 from worlds.AutoWorld import World, WebWorld
@@ -294,9 +294,10 @@ class WitnessWorld(World):
 
         slot_data = self._get_slot_data()
 
-        for option_name in [attr.name for attr in dataclasses.fields(TheWitnessOptions)
-                            if attr not in dataclasses.fields(PerGameCommonOptions)]:
-            slot_data[option_name] = getattr(self.options, option_name).value
+        for option_name in (attr.name for attr in dataclasses.fields(TheWitnessOptions)
+                            if attr not in dataclasses.fields(PerGameCommonOptions)):
+            option = getattr(self.options, option_name)
+            slot_data[option_name] = bool(option.value) if isinstance(option, Toggle) else option.value
 
         return slot_data
 
