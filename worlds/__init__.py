33dff42991cbe50a13ae7252d067a68e1d6277e8
alwaysintreble <mmmcheese158@gmail.com>
2023-11-16 21:53:53 -0600

    some starting progress on lazy loading

--- worlds/__init__.py
+++ worlds/__init__.py
@@ -1,18 +1,18 @@
 import importlib
 import os
+import pkgutil
 import sys
 import typing
 import warnings
 import zipimport
+import orjson
 
-from Utils import user_path, local_path
+from Utils import user_path, local_path, cache_path
 
 local_folder = os.path.dirname(__file__)
 user_folder = user_path("worlds") if user_path() != local_path() else None
 
 __all__ = (
-    "lookup_any_item_id_to_name",
-    "lookup_any_location_id_to_name",
     "network_data_package",
     "AutoWorldRegister",
     "world_sources",
@@ -87,43 +87,93 @@ class WorldSource(typing.NamedTuple):
             return False
 
 
-# find potential world containers, currently folders and zip-importable .apworld's
-world_sources: typing.List[WorldSource] = []
-for folder in (folder for folder in (user_folder, local_folder) if folder):
-    relative = folder == local_folder
-    for entry in os.scandir(folder):
-        # prevent loading of __pycache__ and allow _* for non-world folders, disable files/folders starting with "."
-        if not entry.name.startswith(("_", ".")):
-            file_name = entry.name if relative else os.path.join(folder, entry.name)
-            if entry.is_dir():
-                world_sources.append(WorldSource(file_name, relative=relative))
-            elif entry.is_file() and entry.name.endswith(".apworld"):
-                world_sources.append(WorldSource(file_name, is_zip=True, relative=relative))
+def scan_worlds() -> typing.List[WorldSource]:
+    # find potential world containers, currently folders and zip-importable .apworld's
+    sources: typing.List[WorldSource] = []
+    for folder in (folder for folder in (user_folder, local_folder) if folder):
+        relative = folder == local_folder
+        for entry in os.scandir(folder):
+            # prevent loading of __pycache__ and allow _* for non-world folders, disable files/folders starting with "."
+            if not entry.name.startswith(("_", ".")):
+                file_name = entry.name if relative else os.path.join(folder, entry.name)
+                if entry.is_dir():
+                    sources.append(WorldSource(file_name, relative=relative))
+                elif entry.is_file() and entry.name.endswith(".apworld"):
+                    sources.append(WorldSource(file_name, is_zip=True, relative=relative))
+    return sources
 
-# import all submodules to trigger AutoWorldRegister
-world_sources.sort()
-for world_source in world_sources:
-    world_source.load()
 
-lookup_any_item_id_to_name = {}
-lookup_any_location_id_to_name = {}
-games: typing.Dict[str, GamesPackage] = {}
+def get_world_by_name(name: str) -> str:
+    for world in world_data:
+        if world["game"] == name:
+            return world
+    else:
+        raise ModuleNotFoundError(f"No game found that matches {name}")
 
-from .AutoWorld import AutoWorldRegister  # noqa: E402
 
-# Build the data package for each game.
-for world_name, world in AutoWorldRegister.world_types.items():
-    games[world_name] = world.get_data_package_data()
-    lookup_any_item_id_to_name.update(world.item_id_to_name)
-    lookup_any_location_id_to_name.update(world.location_id_to_name)
+def get_worlds_info() -> typing.Tuple[typing.Dict[str, typing.Dict[str, str]], bool]:
+    should_update = False
+    world_data = {}
+    try:
+        worlds_path = cache_path("worlds.json")
+        world_data = orjson.loads(pkgutil.get_data(__name__, worlds_path))
+        for world in world_sources:
+            if os.path.getmtime(world.resolved_path) > os.path.getmtime(worlds_path):
+                should_update = True
+                break
+    except orjson.JSONDecodeError:
+        should_update = True
 
-network_data_package: DataPackage = {
-    "games": games,
-}
+    return world_data, should_update
 
-# Set entire datapackage to version 0 if any of them are set to 0
-if any(not world.data_version for world in AutoWorldRegister.world_types.values()):
-    import logging
 
-    logging.warning(f"Datapackage is in custom mode. Custom Worlds: "
-                    f"{[world for world in AutoWorldRegister.world_types.values() if not world.data_version]}")
+def load_all_worlds() -> typing.Tuple[DataPackage, typing.Dict[str, typing.Dict[str, str]]]:
+    # import all submodules to trigger AutoWorldRegister
+    from .AutoWorld import AutoWorldRegister
+    world_sources.sort()
+
+    for world_source in world_sources:
+        world_source.load()
+
+    games: typing.Dict[str, GamesPackage] = {}
+    json_data: typing.Dict[str, typing.Dict[str, str]] = {}
+    for world_name, world in AutoWorldRegister.world_types.items():
+        games[world_name] = world.get_data_package_data()
+        json_data[world.__module__] = {"game": world.game}
+        if getattr(world, "settings", None):
+            json_data[world.__module__].update({"settings": str(world.settings_key)})
+
+    return DataPackage(games=games), json_data
+
+
+def load_worlds(games: typing.Union[typing.Iterable[str], str]) -> DataPackage:
+    if isinstance(games, str):
+        games = [games]
+
+    from .AutoWorld import AutoWorldRegister
+    to_load: typing.List[str] = []
+    for source, data in world_data.items():
+        if data["game"] in games:
+            to_load.append(source)
+
+    for world_source in [source for source in world_sources if source.path in to_load]:
+        world_source.load()
+
+    package: typing.Dict[str, GamesPackage] = {}
+    for world_name, world in AutoWorldRegister.world_types.items():
+        package[world_name] = world.get_data_package_data()
+    
+    return DataPackage(games=package)
+
+
+# if typing.TYPE_CHECKING:
+# network_data_package = load_all_worlds()
+
+
+world_sources = scan_worlds()
+world_data, needs_update = get_worlds_info()
+# TODO change to check if we need to update once everything expects lazy loading
+# network_data_package, world_data = load_all_worlds()
+network_data_package = load_worlds("The Messenger")
+with open(cache_path("worlds.json"), "wb") as f:
+    f.write(orjson.dumps(world_data))
