53cc2784fdcda340e567df20f493b13fd625bfb8
rjosephson <ryandj67@hotmail.com>
2023-11-25 17:45:23 -0700

    indent off by one

--- worlds/ror2/__init__.py
+++ worlds/ror2/__init__.py
@@ -104,9 +104,9 @@ class RiskOfRainWorld(World):
                 environment_offset_table = shift_by_offset(environment_sotv_table, environment_offset)
                 environments_pool = {**environments_pool, **environment_offset_table}
             # percollect starting environment for each stage 1
-                unlock = self.random.choices(list(environment_available_orderedstages_table[0].keys()), k=1)
-                self.multiworld.push_precollected(self.create_item(unlock[0]))
-                environments_pool.pop(unlock[0])
+            unlock = self.random.choices(list(environment_available_orderedstages_table[0].keys()), k=1)
+            self.multiworld.push_precollected(self.create_item(unlock[0]))
+            environments_pool.pop(unlock[0])
 
         # Generate item pool
         itempool: List[str] = ["Beads of Fealty", "Radar Scanner"]
