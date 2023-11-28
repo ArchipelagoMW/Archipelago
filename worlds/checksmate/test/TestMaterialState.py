cd6dd8e56191e90a128f957871d57703719ce8e8
chesslogic <chesslogicmaster@gmail.com>
2023-11-27 20:11:10 -0800

    describe material tests

--- worlds/checksmate/test/TestMaterialState.py
+++ worlds/checksmate/test/TestMaterialState.py
@@ -8,11 +8,17 @@ class MaterialStateTestBase(CMTestBase):
 
 
 class TestSimpleMaterial(MaterialStateTestBase):
+    """
+    Checks that goal can be reached based on the math performed by collect()
+
+    If this fails, it's not necessarily the fault of collect(), it might be that the generator isn't adding enough items
+    """
     def test_no_options(self):
         self.assertLess(3950, self.multiworld.state.prog_items[self.player]["Material"])
 
 
 class TestCyclicMaterial(MaterialStateTestBase):
+    """Removes all material, then adds it back again. This tests remove() via sledgehammer method"""
     def test_no_options(self):
         past_material = self.multiworld.state.prog_items[self.player]["Material"]
 
