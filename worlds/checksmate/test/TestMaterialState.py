f51fd6f13c627ea2595cf89cf1cfcb4b202d7bb5
chesslogic <chesslogicmaster@gmail.com>
2023-11-27 20:09:08 -0800

    test for removing material

--- worlds/checksmate/test/TestMaterialState.py
+++ worlds/checksmate/test/TestMaterialState.py
@@ -11,3 +11,13 @@ class TestSimpleMaterial(MaterialStateTestBase):
     def test_no_options(self):
         self.assertLess(3950, self.multiworld.state.prog_items[self.player]["Material"])
 
+
+class TestCyclicMaterial(MaterialStateTestBase):
+    def test_no_options(self):
+        past_material = self.multiworld.state.prog_items[self.player]["Material"]
+
+        self.remove(self.multiworld.itempool)
+        self.collect_all_but("Progressive Pocket Gems", self.multiworld.state)
+
+        self.assertLess(past_material, self.multiworld.state.prog_items[self.player]["Material"])
+
