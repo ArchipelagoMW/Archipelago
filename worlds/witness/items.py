28b6194dcd49f86c6e513c0af2152f3298a41957
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2023-11-30 03:44:53 +0100

    Rename some Rivers to Jungles

--- worlds/witness/items.py
+++ worlds/witness/items.py
@@ -126,7 +126,7 @@ class WitnessPlayerItems:
                                                                         "Swamp Long Bridge (Panel)"}:
                 # These Bridges/Elevators are not logical access because they may leave you stuck.
                 item_data.classification = ItemClassification.useful
-            elif item_name in {"River Monastery Garden Shortcut (Door)",
+            elif item_name in {"Jungle Monastery Garden Shortcut (Door)",
                                "Monastery Laser Shortcut (Door)",
                                "Orchard Second Gate (Door)",
                                "Jungle Bamboo Laser Shortcut (Door)",
