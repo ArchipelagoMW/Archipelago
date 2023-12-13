9e5bf7de91dc2c19fa3b3c6519b50faa4a063970
Chris Wilson <chris@legendserver.info>
2023-12-13 14:12:29 -0500

    Fix labels, add JS handling for TextChoice

--- WebHostLib/static/assets/playerOptions.js
+++ WebHostLib/static/assets/playerOptions.js
@@ -47,4 +47,23 @@ window.addEventListener('load', () => {
       }
     });
   });
+
+  // Handle changes to TextChoice input[type=text]
+  document.querySelectorAll('.text-choice-container input[type=text]').forEach((input) => {
+    const optionName = input.getAttribute('data-option-name');
+    input.addEventListener('input', () => {
+      const select = document.getElementById(optionName);
+      const optionValues = [];
+      select.childNodes.forEach((option) => optionValues.push(option.value));
+      select.value = (optionValues.includes(input.value)) ? input.value : 'custom';
+    });
+  });
+
+  // Handle changes to TextChoice select
+  document.querySelectorAll('.text-choice-container select').forEach((select) => {
+    const optionName = select.getAttribute('id');
+    select.addEventListener('change', () => {
+      document.getElementById(`${optionName}-custom`).value = '';
+    });
+  });
 });
