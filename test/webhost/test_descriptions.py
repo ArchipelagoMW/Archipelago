ff33d872ce061526fe02a321b6588f9024a3d404
Phar <zach@alliware.com>
2023-12-12 20:21:44 -0600

    typing

--- test/webhost/test_descriptions.py
+++ test/webhost/test_descriptions.py
@@ -4,7 +4,7 @@ from worlds.AutoWorld import AutoWorldRegister
 
 
 class TestWebDescriptions(unittest.TestCase):
-    def test_item_descriptions_have_valid_names(self):
+    def test_item_descriptions_have_valid_names(self) -> None:
         """Ensure all item descriptions match an item name or item group name"""
         for game_name, world_type in AutoWorldRegister.world_types.items():
             valid_names = world_type.item_names.union(world_type.item_name_groups)
@@ -13,7 +13,7 @@ class TestWebDescriptions(unittest.TestCase):
                     self.assertIn(name, valid_names,
                                   "All item descriptions must match defined item names")
 
-    def test_location_descriptions_have_valid_names(self):
+    def test_location_descriptions_have_valid_names(self) -> None:
         """Ensure all location descriptions match a location name or location group name"""
         for game_name, world_type in AutoWorldRegister.world_types.items():
             valid_names = world_type.location_names.union(world_type.location_name_groups)
