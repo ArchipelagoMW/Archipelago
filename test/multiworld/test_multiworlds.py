7474aaff34addefae583e048a5bb15a49656c692
alwaysintreble <mmmcheese158@gmail.com>
2023-12-14 09:20:42 -0600

    add docstrings

--- test/multiworld/test_multiworlds.py
+++ test/multiworld/test_multiworlds.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 
 from BaseClasses import CollectionState, Location, MultiWorld
 from Fill import distribute_items_restrictive
+from Options import Accessibility
 from worlds.AutoWorld import AutoWorldRegister, call_all, call_single
 from worlds.alttp import ALTTPWorld
 from ..general import gen_steps, setup_multiworld
@@ -14,6 +15,10 @@ class MultiworldTestBase(TestCase):
     # similar to the implementation in WorldTestBase.test_fill
     # but for multiple players and doesn't allow minimal accessibility
     def fulfills_accessibility(self) -> bool:
+        """
+        Checks that the multiworld satisfies locations accessibility requirements, failing if all locations are cleared
+        but not beatable, or some locations are unreachable.
+        """
         locations = [loc for loc in self.multiworld.get_locations()]
         state = CollectionState(self.multiworld)
         while locations:
@@ -30,6 +35,7 @@ class MultiworldTestBase(TestCase):
         return self.multiworld.has_beaten_game(state, 1)
 
     def assertSteps(self, steps: Tuple[str, ...]) -> None:
+        """Calls each step individually, continuing if a step for a specific world step fails."""
         world_types = {world.__class__ for world in self.multiworld.worlds.values()}
         for step in steps:
             for player, world in self.multiworld.worlds.items():
@@ -46,10 +52,13 @@ class TestAllGamesMultiworld(MultiworldTestBase):
     multiworld: MultiWorld
 
     def test_fills(self) -> None:
+        """Tests that a multiworld with one of every registered game world can generate."""
         all_worlds = list(AutoWorldRegister.world_types.values())
         # TODO LTTP requires a bunch of options not in the options API to function
         all_worlds.remove(ALTTPWorld)
         self.multiworld = setup_multiworld(all_worlds, ())
+        for world in self.multiworld.worlds.values():
+            world.options.accessibility.value = Accessibility.option_locations
         self.assertSteps(gen_steps)
         with self.subTest("filling multiworld", seed=self.multiworld.seed):
             distribute_items_restrictive(self.multiworld)
@@ -59,11 +68,14 @@ class TestAllGamesMultiworld(MultiworldTestBase):
 
 class TestTwoPlayerMulti(MultiworldTestBase):
     def test_two_player_single_game_fills(self) -> None:
+        """Tests that a multiworld of two players for each registered game world can generate."""
         for world in AutoWorldRegister.world_types.values():
             # TODO LTTP requires a bunch of options not in the options API to function
             if world is ALTTPWorld:
                 continue
             self.multiworld = setup_multiworld([world, world], ())
+            for world in self.multiworld.worlds.values():
+                world.options.accessibility.value = Accessibility.option_locations
             self.assertSteps(gen_steps)
         with self.subTest("filling multiworld", seed=self.multiworld.seed):
             distribute_items_restrictive(self.multiworld)
