535ceedc0daa8c39db458ea1d0d804d91f6d4b95
chandler05 <66492208+chandler05@users.noreply.github.com>
2023-12-13 18:03:22 -0600

    Simplify with variable

--- worlds/shorthike/Rules.py
+++ worlds/shorthike/Rules.py
@@ -9,52 +9,52 @@ def create_rules(self, location_table):
     for loc in location_table:
         if loc["needsShovel"]:
             forbid_items_for_player(world.get_location(loc["name"], player), self.item_name_groups['Maps'], player)
-            add_rule(world.get_location(loc["name"], self.player),
-                lambda state: state.has("Shovel", self.player))
+            add_rule(world.get_location(loc["name"], player),
+                lambda state: state.has("Shovel", player))
         if loc["purchase"] and not options.coins_in_shops:
             forbid_items_for_player(world.get_location(loc["name"], player), self.item_name_groups['Coins'], player)
 
         # Minimum Feather Rules
-        add_rule(world.get_location(loc["name"], self.player),
+        add_rule(world.get_location(loc["name"], player),
             lambda state: get_feather_state(self, loc["minGoldenFeathers"], loc["minGoldenFeathersEasy"], loc["minGoldenFeathersBucket"], state))
     add_rule(world.get_location("Shovel Kid Trade", player),
-        lambda state: state.has("Toy Shovel", self.player))
+        lambda state: state.has("Toy Shovel", player))
     add_rule(world.get_location("Sand Castle Golden Feather", player),
-        lambda state: state.has("Toy Shovel", self.player))
+        lambda state: state.has("Toy Shovel", player))
 
     # Fishing Rules
     add_rule(world.get_location("Catch All Fish Reward", player),
-        lambda state: state.has("Fishing Journal", self.player))
+        lambda state: state.has("Fishing Journal", player))
     add_rule(world.get_location("Catch 3 Fish Reward", player),
-        lambda state: state.has("Fishing Rod", self.player))
+        lambda state: state.has("Fishing Rod", player))
     add_rule(world.get_location("Catch Fish with Permit", player),
-        lambda state: state.has("Fishing Rod", self.player))
+        lambda state: state.has("Fishing Rod", player))
     add_rule(world.get_location("Catch All Fish Reward", player),
-        lambda state: state.has("Fishing Rod", self.player))
+        lambda state: state.has("Fishing Rod", player))
 
     # Misc Rules
     add_rule(world.get_location("Return Camping Permit", player),
-        lambda state: state.has("Camping Permit", self.player))
+        lambda state: state.has("Camping Permit", player))
     add_rule(world.get_location("Boat Challenge Reward", player),
-        lambda state: state.has("Motorboat Key", self.player))
+        lambda state: state.has("Motorboat Key", player))
     add_rule(world.get_location("Collect 15 Seashells", player),
-        lambda state: state.has("Seashell", self.player, 15))
+        lambda state: state.has("Seashell", player, 15))
     add_rule(world.get_location("Wristwatch Trade", player),
-        lambda state: state.has("Wristwatch", self.player))
+        lambda state: state.has("Wristwatch", player))
     add_rule(world.get_location("Sue the Rabbit Shoes Reward", player),
-        lambda state: state.has("Headband", self.player))
+        lambda state: state.has("Headband", player))
     add_rule(world.get_location("Return to Shell Kid", player),
-        lambda state: state.has("Shell Necklace", self.player))
+        lambda state: state.has("Shell Necklace", player))
     add_rule(world.get_location("Return to Shell Kid", player),
-        lambda state: state.has("Seashell", self.player, 15))
+        lambda state: state.has("Seashell", player, 15))
     add_rule(world.get_location("Ranger May Shell Necklace Golden Feather", player),
-        lambda state: state.has("Shell Necklace", self.player))
+        lambda state: state.has("Shell Necklace", player))
     add_rule(world.get_location("Beachstickball (10 Hits)", player),
-        lambda state: state.has("Stick", self.player))
+        lambda state: state.has("Stick", player))
     add_rule(world.get_location("Beachstickball (20 Hits)", player),
-        lambda state: state.has("Stick", self.player))
+        lambda state: state.has("Stick", player))
     add_rule(world.get_location("Beachstickball (30 Hits)", player),
-        lambda state: state.has("Stick", self.player))
+        lambda state: state.has("Stick", player))
 
 def get_feather_state(self, min_golden_feathers, min_golden_feathers_easy, min_golden_feathers_bucket, state):
     options = self.options
