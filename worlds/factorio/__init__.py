c0c840300d5d6c4e8070efe4e5432dab0e43f6b2
Katelyn Gigante <clockwork.singularity@gmail.com>
2023-12-16 15:41:27 +1100

    Back to aggressively wanting a lot of techs early.

--- worlds/factorio/__init__.py
+++ worlds/factorio/__init__.py
@@ -202,9 +202,17 @@ class Factorio(World):
                                                     want_progressives(self.multiworld.random))
 
         cost_sorted_locations = sorted(self.science_locations, key=lambda location: location.name)
-        special_index = {"automation": 0,
-                         "logistics": 1,
-                         "rocket-silo": -1}
+        special_index = {"rocket-silo": -1}
+        i = 0
+        priority_techs = []
+        priority_techs.append("automation")
+        priority_techs.append("logistics")
+        priority_techs.extend(base_technology_table["automation"].get_prior_technologies())
+        priority_techs.extend(base_technology_table["logistics"].get_prior_technologies())
+        for tech_name in priority_techs:
+            if tech_name not in special_index:
+                special_index[tech_name] = i
+                i += 1
 
         loc: FactorioScienceLocation
         if self.multiworld.tech_tree_information[player] == TechTreeInformation.option_full:
