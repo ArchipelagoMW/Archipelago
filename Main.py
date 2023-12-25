b5fccde91343931abd618ac16dce857d068c0397
CaitSith2 <d_good@caitsith2.com>
2023-12-24 16:50:40 -0800

    Didn't mean to include this debugging line.

--- Main.py
+++ Main.py
@@ -168,7 +168,6 @@ def main(args, seed=None, baked_server_options: Optional[Dict[str, object]] = No
     # remove starting inventory from pool items.
     # Because some worlds don't actually create items during create_items this has to be as late as possible.
     if any(getattr(world.worlds[player].options, "start_inventory_from_pool", StartInventoryPool({})).value for player in world.player_ids):
-        logging.info("At least one game has start_inventory_from_pool")
         new_items: List[Item] = []
         depletion_pool: Dict[int, Dict[str, int]] = {
             player: getattr(world.worlds[player].options, "start_inventory_from_pool", StartInventoryPool({})).value.copy()
