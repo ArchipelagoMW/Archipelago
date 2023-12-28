1b9c6c0967c7cbdee16dc6d0a4e9f7cec00c828f
lordlou <87331798+lordlou@users.noreply.github.com>
2023-12-28 06:14:13 -0500

    SM: strict rom validation fix (#2632)
    added a more robust ROM tag validation to free oher games to use tag starting with "SM" followed by another letter (SMW, SMZ3, SMRPG, SMMR,...)

--- worlds/sm/Client.py
+++ worlds/sm/Client.py
@@ -61,7 +61,7 @@ class SMSNIClient(SNIClient):
         from SNIClient import snes_buffered_write, snes_flush_writes, snes_read
 
         rom_name = await snes_read(ctx, SM_ROMNAME_START, ROMNAME_SIZE)
-        if rom_name is None or rom_name == bytes([0] * ROMNAME_SIZE) or rom_name[:2] != b"SM" or rom_name[:3] == b"SMW":
+        if rom_name is None or rom_name == bytes([0] * ROMNAME_SIZE) or rom_name[:2] != b"SM" or rom_name[2] not in b"1234567890":
             return False
 
         ctx.game = self.game
