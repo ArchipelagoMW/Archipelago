1e494fc3fd6606e8cd78751a08ccb7e63eff3986
Fabian Dill <fabian.dill@web.de>
2024-01-05 15:15:23 +0100

    CommonClient/MultiServer: fix /missing and !missing for location names with space

--- MultiServer.py
+++ MultiServer.py
@@ -1337,6 +1337,7 @@ class ClientMessageProcessor(CommonCommandProcessor):
                     "Sorry, !remaining requires you to have beaten the game on this server")
                 return False
 
+    @mark_raw
     def _cmd_missing(self, filter_text="") -> bool:
         """List all missing location checks from the server's perspective.
         Can be given text, which will be used as filter."""
