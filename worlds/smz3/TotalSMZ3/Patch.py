10df23f873692baf82b95f0fcdf344843272456c
lordlou <87331798+lordlou@users.noreply.github.com>
2024-01-07 07:13:52 -0500

    SMZ3: 0.4.4 backward compat client fix (#2667)
    fixed broken client compatibility with any seed generated before 0.4.4 introduced with the recent change to the message queue.

--- worlds/smz3/TotalSMZ3/Patch.py
+++ worlds/smz3/TotalSMZ3/Patch.py
@@ -616,7 +616,8 @@ class Patch:
                     "H" if self.myWorld.Config.SMLogic == Config.SMLogic.Hard else \
                     "X"
 
-        self.title = f"ZSM{Patch.Major}{Patch.Minor}{Patch.Patch}{z3Glitch}{smGlitch}{self.myWorld.Id}{self.seed:08x}".ljust(21)[:21]
+        from Utils import __version__
+        self.title = f"ZSM{Patch.Major}{Patch.Minor}{Patch.Patch}{__version__.replace('.', '')[0:3]}{z3Glitch}{smGlitch}{self.myWorld.Id}{self.seed:08x}".ljust(21)[:21]
         self.patches.append((Snes(0x00FFC0), bytearray(self.title, 'utf8')))
         self.patches.append((Snes(0x80FFC0), bytearray(self.title, 'utf8')))
     
