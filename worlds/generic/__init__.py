c9bf1a56fd69d48d0c30f58461236a1a07155f41
deoxis9001 <deoxis9001@users.noreply.github.com>
2024-01-11 12:22:39 +0100

    Add Guide init

--- worlds/generic/__init__.py
+++ worlds/generic/__init__.py
@@ -8,24 +8,42 @@ from NetUtils import SlotType
 
 
 class GenericWeb(WebWorld):
-    advanced_settings = Tutorial('Advanced YAML Guide',
+    # English Language
+    advanced_settings_en = Tutorial('Advanced YAML Guide',
                                  'A guide to reading YAML files and editing them to fully customize your game.',
                                  'English', 'advanced_settings_en.md', 'advanced_settings/en',
                                  ['alwaysintreble', 'Alchav'])
-    commands = Tutorial('Archipelago Server and Client Commands',
+    commands_en = Tutorial('Archipelago Server and Client Commands',
                         'A guide detailing the commands available to the user when participating in an Archipelago session.',
                         'English', 'commands_en.md', 'commands/en', ['jat2980', 'Ijwu'])
-    mac = Tutorial('Archipelago Setup Guide for Mac', 'A guide detailing how to run Archipelago clients on macOS.', 
+    mac_en = Tutorial('Archipelago Setup Guide for Mac', 'A guide detailing how to run Archipelago clients on macOS.', 
                    'English', 'mac_en.md','mac/en', ['Bicoloursnake'])
-    plando = Tutorial('Archipelago Plando Guide', 'A guide to understanding and using plando for your game.',
+    plando_en = Tutorial('Archipelago Plando Guide', 'A guide to understanding and using plando for your game.',
                       'English', 'plando_en.md', 'plando/en', ['alwaysintreble', 'Alchav'])
-    setup = Tutorial('Getting Started',
+    setup_en = Tutorial('Getting Started',
                      'A guide to setting up the Archipelago software, and generating, hosting, and connecting to '
                      'multiworld games.',
                      'English', 'setup_en.md', 'setup/en', ['alwaysintreble'])
-    triggers = Tutorial('Archipelago Triggers Guide', 'A guide to setting up and using triggers in your game settings.',
+    triggers_en = Tutorial('Archipelago Triggers Guide', 'A guide to setting up and using triggers in your game settings.',
                         'English', 'triggers_en.md', 'triggers/en', ['alwaysintreble'])
-    tutorials = [setup, mac, commands, advanced_settings, triggers, plando]
+    # French Language
+    advanced_settings_fr = Tutorial('Guide avancé YAML',
+                                 'Un guide pour lire les fichiers YAML et les éditer afin de personnaliser entièrement votre jeu.',
+                                 'Français', 'advanced_settings_fr.md', 'advanced_settings/fr',
+                                 ['Deoxis'])
+    commands_fr = Tutorial('Commandes du serveur et du client Archipelago',
+                        'Un guide détaillant les commandes disponibles pour l\'utilisateur lorsqu\'il participe à une session d\'Archipelago.',
+                        'Français', 'commands_fr.md', 'commands/fr', ['Deoxis'])
+    mac_fr = Tutorial('Guide d\'installation d\'Archipelago pour Mac', 'Un guide détaillant comment faire fonctionner les clients Archipelago sur macOS.', 
+                   'Français', 'mac_fr.md','mac/fr', ['Deoxis'])
+    plando_fr = Tutorial('Guide du Plando d\'Archipelago', 'Un guide pour comprendre et utiliser le plando pour votre jeu.',
+                      'Français', 'plando_fr.md', 'plando/fr', ['Deoxis'])
+    setup_fr = Tutorial('Pour commencer',
+                     'Un guide pour configurer le logiciel Archipelago, et générer, héberger et se connecter à des jeux MultiWorld.',
+                     'Français', 'setup_fr.md', 'setup/fr', ['Deoxis'])
+    triggers_fr = Tutorial('Guide des Triggers d\'Archipelago', 'Un guide pour configurer et utiliser les triggers dans les paramètres de votre jeu.',
+                        'Français', 'triggers_fr.md', 'triggers/fr', ['Deoxis'])
+    tutorials = [setup_en, mac_en, commands_en, advanced_settings_en, triggers_en, plando_en, setup_fr, mac_fr, commands_fr, advanced_settings_fr, triggers_fr, plando_fr]
 
 
 class GenericWorld(World):
