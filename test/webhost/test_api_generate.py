fa71e4cf65f3b11eea3898eb206527bb62ce6207
Remy Jette <remy@remyjette.com>
2024-01-11 14:44:12 -0800

    WebHost: Fix /api/generate (#2693)

--- test/webhost/test_api_generate.py
+++ test/webhost/test_api_generate.py
@@ -1,5 +1,7 @@
+import io
 import unittest
 import json
+import yaml
 
 
 class TestDocs(unittest.TestCase):
@@ -23,7 +25,7 @@ class TestDocs(unittest.TestCase):
         response = self.client.post("/api/generate")
         self.assertIn("No options found. Expected file attachment or json weights.", response.text)
 
-    def test_generation_queued(self):
+    def test_generation_queued_weights(self):
         options = {
             "Tester1":
                 {
@@ -36,6 +38,22 @@ class TestDocs(unittest.TestCase):
             "/api/generate",
             data=json.dumps({"weights": options}),
             content_type='application/json'
+        )
+        json_data = response.get_json()
+        self.assertTrue(json_data["text"].startswith("Generation of seed "))
+        self.assertTrue(json_data["text"].endswith(" started successfully."))
+
+    def test_generation_queued_file(self):
+        options = {
+            "game": "Archipelago",
+            "name": "Tester",
+            "Archipelago": {}
+        }
+        response = self.client.post(
+            "/api/generate",
+            data={
+                'file': (io.BytesIO(yaml.dump(options, encoding="utf-8")), "test.yaml")
+            },
         )
         json_data = response.get_json()
         self.assertTrue(json_data["text"].startswith("Generation of seed "))
