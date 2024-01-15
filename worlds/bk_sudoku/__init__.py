43fd49e6c0796432e37e0521f46bfb76895eb493
Held_der_Zeit <76132257+ZeitHeld@users.noreply.github.com>
2024-01-15 16:19:36 +0100

    Update __init__.py

--- worlds/bk_sudoku/__init__.py
+++ worlds/bk_sudoku/__init__.py
@@ -9,20 +9,20 @@ class Bk_SudokuWebWorld(WebWorld):
     theme = 'partyTime'
 
     setup_en = Tutorial(
-            tutorial_name='Setup Guide',
-            description='A guide to playing BK Sudoku',
-            language='English',
-            file_name='setup_en.md',
-            link='setup/en',
-            authors=['Jarno']
+        tutorial_name='Setup Guide',
+        description='A guide to playing BK Sudoku',
+        language='English',
+        file_name='setup_en.md',
+        link='setup/en',
+        authors=['Jarno']
     )
     setup_de = Tutorial(
-            tutorial_name='Setup Anleitung',
-            description='Eine Anleitung um BK-Sudoku zu spielen',
-            language='Deutsch',
-            file_name='setup_de.md',
-            link='setup/de',
-            authors=['Held_der_Zeit']
+        tutorial_name='Setup Anleitung',
+        description='Eine Anleitung um BK-Sudoku zu spielen',
+        language='Deutsch',
+        file_name='setup_de.md',
+        link='setup/de',
+        authors=['Held_der_Zeit']
     )
     
     tutorials = [setup_en, setup_de]
