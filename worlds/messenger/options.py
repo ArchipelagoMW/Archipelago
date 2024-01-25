56889f5fda846ed4121a49fa3f62427cd5fc4343
alwaysintreble <mmmcheese158@gmail.com>
2024-01-25 03:18:57 -0600

    does not actually support plando yet

--- worlds/messenger/options.py
+++ worlds/messenger/options.py
@@ -57,7 +57,6 @@ class ShufflePortals(TextChoice):
     """
     Whether the portals lead to random places.
     Entering a portal from its vanilla area will always lead to HQ, and will unlock it if relevant.
-    Supports plando.
     
     None: Portals will take you where they're supposed to.
     Shops: Portals can lead to any area except Music Box and Elemental Skylands, with each portal output guaranteed to not overlap with another portal's. Will only put you at a portal or a shop.
