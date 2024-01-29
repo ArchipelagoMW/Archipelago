96f7fb5cec6c85efce56cab87f6dead20f98226e
alwaysintreble <mmmcheese158@gmail.com>
2024-01-29 17:24:21 -0600

    Core: have webhost slot name links go through the launcher so that components can use them

--- CommonClient.py
+++ CommonClient.py
@@ -885,7 +885,8 @@ def get_base_parser(description: typing.Optional[str] = None):
     return parser
 
 
-def run_as_textclient():
+def run_as_textclient(*args):
+    logger.info(args)
     class TextContext(CommonContext):
         # Text Mode to use !hint and such with games that have no text entry
         tags = CommonContext.tags | {"TextOnly"}
