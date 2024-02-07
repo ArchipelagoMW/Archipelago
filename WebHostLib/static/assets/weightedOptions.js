ebf1dbbe85dcb9b3589b94e4146dfc9b2bf427d0
Chris Wilson <chris@legendserver.info>
2024-02-06 23:36:46 -0500

    Add JS functionality for collapsing game divs, populating span values on range updates. Add <noscript> tag to warn users with JS disabled.

--- WebHostLib/static/assets/weightedOptions.js
+++ WebHostLib/static/assets/weightedOptions.js
@@ -1,3 +1,34 @@
 window.addEventListener('load', () => {
-  console.log('System ready.');
+  // Generic change listener. Detecting unique qualities and acting on them here reduces initial JS initialisation time
+  // and handles dynamically created elements
+  document.addEventListener('change', (evt) => {
+    // Handle clicks on world names
+    if (evt.target.type === 'checkbox' && evt.target.classList.contains('world-collapse')) {
+      // Collapse all option groups within the world if the checkbox was unchecked
+      if (!evt.target.checked) {
+        const worldName = evt.target.getAttribute('data-world-name');
+        document.querySelectorAll(`input[type=checkbox].group-collapse`).forEach((checkbox) => {
+          if (checkbox.hasAttribute('data-world-name') && checkbox.getAttribute('data-world-name') === worldName) {
+            checkbox.checked = false;
+          }
+        });
+      }
+    }
+
+    // Handle updates to range inputs
+    if (evt.target.type === 'range') {
+      // Update span containing range value. All ranges have a corresponding `{rangeId}-value` span
+      document.getElementById(`${evt.target.id}-value`).innerText = evt.target.value;
+    }
+  });
+
+  // TODO: Handle creating new rows for Range options
+
+  // Detect form submission
+  document.getElementById('weighted-options-form').addEventListener('submit', (evt) => {
+    // TODO: Save data to localStorage
+    evt.preventDefault();
+  });
+
+  // TODO: Populate all settings from localStorage on page initialisation
 });
