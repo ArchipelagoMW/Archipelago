1d166eb27402d4f2169a120af060c4fd900990a9
Scipio Wright <scipiowright@gmail.com>
2024-02-07 21:55:04 -0500

    Removed an entrance rule to prevent people from being expected to softlock themselves

--- worlds/tunic/options.py
+++ worlds/tunic/options.py
@@ -37,8 +37,9 @@ class LogicRules(Choice):
     """Set which logic rules to use for your world.
     Restricted: Standard logic, no glitches.
     No Major Glitches: Ice grapples through doors, shooting the west bell, and boss quick kills are included in logic.
+    * Ice grappling through the Ziggurat door is not in logic since you will get stuck in there without Prayer
     Unrestricted: Logic in No Major Glitches, as well as ladder storage to get to certain places early.
-    *Special Shop is not in logic without the Hero's Laurels in Unrestricted due to soft lock potential.
+    *Special Shop is not in logic without the Hero's Laurels due to soft lock potential.
     *Using Ladder Storage to get to individual chests is not in logic to avoid tedium.
     *Getting knocked out of the air by enemies during Ladder Storage to reach places is not in logic, except for in
     Rooted Ziggurat Lower. This is so you're not punished for playing with enemy rando on."""
