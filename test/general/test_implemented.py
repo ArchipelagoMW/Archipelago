e315cc7ab1d4a0b9976d580e663a5dbefda65482
alwaysintreble <mmmcheese158@gmail.com>
2024-02-13 22:35:51 -0600

    Tests: add a test that checks the state of the multiworld for determinism

--- test/general/test_implemented.py
+++ test/general/test_implemented.py
@@ -47,3 +47,32 @@ class TestImplemented(unittest.TestCase):
                 for key, data in multiworld.worlds[1].fill_slot_data().items():
                     self.assertIsInstance(key, str, "keys in slot data must be a string")
                     self.assertIsInstance(encode(data), str, f"object {type(data).__name__} not serializable.")
+
+    def test_world_determinism(self):
+        """Tests that the state of a generated multiworld is the same per world."""
+        for game_name, world_type in AutoWorldRegister.world_types.items():
+            with self.subTest(game=game_name):
+                multi_one = setup_solo_multiworld(world_type, seed=0)
+                multi_two = setup_solo_multiworld(world_type, seed=0)
+                self.assertEqual(multi_one.random.randrange(99999), multi_two.random.randrange(99999))
+                for region_name in multi_one.regions.region_cache:
+                    self.assertIn(region_name, multi_two.regions.region_cache)
+                    self.assertEqual(list(multi_one.regions.region_cache.keys()).index(region_name),
+                                     list(multi_two.regions.region_cache.keys()).index(region_name),
+                                     "regions were created in a separate order")
+                for entrance_name in multi_one.regions.entrance_cache:
+                    self.assertIn(region_name, multi_two.regions.entrance_cache)
+                    self.assertEqual(list(multi_one.regions.entrance_cache.keys()).index(entrance_name),
+                                     list(multi_two.regions.entrance_cache.keys()).index(entrance_name),
+                                     "entrances were created in a different order")
+                for location_name in multi_one.regions.location_cache:
+                    self.assertIn(region_name, multi_two.regions.location_cache)
+                    self.assertEqual(list(multi_one.regions.location_cache.keys()).index(location_name),
+                                     list(multi_two.regions.location_cache.keys()).index(location_name),
+                                     "locations were created in a different order")
+                for multi_one_loc in multi_one.get_filled_locations():
+                    multi_two_loc = multi_two.get_location(multi_one_loc.name, 1)
+                    self.assertEqual(multi_one_loc.item, multi_two_loc.item,
+                                     f"{multi_one_loc} has a different item placed on it between seeds: "
+                                     f"{multi_one_loc.item}, {multi_two_loc.item}")
+                self.assertEqual(multi_one.itempool, multi_two.itempool)
