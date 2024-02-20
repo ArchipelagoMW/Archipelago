d347ae62da42752925a23f35b0983ecf61829c87
black-sliver <59490463+black-sliver@users.noreply.github.com>
2024-02-20 09:22:28 +0100

    Speedups: intset: make `gcc -pedantic -std=c99 -fanalyzer` without warnings
    Unnamed unions are not in C99, this got fixed.
    The overhead of setting count=0 is minimal or optimized-out and silences -fanalizer (see comment).

--- intset.h
+++ intset.h
@@ -23,13 +23,14 @@
 
 #define INTSET_FUNC(name) INTSET_FUNC_(INTSET_NAME, name)
 #define INTSET_BUCKET INTSET_CONCAT(INTSET_NAME, Bucket)
+#define INTSET_UNION INTSET_CONCAT(INTSET_NAME, Union)
 
 typedef struct {
     size_t count;
-    union {
+    union INTSET_UNION {
         INTSET_TYPE val;
         INTSET_TYPE *data;
-    };
+    } v;
 } INTSET_BUCKET;
 
 typedef struct {
@@ -39,13 +40,19 @@ typedef struct {
 
 static INTSET_NAME *INTSET_FUNC(new)(size_t buckets)
 {
+    size_t i, size;
+    INTSET_NAME *set;
+
     if (buckets < 1)
         buckets = 1;
-    size_t size = sizeof(INTSET_NAME) + buckets * sizeof(INTSET_BUCKET);
-    INTSET_NAME *set = (INTSET_NAME*)malloc(size);
+    size = sizeof(INTSET_NAME) + buckets * sizeof(INTSET_BUCKET);
+    set = (INTSET_NAME*)malloc(size);
     if (!set)
         return NULL;
-    memset(set, 0, size);
+    memset(set, 0, size); /* gcc -fanalyzer does not understand this sets all buckets' count to 0 */
+    for (i = 0; i < buckets; i++) {
+        set->buckets[i].count = 0;
+    }
     set->bucket_count = buckets;
     return set;
 }
@@ -55,7 +62,7 @@ static void INTSET_FUNC(free)(INTSET_NAME *set)
     size_t i;
     for (i = 0; i < set->bucket_count; i++) {
         if (set->buckets[i].count > 1)
-            free(set->buckets[i].data);
+            free(set->buckets[i].v.data);
     }
     free(set);
 }
@@ -63,11 +70,11 @@ static void INTSET_FUNC(free)(INTSET_NAME *set)
 static bool INTSET_FUNC(contains)(INTSET_NAME *set, INTSET_TYPE val)
 {
     size_t i;
-    INTSET_BUCKET* bucket = set->buckets + ((size_t)val % set->bucket_count);
+    INTSET_BUCKET* bucket = &set->buckets[(size_t)val % set->bucket_count];
     if (bucket->count == 1)
-        return bucket->val == val;
+        return bucket->v.val == val;
     for (i = 0; i < bucket->count; ++i) {
-        if (bucket->data[i] == val)
+        if (bucket->v.data[i] == val)
             return true;
     }
     return false;
@@ -80,33 +87,34 @@ static bool INTSET_FUNC(add)(INTSET_NAME *set, INTSET_TYPE val)
     if (INTSET_FUNC(contains)(set, val))
         return true; /* ok */
 
-    bucket = set->buckets + ((size_t)val % set->bucket_count);
+    bucket = &set->buckets[(size_t)val % set->bucket_count];
     if (bucket->count == 0) {
-        bucket->val = val;
+        bucket->v.val = val;
         bucket->count = 1;
     } else if (bucket->count == 1) {
-        INTSET_TYPE old = bucket->val;
-        bucket->data = (INTSET_TYPE*)malloc(2 * sizeof(INTSET_TYPE));
-        if (!bucket->data) {
-            bucket->val = old;
+        INTSET_TYPE old = bucket->v.val;
+        bucket->v.data = (INTSET_TYPE*)malloc(2 * sizeof(INTSET_TYPE));
+        if (!bucket->v.data) {
+            bucket->v.val = old;
             return false; /* error */
         }
-        bucket->data[0] = old;
-        bucket->data[1] = val;
+        bucket->v.data[0] = old;
+        bucket->v.data[1] = val;
         bucket->count = 2;
     } else {
         size_t new_bucket_size;
         INTSET_TYPE* new_bucket_data;
 
         new_bucket_size = (bucket->count + 1) * sizeof(INTSET_TYPE);
-        new_bucket_data = (INTSET_TYPE*)realloc(bucket->data, new_bucket_size);
+        new_bucket_data = (INTSET_TYPE*)realloc(bucket->v.data, new_bucket_size);
         if (!new_bucket_data)
             return false; /* error */
-        bucket->data = new_bucket_data;
-        bucket->data[bucket->count++] = val;
+        bucket->v.data = new_bucket_data;
+        bucket->v.data[bucket->count++] = val;
     }
     return true; /* success */
 }
 
 #undef INTSET_FUNC
 #undef INTSET_BUCKET
+#undef INTSET_UNION
