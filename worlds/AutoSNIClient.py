64f5153248b527b73eba26dbac05655aa56e3185
Silvris <58583688+Silvris@users.noreply.github.com>
2024-02-27 01:20:31 -0600

    dynamic SNIClient extensions

--- worlds/AutoSNIClient.py
+++ worlds/AutoSNIClient.py
@@ -3,8 +3,13 @@ from __future__ import annotations
 import abc
 from typing import TYPE_CHECKING, ClassVar, Dict, Tuple, Any, Optional
 
+from worlds.LauncherComponents import Component, SuffixIdentifier, Type, components
+
 if TYPE_CHECKING:
     from SNIClient import SNIContext
+
+component = Component('SNI Client', 'SNIClient', component_type=Type.CLIENT, file_identifier=SuffixIdentifier(".apsoe"))
+components.append(component)
 
 
 class AutoSNIClientRegister(abc.ABCMeta):
@@ -15,6 +20,19 @@ class AutoSNIClientRegister(abc.ABCMeta):
         new_class = super().__new__(cls, name, bases, dct)
         if "game" in dct:
             AutoSNIClientRegister.game_handlers[dct["game"]] = new_class()
+
+        if "patch_suffix" in dct:
+            if dct["patch_suffix"] is not None:
+                existing_identifier: SuffixIdentifier = component.file_identifier
+                new_suffixes = [*existing_identifier.suffixes]
+
+                if type(dct["patch_suffix"]) is str:
+                    new_suffixes.append(dct["patch_suffix"])
+                else:
+                    new_suffixes.extend(dct["patch_suffix"])
+
+                component.file_identifier = SuffixIdentifier(*new_suffixes)
+
         return new_class
 
     @staticmethod
