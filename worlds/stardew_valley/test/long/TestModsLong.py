b32119ff6b8ffa43d18ec20094f39f3674c93031
Jouramie <jouramie@hotmail.com>
2024-02-28 22:58:29 -0500

    fix Jaster tests

--- worlds/stardew_valley/test/long/TestModsLong.py
+++ worlds/stardew_valley/test/long/TestModsLong.py
@@ -39,8 +39,8 @@ class TestGenerateModsOptions(WorldAssertMixin, ModAssertMixin, SVTestCase):
     # @unittest.skip
     def test_troubleshoot_option(self):
         world_options = {
-            options.SkillProgression: options.SkillProgression.option_vanilla,
-            options.Mods: ModNames.archaeology
+            options.Friendsanity: options.Friendsanity.option_all,
+            options.Mods: ModNames.jasper
         }
 
         with self.solo_world_sub_test(world_options=world_options, seed=12619010600507985842, world_caching=False) as (multiworld, _):
