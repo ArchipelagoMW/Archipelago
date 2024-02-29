ca7d18f559a68540e727f7f6a322e6dbb8918d37
electro15 <electro1512@gmail.com>
2024-02-29 17:53:59 -0500

    Messing with init to try to get this to compile (attempt 1).

--- worlds/metroidprime/__init__.py
+++ worlds/metroidprime/__init__.py
@@ -1,10 +1,11 @@
-from BaseClasses import Item, MultiWorld, Tutorial, ItemClassification
+from BaseClasses import Item
 from .Items import MetroidPrimeItem, suit_upgrade_table, artifact_table, item_table
 from .PrimeOptions import metroidprime_options
 from .Locations import every_location
 from .Regions import create_regions
 from .Rules import set_rules
 from worlds.AutoWorld import World
+
 
 class MetroidPrimeWorld(World):
     """
@@ -16,7 +17,7 @@ class MetroidPrimeWorld(World):
     option_definitions = metroidprime_options
     topology_present = True
     item_name_to_id = {name: data.code for name, data in item_table.items()}
-    location_name_to_id = {loc_data.name: loc_data.id for loc_data in every_location}
+    location_name_to_id = every_location
 
     def generate_early(self):
         reqarts = self.option_definitions['required_artifacts']
@@ -59,26 +60,22 @@ class MetroidPrimeWorld(World):
                 items_added += 1
                 continue
             elif i == "Missile Expansion":
-                if spring:
-                    self.multiworld.itempool += [self.create_item("Missile Expansion") for i in range(0, 49)]
-                    items_added += 49
-                else:
-                    self.multiworld.itempool += [self.create_item("Missile Expansion") for i in range(0, 50)]
-                    items_added += 50
                 continue
             elif i == "Energy Tank":
-                self.multiworld.itempool += [self.create_item("Energy Tank") for i in range(0, 14)]
+                for j in range(0, 14):
+                    self.multiworld.itempool += [self.create_item("Energy Tank")]
                 items_added += 14
                 continue
             elif i == "Ice Trap":
                 continue
             elif i == "Power Bomb Expansion":
-                self.multiworld.itempool += [self.create_item("Power Bomb Expansion") for i in range(0, 4)]
+                for j in range(0, 4):
+                    self.multiworld.itempool += [self.create_item("Power Bomb Expansion")]
                 items_added += 4
             else:
                 self.multiworld.itempool += [self.create_item(i)]
                 items_added += 1
-        # add missiles as extra filler
+        # add missiles in whatever slots we have left
         while 100 - items_added > 0:
             self.multiworld.itempool += [self.create_item("Missile Expansion")]
             items_added += 1
