f0e91383be5911508ae450539aae80bf8499ce4c
electro15 <electro1512@gmail.com>
2024-03-03 02:11:53 -0500

    BUG FIXES PASS 18.1: Fixed an issue where the number of items added to the pool was not tracked correctly.

--- worlds/metroidprime/__init__.py
+++ worlds/metroidprime/__init__.py
@@ -94,7 +94,7 @@ class MetroidPrimeWorld(World):
                 items_added += 1
         # add missiles in whatever slots we have left
         remain = 100 - items_added
-        for i in range(0, remain-1):
+        for i in range(0, remain):
             self.multiworld.itempool += [self.create_item("Missile Expansion")]
 
     def set_rules(self) -> None:
