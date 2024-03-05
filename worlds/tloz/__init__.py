69cdc044238bef0f48b78b888f20d021b6c9a466
t3hf1gm3nt <59876300+t3hf1gm3nt@users.noreply.github.com>
2024-03-05 16:55:59 -0500

    TLOZ: use proper rule for completion condition (#2872)
    Was pointed out that using `state.locations.checked` is not the best solution, even if it's for a completion condition and the Ganon event location would always have the Triforce of Power event item. So let's just check for the Triforce of Power instead. Thank you Zunawe for pointing it out and Silvris for providing the proper rule to use.

--- worlds/tloz/__init__.py
+++ worlds/tloz/__init__.py
@@ -180,7 +180,7 @@ class TLoZWorld(World):
 
         self.multiworld.get_location("Zelda", self.player).place_locked_item(self.create_event("Rescued Zelda!"))
         add_rule(self.multiworld.get_location("Zelda", self.player),
-                 lambda state: ganon in state.locations_checked)
+                 lambda state: state.has("Triforce of Power", self.player))
         self.multiworld.completion_condition[self.player] = lambda state: state.has("Rescued Zelda!", self.player)
 
     def apply_base_patch(self, rom):
