f3feb6b7f8bcdc5e7445b34c7142d8f64633588b
Scipio Wright <scipiowright@gmail.com>
2024-03-05 22:02:00 -0500

    Re-fix that quarry bug

--- worlds/tunic/regions.py
+++ worlds/tunic/regions.py
@@ -47,7 +47,7 @@ tunic_ladder_regions: Dict[str, Set[str]] = {
     "Eastern Vault Fortress": {"Beneath the Vault"},
     "Beneath the Vault": {"Eastern Vault Fortress"},
     "Quarry Back": {"Quarry"},
-    "Quarry": {"Lower Quarry", "Rooted Ziggurat"},
+    "Quarry": {"Lower Quarry"},
     "Lower Quarry": {"Rooted Ziggurat"},
     "Rooted Ziggurat": set(),
     "Swamp": {"Swamp Middle"},
