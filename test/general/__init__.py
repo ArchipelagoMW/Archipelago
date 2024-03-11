cb9d3d581d25558790afa4fd45b2a062c00e8f55
Aaron Wagener <mmmcheese158@gmail.com>
2024-03-11 17:22:30 -0500

    Tests: add test for 2-player-multiworlds (#2386)
    * Tests: add test for all games multiworld and test for two player multiworld per game
    
    * make assertSteps behave like call_all
    
    * review improvements
    
    * fix stage calling and loc copying in accessibility
    
    * add docstrings
    
    * lttp is on the options api now
    
    * skip the all games multiworld for now. likely needs to be modified to test specific worlds
    
    * move skip to the class

--- test/general/__init__.py
+++ test/general/__init__.py
@@ -1,8 +1,8 @@
 from argparse import Namespace
-from typing import Optional, Tuple, Type
+from typing import List, Optional, Tuple, Type, Union
 
-from BaseClasses import MultiWorld, CollectionState
-from worlds.AutoWorld import call_all, World
+from BaseClasses import CollectionState, MultiWorld
+from worlds.AutoWorld import World, call_all
 
 gen_steps = ("generate_early", "create_regions", "create_items", "set_rules", "generate_basic", "pre_fill")
 
@@ -18,14 +18,33 @@ def setup_solo_multiworld(
     steps through pre_fill
     :param seed: The seed to be used when creating this multiworld
     """
-    multiworld = MultiWorld(1)
-    multiworld.game[1] = world_type.game
-    multiworld.player_name = {1: "Tester"}
+    return setup_multiworld(world_type, steps, seed)
+
+
+def setup_multiworld(worlds: Union[List[Type[World]], Type[World]], steps: Tuple[str, ...] = gen_steps,
+    seed: Optional[int] = None) -> MultiWorld:
+    """
+    Creates a multiworld with a player for each provided world type, allowing duplicates, setting default options, and
+    calling the provided gen steps.
+    
+    :param worlds: type/s of worlds to generate a multiworld for
+    :param steps: gen steps that should be called before returning. Default calls through pre_fill
+    :param seed: The seed to be used when creating this multiworld
+    """
+    if not isinstance(worlds, list):
+        worlds = [worlds]
+    players = len(worlds)
+    multiworld = MultiWorld(players)
+    multiworld.game = {player: world_type.game for player, world_type in enumerate(worlds, 1)}
+    multiworld.player_name = {player: f"Tester{player}" for player in multiworld.player_ids}
     multiworld.set_seed(seed)
     multiworld.state = CollectionState(multiworld)
     args = Namespace()
-    for name, option in world_type.options_dataclass.type_hints.items():
-        setattr(args, name, {1: option.from_any(option.default)})
+    for player, world_type in enumerate(worlds, 1):
+        for key, option in world_type.options_dataclass.type_hints.items():
+            updated_options = getattr(args, key, {})
+            updated_options[player] = option.from_any(option.default)
+            setattr(args, key, updated_options)
     multiworld.set_options(args)
     for step in steps:
         call_all(multiworld, step)
