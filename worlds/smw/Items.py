0c8e8e448e88ed5203941876fc531d4833e5491d
PoryGone <98504756+PoryGone@users.noreply.github.com>
2024-03-12 17:00:13 -0400

    SMW: v2.0 Content Update (#2762)
    Changelog:
    
    Features:
    - New optional Location Checks
      - 3-Up Moons
      - Hidden 1-Ups
      - Bonus Blocks
      - Blocksanity
        - All blocks that contain coins or items are included, with the exception of:
          - Blocks in Top Secret Area & Front Door/Bowser Castle
          - Blocks that are unreachable without glitches/unreasonable movement
    - New Items
      - Special Zone Clear
      - New Filler Items
        - 1 Coin
        - 5 Coins
        - 10 Coins
        - 50 Coins
      - New Trap Items
        - Reverse Trap
        - Thwimp Trap
    - SFX Shuffle
    - Palette Shuffle Overhaul
      - New Curated Palette can now be used for the Overworld and Level Palette Shuffle options
      - Foreground and Background Shuffle options have been merged into a single setting
    - Max possible Yoshi Egg value is 255
      - UI in-game is updated to handle 3-digits
      - New `Display Received Item Popups` option: `progression_minus_yoshi_eggs`
    
    Quality of Life:
    - In-Game Indicators are now displayed on the map screen for location checks and received items
    - In-level sprites are displayed upon receiving certain items
    - The Camera Scroll unlocking is now only enabled on levels where it needs to be
    - SMW can now handle receiving more than 255 items
    - Significant World Code cleanup
      - New Options API
      - Removal of `world: MultiWorld` across the world
    - The PopTracker pack now has tabs for every level/sublevel, and can automatically swap tabs while playing if connected to the server
    
    Bug Fixes:
    - Several logic tweaks/fixes
    
    "Major credit to @TheLX5 for being the driving force for almost all of this update. We've been collaborating on design and polish of the features for the last few months, but all of the heavy lifting was all @TheLX5."

--- worlds/smw/Items.py
+++ worlds/smw/Items.py
@@ -18,6 +18,10 @@ class SMWItem(Item):
 
 # Separate tables for each type of item.
 junk_table = {
+    ItemName.one_coin:        ItemData(0xBC0017, False),
+    ItemName.five_coins:      ItemData(0xBC0018, False),
+    ItemName.ten_coins:       ItemData(0xBC0019, False),
+    ItemName.fifty_coins:     ItemData(0xBC001A, False),
     ItemName.one_up_mushroom: ItemData(0xBC0001, False),
 }
 
@@ -36,6 +40,7 @@ upgrade_table = {
     ItemName.progressive_powerup: ItemData(0xBC000A, True),
     ItemName.p_balloon:           ItemData(0xBC000B, True),
     ItemName.super_star_active:   ItemData(0xBC000D, True),
+    ItemName.special_world_clear: ItemData(0xBC001B, True),
 }
 
 switch_palace_table = {
@@ -46,10 +51,12 @@ switch_palace_table = {
 }
 
 trap_table = {
-    ItemName.ice_trap:        ItemData(0xBC0013, False, True),
-    ItemName.stun_trap:       ItemData(0xBC0014, False, True),
-    ItemName.literature_trap: ItemData(0xBC0015, False, True),
-    ItemName.timer_trap:      ItemData(0xBC0016, False, True),
+    ItemName.ice_trap:              ItemData(0xBC0013, False, True),
+    ItemName.stun_trap:             ItemData(0xBC0014, False, True),
+    ItemName.literature_trap:       ItemData(0xBC0015, False, True),
+    ItemName.timer_trap:            ItemData(0xBC0016, False, True),
+    ItemName.reverse_controls_trap: ItemData(0xBC001C, False, True),
+    ItemName.thwimp_trap:           ItemData(0xBC001D, False, True),
 }
 
 event_table = {
