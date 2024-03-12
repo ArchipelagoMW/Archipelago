f97caa1c7b11f86f2b4564376519c4bd6bacedba
GodlFire <46984098+GodlFire@users.noreply.github.com>
2024-03-11 19:58:00 -0600

    Added sub regions for Ixupi
    -Added sub regions for Ixupi and moved ixupi capture checks into the sub region.
    -Added missing wax capture possible spot in Shaman room

--- worlds/shivers/Rules.py
+++ worlds/shivers/Rules.py
@@ -8,53 +8,43 @@ if TYPE_CHECKING:
 
 
 def water_capturable(state: CollectionState, player: int) -> bool:
-    return (state.can_reach("Lobby", "Region", player) or (state.can_reach("Janitor Closet", "Region", player) and cloth_capturable(state, player))) \
-        and state.has_all({"Water Pot Bottom", "Water Pot Top", "Water Pot Bottom DUPE", "Water Pot Top DUPE"}, player)
+    return state.has_all({"Water Pot Bottom", "Water Pot Top", "Water Pot Bottom DUPE", "Water Pot Top DUPE"}, player)
 
 
 def wax_capturable(state: CollectionState, player: int) -> bool:
-    return (state.can_reach("Library", "Region", player) or state.can_reach("Anansi", "Region", player)) \
-        and state.has_all({"Wax Pot Bottom", "Wax Pot Top", "Wax Pot Bottom DUPE", "Wax Pot Top DUPE"}, player)
+    return state.has_all({"Wax Pot Bottom", "Wax Pot Top", "Wax Pot Bottom DUPE", "Wax Pot Top DUPE"}, player)
 
 
 def ash_capturable(state: CollectionState, player: int) -> bool:
-    return (state.can_reach("Office", "Region", player) or state.can_reach("Burial", "Region", player)) \
-        and state.has_all({"Ash Pot Bottom", "Ash Pot Top", "Ash Pot Bottom DUPE", "Ash Pot Top DUPE"}, player)
+    return state.has_all({"Ash Pot Bottom", "Ash Pot Top", "Ash Pot Bottom DUPE", "Ash Pot Top DUPE"}, player)
 
 
 def oil_capturable(state: CollectionState, player: int) -> bool:
-    return (state.can_reach("Prehistoric", "Region", player) or state.can_reach("Tar River", "Region", player)) \
-        and state.has_all({"Oil Pot Bottom", "Oil Pot Top", "Oil Pot Bottom DUPE", "Oil Pot Top DUPE"}, player)
+    return state.has_all({"Oil Pot Bottom", "Oil Pot Top", "Oil Pot Bottom DUPE", "Oil Pot Top DUPE"}, player)
 
 
 def cloth_capturable(state: CollectionState, player: int) -> bool:
-    return (state.can_reach("Egypt", "Region", player) or state.can_reach("Burial", "Region", player) or state.can_reach("Janitor Closet", "Region", player)) \
-        and state.has_all({"Cloth Pot Bottom", "Cloth Pot Top", "Cloth Pot Bottom DUPE", "Cloth Pot Top DUPE"}, player)
+    return state.has_all({"Cloth Pot Bottom", "Cloth Pot Top", "Cloth Pot Bottom DUPE", "Cloth Pot Top DUPE"}, player)
 
 
 def wood_capturable(state: CollectionState, player: int) -> bool:
-    return (state.can_reach("Workshop", "Region", player) or state.can_reach("Blue Maze", "Region", player) or state.can_reach("Gods Room", "Region", player) or state.can_reach("Anansi", "Region", player)) \
-        and state.has_all({"Wood Pot Bottom", "Wood Pot Top", "Wood Pot Bottom DUPE", "Wood Pot Top DUPE"}, player)
+    return state.has_all({"Wood Pot Bottom", "Wood Pot Top", "Wood Pot Bottom DUPE", "Wood Pot Top DUPE"}, player)
 
 
 def crystal_capturable(state: CollectionState, player: int) -> bool:
-    return (state.can_reach("Lobby", "Region", player) or state.can_reach("Ocean", "Region", player)) \
-        and state.has_all({"Crystal Pot Bottom", "Crystal Pot Top", "Crystal Pot Bottom DUPE", "Crystal Pot Top DUPE"}, player)
+    return state.has_all({"Crystal Pot Bottom", "Crystal Pot Top", "Crystal Pot Bottom DUPE", "Crystal Pot Top DUPE"}, player)
 
 
 def sand_capturable(state: CollectionState, player: int) -> bool:
-    return (state.can_reach("Greenhouse", "Region", player) or state.can_reach("Ocean", "Region", player)) \
-        and state.has_all({"Sand Pot Bottom", "Sand Pot Top", "Sand Pot Bottom DUPE", "Sand Pot Top DUPE"}, player)
+    return state.has_all({"Sand Pot Bottom", "Sand Pot Top", "Sand Pot Bottom DUPE", "Sand Pot Top DUPE"}, player)
 
 
 def metal_capturable(state: CollectionState, player: int) -> bool:
-    return (state.can_reach("Projector Room", "Region", player) or state.can_reach("Prehistoric", "Region", player) or state.can_reach("Bedroom", "Region", player)) \
-        and state.has_all({"Metal Pot Bottom", "Metal Pot Top", "Metal Pot Bottom DUPE", "Metal Pot Top DUPE"}, player)
+    return state.has_all({"Metal Pot Bottom", "Metal Pot Top", "Metal Pot Bottom DUPE", "Metal Pot Top DUPE"}, player)
 
 
 def lightning_capturable(state: CollectionState, player: int) -> bool:
     return (first_nine_ixupi_capturable or state.multiworld.early_lightning[player].value) \
-        and state.can_reach("Generator", "Region", player) \
         and state.has_all({"Lightning Pot Bottom", "Lightning Pot Top", "Lightning Pot Bottom DUPE", "Lightning Pot Top DUPE"}, player)
 
 
@@ -120,7 +110,8 @@ def get_rules_lookup(player: int):
                         state.can_reach("Prehistoric", "Region", player) and state.can_reach("Tar River", "Region",player) and
                         state.can_reach("Egypt", "Region", player) and state.can_reach("Burial", "Region", player) and
                         state.can_reach("Gods Room", "Region", player) and state.can_reach("Werewolf", "Region", player)),
-            "To Lobby From Slide Room": lambda state: (beths_body_available(state, player))
+            "To Lobby From Slide Room": lambda state: (beths_body_available(state, player)),
+            "To Water Capture From Janitor Closet": lambda state: (cloth_capturable(state, player))
         },
         "locations_required": {
             "Puzzle Solved Anansi Musicbox": lambda state: state.can_reach("Clock Tower", "Region", player),
