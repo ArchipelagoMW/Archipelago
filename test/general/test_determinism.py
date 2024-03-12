dfd8ec3dd36cb8c0a2a84719bc649665598fb6d1
alwaysintreble <mmmcheese158@gmail.com>
2024-03-12 18:29:18 -0500

    use a property to raise to find the problem code

--- test/general/test_determinism.py
+++ test/general/test_determinism.py
@@ -1,4 +1,7 @@
+from random import random
 from unittest import TestCase
+
+from BaseClasses import MultiWorld
 from . import setup_solo_multiworld, gen_steps
 from worlds.AutoWorld import AutoWorldRegister, call_single
 
@@ -6,13 +9,14 @@ from worlds.AutoWorld import AutoWorldRegister, call_single
 class TestDeterminism(TestCase):
     def test_multiworld_random(self) -> None:
         """Tests that Worlds don't use `multiworld.random` outside class methods."""
+        def rand(multiworld: MultiWorld) -> None:
+            raise ValueError("multiworld random should not be called from World instance methods.")
+
         for game_name, world_type in AutoWorldRegister.world_types.items():
-            with self.subTest(game_name):
-                multiworld = setup_solo_multiworld(world_type, ())
-                multiworld.random.seed(0)
-                test_result = multiworld.random.random()
-                multiworld.random.seed(0)
-                for step in gen_steps:
+            multiworld = setup_solo_multiworld(world_type, ())
+            setattr(multiworld, "random", property(rand))
+
+            for step in gen_steps:
+
+                with self.subTest(game_name, step=step):
                     call_single(multiworld, step, 1)
-                    self.assertEqual(test_result, multiworld.random.random())
-                    multiworld.random.seed(0)
