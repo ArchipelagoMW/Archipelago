7f43d84fda78346d7bcca3146a83b4ee1c79f064
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-03-13 21:16:09 +0100

    Update BaseClasses.py
    Co-authored-by: Exempt-Medic <60412657+Exempt-Medic@users.noreply.github.com>

--- BaseClasses.py
+++ BaseClasses.py
@@ -761,7 +761,7 @@ class CollectionState():
         return any(self.prog_items[player][item] for item in items)
 
     def has_all_counts(self, item_counts: Dict[str, int], player: int) -> bool:
-        """Returns True if each item name is in the state at least as many times as specified."""
+        """Returns True if each item name is in state at least as many times as specified."""
         return all(self.prog_items[player][item] >= count for item, count in item_counts.items())
 
     def has_any_count(self, item_counts: Dict[str, int], player: int) -> bool:
