4e0ad3e4504de6be74165bf8cab2b1dfb11a582b
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-03-15 23:45:48 +0100

    Actually item bc that seems more useful

--- BaseClasses.py
+++ BaseClasses.py
@@ -970,7 +970,7 @@ class Region:
             self.locations.append(location_type(self.player, location, address, self))
 
     def add_event(self, location_name: str, item_name: Optional[str] = None,
-                  rule: Optional[Callable[[CollectionState], bool]] = None) -> Location:
+                  rule: Optional[Callable[[CollectionState], bool]] = None) -> Item:
         """
         Adds an event location/item pair to a region.
 
@@ -980,14 +980,16 @@ class Region:
         """
         if item_name is None:
             item_name = location_name
+        event_item = Item(item_name, ItemClassification.progression, None, self.player)
 
         event_location = Location(self.player, location_name, None, self)
         if rule is not None:
             event_location.access_rule = rule
         self.locations.append(event_location)
-        event_location.place_locked_item(Item(item_name, ItemClassification.progression, None, self.player))
 
-        return event_location
+        event_location.place_locked_item(event_item)
+
+        return event_item
 
     def connect(self, connecting_region: Region, name: Optional[str] = None,
                 rule: Optional[Callable[[CollectionState], bool]] = None) -> entrance_type:
