034325f8f7f5ade23d1dbfcd02e1dd744afc2b9d
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-03-16 18:40:22 +0100

    F:/Program Files/Git/

--- worlds/witness/options.py
+++ worlds/witness/options.py
@@ -266,7 +266,7 @@ class HintAmount(Range):
 class AreaHintPercentage(Range):
     """
     There are two types of hints for The Witness.
-    "Location hints" hint one location in your world / containing an item for your world.
+    "Location hints" hint one location in your world or one location containing an item for your world.
     "Area hints" tell you some general info about the items you can find in one of the main geographic areas on the island.
     Use this option to specify how many of your hints you want to be area hints. The rest will be location hints.
     """
