7317fe679eb267cab5c3ca849fb2e019a0ef1c88
panicbit <panicbit.dev@gmail.com>
2024-03-18 23:03:15 +0100

    LADX: fix local and non-local instrument placement

--- worlds/ladx/__init__.py
+++ worlds/ladx/__init__.py
@@ -184,19 +184,21 @@ class LinksAwakeningWorld(World):
         self.pre_fill_items = []
         # For any and different world, set item rule instead
         
-        for option in ["maps", "compasses", "small_keys", "nightmare_keys", "stone_beaks", "instruments"]:
-            option = "shuffle_" + option
+        for dungeon_item_type in ["maps", "compasses", "small_keys", "nightmare_keys", "stone_beaks", "instruments"]:
+            option = "shuffle_" + dungeon_item_type
             option = self.player_options[option]
 
             dungeon_item_types[option.ladxr_item] = option.value
 
+            num_items = 9 if dungeon_item_type == "instruments" else 10
+
             if option.value == DungeonItemShuffle.option_own_world:
                 self.multiworld.local_items[self.player].value |= {
-                    ladxr_item_to_la_item_name[f"{option.ladxr_item}{i}"] for i in range(1, 10)
+                    ladxr_item_to_la_item_name[f"{option.ladxr_item}{i}"] for i in range(1, num_items)
                 }
             elif option.value == DungeonItemShuffle.option_different_world:
                 self.multiworld.non_local_items[self.player].value |= {
-                    ladxr_item_to_la_item_name[f"{option.ladxr_item}{i}"] for i in range(1, 10)
+                    ladxr_item_to_la_item_name[f"{option.ladxr_item}{i}"] for i in range(1, num_items)
                 }
         # option_original_dungeon = 0
         # option_own_dungeons = 1
