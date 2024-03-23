9fad361e5442b7ae8a8332d4262d47617909b51a
Jouramie <jouramie@hotmail.com>
2024-03-22 20:36:15 -0400

    Remove the strings because someone don't link strings...

--- worlds/stardew_valley/test/TestFishsanity.py
+++ worlds/stardew_valley/test/TestFishsanity.py
@@ -5,37 +5,43 @@ from . import SVTestBase
 from ..content.feature import fishsanity
 from ..mods.mod_data import ModNames
 from ..options import Fishsanity, ExcludeGingerIsland, Mods, SpecialOrderLocations
-from ..strings.fish_names import Fish
+from ..strings.fish_names import Fish, SVEFish, DistantLandsFish
 
-pelican_town_legendary_fishes = {"Angler", "Crimsonfish", "Glacierfish", "Legend", "Mutant Carp", }
-pelican_town_hard_special_fishes = {"Lava Eel", "Octopus", "Scorpion Carp", "Ice Pip", "Super Cucumber", }
-pelican_town_medium_special_fishes = {"Blobfish", "Dorado", }
-pelican_town_hard_normal_fishes = {"Lingcod", "Pufferfish", "Void Salmon", }
+pelican_town_legendary_fishes = {Fish.angler, Fish.crimsonfish, Fish.glacierfish, Fish.legend, Fish.mutant_carp, }
+pelican_town_hard_special_fishes = {Fish.lava_eel, Fish.octopus, Fish.scorpion_carp, Fish.ice_pip, Fish.super_cucumber, }
+pelican_town_medium_special_fishes = {Fish.blobfish, Fish.dorado, }
+pelican_town_hard_normal_fishes = {Fish.lingcod, Fish.pufferfish, Fish.void_salmon, }
 pelican_town_medium_normal_fishes = {
-    "Albacore", "Catfish", "Eel", "Flounder", "Ghostfish", Fish.goby, "Halibut", "Largemouth Bass", "Midnight Carp", "Midnight Squid", "Pike", "Red Mullet", "Salmon",
-    "Sandfish", "Slimejack", "Stonefish", "Spook Fish", "Squid", "Sturgeon", "Tiger Trout", "Tilapia", "Tuna", "Woodskip",
+    Fish.albacore, Fish.catfish, Fish.eel, Fish.flounder, Fish.ghostfish, Fish.goby, Fish.halibut, Fish.largemouth_bass, Fish.midnight_carp,
+    Fish.midnight_squid, Fish.pike, Fish.red_mullet, Fish.salmon, Fish.sandfish, Fish.slimejack, Fish.stonefish, Fish.spook_fish, Fish.squid, Fish.sturgeon,
+    Fish.tiger_trout, Fish.tilapia, Fish.tuna, Fish.woodskip,
 }
 pelican_town_easy_normal_fishes = {
-    "Anchovy", "Bream", "Bullhead", "Carp", "Chub", "Herring", "Perch", "Rainbow Trout", "Red Snapper", "Sardine", "Shad", "Sea Cucumber", "Shad",
-    "Smallmouth Bass", "Sunfish", "Walleye",
+    Fish.anchovy, Fish.bream, Fish.bullhead, Fish.carp, Fish.chub, Fish.herring, Fish.perch, Fish.rainbow_trout, Fish.red_snapper, Fish.sardine, Fish.shad,
+    Fish.sea_cucumber, Fish.shad, Fish.smallmouth_bass, Fish.sunfish, Fish.walleye,
 }
-pelican_town_crab_pot_fishes = {"Clam", "Cockle", "Crab", "Crayfish", "Lobster", "Mussel", "Oyster", "Periwinkle", "Shrimp", "Snail", }
+pelican_town_crab_pot_fishes = {
+    Fish.clam, Fish.cockle, Fish.crab, Fish.crayfish, Fish.lobster, Fish.mussel, Fish.oyster, Fish.periwinkle, Fish.shrimp, Fish.snail,
+}
 
-ginger_island_hard_fishes = {"Pufferfish", "Stingray", "Super Cucumber", }
-ginger_island_medium_fishes = {"Blue Discus", "Lionfish", "Tilapia", "Tuna", }
-qi_board_legendary_fishes = {"Ms. Angler", "Son of Crimsonfish", "Glacierfish Jr.", "Legend II", "Radioactive Carp", }
+ginger_island_hard_fishes = {Fish.pufferfish, Fish.stingray, Fish.super_cucumber, }
+ginger_island_medium_fishes = {Fish.blue_discus, Fish.lionfish, Fish.tilapia, Fish.tuna, }
+qi_board_legendary_fishes = {Fish.ms_angler, Fish.son_of_crimsonfish, Fish.glacierfish_jr, Fish.legend_ii, Fish.radioactive_carp, }
 
 sve_pelican_town_hard_fishes = {
-    "Grass Carp", "King Salmon", "Kittyfish", "Meteor Carp", "Puppyfish", "Radioactive Bass", "Undeadfish", "Void Eel",
+    SVEFish.grass_carp, SVEFish.king_salmon, SVEFish.kittyfish, SVEFish.meteor_carp, SVEFish.puppyfish, SVEFish.radioactive_bass, SVEFish.undeadfish,
+    SVEFish.void_eel,
 }
-sve_pelican_town_medium_fishes = {"Bonefish", "Butterfish", "Frog", "Goldenfish", "Snatcher Worm", "Water Grub", "Dulse Seaweed", }
-sve_pelican_town_easy_fishes = {"Bull Trout", "Minnow", }
-sve_ginger_island_hard_fishes = {"Gemfish", "Shiny Lunaloo", }
-sve_ginger_island_medium_fishes = {"Daggerfish", "Lunaloo", "Starfish", "Torpedo Trout", }
-sve_ginger_island_easy_fishes = {"Baby Lunaloo", "Clownfish", "Seahorse", "Sea Sponge", }
+sve_pelican_town_medium_fishes = {
+    SVEFish.bonefish, SVEFish.butterfish, SVEFish.frog, SVEFish.goldenfish, SVEFish.snatcher_worm, SVEFish.water_grub, SVEFish.dulse_seaweed,
+}
+sve_pelican_town_easy_fishes = {SVEFish.bull_trout, SVEFish.minnow, }
+sve_ginger_island_hard_fishes = {SVEFish.gemfish, SVEFish.shiny_lunaloo, }
+sve_ginger_island_medium_fishes = {SVEFish.daggerfish, SVEFish.lunaloo, SVEFish.starfish, SVEFish.torpedo_trout, }
+sve_ginger_island_easy_fishes = {SVEFish.baby_lunaloo, SVEFish.clownfish, SVEFish.seahorse, SVEFish.sea_sponge, }
 
-distant_lands_hard_fishes = {"Giant Horsehoe Crab", }
-distant_lands_easy_fishes = {"Void Minnow", "Purple Algae", "Swamp Leech", }
+distant_lands_hard_fishes = {DistantLandsFish.giant_horsehoe_crab, }
+distant_lands_easy_fishes = {DistantLandsFish.void_minnow, DistantLandsFish.purple_algae, DistantLandsFish.swamp_leech, }
 
 
 class SVFishsanityTestBase(SVTestBase):
