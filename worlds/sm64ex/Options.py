98ce8f8844fd0c62214a5774609382cf6a6bc829
Yussur Mustafa Oraji <N00byKing@hotmail.de>
2024-03-26 14:29:25 +0100

    sm64ex: New Options API and WebHost fix (#2979)

--- worlds/sm64ex/Options.py
+++ worlds/sm64ex/Options.py
@@ -1,5 +1,6 @@
 import typing
-from Options import Option, DefaultOnToggle, Range, Toggle, DeathLink, Choice
+from dataclasses import dataclass
+from Options import DefaultOnToggle, Range, Toggle, DeathLink, Choice, PerGameCommonOptions, OptionSet
 from .Items import action_item_table
 
 class EnableCoinStars(DefaultOnToggle):
@@ -114,35 +115,37 @@ class StrictMoveRequirements(DefaultOnToggle):
     if Move Randomization is enabled"""
     display_name = "Strict Move Requirements"
 
-def getMoveRandomizerOption(action: str):
-    class MoveRandomizerOption(Toggle):
-        """Mario is unable to perform this action until a corresponding item is picked up.
-        This option is incompatible with builds using a 'nomoverando' branch."""
-        display_name = f"Randomize {action}"
-    return MoveRandomizerOption
+class EnableMoveRandomizer(Toggle):
+    """Mario is unable to perform some actions until a corresponding item is picked up.
+    This option is incompatible with builds using a 'nomoverando' branch.
+    Specific actions to randomize can be specified in the YAML."""
+    display_name = "Enable Move Randomizer"
 
+class MoveRandomizerActions(OptionSet):
+    """Which actions to randomize when Move Randomizer is enabled"""
+    display_name = "Randomized Moves"
+    # HACK: Disable randomization for double jump
+    valid_keys = [action for action in action_item_table if action != 'Double Jump']
+    default = valid_keys
 
-sm64_options: typing.Dict[str, type(Option)] = {
-    "AreaRandomizer": AreaRandomizer,
-    "BuddyChecks": BuddyChecks,
-    "ExclamationBoxes": ExclamationBoxes,
-    "ProgressiveKeys": ProgressiveKeys,
-    "EnableCoinStars": EnableCoinStars,
-    "StrictCapRequirements": StrictCapRequirements,
-    "StrictCannonRequirements": StrictCannonRequirements,
-    "StrictMoveRequirements": StrictMoveRequirements,
-    "AmountOfStars": AmountOfStars,
-    "FirstBowserStarDoorCost": FirstBowserStarDoorCost,
-    "BasementStarDoorCost": BasementStarDoorCost,
-    "SecondFloorStarDoorCost": SecondFloorStarDoorCost,
-    "MIPS1Cost": MIPS1Cost,
-    "MIPS2Cost": MIPS2Cost,
-    "StarsToFinish": StarsToFinish,
-    "death_link": DeathLink,
-    "CompletionType": CompletionType,
-}
-
-for action in action_item_table:
-    # HACK: Disable randomization of double jump
-    if action == 'Double Jump': continue
-    sm64_options[f"MoveRandomizer{action.replace(' ','')}"] = getMoveRandomizerOption(action)
+@dataclass
+class SM64Options(PerGameCommonOptions):
+    area_rando: AreaRandomizer
+    buddy_checks: BuddyChecks
+    exclamation_boxes: ExclamationBoxes
+    progressive_keys: ProgressiveKeys
+    enable_coin_stars: EnableCoinStars
+    enable_move_rando: EnableMoveRandomizer
+    move_rando_actions: MoveRandomizerActions
+    strict_cap_requirements: StrictCapRequirements
+    strict_cannon_requirements: StrictCannonRequirements
+    strict_move_requirements: StrictMoveRequirements
+    amount_of_stars: AmountOfStars
+    first_bowser_star_door_cost: FirstBowserStarDoorCost
+    basement_star_door_cost: BasementStarDoorCost
+    second_floor_star_door_cost: SecondFloorStarDoorCost
+    mips1_cost: MIPS1Cost
+    mips2_cost: MIPS2Cost
+    stars_to_finish: StarsToFinish
+    death_link: DeathLink
+    completion_type: CompletionType
