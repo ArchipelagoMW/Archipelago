aad614456539e466a45564de1df417c45c4be363
Carter Hesterman <hestermancarter@gmail.com>
2024-03-28 16:54:04 -0600

    PEP8 formatting

--- MetroidPrimeClient.py
+++ MetroidPrimeClient.py
@@ -7,93 +7,104 @@ import Utils
 from worlds.metroidprime.DolphinClient import DolphinException
 from worlds.metroidprime.MetroidPrimeInterface import MetroidPrimeInterface
 from enum import Enum
+
+
 class MetroidPrimeCommandProcessor(ClientCommandProcessor):
-  def __init__(self, ctx: CommonContext):
-    super().__init__(ctx)
+    def __init__(self, ctx: CommonContext):
+        super().__init__(ctx)
+
 
 class MetroidPrimeContext(CommonContext):
-  command_processor = MetroidPrimeCommandProcessor
-  game_interface: MetroidPrimeInterface
-  game = "Metroid Prime"
-  items_handling = 0b111
-  dolphin_sync_task = None
+    command_processor = MetroidPrimeCommandProcessor
+    game_interface: MetroidPrimeInterface
+    game = "Metroid Prime"
+    items_handling = 0b111
+    dolphin_sync_task = None
 
-  def __init__(self, server_address, password):
-    super().__init__(server_address, password)
-    self.game_interface = MetroidPrimeInterface(logger)
+    def __init__(self, server_address, password):
+        super().__init__(server_address, password)
+        self.game_interface = MetroidPrimeInterface(logger)
 
-  def on_deathlink(self, data: Utils.Dict[str, Utils.Any]) -> None:
-    super().on_deathlink(data)
-    logger.info()
+    def on_deathlink(self, data: Utils.Dict[str, Utils.Any]) -> None:
+        super().on_deathlink(data)
+        logger.info()
 
-  async def server_auth(self, password_requested: bool = False):
-    if password_requested and not self.password:
-        await super(MetroidPrimeContext, self).server_auth(password_requested)
-    await self.get_username()
-    await self.send_connect()
+    async def server_auth(self, password_requested: bool = False):
+        if password_requested and not self.password:
+            await super(MetroidPrimeContext, self).server_auth(password_requested)
+        await self.get_username()
+        await self.send_connect()
+
 
 async def dolphin_sync_task(ctx: MetroidPrimeContext):
-  logger.info("Starting Dolphin connector")
-  while not ctx.exit_event.is_set():
-    try:
-      if ctx.game_interface.is_connected() and ctx.game_interface.is_in_playable_state():
-        await _handle_game_ready(ctx)
-      else:
-        await _handle_game_not_ready(ctx)
-    except Exception as e:
-      if isinstance(e, DolphinException):
-        logger.error(str(e))
-      else:
-        logger.error(traceback.format_exc())
+    logger.info("Starting Dolphin connector")
+    while not ctx.exit_event.is_set():
+        try:
+            if ctx.game_interface.is_connected() and ctx.game_interface.is_in_playable_state():
+                await _handle_game_ready(ctx)
+            else:
+                await _handle_game_not_ready(ctx)
+        except Exception as e:
+            if isinstance(e, DolphinException):
+                logger.error(str(e))
+            else:
+                logger.error(traceback.format_exc())
 
-      logger.info("Attempting to reconnect to Dolphin")
-      await ctx.disconnect()
-      await asyncio.sleep(3)
-      continue
+            logger.info("Attempting to reconnect to Dolphin")
+            await ctx.disconnect()
+            await asyncio.sleep(3)
+            continue
+
 
 async def _handle_game_ready(ctx: MetroidPrimeContext):
-  if ctx.server and ctx.slot:
-    if "DeathLink" in ctx.tags:
-      logger.warn("DeathLink not implemented")
-    # await give_items(ctx)
-    # await check_locations(ctx)
-  await asyncio.sleep(0.5)
+    if ctx.server and ctx.slot:
+        if "DeathLink" in ctx.tags:
+            logger.warn("DeathLink not implemented")
+        # await give_items(ctx)
+        # await check_locations(ctx)
+    await asyncio.sleep(0.5)
+
 
 async def _handle_game_not_ready(ctx: MetroidPrimeContext):
-  """If the game is not connected or not in a playable state, this will attempt to retry connecting to the game."""
-  if not ctx.game_interface.is_connected():
-    logger.info("Attempting to connect to Dolphin")
-    ctx.game_interface.connect_to_game()
-  elif not ctx.game_interface.is_in_playable_state():
-    logger.info("Waiting for player to load a save file or start a new game")
-    await asyncio.sleep(3)
+    """If the game is not connected or not in a playable state, this will attempt to retry connecting to the game."""
+    if not ctx.game_interface.is_connected():
+        logger.info("Attempting to connect to Dolphin")
+        ctx.game_interface.connect_to_game()
+    elif not ctx.game_interface.is_in_playable_state():
+        logger.info(
+            "Waiting for player to load a save file or start a new game")
+        await asyncio.sleep(3)
+
 
 def main(connect=None, password=None):
-  Utils.init_logging("Metroid Prime Client")
+    Utils.init_logging("Metroid Prime Client")
 
-  async def _main(connect, password):
-    ctx = MetroidPrimeContext(connect, password)
-    ctx.server_task = asyncio.create_task(server_loop(ctx), name="ServerLoop")
-    if gui_enabled:
-      ctx.run_gui()
-    await asyncio.sleep(1)
+    async def _main(connect, password):
+        ctx = MetroidPrimeContext(connect, password)
+        ctx.server_task = asyncio.create_task(
+            server_loop(ctx), name="ServerLoop")
+        if gui_enabled:
+            ctx.run_gui()
+        await asyncio.sleep(1)
 
-    ctx.dolphin_sync_task = asyncio.create_task(dolphin_sync_task(ctx), name="DolphinSync")
+        ctx.dolphin_sync_task = asyncio.create_task(
+            dolphin_sync_task(ctx), name="DolphinSync")
 
-    await ctx.exit_event.wait()
-    ctx.server_address = None
+        await ctx.exit_event.wait()
+        ctx.server_address = None
 
-    await ctx.shutdown()
+        await ctx.shutdown()
 
-    if ctx.dolphin_sync_task:
-        await asyncio.sleep(3)
-        await ctx.dolphin_sync_task
+        if ctx.dolphin_sync_task:
+            await asyncio.sleep(3)
+            await ctx.dolphin_sync_task
 
-  import colorama
+    import colorama
 
-  colorama.init()
-  asyncio.run(_main(connect, password))
-  colorama.deinit()
+    colorama.init()
+    asyncio.run(_main(connect, password))
+    colorama.deinit()
+
 
 if __name__ == "__main__":
     parser = get_base_parser()
--- worlds/metroidprime/MetroidPrimeInterface.py
+++ worlds/metroidprime/MetroidPrimeInterface.py
@@ -8,76 +8,80 @@ import py_randomprime
 symbols = py_randomprime.symbols_for_version("0-00")
 game_state_pointer = symbols["g_GameState"]
 cstate_manager_global = symbols["g_StateManager"]
-cplayer_vtable=0x803d96e8
+cplayer_vtable = 0x803d96e8
 
 METROID_PRIME_ID = b"GM8E01"
+
+
 class World(Enum):
-  """Game worlds with their corresponding IDs in memory"""
-  Impact_Crater = 3241871825
-  Phendrana_Drifts = 2831049361
-  Frigate_Orpheon = 361692695
-  Magmoor_Caverns = 1056449404
-  Phazon_Mines = 2980859237
-  Tallon_Overworld = 972217896
-  Chozo_Ruins = 2214002543
-  End_of_Game = 332894565
+    """Game worlds with their corresponding IDs in memory"""
+    Impact_Crater = 3241871825
+    Phendrana_Drifts = 2831049361
+    Frigate_Orpheon = 361692695
+    Magmoor_Caverns = 1056449404
+    Phazon_Mines = 2980859237
+    Tallon_Overworld = 972217896
+    Chozo_Ruins = 2214002543
+    End_of_Game = 332894565
+
 
 def world_by_id(id) -> World:
-  for world in World:
-    if world.value == id:
-      return world
-  return None
-
-class MetroidPrimeInterface:
-  dolphin_client: DolphinClient
-  connection_status: str
-  logger: Logger
-
-  def __init__(self, logger) -> None:
-    self.logger = logger
-    self.dolphin_client = DolphinClient(logger)
-
-  def connect_to_game(self):
-    self.dolphin_client.connect()
-    self.logger.info("Connected to Dolphin Emulator")
-    if self.dolphin_client.read_address(GC_GAME_ID_ADDRESS, 6) != METROID_PRIME_ID:
-      self.logger.error(f"Connected to the wrong game, please connect to Metroid Prime V1 English ({METROID_PRIME_ID})")
-      self.disconnect_from_game()
-
-  def disconnect_from_game(self):
-    self.dolphin_client.disconnect()
-    self.logger.info("Disconnected from Dolphin Emulator")
-
-  def is_connected(self):
-    return self.dolphin_client.is_connected()
-
-  def is_in_playable_state(self) -> bool:
-    """ Check if the player is in the actual game rather than the main menu """
-    return self.get_current_world() != None and self.__is_player_table_ready()
-
-  def get_current_region(self):
-    pass
-
-  def give_item_to_player(self, item_id):
-    pass
-
-  def check_for_new_locations(self):
-    pass
-
-  def get_current_world(self) -> World:
-    world_bytes = self.dolphin_client.read_pointer(
-        game_state_pointer, 0x84, struct.calcsize(">I"))
-    if world_bytes is not None:
-      world_asset_id = struct.unpack(">I", world_bytes)[0]
-      return world_by_id(world_asset_id)
+    for world in World:
+        if world.value == id:
+            return world
     return None
 
-  def __is_player_table_ready(self)-> bool:
-    player_table_bytes = self.dolphin_client.read_pointer(cstate_manager_global + 0x84C, 0, 4)
-    player_table = struct.unpack(">I", player_table_bytes)[0]
-    if player_table == cplayer_vtable:
-      return True
-    else:
-      return False
 
+class MetroidPrimeInterface:
+    dolphin_client: DolphinClient
+    connection_status: str
+    logger: Logger
 
+    def __init__(self, logger) -> None:
+        self.logger = logger
+        self.dolphin_client = DolphinClient(logger)
+
+    def connect_to_game(self):
+        self.dolphin_client.connect()
+        self.logger.info("Connected to Dolphin Emulator")
+        if self.dolphin_client.read_address(GC_GAME_ID_ADDRESS, 6) != METROID_PRIME_ID:
+            self.logger.error(
+                f"Connected to the wrong game, please connect to Metroid Prime V1 English ({METROID_PRIME_ID})")
+            self.disconnect_from_game()
+
+    def disconnect_from_game(self):
+        self.dolphin_client.disconnect()
+        self.logger.info("Disconnected from Dolphin Emulator")
+
+    def is_connected(self):
+        return self.dolphin_client.is_connected()
+
+    def is_in_playable_state(self) -> bool:
+        """ Check if the player is in the actual game rather than the main menu """
+        return self.get_current_world() != None and self.__is_player_table_ready()
+
+    def get_current_region(self):
+        pass
+
+    def give_item_to_player(self, item_id):
+        pass
+
+    def check_for_new_locations(self):
+        pass
+
+    def get_current_world(self) -> World:
+        world_bytes = self.dolphin_client.read_pointer(
+            game_state_pointer, 0x84, struct.calcsize(">I"))
+        if world_bytes is not None:
+            world_asset_id = struct.unpack(">I", world_bytes)[0]
+            return world_by_id(world_asset_id)
+        return None
+
+    def __is_player_table_ready(self) -> bool:
+        player_table_bytes = self.dolphin_client.read_pointer(
+            cstate_manager_global + 0x84C, 0, 4)
+        player_table = struct.unpack(">I", player_table_bytes)[0]
+        if player_table == cplayer_vtable:
+            return True
+        else:
+            return False
