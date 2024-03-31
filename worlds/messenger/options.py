1e84d6f8e699e0c843da069a5983c8021c12a1ca
Aaron Wagener <mmmcheese158@gmail.com>
2024-03-31 11:11:10 -0500

    The Messenger: update docs formatting and fix outdated info (#3033)
    * The Messenger: update docs formatting and fix outdated info
    
    * address review feedback
    
    * 120 chars

--- worlds/messenger/options.py
+++ worlds/messenger/options.py
@@ -89,7 +89,7 @@ class ShuffleTransitions(Choice):
 
 
 class Goal(Choice):
-    """Requirement to finish the game."""
+    """Requirement to finish the game. To win with the power seal hunt goal, you must enter the Music Box through the shop chest."""
     display_name = "Goal"
     option_open_music_box = 0
     option_power_seal_hunt = 1
