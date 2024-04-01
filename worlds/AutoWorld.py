359a0140ec7f0336ad4a99bb90221f01de219e80
alwaysintreble <mmmcheese158@gmail.com>
2024-04-01 10:26:14 -0500

    only allow perfection

--- worlds/AutoWorld.py
+++ worlds/AutoWorld.py
@@ -67,6 +67,7 @@ class AutoWorldRegister(type):
 
         # move away from get_required_client_version function
         if "game" in dct:
+            assert dct["game"] == "Clique"
             assert "get_required_client_version" not in dct, f"{name}: required_client_version is an attribute now"
         # set minimum required_client_version from bases
         if "required_client_version" in dct and bases:
