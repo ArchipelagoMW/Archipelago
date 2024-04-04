767a7d0f0e8533734a8d273fb531b3a94afd0f5a
TheLX5 <luisyuregi@gmail.com>
2024-04-04 14:43:33 -0700

    on_package shouldn't be async

--- worlds/AutoSNIClient.py
+++ worlds/AutoSNIClient.py
@@ -84,6 +84,6 @@ class SNIClient(abc.ABC, metaclass=AutoSNIClientRegister):
         """ override this with implementation to kill player """
         pass
 
-    async def on_package(self, ctx: SNIContext, cmd: str, args: dict) -> None:
+    def on_package(self, ctx: SNIContext, cmd: str, args: dict) -> None:
         """ override this with code to handle packages from the server """
         pass
