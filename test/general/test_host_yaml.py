4b6641673d01567926883a223e99c98031463196
black-sliver <59490463+black-sliver@users.noreply.github.com>
2024-04-05 01:39:41 +0200

    Tests: add settings formatting checks

--- test/general/test_host_yaml.py
+++ test/general/test_host_yaml.py
@@ -1,9 +1,10 @@
 import os
 import unittest
+from io import StringIO
 from tempfile import TemporaryFile
 
-from settings import Settings
 import Utils
+from settings import Settings, Group
 
 
 class TestIDs(unittest.TestCase):
@@ -30,3 +31,44 @@ class TestIDs(unittest.TestCase):
                 self.assertIn(option_key, utils_options)
                 for sub_option_key in option_set:
                     self.assertIn(sub_option_key, utils_options[option_key])
+
+
+class TestSettingsDumper(unittest.TestCase):
+    def test_string_format(self) -> None:
+        """Test that dumping a string will yield the expected output"""
+        # By default, pyyaml has automatic line breaks in strings and quoting is optional.
+        # What we want for consistency instead is single-line strings and always quote them.
+        # Line breaks have to become \n in that quoting style.
+        class AGroup(Group):
+            key: str = " ".join(["x"] * 60) + "\n"  # more than 120 chars, contains spaces and a line break
+
+        with StringIO() as writer:
+            AGroup().dump(writer, 0)
+            expected_value = AGroup.key.replace("\n", "\\n")
+            self.assertEqual(writer.getvalue(), f"key: \"{expected_value}\"\n",
+                             "dumped string has unexpected formatting")
+
+    def test_indentation(self) -> None:
+        """Test that dumping a item will add indentation"""
+        # NOTE: we don't care how many spaces there are, but it has to be a multiple of level
+        class AGroup(Group):
+            key: list = ["a", "b", [1]]
+
+        for level in range(3):
+            with StringIO() as writer:
+                AGroup().dump(writer, level)
+                lines = writer.getvalue().split("\n", 5)
+                key_line = lines[0]
+                key_spaces = len(key_line) - len(key_line.lstrip(" "))
+                value_lines = lines[1:-1]
+                value_spaces = [len(value_line) - len(value_line.lstrip(" ")) for value_line in value_lines]
+                if level == 0:
+                    self.assertEqual(key_spaces, 0)
+                else:
+                    self.assertGreaterEqual(key_spaces, level)
+                    self.assertEqual(key_spaces % level, 0)
+                self.assertGreaterEqual(value_spaces[0], key_spaces)  # a
+                self.assertEqual(value_spaces[1], value_spaces[0])  # b
+                self.assertEqual(value_spaces[2], value_spaces[0])  # start of sub-list
+                self.assertGreater(value_spaces[3], value_spaces[0],
+                                   f"{value_lines[3]} should have more indentation than {value_lines[0]} in {lines}")
