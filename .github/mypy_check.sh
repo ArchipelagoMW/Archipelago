5901ca12e49c3d6fe95904adaffb21fe9230a819
beauxq <beauxq@yahoo.com>
2024-04-08 10:54:15 -0700

    restrict return Any

--- .github/mypy_check.sh
+++ .github/mypy_check.sh
@@ -2,4 +2,4 @@ if [ "$(basename "$(pwd)")" = ".github" ]; then
     cd ..
 fi
 
-xargs mypy --strict --follow-imports=silent --no-warn-unused-ignore --no-warn-return-any --install-types --non-interactive typings < .github/mypy_files.txt
+xargs mypy --strict --follow-imports=silent --no-warn-unused-ignore --install-types --non-interactive typings < .github/mypy_files.txt
