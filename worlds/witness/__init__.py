434479aacfbed206a754e989c525e4012d86bcd0
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-04-12 00:39:26 +0200

    Remove unused import

--- worlds/witness/__init__.py
+++ worlds/witness/__init__.py
@@ -3,7 +3,7 @@ Archipelago init file for The Witness
 """
 import dataclasses
 from logging import error, warning
-from typing import Any, Dict, List, Optional, cast
+from typing import Dict, List, Optional, cast
 
 from BaseClasses import CollectionState, Entrance, Location, Region, Tutorial
 
