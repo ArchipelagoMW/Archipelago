0d7c39d6bf619965e6478d60d4bce0b4e684e9f2
Jarno Westhof <jarnowesthof@gmail.com>
2024-04-18 20:33:17 +0200

    Added trailing comma

--- worlds/factorio/Client.py
+++ worlds/factorio/Client.py
@@ -267,7 +267,7 @@ async def game_watcher(ctx: FactorioContext):
                                         [{"operation": "add", "value": -ctx.energy_link_increment * in_world_bridges},
                                          {"operation": "max", "value": 0}],
                                     "last_deplete": ctx.last_deplete,
-                                    "slot": ctx.slot
+                                    "slot": ctx.slot,
                                 }]))
                             # Above Capacity - (len(Bridges) * ENERGY_INCREMENT)
                             elif in_world_energy > (in_world_bridges * ctx.energy_link_increment * 5) - \
