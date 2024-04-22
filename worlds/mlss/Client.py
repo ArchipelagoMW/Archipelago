84179f17fb63c992015ef2f46dd03c15afa46e2b
jamesbrq <jamesbrq@gmail.com>
2024-04-22 16:18:41 -0400

    Update Client.py

--- worlds/mlss/Client.py
+++ worlds/mlss/Client.py
@@ -168,7 +168,7 @@ class MLSSClient(BizHawkClient):
                     if location >= 0xDA0000:
                         await ctx.send_msgs([{
                             "cmd": "Set",
-                            "key": f"mlss_flag{location - 0xD9FFFF}_{ctx.team}_{ctx.slot}",
+                            "key": f"mlss_flag_{ctx.team}_{ctx.slot}",
                             "default": 0,
                             "want_reply": False,
                             "operations": [{"operation": "or", "value": 1 << (location - 0xDA0000)}]
