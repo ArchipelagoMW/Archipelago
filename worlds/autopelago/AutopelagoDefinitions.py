f68a9e73f1f96bd4b14c9e7d457386534e9e6629
Joe Amenta <airbreather@linux.com>
2024-04-25 21:00:29 -0400

    looks like this'll do it

--- worlds/autopelago/AutopelagoDefinitions.py
+++ worlds/autopelago/AutopelagoDefinitions.py
@@ -20,14 +20,17 @@ AutopelagoGameSpecificItemGroup = dict[Literal['game_specific'], dict[str, list[
 AutopelagoNonProgressionGroupItems = list[AutopelagoItemDefinition | AutopelagoGameSpecificItemGroup]
 AutopelagoNonProgressionItemType = Literal['useful_nonprogression', 'trap', 'filler', 'uncategorized']
 
+
 def _name_of(item: AutopelagoItemDefinition):
     return \
         item if isinstance(item, str) else \
         item[0] if isinstance(item, list) else \
         item['name']
 
+
 def _rat_count_of(item: AutopelagoItemDefinition):
     return item['rat_count'] if isinstance(item, dict) and 'rat_count' in item else None
+
 
 def autopelago_item_classification_of(item: AutopelagoNonProgressionItemType):
     match item:
@@ -50,7 +53,9 @@ class AutopelagoItemDefinitions(TypedDict):
     # any_other_str_key_not_listed: AutopelagoItemDefinition
 
 
-AutopelagoGameRequirement: TypeAlias = 'AutopelagoAllRequirement | AutopelagoAnyRequirement | AutopelagoItemRequirement | AutopelagoAbilityCheckRequirement | AutopelagoRatCountRequirement'
+AutopelagoGameRequirement: TypeAlias = ('AutopelagoAllRequirement | AutopelagoAnyRequirement | '
+                                        'AutopelagoItemRequirement | AutopelagoRatCountRequirement | '
+                                        'AutopelagoAnyTwoRequirement')
 
 
 class AutopelagoAllRequirement(TypedDict):
@@ -61,12 +66,12 @@ class AutopelagoAnyRequirement(TypedDict):
     any: list[AutopelagoGameRequirement]
 
 
+class AutopelagoAnyTwoRequirement(TypedDict):
+    any_two: list[AutopelagoGameRequirement]
+
+
 class AutopelagoItemRequirement(TypedDict):
     item: str
-
-
-class AutopelagoAbilityCheckRequirement(TypedDict):
-    ability_check_with_dc: int
 
 
 class AutopelagoRatCountRequirement(TypedDict):
@@ -97,7 +102,7 @@ class AutopelagoFillerRegionItemsDefinition(TypedDict):
 class AutopelagoFillerRegionDefinition(TypedDict):
     name_template: str
     unrandomized_items: AutopelagoFillerRegionItemsDefinition
-    each_requires: list[AutopelagoGameRequirement]
+    ability_check_dc: int
     exits: list[str]
 
 
@@ -121,11 +126,12 @@ class AutopelagoRegionDefinition:
         self.key = key
         self.exits = exits
         self.locations = locations
-        self.requires = { 'all': requires }
+        self.requires = {'all': requires}
 
 
 with open(PurePath(__file__).with_name('AutopelagoDefinitions.yml'), 'r') as f:
     _defs: AutopelagoDefinitions = parse_yaml(f.read())
+
 
 def _gen_ids():
     next_id = BASE_ID
@@ -133,15 +139,19 @@ def _gen_ids():
         yield next_id
         next_id += 1
 
-item_name_to_id: dict[str, int] = { }
-generic_nonprogression_item_table: dict[AutopelagoNonProgressionItemType, list[str]] = { 'useful_nonprogression': [], 'trap': [], 'filler': [], 'uncategorized': [] }
-item_name_to_defined_classification: dict[str, Optional[ItemClassification]] = { }
-item_name_to_rat_count: dict[str, int] = { }
-game_specific_nonprogression_items: dict[str, dict[AutopelagoNonProgressionItemType, list[str]]] = { }
-item_key_to_name: dict[str, str] = { }
+
+item_name_to_id: dict[str, int] = {}
+generic_nonprogression_item_table: dict[AutopelagoNonProgressionItemType, list[str]] = {'useful_nonprogression': [],
+                                                                                        'trap': [], 'filler': [],
+                                                                                        'uncategorized': []}
+item_name_to_defined_classification: dict[str, Optional[ItemClassification]] = {}
+item_name_to_rat_count: dict[str, int] = {}
+game_specific_nonprogression_items: dict[str, dict[AutopelagoNonProgressionItemType, list[str]]] = {}
+item_key_to_name: dict[str, str] = {}
 _item_id_gen = _gen_ids()
 
-for k, v in ((k, v) for k, v in _defs['items'].items() if k not in { 'rats', 'useful_nonprogression', 'trap', 'filler', 'uncategorized' }):
+for k, v in ((k, v) for k, v in _defs['items'].items() if
+             k not in {'rats', 'useful_nonprogression', 'trap', 'filler', 'uncategorized'}):
     _name = _name_of(v)
     item_name_to_id[_name] = next(_item_id_gen)
     item_name_to_defined_classification[_name] = ItemClassification.progression
@@ -156,6 +166,7 @@ for k, v in _defs['items']['rats'].items():
     item_name_to_defined_classification[_name] = ItemClassification.progression
     item_name_to_rat_count[_name] = _rat_count_of(v) or 1
     item_key_to_name[k] = _name
+
 
 def _append_nonprogression(k: AutopelagoNonProgressionItemType):
     item_classification = autopelago_item_classification_of(k)
@@ -172,9 +183,17 @@ def _append_nonprogression(k: AutopelagoNonProgressionItemType):
             item_name_to_defined_classification[i[0]] = item_classification
             continue
 
+        if 'game_specific' not in i:
+            _name = _name_of(i)
+            generic_nonprogression_item_table[k].append(_name)
+            item_name_to_id[_name] = next(_item_id_gen)
+            item_name_to_defined_classification[_name] = item_classification
+            item_name_to_rat_count[_name] = _rat_count_of(i) or 0
+            continue
+
         for g, v in i['game_specific'].items():
             if g not in game_specific_nonprogression_items:
-                game_specific_nonprogression_items[g] = { }
+                game_specific_nonprogression_items[g] = {}
             game_specific_nonprogression_items[g][k] = []
             for it in v:
                 if isinstance(it, str):
@@ -188,33 +207,34 @@ def _append_nonprogression(k: AutopelagoNonProgressionItemType):
                     item_name_to_id[it[0]] = next(_item_id_gen)
                     item_name_to_defined_classification[it[0]] = item_classification
 
+
 _append_nonprogression('useful_nonprogression')
 _append_nonprogression('trap')
 _append_nonprogression('filler')
 _append_nonprogression('uncategorized')
 
-autopelago_regions: dict[str, AutopelagoRegionDefinition] = { 'Victory': AutopelagoRegionDefinition('Victory', [], ['Victory'], []) }
-location_name_to_unrandomized_progression_item_name: dict[str, str] = { }
-location_name_to_unrandomized_nonprogression_item: dict[str, Literal['useful_nonprogression', 'filler']] = { }
-location_name_to_requirement: dict[str, AutopelagoGameRequirement] = { }
-location_name_to_id: dict[str, int] = { }
+autopelago_regions: dict[str, AutopelagoRegionDefinition] = {
+    'Victory': AutopelagoRegionDefinition('Victory', [], ['Victory'], [])}
+location_name_to_unrandomized_progression_item_name: dict[str, str] = {}
+location_name_to_unrandomized_nonprogression_item: dict[str, Literal['useful_nonprogression', 'filler']] = {}
+location_name_to_requirement: dict[str, AutopelagoGameRequirement] = {}
+location_name_to_id: dict[str, int] = {}
 _location_id_gen = _gen_ids()
 
 for k, r in _defs['regions']['landmarks'].items():
     _name = r['name']
     location_name_to_id[_name] = next(_location_id_gen)
     location_name_to_unrandomized_progression_item_name[_name] = item_key_to_name[r['unrandomized_item']]
-    location_name_to_requirement[_name] = { 'all': r['requires'] }
+    location_name_to_requirement[_name] = {'all': r['requires']}
     autopelago_regions[k] = AutopelagoRegionDefinition(k, r['exits'], [_name], r['requires'])
 for rk, r in _defs['regions']['fillers'].items():
     _locations: list[str] = []
     _cur = 1
-    for k in r['unrandomized_items']['key']:
+    for k in r['unrandomized_items']['key'] if 'key' in r['unrandomized_items'] else []:
         if isinstance(k, str):
             _name = r['name_template'].replace('{n}', f'{_cur}')
             location_name_to_id[_name] = next(_location_id_gen)
             location_name_to_unrandomized_progression_item_name[_name] = item_key_to_name[k]
-            location_name_to_requirement[_name] = { 'all': r['each_requires'] }
             _locations.append(_name)
             _cur += 1
         else:
@@ -222,24 +242,22 @@ for rk, r in _defs['regions']['fillers'].items():
                 _name = r['name_template'].replace('{n}', f'{_cur}')
                 location_name_to_id[_name] = next(_location_id_gen)
                 location_name_to_unrandomized_progression_item_name[_name] = item_key_to_name[k['item']]
-                location_name_to_requirement[_name] = { 'all': r['each_requires'] }
                 _locations.append(_name)
                 _cur += 1
-    for i in range(r['unrandomized_items']['useful_nonprogression']):
+    for i in range(r['unrandomized_items']['useful_nonprogression']) if 'useful_nonprogression' in r['unrandomized_items'] else []:
         _name = r['name_template'].replace('{n}', f'{_cur}')
         location_name_to_id[_name] = next(_location_id_gen)
         location_name_to_unrandomized_nonprogression_item[_name] = 'useful_nonprogression'
-        location_name_to_requirement[_name] = { 'all': r['each_requires'] }
         _locations.append(_name)
         _cur += 1
-    for i in range(r['unrandomized_items']['filler']):
+    for i in range(r['unrandomized_items']['filler']) if 'filler' in r['unrandomized_items'] else []:
         _name = r['name_template'].replace('{n}', f'{_cur}')
         location_name_to_id[_name] = next(_location_id_gen)
         location_name_to_unrandomized_nonprogression_item[_name] = 'filler'
-        location_name_to_requirement[_name] = { 'all': r['each_requires'] }
         _locations.append(_name)
         _cur += 1
     autopelago_regions[rk] = AutopelagoRegionDefinition(rk, r['exits'], _locations, [])
+
 
 def _get_required_rat_count(req: AutopelagoGameRequirement):
     if 'all' in req:
@@ -251,8 +269,13 @@ def _get_required_rat_count(req: AutopelagoGameRequirement):
     else:
         return 0
 
-max_required_rat_count = max(_get_required_rat_count(req) for req in [location_name_to_requirement.values()] + [r.requires for r in autopelago_regions.values()])
-total_available_rat_count = sum(item_name_to_rat_count[i] for i in location_name_to_unrandomized_progression_item_name.values() if i in item_name_to_rat_count)
+
+max_required_rat_count = max(_get_required_rat_count(req) for req in
+                             [location_name_to_requirement.values()] + [r.requires for r in
+                                                                        autopelago_regions.values()])
+total_available_rat_count = sum(
+    item_name_to_rat_count[i] for i in location_name_to_unrandomized_progression_item_name.values() if
+    i in item_name_to_rat_count)
 assert total_available_rat_count >= max_required_rat_count, f'Game is probably not winnable: {max_required_rat_count} rat(s) might be required with only {total_available_rat_count} available'
 
 del _append_nonprogression
--- worlds/autopelago/__init__.py
+++ worlds/autopelago/__init__.py
@@ -6,28 +6,36 @@ from .AutopelagoDefinitions import *
 from BaseClasses import CollectionState, Item, Location, MultiWorld, Region, Tutorial
 from worlds.AutoWorld import World, WebWorld
 
+the_logger = logging.getLogger(GAME_NAME)
+
+
 def _is_trivial(req: AutopelagoGameRequirement):
     if 'all' in req:
         return not req['all']
     elif 'rat_count' in req:
         return req['rat_count'] == 0
-    elif 'ability_check_with_dc' in req:
-        return True
     else:
         return False
 
+
 def _is_satisfied(player: int, req: AutopelagoGameRequirement, state: CollectionState):
     if 'all' in req:
+        req: AutopelagoAllRequirement
         return all(_is_satisfied(player, sub_req, state) for sub_req in req['all'])
     elif 'any' in req:
+        req: AutopelagoAnyRequirement
         return any(_is_satisfied(player, sub_req, state) for sub_req in req['any'])
+    elif 'any_two' in req:
+        req: AutopelagoAnyTwoRequirement
+        return sum(1 if _is_satisfied(player, sub_req, state) else 0 for sub_req in req['any_two']) > 1
     elif 'item' in req:
+        req: AutopelagoItemRequirement
         return state.has(item_key_to_name[req['item']], player)
-    elif 'rat_count' in req:
-        return sum(item_name_to_rat_count[k] * i for k, i in state.prog_items[player].items() if k in item_name_to_rat_count) >= req['rat_count']
     else:
-        assert 'ability_check_with_dc' in req, 'Only AutopelagoAbilityCheckRequirement is expected here'
-        return True
+        assert 'rat_count' in req, 'Only AutopelagoRatCountRequirement is expected here'
+        req: AutopelagoRatCountRequirement
+        return sum(item_name_to_rat_count[k] * i for k, i in state.prog_items[player].items() if
+                   k in item_name_to_rat_count) >= req['rat_count']
 
 
 class AutopelagoItem(Item):
@@ -49,7 +57,8 @@ class AutopelagoRegion(Region):
     game = GAME_NAME
     autopelago_definition: AutopelagoRegionDefinition
 
-    def __init__(self, autopelago_definition: AutopelagoRegionDefinition, player: int, multiworld: MultiWorld, hint: Optional[str] = None):
+    def __init__(self, autopelago_definition: AutopelagoRegionDefinition, player: int, multiworld: MultiWorld,
+                 hint: Optional[str] = None):
         super().__init__(autopelago_definition.key, player, multiworld, hint)
         self.autopelago_definition = autopelago_definition
         self.locations += (AutopelagoLocation(player, loc, self) for loc in autopelago_definition.locations)
@@ -74,7 +83,7 @@ class AutopelagoWorld(World):
     An idle game, in the same vein as ArchipIDLE but intended to be more sophisticated.
     '''
     game = GAME_NAME
-    topology_present = False # it's static, so setting this to True isn't actually helpful
+    topology_present = False  # it's static, so setting this to True isn't actually helpful
     data_version = 0
     web = AutopelagoWebWorld()
 
@@ -86,7 +95,6 @@ class AutopelagoWorld(World):
 
     def __init__(self, multiworld, player):
         super().__init__(multiworld, player)
-        self.logger = logging.getLogger(GAME_NAME)
 
     # insert other ClassVar values... suggestions include:
     # - item_name_groups
@@ -96,25 +104,28 @@ class AutopelagoWorld(World):
     # - hint_blacklist (should it include the goal item?)
 
     def create_item(self, name: str, classification: ItemClassification | None = None):
-        id = item_name_to_id[name] if name in item_name_to_id else None
-        classification = classification or item_name_to_defined_classification[name]
+        item_id = item_name_to_id[name] if name in item_name_to_id else None
+        classification = classification if classification is not None else item_name_to_defined_classification[name]
         assert classification is not None, 'Classification should either be defined, calculated during generate_early, or hardcoded.'
-        item = AutopelagoItem(name, classification, id, self.player)
+        item = AutopelagoItem(name, classification, item_id, self.player)
         return item
 
     def create_items(self):
         new_items = [self.create_item(item) for item in location_name_to_unrandomized_progression_item_name.values()]
 
-        # skip balancing for the normal_rat items that take us beyond the minimum limit
-        rat_items = sorted((item for item in new_items if item.name in item_name_to_rat_count), key=lambda item: (item_name_to_rat_count[item.name], 0 if item.name == item_key_to_name['normal_rat'] else 1))
+        # skip balancing for the pack_rat items that take us beyond the minimum limit
+        rat_items = sorted((item for item in new_items if item.name in item_name_to_rat_count), key=lambda item: (
+        item_name_to_rat_count[item.name], 0 if item.name == item_key_to_name['pack_rat'] else 1))
         for i in range(total_available_rat_count - max_required_rat_count):
-            assert rat_items[i].name == item_key_to_name['normal_rat'], 'Expected there to be enough normal_rat fillers for this calculation.'
+            assert rat_items[i].name == item_key_to_name[
+                'pack_rat'], 'Expected there to be enough pack_rat fillers for this calculation.'
             rat_items[i].classification |= ItemClassification.skip_balancing
 
         self.multiworld.itempool += new_items
 
-        nonprogression_item_table = { c: [item_name for item_name in items] for c, items in generic_nonprogression_item_table.items() }
-        dlc_games = { game for game in game_specific_nonprogression_items }
+        nonprogression_item_table = {c: [item_name for item_name in items] for c, items in
+                                     generic_nonprogression_item_table.items()}
+        dlc_games = {game for game in game_specific_nonprogression_items}
         for category, items in nonprogression_item_table.items():
             self.multiworld.random.shuffle(items)
             replacements_made = 0
@@ -126,8 +137,10 @@ class AutopelagoWorld(World):
                     items[replacements_made] = item
                     replacements_made += 1
 
-        category_to_next_offset: dict[AutopelagoNonProgressionItemType, int] = { category: 0 for category in generic_nonprogression_item_table }
+        category_to_next_offset: dict[AutopelagoNonProgressionItemType, int] = {category: 0 for category in
+                                                                                generic_nonprogression_item_table}
         next_filler_becomes_trap = False
+        nonprog_type: Literal['useful_nonprogression', 'filler', 'trap', 'uncategorized']
         for nonprog_type in location_name_to_unrandomized_nonprogression_item.values():
             if nonprog_type == 'filler':
                 if next_filler_becomes_trap:
@@ -135,22 +148,23 @@ class AutopelagoWorld(World):
                 next_filler_becomes_trap = not next_filler_becomes_trap
 
             classification = autopelago_item_classification_of(nonprog_type)
-            if category_to_next_offset[nonprog_type] > len(nonprogression_item_table[nonprog_type]):
+            if category_to_next_offset[nonprog_type] >= len(nonprogression_item_table[nonprog_type]):
                 nonprog_type = 'uncategorized'
             next_item = nonprogression_item_table[nonprog_type][category_to_next_offset[nonprog_type]]
             self.multiworld.itempool.append(self.create_item(next_item, classification))
             category_to_next_offset[nonprog_type] += 1
 
     def create_regions(self):
-        new_regions = { r.key: AutopelagoRegion(r, self.player, self.multiworld) for r in autopelago_regions.values() }
+        new_regions = {r.key: AutopelagoRegion(r, self.player, self.multiworld) for r in autopelago_regions.values()}
         for r in new_regions.values():
             self.multiworld.regions.append(r)
             req = r.autopelago_definition.requires
-            for exit in r.autopelago_definition.exits:
+            for next_exit in r.autopelago_definition.exits:
                 rule = (lambda req_: lambda state: _is_satisfied(self.player, req_, state))(req)
-                r.connect(new_regions[exit], rule=None if _is_trivial(req) else rule)
+                r.connect(new_regions[next_exit], rule=None if _is_trivial(req) else rule)
 
-        self.multiworld.get_location('Victory', self.player).place_locked_item(self.create_item('Victory', ItemClassification.progression))
+        self.multiworld.get_location('Victory', self.player).place_locked_item(
+            self.create_item('Victory', ItemClassification.progression))
         self.multiworld.completion_condition[self.player] = lambda state: state.has('Victory', self.player)
 
     def get_filler_item_name(self):
