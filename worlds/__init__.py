6dbadcf157d3b2095396b47c4285a623b6c6f058
alwaysintreble <mmmcheese158@gmail.com>
2024-04-27 20:44:15 -0500

    add workaround for GitHub runners

--- worlds/__init__.py
+++ worlds/__init__.py
@@ -1,13 +1,14 @@
+import dataclasses
 import importlib
 import os
 import pkgutil
 import sys
+import time
 import warnings
 import zipimport
+from typing import Dict, Iterable, List, Optional, Tuple, TypedDict, Union
+
 import orjson
-import time
-import dataclasses
-from typing import Dict, List, Iterable, Tuple, TypedDict, Optional, Union
 
 from Utils import cache_path, local_path, user_path
 from .AutoWorld import AutoWorldRegister
@@ -25,7 +26,6 @@ __all__ = {
     "DataPackage",
     "failed_world_loads",
 }
-
 
 failed_world_loads: List[str] = []
 
@@ -127,9 +127,8 @@ def get_worlds_info() -> Tuple[Dict[str, Dict[str, str]], bool]:
     should_update = False
     world_data = {}
     try:
-        worlds_path = cache_path("worlds.json")
-        cached_time = os.path.getmtime(worlds_path)
-        world_data = orjson.loads(pkgutil.get_data(__name__, worlds_path))
+        cached_time = os.path.getmtime(cached_worlds_path)
+        world_data = orjson.loads(pkgutil.get_data(__name__, cached_worlds_path))
         for world in world_sources:
             if os.path.getmtime(world.resolved_path) > cached_time:
                 should_update = True
@@ -149,14 +148,19 @@ def load_all_worlds() -> DataPackage:
 
     for world_name, world in AutoWorldRegister.world_types.items():
         games[world_name] = world.get_data_package_data()
+        # GitHub runners won't allow me to write to their cache
+        if "runner" in cached_worlds_path:
+            continue
         json_name = [name for name in world.__module__.split(".") if "world" not in name][0]
         if world.zip_path:
             json_name += ".apworld"
-        world_json = {"game": world.game, "path": world.__file__, "settings": str(world.settings_key) if hasattr(world, "settings") else None}
+        world_json = {"game": world.game, "path": world.__file__,
+                      "settings": str(world.settings_key) if hasattr(world, "settings") else None}
         json_data[json_name] = world_json
 
-    with open(cache_path("worlds.json"), "wb") as f:
-        f.write(orjson.dumps(json_data))
+    if "runner" not in cached_worlds_path:
+        with open(cached_worlds_path, "wb") as f:
+            f.write(orjson.dumps(json_data))
     return DataPackage(games=games)
 
 
@@ -175,11 +179,12 @@ def load_worlds(games: Union[Iterable[str], str]) -> DataPackage:
     package: Dict[str, GamesPackage] = {}
     for world_name, world in AutoWorldRegister.world_types.items():
         package[world_name] = world.get_data_package_data()
-    
+
     return DataPackage(games=package)
 
 
 world_sources = scan_worlds()
+cached_worlds_path = cache_path("worlds.json")
 world_data, needs_update = get_worlds_info()
 # TODO change to check if we need to update once everything expects lazy loading
 # if needs_update:
