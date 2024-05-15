bacd3c270b4ba58b17dcb07ea523930c39f8dccf
EnvyDragon <138727357+EnvyDragon@users.noreply.github.com>
2024-05-15 17:33:41 -0400

    requested change

--- worlds/sc2/Client.py
+++ worlds/sc2/Client.py
@@ -833,11 +833,6 @@ def calculate_items(ctx: SC2Context) -> typing.Dict[SC2Race, typing.List[int]]:
         if item_data.quantity == 1:
             accumulators[item_data.race][item_data.type.flag_word] |= 1 << item_data.number
 
-            # Guardian Shell breaks without SoA on version 4+, but can be generated without SoA on version 3
-            if ctx.slot_data_version < 4 and name == ItemNames.GUARDIAN_SHELL \
-                    and ctx.spear_of_adun_autonomously_cast_ability_presence != SpearOfAdunAutonomouslyCastAbilityPresence.option_everywhere:
-                ctx.spear_of_adun_autonomously_cast_ability_presence = SpearOfAdunAutonomouslyCastAbilityPresence.option_protoss
-
         # exists multiple times
         elif item_data.quantity > 1:
             flaggroup = item_data.type.flag_word
@@ -974,6 +969,9 @@ def caclulate_soa_options(ctx: SC2Context) -> int:
         soa_autocasts_presence_value = 2
     elif ctx.spear_of_adun_autonomously_cast_ability_presence == SpearOfAdunAutonomouslyCastAbilityPresence.option_everywhere:
         soa_autocasts_presence_value = 3
+    # Guardian Shell breaks without SoA on version 4+, but can be generated without SoA on version 3
+    if ctx.slow_data_version < 4 and soa_autocasts_presence_value < 2:
+        soa_autocasts_presence_value = 2
     options |= soa_autocasts_presence_value << 3
 
     # Bit 5
