65b084beb1555f8899d29880136e16b0bfc5880e
Silvris <58583688+Silvris@users.noreply.github.com>
2024-05-17 20:49:17 -0500

    pep8

--- test/general/test_implemented.py
+++ test/general/test_implemented.py
@@ -52,3 +52,4 @@ class TestImplemented(unittest.TestCase):
     def test_no_failed_world_loads(self):
         if failed_world_loads:
             self.fail(f"The following worlds failed to load: {failed_world_loads}")
+            
