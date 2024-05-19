791c03c70ea8437e6ac8c8fae4d4390b8ed26918
EnvyDragon <138727357+EnvyDragon@users.noreply.github.com>
2024-05-18 22:31:54 -0400

    sc2: fixing defiler item IDs

--- worlds/sc2/Items.py
+++ worlds/sc2/Items.py
@@ -1266,13 +1266,13 @@ item_table = {
         ItemData(277 + SC2HOTS_ITEM_ID_OFFSET, ZergItemType.Mutation_3, 17, SC2Race.ZERG, parent_item=ItemNames.DEFILER,
                  origin={"ext"}),
     ItemNames.DEFILER_TRAPDOOR_ADAPTATION:
-        ItemData(278 + SC2HOTS_ITEM_ID_OFFSET, ZergItemType.Mutation_3, 18, SC2Race.ZERG, parent_item=ItemNames.DEFILER,
+        ItemData(278 + SC2HOTS_ITEM_ID_OFFSET, ZergItemType.Mutation_3, 19, SC2Race.ZERG, parent_item=ItemNames.DEFILER,
                  origin={"ext"}),
     ItemNames.DEFILER_PREDATORY_CONSUMPTION:
-        ItemData(279 + SC2HOTS_ITEM_ID_OFFSET, ZergItemType.Mutation_3, 19, SC2Race.ZERG, parent_item=ItemNames.DEFILER,
+        ItemData(279 + SC2HOTS_ITEM_ID_OFFSET, ZergItemType.Mutation_3, 20, SC2Race.ZERG, parent_item=ItemNames.DEFILER,
                  origin={"ext"}),
     ItemNames.DEFILER_COMORBIDITY:
-        ItemData(280 + SC2HOTS_ITEM_ID_OFFSET, ZergItemType.Mutation_3, 20, SC2Race.ZERG, parent_item=ItemNames.DEFILER,
+        ItemData(280 + SC2HOTS_ITEM_ID_OFFSET, ZergItemType.Mutation_3, 18, SC2Race.ZERG, parent_item=ItemNames.DEFILER,
                  origin={"ext"}),
 
     ItemNames.ZERGLING_RAPTOR_STRAIN:
