e765ced2e69e64534420805de48b3af10bb15800
Alex Gilbert <alexgilbert@yahoo.com>
2024-05-20 01:41:33 -0400

    - Fixes and improvements to flavored items shipsanity

--- worlds/stardew_valley/strings/artisan_good_names.py
+++ worlds/stardew_valley/strings/artisan_good_names.py
@@ -22,11 +22,11 @@ class ArtisanGood:
     green_tea = "Green Tea"
     mead = "Mead"
     mystic_syrup = "Mystic Syrup"
-    dried_fruit = "Dried (Fruit)"
-    dried_mushroom = "Dried (Mushroom)"
+    dried_fruit = "Dried Fruit"
+    dried_mushroom = "Dried Mushrooms"
     raisins = "Raisins"
     stardrop_tea = "Stardrop Tea"
-    smoked_fish = "Smoked"
+    smoked_fish = "Smoked Fish"
 
     @classmethod
     def specific_wine(cls, fruit: str) -> str:
