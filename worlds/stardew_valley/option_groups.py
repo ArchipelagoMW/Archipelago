ee3b18c718a1d194b8f3bee603c5fdb0f4a2bcbf
Alex Gilbert <alexgilbert@yahoo.com>
2024-05-22 09:58:44 -0400

    - Cleaned up the imports

--- worlds/stardew_valley/option_groups.py
+++ worlds/stardew_valley/option_groups.py
@@ -1,13 +1,13 @@
 from Options import OptionGroup, DeathLink, ProgressionBalancing, Accessibility
-from .options import Goal, StartingMoney, ProfitMargin, BundleRandomization, BundlePrice, EntranceRandomization, \
-    SeasonRandomization, Cropsanity, \
-    BackpackProgression, ToolProgression, ElevatorProgression, SkillProgression, BuildingProgression, FestivalLocations, \
-    ArcadeMachineLocations, \
-    SpecialOrderLocations, QuestLocations, Fishsanity, Museumsanity, Friendsanity, FriendsanityHeartSize, \
-    NumberOfMovementBuffs, NumberOfLuckBuffs, \
-    ExcludeGingerIsland, TrapItems, MultipleDaySleepEnabled, MultipleDaySleepCost, ExperienceMultiplier, \
-    FriendshipMultiplier, DebrisMultiplier, QuickStart, \
-    Gifting, FarmType, Monstersanity, Shipsanity, Cooksanity, Chefsanity, Craftsanity, Mods
+from .options import (Goal, StartingMoney, ProfitMargin, BundleRandomization, BundlePrice,
+                      EntranceRandomization, SeasonRandomization, Cropsanity, BackpackProgression,
+                      ToolProgression, ElevatorProgression, SkillProgression, BuildingProgression,
+                      FestivalLocations, ArcadeMachineLocations, SpecialOrderLocations,
+                      QuestLocations, Fishsanity, Museumsanity, Friendsanity, FriendsanityHeartSize,
+                      NumberOfMovementBuffs, NumberOfLuckBuffs, ExcludeGingerIsland, TrapItems,
+                      MultipleDaySleepEnabled, MultipleDaySleepCost, ExperienceMultiplier,
+                      FriendshipMultiplier, DebrisMultiplier, QuickStart, Gifting, FarmType,
+                      Monstersanity, Shipsanity, Cooksanity, Chefsanity, Craftsanity, Mods)
 
 sv_option_groups = [
     OptionGroup('General', [
