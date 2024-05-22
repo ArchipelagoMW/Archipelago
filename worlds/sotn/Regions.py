76d17b79ad2a99e4275d91a37820eae915030ac8
Felipe Delduque <fdelduque@gmail.com>
2024-05-22 16:01:42 -0300

    Added functions to better handle rules

--- worlds/sotn/Regions.py
+++ worlds/sotn/Regions.py
@@ -1,6 +1,7 @@
 from BaseClasses import MultiWorld, Region
 
 from .Locations import SotnLocation, enemy_locations, drop_locations, castle_table, exploration_table
+from .Rules import sotn_has_flying, sotn_has_any, sotn_has_wolf, sotn_has_reverse, sotn_has_dracula
 
 
 def create_regions(multiworld: MultiWorld, player: int) -> None:
@@ -53,7 +54,6 @@ def create_regions(multiworld: MultiWorld, player: int) -> None:
     for k, v in exploration_table.items():
         no3.locations.append(SotnLocation(player, k, v.location_id, no3))
 
-
     if esanity:
         for k, v in enemy_locations.items():
             region = name_to_region[v.zone]
@@ -69,122 +69,111 @@ def create_regions(multiworld: MultiWorld, player: int) -> None:
     menu.connect(name_to_region["Castle Entrance"])
     # Colosseum
     if not open_are:
-        name_to_region["Colosseum"].connect(name_to_region["Royal Chapel"], "ARE->DAI", lambda state: (state.has("Form of mist", player) and
-                    state.has("Power of mist", player)) or state.has("Gravity boots", player) or
-                    state.has("Leap stone", player) or state.has("Soul of bat", player))
+        name_to_region["Colosseum"].connect(name_to_region["Royal Chapel"], "ARE->DAI", lambda state:
+                                            sotn_has_any(state, player))
     else:
         # Could access normally or thru back door coming from NZ1 or NZ0
-        name_to_region["Colosseum"].connect(name_to_region["Royal Chapel"], "ARE->DAI", lambda state: (state.has("Form of mist", player) and
-                    state.has("Power of mist", player)) or state.has("Gravity boots", player) or
-                    state.has("Leap stone", player) or state.has("Soul of bat", player) or
-                    state.has("Jewel of open", player))
+        name_to_region["Colosseum"].connect(name_to_region["Royal Chapel"], "ARE->DAI", lambda state:
+                                            state.has("Jewel of open", player) or sotn_has_any(state, player))
     name_to_region["Colosseum"].connect(name_to_region["Olrox's Quarters"])
     # Catacombs
     name_to_region["Catacombs"].connect(name_to_region["Abandoned Mine"])
     # Abandoned Mine
     name_to_region["Abandoned Mine"].connect(name_to_region["Catacombs"])
-    name_to_region["Abandoned Mine"].connect(name_to_region["Underground Caverns"])
+    if not open_no4:
+        name_to_region["Abandoned Mine"].connect(name_to_region["Underground Caverns"], "CHI->NO4", lambda state:
+                                                 (state.has("Leap stone", player) or sotn_has_flying(state, player) or
+                                                  sotn_has_wolf(state, player)) and
+                                                 state.has("Jewel of open", player))
+    else:
+        name_to_region["Abandoned Mine"].connect(name_to_region["Underground Caverns"], "CHI->NO4", lambda state:
+                                                 sotn_has_flying(state, player) or ((state.has("Leap stone", player) or
+                                                                                     sotn_has_flying(state, player) or
+                                                                                     sotn_has_wolf(state, player)) and
+                                                                                    state.has("Jewel of open", player)))
     # Royal Chapel
-    name_to_region["Royal Chapel"].connect(name_to_region["Alchemy Laboratory"], "DAI->NZ0", lambda state: state.has("Jewel of open", player) or
-                state.has("Leap stone", player) or state.has("Soul of bat", player) or
-                (state.has("Form of mist", player) and state.has("Power of mist", player)))
+    name_to_region["Royal Chapel"].connect(name_to_region["Alchemy Laboratory"], "DAI->NZ0", lambda state:
+                                           state.has("Jewel of open", player) or
+                                           sotn_has_any(state, player))
     if not open_are:
-        name_to_region["Royal Chapel"].connect(name_to_region["Colosseum"], "DAI->ARE", lambda state: (state.has("Form of mist", player) and
-                    state.has("Power of mist", player)) or state.has("Gravity boots", player) or
-                    state.has("Leap stone", player) or state.has("Soul of bat", player))
+        name_to_region["Royal Chapel"].connect(name_to_region["Colosseum"], "DAI->ARE", lambda state: sotn_has_any(state, player))
     else:
-        name_to_region["Royal Chapel"].connect(name_to_region["Colosseum"], "DAI->ARE", lambda state: (state.has("Form of mist", player) and
-                    state.has("Power of mist", player)) or state.has("Gravity boots", player) or
-                    state.has("Leap stone", player) or state.has("Soul of bat", player) or
-                    state.has("Jewel of open", player))
+        name_to_region["Royal Chapel"].connect(name_to_region["Colosseum"], "DAI->ARE", lambda state:
+                                               sotn_has_any(state, player) or state.has("Jewel of open", player))
     if not open_no2:
-        name_to_region["Royal Chapel"].connect(name_to_region["Olrox's Quarters"], "DAI->NO2", lambda state: (state.has("Form of mist", player) and
-                    state.has("Power of mist", player)) or state.has("Soul of bat", player) or
-                    state.has("Leap stone", player) or state.has("Gravity boots", player))
+        name_to_region["Royal Chapel"].connect(name_to_region["Olrox's Quarters"], "DAI->NO2", lambda state:
+                                               sotn_has_flying(state, player))
     else:
-        name_to_region["Royal Chapel"].connect(name_to_region["Olrox's Quarters"], "DAI->NO2", lambda state: (state.has("Form of mist", player) and
-                    state.has("Power of mist", player)) or state.has("Soul of bat", player) or
-                    state.has("Leap stone", player) or state.has("Gravity boots", player) or
-                    state.has("Jewel of open", player))
+        name_to_region["Royal Chapel"].connect(name_to_region["Olrox's Quarters"], "DAI->NO2", lambda state:
+                                               sotn_has_any(state, player) or state.has("Jewel of open", player))
 
     name_to_region["Royal Chapel"].connect(name_to_region["Castle Keep"])
     # Long Library
     name_to_region["Long Library"].connect(name_to_region["Outer Wall"])
     # Marble Gallery
     name_to_region["Marble Gallery"].connect(name_to_region["Outer Wall"])
-    name_to_region["Marble Gallery"].connect(name_to_region["Olrox's Quarters"], "NO0->NO2", lambda state: state.has("Leap stone", player) or
-                state.has("Soul of bat", player) or (state.has("Form of mist", player) and
-                state.has("Power of mist", player)) or state.has("Gravity boots", player))
-    name_to_region["Marble Gallery"].connect(name_to_region["Underground Caverns"], "NO0->NO4", lambda state: state.has("Jewel of open", player))
+    name_to_region["Marble Gallery"].connect(name_to_region["Olrox's Quarters"], "NO0->NO2", lambda state:
+                                             sotn_has_any(state, player))
+    name_to_region["Marble Gallery"].connect(name_to_region["Underground Caverns"], "NO0->NO4", lambda state:
+                                             state.has("Jewel of open", player))
     name_to_region["Marble Gallery"].connect(name_to_region["Castle Entrance"])
     name_to_region["Marble Gallery"].connect(name_to_region["Alchemy Laboratory"])
     # Outer Wall
     name_to_region["Outer Wall"].connect(name_to_region["Long Library"])
     name_to_region["Outer Wall"].connect(name_to_region["Marble Gallery"])
-    name_to_region["Outer Wall"].connect(name_to_region["Clock Tower"], "NO1->NZ1", lambda state: state.has("Leap stone", player) or
-                state.has("Soul of bat", player) or
-                (state.has("Form of mist", player) and state.has("Power of mist", player)) or
-                state.has("Gravity boots", player))
-    # reinforce NZ1->TOP
+    name_to_region["Outer Wall"].connect(name_to_region["Clock Tower"], "NO1->NZ1", lambda state:
+                                         sotn_has_any(state, player))
     # Olrox's Quarters
     name_to_region["Olrox's Quarters"].connect(name_to_region["Royal Chapel"])
     if not open_no2:
         name_to_region["Olrox's Quarters"].connect(name_to_region["Colosseum"])
     else:
-        (name_to_region["Olrox's Quarters"].connect(name_to_region["Colosseum"], "NO2->ARE"), lambda state: state.has("Soul of bat", player) or
-                    (state.has("Gravity boots", player)) and (state.has("Leap stone", player) or
-                                                               state.has("Soul of wolf", player) or
-                                                               state.has("Form of mist", player)) or
-                    (state.has("Form of mist", player) and state.has("Power of mist", player)))
-    name_to_region["Olrox's Quarters"].connect(name_to_region["Marble Gallery"], "NO2->NO0", lambda state: state.has("Leap stone", player) or
-                state.has("Soul of bat", player) or
-                (state.has("Form of mist", player) and state.has("Power of mist", player)))
+        name_to_region["Olrox's Quarters"].connect(name_to_region["Colosseum"], "NO2->ARE", lambda state:
+                                                   sotn_has_flying(state, player))
+    name_to_region["Olrox's Quarters"].connect(name_to_region["Marble Gallery"], "NO2->NO0", lambda state:
+                                               sotn_has_any(state, player))
     # Castle Entrance
     if not open_no4:
-        name_to_region["Castle Entrance"].connect(name_to_region["Underground Caverns"], "NO3->NO4", lambda state: (state.has("Jewel of open", player)))
+        name_to_region["Castle Entrance"].connect(name_to_region["Underground Caverns"], "NO3->NO4", lambda state:
+                                                  state.has("Jewel of open", player))
     else:
         name_to_region["Castle Entrance"].connect(name_to_region["Underground Caverns"])
     name_to_region["Castle Entrance"].connect(name_to_region["Alchemy Laboratory"])
     name_to_region["Castle Entrance"].connect(name_to_region["Marble Gallery"])
     # Underground Caverns
-    name_to_region["Underground Caverns"].connect(name_to_region["Marble Gallery"], "NO4->NO0", lambda state: state.has("Jewel of open", player))
+    name_to_region["Underground Caverns"].connect(name_to_region["Marble Gallery"], "NO4->NO0", lambda state:
+                                                  state.has("Jewel of open", player))
     if not open_no4:
-        name_to_region["Underground Caverns"].connect(name_to_region["Castle Entrance"], "NO4->NO3", lambda state: state.has("Jewel of open", player))
-        name_to_region["Underground Caverns"].connect(name_to_region["Abandoned Mine"], "NO4->CHI", lambda state: state.has("Leap stone", player) or
-                    state.has("Soul of bat", player) or (state.has("Form of mist", player) and
-                                                         state.has("Power of mist", player)) or
-                    (state.has("Gravity boots", player) and state.has("Leap stone", player)) or
-                    (state.has("Soul of wolf", player) and state.has("Power of wolf", player)))
+        name_to_region["Underground Caverns"].connect(name_to_region["Castle Entrance"], "NO4->NO3", lambda state:
+                                                      state.has("Jewel of open", player))
+        name_to_region["Underground Caverns"].connect(name_to_region["Abandoned Mine"], "NO4->CHI", lambda state:
+                                                      sotn_has_wolf(state, player) or
+                                                      state.has("Leap stone", player) or sotn_has_flying(state, player))
     else:
         name_to_region["Underground Caverns"].connect(name_to_region["Castle Entrance"])
-        name_to_region["Underground Caverns"].connect(name_to_region["Abandoned Mine"], "NO4->CHI", lambda state: state.has("Soul of bat", player) or
-                    (state.has("Form of mist", player) and state.has("Power of mist", player)) or
-                    (state.has("Gravity boots", player) and (state.has("Leap stone", player) or
-                                                             state.has("Soul of wolf", player) or
-                                                             state.has("Form of mist", player))))
+        name_to_region["Underground Caverns"].connect(name_to_region["Abandoned Mine"], "NO4->CHI", lambda state:
+                                                      (state.has("Jewel of Open", player) and
+                                                       (state.has("Leap stone", player) or
+                                                        sotn_has_wolf(state, player))) or
+                                                      sotn_has_flying(state, player))
     # Alchemy Laboratory
     name_to_region["Alchemy Laboratory"].connect(name_to_region["Marble Gallery"])
     name_to_region["Alchemy Laboratory"].connect(name_to_region["Castle Entrance"])
-    name_to_region["Alchemy Laboratory"].connect(name_to_region["Royal Chapel"], "NZ0->DAI", lambda state: (state.has("Jewel of open", player)))
+    name_to_region["Alchemy Laboratory"].connect(name_to_region["Royal Chapel"], "NZ0->DAI", lambda state:
+                                                 state.has("Jewel of open", player))
     # Clock Tower
     name_to_region["Clock Tower"].connect(name_to_region["Outer Wall"])
-    name_to_region["Clock Tower"].connect(name_to_region["Castle Keep"], "NZ1->TOP", lambda state: state.has("Leap stone", player) or
-                state.has("Soul of bat", player) or state.has("Gravity boots", player) or
-                (state.has("Form of mist", player) and state.has("Power of mist", player)))
+    name_to_region["Clock Tower"].connect(name_to_region["Castle Keep"], "NZ1->TOP", lambda state:
+                                          sotn_has_any(state, player))
     # Castle Keep
-    name_to_region["Castle Keep"].connect(name_to_region["Clock Tower"], "TOP->NZ1", lambda state: state.has("Soul of bat", player) or
-                state.has("Gravity boots", player) or
-                (state.has("Form of mist", player) and state.has("Power of mist", player)))
+    name_to_region["Castle Keep"].connect(name_to_region["Clock Tower"], "TOP->NZ1", lambda state:
+                                          sotn_has_flying(state, player))
     name_to_region["Castle Keep"].connect(name_to_region["Royal Chapel"])
-    name_to_region["Castle Keep"].connect(name_to_region["Reverse Castle Keep"], "TOP->RTOP", lambda state: state.has("Holy glasses", player) and
-                (state.has("Soul of bat", player) or (state.has("Gravity boots", player) and
-                (state.has("Soul of wolf", player) or state.has("Form of mist", player))) or
-                (state.has("Form of mist", player) and state.has("Power of mist", player))))
+    name_to_region["Castle Keep"].connect(name_to_region["Reverse Castle Keep"], "TOP->RTOP", lambda state:
+                                          sotn_has_reverse(state,player))
     # Reverse Keep
-    name_to_region["Reverse Castle Keep"].connect(name_to_region["Castle Keep"], "RTOP->TOP", lambda state: state.has("Holy glasses", player) and
-                (state.has("Soul of bat", player) or (state.has("Gravity boots", player) and
-                (state.has("Soul of wolf", player) or state.has("Form of mist", player))) or
-                (state.has("Form of mist", player) and state.has("Power of mist", player))))
+    name_to_region["Reverse Castle Keep"].connect(name_to_region["Castle Keep"], "RTOP->TOP", lambda state:
+                                                  sotn_has_reverse(state, player))
     name_to_region["Reverse Castle Keep"].connect(name_to_region["Anti-Chapel"])
     name_to_region["Reverse Castle Keep"].connect(name_to_region["Reverse Clock Tower"])
     # Anti-Chapel
@@ -216,9 +205,8 @@ def create_regions(multiworld: MultiWorld, player: int) -> None:
     name_to_region["Black Marble Gallery"].connect(name_to_region["Reverse Caverns"])
     name_to_region["Black Marble Gallery"].connect(name_to_region["Reverse Entrance"])
     name_to_region["Black Marble Gallery"].connect(name_to_region["Necromancy Laboratory"])
-    name_to_region["Black Marble Gallery"].connect(name_to_region["Reverse Center Cube"], "RNO0->RCEN", lambda state: state.has("Heart of vlad", player) and
-                 state.has("Tooth of vlad", player) and state.has("Rib of vlad", player) and
-                 state.has("Ring of vlad", player) and state.has("Eye of vlad", player))
+    name_to_region["Black Marble Gallery"].connect(name_to_region["Reverse Center Cube"], "RNO0->RCEN", lambda state:
+                                                   sotn_has_dracula(state, player))
     # Reverse Center Cube
     name_to_region["Reverse Center Cube"]. connect(name_to_region["Black Marble Gallery"])
     # Reverse Castle Entrance
