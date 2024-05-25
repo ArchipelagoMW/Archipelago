68bea82e8bcb38e1c3288985f3ea040b5ca386af
Jouramie <jouramie@hotmail.com>
2024-05-24 23:10:06 -0400

    add time rule for item grind

--- worlds/stardew_valley/logic/time_logic.py
+++ worlds/stardew_valley/logic/time_logic.py
@@ -3,12 +3,19 @@ from typing import Union
 
 from Utils import cache_self1
 from .base_logic import BaseLogic, BaseLogicMixin
+from .has_logic import HasLogicMixin
 from .received_logic import ReceivedLogicMixin
 from ..stardew_rule import StardewRule, HasProgressionPercent, True_
 
 ONE_YEAR = 4
-MAX_MONTHS = 12
-MONTH_COEFFICIENT = 24 // MAX_MONTHS
+MAX_MONTHS = 3 * ONE_YEAR
+PERCENT_REQUIRED_FOR_MAX_MONTHS = 24
+MONTH_COEFFICIENT = PERCENT_REQUIRED_FOR_MAX_MONTHS // MAX_MONTHS
+
+MIN_ITEMS = 10
+MAX_ITEMS = 999
+PERCENT_REQUIRED_FOR_MAX_ITEM = 24
+ITEMS_COEFFICIENT = PERCENT_REQUIRED_FOR_MAX_ITEM // MAX_ITEMS
 
 
 class TimeLogicMixin(BaseLogicMixin):
@@ -17,7 +24,15 @@ class TimeLogicMixin(BaseLogicMixin):
         self.time = TimeLogic(*args, **kwargs)
 
 
-class TimeLogic(BaseLogic[Union[TimeLogicMixin, ReceivedLogicMixin]]):
+class TimeLogic(BaseLogic[Union[TimeLogicMixin, HasLogicMixin, ReceivedLogicMixin]]):
+
+    @cache_self1
+    def has_can_grind_item(self, quantity: int) -> StardewRule:
+        if quantity <= MIN_ITEMS:
+            return self.logic.true_
+
+        quantity = min(quantity, MAX_ITEMS)
+        return HasProgressionPercent(self.player, quantity * ITEMS_COEFFICIENT)
 
     @cache_self1
     def has_lived_months(self, number: int) -> StardewRule:
@@ -32,8 +47,8 @@ class TimeLogic(BaseLogic[Union[TimeLogicMixin, ReceivedLogicMixin]]):
 
     @cached_property
     def has_year_two(self) -> StardewRule:
-        return self.logic.time.has_lived_months(4)
+        return self.logic.time.has_lived_months(2 * ONE_YEAR)
 
     @cached_property
     def has_year_three(self) -> StardewRule:
-        return self.logic.time.has_lived_months(8)
+        return self.logic.time.has_lived_months(3 * ONE_YEAR)
