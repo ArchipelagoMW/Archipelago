70c61717c13d951b26ba46e5e008eec8fb96c858
Alex Gilbert <alexgilbert@yahoo.com>
2024-05-28 07:15:02 -0400

    I need an adult

--- worlds/stardew_valley/option_groups.py
+++ worlds/stardew_valley/option_groups.py
@@ -1,4 +1,6 @@
-from Options import OptionGroup, DeathLink, ProgressionBalancing, Accessibility
+import logging
+
+from Options import DeathLink, ProgressionBalancing, Accessibility
 from .options import (Goal, StartingMoney, ProfitMargin, BundleRandomization, BundlePrice,
                       EntranceRandomization, SeasonRandomization, Cropsanity, BackpackProgression,
                       ToolProgression, ElevatorProgression, SkillProgression, BuildingProgression,
@@ -9,59 +11,65 @@ from .options import (Goal, StartingMoney, ProfitMargin, BundleRandomization, Bu
                       FriendshipMultiplier, DebrisMultiplier, QuickStart, Gifting, FarmType,
                       Monstersanity, Shipsanity, Cooksanity, Chefsanity, Craftsanity, Mods, Booksanity, Walnutsanity)
 
-sv_option_groups = [
-    OptionGroup("General", [
-        Goal,
-        FarmType,
-        BundleRandomization,
-        BundlePrice,
-        EntranceRandomization,
-        ExcludeGingerIsland,
-    ]),
-    OptionGroup("Major Unlocks", [
-        SeasonRandomization,
-        Cropsanity,
-        BackpackProgression,
-        ToolProgression,
-        ElevatorProgression,
-        SkillProgression,
-        BuildingProgression,
-    ]),
-    OptionGroup("Extra Shuffling", [
-        FestivalLocations,
-        ArcadeMachineLocations,
-        SpecialOrderLocations,
-        QuestLocations,
-        Fishsanity,
-        Museumsanity,
-        Friendsanity,
-        FriendsanityHeartSize,
-        Monstersanity,
-        Shipsanity,
-        Cooksanity,
-        Chefsanity,
-        Craftsanity,
-        Booksanity,
-        Walnutsanity,
-    ]),
-    OptionGroup("Multipliers and Buffs", [
-        StartingMoney,
-        ProfitMargin,
-        ExperienceMultiplier,
-        FriendshipMultiplier,
-        DebrisMultiplier,
-        NumberOfMovementBuffs,
-        NumberOfLuckBuffs,
-        TrapItems,
-        MultipleDaySleepEnabled,
-        MultipleDaySleepCost,
-        QuickStart,
-    ]),
-    OptionGroup("Advanced Options", [
-        Gifting,
-        DeathLink,
-        Mods,
-        ProgressionBalancing,
-        Accessibility,
-    ]),
-]
+sv_option_groups = []
+try:
+    from Options import OptionGroup
+except:
+    logging.warning("Old AP Version, OptionGroup not available.")
+else:
+    sv_option_groups = [
+        OptionGroup("General", [
+            Goal,
+            FarmType,
+            BundleRandomization,
+            BundlePrice,
+            EntranceRandomization,
+            ExcludeGingerIsland,
+        ]),
+        OptionGroup("Major Unlocks", [
+            SeasonRandomization,
+            Cropsanity,
+            BackpackProgression,
+            ToolProgression,
+            ElevatorProgression,
+            SkillProgression,
+            BuildingProgression,
+        ]),
+        OptionGroup("Extra Shuffling", [
+            FestivalLocations,
+            ArcadeMachineLocations,
+            SpecialOrderLocations,
+            QuestLocations,
+            Fishsanity,
+            Museumsanity,
+            Friendsanity,
+            FriendsanityHeartSize,
+            Monstersanity,
+            Shipsanity,
+            Cooksanity,
+            Chefsanity,
+            Craftsanity,
+            Booksanity,
+            Walnutsanity,
+        ]),
+        OptionGroup("Multipliers and Buffs", [
+            StartingMoney,
+            ProfitMargin,
+            ExperienceMultiplier,
+            FriendshipMultiplier,
+            DebrisMultiplier,
+            NumberOfMovementBuffs,
+            NumberOfLuckBuffs,
+            TrapItems,
+            MultipleDaySleepEnabled,
+            MultipleDaySleepCost,
+            QuickStart,
+        ]),
+        OptionGroup("Advanced Options", [
+            Gifting,
+            DeathLink,
+            Mods,
+            ProgressionBalancing,
+            Accessibility,
+        ]),
+    ]
