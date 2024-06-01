4e5b6bb3d23277aad83865f2c909e627ca64f2c2
Silvris <58583688+Silvris@users.noreply.github.com>
2024-06-01 06:34:41 -0500

    Core: move PlandoConnections and PlandoTexts to the options system (#2904)
    Co-authored-by: Doug Hoskisson <beauxq@users.noreply.github.com>
    Co-authored-by: Scipio Wright <scipiowright@gmail.com>
    Co-authored-by: beauxq <beauxq@yahoo.com>
    Co-authored-by: alwaysintreble <mmmcheese158@gmail.com>

--- worlds/alttp/Rom.py
+++ worlds/alttp/Rom.py
@@ -2538,12 +2538,12 @@ def write_strings(rom, world, player):
         tt['menu_start_2'] = "{MENU}\n{SPEED0}\n≥@'s house\n Dark Chapel\n{CHOICE3}"
         tt['menu_start_3'] = "{MENU}\n{SPEED0}\n≥@'s house\n Dark Chapel\n Mountain Cave\n{CHOICE2}"
 
-    for at, text in world.plando_texts[player].items():
+    for at, text, _ in world.plando_texts[player]:
 
         if at not in tt:
             raise Exception(f"No text target \"{at}\" found.")
         else:
-            tt[at] = text
+            tt[at] = "\n".join(text)
 
     rom.write_bytes(0xE0000, tt.getBytes())
 
