5d858f2ecd5eeb5bdd3bf8d9ba9f6189741827a6
Zach Parks <zach@alliware.com>
2024-06-01 17:30:10 -0500

    Utils: Fix mistake made with `KeyedDefaultDict` from #1933 that broke tracker functionality.

--- Utils.py
+++ Utils.py
@@ -458,8 +458,8 @@ class KeyedDefaultDict(collections.defaultdict):
     """defaultdict variant that uses the missing key as argument to default_factory"""
     default_factory: typing.Callable[[typing.Any], typing.Any]
 
-    def __init__(self, default_factory: typing.Callable[[Any], Any] = None, **kwargs):
-        super().__init__(default_factory, **kwargs)
+    def __init__(self, default_factory: typing.Callable[[Any], Any] = None, *args, **kwargs):
+        super().__init__(default_factory, *args, **kwargs)
 
     def __missing__(self, key):
         self[key] = value = self.default_factory(key)
