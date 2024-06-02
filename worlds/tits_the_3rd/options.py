1c9792c98b2c53b5778ae2e7977e8ab70e130ef5
Trevor Dylan Kollins <tdkollins@uwaterloo.ca>
2024-06-02 16:26:44 -0400

    Boilerplate code for the Trails in the Sky the 3rd World

---
+++ worlds/tits_the_3rd/options.py
@@ -0,0 +1,8 @@
+"""This module represents options definitions for Trails in the Sky the 3rd"""
+from dataclasses import dataclass
+
+from Options import PerGameCommonOptions
+
+@dataclass
+class TitsThe3rdOptions(PerGameCommonOptions):
+    """Trails in the Sky the 3rd options Definition"""
---
+++ worlds/tits_the_3rd/web.py
@@ -0,0 +1,5 @@
+"""Web UI related classes for Trails in the Sky the 3rd"""
+from worlds.AutoWorld import WebWorld
+
+class TitsThe3rdWeb(WebWorld):
+    """Web integration for Trails in the Sky the 3rd"""
