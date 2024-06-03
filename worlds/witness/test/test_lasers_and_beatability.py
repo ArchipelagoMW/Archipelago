5e22318e9e6aca18f20e1a1df48a0bbd6d646934
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-06-03 04:19:42 +0200

    Add more minimal beatability tests

--- worlds/witness/test/test_lasers_and_beatability.py
+++ worlds/witness/test/test_lasers_and_beatability.py
@@ -18,8 +18,9 @@ class TestDisableNonRandomizedLasers(WitnessTestBase):
         self.assertTrue(_has_lasers(2, self.world, False)(self.multiworld.state))
 
 
-class TestSymbolsRequiredToWinNormal(WitnessTestBase):
+class TestSymbolsRequiredToWinElevatorNormal(WitnessTestBase):
     options = {
+        "shuffle_lasers": True,
         "puzzle_randomization": "sigma_normal",
         "mountain_lasers": 1,
         "victory_condition": "elevator",
@@ -28,6 +29,7 @@ class TestSymbolsRequiredToWinNormal(WitnessTestBase):
 
     def test_symbols_to_win(self):
         exact_requirement = {
+            "Monastery Laser": 1,
             "Progressive Dots": 2,
             "Progressive Stars": 2,
             "Progressive Symmetry": 2,
@@ -41,8 +43,9 @@ class TestSymbolsRequiredToWinNormal(WitnessTestBase):
         self.assert_can_beat_with_minimally(exact_requirement)
 
 
-class TestSymbolsRequiredToWinExpert(WitnessTestBase):
+class TestSymbolsRequiredToWinElevatorExpert(WitnessTestBase):
     options = {
+        "shuffle_lasers": True,
         "mountain_lasers": 1,
         "victory_condition": "elevator",
         "early_symbol_item": False,
@@ -51,6 +54,7 @@ class TestSymbolsRequiredToWinExpert(WitnessTestBase):
 
     def test_symbols_to_win(self):
         exact_requirement = {
+            "Monastery Laser": 1,
             "Progressive Dots": 2,
             "Progressive Stars": 2,
             "Progressive Symmetry": 2,
@@ -66,8 +70,9 @@ class TestSymbolsRequiredToWinExpert(WitnessTestBase):
         self.assert_can_beat_with_minimally(exact_requirement)
 
 
-class TestSymbolsRequiredToWinVanilla(WitnessTestBase):
+class TestSymbolsRequiredToWinElevatorVanilla(WitnessTestBase):
     options = {
+        "shuffle_lasers": True,
         "mountain_lasers": 1,
         "victory_condition": "elevator",
         "early_symbol_item": False,
@@ -76,6 +81,7 @@ class TestSymbolsRequiredToWinVanilla(WitnessTestBase):
 
     def test_symbols_to_win(self):
         exact_requirement = {
+            "Monastery Laser": 1,
             "Progressive Dots": 2,
             "Progressive Stars": 2,
             "Progressive Symmetry": 1,
@@ -84,6 +90,68 @@ class TestSymbolsRequiredToWinVanilla(WitnessTestBase):
             "Shapers": 1,
             "Rotated Shapers": 1,
             "Eraser": 1,
+        }
+
+        self.assert_can_beat_with_minimally(exact_requirement)
+
+
+class TestPanelsRequiredToWinElevator(WitnessTestBase):
+    options = {
+        "shuffle_lasers": True,
+        "mountain_lasers": 1,
+        "victory_condition": "elevator",
+        "early_symbol_item": False,
+        "shuffle_symbols": False,
+        "shuffle_doors": "panels",
+        "door_groupings": "off",
+    }
+
+    def test_panels_to_win(self):
+        exact_requirement = {
+            "Desert Laser": 1,
+            "Town Desert Laser Redirect Control (Panel)": 1,
+            "Mountain Floor 1 Light Bridge (Panel)": 1,
+            "Mountain Floor 2 Light Bridge Near (Panel)": 1,
+            "Mountain Floor 2 Light Bridge Far (Panel)": 1,
+            "Mountain Floor 2 Elevator Control (Panel)": 1,
+        }
+
+        self.assert_can_beat_with_minimally(exact_requirement)
+
+
+class TestDoorsRequiredToWinElevator(WitnessTestBase):
+    options = {
+        "shuffle_lasers": True,
+        "mountain_lasers": 1,
+        "victory_condition": "elevator",
+        "early_symbol_item": False,
+        "shuffle_symbols": False,
+        "shuffle_doors": "doors",
+        "door_groupings": "off",
+    }
+
+    def test_win_from_upper_mountain(self):
+        exact_requirement = {
+            "Monastery Laser": 1,
+            "Mountain Floor 1 Exit (Door)": 1,
+            "Mountain Floor 2 Staircase Near (Door)": 1,
+            "Mountain Floor 2 Staircase Far (Door)": 1,
+            "Mountain Floor 2 Exit (Door)": 1,
+            "Mountain Bottom Floor Giant Puzzle Exit (Door)": 1,
+            "Mountain Bottom Floor Pillars Room Entry (Door)": 1,
+        }
+
+        self.assert_can_beat_with_minimally(exact_requirement)
+
+    def test_win_through_caves(self):
+        # Note: Keep Laser is always accessible in these settings.
+
+        exact_requirement = {
+            "Monastery Laser": 1,  # Elevator Panel itself has a laser lock
+            "Caves Mountain Shortcut (Door)": 1,
+            "Caves Entry (Door)": 1,
+            "Mountain Bottom Floor Rock (Door)": 1,
+            "Mountain Bottom Floor Pillars Room Entry (Door)": 1,
         }
 
         self.assert_can_beat_with_minimally(exact_requirement)
