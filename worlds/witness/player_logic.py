2ff6422be659d37ba501f51feb7944b9206b85a4
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-06-08 23:24:23 +0200

    Pull out Entity Hunt Picking into its own class, split it into many functions. Kept a lot of the comments tho

--- worlds/witness/player_logic.py
+++ worlds/witness/player_logic.py
@@ -17,8 +17,6 @@ When the world has parsed its options, a second function is called to finalize t
 
 import copy
 from collections import defaultdict
-from logging import debug
-from pprint import pformat
 from typing import TYPE_CHECKING, Dict, List, Set, Tuple, cast
 
 from .data import static_logic as static_witness_logic
@@ -52,6 +50,7 @@ from .data.utils import (
     logical_or_witness_rules,
     parse_lambda,
 )
+from .entity_hunt import EntityHuntPicker
 
 if TYPE_CHECKING:
     from . import WitnessWorld
@@ -409,7 +408,7 @@ class WitnessPlayerLogic:
 
         if disable_mountain_lasers:
             self.DISABLE_EVERYTHING_BEHIND.add("0x09F7F")  # Short box
-            self.HUNT_ENTITIES.add("0x09F7F")
+            self.PRE_PICKED_HUNT_ENTITIES.add("0x09F7F")
             self.COMPLETELY_DISABLED_ENTITIES.add("0x3D9A9")  # Elevator Start
 
             # If mountain lasers are disabled, and challenge lasers > 7, the box will need to be rotated
@@ -421,7 +420,7 @@ class WitnessPlayerLogic:
 
         if disable_challenge_lasers:
             self.DISABLE_EVERYTHING_BEHIND.add("0xFFF00")  # Long box
-            self.HUNT_ENTITIES.add("0xFFF00")
+            self.PRE_PICKED_HUNT_ENTITIES.add("0xFFF00")
             self.COMPLETELY_DISABLED_ENTITIES.add("0x0A332")  # Challenge Timer
 
         return postgame_adjustments
@@ -854,140 +853,6 @@ class WitnessPlayerLogic:
             item_name for item_name, is_required in is_item_required_dict.items() if not is_required
         }
 
-    def pick_panel_hunt_panels(self, world: "WitnessWorld"):
-        disallowed_entities_for_panel_hunt = {
-            "0x03629",  # Tutorial Gate Open, which is the panel that is locked by panel hunt
-            "0x03505",  # Tutorial Gate Close (same thing)
-            "0x3352F",  # Gate EP (same thing)
-            "0x00CDB",  # Challenge Reallocating
-            "0x0051F",  # Challenge Reallocating
-            "0x00524",  # Challenge Reallocating
-            "0x00CD4",  # Challenge Reallocating
-            "0x00CB9",  # Challenge May Be Unsolvable
-            "0x00CA1",  # Challenge May Be Unsolvable
-            "0x00C80",  # Challenge May Be Unsolvable
-            "0x00C68",  # Challenge May Be Unsolvable
-            "0x00C59",  # Challenge May Be Unsolvable
-            "0x00C22",  # Challenge May Be Unsolvable
-            "0x0A3A8",  # Reset PP
-            "0x0A3B9",  # Reset PP
-            "0x0A3BB",  # Reset PP
-            "0x0A3AD",  # Reset PP
-        }
-
-        all_eligible_panels = [
-            entity_hex for entity_hex, entity_obj in static_witness_logic.ENTITIES_BY_HEX.items()
-            if entity_obj["entityType"] == "Panel" and self.solvability_guaranteed(entity_hex)
-            and not (
-                # Due to an edge case, Discards have to be on in disable_non_randomized even if Discard Shuffle is off.
-                # However, I don't think they should be hunt panels in this case.
-                world.options.disable_non_randomized_puzzles
-                and not world.options.shuffle_discarded_panels
-                and entity_obj["locationType"] == "Discard"
-            )
-            and entity_hex not in disallowed_entities_for_panel_hunt
-            and entity_hex not in self.HUNT_ENTITIES
-        ]
-
-        total_panels = world.options.panel_hunt_total.value
-        same_area_discouragement_factor = world.options.panel_hunt_discourage_same_area_factor / 100
-
-        # If we're using random picking, just choose all the panels now and return
-        if not same_area_discouragement_factor:
-            hunt_panels = world.random.choices(all_eligible_panels, k=total_panels - len(self.HUNT_ENTITIES))
-            self.HUNT_ENTITIES.update(hunt_panels)
-            return
-
-        # Make a lookup of eligible hunt panels per area
-        eligible_panels_by_area = defaultdict(set)
-        for eligible_panel in all_eligible_panels:
-            associated_area = static_witness_logic.ENTITIES_BY_HEX[eligible_panel]["area"]["name"]
-            eligible_panels_by_area[associated_area].add(eligible_panel)
-
-        # If we're discouraging panels from the same area being picked, we have to pick panels one at a time
-        # For higher total counts, we do them in small batches for performance
-        batch_size = max(1, total_panels // 20)
-
-        contributing_percentage_per_area = {area: 0 for area in eligible_panels_by_area}
-
-        while len(self.HUNT_ENTITIES) < total_panels:
-            actual_amount_to_pick = min(batch_size, total_panels - len(self.HUNT_ENTITIES))
-
-            max_percentage = max(contributing_percentage_per_area.values())
-            if max_percentage == 0:
-                allowance_per_area = {area: 1 for area in contributing_percentage_per_area}
-            else:
-                allowance_per_area = {
-                    area: (max_percentage - current_percentage) / max_percentage
-                    for area, current_percentage in contributing_percentage_per_area.items()
-                }
-                # use same_area_discouragement as lerp factor
-                allowance_per_area = {
-                    area: (1.0 - same_area_discouragement_factor) + (weight * same_area_discouragement_factor)
-                    for area, weight in allowance_per_area.items()
-                }
-
-            assert min(allowance_per_area.values()) >= 0, (f"Somehow, an area had a negative weight when picking"
-                                                           f" hunt panels: {allowance_per_area}")
-
-            remaining_panels, remaining_panels_weights = [], []
-            for area, eligible_panels in eligible_panels_by_area.items():
-                for panel in eligible_panels - self.HUNT_ENTITIES:
-                    remaining_panels.append(panel)
-                    remaining_panels_weights.append(allowance_per_area[area])
-
-            # I don't think this can ever happen, but let's be safe
-            if sum(remaining_panels_weights) == 0:
-                remaining_panels_weights = [1] * len(remaining_panels_weights)
-
-            self.HUNT_ENTITIES.update(
-                world.random.choices(remaining_panels, weights=remaining_panels_weights, k=actual_amount_to_pick)
-            )
-
-            contributing_percentage_per_area = dict()
-            for area, eligible_panels in eligible_panels_by_area.items():
-                amount_of_already_chosen_panels = len(eligible_panels_by_area[area] & self.HUNT_ENTITIES)
-                current_percentage = amount_of_already_chosen_panels / len(self.HUNT_ENTITIES)
-                contributing_percentage_per_area[area] = current_percentage
-
-        # Replace some of the connected panels with the one you're guaranteed to be able to solve first
-        replacements = {
-            "0x18488": "0x00609",  # Replace Swamp Sliding Bridge Underwater with Swamp Sliding Bridge Above Water
-            "0x03676": "0x03678",  # Replace Quarry Upper Ramp Control with Lower Ramp Control
-            "0x03675": "0x03679",  # Replace Quarry Upper Lift Control with Lower Lift Control
-        }
-
-        if world.options.shuffle_doors < 2:
-            replacements.update({
-                "0x334DC": "0x334DB",  # In door shuffle, the Shadows Timer Panels are disconnected
-                "0x17CBC": "0x2700B",  # In door shuffle, the Laser Timer Panels are disconnected
-            })
-
-        for bad_entitiy, good_entity in replacements.items():
-            # If the bad entity was picked as a hunt entity ...
-            if bad_entitiy not in self.HUNT_ENTITIES:
-                continue
-
-            # ... and the good entity was not ...
-            if good_entity in self.HUNT_ENTITIES or good_entity not in all_eligible_panels:
-                continue
-
-            # ... replace the bad entity with the good entity.
-            self.HUNT_ENTITIES.remove(bad_entitiy)
-            self.HUNT_ENTITIES.add(good_entity)
-
-        # Some logging
-        sorted_area_percentages_dict = dict(sorted(contributing_percentage_per_area.items(), key=lambda x: x[1]))
-        sorted_area_percentages_dict = {
-            area: str(percentage) + (" (maxed)" if eligible_panels_by_area[area] <= self.HUNT_ENTITIES else "")
-            for area, percentage in sorted_area_percentages_dict.items()
-        }
-        player_name = world.multiworld.get_player_name(world.player)
-        debug(
-            f'Final area percentages for player "{player_name}" ({same_area_discouragement_factor} discouragement): '
-            f"{pformat(sorted_area_percentages_dict)}"
-        )
-
     def make_event_panel_lists(self) -> None:
         """
         Makes event-item pairs for entities with associated events, unless these entities are disabled.
@@ -1068,6 +933,7 @@ class WitnessPlayerLogic:
         self.ADDED_CHECKS = set()
         self.VICTORY_LOCATION = "0x0356B"
 
+        self.PRE_PICKED_HUNT_ENTITIES = set()
         self.HUNT_ENTITIES = set()
 
         self.ALWAYS_EVENT_NAMES_BY_HEX = {
@@ -1102,7 +968,7 @@ class WitnessPlayerLogic:
         self.make_dependency_reduced_checklist()
 
         if world.options.victory_condition == "panel_hunt":
-            self.pick_panel_hunt_panels(world)
+            self.HUNT_ENTITIES = EntityHuntPicker(self, world, self.PRE_PICKED_HUNT_ENTITIES).pick_panel_hunt_panels()
 
         # Finalize which items actually exist in the MultiWorld and which get grouped into progressive items.
         self.finalize_items()
