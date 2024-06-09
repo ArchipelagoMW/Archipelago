d3ca17e722d0225c113419561bbc0ce41f0def68
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-06-09 22:16:50 +0200

    Fix postgame thinking it's the wrong panel

--- worlds/witness/player_logic.py
+++ worlds/witness/player_logic.py
@@ -399,6 +399,16 @@ class WitnessPlayerLogic:
         mnt_lasers = world.options.mountain_lasers
         chal_lasers = world.options.challenge_lasers
 
+        # Victory Condition
+        if victory == "elevator":
+            self.VICTORY_LOCATION = "0x3D9A9"
+        elif victory == "challenge":
+            self.VICTORY_LOCATION = "0x0356B"
+        elif victory == "mountain_box_short":
+            self.VICTORY_LOCATION = "0x09F7F"
+        elif victory == "mountain_box_long":
+            self.VICTORY_LOCATION = "0xFFF00"
+
         # Exclude panels from the post-game if shuffle_postgame is false.
         if not world.options.shuffle_postgame:
             adjustment_linesets_in_order += self.handle_postgame(world)
@@ -417,17 +427,6 @@ class WitnessPlayerLogic:
             adjustment_linesets_in_order.append(get_vault_exclusion_list())
             if not victory == "challenge":
                 adjustment_linesets_in_order.append(["Disabled Locations:", "0x0A332"])
-
-        # Victory Condition
-
-        if victory == "elevator":
-            self.VICTORY_LOCATION = "0x3D9A9"
-        elif victory == "challenge":
-            self.VICTORY_LOCATION = "0x0356B"
-        elif victory == "mountain_box_short":
-            self.VICTORY_LOCATION = "0x09F7F"
-        elif victory == "mountain_box_long":
-            self.VICTORY_LOCATION = "0xFFF00"
 
         # Long box can usually only be solved by opening Mountain Entry. However, if it requires 7 lasers or less
         # (challenge_lasers <= 7), you can now solve it without opening Mountain Entry first.
