96913a03682b96e53f47c6df4667555e334f15f0
Jouramie <jouramie@hotmail.com>
2024-06-12 20:04:39 -0400

    remove max level to skills

--- worlds/stardew_valley/data/skill.py
+++ worlds/stardew_valley/data/skill.py
@@ -7,9 +7,3 @@ from ..data.game_item import kw_only
 class Skill:
     name: str
     has_mastery: bool = field(**kw_only)
-
-    @property
-    def max_level(self):
-        if self.has_mastery:
-            return 15
-        return 10
