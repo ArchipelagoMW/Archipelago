8e9dd65e21f9f3ad5c53ee9b8199625cb916ad46
Silvris <58583688+Silvris@users.noreply.github.com>
2024-06-14 19:57:49 -0500

    presets!

--- ColorPicker.py
+++ ColorPicker.py
@@ -1,24 +1,28 @@
+import os
+
 from kivy.app import App
 from kivy.metrics import dp
+from kivy.uix.textinput import TextInput
 
 import Utils
 import typing
 from kvui import ContainerLayout, MainLayout, KivyJSONtoTextParser
+from kivy.lang.builder import Builder
+from kivy.lang.parser import Parser
 from kivy.uix.button import Button
 from kivy.uix.colorpicker import ColorPicker
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.scrollview import ScrollView
-from kivy.effects.scroll import ScrollEffect
 from kivy.uix.boxlayout import BoxLayout
-
-from kivy.graphics import Color, Rectangle
+from kivy.uix.dropdown import DropDown
+from kivy.uix.popup import Popup
+from kivy.uix.widget import Widget
 
 if __name__ == "__main__":
     import ModuleUpdate
     ModuleUpdate.update()
 
 
-def hex_color_to_tuple(color: str):
+def hex_color_to_tuple(color: str) -> typing.Tuple[float, float, float, float]:
     if len(color) != 6:
         color = color[:5]
     red = color[:2]
@@ -30,7 +34,7 @@ def hex_color_to_tuple(color: str):
     return r_ratio, g_ratio, b_ratio, 1.0
 
 
-default_colors = {
+default_colors: typing.Dict[str, str] = {
         "white": "FFFFFF",
         "black": "000000",
         "red": "EE0000",
@@ -51,11 +55,14 @@ class ColorPickerApp(App):
     grid: MainLayout
     color_picker: ColorPicker
     button_layout: GridLayout
-    text_colors = default_colors.copy()
-    buttons = {}
-    current_color = "red"
+    options_layout: BoxLayout
+    presets_button: Button
+    preset_dropdown: DropDown
+    text_colors: typing.Dict[str, str] = default_colors.copy()
+    buttons: typing.Dict[str, Button] = {}
+    current_color: str = "red"
 
-    def __init__(self, ctx=None):
+    def __init__(self, ctx=None) -> None:
         self.title = self.base_title
         self.ctx = ctx
         self.icon = r"data/icon.png"
@@ -64,19 +71,29 @@ class ColorPickerApp(App):
         for color in self.text_colors:
             self.text_colors[color] = getattr(text_colors, color, "FFFFFF")
 
-    def build(self):
+    def build(self) -> Widget:
         self.container = ContainerLayout()
         self.grid = MainLayout(cols=1)
         self.grid.spacing = 20
         self.grid.padding = 20
         self.container.add_widget(self.grid)
         self.color_picker = ColorPicker(color=hex_color_to_tuple(self.text_colors[self.current_color]))
-        self.color_picker.bind(color=self.on_color)
+        self.color_picker.bind(color=lambda picker, x: self.on_color(picker, x))
         self.grid.add_widget(self.color_picker)
         self.options_layout = BoxLayout(size_hint_y=None, height=dp(40), orientation="horizontal")
         defaults_button = Button(text="Restore Defaults")
-        defaults_button.bind(on_release=self.restore_defaults)
+        defaults_button.bind(on_release=lambda _: self.restore_defaults())
         self.options_layout.add_widget(defaults_button)
+        self.presets_button = Button(text="Preset Loaded: None")
+        self.preset_dropdown = DropDown()
+        self.populate_dropdown(self.preset_dropdown)
+
+        def open_dropdown(button: Button) -> None:
+            self.preset_dropdown.open(button)
+
+        self.presets_button.bind(on_release=self.preset_dropdown.open)
+        self.preset_dropdown.bind(on_select=lambda instance, x: self.set_preset(instance, x))
+        self.options_layout.add_widget(self.presets_button)
         self.grid.add_widget(self.options_layout)
         self.button_layout = GridLayout(cols=3, size_hint_y=0.5)
         self.button_layout.spacing = 5
@@ -84,47 +101,105 @@ class ColorPickerApp(App):
 
         for color in self.text_colors:
             new_button = Button(text=color.title(), background_color=hex_color_to_tuple(self.text_colors[color]))
-            new_button.bind(on_release=self.set_color)
+            new_button.bind(on_release=lambda button: self.set_color(button))
             self.buttons[color] = new_button
             self.button_layout.add_widget(new_button)
 
         self.grid.add_widget(self.button_layout)
         return self.container
 
-    @staticmethod
-    def set_color(button):
-        picker_app: ColorPickerApp = App.get_running_app()
-        picker_app.current_color = button.text.lower()
-        picker_app.color_picker.set_color(hex_color_to_tuple(picker_app.text_colors[picker_app.current_color]))
+    def populate_dropdown(self, dropdown: DropDown) -> None:
+        dropdown.clear_widgets()
+        path = Utils.local_path("data", "presets")
+        for dirpath, _, files in os.walk(path):
+            for file in files:
+                if file.endswith(".kv"):
+                    new_button = Button(text=file.replace(".kv", ""), size_hint_y=None)
+                    new_button.path = os.path.join(dirpath, file)
+                    new_button.bind(on_release=lambda button: self.preset_dropdown.select(button.path))
+                    dropdown.add_widget(new_button)
+        save_button = Button(text="Save as new Preset?", size_hint_y=None)
+        save_button.bind(on_release=lambda button: self.save_preset_menu())
+        dropdown.add_widget(save_button)
+
+    def set_preset(self, _: DropDown, path: str) -> None:
+        preset_name, _ = os.path.splitext(os.path.basename(path))
+        setattr(self.presets_button, "text", f"Preset Loaded: {preset_name}")
+        self.parse_preset_kv(path)
+
+    def set_color(self, button: Button):
+        self.current_color = button.text.lower()
+        self.color_picker.set_color(hex_color_to_tuple(self.text_colors[self.current_color]))
+
+    def get_color(self, button):
+        return hex_color_to_tuple(self.text_colors[button.text.lower()])
+
+    def on_color(self, instance, value):
+        self.text_colors[self.current_color] = instance.hex_color[1:-2]  # formatted usually #RRGGBBAA
+        self.buttons[self.current_color].background_color = value
+
+    def restore_defaults(self):
+        self.set_colors(default_colors)
+
+    def set_colors(self, colors):
+        self.text_colors = colors.copy()
+        for button in self.buttons:
+            self.buttons[button].background_color = hex_color_to_tuple(self.text_colors[button])
+        self.color_picker.set_color(hex_color_to_tuple(self.text_colors[self.current_color]))
+
+    def parse_preset_kv(self, path):
+        colors = default_colors.copy()
+        # This is thick in the Kivy weeds
+        parser = Parser(content=open(path, 'r').read())
+        for selector, rule in parser.rules:
+            if selector.key == "label":
+                colors["white"] = rule.properties["color"].co_value
+            elif selector.key == "textcolors":
+                for prop in rule.properties:
+                    colors[prop] = rule.properties[prop].co_value
+        self.set_colors(colors)
+
+    def save_preset_menu(self):
+        save: bool = False
+        outer_box = BoxLayout(orientation="vertical", spacing=10)
+        preset_name = TextInput(size_hint_y=None, height=50)
+        inner_box = BoxLayout(orientation="horizontal", spacing=10)
+        outer_box.add_widget(preset_name)
+        outer_box.add_widget(inner_box)
+        cancel = Button(text="Cancel", size_hint_y=None, height=50)
+        confirm = Button(text="Confirm", size_hint_y=None, height=50)
+        inner_box.add_widget(cancel)
+        inner_box.add_widget(confirm)
+        popup = Popup(title="Save Preset As...",
+                      content=outer_box, size_hint=(0.5, 0.3))
+
+        def on_close(res):
+            if res:
+                name = preset_name.text
+                if name:
+                    popup.dismiss()
+                    file_path = os.path.join(Utils.local_path("data", "presets", f"{name}.kv"))
+                    self.write_color_file(open(file_path, 'w'), self.text_colors)
+                    self.populate_dropdown(self.preset_dropdown)
+                    self.preset_dropdown.select(file_path)
+                    self.preset_dropdown.dismiss()
+            else:
+                popup.dismiss()
+
+        cancel.bind(on_release=lambda button: on_close(False))
+        confirm.bind(on_release=lambda button: on_close(True))
+        popup.open()
 
     @staticmethod
-    def get_color(button):
-        picker_app: ColorPickerApp = App.get_running_app() # little surprised this doesn't give type warnings
-        return hex_color_to_tuple(picker_app.text_colors[button.text.lower()])
-
-    @staticmethod
-    def on_color(instance, value):
-        picker_app: ColorPickerApp = App.get_running_app()
-        picker_app.text_colors[picker_app.current_color] = instance.hex_color[1:-2] # formatted usually #RRGGBBAA
-        picker_app.buttons[picker_app.current_color].background_color = value
-
-    @staticmethod
-    def restore_defaults(button):
-        picker_app: ColorPickerApp = App.get_running_app()
-        picker_app.text_colors = default_colors.copy()
-        for button in picker_app.buttons:
-            picker_app.buttons[button].background_color = hex_color_to_tuple(picker_app.text_colors[button])
-        picker_app.color_picker.set_color(hex_color_to_tuple(picker_app.text_colors[picker_app.current_color]))
-
-    def set_colors(self, text_colors: KivyJSONtoTextParser.TextColors):
-        pass
+    def write_color_file(file, colors):
+        file.write("<TextColors>:\n")
+        for color in colors:
+            file.write(f"\t{color}: \"{colors[color].upper()}\"\n")
+        file.write(f"<Label>:\n\tcolor: \"{colors['white']}\"")
 
     def on_stop(self):
         user_colors = open(Utils.local_path("data", "user.kv"), 'w')
-        user_colors.write("<TextColors>:\n")
-        for color in self.text_colors:
-            user_colors.write(f"\t{color}: \"{self.text_colors[color].upper()}\"\n")
-        user_colors.write(f"<Label>:\n\tcolor: \"{self.text_colors['white']}\"")
+        self.write_color_file(user_colors, self.text_colors)
         super().on_stop()
 
 
