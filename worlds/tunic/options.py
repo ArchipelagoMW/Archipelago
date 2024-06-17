e67ffc5610960b15b5648f0bf176ab682a6cac82
Scipio Wright <scipiowright@gmail.com>
2024-06-17 09:50:52 -0400

    Further update LS description

--- worlds/tunic/options.py
+++ worlds/tunic/options.py
@@ -198,7 +198,7 @@ class LadderStorage(Choice):
     Choose whether Ladder Storage is in logic.
     Easy includes uses of Ladder Storage to get to open doors over a long distance without too much difficulty. May include convenient elevation changes (going up Mountain stairs, stairs in front of Special Shop, etc.).
     Medium includes the above as well as changing your elevation using the environment and getting knocked down by melee enemies mid-LS.
-    Hard includes the above as well as going behind the map to enter closed doors from behind. Also includes getting into the Cathedral Secret Legend room mid-LS.
+    Hard includes the above as well as going behind the map to enter closed doors from behind, shooting a fuse with the magic wand to knock yourself down at close range, and getting into the Cathedral Secret Legend room mid-LS.
     Enabling any of these difficulty options will give the player the Torch item to return to the Overworld checkpoint to avoid softlocks. Using the Torch is considered in logic.
     Opening individual chests while doing ladder storage is excluded due to tedium.
     Knocking yourself out of LS with a bomb is excluded due to the problematic nature of consumables in logic.
