edc8cef7df251a589472378d6f00c6e6ceace7e0
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-06-30 01:21:42 +0200

    Update entity_hunt.py

--- worlds/witness/entity_hunt.py
+++ worlds/witness/entity_hunt.py
@@ -192,6 +192,7 @@ class EntityHuntPicker:
             "0x03542": "0x002A6",  # Mountainside Vault Box -> Mountainside Vault Panel
             "0x03481": "0x033D4",  # Tutorial Vault Box -> Tutorial Vault Panel
             "0x0339E": "0x0CC7B",  # Desert Vault Box -> Desert Vault Panel
+            "0x03535": "0x00AFB",  # Shipwreck Vault Box -> Shipwreck Vault Panel
         }
 
         if self.player_options.shuffle_doors < 2:
