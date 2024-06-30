faa791b5b7115b809e31e51f82b1037583fb64c4
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-06-30 12:54:52 +0200

    lint

--- worlds/witness/rules.py
+++ worlds/witness/rules.py
@@ -10,7 +10,6 @@ from worlds.generic.Rules import CollectionRule, set_rule
 
 from .data import static_logic as static_witness_logic
 from .data.utils import WitnessRule
-from .locations import WitnessPlayerLocations
 from .player_logic import WitnessPlayerLogic
 
 if TYPE_CHECKING:
