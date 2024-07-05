97415553349cd663b3d2d06eb13db8bdadb442ad
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-07-05 03:06:43 +0200

    Collapse into one test :(

--- worlds/witness/test/test_ep_shuffle.py
+++ worlds/witness/test/test_ep_shuffle.py
@@ -12,39 +12,26 @@ class TestIndividualEPs(WitnessTestBase):
         "early_caves": "off",
     }
 
-    def test_eps_exist(self) -> None:
+    def test_correct_eps_exist_and_are_locked(self) -> None:
         """
-        Test that EP locations exist in shuffle_EPs by checking for the existence of Tutorial First Hallway EP
+        Test that EP locations exist in shuffle_EPs, but only the ones that actually should (based on options)
         """
+
+        # Test Tutorial First Hallways EP as a proxy for "EPs exist at all"
+        # Don't wrap in a subtest - If this fails, there is no point.
         self.assert_location_exists("Tutorial First Hallway EP")
 
-    def test_disable_non_randomized_eps(self) -> None:
-        """
-        Test that disable_non_randomized also disables EPs that are unreachable as a consequence of disabled puzzles.
-        One example is Monastery Garden Left EP, which would be unreachable due to Monastery Puzzles being disabled.
-        """
-        self.assert_location_does_not_exist("Monastery Garden Left EP")
+        with self.subTest("Test that disable_non_randomized disables Monastery Garden Left EP"):
+            self.assert_location_does_not_exist("Monastery Garden Left EP")
 
-    def test_postgame_eps(self) -> None:
-        """
-        Test that shuffle_postgame correctly excludes postgame EPs.
-        Specifically, we check for Caves Skylight EP, which is in the Caves, which is a postgame location.
-        """
-        self.assert_location_does_not_exist("Caves Skylight EP")
+        with self.subTest("Test that shuffle_postgame being off disables postgame EPs."):
+            self.assert_location_does_not_exist("Caves Skylight EP")
 
-    def test_ep_difficulty(self) -> None:
-        """
-        Test that ep_difficulty correctly excludes the EPs it's supposed to.
-        Shipwreck Couch EP always exists on tedious, but not on normal, so we test that it doesn't exist.
-        """
-        self.assert_location_does_not_exist("Shipwreck Couch EP")
+        with self.subTest("Test that ep_difficulty being set to normal excludes tedious EPs."):
+            self.assert_location_does_not_exist("Shipwreck Couch EP")
 
-    def test_obelisk_keys_work(self) -> None:
-        """
-        Test that EPs depend on their Obelisk's key by asserting that Desert Sand Snake EP needs the Desert Obelisk Key.
-        """
-
-        self.assertAccessDependency(["Desert Sand Snake EP"], [["Desert Obelisk Key"]], True)
+        with self.subTest("Test that EPs are being locked by Obelisk Keys."):
+            self.assertAccessDependency(["Desert Sand Snake EP"], [["Desert Obelisk Key"]], True)
 
 
 class TestObeliskSides(WitnessTestBase):
