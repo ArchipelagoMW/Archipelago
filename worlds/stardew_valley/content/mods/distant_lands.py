4198ca80313b9533ef69b980df2e37f528396314
Witchybun <embenham05@gmail.com>
2024-07-06 09:41:10 -0500

    Fix Distant Lands Cropsanity

--- worlds/stardew_valley/content/mods/distant_lands.py
+++ worlds/stardew_valley/content/mods/distant_lands.py
@@ -1,7 +1,14 @@
 from ..game_content import ContentPack
 from ..mod_registry import register_mod_content_pack
 from ...data import villagers_data, fish_data
+from ...data.game_item import ItemTag, Tag
+from ...data.harvest import ForagingSource, HarvestCropSource
 from ...mods.mod_data import ModNames
+from ...strings.crop_names import DistantLandsCrop
+from ...strings.forageable_names import DistantLandsForageable
+from ...strings.region_names import Region
+from ...strings.season_names import Season
+from ...strings.seed_names import DistantLandsSeed
 
 register_mod_content_pack(ContentPack(
     ModNames.distant_lands,
@@ -13,5 +20,13 @@ register_mod_content_pack(ContentPack(
     ),
     villagers=(
         villagers_data.zic,
-    )
+    ),
+    harvest_sources={
+        DistantLandsForageable.swamp_herb: (ForagingSource(regions=Region.witch_swamp),),
+        DistantLandsForageable.brown_amanita: (ForagingSource(regions=Region.witch_swamp),),
+        DistantLandsSeed.void_mint: (ForagingSource(regions=Region.witch_swamp),),
+        DistantLandsCrop.void_mint: (Tag(ItemTag.VEGETABLE), HarvestCropSource(seed=DistantLandsSeed.void_mint, seasons=(Season.spring, Season.summer, Season.fall)),),
+        DistantLandsSeed.vile_ancient_fruit: (ForagingSource(regions=Region.witch_swamp),),
+        DistantLandsCrop.vile_ancient_fruit: (Tag(ItemTag.FRUIT), HarvestCropSource(seed=DistantLandsSeed.vile_ancient_fruit, seasons=(Season.spring, Season.summer, Season.fall)),)
+    }
 ))
