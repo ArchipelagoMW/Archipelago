ae35da52cd36ae77cf322d5ea391115f6500c6e2
alwaysintreble <mmmcheese158@gmail.com>
2024-07-10 11:48:17 -0500

    reduce nesting by a level

--- Fill.py
+++ Fill.py
@@ -543,22 +543,20 @@ def distribute_items_restrictive(multiworld: MultiWorld,
         print_data = {"items": items_counter, "locations": locations_counter}
         logging.info(f"Per-Player counts: {print_data})")
         player_diffs = locations_counter - items_counter
+        for player in multiworld.player_ids:
+            player_diff = player_diffs[player]
+            if player_diff > 0:
+                logging.warning(
+                    f"Player {multiworld.get_player_name(player)} had {player_diff} more locations than items.")
+            elif player_diff < 0:
+                logging.warning(
+                    f"Player {multiworld.get_player_name(player)} had {player_diff} more items than locations.")
         if unfilled:
-            for player in multiworld.player_ids:
-                player_diff = player_diffs[player]
-                if player_diff > 0:
-                    logging.warning(
-                        f"Player {multiworld.get_player_name(player)} had {player_diff} more locations than items.")
             raise FillError(
                 f"Unable to fill all locations.\n" +
                 f"Unfilled Locations({len(unfilled)}): {unfilled}"
             )
         else:
-            for player in multiworld.player_ids:
-                player_diff = player_diffs[player]
-                if player_diff > 0:
-                    logging.warning(
-                        f"Player {multiworld.get_player_name(player)} had {player_diff} more items than locations.")
             logging.warning(
                 f"Unable to place all items.\n" +
                 f"Unplaced items({len(unplaced)}): {unplaced}"
