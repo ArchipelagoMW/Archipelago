7d53437b07400c0fd2fc3ffcc46d56dc2b652e49
Jouramie <jouramie@hotmail.com>
2024-07-09 23:17:29 -0400

    add UT test and fix bundle test

---
+++ worlds/stardew_valley/test/stability/TestUniversalTracker.py
@@ -0,0 +1,49 @@
+import unittest
+from unittest.mock import Mock
+
+from .. import SVTestBase, create_args
+from ... import STARDEW_VALLEY, options
+
+
+class TestUniversalTrackerGenerationIsStable(SVTestBase):
+    options = {
+        options.EntranceRandomization.internal_name: options.EntranceRandomization.option_buildings,
+        options.BundleRandomization.internal_name: options.BundleRandomization.option_shuffled,
+        options.FarmType.internal_name: options.FarmType.option_standard,  # Need to choose one  otherwise it's random
+    }
+
+    def test_all_locations_and_items_are_the_same_between_two_generations(self):
+        # This might open a kivy window temporarily, but it's the only way to test this...
+        if self.skip_long_tests:
+            raise unittest.SkipTest("Long tests disabled")
+
+        try:
+            from worlds.tracker.TrackerClient import TrackerGameContext  # noqa
+        except ImportError:
+            raise unittest.SkipTest("UT not loaded, skipping test")
+
+        slot_data = self.world.fill_slot_data()
+        ut_data = self.world.interpret_slot_data(slot_data)
+
+        fake_context = Mock()
+        fake_context.re_gen_passthrough = {STARDEW_VALLEY: ut_data}
+        args = create_args({0: self.options})
+        args.outputpath = None
+        args.outputname = None
+        args.multi = 1
+        args.race = None
+        args.plando_options = self.multiworld.plando_options
+        args.plando_items = self.multiworld.plando_items
+        args.plando_texts = self.multiworld.plando_texts
+        args.plando_connections = self.multiworld.plando_connections
+        args.game = self.multiworld.game
+        args.name = self.multiworld.player_name
+        args.sprite = {}
+        args.sprite_pool = {}
+        args.skip_output = True
+
+        generated_multi_world = TrackerGameContext.TMain(fake_context, args, self.multiworld.seed)
+        generated_slot_data = generated_multi_world.worlds[1].fill_slot_data()
+
+        # Just checking slot data should prove that UT generates the same result as AP generation.
+        self.assertEqual(slot_data, generated_slot_data)
