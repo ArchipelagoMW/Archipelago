1e06911b9877cdfccf3798943c07ab0a30662aa4
Ziktofel <ziktofel@gmail.com>
2024-07-13 19:10:42 +0200

    Run `/download_data` from API4 maps

--- worlds/sc2/client.py
+++ worlds/sc2/client.py
@@ -74,7 +74,7 @@ VICTORY_MODULO: int = 100
 # GitHub repo where the Map/mod data is hosted for /download_data command
 DATA_REPO_OWNER = "Ziktofel"
 DATA_REPO_NAME = "Archipelago-SC2-data"
-DATA_API_VERSION = "API3"
+DATA_API_VERSION = "API4"
 
 # Bot controller
 CONTROLLER_HEALTH: int = 38281
