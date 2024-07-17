7215bc04ed9db6422c0406737eafe3d7e9cb6a6c
Star Rauchenberger <fefferburbia@gmail.com>
2024-07-17 13:41:50 -0400

    Added unit test

--- worlds/lingo/test/TestPilgrimage.py
+++ worlds/lingo/test/TestPilgrimage.py
@@ -88,6 +88,35 @@ class TestPilgrimageNoRoofNoPaintings(LingoTestBase):
         self.assertTrue(self.can_reach_location("Pilgrim Antechamber - PILGRIM"))
 
 
+class TestPilgrimageRequireStartingRoom(LingoTestBase):
+    options = {
+        "enable_pilgrimage": "true",
+        "shuffle_colors": "false",
+        "shuffle_doors": "complex",
+        "pilgrimage_allows_roof_access": "false",
+        "pilgrimage_allows_paintings": "false",
+        "early_color_hallways": "false"
+    }
+
+    def test_access(self):
+        doors = ["Second Room - Exit Door", "Crossroads - Roof Access", "Hub Room - Crossroads Entrance",
+                 "Outside The Undeterred - Green Painting", "Outside The Undeterred - Number Hunt",
+                 "Starting Room - Street Painting", "Outside The Initiated - Shortcut to Hub Room",
+                 "Directional Gallery - Shortcut to The Undeterred", "Orange Tower First Floor - Salt Pepper Door",
+                 "Color Hunt - Shortcut to The Steady", "The Bearer - Entrance",
+                 "Orange Tower Fifth Floor - Quadruple Intersection", "The Tenacious - Shortcut to Hub Room",
+                 "Outside The Agreeable - Tenacious Entrance", "Crossroads - Tower Entrance",
+                 "Orange Tower Fourth Floor - Hot Crusts Door", "Challenge Room - Welcome Door",
+                 "Welcome Back Area - Shortcut to Starting Room", "Number Hunt - Challenge Entrance"]
+
+        for door in doors:
+            print(door)
+            self.assertFalse(self.can_reach_location("Pilgrim Antechamber - PILGRIM"))
+            self.collect_by_name(door)
+
+        self.assertTrue(self.can_reach_location("Pilgrim Antechamber - PILGRIM"))
+
+
 class TestPilgrimageYesRoofNoPaintings(LingoTestBase):
     options = {
         "enable_pilgrimage": "true",
