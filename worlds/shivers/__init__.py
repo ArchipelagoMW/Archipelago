049805f2d1df30e86155c82de387f757462349f5
GodlFire <46984098+GodlFire@users.noreply.github.com>
2024-07-21 11:34:23 -0600

    Add missing .value on slot data
    Add missing .value on slot data

--- worlds/shivers/__init__.py
+++ worlds/shivers/__init__.py
@@ -219,7 +219,7 @@ class ShiversWorld(World):
             "ElevatorsStaySolved": self.options.elevators_stay_solved.value,
             "EarlyBeth": self.options.early_beth.value,
             "EarlyLightning": self.options.early_lightning.value,
-            "FrontDoorUsable": self.options.front_door_usable
+            "FrontDoorUsable": self.options.front_door_usable.value
         }
 
 
