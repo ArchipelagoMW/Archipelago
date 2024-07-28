8679ff2b9e13b33cf330810d5db28b74b632d9d8
Scipio Wright <scipiowright@gmail.com>
2024-07-28 12:47:35 -0400

    Fix bug in seed groups with fixed shop on and off

--- worlds/tunic/er_scripts.py
+++ worlds/tunic/er_scripts.py
@@ -26,8 +26,13 @@ def create_er_regions(world: "TunicWorld") -> Dict[Portal, Portal]:
     if world.options.entrance_rando:
         for region_name, region_data in tunic_er_regions.items():
             # if fewer shops is off, zig skip is not made
-            if region_name == "Zig Skip Exit" and not world.options.fixed_shop:
-                continue
+            if region_name == "Zig Skip Exit":
+                # need to check if there's a seed group for this first
+                if world.options.entrance_rando.value not in EntranceRando.options.values():
+                    if not world.seed_groups[world.options.entrance_rando.value]["fixed_shop"]:
+                        continue
+                elif not world.options.fixed_shop:
+                    continue
             regions[region_name] = Region(region_name, world.player, world.multiworld)
 
         portal_pairs = pair_portals(world, regions)
