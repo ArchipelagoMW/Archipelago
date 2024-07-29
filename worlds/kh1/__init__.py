c873d3a383b85504b81c293418e31f072be0c236
gaithern <36639398+gaithern@users.noreply.github.com>
2024-07-29 13:23:05 -0500

    Update worlds/kh1/__init__.py
    Co-authored-by: Exempt-Medic <60412657+Exempt-Medic@users.noreply.github.com>

--- worlds/kh1/__init__.py
+++ worlds/kh1/__init__.py
@@ -91,7 +91,7 @@ class KH1World(World):
 
             level_up_locations = list(get_locations_by_category("Levels").keys())
             self.random.shuffle(level_up_item_pool)
-            starting_level_for_stats_only = self.options.force_stats_on_levels - 1
+            starting_level_for_stats_only = self.options.force_stats_on_levels.value - 1
             while len(level_up_item_pool) > 0 and starting_level_for_stats_only < self.options.level_checks:
                 self.multiworld.get_location(level_up_locations[starting_level_for_stats_only], self.player).place_locked_item(self.create_item(level_up_item_pool.pop()))
                 starting_level_for_stats_only += 1
