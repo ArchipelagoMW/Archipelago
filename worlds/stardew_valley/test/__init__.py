3ae94355f6a0c9ac9db60d0f680276425d2aa9b0
Alex Gilbert <alexgilbert@yahoo.com>
2024-08-05 17:06:39 -0400

    - Use the compound source of the monster compoundium (ironic, I know)

--- worlds/stardew_valley/test/__init__.py
+++ worlds/stardew_valley/test/__init__.py
@@ -256,10 +256,10 @@ class SVTestBase(RuleAssertMixin, WorldTestBase, SVTestCase):
             return False
         return super().run_default_tests
 
-    def collect_lots_of_money(self):
+    def collect_lots_of_money(self, percent: float = 0.25):
         self.multiworld.state.collect(self.world.create_item("Shipping Bin"), prevent_sweep=False)
         real_total_prog_items = self.multiworld.worlds[self.player].total_progression_items
-        required_prog_items = int(round(real_total_prog_items * 0.25))
+        required_prog_items = int(round(real_total_prog_items * percent))
         for i in range(required_prog_items):
             self.multiworld.state.collect(self.world.create_item("Stardrop"), prevent_sweep=False)
         self.multiworld.worlds[self.player].total_progression_items = real_total_prog_items
