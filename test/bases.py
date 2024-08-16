1af72a897e124f3d7ff32541c452ff829f651ba0
Exempt-Medic <60412657+Exempt-Medic@users.noreply.github.com>
2024-08-16 14:49:37 -0400

    Core: Require excluded locations to be reachable with full/locations accessibility (#3802)
    * Make excludeds reachable
    
    * Update all_state tests

--- test/bases.py
+++ test/bases.py
@@ -293,13 +293,11 @@ class WorldTestBase(unittest.TestCase):
         if not (self.run_default_tests and self.constructed):
             return
         with self.subTest("Game", game=self.game, seed=self.multiworld.seed):
-            excluded = self.multiworld.worlds[self.player].options.exclude_locations.value
             state = self.multiworld.get_all_state(False)
             for location in self.multiworld.get_locations():
-                if location.name not in excluded:
-                    with self.subTest("Location should be reached", location=location.name):
-                        reachable = location.can_reach(state)
-                        self.assertTrue(reachable, f"{location.name} unreachable")
+                with self.subTest("Location should be reached", location=location.name):
+                    reachable = location.can_reach(state)
+                    self.assertTrue(reachable, f"{location.name} unreachable")
             with self.subTest("Beatable"):
                 self.multiworld.state = state
                 self.assertBeatable(True)
