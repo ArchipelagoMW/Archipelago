b4bc49b01e609cc7ad1ea934a8903697541c3ff8
digiholic <digikun@gmail.com>
2024-08-16 14:10:30 -0600

    [OSRS] Fixes Incorrect filler item names causing failures on tests. (#3768)
    * Updates filler item names to match the actual item names
    
    * Adds more descriptive error message in case this error comes back
    
    * Properly raises exception instead of just text
    
    * Replaces exception with assert

--- worlds/osrs/__init__.py
+++ worlds/osrs/__init__.py
@@ -524,7 +524,9 @@ class OSRSWorld(World):
         return region
 
     def create_item(self, item_name: str) -> "Item":
-        item = [item for item in item_rows if item.name == item_name][0]
+        items = [item for item in item_rows if item.name == item_name]
+        assert len(items) > 0, f"No matching item found for name {item_name} for player {self.player_name}"
+        item = items[0]
         index = item_rows.index(item)
         return OSRSItem(item.name, item.progression, self.base_id + index, self.player)
 
