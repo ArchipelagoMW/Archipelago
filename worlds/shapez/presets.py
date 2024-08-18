feb1ec66599293cf2e0a4f71a5634b91e95f765d
Slimey <blastslimeyt@gmail.com>
2024-08-18 02:32:40 +0200

    More presets

--- worlds/shapez/presets.py
+++ worlds/shapez/presets.py
@@ -10,6 +10,24 @@ options_presets = {
         # "additional_locations": True,
         # "exclude_softlock_achievements": False,
         # "exclude_long_playtime_achievements": False,
-        "shapesanity_amount": 2144
+        "shapesanity_amount": 5664
+    },
+    "Restrictive start": {
+        "goal": 0,
+        "randomize_level_requirements": True,
+        "randomize_upgrade_requirements": True,
+        "randomize_level_logic": 4,
+        "randomize_upgrade_logic": 2,
+        "early_balancer_tunnel_and_trash": 3,
+        "shapesanity_amount": 4
+    },
+    "Quick game": {
+        "goal": 3,
+        "required_shapes_multiplier": 1,
+        "randomize_level_requirements": True,
+        "randomize_upgrade_requirements": True,
+        "randomize_level_logic": 4,
+        "randomize_upgrade_logic": 2,
+        "shapesanity_amount": 4
     }
 }
