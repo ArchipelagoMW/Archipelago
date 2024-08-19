d5c2338c531fc03a608d276d1cbd1b112ff318ae
Doug Hoskisson <beauxq@users.noreply.github.com>
2024-08-19 11:37:36 -0700

    Docs: `NetworkItem.player` (#3811)
    * Docs: `NetworkItem.player`
    
    In many contexts, it's difficult to tell whether this is the sending player or the receiving player.
    
    * correct player info
    
    * Update NetUtils.py
    
    Co-authored-by: Aaron Wagener <mmmcheese158@gmail.com>
    
    ---------
    
    Co-authored-by: Aaron Wagener <mmmcheese158@gmail.com>

--- NetUtils.py
+++ NetUtils.py
@@ -79,6 +79,7 @@ class NetworkItem(typing.NamedTuple):
     item: int
     location: int
     player: int
+    """ Sending player, except in LocationInfo (from LocationScouts), where it is the receiving player. """
     flags: int = 0
 
 
