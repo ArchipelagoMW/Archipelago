0f5a4820303531ea107cb48d0fcd60ba5b7d6c04
Salzkorn <salzkitty@gmail.com>
2024-08-24 00:14:19 +0200

    Add unit test for locked/start inventory + item rule

--- worlds/sc2/test/test_custom_mission_orders.py
+++ worlds/sc2/test/test_custom_mission_orders.py
@@ -4,9 +4,11 @@ Unit tests for custom mission orders
 
 from .test_base import Sc2SetupTestBase
 from .. import MissionFlag
+from .. import item_names
+from ....BaseClasses import ItemClassification
 
 class TestCustomMissionOrders(Sc2SetupTestBase):
-    
+   
    def test_mini_wol_generates(self):
       world_options = {
          'mission_order': 'custom',
@@ -101,3 +103,86 @@ class TestCustomMissionOrders(Sc2SetupTestBase):
       self.assertEqual(flags.get(MissionFlag.Zerg, 0), 0)
       sc2_regions = set(self.multiworld.regions.region_cache[self.player]) - {"Menu"}
       self.assertEqual(len(self.world.custom_mission_order.get_used_missions()), len(sc2_regions))
+
+   def test_locked_and_necessary_item_appears_once(self):
+      # This is a filler upgrade with a parent
+      test_item = item_names.ZERGLING_METABOLIC_BOOST
+      world_options = {
+         'mission_order': 'custom',
+         'locked_items': { test_item: 1 },
+         'custom_mission_order': {
+            'test': {
+               'type': 'column',
+               'size': 5, # Give the generator some space to place the key
+               'max_difficulty': 'easy',
+               'missions': [{
+                  'index': 4,
+                  'entry_rules': [{
+                     'items': { test_item: 1 }
+                  }]
+               }]
+            }
+         }
+      }
+
+      self.generate_world(world_options)
+      test_items_in_pool = [item for item in self.multiworld.itempool if item.name == test_item]
+      self.assertEqual(len(test_items_in_pool), 1)
+      self.assertEqual(test_items_in_pool[0].classification == ItemClassification.progression)
+
+   def test_start_inventory_and_necessary_item_appears_once(self):
+      # This is a filler upgrade with a parent
+      test_item = item_names.ZERGLING_METABOLIC_BOOST
+      world_options = {
+         'mission_order': 'custom',
+         'start_inventory': { test_item: 1 },
+         'custom_mission_order': {
+            'test': {
+               'type': 'column',
+               'size': 5, # Give the generator some space to place the key
+               'max_difficulty': 'easy',
+               'missions': [{
+                  'index': 4,
+                  'entry_rules': [{
+                     'items': { test_item: 1 }
+                  }]
+               }]
+            }
+         }
+      }
+
+      self.generate_world(world_options)
+      test_items_in_pool = [item for item in self.multiworld.itempool if item.name == test_item]
+      self.assertEqual(len(test_items_in_pool), 0)
+      test_items_in_start_inventory = [item for item in self.multiworld.precollected_items[self.player] if item.name == test_item]
+      self.assertEqual(len(test_items_in_start_inventory), 1)
+      self.assertEqual(test_items_in_start_inventory[0].classification == ItemClassification.progression)
+
+   def test_start_inventory_and_locked_and_necessary_item_appears_once(self):
+      # This is a filler upgrade with a parent
+      test_item = item_names.ZERGLING_METABOLIC_BOOST
+      world_options = {
+         'mission_order': 'custom',
+         'start_inventory': { test_item: 1 },
+         'locked_items': { test_item: 1 },
+         'custom_mission_order': {
+            'test': {
+               'type': 'column',
+               'size': 5, # Give the generator some space to place the key
+               'max_difficulty': 'easy',
+               'missions': [{
+                  'index': 4,
+                  'entry_rules': [{
+                     'items': { test_item: 1 }
+                  }]
+               }]
+            }
+         }
+      }
+
+      self.generate_world(world_options)
+      test_items_in_pool = [item for item in self.multiworld.itempool if item.name == test_item]
+      self.assertEqual(len(test_items_in_pool), 0)
+      test_items_in_start_inventory = [item for item in self.multiworld.precollected_items[self.player] if item.name == test_item]
+      self.assertEqual(len(test_items_in_start_inventory), 1)
+      self.assertEqual(test_items_in_start_inventory[0].classification == ItemClassification.progression)
