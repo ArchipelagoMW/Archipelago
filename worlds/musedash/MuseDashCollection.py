f21cd88760a496f458795e050a80f680acff6b29
Justus Lind <DeamonHunter@users.noreply.github.com>
2024-08-23 19:44:01 +1000

    Add the replaced song to the removed list.

--- worlds/musedash/MuseDashCollection.py
+++ worlds/musedash/MuseDashCollection.py
@@ -46,6 +46,7 @@ class MuseDashCollections:
         "CHAOS Glitch",
         "FM 17314 SUGAR RADIO",
         "Yume Ou Mono Yo Secret",
+        "Tsukuyomi Ni Naru Replaced"
     ]
 
     album_items: Dict[str, AlbumData] = {}
