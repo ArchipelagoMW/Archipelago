65cad26e51377197fd674b805d5a9063b5cf67ea
Silvris <58583688+Silvris@users.noreply.github.com>
2024-08-28 01:08:13 -0500

    lots

--- worlds/options_select/client.py
+++ worlds/options_select/client.py
@@ -2,13 +2,18 @@ from kvui import (App, ScrollBox, Button, MainLayout, ContainerLayout, dp, Widge
                   Label)
 from kivy.uix.slider import Slider
 from kivy.uix.dropdown import DropDown
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.textinput import TextInput
+from kivy.uix.togglebutton import ToggleButton
+from kivy.metrics import sp
+from textwrap import dedent
 import Utils
 import typing
 import webbrowser
 from urllib.parse import urlparse
 from worlds.AutoWorld import AutoWorldRegister, World
 from Options import (Option, Toggle, TextChoice, Choice, FreeText, NamedRange, Range, OptionSet, OptionList, OptionDict,
-                     Removed, Visibility)
+                     Removed, Visibility, VerifyKeys, PlandoTexts, PlandoConnections, ItemLinks)
 
 
 def validate_url(x):
@@ -19,11 +24,27 @@ def validate_url(x):
         return False
 
 
+def filter_tooltip(tooltip):
+    return dedent(tooltip).replace("\n", "<br>").replace("&", "&amp;").replace("[", "&bl;").replace("]", "&br;")
+
+
+def option_can_be_randomized(option: typing.Type[Option]):
+    # most options can be randomized, so we should just check for those that cannot
+    if any(issubclass(option, option_type) for option_type in (VerifyKeys, Removed, PlandoTexts,
+                                                               PlandoConnections, ItemLinks)):
+        return False
+    elif issubclass(option, FreeText) and not issubclass(option, TextChoice):
+        return False
+    return True
+
 class YamlCreator(App):
     container: ContainerLayout
     main_layout: MainLayout
     scrollbox: ScrollBox
+    main_panel: MainLayout
+    player_options: MainLayout
     option_layout: MainLayout
+    name_input: TextInput
     options: typing.Dict[str, typing.Any]
 
     def create_range(self, option: typing.Type[Range], name: str):
@@ -101,22 +122,71 @@ class YamlCreator(App):
         return main_button
 
     def create_text_choice(self, option: typing.Type[TextChoice], name: str):
-        pass
+        choice_button = self.create_choice(option, name)
+        text = TextInput(multiline=False, font_size=sp(11))
+        box = BoxLayout(orientation="vertical")
+        box.add_widget(choice_button)
+        box.add_widget(text)
+
+        def set_value(instance):
+            setattr(choice_button, "text", "Custom")
+            self.options[name] = instance.text
+
+        text.bind(on_text_validate=set_value)
+        return box
+
+    def create_toggle(self, option: typing.Type[Toggle], name: str) -> Widget:
+        def set_value(instance, value):
+            self.options[name] = bool(value)
+        checkbox = CheckBox(active=option.default)
+        checkbox.bind(active=set_value)
+        self.options[name]=bool(option.default)
+        return checkbox
+
 
     def create_option(self, option: typing.Type[Option], name: str) -> Widget:
         option_base = BoxLayout(orientation="vertical", size_hint_y=None)
-        tooltip = option.__doc__.replace("\n    ", " ").replace("\n", "")
+        tooltip = filter_tooltip(option.__doc__)
         option_label = TooltipLabel(text=f"[ref=0|{tooltip}]{getattr(option, 'display_name', name)}",
                                     size_hint_y=None)
-        option_base.add_widget(option_label)
+        label_box = BoxLayout(orientation="horizontal")
+        label_box.add_widget(option_label)
+        if option_can_be_randomized(option):
+            def randomize_option(instance: Widget, value: str):
+                value = value == "down"
+                if value:
+                    self.options[name] = "random-" + str(self.options[name])
+                else:
+                    self.options[name] = self.options[name].replace("random-", "")
+                    if self.options[name].isnumeric() or self.options[name] in ("True", "False"):
+                        self.options[name] = eval(self.options[name])
+
+                def recursive_disable(inst: Widget, val: bool):
+                    inst.disabled = val
+                    for child in inst.children:
+                        recursive_disable(child, val)
+
+                base_object = instance.parent.parent
+                label_object = instance.parent
+                for child in base_object.children:
+                    if child is not label_object:
+                        recursive_disable(child, value)
+
+            random_toggle = ToggleButton(size_hint_x=None, width=100, text="Random?")
+            random_toggle.bind(state=randomize_option)
+            label_box.add_widget(random_toggle)
+
+        option_base.add_widget(label_box)
         if issubclass(option, NamedRange):
             option_base.add_widget(self.create_named_range(option, name))
         elif issubclass(option, Range):
             option_base.add_widget(self.create_range(option, name))
         elif issubclass(option, TextChoice):
-            pass
+            option_base.add_widget(self.create_text_choice(option, name))
         elif issubclass(option, Choice):
             option_base.add_widget(self.create_choice(option, name))
+        elif issubclass(option, Toggle):
+            label_box.add_widget(self.create_toggle(option, name), index=1)
         return option_base
 
     def create_options_panel(self, world_button):
@@ -158,12 +228,22 @@ class YamlCreator(App):
             world_button.bind(on_release=self.create_options_panel)
             world_button.world_cls = cls
             self.scrollbox.layout.add_widget(world_button)
+        self.main_panel = MainLayout(rows=2)
+        self.player_options = BoxLayout(orientation="horizontal", size_hint_y=None, height=dp(60))
+        name_box = BoxLayout(orientation="vertical", size_hint_y=None)
+        name_box.add_widget(Label(text="Player Name"))
+        self.name_input = TextInput(multiline=False)
+        name_box.add_widget(self.name_input)
+        self.player_options.add_widget(name_box)
+        export_button = Button(text="Export Options")
+        self.player_options.add_widget(export_button)
         self.option_layout = MainLayout(cols=1)
+        self.main_panel.add_widget(self.player_options)
+        self.main_panel.add_widget(self.option_layout)
         self.main_layout.add_widget(self.scrollbox)
-        self.main_layout.add_widget(self.option_layout)
+        self.main_layout.add_widget(self.main_panel)
 
         return self.container
-
 
 def launch():
     YamlCreator().run()
