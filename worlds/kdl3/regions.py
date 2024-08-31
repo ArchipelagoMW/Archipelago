91f29bbf442f07b39ed11713761ad7b946f6976d
Silvris <58583688+Silvris@users.noreply.github.com>
2024-08-31 06:15:00 -0500

    KDL3: Version 2.0.0 (#3323)
    * initial work on procedure patch
    
    * more flexibility
    
    load default procedure for version 5 patches
    add args for procedure
    add default extension for tokens and bsdiff
    allow specifying additional required extensions for generation
    
    * pushing current changes to go fix tloz bug
    
    * move tokens into a separate inheritable class
    
    * forgot the commit to remove token from ProcedurePatch
    
    * further cleaning from bad commit
    
    * start on docstrings
    
    * further work on docstrings and typing
    
    * improve docstrings
    
    * fix incorrect docstring
    
    * cleanup
    
    * clean defaults and docstring
    
    * define interface that has only the bare minimum required
    for `Patch.create_rom_file`
    
    * change to dictionary.get
    
    * remove unnecessary if statement
    
    * update to explicitly check for procedure, restore compatible version and manual override
    
    * Update Files.py
    
    * remove struct uses
    
    * Update Rom.py
    
    * convert KDL3 to APPP
    
    * change class variables to instance variables
    
    * Update worlds/Files.py
    
    Co-authored-by: black-sliver <59490463+black-sliver@users.noreply.github.com>
    
    * Update worlds/Files.py
    
    Co-authored-by: black-sliver <59490463+black-sliver@users.noreply.github.com>
    
    * move required_extensions to tuple
    
    * fix missing tuple ellipsis
    
    * fix classvar mixup
    
    * rename tokens to _tokens. use hasattr
    
    * type hint cleanup
    
    * Update Files.py
    
    * initial base for local items, need to finish
    
    * coo not clean
    
    * handle local items for real, appp cleanup
    
    * actually make bosses send their locations
    
    * fix cloudy park 4 rule, zero deathlink message
    
    * remove redundant door_shuffle bool
    
    when generic ER gets in, this whole function gets rewritten. So just clean it a little now.
    
    * properly fix deathlink messages, fix fill error
    
    * update docs
    
    * add prefill items
    
    * fix kine fill error
    
    * Update Rom.py
    
    * Update Files.py
    
    * mypy and softlock fix
    
    * Update Gifting.py
    
    * mypy phase 1
    
    * fix rare async client bug
    
    * Update __init__.py
    
    * typing cleanup
    
    * fix stone softlock
    
    because of the way Kine's Stone works, you can't clear the stone blocks before clearing the burning blocks, so we have to bring Burning from outside
    
    * Update Rom.py
    
    * Add option groups
    
    * Rename to lowercase
    
    * finish rename
    
    * whoops broke the world
    
    * fix animal duplication bug
    
    * overhaul filler generation
    
    * add Miku flavor
    
    * Update gifting.py
    
    * fix issues related to max_hs increase
    
    * Update test_locations.py
    
    * fix boss shuffle not working if level shuffle is disabled
    
    * fix bleeding default levels
    
    * Update options.py
    
    * thought this would print seed
    
    * yay bad merges
    
    * forgot options too
    
    * yeah lets just break generation while at it
    
    * this is probably a problem
    
    * cap required heart stars
    
    * Revert "cap required heart stars"
    
    This reverts commit 759efd3e2b14ec2855082de041ac989cb9c5d500.
    
    * fix duplication removal placement, deprecated test option
    
    * forgot that we need to account for what we place
    
    * move location ids
    
    * rewrite trap handling
    
    * further stage renumber fixes
    
    * forgot one more
    
    * basic UT support
    
    * fix local heart star checks
    
    * fix pattern
    
    ---------
    
    Co-authored-by: beauxq <beauxq@yahoo.com>
    Co-authored-by: black-sliver <59490463+black-sliver@users.noreply.github.com>
    Co-authored-by: NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>

--- worlds/kdl3/regions.py
+++ worlds/kdl3/regions.py
@@ -1,60 +1,62 @@
 import orjson
 import os
 from pkgutil import get_data
+from copy import deepcopy
 
-from typing import TYPE_CHECKING, List, Dict, Optional, Union
-from BaseClasses import Region
+from typing import TYPE_CHECKING, List, Dict, Optional, Union, Callable
+from BaseClasses import Region, CollectionState
 from worlds.generic.Rules import add_item_rule
-from .Locations import KDL3Location
-from .Names import LocationName
-from .Options import BossShuffle
-from .Room import KDL3Room
+from .locations import KDL3Location
+from .names import location_name
+from .options import BossShuffle
+from .room import KDL3Room
 
 if TYPE_CHECKING:
     from . import KDL3World
 
 default_levels = {
-    1: [0x770001, 0x770002, 0x770003, 0x770004, 0x770005, 0x770006, 0x770200],
-    2: [0x770007, 0x770008, 0x770009, 0x77000A, 0x77000B, 0x77000C, 0x770201],
-    3: [0x77000D, 0x77000E, 0x77000F, 0x770010, 0x770011, 0x770012, 0x770202],
-    4: [0x770013, 0x770014, 0x770015, 0x770016, 0x770017, 0x770018, 0x770203],
-    5: [0x770019, 0x77001A, 0x77001B, 0x77001C, 0x77001D, 0x77001E, 0x770204],
+    1: [0x770000, 0x770001, 0x770002, 0x770003, 0x770004, 0x770005, 0x770200],
+    2: [0x770006, 0x770007, 0x770008, 0x770009, 0x77000A, 0x77000B, 0x770201],
+    3: [0x77000C, 0x77000D, 0x77000E, 0x77000F, 0x770010, 0x770011, 0x770202],
+    4: [0x770012, 0x770013, 0x770014, 0x770015, 0x770016, 0x770017, 0x770203],
+    5: [0x770018, 0x770019, 0x77001A, 0x77001B, 0x77001C, 0x77001D, 0x770204],
 }
 
 first_stage_blacklist = {
     # We want to confirm that the first stage can be completed without any items
-    0x77000B,  # 2-5 needs Kine
-    0x770011,  # 3-5 needs Cutter
-    0x77001C,  # 5-4 needs Burning
+    0x77000A,  # 2-5 needs Kine
+    0x770010,  # 3-5 needs Cutter
+    0x77001B,  # 5-4 needs Burning
 }
 
 first_world_limit = {
     # We need to limit the number of very restrictive stages in level 1 on solo gens
     *first_stage_blacklist,  # all three of the blacklist stages need 2+ items for both checks
+    0x770006,
     0x770007,
-    0x770008,
-    0x770013,
-    0x77001E,
+    0x770012,
+    0x77001D,
 
 }
 
 
 def generate_valid_level(world: "KDL3World", level: int, stage: int,
-                         possible_stages: List[int], placed_stages: List[int]):
+                         possible_stages: List[int], placed_stages: List[Optional[int]]) -> int:
     new_stage = world.random.choice(possible_stages)
     if level == 1:
         if stage == 0 and new_stage in first_stage_blacklist:
+            possible_stages.remove(new_stage)
             return generate_valid_level(world, level, stage, possible_stages, placed_stages)
         elif (not (world.multiworld.players > 1 or world.options.consumables or world.options.starsanity) and
-                new_stage in first_world_limit and
-                sum(p_stage in first_world_limit for p_stage in placed_stages)
+              new_stage in first_world_limit and
+              sum(p_stage in first_world_limit for p_stage in placed_stages)
               >= (2 if world.options.open_world else 1)):
             return generate_valid_level(world, level, stage, possible_stages, placed_stages)
     return new_stage
 
 
-def generate_rooms(world: "KDL3World", level_regions: Dict[int, Region]):
-    level_names = {LocationName.level_names[level]: level for level in LocationName.level_names}
+def generate_rooms(world: "KDL3World", level_regions: Dict[int, Region]) -> None:
+    level_names = {location_name.level_names[level]: level for level in location_name.level_names}
     room_data = orjson.loads(get_data(__name__, os.path.join("data", "Rooms.json")))
     rooms: Dict[str, KDL3Room] = dict()
     for room_entry in room_data:
@@ -63,7 +65,7 @@ def generate_rooms(world: "KDL3World", level_regions: Dict[int, Region]):
                         room_entry["default_exits"], room_entry["animal_pointers"], room_entry["enemies"],
                         room_entry["entity_load"], room_entry["consumables"], room_entry["consumables_pointer"])
         room.add_locations({location: world.location_name_to_id[location] if location in world.location_name_to_id else
-                            None for location in room_entry["locations"]
+        None for location in room_entry["locations"]
                             if (not any(x in location for x in ["1-Up", "Maxim"]) or
                                 world.options.consumables.value) and ("Star" not in location
                                                                       or world.options.starsanity.value)},
@@ -83,8 +85,8 @@ def generate_rooms(world: "KDL3World", level_regions: Dict[int, Region]):
             if room.stage == 7:
                 first_rooms[0x770200 + room.level - 1] = room
             else:
-                first_rooms[0x770000 + ((room.level - 1) * 6) + room.stage] = room
-        exits = dict()
+                first_rooms[0x770000 + ((room.level - 1) * 6) + room.stage - 1] = room
+        exits: Dict[str, Callable[[CollectionState], bool]] = dict()
         for def_exit in room.default_exits:
             target = f"{level_names[room.level]} {room.stage} - {def_exit['room']}"
             access_rule = tuple(def_exit["access_rule"])
@@ -115,50 +117,54 @@ def generate_rooms(world: "KDL3World", level_regions: Dict[int, Region]):
         if world.options.open_world:
             level_regions[level].add_exits([first_rooms[0x770200 + level - 1].name])
         else:
-            world.multiworld.get_location(world.location_id_to_name[world.player_levels[level][5]], world.player)\
+            world.multiworld.get_location(world.location_id_to_name[world.player_levels[level][5]], world.player) \
                 .parent_region.add_exits([first_rooms[0x770200 + level - 1].name])
 
 
-def generate_valid_levels(world: "KDL3World", enforce_world: bool, enforce_pattern: bool) -> dict:
-    levels: Dict[int, List[Optional[int]]] = {
-        1: [None] * 7,
-        2: [None] * 7,
-        3: [None] * 7,
-        4: [None] * 7,
-        5: [None] * 7,
-    }
+def generate_valid_levels(world: "KDL3World", shuffle_mode: int) -> Dict[int, List[int]]:
+    if shuffle_mode:
+        levels: Dict[int, List[Optional[int]]] = {
+            1: [None] * 7,
+            2: [None] * 7,
+            3: [None] * 7,
+            4: [None] * 7,
+            5: [None] * 7,
+        }
 
-    possible_stages = [default_levels[level][stage] for level in default_levels for stage in range(6)]
-    if world.options.plando_connections:
-        for connection in world.options.plando_connections:
-            try:
-                entrance_world, entrance_stage = connection.entrance.rsplit(" ", 1)
-                stage_world, stage_stage = connection.exit.rsplit(" ", 1)
-                new_stage = default_levels[LocationName.level_names[stage_world.strip()]][int(stage_stage) - 1]
-                levels[LocationName.level_names[entrance_world.strip()]][int(entrance_stage) - 1] = new_stage
-                possible_stages.remove(new_stage)
+        possible_stages = [default_levels[level][stage] for level in default_levels for stage in range(6)]
+        if world.options.plando_connections:
+            for connection in world.options.plando_connections:
+                try:
+                    entrance_world, entrance_stage = connection.entrance.rsplit(" ", 1)
+                    stage_world, stage_stage = connection.exit.rsplit(" ", 1)
+                    new_stage = default_levels[location_name.level_names[stage_world.strip()]][int(stage_stage) - 1]
+                    levels[location_name.level_names[entrance_world.strip()]][int(entrance_stage) - 1] = new_stage
+                    possible_stages.remove(new_stage)
 
-            except Exception:
-                raise Exception(
-                    f"Invalid connection: {connection.entrance} =>"
-                    f" {connection.exit} for player {world.player} ({world.player_name})")
+                except Exception:
+                    raise Exception(
+                        f"Invalid connection: {connection.entrance} =>"
+                        f" {connection.exit} for player {world.player} ({world.player_name})")
 
-    for level in range(1, 6):
-        for stage in range(6):
-            # Randomize bosses separately
-            try:
+        for level in range(1, 6):
+            for stage in range(6):
+                # Randomize bosses separately
                 if levels[level][stage] is None:
                     stage_candidates = [candidate for candidate in possible_stages
-                                        if (enforce_world and candidate in default_levels[level])
-                                        or (enforce_pattern and ((candidate - 1) & 0x00FFFF) % 6 == stage)
-                                        or (enforce_pattern == enforce_world)
+                                        if (shuffle_mode == 1 and candidate in default_levels[level])
+                                        or (shuffle_mode == 2 and (candidate & 0x00FFFF) % 6 == stage)
+                                        or (shuffle_mode == 3)
                                         ]
+                    if not stage_candidates:
+                        raise Exception(
+                            f"Failed to find valid stage for {level}-{stage}. Remaining Stages:{possible_stages}")
                     new_stage = generate_valid_level(world, level, stage, stage_candidates, levels[level])
                     possible_stages.remove(new_stage)
                     levels[level][stage] = new_stage
-            except Exception:
-                raise Exception(f"Failed to find valid stage for {level}-{stage}. Remaining Stages:{possible_stages}")
-
+    else:
+        levels = deepcopy(default_levels)
+        for level in levels:
+            levels[level][6] = None
     # now handle bosses
     boss_shuffle: Union[int, str] = world.options.boss_shuffle.value
     plando_bosses = []
@@ -168,17 +174,17 @@ def generate_valid_levels(world: "KDL3World", enforce_world: bool, enforce_patte
         boss_shuffle = BossShuffle.options[options.pop()]
         for option in options:
             if "-" in option:
-                loc, boss = option.split("-")
+                loc, plando_boss = option.split("-")
                 loc = loc.title()
-                boss = boss.title()
-                levels[LocationName.level_names[loc]][6] = LocationName.boss_names[boss]
-                plando_bosses.append(LocationName.boss_names[boss])
+                plando_boss = plando_boss.title()
+                levels[location_name.level_names[loc]][6] = location_name.boss_names[plando_boss]
+                plando_bosses.append(location_name.boss_names[plando_boss])
             else:
                 option = option.title()
                 for level in levels:
                     if levels[level][6] is None:
-                        levels[level][6] = LocationName.boss_names[option]
-                        plando_bosses.append(LocationName.boss_names[option])
+                        levels[level][6] = location_name.boss_names[option]
+                        plando_bosses.append(location_name.boss_names[option])
 
     if boss_shuffle > 0:
         if boss_shuffle == BossShuffle.option_full:
@@ -223,15 +229,14 @@ def create_levels(world: "KDL3World") -> None:
         5: level5,
     }
     level_shuffle = world.options.stage_shuffle.value
-    if level_shuffle != 0:
-        world.player_levels = generate_valid_levels(
-            world,
-            level_shuffle == 1,
-            level_shuffle == 2)
+    if hasattr(world.multiworld, "re_gen_passthrough"):
+        world.player_levels = getattr(world.multiworld, "re_gen_passthrough")["Kirby's Dream Land 3"]["player_levels"]
+    else:
+        world.player_levels = generate_valid_levels(world, level_shuffle)
 
     generate_rooms(world, levels)
 
-    level6.add_locations({LocationName.goals[world.options.goal]: None}, KDL3Location)
+    level6.add_locations({location_name.goals[world.options.goal.value]: None}, KDL3Location)
 
     menu.connect(level1, "Start Game")
     level1.connect(level2, "To Level 2")
