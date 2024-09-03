b0a76e6f0d6327bbca6d18b2d08ed154402dc176
Philiard <philoffalot@gmail.com>
2024-09-02 22:29:01 -0400

    Add dll

--- worlds/sly1/Client/obj/Debug/net8.0/Sly1AP.AssemblyInfo.cs
+++ worlds/sly1/Client/obj/Debug/net8.0/Sly1AP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sly1AP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a48ab283ec1885e2918c0fa109c8ad2ce695f9f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce5f2afea3c93ea5939f5ebd3b624ffa7e1a3582")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sly1AP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sly1AP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
