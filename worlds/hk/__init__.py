c399fb65e217d3e22b1a8d59e872ba13ed03a7c4
qwint <qwint.42@gmail.com>
2024-09-05 09:28:06 -0500

    speed up collect, will be obsolete after #3786

--- worlds/hk/__init__.py
+++ worlds/hk/__init__.py
@@ -601,7 +601,7 @@ class HKWorld(World):
         if change:
             for effect_name, effect_value in item_effects.get(item.name, {}).items():
                 state.prog_items[item.player][effect_name] += effect_value
-        if item.name in {"Left_Mothwing_Cloak", "Right_Mothwing_Cloak"}:
+        if change and item.name in {"Left_Mothwing_Cloak", "Right_Mothwing_Cloak"}:
             if state.prog_items[item.player].get('RIGHTDASH', 0) and \
                     state.prog_items[item.player].get('LEFTDASH', 0):
                 (state.prog_items[item.player]["RIGHTDASH"], state.prog_items[item.player]["LEFTDASH"]) = \
