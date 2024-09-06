6196c8592a655a3252ac345831085462557c1e3f
Silvris <58583688+Silvris@users.noreply.github.com>
2024-09-06 18:55:07 -0500

    fix emerald and incorrect kh2

--- worlds/kh2/__init__.py
+++ worlds/kh2/__init__.py
@@ -420,7 +420,7 @@ class KH2World(World):
         Fills keyblade slots with abilities determined on player's setting
         """
         keyblade_locations = [self.multiworld.get_location(location, self.player) for location in Keyblade_Slots.keys()]
-        state = self.multiworld.get_all_state(False, True)
+        state = self.multiworld.get_all_state(False, False)
         keyblade_ability_pool_copy = self.keyblade_ability_pool.copy()
         fill_restrictive(self.multiworld, state, keyblade_locations, keyblade_ability_pool_copy, True, True, allow_excluded=True)
 
