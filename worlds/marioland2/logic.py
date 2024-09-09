c1504c795e3b342f52d27db18cdeb72c20c7a1fa
alchav <alchav@jalchavware.com>
2024-09-08 22:40:53 -0400

    Macro Zone 2 return false

--- worlds/marioland2/logic.py
+++ worlds/marioland2/logic.py
@@ -550,7 +550,7 @@ def macro_zone_2_coins(state, player, coins):
         if state.has("Macro Zone 2 Midway Bell", player):
             # Cannot return to the first section from the bell
             return coins <= 42
-
+    return False
 
 def macro_zone_3_normal_exit(state, player):
     return ((has_pipe_down(state, player) and has_pipe_up(state, player))
