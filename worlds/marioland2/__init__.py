94e58b25c79c69a5fb81fcdcc68fa52da00d7093
Alchav <59858495+Alchav@users.noreply.github.com>
2024-09-10 10:21:39 -0400

    Update worlds/marioland2/__init__.py
    Co-authored-by: Exempt-Medic <60412657+Exempt-Medic@users.noreply.github.com>

--- worlds/marioland2/__init__.py
+++ worlds/marioland2/__init__.py
@@ -108,7 +108,7 @@ class MarioLand2World(World):
         if self.options.auto_scroll_chances == "vanilla":
             self.auto_scroll_levels = [int(i in [19, 25, 30]) for i in range(32)]
         else:
-            self.auto_scroll_levels = [int(self.random.randint(1, 100) < self.options.auto_scroll_chances)
+            self.auto_scroll_levels = [int(self.random.randint(1, 100) <= self.options.auto_scroll_chances)
                                        for _ in range(32)]
 
         self.auto_scroll_levels[level_name_to_id["Mario's Castle"]] = 0
