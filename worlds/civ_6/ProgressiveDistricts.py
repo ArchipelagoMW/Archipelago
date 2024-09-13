2f447264bdf305ff48490d1d7ad8351066b72092
Carter Hesterman <hestermancarter@gmail.com>
2024-09-12 20:44:07 -0600

    Minor change

--- worlds/civ_6/ProgressiveDistricts.py
+++ worlds/civ_6/ProgressiveDistricts.py
@@ -2,6 +2,7 @@ from typing import Dict, List
 
 from .Data import get_progressive_districts_data
 
+
 def get_flat_progressive_districts() -> Dict[str, str]:
     """Returns a dictionary of all items that are associated with a progressive item.
     Key is the item name ("TECH_WRITING") and the value is the associated progressive
@@ -18,10 +19,4 @@ def convert_items_to_have_progression(items: List[str]):
     """ converts a list of items to instead be their associated progressive item if
     they have one. ["TECH_MINING", "TECH_WRITING"] -> ["TECH_MINING", "PROGRESSIVE_CAMPUS]"""
     flat_progressive_techs = get_flat_progressive_districts()
-    new_list = []
-    for item in items:
-        if item in flat_progressive_techs.keys():
-            new_list.append(flat_progressive_techs[item])
-        else:
-            new_list.append(item)
-    return new_list
+    return [flat_progressive_techs.get(item, item) for item in items]
