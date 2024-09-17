3e24d2dbc27a5aa68120547ecd7226c059b16bb9
Bryce Wilson <gyroscope15@gmail.com>
2024-09-17 14:17:41 -0700

    Core: Improve death link option description (#3951)

--- Options.py
+++ Options.py
@@ -1335,7 +1335,7 @@ class PriorityLocations(LocationSet):
 
 
 class DeathLink(Toggle):
-    """When you die, everyone dies. Of course the reverse is true too."""
+    """When you die, everyone who enabled death link dies. Of course, the reverse is true too."""
     display_name = "Death Link"
     rich_text_doc = True
 
