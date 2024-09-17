78348fb4dc6d9164e149495aa41b0453d2a080cf
Mrks <68022469+mrkssr@users.noreply.github.com>
2024-09-17 23:56:40 +0200

    LADX: Adding Slot Data For Magpie Tracker (#3582)
    * wip: LADX slot_data
    
    * LADX: slot_data
    
    * Sending slot_data to magpie.
    
    * Moved sending slot_data from pushing to pull by Magpie request.
    
    * Adding EoF newline to tracker.py.
    
    * Update Tracker.py
    
    * Update __init__.py
    
    * Update LinksAwakeningClient.py
    
    ---------
    
    Co-authored-by: NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>

--- LinksAwakeningClient.py
+++ LinksAwakeningClient.py
@@ -497,6 +497,8 @@ class LinksAwakeningContext(CommonContext):
     def __init__(self, server_address: typing.Optional[str], password: typing.Optional[str],
                  magpie: typing.Optional[bool], collect: typing.Optional[bool]) -> None:
         self.client = LinksAwakeningClient()
+        self.slot_data = {}
+
         if magpie:
             self.magpie_enabled = True
             self.magpie = MagpieBridge()
@@ -605,6 +607,8 @@ class LinksAwakeningContext(CommonContext):
         if cmd == "Connected":
             self.game = self.slot_info[self.slot].game
             self.examine_collected_checks = True
+            self.slot_data = args.get("slot_data", {})
+            
         # TODO - use watcher_event
         elif cmd == "ReceivedItems":
             for index, item in enumerate(args["items"], start=args["index"]):
@@ -717,6 +721,7 @@ class LinksAwakeningContext(CommonContext):
                             self.magpie.set_checks(self.client.tracker.all_checks)
                             await self.magpie.set_item_tracker(self.client.item_tracker)
                             await self.magpie.send_gps(self.client.gps_tracker)
+                            self.magpie.slot_data = self.slot_data
                         except Exception:
                             # Don't let magpie errors take out the client
                             pass
