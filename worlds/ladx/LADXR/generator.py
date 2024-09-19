09bd7e21a8536cd2a5ff3825a86537762c009e26
threeandthreee <a.l.nordstrom@gmail.com>
2024-09-19 09:26:32 -0400

    drop whitespace changes

--- worlds/ladx/LADXR/generator.py
+++ worlds/ladx/LADXR/generator.py
@@ -249,7 +249,7 @@ def generateRom(args, world: "LinksAwakeningWorld"):
         patches.core.quickswap(rom, 1)
     elif world.ladxr_settings.quickswap == 'b':
         patches.core.quickswap(rom, 0)
-
+    
     patches.core.addBootsControls(rom, world.options.boots_controls)
 
 
@@ -355,7 +355,7 @@ def generateRom(args, world: "LinksAwakeningWorld"):
             # Then put new text in
             for bucket_idx, (orig_idx, data) in enumerate(bucket):
                 rom.texts[shuffled[bucket_idx][0]] = data
-
+    
 
     if world.options.trendy_game != TrendyGame.option_normal:
 
