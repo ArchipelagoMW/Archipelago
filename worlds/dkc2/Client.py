7e3719ad44ef0f2c94eb7e1f0bf6031b0e84967f
TheLX5 <luisyuregi@gmail.com>
2024-09-20 18:56:21 -0700

    client now detects locations

--- worlds/dkc2/Client.py
+++ worlds/dkc2/Client.py
@@ -14,60 +14,30 @@ WRAM_START = 0xF50000
 WRAM_SIZE = 0x20000
 SRAM_START = 0xE00000
 
-MMX_GAME_STATE          = WRAM_START + 0x000D1
-MMX_MENU_STATE          = WRAM_START + 0x000D2
-MMX_GAMEPLAY_STATE      = WRAM_START + 0x000D3
-MMX_LEVEL_INDEX         = WRAM_START + 0x01F7A
-
-MMX_WEAPON_ARRAY            = WRAM_START + 0x01F88
-MMX_SUB_TANK_ARRAY          = WRAM_START + 0x01F83
-MMX_UPGRADES                = WRAM_START + 0x01F99
-MMX_HEART_TANKS             = WRAM_START + 0x01F9C
-MMX_HADOUKEN                = WRAM_START + 0x01F7E
-MMX_LIFE_COUNT              = WRAM_START + 0x01F80
-MMX_MAX_HP                  = WRAM_START + 0x01F9A
-MMX_CURRENT_HP              = WRAM_START + 0x00BCF
-MMX_UNLOCKED_CHARGED_SHOT   = WRAM_START + 0x1EE16
-MMX_UNLOCKED_AIR_DASH       = WRAM_START + 0x1EE22
-
-MMX_SFX_FLAG            = WRAM_START + 0x1EE03
-MMX_SFX_NUMBER          = WRAM_START + 0x1EE04
-
-MMX_SIGMA_ACCESS            = WRAM_START + 0x1EE02
-MMX_COLLECTED_HEART_TANKS   = WRAM_START + 0x1EE05
-MMX_COLLECTED_UPGRADES      = WRAM_START + 0x1EE06
-MMX_COLLECTED_HADOUKEN      = WRAM_START + 0x1EE07
-MMX_DEFEATED_BOSSES         = WRAM_START + 0x1EE80
-MMX_COMPLETED_LEVELS        = WRAM_START + 0x1EE60
-MMX_COLLECTED_PICKUPS       = WRAM_START + 0x1EEC0
-MMX_UNLOCKED_LEVELS         = WRAM_START + 0x1EE40
-
-MMX_RECV_INDEX          = WRAM_START + 0x1EE00
-MMX_ENERGY_LINK_PACKET  = WRAM_START + 0x1EE09
-MMX_VALIDATION_CHECK    = WRAM_START + 0x1EE13
-
-MMX_RECEIVING_ITEM          = WRAM_START + 0x1EE15
-MMX_ENABLE_HEART_TANK       = WRAM_START + 0x1EE0B
-MMX_ENABLE_HP_REFILL        = WRAM_START + 0x1EE0F
-MMX_HP_REFILL_AMOUNT        = WRAM_START + 0x1EE10
-MMX_ENABLE_GIVE_1UP         = WRAM_START + 0x1EE12
-MMX_ENABLE_WEAPON_REFILL    = WRAM_START + 0x1EE1A
-MMX_WEAPON_REFILL_AMOUNT    = WRAM_START + 0x1EE1B
-
-MMX_PAUSE_STATE        = WRAM_START + 0x01F24
-MMX_CAN_MOVE           = WRAM_START + 0x01F13
-
-MMX_PICKUPSANITY_ACTIVE    = ROM_START + 0x17FFE7
-MMX_ENERGY_LINK_ENABLED    = ROM_START + 0x17FFE8
-MMX_DEATH_LINK_ACTIVE      = ROM_START + 0x17FFE9
-MMX_JAMMED_BUSTER_ACTIVE   = ROM_START + 0x17FFEA
-MMX_ABILITIES_FLAGS        = ROM_START + 0x17FFF1
-
-EXCHANGE_RATE = 500000000
-
 STARTING_ID = 0xBE0800
 
-MMX_ROMHASH_START = 0x7FC0
+DKC2_MISC_FLAGS = WRAM_START + 0x08D2
+DKC2_GAME_FLAGS = WRAM_START + 0x59B2
+
+DKC2_SRAM = SRAM_START + 0x800
+DKC2_RECV_INDEX = DKC2_SRAM + 0x020
+DKC2_INIT_FLAG = DKC2_SRAM + 0x022
+
+DKC2_IN_LEVEL = WRAM_START + 0x01FF
+DKC2_CURRENT_LEVEL = WRAM_START + 0x00D3
+
+DKC2_CRANKY_FLAGS = WRAM_START + 0x08D2
+DKC2_WRINKLY_FLAGS = WRAM_START + 0x08E0
+DKC2_FUNKY_FLAGS = WRAM_START + 0x08E7
+DKC2_SWANKY_FLAGS = WRAM_START + 0x08F0
+DKC2_KLUBBA_TOLLS = WRAM_START + 0x08FA
+DKC2_KONG_LETTERS = WRAM_START + 0x0902
+
+DKC2_BONUS_FLAGS = WRAM_START + 0x59B2
+DKC2_DK_COIN_FLAGS = WRAM_START + 0x59D2
+DKC2_STAGE_FLAGS = WRAM_START + 0x59F2
+
+DKC2_ROMHASH_START = 0x7FC0
 ROMHASH_SIZE = 0x15
 
 class DKC2SNIClient(SNIClient):
@@ -91,8 +61,7 @@ class DKC2SNIClient(SNIClient):
     async def validate_rom(self, ctx):
         from SNIClient import snes_read
 
-        energy_link = await snes_read(ctx, MMX_ENERGY_LINK_ENABLED, 0x1)
-        rom_name = await snes_read(ctx, MMX_ROMHASH_START, ROMHASH_SIZE)
+        rom_name = await snes_read(ctx, DKC2_ROMHASH_START, ROMHASH_SIZE)
         if rom_name is None or rom_name == bytes([0] * ROMHASH_SIZE) or rom_name[:4] != b"DKC2":
             return False
         
@@ -102,9 +71,9 @@ class DKC2SNIClient(SNIClient):
         ctx.send_option = 0
         ctx.allow_collect = True
 
-        death_link = await snes_read(ctx, MMX_DEATH_LINK_ACTIVE, 1)
-        if death_link[0]:
-            await ctx.update_death_link(bool(death_link[0] & 0b1))
+        #death_link = await snes_read(ctx, MMX_DEATH_LINK_ACTIVE, 1)
+        #if death_link[0]:
+        #    await ctx.update_death_link(bool(death_link[0] & 0b1))
 
         ctx.rom = rom_name
 
@@ -114,19 +83,12 @@ class DKC2SNIClient(SNIClient):
     async def game_watcher(self, ctx):
         from SNIClient import snes_buffered_write, snes_flush_writes, snes_read
 
-        game_state = await snes_read(ctx, MMX_GAME_STATE, 0x1)
-        menu_state = await snes_read(ctx, MMX_MENU_STATE, 0x1)
-        # Discard uninitialized ROMs
-        if menu_state is None:
-            self.game_state = False
-            return
-    
-        if game_state[0] == 0:
+        in_level = await snes_read(ctx, DKC2_IN_LEVEL, 0x01)
+        if in_level[0] != 0x80:
             self.game_state = False
             return
         
-        validation = await snes_read(ctx, MMX_VALIDATION_CHECK, 0x2)
-        validation = validation[0] | (validation[1] << 8)
+        validation = int.from_bytes(await snes_read(ctx, DKC2_INIT_FLAG, 0x2), "little")
         if validation != 0xDEAD:
             snes_logger.info(f'ROM not properly validated.')
             self.game_state = False
@@ -136,17 +98,42 @@ class DKC2SNIClient(SNIClient):
         from worlds import AutoWorldRegister
 
         new_checks = []
+        current_level = int.from_bytes(await snes_read(ctx, DKC2_CURRENT_LEVEL, 0x01))
+        game_flags = await snes_read(ctx, DKC2_GAME_FLAGS, 0x60)
+        misc_flags = await snes_read(ctx, DKC2_MISC_FLAGS, 0x80)
+        kong_flags = misc_flags[0x30]
+        stage_flags = game_flags[0x40:0x60]
+        bonus_flags = list(game_flags[0x00:0x20])
+        dk_coin_flags = list(game_flags[0x20:0x40])
         for loc_name, data in location_id_to_level_id.items():
             loc_id = AutoWorldRegister.world_types[ctx.game].location_name_to_id[loc_name]
             if loc_id not in ctx.locations_checked:
-                pass
+                loc_type = data[0]
+                level_num = data[1]
+                level_offset = (level_num >> 3) & 0x1E
+                level_bit = 1 << (level_num & 0x0F)
+
+                if loc_type == 0x00:
+                    # Level clear
+                    level_data = int.from_bytes(stage_flags[level_offset:level_offset+2], "little")
+                    if level_data & level_bit:
+                        new_checks.append(loc_id)
+                elif loc_type == 0x01:
+                    # KONG
+                    if level_num == current_level and kong_flags & 0x0F == 0x0F:
+                        new_checks.append(loc_id)
+                elif loc_type == 0x02:
+                    # DK Coin
+                    level_data = int.from_bytes(dk_coin_flags[level_offset:level_offset+2], "little")
+                    if level_data & level_bit:
+                        new_checks.append(loc_id)
+                elif loc_type == 0x03:
+                    # Bonus
+                    level_data = int.from_bytes(bonus_flags[level_offset:level_offset+2], "little")
+                    if level_data & level_bit:
+                        new_checks.append(loc_id)
  
-        verify_game_state = await snes_read(ctx, MMX_GAMEPLAY_STATE, 1)
-        if verify_game_state is None:
-            snes_logger.info(f'Exit Game.')
-            return
-        
-        rom = await snes_read(ctx, MMX_ROMHASH_START, ROMHASH_SIZE)
+        rom = await snes_read(ctx, DKC2_ROMHASH_START, ROMHASH_SIZE)
         if rom != ctx.rom:
             ctx.rom = None
             snes_logger.info(f'Exit ROM.')
@@ -159,7 +146,7 @@ class DKC2SNIClient(SNIClient):
                 f'New Check: {location} ({len(ctx.locations_checked)}/{len(ctx.missing_locations) + len(ctx.checked_locations)})')
             await ctx.send_msgs([{"cmd": 'LocationChecks', "locations": [new_check_id]}])
 
-        recv_count = await snes_read(ctx, MMX_RECV_INDEX, 2)
+        recv_count = await snes_read(ctx, DKC2_RECV_INDEX, 2)
         if recv_count is None:
             # Add a small failsafe in case we get a None. Other SNI games do this...
             return
@@ -175,10 +162,10 @@ class DKC2SNIClient(SNIClient):
                 color(ctx.player_names[item.player], 'yellow'),
                 ctx.location_names.lookup_in_slot(item.location, item.player), recv_index, len(ctx.items_received)))
             
-            snes_buffered_write(ctx, MMX_RECV_INDEX, bytes([recv_index]))
+            snes_buffered_write(ctx, DKC2_RECV_INDEX, bytes([recv_index]))
             await snes_flush_writes(ctx)
             
-            if item.item in 0x0:
+            if item.item in {0x0}:
                 pass
                 
         # Handle collected locations
@@ -195,8 +182,3 @@ class DKC2SNIClient(SNIClient):
                 i += 1
 
                 data = location_id_to_level_id[loc_name]
-
-
-    def on_package(self, ctx, cmd: str, args: dict):
-        super().on_package(ctx, cmd, args)
-        pass
