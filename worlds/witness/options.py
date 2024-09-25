f3d0ea435d18484467c29d30ae5b0b83ffe32f75
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-09-25 21:09:50 +0200

    ruff

--- worlds/witness/options.py
+++ worlds/witness/options.py
@@ -2,8 +2,17 @@ from dataclasses import dataclass
 
 from schema import And, Schema
 
-from Options import Choice, DefaultOnToggle, OptionDict, OptionGroup, PerGameCommonOptions, Range, Toggle, Visibility, \
-    OptionSet
+from Options import (
+    Choice,
+    DefaultOnToggle,
+    OptionDict,
+    OptionGroup,
+    OptionSet,
+    PerGameCommonOptions,
+    Range,
+    Toggle,
+    Visibility,
+)
 
 from .data import static_logic as static_witness_logic
 from .data.item_definition_classes import ItemCategory, WeightedItemDefinition
