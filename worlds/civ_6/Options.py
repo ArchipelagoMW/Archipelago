0f10369f4c3456ddc9a733c90aa3e369694d3f4f
Carter Hesterman <hestermancarter@gmail.com>
2024-10-20 14:01:55 -0600

    Update docs for options to be more readable

--- worlds/civ_6/Options.py
+++ worlds/civ_6/Options.py
@@ -4,17 +4,16 @@ from .Enum import CivVIHintClassification
 
 
 class ProgressionStyle(Choice):
-    """Determines what progressive items (if any) should be included.
-    Districts Only: Each tech/civic that would normally unlock a district or building now has a logical progression.
-    Example: TECH_BRONZE_WORKING is now PROGRESSIVE_ENCAMPMENT
-    Eras and Districts: Players will be defeated if they play until the world era advances beyond the currently unlocked
-    maximum era.
-    A notification will be shown as the end of the era approaches letting the player know if they don't have enough
-    progressive era items.
-    Unlocked eras can be seen in both the tech and civic trees. Includes all progressive districts.
-    None: No progressive items will be included. This means you can get district upgrades that won't be usable until the
-    relevant district is unlocked.
     """
+    **Districts Only**: Each tech/civic that would normally unlock a district or building now has a logical progression.
+    Example: TECH_BRONZE_WORKING is now PROGRESSIVE_ENCAMPMENT
+
+    **Eras and Districts**: Players will be defeated if they play until the world era advances beyond the currently unlocked maximum era.
+    Unlocked eras can be seen in both the tech and civic trees. Includes all progressive districts.
+
+    **None**: No progressive items will be included. This means you can get district upgrades that won't be usable until the relevant district is unlocked.
+    """
+    rich_text_doc = True
     display_name = "Progression Style"
     option_districts_only = 0
     option_eras_and_districts = 1
@@ -23,15 +22,18 @@ class ProgressionStyle(Choice):
 
 
 class ShuffleGoodyHuts(DefaultOnToggle):
-    """Shuffles the goody hut rewards. Goody huts will only contain junk items and locations are checked sequentially
-    (First goody hut gives GOODY_HUT_1, second gives GOODY_HUT_2, etc.)."""
+    """Shuffles the goody hut rewards.
+    Goody huts will only contain junk items and locations are checked sequentially (First goody hut gives GOODY_HUT_1, second gives GOODY_HUT_2, etc.)."""
     display_name = "Shuffle Goody Hut Rewards"
 
 
 class BoostSanity(Toggle):
     """Boosts for Civics/Techs are location checks. Boosts can now be triggered even if the item has already been
-    researched. If it is dependent upon a unit that is now obsolete, you can click to toggle on/off the relevant tech in
+    researched.
+
+    **Note**: If a boost is dependent upon a unit that is now obsolete, you can click to toggle on/off the relevant tech in
     the tech tree."""
+    rich_text_doc = True
     display_name = "Boostsanity"
 
 
@@ -62,11 +64,16 @@ class InGameFlagProgressionItems(DefaultOnToggle):
 
 class DeathLinkEffect(OptionSet):
     """What happens when a unit dies.
-    Unit Killed: A random unit will be killed when a death link is received.
-    Faith: Faith will be decreased by the amount specified in 'Death Link Effect Percent'.
-    Gold: Gold will be decreased by the amount specified in 'Death Link Effect Percent'.
-    Era Score: Era score is decreased by 1.
+
+    **Unit Killed**: A random unit will be killed when a death link is received.
+
+    **Faith**: Faith will be decreased by the amount specified in 'Death Link Effect Percent'.
+
+    **Gold**: Gold will be decreased by the amount specified in 'Death Link Effect Percent'.
+
+    **Era Score**: Era score is decreased by 1.
     """
+    rich_text_doc = True
     display_name = "Death Link Effect"
     valid_keys = ["Unit Killed", "Faith", "Gold", "Era Score"]  # type: ignore
     default = frozenset({"Unit Killed"})
