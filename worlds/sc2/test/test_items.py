d99a78ad6c54d0632430ddd30bb412bfa287bab2
Ziktofel <ziktofel@gmail.com>
2024-10-27 15:30:19 +0100

    Fix test_items

--- worlds/sc2/test/test_items.py
+++ worlds/sc2/test/test_items.py
@@ -1,7 +1,7 @@
 import unittest
 from typing import List, Set
 
-from worlds.sc2 import items
+from worlds.sc2 import item_tables
 
 
 class TestItems(unittest.TestCase):
@@ -10,8 +10,8 @@ class TestItems(unittest.TestCase):
         Tests if grouped upgrades have set number correctly
         :return:
         """
-        bundled_items = items.upgrade_bundles.keys()
-        bundled_item_data = [items.get_full_item_list()[item_name] for item_name in bundled_items]
+        bundled_items = item_tables.upgrade_bundles.keys()
+        bundled_item_data = [item_tables.get_full_item_list()[item_name] for item_name in bundled_items]
         bundled_item_numbers = [item_data.number for item_data in bundled_item_data]
 
         check_numbers = [number == -1 for number in bundled_item_numbers]
@@ -24,13 +24,13 @@ class TestItems(unittest.TestCase):
         :return:
         """
         check_modulo = 4
-        bundled_items = items.upgrade_bundles.keys()
+        bundled_items = item_tables.upgrade_bundles.keys()
         non_bundled_upgrades = [
-            item_name for item_name in items.get_full_item_list().keys()
+            item_name for item_name in item_tables.get_full_item_list().keys()
             if (item_name not in bundled_items
-                and items.get_full_item_list()[item_name].type in items.upgrade_item_types)
+                and item_tables.get_full_item_list()[item_name].type in item_tables.upgrade_item_types)
         ]
-        non_bundled_upgrade_data = [items.get_full_item_list()[item_name] for item_name in non_bundled_upgrades]
+        non_bundled_upgrade_data = [item_tables.get_full_item_list()[item_name] for item_name in non_bundled_upgrades]
         non_bundled_upgrade_numbers = [item_data.number for item_data in non_bundled_upgrade_data]
 
         check_numbers = [number % check_modulo == 0 for number in non_bundled_upgrade_numbers]
@@ -42,8 +42,8 @@ class TestItems(unittest.TestCase):
         Checks if there are no bundles within bundles.
         :return:
         """
-        bundled_items = items.upgrade_bundles.keys()
-        bundle_elements: List[str] = [item_name for values in items.upgrade_bundles.values() for item_name in values]
+        bundled_items = item_tables.upgrade_bundles.keys()
+        bundle_elements: List[str] = [item_name for values in item_tables.upgrade_bundles.values() for item_name in values]
 
         for element in bundle_elements:
             self.assertNotIn(element, bundled_items)
@@ -53,41 +53,41 @@ class TestItems(unittest.TestCase):
         Checks if Weapon/Armor upgrade level is correctly set to all Weapon/Armor upgrade items.
         :return:
         """
-        weapon_armor_upgrades = [item for item in items.get_full_item_list() if items.get_item_table()[item].type in items.upgrade_item_types]
+        weapon_armor_upgrades = [item for item in item_tables.get_full_item_list() if item_tables.get_item_table()[item].type in item_tables.upgrade_item_types]
 
         for weapon_armor_upgrade in weapon_armor_upgrades:
-            self.assertEqual(items.get_full_item_list()[weapon_armor_upgrade].quantity, items.WEAPON_ARMOR_UPGRADE_MAX_LEVEL)
+            self.assertEqual(item_tables.get_full_item_list()[weapon_armor_upgrade].quantity, item_tables.WEAPON_ARMOR_UPGRADE_MAX_LEVEL)
 
     def test_item_ids_distinct(self) -> None:
         """
         Verifies if there are no duplicates of item ID.
         :return:
         """
-        item_ids: Set[int] = {items.get_full_item_list()[item_name].code for item_name in items.get_full_item_list()}
+        item_ids: Set[int] = {item_tables.get_full_item_list()[item_name].code for item_name in item_tables.get_full_item_list()}
 
-        self.assertEqual(len(item_ids), len(items.get_full_item_list()))
+        self.assertEqual(len(item_ids), len(item_tables.get_full_item_list()))
 
     def test_number_distinct_in_item_type(self) -> None:
         """
         Tests if each item is distinct for sending into the mod.
         :return:
         """
-        item_types: List[items.ItemTypeEnum] = [
-            *[item.value for item in items.TerranItemType],
-            *[item.value for item in items.ZergItemType],
-            *[item.value for item in items.ProtossItemType],
-            *[item.value for item in items.FactionlessItemType]
+        item_types: List[item_tables.ItemTypeEnum] = [
+            *[item.value for item in item_tables.TerranItemType],
+            *[item.value for item in item_tables.ZergItemType],
+            *[item.value for item in item_tables.ProtossItemType],
+            *[item.value for item in item_tables.FactionlessItemType]
         ]
 
         self.assertGreater(len(item_types), 0)
 
         for item_type in item_types:
             item_names: List[str] = [
-                item_name for item_name in items.get_full_item_list()
-                if items.get_full_item_list()[item_name].number >= 0  # Negative numbers have special meaning
-                   and items.get_full_item_list()[item_name].type == item_type
+                item_name for item_name in item_tables.get_full_item_list()
+                if item_tables.get_full_item_list()[item_name].number >= 0  # Negative numbers have special meaning
+                   and item_tables.get_full_item_list()[item_name].type == item_type
             ]
-            item_numbers: Set[int] = {items.get_full_item_list()[item_name] for item_name in item_names}
+            item_numbers: Set[int] = {item_tables.get_full_item_list()[item_name] for item_name in item_names}
 
             self.assertEqual(len(item_names), len(item_numbers))
 
@@ -96,16 +96,16 @@ class TestItems(unittest.TestCase):
         Checks if the quantity attribute has been set for progressive items.
         :return:
         """
-        progressive_groups: List[items.ItemTypeEnum] = [
-            items.TerranItemType.Progressive,
-            items.TerranItemType.Progressive_2,
-            items.ProtossItemType.Progressive,
-            items.ZergItemType.Progressive
+        progressive_groups: List[item_tables.ItemTypeEnum] = [
+            item_tables.TerranItemType.Progressive,
+            item_tables.TerranItemType.Progressive_2,
+            item_tables.ProtossItemType.Progressive,
+            item_tables.ZergItemType.Progressive
         ]
 
         quantities: List[int] = [
-            items.get_full_item_list()[item].quantity for item in items.get_full_item_list()
-            if items.get_full_item_list()[item].type in progressive_groups
+            item_tables.get_full_item_list()[item].quantity for item in item_tables.get_full_item_list()
+            if item_tables.get_full_item_list()[item].type in progressive_groups
         ]
 
         self.assertNotIn(1, quantities)
@@ -115,46 +115,46 @@ class TestItems(unittest.TestCase):
         Check if non-progressive items have quantity at most 1.
         :return:
         """
-        non_progressive_single_entity_groups: List[items.ItemTypeEnum] = [
+        non_progressive_single_entity_groups: List[item_tables.ItemTypeEnum] = [
             # Terran
-            items.TerranItemType.Unit,
-            items.TerranItemType.Unit_2,
-            items.TerranItemType.Mercenary,
-            items.TerranItemType.Armory_1,
-            items.TerranItemType.Armory_2,
-            items.TerranItemType.Armory_3,
-            items.TerranItemType.Armory_4,
-            items.TerranItemType.Armory_5,
-            items.TerranItemType.Armory_6,
-            items.TerranItemType.Armory_7,
-            items.TerranItemType.Building,
-            items.TerranItemType.Laboratory,
-            items.TerranItemType.Nova_Gear,
+            item_tables.TerranItemType.Unit,
+            item_tables.TerranItemType.Unit_2,
+            item_tables.TerranItemType.Mercenary,
+            item_tables.TerranItemType.Armory_1,
+            item_tables.TerranItemType.Armory_2,
+            item_tables.TerranItemType.Armory_3,
+            item_tables.TerranItemType.Armory_4,
+            item_tables.TerranItemType.Armory_5,
+            item_tables.TerranItemType.Armory_6,
+            item_tables.TerranItemType.Armory_7,
+            item_tables.TerranItemType.Building,
+            item_tables.TerranItemType.Laboratory,
+            item_tables.TerranItemType.Nova_Gear,
             # Zerg
-            items.ZergItemType.Unit,
-            items.ZergItemType.Mercenary,
-            items.ZergItemType.Morph,
-            items.ZergItemType.Strain,
-            items.ZergItemType.Mutation_1,
-            items.ZergItemType.Mutation_2,
-            items.ZergItemType.Mutation_3,
-            items.ZergItemType.Evolution_Pit,
-            items.ZergItemType.Ability,
+            item_tables.ZergItemType.Unit,
+            item_tables.ZergItemType.Mercenary,
+            item_tables.ZergItemType.Morph,
+            item_tables.ZergItemType.Strain,
+            item_tables.ZergItemType.Mutation_1,
+            item_tables.ZergItemType.Mutation_2,
+            item_tables.ZergItemType.Mutation_3,
+            item_tables.ZergItemType.Evolution_Pit,
+            item_tables.ZergItemType.Ability,
             # Protoss
-            items.ProtossItemType.Unit,
-            items.ProtossItemType.Unit_2,
-            items.ProtossItemType.Building,
-            items.ProtossItemType.Forge_1,
-            items.ProtossItemType.Forge_2,
-            items.ProtossItemType.Forge_3,
-            items.ProtossItemType.Forge_4,
-            items.ProtossItemType.Solarite_Core,
-            items.ProtossItemType.Spear_Of_Adun
+            item_tables.ProtossItemType.Unit,
+            item_tables.ProtossItemType.Unit_2,
+            item_tables.ProtossItemType.Building,
+            item_tables.ProtossItemType.Forge_1,
+            item_tables.ProtossItemType.Forge_2,
+            item_tables.ProtossItemType.Forge_3,
+            item_tables.ProtossItemType.Forge_4,
+            item_tables.ProtossItemType.Solarite_Core,
+            item_tables.ProtossItemType.Spear_Of_Adun
         ]
 
         quantities: List[int] = [
-            items.get_full_item_list()[item].quantity for item in items.get_full_item_list()
-            if items.get_full_item_list()[item].type in non_progressive_single_entity_groups
+            item_tables.get_full_item_list()[item].quantity for item in item_tables.get_full_item_list()
+            if item_tables.get_full_item_list()[item].type in non_progressive_single_entity_groups
         ]
 
         for quantity in quantities:
@@ -165,14 +165,14 @@ class TestItems(unittest.TestCase):
         Checks if all item numbers are within bounds supported by game mod.
         :return:
         """
-        not_checked_item_types: List[items.ItemTypeEnum] = [
-            items.ZergItemType.Level
+        not_checked_item_types: List[item_tables.ItemTypeEnum] = [
+            item_tables.ZergItemType.Level
         ]
         items_to_check: List[str] = [
-            item for item in items.get_full_item_list()
-            if items.get_full_item_list()[item].type not in not_checked_item_types
+            item for item in item_tables.get_full_item_list()
+            if item_tables.get_full_item_list()[item].type not in not_checked_item_types
         ]
 
         for item in items_to_check:
-            item_number = items.get_full_item_list()[item].number
+            item_number = item_tables.get_full_item_list()[item].number
             self.assertLess(item_number, 30)
