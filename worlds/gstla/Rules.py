18be9fad15891d19dae848f69c399b99b3c9be30
Dragion147 <albert_haaksema@hotmail.com>
2024-11-09 21:26:29 +0100

    Add lucky medal logic to eclipse location.

--- worlds/gstla/Rules.py
+++ worlds/gstla/Rules.py
@@ -595,6 +595,9 @@ def set_access_rules(world: 'GSTLAWorld'):
     #Lemuria
     add_rule(world.get_location(LocationName.Rime),
              lambda state: state.has(ItemName.Grindstone, player) and state.has(ItemName.Cyclone_Chip, player) and state.has(ItemName.Tremor_Bit, player))
+    
+    add_rule(world.get_location(LocationName.Lemuria_Eclipse),
+             lambda state: state.has(ItemName.Lucky_Medal, player))
 
     #Western Sea
 
