f0c37d162bb1d9b5b8953dd66ae48875f977a619
palex00 <32203971+palex00@users.noreply.github.com>
2024-11-11 18:02:37 +0100

    [PKMN RB] Adds slot data that tells the tracker V5 logic should be considered (#3995)
    * Adds v5_update-info to slot data
    
    * Adds a comma
    
    Co-authored-by: Scipio Wright <scipiowright@gmail.com>
    
    ---------
    
    Co-authored-by: Scipio Wright <scipiowright@gmail.com>

--- worlds/pokemon_rb/__init__.py
+++ worlds/pokemon_rb/__init__.py
@@ -712,6 +712,7 @@ class PokemonRedBlueWorld(World):
             "require_pokedex": self.options.require_pokedex.value,
             "area_1_to_1_mapping": self.options.area_1_to_1_mapping.value,
             "blind_trainers": self.options.blind_trainers.value,
+            "v5_update": True,
 
         }
         if self.options.type_chart_seed == "random" or self.options.type_chart_seed.value.isdigit():
