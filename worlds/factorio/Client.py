f3413e9cef1f3b719d4e039ef170544b52476482
Fabian Dill <Berserker66@users.noreply.github.com>
2024-11-11 11:43:16 +0100

    Factorio: support 2.0 update (#4110)
    - removed tutorialization (Craft/Do X to unlock tech)
    - start with  everything needed for power, electric mining drills, science lab and automation science already unlocked
    - updated world gen
    - updated mod api use
       - updated fluid boxes (CaitSith2)
    - new option: free sample quality (needs quality mod)
    - removed old gruft, faster gen speed, faster load time
    - lists space age as explicitly not supported, so it prevents the game from trying to load both
    - fixes Y offset of traps being wrong (way higher than intended)
    - client now has a 5 second timeout to communicate with the bound factorio server, so it aborts actions if the server died
    - savegames are now stored  in write_data_directory -> saves -> Archipelago
    - add cargo-landing-pad  handling
    - starting rocket silo and cargo landing pad respect free sample quality 
    - supports Factorio 2.0
    
    ---------
    
    Co-authored-by: CaitSith2 <d_good@caitsith2.com>

--- worlds/factorio/Client.py
+++ worlds/factorio/Client.py
@@ -304,13 +304,13 @@ def stream_factorio_output(pipe, queue, process):
 
 
 async def factorio_server_watcher(ctx: FactorioContext):
-    savegame_name = os.path.abspath(ctx.savegame_name)
+    savegame_name = os.path.abspath(os.path.join(ctx.write_data_path, "saves", "Archipelago", ctx.savegame_name))
     if not os.path.exists(savegame_name):
         logger.info(f"Creating savegame {savegame_name}")
         subprocess.run((
             executable, "--create", savegame_name, "--preset", "archipelago"
         ))
-    factorio_process = subprocess.Popen((executable, "--start-server", ctx.savegame_name,
+    factorio_process = subprocess.Popen((executable, "--start-server", savegame_name,
                                          *(str(elem) for elem in server_args)),
                                         stderr=subprocess.PIPE,
                                         stdout=subprocess.PIPE,
@@ -331,7 +331,8 @@ async def factorio_server_watcher(ctx: FactorioContext):
                 factorio_queue.task_done()
 
                 if not ctx.rcon_client and "Starting RCON interface at IP ADDR:" in msg:
-                    ctx.rcon_client = factorio_rcon.RCONClient("localhost", rcon_port, rcon_password)
+                    ctx.rcon_client = factorio_rcon.RCONClient("localhost", rcon_port, rcon_password,
+                                                               timeout=5)
                     if not ctx.server:
                         logger.info("Established bridge to Factorio Server. "
                                     "Ready to connect to Archipelago via /connect")
@@ -405,8 +406,7 @@ async def get_info(ctx: FactorioContext, rcon_client: factorio_rcon.RCONClient):
     info = json.loads(rcon_client.send_command("/ap-rcon-info"))
     ctx.auth = info["slot_name"]
     ctx.seed_name = info["seed_name"]
-    # 0.2.0 addition, not present earlier
-    death_link = bool(info.get("death_link", False))
+    death_link = info["death_link"]
     ctx.energy_link_increment = info.get("energy_link", 0)
     logger.debug(f"Energy Link Increment: {ctx.energy_link_increment}")
     if ctx.energy_link_increment and ctx.ui:
