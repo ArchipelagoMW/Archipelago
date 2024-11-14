925340f15b7e3415346930ed26b33c6d0ac43f2a
Silvris <58583688+Silvris@users.noreply.github.com>
2024-11-13 23:24:19 -0600

    Split export to top level script

--- worlds/Export.py
+++ worlds/Export.py
@@ -1,24 +1,18 @@
 # TODO: handle data
 # TODO: handle clients
 # TODO: handle yaml templates
-import argparse
+
 import json
 import os.path
 import platform
 import sys
 import zipfile
 from pathlib import Path
-
-if __name__ == "__main__":
-    sys.path.remove(os.path.dirname(__file__))
-    ap_root = os.path.normpath(os.path.join(os.path.dirname(__file__), os.pardir))
-    os.chdir(ap_root)
-    sys.path.append(ap_root)
-
-from worlds.AutoWorld import AutoWorldRegister, World
+from .AutoWorld import World
 
 METADATA_KEYS = [
     "game",
+    "file_types",
     # license
     # description
     # maintainers
@@ -86,15 +80,3 @@ def export_world(libfolder, world_type, output_dir, is_frozen):
         zf.close()
     return output_name
 
-
-if __name__ == "__main__":
-    ap_root_path = Path(ap_root)
-
-    parser = argparse.ArgumentParser("simple_example")
-    parser.add_argument("world_name", help="World to be exported.", type=str)
-    parser.add_argument("output_dir", help="Export directory.", type=Path, default=ap_root_path / "worlds", nargs='?')
-    args = parser.parse_args()
-
-    output = export_world(ap_root_path, AutoWorldRegister.world_types[args.world_name], args.output_dir,
-                          is_frozen=False)
-    print(f"Output to {output}")
