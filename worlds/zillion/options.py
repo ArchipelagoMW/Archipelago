9b69e572015850c7c6eece645bf09b687fbf5382
beauxq <beauxq@yahoo.com>
2024-11-20 20:40:30 -0800

    Zillion: new priority dead ends feature

--- worlds/zillion/options.py
+++ worlds/zillion/options.py
@@ -263,13 +263,27 @@ class ZillionMapGen(Choice):
     option_full = 2
     default = 0
 
-    def zz_value(self) -> Literal['none', 'rooms', 'full']:
+    def zz_value(self) -> Literal["none", "rooms", "full"]:
         if self.value == ZillionMapGen.option_none:
             return "none"
         if self.value == ZillionMapGen.option_rooms:
             return "rooms"
         assert self.value == ZillionMapGen.option_full
         return "full"
+
+
+class ZillionPriorityDeadEnds(DefaultOnToggle):
+    """
+    Single locations that are in a dead end behind a door
+    (example: vanilla Apple location)
+    are prioritized for progression items.
+    """
+    display_name = "priority dead ends"
+
+    vanilla_dead_ends: ClassVar = frozenset(("E-5 top far right", "J-4 top left"))
+    """ dead ends when not generating these rooms """
+    always_dead_ends: ClassVar = frozenset(("A-6 top right",))
+    """ dead ends in rooms that never get generated """
 
 
 @dataclass
@@ -293,6 +307,7 @@ class ZillionOptions(PerGameCommonOptions):
     skill: ZillionSkill
     starting_cards: ZillionStartingCards
     map_gen: ZillionMapGen
+    priority_dead_ends: ZillionPriorityDeadEnds
 
     room_gen: Removed
 
