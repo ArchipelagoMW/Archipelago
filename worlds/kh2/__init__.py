69e4bcc1cd5bbe187658216c05e51216dead09d9
Exempt-Medic <60412657+Exempt-Medic@users.noreply.github.com>
2024-11-22 13:09:24 -0500

    Change to fast_fill

--- worlds/kh2/__init__.py
+++ worlds/kh2/__init__.py
@@ -2,7 +2,7 @@ import logging
 from typing import List
 
 from BaseClasses import Tutorial, ItemClassification
-from Fill import fill_restrictive
+from Fill import fast_fill
 from worlds.LauncherComponents import Component, components, Type, launch_subprocess
 from worlds.AutoWorld import World, WebWorld
 from .Items import *
@@ -287,7 +287,7 @@ class KH2World(World):
 
     def pre_fill(self):
         """
-        Plandoing Events and Fill_Restrictive for donald,goofy and sora
+        Plandoing Events and Fast_Fill for donald,goofy and sora
         """
         self.donald_pre_fill()
         self.goofy_pre_fill()
@@ -431,9 +431,10 @@ class KH2World(World):
         Fills keyblade slots with abilities determined on player's setting
         """
         keyblade_locations = [self.multiworld.get_location(location, self.player) for location in Keyblade_Slots.keys()]
-        state = self.multiworld.get_all_state(False)
         keyblade_ability_pool_copy = self.keyblade_ability_pool.copy()
-        fill_restrictive(self.multiworld, state, keyblade_locations, keyblade_ability_pool_copy, True, True, allow_excluded=True)
+        fast_fill(self.multiworld, keyblade_ability_pool_copy, keyblade_locations)
+        for location in keyblade_locations:
+            location.locked = True
 
     def starting_invo_verify(self):
         """
