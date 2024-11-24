9e7cfd9a25478834b49bf2e67b08d36d844ca544
Jouramie <jouramie@hotmail.com>
2024-11-24 14:59:08 -0500

    use new assets to ease readability

--- worlds/stardew_valley/test/TestWalnutsanity.py
+++ worlds/stardew_valley/test/TestWalnutsanity.py
@@ -66,7 +66,6 @@ class TestWalnutsanityPuzzles(SVTestBase):
     def test_field_office_locations_require_professor_snail(self):
         location_names = ["Complete Large Animal Collection", "Complete Snake Collection", "Complete Mummified Frog Collection",
                           "Complete Mummified Bat Collection", "Purple Flowers Island Survey", "Purple Starfish Island Survey", ]
-        locations = [location for location in self.multiworld.get_locations() if location.name in location_names]
         self.collect("Island Obelisk")
         self.collect("Island North Turtle")
         self.collect("Island West Turtle")
@@ -80,11 +79,11 @@ class TestWalnutsanityPuzzles(SVTestBase):
         self.collect("Progressive Sword", 5)
         self.collect("Combat Level", 10)
         self.collect("Mining Level", 10)
-        for location in locations:
-            self.assert_reach_location_false(location, self.multiworld.state)
+        for location in location_names:
+            self.assert_location_cannot_be_reached(location)
         self.collect("Open Professor Snail Cave")
-        for location in locations:
-            self.assert_reach_location_true(location, self.multiworld.state)
+        for location in location_names:
+            self.assert_location_can_be_reached(location)
 
 
 class TestWalnutsanityBushes(SVTestBase):
--- worlds/stardew_valley/test/rules/TestBuildings.py
+++ worlds/stardew_valley/test/rules/TestBuildings.py
@@ -9,41 +9,37 @@ class TestBuildingLogic(SVTestBase):
     }
 
     def test_coop_blueprint(self):
-        coop_blueprint_rule = self.get_can_reach_location_rule("Coop Blueprint")
-        self.assert_rule_false(coop_blueprint_rule, self.multiworld.state)
+        self.assert_location_cannot_be_reached("Coop Blueprint")
 
         self.collect_lots_of_money()
-        self.assert_rule_true(coop_blueprint_rule, self.multiworld.state)
+        self.assert_location_can_be_reached("Coop Blueprint")
 
     def test_big_coop_blueprint(self):
-        big_coop_blueprint_rule = self.get_can_reach_location_rule("Big Coop Blueprint")
-        self.assert_rule_false(big_coop_blueprint_rule, self.multiworld.state)
+        self.assert_location_cannot_be_reached("Big Coop Blueprint")
 
         self.collect_lots_of_money()
-        self.assert_rule_false(big_coop_blueprint_rule, self.multiworld.state)
+        self.assert_location_cannot_be_reached("Big Coop Blueprint")
 
         self.multiworld.state.collect(self.create_item("Progressive Coop"))
-        self.assert_rule_true(big_coop_blueprint_rule, self.multiworld.state)
+        self.assert_location_can_be_reached("Big Coop Blueprint")
 
     def test_deluxe_coop_blueprint(self):
-        deluxe_coop_blueprint_rule = self.get_can_reach_location_rule("Deluxe Coop Blueprint")
-        self.assert_rule_false(deluxe_coop_blueprint_rule, self.multiworld.state)
+        self.assert_location_cannot_be_reached("Deluxe Coop Blueprint")
 
         self.collect_lots_of_money()
-        self.assert_rule_false(deluxe_coop_blueprint_rule, self.multiworld.state)
+        self.assert_location_cannot_be_reached("Deluxe Coop Blueprint")
 
         self.multiworld.state.collect(self.create_item("Progressive Coop"))
-        self.assert_rule_false(deluxe_coop_blueprint_rule, self.multiworld.state)
+        self.assert_location_cannot_be_reached("Deluxe Coop Blueprint")
 
         self.multiworld.state.collect(self.create_item("Progressive Coop"))
-        self.assert_rule_true(deluxe_coop_blueprint_rule, self.multiworld.state)
+        self.assert_location_can_be_reached("Deluxe Coop Blueprint")
 
     def test_big_shed_blueprint(self):
-        big_shed_rule = self.get_can_reach_location_rule("Big Shed Blueprint")
-        self.assert_rule_false(big_shed_rule, self.multiworld.state)
+        self.assert_location_cannot_be_reached("Big Shed Blueprint")
 
         self.collect_lots_of_money()
-        self.assert_rule_false(big_shed_rule, self.multiworld.state)
+        self.assert_location_cannot_be_reached("Big Shed Blueprint")
 
         self.multiworld.state.collect(self.create_item("Progressive Shed"))
-        self.assert_rule_true(big_shed_rule, self.multiworld.state)
+        self.assert_location_can_be_reached("Big Shed Blueprint")
