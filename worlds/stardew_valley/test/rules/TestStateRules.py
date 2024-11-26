4054827d54ea4f8534ae43802b641dc6d7b7b62b
agilbert1412 <alexgilbert@yahoo.com>
2024-11-26 18:44:33 -0500

    Stardew Valley: Make progressive movie theater a progression trap (#3985)

--- worlds/stardew_valley/test/rules/TestStateRules.py
+++ worlds/stardew_valley/test/rules/TestStateRules.py
@@ -8,5 +8,5 @@ class TestHasProgressionPercent(unittest.TestCase):
     def test_max_item_amount_is_full_collection(self):
         # Not caching because it fails too often for some reason
         with solo_multiworld(world_caching=False) as (multiworld, world):
-            progression_item_count = sum(1 for i in multiworld.get_items() if ItemClassification.progression in i.classification)
+            progression_item_count = sum(1 for i in multiworld.get_items() if i.classification & ItemClassification.progression)
             self.assertEqual(world.total_progression_items, progression_item_count - 1)  # -1 to skip Victory
