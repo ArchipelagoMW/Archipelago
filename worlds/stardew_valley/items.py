75624042f7a56b7c394b5a5e36936635cb45e0f8
agilbert1412 <alexgilbert@yahoo.com>
2024-11-26 18:44:33 -0500

    Stardew Valley: Make progressive movie theater a progression trap (#3985)

--- worlds/stardew_valley/items.py
+++ worlds/stardew_valley/items.py
@@ -2,6 +2,7 @@ import csv
 import enum
 import logging
 from dataclasses import dataclass, field
+from functools import reduce
 from pathlib import Path
 from random import Random
 from typing import Dict, List, Protocol, Union, Set, Optional
@@ -134,7 +135,7 @@ def load_item_csv():
         item_reader = csv.DictReader(file)
         for item in item_reader:
             id = int(item["id"]) if item["id"] else None
-            classification = ItemClassification[item["classification"]]
+            classification = reduce((lambda a, b: a | b), {ItemClassification[str_classification] for str_classification in item["classification"].split(",")})
             groups = {Group[group] for group in item["groups"].split(",") if group}
             mod_name = str(item["mod_name"]) if item["mod_name"] else None
             items.append(ItemData(id, item["name"], classification, mod_name, groups))
