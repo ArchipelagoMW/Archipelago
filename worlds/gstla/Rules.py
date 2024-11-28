36649e4135bd68d88cf9cc1feedc6ea9ff61e96c
Dragion147 <albert_haaksema@hotmail.com>
2024-11-28 21:07:48 +0100

    Remove pound requiremnt for trainerswhip

--- worlds/gstla/Rules.py
+++ worlds/gstla/Rules.py
@@ -272,9 +272,6 @@ def set_access_rules(world: 'GSTLAWorld'):
     add_rule(world.get_location(LocationName.Yampi_Desert_Trainers_Whip),
              lambda state: state.has(ItemName.Lash_Pebble, player) or state.has(ItemName.Sand, player))
 
-    add_rule(world.get_location(LocationName.Yampi_Desert_Trainers_Whip),
-             lambda state: state.has(ItemName.Pound_Cube, player))
-
     add_rule(world.get_location(LocationName.Yampi_Desert_Cave_Water_of_Life),
              lambda state: state.has(ItemName.Sand, player))
 
