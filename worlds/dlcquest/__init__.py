79a557fc9a7fa2c1bdb70b8f24c5d9b697cc2c31
axe-y <hamel.antoine@gmail.com>
2024-11-27 23:29:17 -0500

    DLC Quest Bug Fix

--- worlds/dlcquest/__init__.py
+++ worlds/dlcquest/__init__.py
@@ -72,10 +72,16 @@ class DLCqworld(World):
 
         self.multiworld.itempool += created_items
 
-        if self.options.campaign == Options.Campaign.option_basic or self.options.campaign == Options.Campaign.option_both:
-            self.multiworld.early_items[self.player]["Movement Pack"] = 1
-            if self.options.campaign == Options.Campaign.option_both and self.options.coinsanity and self.options.coinbundlequantity > 50:
-                self.multiworld.local_early_items[self.player]["Live Freemium or Die: Coin Bundle"] = 1
+        campaign = self.options.campaign
+        has_both = campaign == Options.Campaign.option_both
+        has_base = campaign == Options.Campaign.option_basic or has_both
+        has_big_bundles = self.options.coinsanity and self.options.coinbundlequantity > 50
+        early_items = self.multiworld.early_items
+        if has_base:
+            if has_both and has_big_bundles:
+                early_items[self.player]["Incredibly Important Pack"] = 1
+            else:
+                early_items[self.player]["Movement Pack"] = 1
 
         for item in items_to_exclude:
             if item in self.multiworld.itempool:
