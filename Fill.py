a636d3c8e3da7b5264732af0bf191c4577e18e03
Scipio Wright <scipiowright@gmail.com>
2024-11-29 13:40:41 -0500

    Adjust txt for raised fill error

--- Fill.py
+++ Fill.py
@@ -509,7 +509,8 @@ def distribute_items_restrictive(multiworld: MultiWorld,
         if progitempool:
             raise FillError(
                 f"Not enough locations for progression items. "
-                f"There are {len(progitempool)} more progression items than there are available locations.",
+                f"There are {len(progitempool)} more progression items than there are available locations.\n"
+                f"Unfilled locations: {multiworld.get_unfilled_locations()}.",
                 multiworld=multiworld,
             )
         accessibility_corrections(multiworld, multiworld.state, defaultlocations)
