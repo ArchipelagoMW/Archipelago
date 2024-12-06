015d2fab3f6e8a1ab5d76fe47fd06063c1737417
AutomaticFrenzy <beerensd@gmail.com>
2024-12-06 18:47:01 -0500

    base init.py

--- worlds/loonyland/__init__.py
+++ worlds/loonyland/__init__.py
@@ -1,31 +1,34 @@
-from typing import Dict
+# world/Loonyland/__init__.py
 
-from BaseClasses import Tutorial
-from ..AutoWorld import WebWorld, World
-
-class LoonylandWeb(WebWorld):
-    options_page = False
-    theme = 'partyTime'
-
-    setup_en = Tutorial(
-        tutorial_name='Setup Guide',
-        description='A guide to playing Loonyland',
-        language='English',
-        file_name='setup_en.md',
-        link='setup/en',
-        authors=['AutomaticFrenzy']
-    )
-    
-    tutorials = [setup_en]
-
-class LoonylandWorld(World):
-    """
-    Loonyland Halloween Hill
-    """
-    game = "Loonyland"
-    web = LoonylandWeb()
-
-    item_name_to_id: Dict[str, int] = {}
-    location_name_to_id: Dict[str, int] = {}
+import settings
+import typing
+from .options import LoonylandOptions  
+from .items import Loonyland_items, item_frequencies, LoonylandItem
+from .locations import Loonyland_locations, LoonylandLocation
+from worlds.AutoWorld import World
+from BaseClasses import Region, Location, Entrance, Item, RegionType, ItemClassification
 
 
+class MyGameSettings(settings.Group):
+
+
+class MyGameWorld(World):
+    """Insert description of the world/game here."""
+    game = "Loonyland"  
+    options_dataclass = LoonylandOptions  # options the player can set
+    options: LoonylandOptions  # typing hints for option results
+    settings: typing.ClassVar[MyGameSettings]  # will be automatically assigned from type hint
+    topology_present = True  # show path to required location checks in spoiler
+
+    # ID of first item and location, could be hard-coded but code may be easier
+    # to read with this as a property.
+    base_id = 2876900
+    # instead of dynamic numbering, IDs could be part of data
+
+    # The following two dicts are required for the generation to know which
+    # items exist. They could be generated from json or something else. They can
+    # include events, but don't have to since events will be placed manually.
+    item_name_to_id = {name: id for
+                       id, name in enumerate(mygame_items, base_id)}
+    location_name_to_id = {name: id for
+                           id, name in enumerate(mygame_locations, base_id)}
