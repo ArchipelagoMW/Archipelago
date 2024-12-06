ba96272cef4ab8c0fadc0df15a55b1d3f12cfe9a
AutomaticFrenzy <beerensd@gmail.com>
2024-12-06 17:29:07 -0500

    Ids for Items locations regions

--- worlds/loonyland/Items.py
+++ worlds/loonyland/Items.py
@@ -20,6 +20,7 @@ class ItemCategory(Enum):
     
     
 class ItemData(NamedTuple):
+    id: int
     category: ItemCategory
     classification: ItemClassification
 
@@ -36,36 +37,40 @@ item_frequencies = {
 }    
     
 item_table: Dict[str, ItemData] = {
-    "Heart": ItemData(ItemCategory.ITEM, ItemClassification.useful),
-    "Lightning": ItemData(ItemCategory.ITEM, ItemClassification.useful),
-    "Arrow": ItemData(ItemCategory.ITEM, ItemClassification.useful),
-    "Pants": ItemData(ItemCategory.ITEM, ItemClassification.useful),
-    "Mushroom": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Orb": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Bombs": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Shock Wand": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Ice spear": ItemData(ItemCategory.ITEM,  ItemClassification.progression),
-    "Cactus": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Boomerang": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Whoopee": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Hot Pants": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Skull Key": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Bat Key": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Pumpkin Key": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Boots": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Stick": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Fertilizer": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Silver": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Doom Daisy": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Ghost Potion": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Vamp Statue": ItemData(ItemCategory.ITEM,  ItemClassification.progression),
-    "Cat": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Big Gem": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Zombie Reward": ItemData(ItemCategory.ITEM, ItemClassification.filler),
-    "3 way": ItemData(ItemCategory.ITEM, ItemClassification.useful),
-    "Happy Stick": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Bat Statue": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Lantern": ItemData(ItemCategory.ITEM, ItemClassification.progression),
-    "Reflect": ItemData(ItemCategory.ITEM, ItemClassification.useful),
-    "Silver Sling": ItemData(ItemCategory.ITEM, ItemClassification.progression)
+    "Heart":        ItemData(loonyland_base_id + 0, ItemCategory.ITEM, ItemClassification.useful),
+    "Lightning":    ItemData(loonyland_base_id + 1, ItemCategory.ITEM, ItemClassification.useful),
+    "Arrow":        ItemData(loonyland_base_id + 2, ItemCategory.ITEM, ItemClassification.useful),
+    "Pants":        ItemData(loonyland_base_id + 3, ItemCategory.ITEM, ItemClassification.useful),
+    "Mushroom":     ItemData(loonyland_base_id + 4, ItemCategory.ITEM, ItemClassification.progression),
+    "Orb":          ItemData(loonyland_base_id + 5, ItemCategory.ITEM, ItemClassification.progression),
+    "Bombs":        ItemData(loonyland_base_id + 6, ItemCategory.ITEM, ItemClassification.progression),
+    "Shock Wand":   ItemData(loonyland_base_id + 7, ItemCategory.ITEM, ItemClassification.progression),
+    "Ice spear":    ItemData(loonyland_base_id + 8, ItemCategory.ITEM, ItemClassification.progression),
+    "Cactus":       ItemData(loonyland_base_id + 9, ItemCategory.ITEM, ItemClassification.progression),
+    "Boomerang":    ItemData(loonyland_base_id + 10, ItemCategory.ITEM, ItemClassification.progression),
+    "Whoopee":      ItemData(loonyland_base_id + 11, ItemCategory.ITEM, ItemClassification.progression),
+    "Hot Pants":    ItemData(loonyland_base_id + 12, ItemCategory.ITEM, ItemClassification.progression),
+    "Skull Key":    ItemData(loonyland_base_id + 13, ItemCategory.ITEM, ItemClassification.progression),
+    "Bat Key":      ItemData(loonyland_base_id + 14, ItemCategory.ITEM, ItemClassification.progression),
+    "Pumpkin Key":  ItemData(loonyland_base_id + 15, ItemCategory.ITEM, ItemClassification.progression),
+    "Boots":        ItemData(loonyland_base_id + 16, ItemCategory.ITEM, ItemClassification.progression),
+    "Stick":        ItemData(loonyland_base_id + 17, ItemCategory.ITEM, ItemClassification.progression),
+    "Fertilizer":   ItemData(loonyland_base_id + 18, ItemCategory.ITEM, ItemClassification.progression),
+    "Silver":       ItemData(loonyland_base_id + 19, ItemCategory.ITEM, ItemClassification.progression),
+    "Doom Daisy":   ItemData(loonyland_base_id + 20, ItemCategory.ITEM, ItemClassification.progression),
+    "Ghost Potion": ItemData(loonyland_base_id + 21, ItemCategory.ITEM, ItemClassification.progression),
+    "Vamp Statue":  ItemData(loonyland_base_id + 22, ItemCategory.ITEM, ItemClassification.progression),
+    "Cat":          ItemData(loonyland_base_id + 23, ItemCategory.ITEM, ItemClassification.progression),
+    "Big Gem":      ItemData(loonyland_base_id + 24, ItemCategory.ITEM, ItemClassification.progression),
+    "Zombie Reward":ItemData(loonyland_base_id + 25, ItemCategory.ITEM, ItemClassification.filler),
+    "3 way":        ItemData(loonyland_base_id + 26, ItemCategory.ITEM, ItemClassification.useful),
+    "Happy Stick":  ItemData(loonyland_base_id + 27, ItemCategory.ITEM, ItemClassification.progression),
+    "Bat Statue":   ItemData(loonyland_base_id + 28, ItemCategory.ITEM, ItemClassification.progression),
+    "Lantern":      ItemData(loonyland_base_id + 29, ItemCategory.ITEM, ItemClassification.progression),
+    "Reflect":      ItemData(loonyland_base_id + 30, ItemCategory.ITEM, ItemClassification.useful),
+    "Silver Sling": ItemData(loonyland_base_id + 31, ItemCategory.ITEM, ItemClassification.progression)
+    #TODO add cheats
+    #TODO filler
+    #TODO traps
+    #TODO event
     }
