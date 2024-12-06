082dda17dda2a23cda7f9841e24ca50c674bfde6
AutomaticFrenzy <beerensd@gmail.com>
2024-12-06 16:45:39 -0500

    forgot to save

--- worlds/loonyland/Items.py
+++ worlds/loonyland/Items.py
@@ -36,35 +36,36 @@ item_frequencies = {
 }    
     
 item_table: Dict[str, ItemData] = {
-    "Heart": ItemData(ItemCategory.ITEM, ItemClassification.useful
-    "Lightning": ItemCategory.ITEM, ItemClassification.useful
-    "Arrow": ItemCategory.ITEM, ItemClassification.useful
-    "Pants": ItemCategory.ITEM, ItemClassification.useful
-    "Mushroom": ItemCategory.ITEM, ItemClassification.progression
-    "Orb": ItemCategory.ITEM, ItemClassification.progression
-    "Bombs": ItemCategory.ITEM, ItemClassification.progression
-    "Shock Wand": ItemCategory.ITEM, ItemClassification.progression
-    "Ice spear": ItemCategory.ITEM,  ItemClassification.progression
-    "Cactus": ItemCategory.ITEM, ItemClassification.progression
-    "Boomerang": ItemCategory.ITEM, ItemClassification.progression
-    "Whoopee": ItemCategory.ITEM, ItemClassification.progression
-    "Hot Pants": ItemCategory.ITEM, ItemClassification.progression
-    "Skull Key": ItemCategory.ITEM, ItemClassification.progression
-    "Bat Key": ItemCategory.ITEM, ItemClassification.progression
-    "Pumpkin Key": ItemCategory.ITEM, ItemClassification.progression
-    "Boots": ItemCategory.ITEM, ItemClassification.progression
-    "Stick": ItemCategory.ITEM, ItemClassification.progression
-    "Fertilizer": ItemCategory.ITEM, ItemClassification.progression
-    "Silver": ItemCategory.ITEM, ItemClassification.progression
-    "Doom Daisy": ItemCategory.ITEM, ItemClassification.progression
-    "Ghost Potion": ItemCategory.ITEM, ItemClassification.progression
-    "Vamp Statue": ItemCategory.ITEM,  ItemClassification.progression
-    "Cat": ItemCategory.ITEM, ItemClassification.progression
-    "Big Gem": ItemCategory.ITEM, ItemClassification.progression
-    "Zombie Reward": ItemCategory.ITEM, ItemClassification.filler
-    "3 way": ItemCategory.ITEM, ItemClassification.useful
-    "Happy Stick": ItemCategory.ITEM, ItemClassification.progression
-    "Bat Statue": ItemCategory.ITEM, ItemClassification.progression
-    "Lantern": ItemCategory.ITEM, ItemClassification.progression
-    "Reflect": ItemCategory.ITEM, ItemClassification.useful
-    "Silver Sling": ItemCategory.ITEM, ItemClassification.progression
+    "Heart": ItemData(ItemCategory.ITEM, ItemClassification.useful),
+    "Lightning": ItemData(ItemCategory.ITEM, ItemClassification.useful),
+    "Arrow": ItemData(ItemCategory.ITEM, ItemClassification.useful),
+    "Pants": ItemData(ItemCategory.ITEM, ItemClassification.useful),
+    "Mushroom": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Orb": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Bombs": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Shock Wand": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Ice spear": ItemData(ItemCategory.ITEM,  ItemClassification.progression),
+    "Cactus": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Boomerang": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Whoopee": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Hot Pants": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Skull Key": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Bat Key": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Pumpkin Key": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Boots": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Stick": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Fertilizer": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Silver": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Doom Daisy": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Ghost Potion": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Vamp Statue": ItemData(ItemCategory.ITEM,  ItemClassification.progression),
+    "Cat": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Big Gem": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Zombie Reward": ItemData(ItemCategory.ITEM, ItemClassification.filler),
+    "3 way": ItemData(ItemCategory.ITEM, ItemClassification.useful),
+    "Happy Stick": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Bat Statue": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Lantern": ItemData(ItemCategory.ITEM, ItemClassification.progression),
+    "Reflect": ItemData(ItemCategory.ITEM, ItemClassification.useful),
+    "Silver Sling": ItemData(ItemCategory.ITEM, ItemClassification.progression)
+    }
