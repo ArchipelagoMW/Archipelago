14be2c2003ddfc7a308b3438e8bf06c83705b209
Jouramie <16137441+Jouramie@users.noreply.github.com>
2024-12-14 14:41:27 -0500

    rename test cuz I named them too long

--- worlds/stardew_valley/test/content/feature/TestToolProgression.py
+++ worlds/stardew_valley/test/content/feature/TestToolProgression.py
@@ -7,7 +7,7 @@ from ....strings.tool_names import Tool
 
 class TestToolDistribution(unittest.TestCase):
 
-    def test_given_vanilla_tool_progression_when_create_tool_progression_feature_then_only_one_scythe_is_randomized(self):
+    def test_given_vanilla_tool_progression_when_create_feature_then_only_one_scythe_is_randomized(self):
         tool_progression = ToolProgression(ToolProgression.option_vanilla)
         skill_progression = SkillProgression.from_text("random")
 
@@ -17,9 +17,9 @@ class TestToolDistribution(unittest.TestCase):
             Tool.scythe: 1,
         })
 
-    def test_given_progressive_tool_when_create_tool_progression_feature_then_all_tool_upgrades_are_randomized(self):
+    def test_given_progressive_tool_when_create_feature_then_all_tool_upgrades_are_randomized(self):
         tool_progression = ToolProgression(ToolProgression.option_progressive)
-        skill_progression = SkillProgression(SkillProgression.option_vanilla)
+        skill_progression = SkillProgression(SkillProgression.option_progressive)
 
         feature = choose_tool_progression(tool_progression, skill_progression)
 
@@ -34,8 +34,7 @@ class TestToolDistribution(unittest.TestCase):
             Tool.fishing_rod: 4,
         })
 
-    def test_given_progressive_tool_and_skill_masteries_randomized_when_create_tool_progression_feature_then_additional_scythe_and_fishing_rod_are_randomized(
-            self):
+    def test_given_progressive_tool_and_skill_masteries_when_create_feature_then_additional_scythe_and_fishing_rod_are_randomized(self):
         tool_progression = ToolProgression(ToolProgression.option_progressive)
         skill_progression = SkillProgression(SkillProgression.option_progressive_with_masteries)
 
