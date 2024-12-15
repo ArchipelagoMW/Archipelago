4bad19b452c223e525abd8b81c09c989f584a7e5
AutomaticFrenzy <beerensd@gmail.com>
2024-12-15 14:36:11 -0500

    add difficulty as an option

--- worlds/loonyland/Options.py
+++ worlds/loonyland/Options.py
@@ -4,13 +4,30 @@ from Options import Choice, PerGameCommonOptions
 
 
 class WinCondition(Choice):
-    """Sets Win Condition."""
+    """Evilizer: Beat the final boss
+    NOT IMPLEMENTED: 40 badges: get all 40 badge locs
+    NOT IMPLEMENTED: normal_remix: Beat the final boss in normal and remix"""
+
     display_name = "Win Condition"
     option_evilizer = 0
     option_40badges = 1
+    option_normal_remix = 2
     default = 0
+
+class Difficulty(Choice):
+    """Difficulty Setting"""
+    display_name = "Difficulty"
+    option_beginner = 0
+    option_normal = 1
+    option_hard = 2
+    option_challenge = 3
+    option_mad = 4
+    option_loony = 5
+    default = 2
 
 
 @dataclass
 class LoonylandOptions(PerGameCommonOptions):
     win_condition: WinCondition
+    difficulty: Difficulty
+
