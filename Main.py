94f4227f3f597002b0004ec9f047af02d6fc903e
Exempt-Medic <ExemptMedic@Gmail.com>
2024-12-24 13:08:52 -0500

    Change timing of local/non-local clearing

--- Main.py
+++ Main.py
@@ -84,6 +84,11 @@ def main(args, seed=None, baked_server_options: Optional[Dict[str, object]] = No
     if not args.skip_output:
         AutoWorld.call_stage(multiworld, "assert_generate")
 
+    # Clear non-applicable local and non-local items.
+    if multiworld.players == 1:
+        multiworld.worlds[1].options.non_local_items.value = set()
+        multiworld.worlds[1].options.local_items.value = set()
+
     AutoWorld.call_all(multiworld, "generate_early")
 
     logger.info('')
@@ -145,10 +150,7 @@ def main(args, seed=None, baked_server_options: Optional[Dict[str, object]] = No
     # Set local and non-local item rules.
     if multiworld.players > 1:
         locality_rules(multiworld)
-    else:
-        multiworld.worlds[1].options.non_local_items.value = set()
-        multiworld.worlds[1].options.local_items.value = set()
-    
+
     AutoWorld.call_all(multiworld, "generate_basic")
 
     # remove starting inventory from pool items.
