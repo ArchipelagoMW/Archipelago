d517f0a4d623d15c422a9c42668b86aadad9d782
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-12-26 02:04:49 +0100

    option docstring adjustment

--- worlds/witness/options.py
+++ worlds/witness/options.py
@@ -145,7 +145,7 @@ class ShuffleEnvironmentalPuzzles(Choice):
 
 class EasterEggHunt(Choice):
     """
-    Adds up to 100 Easter Eggs to the game, placed by NewSoupVi, Exempt-Medic, hatkirby, Scipio, and Rever.
+    Adds up to 120 Easter Eggs to the game, placed by NewSoupVi, Exempt-Medic, hatkirby, Scipio, and Rever.
     These can be collected by simply clicking on them.
 
     Each consecutive check will logically expect 5 Easter Eggs to be collected.
@@ -155,7 +155,7 @@ class EasterEggHunt(Choice):
     - Hard: For every 4 Easter Eggs collected, an item is sent
     - Very Hard: For every 5 Easter Eggs collected, an item is sent (You are expected to find every Easter Egg)
 
-    On Easy, there will still be a location check for "100 Easter Eggs Collected" (provided there aren't any unreachable eggs due to options like shuffle_postgame: false), but all checks after 40 Eggs will be excluded.
+    On Easy, there will still be a location check for "120 Easter Eggs Collected" (provided there aren't any unreachable eggs due to options like shuffle_postgame: false), but all checks after 48 Eggs will be excluded.
     """
 
     visibility = Visibility.all if datetime.now().month == 4 else Visibility.none
