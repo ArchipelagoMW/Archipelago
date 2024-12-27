94e78dd232f95a5bb448c694fd2e3116e2566c80
silent-destroyer <osilentdestroyer@gmail.com>
2024-12-27 16:00:42 -0500

    Update option value passthrough

--- worlds/tunic/__init__.py
+++ worlds/tunic/__init__.py
@@ -127,7 +127,7 @@ class TunicWorld(World):
                 self.options.lanternless.value = passthrough["lanternless"]
                 self.options.maskless.value = passthrough["maskless"]
                 self.options.hexagon_quest.value = passthrough["hexagon_quest"]
-                self.options.hexagon_quest_ability_type.value = passthrough["hexagon_quest_ability_type"]
+                self.options.hexagon_quest_ability_type.value = passthrough.get("hexagon_quest_ability_type", 0)
                 self.options.entrance_rando.value = passthrough["entrance_rando"]
                 self.options.shuffle_ladders.value = passthrough["shuffle_ladders"]
                 self.options.fixed_shop.value = self.options.fixed_shop.option_false
