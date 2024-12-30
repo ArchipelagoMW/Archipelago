9a7acd9a95be75017fd4522cf638c0940ad89752
Nikkilite <Niko6697n@gmail.com>
2024-12-30 03:22:41 +0100

    Add correct base id and change name of royalArt

--- worlds/cat_quest/Locations.py
+++ worlds/cat_quest/Locations.py
@@ -4,7 +4,9 @@ class Location(TypedDict):
     id: int
     inGameId: str
     title: str
-    royalArt: str
+    art: str
+
+base_id = 123500
 
 locations: List[Location] = [
     # Main Quests
@@ -12,73 +14,73 @@ locations: List[Location] = [
         "id": base_id + 1,
         "inGameId": "MainQuest_001",
         "title": "A Whole New World",
-        "royalArt": "none"
+        "art": "none"
     },
     {
         "id": base_id + 2,
         "inGameId": "MainQuest_002",
         "title": "The Catpital (1)",
-        "royalArt": "none"
+        "art": "none"
     },
     {
         "id": base_id + 3,
         "inGameId": "MainQuest_003",
         "title": "The Catpital (2)",
-        "royalArt": "none"
+        "art": "none"
     },
     {
         "id": base_id + 4,
         "inGameId": "MainQuest_004",
         "title": "The Dragon and the Cat",
-        "royalArt": "none"
+        "art": "none"
     },
     {
         "id": base_id + 5,
         "inGameId": "MainQuest_005",
         "title": "The Old Ruins",
-        "royalArt": "none"
+        "art": "none"
     },
     {
         "id": base_id + 6,
         "inGameId": "MainQuest_006",
         "title": "Of Sea and Rock (1)",
-        "royalArt": "none"
+        "art": "none"
     },
     {
         "id": base_id + 7,
         "inGameId": "MainQuest_007",
         "title": "Of Sea and Rock (2)",
-        "royalArt": "none"
+        "art": "none"
     },
     {
         "id": base_id + 8,
         "inGameId": "MainQuest_008",
         "title": "The Old Friend",
-        "royalArt": "none"
+        "art": "none"
     },
     {
         "id": base_id + 9,
         "inGameId": "MainQuest_009",
         "title": "The Dragonblood",
-        "royalArt": "none"
+        "art": "none"
     },
     {
         "id": base_id + 10,
         "inGameId": "MainQuest_010",
         "title": "The Dragonsbane",
-        "royalArt": "none"
+        "art": "none"
     },
     {
         "id": base_id + 11,
         "inGameId": "MainQuest_011",
         "title": "The Dragons Void",
-        "royalArt": "none"
+        "art": "none"
     },
     {
         "id": base_id + 12,
         "inGameId": "MainQuest_012",
         "title": "Epilogue",
-        "royalArt": "none"
+        "art": "none"
     },
 
     # Sanctuary Quests
@@ -86,456 +88,430 @@ locations: List[Location] = [
         "id": base_id + 13,
         "inGameId": "sanctuary_one",
         "title": "I. The Strange Charm",
-        "royalArt": "none"
+        "art": "none"
     },
     {
         "id": base_id + 14,
         "inGameId": "sanctuary_two",
         "title": "II. The Escape",
-        "royalArt": "none"
+        "art": "none"
     },
     {
         "id": base_id + 15,
         "inGameId": "sanctuary_three",
         "title": "III. The Sacrifice",
-        "royalArt": "none"
+        "art": "none"
     },
     {
         "id": base_id + 16,
         "inGameId": "sanctuary_four",
         "title": "IV. The Sanctuary",
-        "royalArt": "none"
+        "art": "none"
     },
 
     # Great Spirit Quests
     {
-        "id": base_id + 19,
+        "id": base_id + 17,
         "inGameId": "greatspirit_one",
         "title": "I. The Servant of God",
-        "royalArt": "none"
+        "art": "none"
+    },
+    {
+        "id": base_id + 18,
+        "inGameId": "greatspirit_two",
+        "title": "II. The Offerings",
+        "art": "none"
+    },
+    {
+        "id": base_id + 19,
+        "inGameId": "greatspirit_three",
+        "title": "III. The Prayer",
+        "art": "none"
     },
     {
         "id": base_id + 20,
-        "inGameId": "greatspirit_two",
-        "title": "II. The Offerings",
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 21,
-        "inGameId": "greatspirit_three",
-        "title": "III. The Prayer",
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 22,
         "inGameId": "greatspirit_four",
         "title": "IV. The Godcat, Mauth",
-        "royalArt": "none"
+        "art": "none"
     },
 
     # Kitmas Quests
     {
-        "id": base_id + 23, 
+        "id": base_id + 21, 
         "inGameId": "kitmas_one", 
         "title": "The First Day of Kitmas", 
-        "royalArt": "none"
+        "art": "none"
+    },
+    {
+        "id": base_id + 22, 
+        "inGameId": "kitmas_two", 
+        "title": "The Second Day of Kitmas", 
+        "art": "none"
+    },
+    {
+        "id": base_id + 23, 
+        "inGameId": "kitmas_three", 
+        "title": "The Third Day of Kitmas", 
+        "art": "none"
     },
     {
         "id": base_id + 24, 
-        "inGameId": "kitmas_two", 
-        "title": "The Second Day of Kitmas", 
-        "royalArt": "none"
+        "inGameId": "kitmas_four", 
+        "title": "The Fourth Day of Kitmas", 
+        "art": "none"
     },
     {
         "id": base_id + 25, 
-        "inGameId": "kitmas_three", 
-        "title": "The Third Day of Kitmas", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 26, 
-        "inGameId": "kitmas_four", 
-        "title": "The Fourth Day of Kitmas", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 27, 
         "inGameId": "kitmas_five", 
         "title": "Mewry Kitmas!", 
-        "royalArt": "none"
+        "art": "none"
     },
 
     # Missing Quests
     {
-        "id": base_id + 28, 
+        "id": base_id + 26, 
         "inGameId": "missing_one", 
         "title": "I. The Missing Soldiers", 
-        "royalArt": "none"
+        "art": "none"
     },
     {
-        "id": base_id + 29, 
+        "id": base_id + 27, 
         "inGameId": "missing_two", 
         "title": "II. The Rescue", 
-        "royalArt": "none"
+        "art": "none"
     },
 
     # Faded King Quests
     {
-        "id": base_id + 30, 
+        "id": base_id + 28, 
         "inGameId": "faded_king_one", 
         "title": "I. The King's Mage", 
-        "royalArt": "none"
+        "art": "none"
+    },
+    {
+        "id": base_id + 29, 
+        "inGameId": "faded_king_three", 
+        "title": "II. The Mage Search", 
+        "art": "none"
+    },
+    {
+        "id": base_id + 30, 
+        "inGameId": "faded_king_four", 
+        "title": "III. The Spirits", 
+        "art": "none"
     },
     {
         "id": base_id + 31, 
-        "inGameId": "faded_king_three", 
-        "title": "II. The Mage Search", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 32, 
-        "inGameId": "faded_king_four", 
-        "title": "III. The Spirits", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 33, 
         "inGameId": "faded_king_five", 
         "title": "IV. The Lion King", 
-        "royalArt": "none"
+        "art": "none"
     },
 
     # East Quests
     {
-        "id": base_id + 34, 
+        "id": base_id + 32, 
         "inGameId": "east_one", 
         "title": "I. The East Suspicion", 
-        "royalArt": "none"
+        "art": "none"
+    },
+    {
+        "id": base_id + 33, 
+        "inGameId": "east_two", 
+        "title": "II. The East Shipment", 
+        "art": "none"
+    },
+    {
+        "id": base_id + 34, 
+        "inGameId": "east_three", 
+        "title": "III. The East Escort", 
+        "art": "none"
     },
     {
         "id": base_id + 35, 
-        "inGameId": "east_two", 
-        "title": "II. The East Shipment", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 36, 
-        "inGameId": "east_three", 
-        "title": "III. The East Escort", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 37, 
         "inGameId": "east_four", 
         "title": "IV. The East Catfrontation", 
-        "royalArt": "none"
+        "art": "none"
     },
 
     # Meat Quests
     {
-        "id": base_id + 38, 
+        "id": base_id + 36, 
         "inGameId": "meatmeatmeat", 
         "title": "I. Meat Meat Meat!", 
-        "royalArt": "none"
+        "art": "none"
+    },
+    {
+        "id": base_id + 37, 
+        "inGameId": "red_riding_kitty", 
+        "title": "II. Meatmeatmeatmeat!!", 
+        "art": "none"
+    },
+    {
+        "id": base_id + 38, 
+        "inGameId": "distraction", 
+        "title": "III. Meat Disposal Crew", 
+        "art": "none"
     },
     {
         "id": base_id + 39, 
-        "inGameId": "red_riding_kitty", 
-        "title": "II. Meatmeatmeatmeat!!", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 40, 
-        "inGameId": "distraction", 
-        "title": "III. Meat Disposal Crew", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 41, 
         "inGameId": "crafty_merchant", 
         "title": "IV. The Meat Seller", 
-        "royalArt": "none"
+        "art": "none"
     },
 
     # Whisperer Quests
     {
-        "id": base_id + 42, 
+        "id": base_id + 40, 
         "inGameId": "the_whisperer_one", 
         "title": "I. The Growling Peasant", 
-        "royalArt": "none"
+        "art": "none"
+    },
+    {
+        "id": base_id + 41, 
+        "inGameId": "the_whisperer_two", 
+        "title": "II. The Catnip Ritual", 
+        "art": "none"
+    },
+    {
+        "id": base_id + 42, 
+        "inGameId": "the_whisperer_three", 
+        "title": "III. The Circle", 
+        "art": "none"
     },
     {
         "id": base_id + 43, 
-        "inGameId": "the_whisperer_two", 
-        "title": "II. The Catnip Ritual", 
-        "royalArt": "none"
+        "inGameId": "the_whisperer_four", 
+        "title": "IV. Fur-reedom!", 
+        "art": "none"
     },
     {
         "id": base_id + 44, 
-        "inGameId": "the_whisperer_three", 
-        "title": "III. The Circle", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 45, 
-        "inGameId": "the_whisperer_four", 
-        "title": "IV. Fur-reedom!", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 46, 
         "inGameId": "the_whisperer_five", 
         "title": "V. The Whispurrer", 
-        "royalArt": "none"
+        "art": "none"
     },
 
     # Waters Quests
     {
-        "id": base_id + 47, 
+        "id": base_id + 45, 
         "inGameId": "waters_one", 
         "title": "I. Path to Water Walking", 
-        "royalArt": "none"
+        "art": "none"
+    },
+    {
+        "id": base_id + 46, 
+        "inGameId": "waters_three", 
+        "title": "II. The Fusion", 
+        "art": "none"
+    },
+    {
+        "id": base_id + 47, 
+        "inGameId": "waters_four", 
+        "title": "III. Rogue Mages", 
+        "art": "none"
     },
     {
         "id": base_id + 48, 
-        "inGameId": "waters_three", 
-        "title": "II. The Fusion", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 49, 
-        "inGameId": "waters_four", 
-        "title": "III. Rogue Mages", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 50, 
         "inGameId": "waters_five", 
         "title": "IV. The Miracle", 
-        "royalArt": "none"
-    },
-
-    # Mage vs Science Quests
-    {#?
-        "id": base_id + 51, 
-        "inGameId": "magevscience_one", 
-        "title": "I. The Peace Offering", 
-        "royalArt": "none"
-    },
-    {#?
-        "id": base_id + 52, 
-        "inGameId": "magevscience_two", 
-        "title": "II. The Blame", 
-        "royalArt": "none"
-    },
-    {#?
-        "id": base_id + 53, 
-        "inGameId": "magevscience_three", 
-        "title": "III. Road to Recovery", 
-        "royalArt": "none"
+        "art": "none"
     },
 
     # West Quests
     {
-        "id": base_id + 54, 
+        "id": base_id + 49, 
         "inGameId": "west_one", 
         "title": "I. The West Investigation", 
-        "royalArt": "none"
+        "art": "none"
+    },
+    {
+        "id": base_id + 50, 
+        "inGameId": "west_two", 
+        "title": "II. The West Heist", 
+        "art": "none"
+    },
+    {
+        "id": base_id + 51, 
+        "inGameId": "west_three", 
+        "title": "III. The Magic Lock", 
+        "art": "none"
+    },
+    {
+        "id": base_id + 52, 
+        "inGameId": "west_four", 
+        "title": "IV. The Revelation", 
+        "art": "none"
+    },
+
+    # Flight Quests
+    {
+        "id": base_id + 53, 
+        "inGameId": "magesold_one", 
+        "title": "I. The Flying Bush", 
+        "art": "none"
+    },
+    {
+        "id": base_id + 54, 
+        "inGameId": "magesold_two", 
+        "title": "II. The Magic Tree", 
+        "art": "none"
     },
     {
         "id": base_id + 55, 
-        "inGameId": "west_two", 
-        "title": "II. The West Heist", 
-        "royalArt": "none"
+        "inGameId": "magesold_three", 
+        "title": "III. The Cult", 
+        "art": "none"
     },
     {
         "id": base_id + 56, 
-        "inGameId": "west_three", 
-        "title": "III. The Magic Lock", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 57, 
-        "inGameId": "west_four", 
-        "title": "IV. The Revelation", 
-        "royalArt": "none"
-    },
-
-    # Flíght Quests
-    {
-        "id": base_id + 58, 
-        "inGameId": "magesold_one", 
-        "title": "I. The Flying Bush", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 59, 
-        "inGameId": "magesold_two", 
-        "title": "II. The Magic Tree", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 60, 
-        "inGameId": "magesold_three", 
-        "title": "III. The Cult", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 61, 
         "inGameId": "magesold_four", 
         "title": "IV. Dragonflight", 
-        "royalArt": "none"
+        "art": "none"
     },
 
     # Dark Past Quests
     {
-        "id": base_id + 62, 
+        "id": base_id + 57, 
         "inGameId": "darkpast_one", 
         "title": "I. The Missing Pages", 
-        "royalArt": "none"
+        "art": "none"
     },
     {
-        "id": base_id + 63, 
+        "id": base_id + 58, 
         "inGameId": "darkpast_two", 
         "title": "II. The Protected Pages", 
-        "royalArt": "none"
+        "art": "none"
     },
     {
-        "id": base_id + 64, 
+        "id": base_id + 59, 
         "inGameId": "darkpast_three", 
         "title": "III. The Runaway Archeologist", 
-        "royalArt": "none"
+        "art": "none"
     },
     {
-        "id": base_id + 65, 
+        "id": base_id + 60, 
         "inGameId": "darkpast_four", 
         "title": "IV. The Treasure", 
-        "royalArt": "none"
+        "art": "none"
     },
 
     # Blacksmith Quests
     {
-        "id": base_id + 66, 
+        "id": base_id + 61, 
         "inGameId": "blacksmith_assistance", 
         "title": "I. Blacksmith Assistance", 
-        "royalArt": "none"
+        "art": "none"
     },
     {
-        "id": base_id + 67, 
+        "id": base_id + 62, 
         "inGameId": "blacksmith_apprentice", 
         "title": "II. Blacksmith Apprentice", 
-        "royalArt": "none"
+        "art": "none"
     },
     {
-        "id": base_id + 68, 
+        "id": base_id + 63, 
         "inGameId": "blacksmith_journeyman", 
         "title": "III. Blacksmith Journeyman", 
-        "royalArt": "none"
+        "art": "none"
     },
     {
-        "id": base_id + 69, 
+        "id": base_id + 64, 
         "inGameId": "blacksmith_master", 
         "title": "IV. Blacksmith Master", 
-        "royalArt": "none"
+        "art": "none"
     },
 
     # Misc Quests
     {#?
-        "id": base_id + 70, 
+        "id": base_id + 65, 
         "inGameId": "wyvern_attack", 
         "title": "The Dragon Worshippers", 
-        "royalArt": "none"
+        "art": "none"
+    },
+    {
+        "id": base_id + 66, 
+        "inGameId": "the_heirloom", 
+        "title": "The Heirloom Armor", 
+        "art": "none"
+    },
+    {
+        "id": base_id + 67, 
+        "inGameId": "furbidden_mystery", 
+        "title": "The Furbidden History", 
+        "art": "none"
+    },
+    {
+        "id": base_id + 68, 
+        "inGameId": "golden_key", 
+        "title": "The Golden Key", 
+        "art": "none"
+    },
+    {
+        "id": base_id + 69, 
+        "inGameId": "ultimate_dragonsbane", 
+        "title": "The Ultimate Dragonsbane", 
+        "art": "none"
+    },
+    {
+        "id": base_id + 70, 
+        "inGameId": "pawtato_one", 
+        "title": "The Pawtato Mystery", 
+        "art": "none"
     },
     {
         "id": base_id + 71, 
-        "inGameId": "the_heirloom", 
-        "title": "The Heirloom Armor", 
-        "royalArt": "none"
+        "inGameId": "advertising_one", 
+        "title": "Everyone's invited!", 
+        "art": "none"
     },
     {
         "id": base_id + 72, 
-        "inGameId": "furbidden_mystery", 
-        "title": "The Furbidden History", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 73, 
-        "inGameId": "golden_key", 
-        "title": "The Golden Key", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 74, 
-        "inGameId": "ultimate_dragonsbane", 
-        "title": "The Ultimate Dragonsbane", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 75, 
-        "inGameId": "pawtato_one", 
-        "title": "The Pawtato Mystery", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 76, 
-        "inGameId": "advertising_one", 
-        "title": "Everyone's invited!", 
-        "royalArt": "none"
-    },
-    {
-        "id": base_id + 77, 
         "inGameId": "slashy_one", 
         "title": "The Forgotten Hero", 
-        "royalArt": "none"
-    },
-    {#?
-        "id": base_id + 78, 
-        "inGameId": "TutorialQuest_001", 
-        "title": "Beginnings", 
-        "royalArt": "none"
+        "art": "none"
     },
 
     # Catnip Cure Quests
     {
-        "id": base_id + 79, 
+        "id": base_id + 73, 
         "inGameId": "catnip_cure_A", 
         "title": "I. The Catnip Cure", 
-        "royalArt": "none"
+        "art": "none"
     },
     {
-        "id": base_id + 80, 
+        "id": base_id + 74, 
         "inGameId": "catnip_cure_B", 
         "title": "I. The Catnip Cure...again", 
-        "royalArt": "none"
+        "art": "none"
     },
     {
-        "id": base_id + 81, 
+        "id": base_id + 75, 
         "inGameId": "knightmare_A", 
         "title": "II. The Knightmare", 
-        "royalArt": "none"
+        "art": "none"
     },
     {
-        "id": base_id + 82, 
+        "id": base_id + 76, 
         "inGameId": "knightmare_B", 
         "title": "II. The Knightmare... again", 
-        "royalArt": "none"
+        "art": "none"
     },
     {
-        "id": base_id + 83, 
+        "id": base_id + 77, 
         "inGameId": "book_A", 
         "title": "III. The Book", 
-        "royalArt": "none"
+        "art": "none"
     },
     {
-        "id": base_id + 84, 
+        "id": base_id + 78, 
         "inGameId": "book_B", 
         "title": "III. The Book...again", 
-        "royalArt": "none"
+        "art": "none"
     },
     {
-        "id": base_id + 85, 
+        "id": base_id + 79, 
         "inGameId": "resolution_A", 
         "title": "IV. The Twin Resolution", 
-        "royalArt": "none"
+        "art": "none"
     }
 ]
