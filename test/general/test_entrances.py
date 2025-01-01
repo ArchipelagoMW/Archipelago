ced19aa466dbb617127678284aab90957aaeed09
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2025-01-01 22:31:26 +0100

    Rewrite test a bit

--- test/general/test_entrances.py
+++ test/general/test_entrances.py
@@ -1,11 +1,18 @@
 import unittest
-from worlds.AutoWorld import AutoWorldRegister, call_all
+from worlds.AutoWorld import AutoWorldRegister, call_all, World
 from . import setup_solo_multiworld
 
 
 class TestBase(unittest.TestCase):
     def test_entrance_connection_steps(self):
         """Tests that Regions and Locations aren't created after `create_items`."""
+        def get_entrance_name_to_source_and_target_dict(world: World):
+            return [
+                (entrance.name, entrance.parent_region.name, entrance.target.name)
+                for entrance in world.get_entrances()
+            ]
+
+
         gen_steps = ("generate_early", "create_regions", "create_items", "set_rules", "connect_entrances")
         additional_steps = ("generate_basic", "pre_fill")
 
@@ -13,13 +20,12 @@ class TestBase(unittest.TestCase):
             with self.subTest("Game", game_name=game_name):
                 multiworld = setup_solo_multiworld(world_type, gen_steps)
 
-                entrances = multiworld.get_entrances()
+                original_entrances = get_entrance_name_to_source_and_target_dict()
 
                 for step in additional_steps:
                     with self.subTest("Step", step=step):
                         call_all(multiworld, "steps")
+                        entrances_after_step = get_entrance_name_to_source_and_target_dict
                         self.assertEqual(
-                            entrances,
-                            multiworld.get_entrances(),
-                            f"{game_name} modified entrances during {step}"
+                            original_entrances, entrances_after_step, f"{game_name} modified entrances during {step}"
                         )
