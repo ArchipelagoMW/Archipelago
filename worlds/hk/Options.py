7d506990f57d22766ff07eadd0dce9bceba3abc6
qwint <qwint.42@gmail.com>
2025-01-06 09:35:12 -0500

    HK: add location counts to option descriptions (#4083)

--- worlds/hk/Options.py
+++ worlds/hk/Options.py
@@ -132,7 +132,13 @@ splitter_pattern = re.compile(r'(?<!^)(?=[A-Z])')
 for option_name, option_data in pool_options.items():
     extra_data = {"__module__": __name__, "items": option_data[0], "locations": option_data[1]}
     if option_name in option_docstrings:
-        extra_data["__doc__"] = option_docstrings[option_name]
+        if option_name == "RandomizeFocus":
+            # pool options for focus are just lying
+            count = 1
+        else:
+            count = len([loc for loc in option_data[1] if loc != "Start"])
+        extra_data["__doc__"] = option_docstrings[option_name] + \
+            f"\n    This option adds approximately {count} location{'s' if count != 1 else ''}."
     if option_name in default_on:
         option = type(option_name, (DefaultOnToggle,), extra_data)
     else:
