955080b64ae420601278c803cf4f20c05a120291
Nikkilite <Niko6697n@gmail.com>
2025-01-16 23:03:22 +0100

    Changed values of filler items

--- worlds/cat_quest/Items.py
+++ worlds/cat_quest/Items.py
@@ -28,19 +28,23 @@ items: List[Item] = [
     {"name": "Golden Key", "id": base_id + 9, "inGameId": "key.golden", "count": 1, "classification": ItemClassification.useful},
 
     # Gold
-    {"name": "50 Gold", "id": base_id + 10, "inGameId": "gold.50", "count": 2, "classification": ItemClassification.filler},
-    {"name": "200 Gold", "id": base_id + 11, "inGameId": "gold.200", "count": 3, "classification": ItemClassification.filler},
-    {"name": "500 Gold", "id": base_id + 12, "inGameId": "gold.500", "count": 5, "classification": ItemClassification.filler},
-    {"name": "750 Gold", "id": base_id + 13, "inGameId": "gold.750", "count": 2, "classification": ItemClassification.filler},
-    {"name": "1000 Gold", "id": base_id + 14, "inGameId": "gold.1000", "count": 1, "classification": ItemClassification.filler},
+    {"name": "50 Gold", "id": base_id + 10, "inGameId": "gold.50", "count": 1, "classification": ItemClassification.filler},
+    {"name": "500 Gold", "id": base_id + 12, "inGameId": "gold.500", "count": 3, "classification": ItemClassification.filler},
+    {"name": "750 Gold", "id": base_id + 13, "inGameId": "gold.750", "count": 5, "classification": ItemClassification.filler},
+    {"name": "1000 Gold", "id": base_id + 11, "inGameId": "gold.1000", "count": 7, "classification": ItemClassification.filler},
+    {"name": "5000 Gold", "id": base_id + 14, "inGameId": "gold.5000", "count": 2, "classification": ItemClassification.filler},
+    {"name": "10K Gold", "id": base_id + 14, "inGameId": "gold.10000", "count": 1, "classification": ItemClassification.filler},
 
     # Exp
-    {"name": "500 Exp", "id": base_id + 15, "inGameId": "exp.500", "count": 5, "classification": ItemClassification.filler},
-    {"name": "1000 Exp", "id": base_id + 16, "inGameId": "exp.1000", "count": 5, "classification": ItemClassification.filler},
+    {"name": "500 Exp", "id": base_id + 21, "inGameId": "exp.500", "count": 0, "classification": ItemClassification.filler},
+    {"name": "1000 Exp", "id": base_id + 21, "inGameId": "exp.1000", "count": 0, "classification": ItemClassification.filler},
     {"name": "5000 Exp", "id": base_id + 17, "inGameId": "exp.5000", "count": 5, "classification": ItemClassification.filler},
     {"name": "7500 Exp", "id": base_id + 18, "inGameId": "exp.7500", "count": 5, "classification": ItemClassification.filler},
-    {"name": "10000 Exp", "id": base_id + 19, "inGameId": "exp.10000", "count": 5, "classification": ItemClassification.filler},
-    {"name": "20000 Exp", "id": base_id + 20, "inGameId": "exp.20000", "count": 2, "classification": ItemClassification.filler},
+    {"name": "10K Exp", "id": base_id + 19, "inGameId": "exp.10000", "count": 4, "classification": ItemClassification.filler},
+    {"name": "20K Exp", "id": base_id + 20, "inGameId": "exp.20000", "count": 4, "classification": ItemClassification.filler},
+    {"name": "50K Exp", "id": base_id + 20, "inGameId": "exp.50000", "count": 2, "classification": ItemClassification.filler},
+    {"name": "100K Exp", "id": base_id + 20, "inGameId": "exp.100000", "count": 1, "classification": ItemClassification.filler},
+]
 
     # Balancing filler
     {"name": "500 Exp", "id": base_id + 21, "inGameId": "exp.500", "count": 0, "classification": ItemClassification.filler},
