3029abf4b3a64dc6bc6f88e069c9352b782dc1a5
unknown <damasta21th@aol.com>
2025-01-18 11:12:09 -0500

    Add custom deathlink description

--- worlds/checksfinder/Options.py
+++ worlds/checksfinder/Options.py
@@ -2,7 +2,12 @@ import typing
 from dataclasses import dataclass
 from Options import DeathLink, PerGameCommonOptions
 
+# Python hates it when you try to make an empty ChecksFinderOptions.
+# Whether you are inheriting from PerGameCommonOptions or PerGameCommonOptions + DeathLinkMixin.
+# Until ChecksFinder has a unique additional option, DeathLink has to be added as like this instead of DeathLinkMixin.
+class ChecksFinderDeathLink(DeathLink):
+    __doc__ = DeathLink.__doc__ + "\n\n    Good Luck!"
 
 @dataclass
 class ChecksFinderOptions(PerGameCommonOptions):
-    death_link: DeathLink
+    death_link: ChecksFinderDeathLink
