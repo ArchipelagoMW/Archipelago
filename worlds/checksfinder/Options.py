2920885db7ee17edb410195c6dec2ead22c195ef
unknown <damasta21th@aol.com>
2025-01-18 21:36:59 -0500

    clearly i didnt PASS this lesson

--- worlds/checksfinder/Options.py
+++ worlds/checksfinder/Options.py
@@ -1,13 +1,6 @@
-import typing
 from dataclasses import dataclass
-from Options import DeathLink, PerGameCommonOptions
-
-# Python hates it when you try to make an empty ChecksFinderOptions.
-# Whether you are inheriting from PerGameCommonOptions or PerGameCommonOptions + DeathLinkMixin.
-# Until ChecksFinder has a unique additional option, DeathLink has to be added as like this instead of DeathLinkMixin.
-class ChecksFinderDeathLink(DeathLink):
-    __doc__ = DeathLink.__doc__ + "\n\n    Good Luck!"
+from Options import DeathLinkMixin, PerGameCommonOptions
 
 @dataclass
-class ChecksFinderOptions(PerGameCommonOptions):
-    death_link: ChecksFinderDeathLink
+class ChecksFinderOptions(PerGameCommonOptions, DeathLinkMixin):
+    pass
