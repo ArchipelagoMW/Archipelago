34373a86562eee3f52f49c4f85d1c8575d142e00
Snarky <sparkykueken@gmail.com>
2025-01-20 22:26:22 +0100

    Add Instigator Reconstruction

--- worlds/sc2/item/item_names.py
+++ worlds/sc2/item/item_names.py
@@ -718,6 +718,7 @@ ADEPT_PHASE_BULWARK                                     = "Phase Bulwark (Adept)
 STALKER_INSTIGATOR_SLAYER_DISINTEGRATING_PARTICLES      = "Disintegrating Particles (Stalker/Instigator/Slayer)"
 STALKER_INSTIGATOR_SLAYER_PARTICLE_REFLECTION           = "Particle Reflection (Stalker/Instigator/Slayer)"
 INSTIGATOR_BLINK_OVERDRIVE                              = "Blink Overdrive (Instigator)"
+INSTIGATOR_RECONSTRUCTION                               = "Reconstruction (Instigator)"
 DRAGOON_HIGH_IMPACT_PHASE_DISRUPTORS                    = "High Impact Phase Disruptor (Dragoon)"
 DRAGOON_TRILLIC_COMPRESSION_SYSTEM                      = "Trillic Compression System (Dragoon)"
 DRAGOON_SINGULARITY_CHARGE                              = "Singularity Charge (Dragoon)"
