f11834f99054158914c69741b4785bfe696ee9c9
Rob B <computerguy440+gh@gmail.com>
2025-01-24 00:12:39 -0500

    Add 1 "Bundle: Solid Biofuel" to default starting items (for later chainsaw usage or early power gen)

--- worlds/satisfactory/Options.py
+++ worlds/satisfactory/Options.py
@@ -296,6 +296,7 @@ _default_starting_items = _skip_tutorial_starting_items + [
     "Bundle: Iron Ingot",
     "Bundle: Copper Ingot",
     "Bundle: Concrete",
+    "Bundle: Solid Biofuel", # user's choice if they want to hold onto it for chainsaw or burn it right away
     "Building: Blueprint Designer",
     "Expanded Toolbelt",
     "Inflated Pocket Dimension",
