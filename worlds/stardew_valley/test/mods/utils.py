cc73a2aa08c92417795692abd6864a6f4275e0f8
Jouramie <16137441+Jouramie@users.noreply.github.com>
2025-01-26 22:52:30 -0500

    add decorator and use it on existing entrance rando test

---
+++ worlds/stardew_valley/test/mods/utils.py
@@ -0,0 +1,44 @@
+import unittest
+from collections.abc import Collection
+from functools import wraps, partial
+from typing import Type
+
+from worlds.stardew_valley import options
+
+
+def must_test_all_mods(cls: Type[unittest.TestCase] | None = None, /, *, excluded_mods: Collection[str] = None):
+    if cls is None:
+        return partial(must_test_all_mods, excluded_mods=excluded_mods)
+
+    if excluded_mods is None:
+        setattr(cls, "tested_mods", set())
+    else:
+        setattr(cls, "tested_mods", set(excluded_mods))
+
+    orignal_tear_down_class = cls.tearDownClass
+
+    @wraps(cls.tearDownClass)
+    def wrapper():
+        tested_mods: set[str] = getattr(cls, "tested_mods")
+
+        diff = set(options.Mods.valid_keys) - tested_mods
+        if diff:
+            raise AssertionError(f"Mods {diff} were not tested")
+
+        return orignal_tear_down_class()
+
+    cls.tearDownClass = wrapper
+
+    return cls
+
+
+def testing_mod(func=None, /, *, mod: str):
+    if func is None:
+        return partial(testing_mod, mod=mod)
+
+    @wraps(func)
+    def wrapper(self: unittest.TestCase, *args, **kwargs):
+        getattr(self, "tested_mods").add(mod)
+        return func(self, *args, **kwargs)
+
+    return wrapper
