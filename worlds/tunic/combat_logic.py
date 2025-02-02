ab8a53f6cbde737b28fc66481fde4a1c319a865e
Scipio Wright <scipiowright@gmail.com>
2025-02-02 14:06:50 -0500

    Silly static method thing

--- worlds/tunic/combat_logic.py
+++ worlds/tunic/combat_logic.py
@@ -461,7 +461,7 @@ class TunicState(LogicMixin):
         self.tunic_area_combat_state = defaultdict(lambda: defaultdict(lambda: CombatState.unchecked))
 
     @staticmethod
-    def copy_mixin(new_state: CollectionState) -> CollectionState:
+    def copy_mixin(self, new_state: CollectionState) -> CollectionState:
         # the per-player need to reset the combat state when collecting a combat item
         new_state.tunic_need_to_reset_combat_from_collect = defaultdict(lambda: False)
         # the per-player need to reset the combat state when removing a combat item
