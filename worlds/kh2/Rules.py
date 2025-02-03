de0b6f1d8d3959015d392a7f857d253dae3f6e88
JaredWeakStrike <jddoc31@gmail.com>
2025-02-03 17:30:41 -0500

    a

--- worlds/kh2/Rules.py
+++ worlds/kh2/Rules.py
@@ -263,14 +263,15 @@ class KH2WorldRules(KH2Rules):
 
         weapon_region = self.multiworld.get_region(RegionName.Keyblade, self.player)
         for location in weapon_region.locations:
-            exclusion_item = exclusion_table["WeaponSlots"][location.name]
-            add_rule(location, lambda state, e_item=exclusion_item: state.has(e_item, self.player))
-            if location.name in Goofy_Checks:
-                add_item_rule(location, lambda item: item.player == self.player and item.name in GoofyAbility_Table.keys())
-            elif location.name in Donald_Checks:
-                add_item_rule(location, lambda item: item.player == self.player and item.name in DonaldAbility_Table.keys())
-            else:
-                add_item_rule(location, lambda item: item.player == self.player and item.name in SupportAbility_Table.keys())
+            if location.name in exclusion_table["WeaponSlots"]:  # shop items and starting items are not in this list
+                exclusion_item = exclusion_table["WeaponSlots"][location.name]
+                add_rule(location, lambda state, e_item=exclusion_item: state.has(e_item, self.player))
+                if location.name in Goofy_Checks:
+                    add_item_rule(location, lambda item: item.player == self.player and item.name in GoofyAbility_Table.keys())
+                elif location.name in Donald_Checks:
+                    add_item_rule(location, lambda item: item.player == self.player and item.name in DonaldAbility_Table.keys())
+                else:
+                    add_item_rule(location, lambda item: item.player == self.player and item.name in SupportAbility_Table.keys())
 
     def set_kh2_goal(self):
         final_xemnas_location = self.multiworld.get_location(LocationName.FinalXemnasEventLocation, self.player)
