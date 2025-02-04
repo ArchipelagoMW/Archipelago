5d3c306bc5027130eb923a91e2639a86fc103dc4
BootsinSoots <theron.swanson@gmail.com>
2025-02-04 12:46:04 -0500

    Even out hints

--- worlds/luigismansion/Hints.py
+++ worlds/luigismansion/Hints.py
@@ -48,7 +48,7 @@ def get_hints_by_option(multiworld: MultiWorld, player: int) -> Dict[str, Dict[s
                 loc: Location = multiworld.find_item(iname, player)
             if world.options.hint_distribution.value == 4:
                 hintfo = f"<SAY><COLOR>(7){multiworld.player_name[loc.item.player]}'s\\n <COLOR>(5){loc.item.name}"+ \
-                    f"\n<ANYKEY>\n<SAY>COLOR>(0)is somewhere in\\n <COLOR>(3){multiworld.player_name[loc.player]}'s\\n{loc.game}"
+                    f"\n<ANYKEY>\n<SAY>COLOR>(0) is somewhere in\\n <COLOR>(3){multiworld.player_name[loc.player]}'s\\n{loc.game}"
             elif world.options.hint_distribution.value == 1:
                 joke = world.random.choice(str.splitlines(jokes)).replace("\\n", "\n")
                 hintfo = f"<SAY><COLOR>(0){joke}"
@@ -56,7 +56,7 @@ def get_hints_by_option(multiworld: MultiWorld, player: int) -> Dict[str, Dict[s
                 hintfo = "<SAY><COLOR>(2)I see you've turned off hints"
             else:
                 hintfo = f"<SAY><COLOR>(7){multiworld.player_name[loc.item.player]}'s\\n <COLOR>(5){loc.item.name}" + \
-                    f"\n<ANYKEY>\n<SAY><COLOR>(0)can be found at\\n <COLOR>(1){multiworld.player_name[loc.player]}'s" + \
+                    f"\n<ANYKEY>\n<SAY><COLOR>(0) can be found at\\n <COLOR>(1){multiworld.player_name[loc.player]}'s" + \
                     f"\\n{loc.name}"
             hint = {name: hintfo}
             already_hinted_locations.append(loc)
@@ -88,7 +88,7 @@ def get_hints_by_option(multiworld: MultiWorld, player: int) -> Dict[str, Dict[s
                 icolor = 6
             if world.options.hint_distribution == 4:
                 hintfo = f"<SAY><COLOR>(7){multiworld.player_name[loc.item.player]}'s\\n" + \
-                    f"<COLOR>({icolor}){loc.item.name}\n<ANYKEY>\n<SAY><COLOR>(0)is somewhere in\\n " + \
+                    f"<COLOR>({icolor}){loc.item.name}\n<ANYKEY>\n<SAY><COLOR>(0) is somewhere in\\n " + \
                     f"<COLOR>(3){multiworld.player_name[loc.player]}'s\\n{loc.game}"
             elif world.options.hint_distribution == 5:
                 hintfo = "<SAY><COLOR>(2)I see you've turned off hints"
@@ -97,7 +97,7 @@ def get_hints_by_option(multiworld: MultiWorld, player: int) -> Dict[str, Dict[s
                 hintfo = f"<SAY><COLOR>(0){joke}"
             else:
                 hintfo = f"<SAY><COLOR>(7){multiworld.player_name[loc.item.player]}'s\\n " + \
-                    f"<COLOR>(5){loc.item.name}\n<ANYKEY>\n<SAY><COLOR>(0)can be found at\\n " + \
+                    f"<COLOR>(5){loc.item.name}\n<ANYKEY>\n<SAY><COLOR>(0) can be found at\\n " + \
                     f"<COLOR>(1){multiworld.player_name[loc.player]}'s\\n {loc.name}"
             hint = {name: hintfo}
             already_hinted_locations.append(loc)
