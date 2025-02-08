cecb60c78d1d8156ae8997e020ca543560df672d
Jouramie <16137441+Jouramie@users.noreply.github.com>
2025-02-07 22:56:11 -0500

    self review

--- worlds/stardew_valley/test/TestCrops.py
+++ worlds/stardew_valley/test/TestCrops.py
@@ -8,12 +8,13 @@ class TestCropsanityRules(SVTestBase):
     }
 
     def test_need_greenhouse_for_cactus(self):
-        self.assert_cannot_reach_location("Harvest Cactus Fruit")
+        harvest_cactus = "Harvest Cactus Fruit"
+        self.assert_cannot_reach_location(harvest_cactus)
 
         self.multiworld.state.collect(self.create_item("Cactus Seeds"))
         self.multiworld.state.collect(self.create_item("Shipping Bin"))
         self.multiworld.state.collect(self.create_item("Desert Obelisk"))
-        self.assert_cannot_reach_location("Harvest Cactus Fruit")
+        self.assert_cannot_reach_location(harvest_cactus)
 
         self.multiworld.state.collect(self.create_item("Greenhouse"))
-        self.assert_can_reach_location("Harvest Cactus Fruit")
+        self.assert_can_reach_location(harvest_cactus)
