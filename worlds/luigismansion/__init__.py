bca817079e78710ae2e5eeaaadb2a93080e9fa40
BootsinSoots <theron.swanson@gmail.com>
2025-02-10 12:23:29 -0500

    Remove unintentional Boo Radar requirement for Lights

--- worlds/luigismansion/__init__.py
+++ worlds/luigismansion/__init__.py
@@ -287,8 +287,6 @@ class LMWorld(World):
             for location, data in LIGHT_LOCATION_TABLE.items():
                 region = self.multiworld.get_region(data.region, self.player)
                 entry = LMLocation(self.player, location, region, data)
-                if self.options.boo_gates == 1 and self.options.boo_radar != 2:
-                    add_rule(entry, lambda state: state.has("Boo Radar", self.player), "and")
                 if entry.code == 745:
                     add_rule(entry,
                              lambda state: state.has_group("Mario Item", self.player, self.options.mario_items.value), "and")
