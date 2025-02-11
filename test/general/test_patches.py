20c0d6794298e9523e819d82bd96d272c192d13e
Justus Lind <deamonhunterextra@outlook.com>
2025-02-11 21:28:00 +1000

    Add a test for checking that registered patches have a match in worlds.

---
+++ test/general/test_patches.py
@@ -0,0 +1,10 @@
+﻿import unittest
+from worlds.AutoWorld import AutoWorldRegister
+from worlds.Files import AutoPatchRegister
+
+class TestPatches(unittest.TestCase):
+    def test_patch_name_matches_game(self) -> None:
+        for gamename in AutoPatchRegister.patch_types.keys():
+            with self.subTest(game=gamename):
+                self.assertTrue(gamename in AutoWorldRegister.world_types, 
+                                f"Patch \"{gamename}\" does not match the name of any world.")
