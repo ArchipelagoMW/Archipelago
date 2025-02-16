53881fa6c830c578c44eb12eb439a9b13fefd0e6
Justus Lind <deamonhunterextra@outlook.com>
2025-02-16 14:28:20 +1000

    Apply style changes and correct assertion.

--- test/general/test_patches.py
+++ test/general/test_patches.py
@@ -2,9 +2,10 @@
 from worlds.AutoWorld import AutoWorldRegister
 from worlds.Files import AutoPatchRegister
 
+
 class TestPatches(unittest.TestCase):
     def test_patch_name_matches_game(self) -> None:
-        for gamename in AutoPatchRegister.patch_types.keys():
-            with self.subTest(game=gamename):
-                self.assertTrue(gamename in AutoWorldRegister.world_types, 
-                                f"Patch \"{gamename}\" does not match the name of any world.")
+        for game_name in AutoPatchRegister.patch_types:
+            with self.subTest(game=game_name):
+                self.assertIn(game_name, AutoWorldRegister.world_types, 
+                                f"Patch '{game_name}' does not match the name of any world.")
