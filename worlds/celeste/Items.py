d42074b9164f40346260ea0ea53488ab3f5265c5
PoryGone <porygoneplays@gmail.com>
2025-02-16 21:25:28 -0500

    All interactable items

--- worlds/celeste/Items.py
+++ worlds/celeste/Items.py
@@ -22,7 +22,46 @@ collectable_item_data_table: Dict[str, CelesteItemData] = {
 }
 
 interactable_item_data_table: Dict[str, CelesteItemData] = {
-    ItemName.springs:  CelesteItemData(celeste_base_id + 0x200 + 0x1, ItemClassification.progression),
+    ItemName.springs:              CelesteItemData(celeste_base_id + 0x200 + 0x00, ItemClassification.progression),
+    ItemName.traffic_blocks:       CelesteItemData(celeste_base_id + 0x200 + 0x01, ItemClassification.progression),
+    ItemName.pink_cassette_blocks: CelesteItemData(celeste_base_id + 0x200 + 0x02, ItemClassification.progression),
+    ItemName.blue_cassette_blocks: CelesteItemData(celeste_base_id + 0x200 + 0x03, ItemClassification.progression),
+
+    ItemName.dream_blocks: CelesteItemData(celeste_base_id + 0x200 + 0x04, ItemClassification.progression),
+    ItemName.coins:        CelesteItemData(celeste_base_id + 0x200 + 0x05, ItemClassification.progression),
+
+    ItemName.moving_platforms:  CelesteItemData(celeste_base_id + 0x200 + 0x06, ItemClassification.progression),
+
+    ItemName.blue_boosters: CelesteItemData(celeste_base_id + 0x200 + 0x07, ItemClassification.progression),
+    ItemName.blue_clouds:   CelesteItemData(celeste_base_id + 0x200 + 0x08, ItemClassification.progression),
+    ItemName.move_blocks:   CelesteItemData(celeste_base_id + 0x200 + 0x09, ItemClassification.progression),
+
+    ItemName.swap_blocks:  CelesteItemData(celeste_base_id + 0x200 + 0x0A, ItemClassification.progression),
+    ItemName.red_boosters: CelesteItemData(celeste_base_id + 0x200 + 0x0B, ItemClassification.progression),
+    ItemName.theo_crystal: CelesteItemData(celeste_base_id + 0x200 + 0x0C, ItemClassification.progression),
+
+    ItemName.feathers:     CelesteItemData(celeste_base_id + 0x200 + 0x0D, ItemClassification.progression),
+    ItemName.bumpers:      CelesteItemData(celeste_base_id + 0x200 + 0x0E, ItemClassification.progression),
+    ItemName.kevin_blocks: CelesteItemData(celeste_base_id + 0x200 + 0x0F, ItemClassification.progression),
+
+    ItemName.pink_clouds:       CelesteItemData(celeste_base_id + 0x200 + 0x10, ItemClassification.progression),
+    ItemName.badeline_boosters: CelesteItemData(celeste_base_id + 0x200 + 0x11, ItemClassification.progression),
+
+    ItemName.fire_ice_balls: CelesteItemData(celeste_base_id + 0x200 + 0x12, ItemClassification.progression),
+    ItemName.core_toggles:   CelesteItemData(celeste_base_id + 0x200 + 0x13, ItemClassification.progression),
+    ItemName.core_blocks:    CelesteItemData(celeste_base_id + 0x200 + 0x14, ItemClassification.progression),
+
+    ItemName.pufferfish:             CelesteItemData(celeste_base_id + 0x200 + 0x15, ItemClassification.progression),
+    ItemName.jellyfish:              CelesteItemData(celeste_base_id + 0x200 + 0x16, ItemClassification.progression),
+    ItemName.breaker_boxes:          CelesteItemData(celeste_base_id + 0x200 + 0x17, ItemClassification.progression),
+    ItemName.dash_refills:           CelesteItemData(celeste_base_id + 0x200 + 0x18, ItemClassification.progression),
+    ItemName.double_dash_refills:    CelesteItemData(celeste_base_id + 0x200 + 0x19, ItemClassification.progression),
+    ItemName.yellow_cassette_blocks: CelesteItemData(celeste_base_id + 0x200 + 0x1A, ItemClassification.progression),
+    ItemName.green_cassette_blocks:  CelesteItemData(celeste_base_id + 0x200 + 0x1B, ItemClassification.progression),
+
+    ItemName.dash_switch: CelesteItemData(celeste_base_id + 0x200 + 0x1C, ItemClassification.progression),
+    ItemName.seeker:      CelesteItemData(celeste_base_id + 0x200 + 0x1D, ItemClassification.progression),
+    ItemName.keys:        CelesteItemData(celeste_base_id + 0x200 + 0x1E, ItemClassification.progression),
 }
 
 item_data_table: Dict[str, CelesteItemData] = {**collectable_item_data_table, **interactable_item_data_table}
--- worlds/celeste/Names/ItemName.py
+++ worlds/celeste/Names/ItemName.py
@@ -14,4 +14,44 @@ dl_dash = "Down-Left Dash"
 ul_dash = "Up-Left Dash"
 
 # Interactables
-springs = "Springs"
+springs              = "Springs"
+traffic_blocks       = "Traffic Blocks"
+pink_cassette_blocks = "Pink Cassette Blocks"
+blue_cassette_blocks = "Blue Cassette Blocks"
+
+dream_blocks = "Dream Blocks"
+coins        = "Coins"
+
+moving_platforms = "Moving Platforms"
+
+blue_boosters = "Blue Boosters"
+blue_clouds   = "Blue Clouds"
+move_blocks   = "Move Blocks"
+
+swap_blocks  = "Swap Blocks"
+red_boosters = "Red Boosters"
+theo_crystal = "Theo Crystal"
+
+feathers     = "Feathers"
+bumpers      = "Bumpers"
+kevin_blocks = "Kevins"
+
+pink_clouds       = "Pink Clouds"
+badeline_boosters = "Badeline Boosters"
+
+fire_ice_balls = "Fire and Ice Balls"
+core_toggles   = "Core Toggles"
+core_blocks    = "Core Blocks"
+
+pufferfish             = "Pufferfish"
+jellyfish              = "Jellyfish"
+breaker_boxes          = "Breaker Boxes"
+dash_refills           = "Dash Refills"
+double_dash_refills    = "Double Dash Refills"
+yellow_cassette_blocks = "Yellow Cassette Blocks"
+green_cassette_blocks  = "Green Cassette Blocks"
+
+# Questionable
+dash_switch = "Dash Switches"
+seeker      = "Seekers"
+keys        = "Keys"
