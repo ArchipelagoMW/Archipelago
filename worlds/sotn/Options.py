489b17475879f3c4b5dd5f8f10f34da9db776300
Felipe Delduque <fdelduque@gmail.com>
2025-02-24 17:22:53 -0300

    Remove options until merge with the new system

--- worlds/sotn/Options.py
+++ worlds/sotn/Options.py
@@ -1,13 +1,21 @@
-from typing import Dict, Union
+from dataclasses import dataclass
 from BaseClasses import MultiWorld
-from Options import Option, Toggle, Choice, Range, FreeText
+from Options import (OptionGroup, Toggle, Choice, Range, FreeText, ItemsAccessibility, StartInventoryPool,
+                     PerGameCommonOptions)
 
 
 class OpenedNO4NO3(Toggle):
     """
         If true, the back door of Underground Caverns will be open
     """
-    display_name = "Opened NO4 Backdoor"
+    display_name = "Opened NO4 Backdoor in NP3"
+
+
+class EarlyOpenedNO4NO3(Toggle):
+    """
+        If true, the back door of Underground Caverns will be open from the start
+    """
+    display_name = "Opened NO4 Backdoor in NO3"
 
 
 class OpenedDAIARE(Toggle):
@@ -17,233 +25,16 @@ class OpenedDAIARE(Toggle):
     display_name = "Opened ARE Backdoor"
 
 
-class OpenedDAINO2(Toggle):
-    """
-        If true, the back door of Olrox's Quarters will be open
-    """
-    display_name = "Opened NO2 Backdoor"
-
-
-class Goal(Choice):
-    """
-    Goal
-    """
-    display_name = "Goal"
-    option_richter = 0
-    option_shaft = 1
-    option_rs = 2
-    option_dracula = 3
-    option_talisman = 4
-    option_td = 5
-    default = 3
-
-
-class NumberOfTalismans(Range):
-    """
-    Amount of Talismans in game. It might be less based on available locations
-    """
-    display_name = "Max Number of Talismans"
-    range_start = 1
-    range_end = 255
-    default = 100
-
-
-class PercentageOfTalismans(Range):
-    """
-    How many talismans to beat the game
-    """
-    display_name = "Required Percentage of Talismans"
-    range_start = 1
-    range_end = 100
-    default = 80
-
-
-class Difficult(Choice):
-    """
-    Determines the difficult
-    """
-    display_name = "Difficult"
-    option_easy = 0
-    option_normal = 1
-    option_hard = 2
-    option_insane = 3
-    default = 1
-
-
-class XpModifier(Range):
-    """Override Monster XP modifier in percentage"""
-    display_name = "XP modifier"
-    range_start = 0
-    range_end = 300
-    default = 0
-
-
-class AttModifier(Range):
-    """Override Monster attack modifier in percentage"""
-    display_name = "Attack modifier"
-    range_start = 0
-    range_end = 300
-    default = 0
-
-
-class HpModifier(Range):
-    """Override Monster HP modifier in percentage"""
-    display_name = "HP modifier"
-    range_start = 0
-    range_end = 300
-    default = 0
-
-
-class DropModifier(Range):
-    """Multiply Monster drop chance"""
-    display_name = "Drop modifier"
-    range_start = 0
-    range_end = 300
-    default = 0
-
-
-class BossesNeed(Range):
-    """Bosses required to beat Dracula."""
-    display_name = "Required Bosses Tokens"
-    range_start = 0
-    range_end = 20
-    default = 0
-
-
-class RngSongs(Toggle):
-    """
-        Randomize the zone songs
-    """
-    display_name = "Randomize Songs"
-
-
-class RngShop(Toggle):
-    """
-        Randomize shop items
-    """
-    display_name = "Randomize shop items"
-
-
-class ProgShop(Toggle):
-    """
-        Shop items can be progression items
-    """
-    display_name = "Forbid progression items on shop"
-
-
-class LibCardShop(Toggle):
-    """
-        Shop items will have a library card in stock
-    """
-    display_name = "Library card will be available on librarian"
-
-
-class RngPrices(Choice):
-    """
-    Randomize shop prices
-    """
-    display_name = "Shop prices"
-    option_disable = 0
-    option_cheap = 1
-    option_normal = 2
-    option_expensive = 3
-    default = 0
-
-
-class ExpNeed(Range):
-    """Exploration tokens required to beat Dracula."""
-    display_name = "Required Exploration Tokens"
-    range_start = 0
-    range_end = 20
-    default = 0
-
-
-class RngCandles(Choice):
-    """
-    Randomize candle drops
-    """
-    display_name = "Randomize candle drop"
-    option_off = 0
-    option_on = 1
-    option_crazy = 2
-    default = 0
-
-
-class ProgCandle(Toggle):
-    """
-        Candle drop can be progression items
-    """
-    display_name = "Forbid progression items on candles"
-
-
-class RngDrops(Choice):
-    """
-    Randomize enemies drops
-    """
-    display_name = "Randomize enemy drop"
-    option_off = 0
-    option_on = 1
-    option_crazy = 2
-    default = 0
-
-
-class ProgDrop(Toggle):
-    """
-        Enemy drop can be progression items
-    """
-    display_name = "Forbid progression items on enemies"
-
-
-class Enemysanity(Toggle):
-    """
-        Enemies become a location check
-    """
-    display_name = "Enemies become a location check"
-
-
-class Dropsanity(Toggle):
-    """
-        Enemies second item drop become a location check
-    """
-    display_name = "Enemies drops are locations checks"
-
-
-class Boostqty(Range):
-    """Boosts quantity in the pool."""
-    display_name = "Boost quantity"
-    range_start = 0
-    range_end = 50
-    default = 0
-
-
-class BoostWeight(FreeText):
-    """Weights for randomize boosts"""
-    display_name = "Boosts weights"
-    default = "10;6;1;6;1;6;1;6;1;7;7;7"
-
-
-class Trapqty(Range):
-    """Traps quantity in the pool."""
-    display_name = "Trap quantity"
-    range_start = 0
-    range_end = 50
-    default = 0
-
-
-class TrapWeight(FreeText):
-    """Weights for randomize traps"""
-    display_name = "Traps weights"
-    default = "1;2;1;2;1;2;7;3;7;3;8;5;7;6;2;6;2;6;2;4;2"
-
-
-class RandRules(Choice):
-    """
-    Define randomize rules
-    """
-    display_name = "Randomize random rules"
+class Extension(Choice):
+    """full: Every location on the map are added to the pool
+    relic_prog: Only relics, silver/gold rings, spike breaker and holy glasses locations are added
+    guarded: relic_prog + some items behind bosses are locations
+    equipment: guarded + most floor equipments are locations"""
+    display_name = "Extension"
     option_full = 0
-    option_limited = 1
-    option_expanded = 2
+    option_relic_prog = 1
+    option_guarded = 2
+    option_equipment = 3
     default = 0
 
 
@@ -251,69 +42,45 @@ class InfiniteWing(Toggle):
     """
         Makes wing smash remains until hit a wall or run out of MP
     """
-    display_name = "Infinite wing smash while transform into bat"
+    display_name = "Infinite wing smash"
 
 
-class AutoHeal(Toggle):
+class RandomizeNonLocations(Toggle):
     """
-        Heal just by entering a save room
+        Will randomize items that aren't locations on the seed
     """
-    display_name = "Heal when enter a save room"
+    display_name = "Randomize extra items"
 
 
-class MultipleTrap(Toggle):
+class ExtraPool(Toggle):
     """
-        Traps are applied all at once when possible
+        Try to add powerful items to the pool: Duplicator, Crissaegrim, Ring of varda, Mablung sword, Masamune, Marsil, Yasutsuna
     """
-    display_name = "Multiple Traps"
+    display_name = "Add extra items to the pool"
 
 
-class ExtraPool(FreeText):
-    """Extra item added to the pool"""
-    display_name = "Extra items"
-    default = {}
+@dataclass
+class SOTNOptions(PerGameCommonOptions):
+    accessibility: ItemsAccessibility
+    start_inventory: StartInventoryPool
+    open_no4: OpenedNO4NO3
+    early_open_no4: EarlyOpenedNO4NO3
+    open_are: OpenedDAIARE
+    extension: Extension
+    infinite_wing: InfiniteWing
+    randomize_items: RandomizeNonLocations
+    extra_pool: ExtraPool
 
 
-sotn_option_definitions: Dict[str, type(Option)] = {
-    "opened_no4": OpenedNO4NO3,
-    "opened_are": OpenedDAIARE,
-    "opened_no2": OpenedDAINO2,
-    "goal": Goal,
-    "num_talisman": NumberOfTalismans,
-    "per_talisman": PercentageOfTalismans,
-    "difficult": Difficult,
-    "xp_mod": XpModifier,
-    "att_mod": AttModifier,
-    "hp_mod": HpModifier,
-    "drop_mod": DropModifier,
-    "bosses_need": BossesNeed,
-    "rng_songs": RngSongs,
-    "rng_shop": RngShop,
-    "prog_shop": ProgShop,
-    "lib_shop": LibCardShop,
-    "rng_prices": RngPrices,
-    "exp_need": ExpNeed,
-    "rng_candles": RngCandles,
-    "prog_candles": ProgCandle,
-    "rng_drops": RngDrops,
-    "prog_drops": ProgDrop,
-    "enemysanity": Enemysanity,
-    "dropsanity": Dropsanity,
-    "boostqty": Boostqty,
-    "boostweight": BoostWeight,
-    "trapqty": Trapqty,
-    "trapweight": TrapWeight,
-    "rand_rules": RandRules,
-    "infinite_wing": InfiniteWing,
-    "auto_heal": AutoHeal,
-    "multiple_trap": MultipleTrap,
-    "extra_pool": ExtraPool
-}
+sotn_option_groups = [
+    OptionGroup("gameplay tweaks", [
+        EarlyOpenedNO4NO3, OpenedNO4NO3, OpenedDAIARE, Extension, RandomizeNonLocations
+    ]),
+    OptionGroup("QOL", [
+        InfiniteWing, ExtraPool
+    ])
+]
 
 
-def get_option_value(multiworld: MultiWorld, player: int, name: str) -> Union[int, dict]:
-    option = getattr(multiworld, name, None)
-    if option is None:
-        return 0
 
-    return option[player].value
+
