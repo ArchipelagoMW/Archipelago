4ee3d089b54920e0d8f9327e10210d83065a5eb1
Ixrec <ericrhitchcock@gmail.com>
2025-02-26 20:47:18 +0000

    add locations for giving Kuafu items to forge the other two arrow types

--- worlds/nine_sols/test/__init__.py
+++ worlds/nine_sols/test/__init__.py
@@ -81,4 +81,4 @@ class TestDefaultWorld(NineSolsTestBase):
     options = {}
 
     def test_default_world(self):
-        self.assertEqual(self.getLocationCount(), 320)  # 315 default locations + 5 events
+        self.assertEqual(self.getLocationCount(), 322)  # 317 default locations + 5 events
