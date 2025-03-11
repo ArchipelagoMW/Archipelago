a7c9b3c89330667f4346ce9edb84765230b78459
BootsinSoots <theron.swanson@gmail.com>
2025-03-11 12:26:23 -0400

    Make presets pass unittests

--- worlds/luigismansion/LuigiOptions.py
+++ worlds/luigismansion/LuigiOptions.py
@@ -8,11 +8,12 @@ from Options import Toggle, Range, PerGameCommonOptions, Choice, StartInventoryP
 class Deathlink(Toggle):
     """All players who have deathlink enabled will die when one person with deathlink on does"""
     display_name = "Deathlink"
-
+    internal_name = "deathlink"
 
 class LuigiWalkSpeed(Choice):
     """Choose how fast Luigi moves"""
     display_name = "Walk Speed"
+    internal_name = "walk_speed"
     option_normal_speed = 0
     option_kinda_fast = 1
     option_schmoovin = 2
@@ -22,11 +23,13 @@ class LuigiWalkSpeed(Choice):
 class RandomMusic(Toggle):
     """Randomize Music"""
     display_name = "Music Randomization"
+    internal_name = "random_music"
 
 
 class BundleWeight(Range):
     """Set the weight for how often coin & bill bundles get chosen as filler."""
     display_name = "Money Bundle Weight"
+    internal_name = "bundle_weight"
     range_start = 0
     range_end = 100
     default = 10
@@ -35,6 +38,7 @@ class BundleWeight(Range):
 class CoinWeight(Range):
     """Set the weight for how often coins get chosen as filler."""
     display_name = "Coin Weight"
+    internal_name = "coin_weight"
     range_start = 0
     range_end = 100
     default = 15
@@ -43,6 +47,7 @@ class CoinWeight(Range):
 class BillWeight(Range):
     """Set the weight for how often bills get chosen as filler."""
     display_name = "Bill Weight"
+    internal_name = "bill_weight"
     range_start = 0
     range_end = 100
     default = 10
@@ -51,6 +56,7 @@ class BillWeight(Range):
 class BarsWeight(Range):
     """Set the weight for how often gold bars get chosen as filler."""
     display_name = "Gold Bars Weight"
+    internal_name = "bars_weight"
     range_start = 0
     range_end = 100
     default = 10
@@ -59,6 +65,7 @@ class BarsWeight(Range):
 class GemsWeight(Range):
     """Set the weight for how often gemstones get chosen as filler."""
     display_name = "Gems Weight"
+    internal_name = "gems_weight"
     range_start = 0
     range_end = 100
     default = 5
@@ -67,6 +74,7 @@ class GemsWeight(Range):
 class PoisonTrapWeight(Range):
     """Set the weight for how often poison mushrooms get chosen as traps."""
     display_name = "Poison Trap Weight"
+    internal_name = "poison_trap_weight"
     range_start = 0
     range_end = 100
     default = 15
@@ -75,6 +83,7 @@ class PoisonTrapWeight(Range):
 class BombWeight(Range):
     """Set the weight for how often bombs get chosen as traps."""
     display_name = "Bomb Weight"
+    internal_name = "bomb_trap_weight"
     range_start = 0
     range_end = 100
     default = 15
@@ -83,6 +92,7 @@ class BombWeight(Range):
 class IceTrapWeight(Range):
     """Set the weight for how often ice traps get chosen as traps."""
     display_name = "Ice Trap Weight"
+    internal_name = "ice_trap_weight"
     range_start = 0
     range_end = 100
     default = 15
@@ -91,6 +101,7 @@ class IceTrapWeight(Range):
 class BananaTrapWeight(Range):
     """Set the weight for how often bananas get chosen as traps."""
     display_name = "Banana Trap Weight"
+    internal_name = "banana_trap_weight"
     range_start = 0
     range_end = 100
     default = 15
@@ -99,6 +110,7 @@ class BananaTrapWeight(Range):
 class NothingWeight(Range):
     """Set the weight for how often nothing is chosen as filler."""
     display_name = "'Nothing' Weight"
+    internal_name = "mothing_weight"
     range_start = 0
     range_end = 100
     default = 40
@@ -107,6 +119,7 @@ class NothingWeight(Range):
 class HeartWeight(Range):
     """Set the weight for how often hearts get chosen as filler."""
     display_name = "Heart Weight"
+    internal_name = "heart_weight"
     range_start = 0
     range_end = 100
     default = 10
@@ -115,6 +128,7 @@ class HeartWeight(Range):
 class BetterVacuum(Choice):
     """Choose whether to include the Poltergust 4000"""
     display_name = "Poltergust 4000"
+    internal_name = "good_vacuum"
     option_start_with = 0
     option_include = 1
     option_exclude = 2
@@ -125,11 +139,13 @@ class BetterVacuum(Choice):
 class StartHiddenMansion(Toggle):
     """Begin in the Hidden Mansion"""
     display_name = "Hidden Mansion"
+    internal_name = "hidden_mansion"
 
 
 class SpeedySpirits(Toggle):
     """Adds Blue Ghosts to location pool"""
     display_name = "Speedy Spirits"
+    internal_name = "speedy_spirits"
 
 
 class StartWithBooRadar(Choice):
@@ -143,6 +159,7 @@ class StartWithBooRadar(Choice):
     exclude: No Boo Radar - Boo Gates and Boosanity will be disabled if excluded
     """
     display_name = "Boo Radar"
+    internal_name = "boo_radar"
     option_start_with = 0
     option_include = 1
     option_exclude = 2
@@ -150,13 +167,15 @@ class StartWithBooRadar(Choice):
 
 
 class PortraitHints(Toggle):
-    """Choose to add hints to the scans of the Portrait Ghosts in the mansions"""
+    """Choose to add hints to the scans of the Portrait Ghosts' Hearts in the mansions"""
     display_name = "Portrait Ghost Hints"
+    internal_name = "portrait_hints"
 
 
 class HintDistribution(Choice):
     """Choose the level of hint from in-game hints. Will affect Portrait Ghost hints if the option is on."""
     display_name = "Hint Distribution"
+    internal_name = "hint_distribution"
     option_balanced = 0
     option_junk = 1
     option_chaos = 2
@@ -169,26 +188,31 @@ class HintDistribution(Choice):
 class Plants(Toggle):
     """Adds all plants to location pool"""
     display_name = "Plantsanity"
+    internal_name = "plantsanity"
 
 
 class PickupAnim(Toggle):
     """Disable Luigi's pickup animations"""
     display_name = "Pickup Animation"
+    internal_name = "pickup_animation"
 
 
 class Toadsanity(Toggle):
     """Add Toads as locations to be checked. These are remote only checks."""
     display_name = "Toadsanity"
+    internal_name = "toadsanity"
 
 
 class Lightsanity(Toggle):
     """Adds the act of lighting up rooms as locations. These are remote only checks"""
     display_name = "Lightsanity"
+    internal_name = "lightsanity"
 
 
 class Walksanity(Toggle):
     """Adds the act of visiting rooms as locations. These are remote only checks"""
     display_name = "Walksanity"
+    internal_name = "walksanity"
 
 
 class Furnisanity(Toggle):
@@ -196,6 +220,7 @@ class Furnisanity(Toggle):
     Adds interactable objects, such a dressers, paintings, candles, and light fixtures, to the location pool
     """
     display_name = "Furnisanity"
+    internal_name = "furnisanity"
 
 
 class BooGates(Toggle):
@@ -205,12 +230,14 @@ class BooGates(Toggle):
     Default to on. If this is turned off, the Boo Count options are ignored.
     """
     display_name = "Boo Gates"
+    internal_name = "boo_gates"
     default = 1
 
 
 class MarioItems(Range):
     """How many Mario Items it takes to capture the Fortune-Teller. 0 = Starts Capturable"""
     display_name = "Fortune-Teller Requirements"
+    internal_name = "mario_items"
     range_start = 0
     range_end = 5
     default = 5
@@ -219,6 +246,7 @@ class MarioItems(Range):
 class WashroomBooCount(Range):
     """Set the number of Boos required to reach the 1F Washroom. 0 = Starts Open"""
     display_name = "Washroom Boo Count"
+    internal_name = "washroom_boo_count"
     range_start = 0
     range_end = 50
     default = 5
@@ -227,6 +255,7 @@ class WashroomBooCount(Range):
 class BalconyBooCount(Range):
     """Set the number of Boos required to reach the Balcony. 0 = Starts Open"""
     display_name = "Balcony Boo Count"
+    internal_name = "balcony_boo_count"
     range_start = 0
     range_end = 36
     default = 20
@@ -235,6 +264,7 @@ class BalconyBooCount(Range):
 class KingBooHealth(Range):
     """Set King Boo's health in the final fight."""
     display_name = "King Boo's Health"
+    internal_name = "king_boo_health"
     range_start = 1
     range_end = 1000
     default = 500
@@ -243,6 +273,7 @@ class KingBooHealth(Range):
 class FinalBooCount(Range):
     """Set the number of Boos required to reach the Secret Altar. 0 = Starts Open"""
     display_name = "Altar Boo Count"
+    internal_name = "final_boo_count"
     range_start = 0
     range_end = 50
     default = 40
@@ -251,11 +282,13 @@ class FinalBooCount(Range):
 class Boosanity(Toggle):
     """Turns Boos into Items and Locations. These are remote only checks."""
     display_name = "Boosanity"
+    internal_name = "boosanity"
 
 
 class Portrification(Toggle):
     """Turn Portrait Ghosts into checks in addition to their clear chests. These are remote only checks."""
     display_name = "Portrification"
+    internal_name = "portrification"
 
 
 class Enemizer(Choice):
@@ -269,6 +302,7 @@ class Enemizer(Choice):
     No Elements: Remove ghost elements, randomize waves
     """
     display_name = "Enemizer"
+    internal_name = "enemizer"
     option_vanilla = 0
     option_randomized_elements = 1
     option_no_elements = 2
@@ -278,11 +312,13 @@ class Enemizer(Choice):
 class DoorRando(Toggle):
     """Randomize which doors are locked or unlocked in the mansion."""
     display_name = "Door Randomization"
+    internal_name = "door_rando"
 
 
 class LuigiFearAnim(Toggle):
     """Turn off Luigi being scared by ghosts if they spawn close to him"""
     display_name = "Courageous Luigi"
+    internal_name = "fear_animation"
 
 
 class ChestTypes(Choice):
@@ -298,6 +334,7 @@ class ChestTypes(Choice):
     size_and_color: Both size and color are determined by AP item classification
     """
     display_name = "Chest Cosmetics"
+    internal_name = "chest_types"
     option_default = 0
     option_full_random = 1
     option_color = 2
@@ -316,6 +353,7 @@ class TrapChestType(Choice):
     anything: Trap chests can appear as anything
     """
     display_name = "Trap Appearance"
+    internal_name = "trap_chests"
     option_filler = 0
     option_progression = 1
     option_anything = 2
@@ -330,6 +368,7 @@ class Goal(Choice):
     Rank Requirement: Gather enough money to reach the specified rank before beating King Boo *experimental
     """
     display_name = "Goal"
+    internal_name = "goal"
     option_king_boo = 0
     option_rank_requirement = 1
     default = 0
@@ -340,6 +379,7 @@ class RankRequirement(Choice):
     If Rank Requirement is chosen as goal, choose the required rank (H to A) with A being the highest
     """
     display_name = "Rank Requirement"
+    internal_name = "rank_requirement"
     option_rank_h = 0
     option_rank_g = 1
     option_rank_f = 2
