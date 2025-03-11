c0aa8d306458a05089de24551ab72dc595cda0a8
SomeJakeGuy <somejakeguy@gmail.com>
2025-03-11 02:30:19 -0400

    Changed certain ram fields to be required.

--- worlds/luigismansion/Helper_Functions.py
+++ worlds/luigismansion/Helper_Functions.py
@@ -2,9 +2,9 @@ from typing import NamedTuple, Optional
 
 
 class LMRamData(NamedTuple):
-    ram_addr: Optional[int] = None
-    bit_flag: Optional[bool] = None
-    bit_position: Optional[int] = None
-    ram_byte_size: Optional[int] = None
+    ram_addr: int = None
+    bit_flag: bool = None
+    bit_position: int = None
+    ram_byte_size: int = None
     pointer_offset: Optional[int] = None
     in_game_room_id: Optional[int] = None
