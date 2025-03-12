ad8b929463b547192b709fa3a8bf0b1c299a2dfe
SomeJakeGuy <somejakeguy@gmail.com>
2025-03-12 03:12:57 -0400

    Removed bit flag boolean, made anything other than address optional.

--- worlds/luigismansion/Helper_Functions.py
+++ worlds/luigismansion/Helper_Functions.py
@@ -3,9 +3,8 @@ from typing import NamedTuple, Optional
 
 class LMRamData(NamedTuple):
     ram_addr: int = None
-    bit_flag: bool = None
-    bit_position: int = None
-    ram_byte_size: int = None
+    bit_position: Optional[int] = None
+    ram_byte_size: Optional[int] = None
     pointer_offset: Optional[int] = None
     in_game_room_id: Optional[int] = None
     item_count: Optional[int] = None
