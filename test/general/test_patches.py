6c3cbd97654e922ab86f8850cc1207217c193714
Justus Lind <DeamonHunter@users.noreply.github.com>
2025-03-12 17:29:07 +1000

    Apply Qwints Suggestions.
    Co-authored-by: qwint <qwint.42@gmail.com>

--- test/general/test_patches.py
+++ test/general/test_patches.py
@@ -7,5 +7,5 @@ class TestPatches(unittest.TestCase):
     def test_patch_name_matches_game(self) -> None:
         for game_name in AutoPatchRegister.patch_types:
             with self.subTest(game=game_name):
-                self.assertIn(game_name, AutoWorldRegister.world_types, 
-                                f"Patch '{game_name}' does not match the name of any world.")
+                self.assertIn(game_name, AutoWorldRegister.world_types.keys(),
+                              f"Patch '{game_name}' does not match the name of any world.")
