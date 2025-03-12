d0d27433cf8411bed520be3240f6ce1d8f0a8716
JaredWeakStrike <jddoc31@gmail.com>
2025-03-12 07:12:04 -0400

    backwards compat will revert

--- KH2Client.py
+++ KH2Client.py
@@ -1,6 +1,6 @@
 import ModuleUpdate
 import Utils
-from worlds.kh2.Client.Client import launch
+from worlds.kh2.Client import launch
 ModuleUpdate.update()
 
 if __name__ == '__main__':
---
+++ worlds/kh2/Client.py
@@ -0,0 +1,4 @@
+# this is just here for backwards compatibility so people can test.
+# ClientStuff will be named Client in the fututre and will require KH2Client.py to use
+# from worlds.kh2.Client.Client import launch
+from .ClientStuff.Client import launch
