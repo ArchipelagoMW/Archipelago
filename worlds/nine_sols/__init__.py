b68e961b890c66e09ec1802e63bea87228b95b33
Ixrec <ericrhitchcock@gmail.com>
2025-03-17 23:17:21 +0000

    bump version to 0.1.3

--- worlds/nine_sols/__init__.py
+++ worlds/nine_sols/__init__.py
@@ -91,6 +91,6 @@ class NineSolsWorld(World):
         )
         # Archipelago does not yet have apworld versions (data_version is deprecated),
         # so we have to roll our own with slot_data for the time being
-        slot_data["apworld_version"] = "0.1.2"
+        slot_data["apworld_version"] = "0.1.3"
         return slot_data
 
