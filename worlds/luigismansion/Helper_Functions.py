f2d2608f490f59df53a08cc8424d28f29ade471b
SomeJakeGuy <somejakeguy@gmail.com>
2025-03-22 22:05:46 -0400

    Added string helper methods to Helper_Functions.py

--- worlds/luigismansion/Helper_Functions.py
+++ worlds/luigismansion/Helper_Functions.py
@@ -8,3 +8,30 @@ class LMRamData(NamedTuple):
     pointer_offset: Optional[int] = None
     in_game_room_id: Optional[int] = None
     item_count: Optional[int] = None
+
+class StringByteFunction:
+    # Encodes a provided string to UTF-8 format. Adds padding until the expected length is reached.
+    # If provided string is longer than expected length, raise an exception
+    @staticmethod
+    def string_to_bytes(user_string: str, encoded_byte_length: int):
+        encoded_string = user_string.encode('utf-8')
+
+        if len(encoded_string) < encoded_byte_length:
+            encoded_string += b'\x00' * (encoded_byte_length - len(encoded_string))
+        elif len(encoded_string) > encoded_byte_length:
+            raise Exception("Provided string '" + user_string + "' was longer than the expected byte length of '" +
+                            str(encoded_byte_length) + "', which will not be accepted by the info file.")
+
+        return encoded_string
+
+    # Strips the un-necessary padding / bytes that are not a part of the core string.
+    @staticmethod
+    def byte_string_strip(bytes_input: bytes):
+        result = []
+
+        for single_byte in bytes_input:
+            if single_byte < 32 or single_byte > 127:
+                break
+            result.append(chr(single_byte))
+
+        return ''.join(result)
