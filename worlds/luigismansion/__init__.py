f0ad335bbad3d92f00cc0b89da4df4838ecc218b
BootsinSoots <theron.swanson@gmail.com>
2025-03-29 00:10:21 -0400

    Use value for luigi max helath option in slot data

--- worlds/luigismansion/__init__.py
+++ worlds/luigismansion/__init__.py
@@ -818,8 +818,8 @@ class LMWorld(World):
             "spawn_region": self.origin_region_name,
             "death_link": self.options.death_link.value,
             "trap_link": self.options.trap_link.value,
-            "luigi max health": self.options.luigi_max_health,
-            "pickup animation": self.options.pickup_animation,
+            "luigi max health": self.options.luigi_max_health.value,
+            "pickup animation": self.options.pickup_animation.value,
             "apworld version": CLIENT_VERSION,
             "seed": self.multiworld.seed,
         }
