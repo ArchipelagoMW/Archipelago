18bb535346344ba42252725a4481978ebe849aee
Silvris <58583688+Silvris@users.noreply.github.com>
2025-03-29 18:26:16 -0500

    Update kvui.py
    Co-authored-by: Fabian Dill <Berserker66@users.noreply.github.com>

--- kvui.py
+++ kvui.py
@@ -819,8 +819,8 @@ class HintLayout(BoxLayout):
 
     def fix_heights(self):
         for child in self.children:
-            if hasattr(child, "fix_heights"):
-                getattr(child, "fix_heights")()
+            fix_func = getattr(child, "fix_heights", None):
+            if fix_func: fix_func()
 
         
 status_names: typing.Dict[HintStatus, str] = {
