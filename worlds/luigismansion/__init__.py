058ff438cfab64bada35e706c6ea7827f717ec1f
SomeJakeGuy <somejakeguy@gmail.com>
2025-03-28 23:19:07 -0400

    Added pickup animation to slot data.

--- worlds/luigismansion/__init__.py
+++ worlds/luigismansion/__init__.py
@@ -818,6 +818,7 @@ class LMWorld(World):
             "death_link": self.options.death_link.value,
             "trap_link": self.options.trap_link.value,
             "luigi max health": self.options.luigi_max_health,
+            "pickup animation": self.options.pickup_animation,
             "apworld version": CLIENT_VERSION,
             "seed": self.multiworld.seed,
         }
