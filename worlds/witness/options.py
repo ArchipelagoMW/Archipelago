05bb4a5b8a9355aa5a25cedaa962c98bf1837020
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2025-04-04 22:20:10 +0200

    Unnecessary line break

--- worlds/witness/options.py
+++ worlds/witness/options.py
@@ -461,7 +461,6 @@ class VagueHints(Choice):
     If set to "stable", only location groups will be used. If location groups aren't implemented for the game your item ended up in, your hint will instead only tell you that the item is "somewhere in" that game.
     If set to "experimental", region names will be eligible as well, and you will never receive a "somewhere in" hint. Keep in mind that region names are not always intended to be comprehensible to players — only turn this on if you are okay with a bit of chaos.
 
-
     The distinction does not matter in single player, as Witness implements location groups for every location.
 
     Also, please don't pester any devs about implementing location groups. Bring it up nicely, accept their response even if it is "No".
