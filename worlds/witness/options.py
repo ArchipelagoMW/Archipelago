0a44c3ec49e93a237ba8ea47dfdae090ecba8b17
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2025-04-05 17:48:18 +0200

    The Witness: Move the Easter Egg Hunt option group lower so that the tooltip isn't cut off (#4789)

--- worlds/witness/options.py
+++ worlds/witness/options.py
@@ -625,7 +625,7 @@ if is_easter_time():
     easter_special_option_group = OptionGroup("EASTER SPECIAL", [
         EasterEggHunt,
     ])
-    witness_option_groups = [easter_special_option_group, *witness_option_groups]
+    witness_option_groups.insert(2, easter_special_option_group)
 else:
     silly_options_group = next(group for group in witness_option_groups if group.name == "Silly Options")
     silly_options_group.options.append(EasterEggHunt)
