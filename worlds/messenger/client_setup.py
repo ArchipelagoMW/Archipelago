c5893924f20bf2862b1b07b177032fc9ef3a2e73
alwaysintreble <mmmcheese158@gmail.com>
2025-04-06 13:05:11 -0500

    messenger needs to be lazy too

--- worlds/messenger/client_setup.py
+++ worlds/messenger/client_setup.py
@@ -6,17 +6,19 @@ import requests
 import subprocess
 import urllib.request
 from shutil import which
-from typing import Any, Callable
+from typing import Any, Callable, TYPE_CHECKING
 from zipfile import ZipFile
-from kvui import ButtonsPrompt
 from Utils import is_windows, messagebox, open_file, tuplize_version
 
+if TYPE_CHECKING:
+    from kvui import ButtonsPrompt
 
 
 MOD_URL = "https://api.github.com/repos/alwaysintreble/TheMessengerRandomizerModAP/releases/latest"
 
 
-def create_yes_no_popup(title: str, text: str, callback: Callable[[str], None]) -> ButtonsPrompt:
+def create_yes_no_popup(title: str, text: str, callback: Callable[[str], None]) -> "ButtonsPrompt":
+    from kvui import ButtonsPrompt
     buttons = ["Yes", "No", "Cancel"]
 
     prompt = ButtonsPrompt(title, text, callback, *buttons)
