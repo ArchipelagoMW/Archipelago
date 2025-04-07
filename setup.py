5404e59bce86b8c308b67e5e5dd0812844e10d88
Fabian Dill <fabian.dill@web.de>
2025-04-07 02:03:26 +0200

    LttP: remove sprite download from setup flow LttP: make sprite repository dynamic

--- setup.py
+++ setup.py
@@ -416,6 +416,7 @@ class BuildExeCommand(cx_Freeze.command.build_exe.build_exe):
                 os.system(signtool + os.path.join(self.buildfolder, "lib", "worlds", "oot", "data", *exe_path))
 
         remove_sprites_from_folder(self.buildfolder / "data" / "sprites" / "alttpr")
+        remove_sprites_from_folder(self.buildfolder / "data" / "sprites" / "remote")
 
         self.create_manifest()
 
