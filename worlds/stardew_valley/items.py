ffe2fe899b5efb45757386a18fc3610678948779
Jouramie <16137441+Jouramie@users.noreply.github.com>
2025-04-07 20:28:22 -0400

    correctly exclude maximum one fillers from pool when in start inventory

--- worlds/stardew_valley/items.py
+++ worlds/stardew_valley/items.py
@@ -181,7 +181,7 @@ def create_items(item_factory: StardewItemFactory, locations_count: int, items_t
     items += unique_filler_items
     logger.debug(f"Created {len(unique_filler_items)} unique filler items")
 
-    resource_pack_items = fill_with_resource_packs_and_traps(item_factory, options, random, items, locations_count)
+    resource_pack_items = fill_with_resource_packs_and_traps(item_factory, options, random, items + items_to_exclude, locations_count - len(items))
     items += resource_pack_items
     logger.debug(f"Created {len(resource_pack_items)} resource packs")
 
@@ -727,7 +727,7 @@ def weapons_count(options: StardewValleyOptions):
 
 def fill_with_resource_packs_and_traps(item_factory: StardewItemFactory, options: StardewValleyOptions, random: Random,
                                        items_already_added: List[Item],
-                                       number_locations: int) -> List[Item]:
+                                       available_item_slots: int) -> List[Item]:
     include_traps = options.trap_items != TrapItems.option_no_traps
     items_already_added_names = [item.name for item in items_already_added]
     useful_resource_packs = [pack for pack in items_by_group[Group.RESOURCE_PACK_USEFUL]
@@ -750,10 +750,9 @@ def fill_with_resource_packs_and_traps(item_factory: StardewItemFactory, options
     priority_filler_items = remove_excluded_items(priority_filler_items, options)
 
     number_priority_items = len(priority_filler_items)
-    required_resource_pack = number_locations - len(items_already_added)
-    if required_resource_pack < number_priority_items:
+    if available_item_slots < number_priority_items:
         chosen_priority_items = [item_factory(resource_pack) for resource_pack in
-                                 random.sample(priority_filler_items, required_resource_pack)]
+                                 random.sample(priority_filler_items, available_item_slots)]
         return chosen_priority_items
 
     items = []
@@ -761,24 +760,24 @@ def fill_with_resource_packs_and_traps(item_factory: StardewItemFactory, options
                                           ItemClassification.trap if resource_pack.classification == ItemClassification.trap else ItemClassification.useful)
                              for resource_pack in priority_filler_items]
     items.extend(chosen_priority_items)
-    required_resource_pack -= number_priority_items
+    available_item_slots -= number_priority_items
     all_filler_packs = [filler_pack for filler_pack in all_filler_packs
                         if Group.MAXIMUM_ONE not in filler_pack.groups or
                         (filler_pack.name not in [priority_item.name for priority_item in
                                                   priority_filler_items] and filler_pack.name not in items_already_added_names)]
 
-    while required_resource_pack > 0:
+    while available_item_slots > 0:
         resource_pack = random.choice(all_filler_packs)
         exactly_2 = Group.EXACTLY_TWO in resource_pack.groups
-        while exactly_2 and required_resource_pack == 1:
+        while exactly_2 and available_item_slots == 1:
             resource_pack = random.choice(all_filler_packs)
             exactly_2 = Group.EXACTLY_TWO in resource_pack.groups
         classification = ItemClassification.useful if resource_pack.classification == ItemClassification.progression else resource_pack.classification
         items.append(item_factory(resource_pack, classification))
-        required_resource_pack -= 1
+        available_item_slots -= 1
         if exactly_2:
             items.append(item_factory(resource_pack, classification))
-            required_resource_pack -= 1
+            available_item_slots -= 1
         if exactly_2 or Group.MAXIMUM_ONE in resource_pack.groups:
             all_filler_packs.remove(resource_pack)
 
