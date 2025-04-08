23d08837c8d6d35022df81e76dbebcb5a6a1f650
udime123 <udime123@gmail.com>
2025-04-08 01:29:11 -0600

    Added the first item step to Init

--- worlds/silverdaze/__init__.py
+++ worlds/silverdaze/__init__.py
@@ -1,6 +1,7 @@
 #Sawyer: I'll get to this sooner or later!
 
 from typing import List
+
 from BaseClasses import Tutorial
 from worlds.AutoWorld import WebWorld, World
 from .Items import SDItem, SDItemData, event_item_table, get_items_by_category, item_table
@@ -8,3 +9,47 @@ from .Locations import SDLocation, location_table
 from .Options import SDOptions
 from .Presets import sd_options_presets
 from .Rules import set_rules
+
+class SDWorld(World):
+    """
+    This is where I'll describe Silver Daze once we get that far.
+    """
+    game = "Silver Daze"
+    options_dataclass = SDOptions
+    options: SDOptions
+    required_client_version = (0, 5, 4)
+
+    item_name_to_id = {name: data.code for name, data in item_table.items() if data.code is not None}
+    location_name_to_id = {name: data.code for name, data in location_table.items() if data.code is not None}
+    
+def create_items(self):
+    item_pool: List[SDItem] = []
+     #Characters, I think?
+    starting_character = "Pinn"
+    #Later, let's make this an option
+    itempool.remove(starting_character)
+    self.multiworld.push_precollected(self.create_item(starting_character))
+    
+    #Start adding the actual items
+    total_locations = len(self.multiworld.get_unfilled_locations(self.player))
+            for name, data in item_table.items():
+                quantity = data.max_quantity
+                item_pool += [self.create_item(name) for _ in range(0, quantity)]
+            
+
+
+    
+def create_item(self, name: str) -> SDItem:
+    data = item_table[name]
+    return SDItem(name, data.classification, data.code, self.player)
+
+def create_event(self, name: str) -> SDItem:
+    data = event_item_table[name]
+    return SDItem(name, data.classification, data.code, self.player)
+
+def set_rules(self):
+    set_rules(self, self.player)
+
+def create_regions(self):
+    create_regions(self)
+    self._place_events()
