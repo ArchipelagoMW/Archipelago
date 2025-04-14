fe24adb143b846ce45e6579198fe0ebb0caf7539
PIEisFANTASTIC <49802408+PIEisFANTASTIC@users.noreply.github.com>
2025-04-14 04:40:24 -0400

    Bugfix: Prevent possible .remove() crash

--- worlds/kh2/__init__.py
+++ worlds/kh2/__init__.py
@@ -480,9 +480,11 @@ class KH2World(World):
                 self.random_super_boss_list.remove(location)
 
         if self.options.LevelDepth == "level_1":
-            self.random_super_boss_list.remove(LocationName.Lvl50)
-            self.random_super_boss_list.remove(LocationName.Lvl99)
-
+            if LocationName.Lvl50 in self.random_super_boss_list:
+                self.random_super_boss_list.remove(LocationName.Lvl50)
+            if LocationName.Lvl99 in self.random_super_boss_list:
+                self.random_super_boss_list.remove(LocationName.Lvl99)
+            
         # We only want the bounty corresponding to our max level, remove the other level bounty
         if self.options.LevelDepth in ["level_50", "level_50_sanity"] and LocationName.Lvl99 in self.random_super_boss_list:
             self.random_super_boss_list.remove(LocationName.Lvl99)
