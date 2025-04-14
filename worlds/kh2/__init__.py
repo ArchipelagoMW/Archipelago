b448a9081d6aed76631848014475ae93ae3113cc
PIEisFANTASTIC <49802408+PIEisFANTASTIC@users.noreply.github.com>
2025-04-14 01:39:48 -0400

    Casual Bounty: Adjust level bounty logic to correspond with max level check setting

--- worlds/kh2/__init__.py
+++ worlds/kh2/__init__.py
@@ -483,8 +483,13 @@ class KH2World(World):
             self.random_super_boss_list.remove(LocationName.Lvl50)
             self.random_super_boss_list.remove(LocationName.Lvl99)
 
-        if self.options.LevelDepth not in ["level_99", "level_99_sanity"] and LocationName.Lvl99 in self.random_super_boss_list:
+        # We only want the bounty corresponding to our max level, remove the other level bounty
+        if self.options.LevelDepth in ["level_50", "level_50_sanity"] and LocationName.Lvl99 in self.random_super_boss_list:
             self.random_super_boss_list.remove(LocationName.Lvl99)
+
+        # We only want the bounty corresponding to our max level, remove the other level bounty
+        if self.options.LevelDepthin in ["level_99", "level_99_sanity"] and LocationName.Lvl50 in self.random_super_boss_list:
+            self.random_super_boss_list.remove(LocationName.Lvl50)
 
         if not self.options.SummonLevelLocationToggle and LocationName.Summonlvl7 in self.random_super_boss_list:
             self.random_super_boss_list.remove(LocationName.Summonlvl7)
