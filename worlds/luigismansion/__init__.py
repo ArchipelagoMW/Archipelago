e2f467f41c1c4d92ffa86636652a2907de7ecb2b
SomeJakeGuy <somejakeguy@gmail.com>
2025-04-16 00:44:20 -0400

    Increment client version to 0.3.0

--- worlds/luigismansion/__init__.py
+++ worlds/luigismansion/__init__.py
@@ -38,7 +38,7 @@ components.append(
 
 icon_paths["archiboolego"] = f"ap:{__name__}/data/archiboolego.png"
 
-CLIENT_VERSION = "0.2.5"
+CLIENT_VERSION = "0.3.0"
 
 class LuigisMansionSettings(settings.Group):
     class ISOFile(settings.UserFilePath):
