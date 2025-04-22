5ba2718cb53fea8f3e41da788638fd64cad3377f
Ziktofel <ziktofel@gmail.com>
2025-04-23 01:00:21 +0200

    Minor logic fix

--- worlds/sc2/rules.py
+++ worlds/sc2/rules.py
@@ -1745,7 +1745,7 @@ class SC2Logic:
                 state.has_any((
                     item_names.ABERRATION, item_names.INFESTED_DIAMONDBACK, item_names.INFESTED_BANSHEE,
                 ), self.player)
-                or state.has_all({item_names.MUTALISK, item_names.MUTALISK_SEVERING_GLAIVE}, self.player)
+                or state.has_all({item_names.MUTALISK, item_names.MUTALISK_SUNDERING_GLAIVE}, self.player)
                 or state.has_all((item_names.HYDRALISK, item_names.HYDRALISK_MUSCULAR_AUGMENTS), self.player)
                 or (
                         state.has(item_names.ZERGLING, self.player)
