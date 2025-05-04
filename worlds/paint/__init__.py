3786b1ca4f2b84f0dbf99aed9357ce8a45df0ac9
MarioManTAW <mariomantaw@gmail.com>
2025-05-04 09:17:48 -0500

    Paint: Implement New Game

---
+++ worlds/paint/__init__.py
@@ -0,0 +1,81 @@
+from typing import Dict, Any
+
+from BaseClasses import Tutorial, Region, Entrance
+from worlds.AutoWorld import World, WebWorld
+from .items import item_table, PaintItem, item_data_table
+from .locations import location_table, PaintLocation, location_data_table
+from .options import PaintOptions
+
+
+class PaintWebWorld(WebWorld):
+    theme = "partyTime"
+
+    setup_en = Tutorial(
+        tutorial_name="Start Guide",
+        description="A guide to playing Paint in Archipelago.",
+        language="English",
+        file_name="guide_en.md",
+        link="guide/en",
+        authors=["MarioManTAW"]
+    )
+
+    tutorials = [setup_en]
+
+
+class PaintWorld(World):
+    game = "Paint"
+    options_dataclass = PaintOptions
+    options = PaintOptions
+    location_name_to_id = location_table
+    item_name_to_id = item_table
+
+    def create_item(self, name: str) -> PaintItem:
+        item = PaintItem(name, item_data_table[name].type, item_data_table[name].code, self.player)
+        return item
+
+    def create_items(self):
+        starting_tools = ["Brush", "Pencil", "Eraser/Color Eraser", "Airbrush", "Line", "Rectangle", "Ellipse",
+                          "Rounded Rectangle"]
+        self.multiworld.push_precollected(self.create_item("Magnifier"))
+        self.multiworld.push_precollected(self.create_item(starting_tools[self.options.starting_tool]))
+        starting_tools.pop(self.options.starting_tool)
+        items_to_create = ["Free-Form Select", "Select", "Fill With Color", "Pick Color", "Text", "Curve", "Polygon"]
+        items_to_create += starting_tools
+        items_to_create += ["Progressive Canvas Width"] * 4
+        items_to_create += ["Progressive Canvas Height"] * 3
+        items_to_create += ["Progressive Color Depth (Red)"] * 7
+        items_to_create += ["Progressive Color Depth (Green)"] * 7
+        items_to_create += ["Progressive Color Depth (Blue)"] * 7
+        pre_filled = len(items_to_create)
+        to_fill = len(self.multiworld.get_region("Canvas", self.player).locations)
+        while len(items_to_create) < (to_fill - pre_filled) * (self.options.trap_count / 100) + pre_filled:
+            if self.options.death_link: items_to_create += [self.multiworld.random.choice(
+            ["Invert Colors Trap", "Flip Horizontal Trap", "Flip Vertical Trap"])]
+            else: items_to_create += [self.multiworld.random.choice(
+            ["Undo Trap", "Clear Image Trap", "Invert Colors Trap", "Flip Horizontal Trap", "Flip Vertical Trap"])]
+        while len(items_to_create) < to_fill:
+            items_to_create += ["Additional Palette Color"]
+        self.multiworld.itempool += [self.create_item(item) for item in items_to_create]
+
+    def create_regions(self):
+        menu = Region("Menu", self.player, self.multiworld)
+        canvas = Region("Canvas", self.player, self.multiworld)
+        canvas.locations += [PaintLocation(self.player, loc_name, loc_data.address, canvas)
+                             for loc_name, loc_data in location_data_table.items()
+                             if loc_data.address <= 198600 + self.options.logic_percent * 4 and (loc_data.address % 4 == 0 or
+                             (loc_data.address - 198600 > self.options.half_percent_checks * 4 and loc_data.address % 2 == 0) or
+                             loc_data.address - 198600 > self.options.quarter_percent_checks * 4)]
+
+        connection = Entrance(self.player, "New Canvas", menu)
+        menu.exits.append(connection)
+        connection.connect(canvas)
+        self.multiworld.regions += [menu, canvas]
+
+    def set_rules(self):
+        from .rules import set_rules, set_completion_rules
+        set_rules(self, self.player)
+        set_completion_rules(self, self.player)
+
+    def fill_slot_data(self) -> Dict[str, Any]:
+        return dict(self.options.as_dict("logic_percent", "goal_percent", "goal_image", "death_link"),
+                    version = "0.4.0")
---
+++ worlds/paint/options.py
@@ -0,0 +1,85 @@
+from dataclasses import dataclass
+
+from Options import Range, PerGameCommonOptions, Toggle, Choice, Visibility
+
+
+class LogicPercent(Range):
+    """Sets the maximum percent similarity required for a check to be in logic.
+    Higher values are more difficult and items/locations will not be generated beyond this number."""
+    display_name = "Logic Percent"
+    range_start = 50
+    range_end = 95
+    default = 80
+
+class GoalPercent(Range):
+    """Sets the percent similarity required to achieve your goal.
+    If this number is higher than the value for logic percent,
+    reaching goal will be in logic upon obtaining all progression items."""
+    display_name = "Goal Percent"
+    range_start = 50
+    range_end = 95
+    default = 80
+
+class HalfPercentChecks(Range):
+    """Sets the lowest percent at which locations will be created for each 0.5% of similarity.
+    Below this number, there will be a check every 1%.
+    Above this number, there will be a check every 0.5%."""
+    display_name = "Half Percent Checks"
+    range_start = 0
+    range_end = 95
+    default = 50
+
+class QuarterPercentChecks(Range):
+    """Sets the lowest percent at which locations will be created for each 0.25% of similarity.
+    This number will override Half Percent Checks if it is lower."""
+    display_name = "Quarter Percent Checks"
+    range_start = 0
+    range_end = 95
+    default = 70
+
+class GoalImage(Range):
+    """Sets the numbered image you will be required to match.
+    See https://github.com/MarioManTAW/jspaint/tree/master/images/archipelago
+    for a list of possible images or choose random.
+    This can also be overwritten client-side by using File->Open."""
+    display_name = "Goal Image"
+    range_start = 1
+    range_end = 1
+    default = "random"
+    visibility = Visibility.none
+
+class StartingTool(Choice):
+    """Sets which tool (other than Magnifier) you will be able to use from the start."""
+    option_brush = 0
+    option_pencil = 1
+    option_eraser = 2
+    option_airbrush = 3
+    option_line = 4
+    option_rectangle = 5
+    option_ellipse = 6
+    option_rounded_rectangle = 7
+    default = 0
+
+class TrapCount(Range):
+    """Sets the percentage of filler items to be replaced by random traps."""
+    display_name = "Trap Fill Percent"
+    range_start = 0
+    range_end = 100
+    default = 0
+
+class DeathLink(Toggle):
+    """If on, using the Undo or Clear Image functions will send a death to all other players with death link on.
+    Receiving a death will clear the image and reset the history.
+    This option also prevents Undo and Clear Image traps from being generated in the item pool."""
+    display_name = "Death Link"
+
+@dataclass
+class PaintOptions(PerGameCommonOptions):
+    logic_percent: LogicPercent
+    goal_percent: GoalPercent
+    half_percent_checks: HalfPercentChecks
+    quarter_percent_checks: QuarterPercentChecks
+    goal_image: GoalImage
+    starting_tool: StartingTool
+    trap_count: TrapCount
+    death_link: DeathLink
