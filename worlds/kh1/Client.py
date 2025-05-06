d112818963a7680a6010c114e54d563fa145fdd7
omnises <OmnisGamers@gmail.com>
2025-05-06 13:47:20 -0700

    Corrected typos

--- worlds/kh1/Client.py
+++ worlds/kh1/Client.py
@@ -180,19 +180,19 @@ class KH1Context(CommonContext):
             if args["type"] == "ItemSend":
                 item = args["item"]
                 networkItem = NetworkItem(*item)
-                recieverID = args["receiving"]
+                receiverID = args["receiving"]
                 senderID = networkItem.player
                 locationID = networkItem.location
-                if recieverID == self.slot or senderID == self.slot:
-                    itemName = self.item_names.lookup_in_slot(networkItem.item, recieverID)[:20]
+                if receiverID == self.slot or senderID == self.slot:
+                    itemName = self.item_names.lookup_in_slot(networkItem.item, receiverID)[:20]
                     itemCategory = networkItem.flags
-                    recieverName = self.player_names[recieverID][:20]
+                    receiverName = self.player_names[receiverID][:20]
                     senderName = self.player_names[senderID][:20]
                     message = ""
-                    if recieverID == self.slot and recieverID != senderID: # Item received from someone else
+                    if receiverID == self.slot and receiverID != senderID: # Item received from someone else
                         message = "From " + senderName + "\n" + itemName
-                    elif senderID == self.slot and recieverID != senderID: # Item sent to someone else
-                        message = itemName + "\nTo " + recieverName
+                    elif senderID == self.slot and receiverID != senderID: # Item sent to someone else
+                        message = itemName + "\nTo " + receiverName
                     elif locationID in remote_location_ids: # Found a remote item
                         message = itemName
                     filename = "msg"
@@ -204,19 +204,19 @@ class KH1Context(CommonContext):
             if args["type"] == "ItemSend":
                 item = args["item"]
                 networkItem = NetworkItem(*item)
-                recieverID = args["receiving"]
+                receiverID = args["receiving"]
                 senderID = networkItem.player
                 locationID = networkItem.location
-                if recieverID == self.slot or senderID == self.slot:
-                    itemName = self.item_names.lookup_in_slot(networkItem.item, recieverID)[:20]
+                if receiverID == self.slot or senderID == self.slot:
+                    itemName = self.item_names.lookup_in_slot(networkItem.item, receiverID)[:20]
                     itemCategory = networkItem.flags
-                    recieverName = self.player_names[recieverID][:20]
+                    receiverName = self.player_names[receiverID][:20]
                     senderName = self.player_names[senderID][:20]
                     message = ""
-                    if recieverID == self.slot and recieverID != senderID: # Item received from someone else
+                    if receiverID == self.slot and receiverID != senderID: # Item received from someone else
                         message = "From " + senderName + "\n" + itemName
-                    elif senderID == self.slot and recieverID != senderID: # Item sent to someone else
-                        message = itemName + "\nto " + recieverName
+                    elif senderID == self.slot and receiverID != senderID: # Item sent to someone else
+                        message = itemName + "\nto " + receiverName
                     elif locationID in remote_location_ids: # Found a remote item
                         message = itemName
                     filename = "msg"
@@ -227,11 +227,11 @@ class KH1Context(CommonContext):
             if args["type"] == "ItemCheat":
                 item = args["item"]
                 networkItem = NetworkItem(*item)
-                recieverID = args["receiving"]
-                if recieverID == self.slot:
-                    itemName = self.item_names.lookup_in_slot(networkItem.item, recieverID)[:20]
+                receiverID = args["receiving"]
+                if receiverID == self.slot:
+                    itemName = self.item_names.lookup_in_slot(networkItem.item, receiverID)[:20]
                     filename = "msg"
-                    message = "Recieved " + itemName + "\nfrom server"
+                    message = "Received " + itemName + "\nfrom server"
                     with open(os.path.join(self.game_communication_path, filename), 'w') as f:
                         f.write(message)
                         f.close()
