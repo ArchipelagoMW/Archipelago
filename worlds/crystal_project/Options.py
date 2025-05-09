d51e08ff7e50459a1e0690cb54a601d655297b86
dragonsbutalsorabbits <kathrynibarber@gmail.com>
2025-05-09 15:07:51 -0700

    Organizing and updating the options menu pt 1

--- worlds/crystal_project/Options.py
+++ worlds/crystal_project/Options.py
@@ -4,41 +4,58 @@ from typing import Dict
 from Options import Toggle, DefaultOnToggle, DeathLink, Choice, Range, OptionDict, OptionList, Visibility, Option
 from Options import PerGameCommonOptions, DeathLinkMixin, AssembleOptions, StartInventoryPool
 
+"""Goal Options"""
 class Goal(Choice):
     """
     Choose what is required to complete the game.
     
     Checking the first sign in the Spawning Meadows will tell you the selected goal.
     
-    Astley: Defeat Astley in the new world. A new world stone will be given to the player after obtaining a certain number of jobs.
+    Astley: Defeat Astley in the New World. A New World Stone will be given to the player after obtaining a certain number of jobs.
     
-    True Astley: Defeat Astley but more somehow
+    True Astley: Defeat Astley but more somehow.
 
-    Clamshells: Collect enough clamshells for the clam lover
+    Clamshells: Collect enough clamshells for the clam lover.
     """
     display_name = "Goal"
     option_astley = 0
     option_true_astley = 1
     option_clamshells = 2
     default = 0
-    
+
+class NewWorldStoneJobQuantity(Range):
+    """
+    If your goal is Astley, select how many Jobs you need to find before being sent the New World Stone for the final fight.
+
+    (NOTE: Starting Jobs do not count towards this number).
+    """
+    display_name = "Job count that locks the new world stone"
+    range_start = 1
+    range_end = 18
+    default = 18
+
 class ClamshellsQuantity(Range):
-    """If your goal is Clamshells, this is where you select how many you need to win."""
+    """
+    If your goal is Clamshells, select how many you need to win.
+    """
     display_name = "Clamshells needed to win"
     range_start = 1
     range_end = 100
     default = 13
     
 class ClamshellsInPool(Range):
-    """This determines how many clamshells end up in the pool.  You probably only care if your goal is set to clamshells."""
+    """
+    Select how many total Clamshells are in the item pool.  You probably only care if your goal is set to Clamshells.
+    """
     display_name = "Clamshells in the pool"
     range_start = 1
     range_end = 100
     default = 19
 
+"""Location Options"""
 class RandomizeJobs(DefaultOnToggle):
     """
-    When enabled, Jobs can be found anywhere, when disabled they are always on crystals.
+    When enabled, Jobs can be found anywhere; when disabled, they are always on crystals.
     """
     display_name = "Randomize Jobs"
 
@@ -48,52 +65,28 @@ class RandomizeStartingJobs(DefaultOnToggle):
     """
     display_name = "Randomize starting Jobs"
 
-class NewWorldStoneJobQuantity(Range):
-    """If your goal is Astley, this is where you select how many jobs you need to find before being sent the new world stone (NOTE: the starting jobs do not count towards this number)."""
-    display_name = "Job count that locks the new world stone"
-    range_start = 1
-    range_end = 18
-    default = 18
+class KillBossesMode(DefaultOnToggle):
+    """
+    When enabled, defeating a boss will provide checks.
+    """
+    display_name = "Kill Bosses Mode"
 
-class EasyLeveling(DefaultOnToggle):
-    """
-    When enabled, characters will not recieve reduced exp for being dead nor for being a higher level than the fought enemy.
-    """
-    display_name = "Easy Leveling"
-
-class RandomizeMusic(DefaultOnToggle):
-    """
-    When enabled, music will be randomized upon connecting to the AP World.
-    """
-    display_name = "Randomize Music"
-
-class StartWithTreasureFinder(DefaultOnToggle):
-    """
-    When enabled, the player will start with the treasure finder in their inventory.
-    """
-    display_name = "Begin with Treasure Finder"
-
-class StartWithMaps(DefaultOnToggle):
-    """
-    When enabled, the player will start with all of the maps in their inventory.
-    """
-    display_name = "Begin with Area Maps"
 
 class IncludedRegions(Choice):
     """
     Choose which regions will have locations worth checking in them.
-    
-    Any treasures, npcs, and crystals outside of this region will not give you checks when you interact with them.
+
+    Any Treasures, NPCs, or Crystals outside of this region will not give you checks when you interact with them.
 
     This can be used to make your game shorter/easier or longer/harder.
-    
+
     Beginner: Only regions up through the end of Skumparadise will have checks.
-    
-    Advanced: Regions up to Salmon Bay will have checks
 
-    Expert: Regions up to the deep sea will have checks (note the depths will not be included)
+    Advanced: Regions up to Salmon Bay will have checks.
 
-    All: Every region is included
+    Expert: Regions up to The Deep Sea will have checks. (Note: The Depths will not be included).
+
+    All: Every region is included.
     """
     display_name = "Regions to include in game"
     option_beginner = 0
@@ -101,6 +94,13 @@ class IncludedRegions(Choice):
     option_expert = 2
     option_all = 3
     default = 3
+
+"""Progression Options"""
+class EasyLeveling(DefaultOnToggle):
+    """
+    When enabled, characters will not receive reduced exp for being dead or for being a higher level than the fought enemy.
+    """
+    display_name = "Easy Leveling"
 
 class ProgressiveEquipmentMode(DefaultOnToggle):
     """
@@ -113,12 +113,12 @@ class ProgressiveEquipmentMode(DefaultOnToggle):
 class KeyMode(Choice):
     """
     Choose how keys behave.
-    
-    Skeleton Key: Only the skeleton key which can open any locked door will be in your item pool.
-    
-    Key Ring: One check will give all the keys needed to complete one dungeon, the skeleton key will still be in the pool NOT IMPLEMENTED
 
-    Vanilla: All vanilla keys will be included in checks
+    Skeleton Key: Only the Skeleton Key, which can open any locked door, will be in your item pool.
+
+    Key Ring: One check will give all keys needed to complete one dungeon. The Skeleton Key will still be in the pool. NOT IMPLEMENTED
+
+    Vanilla: All vanilla keys will be in the item pool.
     """
     display_name = "Key Mode"
     option_skeleton = 0
@@ -126,40 +126,56 @@ class KeyMode(Choice):
     option_vanilla = 2
     default = 2
 
-class KillBossesMode(DefaultOnToggle):
+"""Item Pool Options"""
+class StartWithTreasureFinder(DefaultOnToggle):
     """
-    When enabled, defeating a boss will provide checks.
+    When enabled, the player will start with the Treasure Finder in their inventory. When disabled, it will be in the item pool.
+
+    Note: Having the Treasure Finder is required for the in-game minimap to display nearby checks.
     """
-    display_name = "Kill Bosses Mode"
+    display_name = "Begin with Treasure Finder"
+
+class StartWithMaps(DefaultOnToggle):
+    """
+    When enabled, the player will start with all maps in their inventory.
+    """
+    display_name = "Begin with Area Maps"
 
 class IncludeSummonAbilities(DefaultOnToggle):
     """
-    When enabled, Summon abilities can be found anywhere, when disabled they will be in the regular spot.
+    When enabled, Summon abilities can be found anywhere; when disabled, they will be in the regular spot.
     """
     display_name = "Include Summoner Abilities in the item pool"
     
 class IncludeScholarAbilities(DefaultOnToggle):
     """
-    When enabled, Scholar abilities can be found anywhere, they can still be learned as normal from enemies.
+    When enabled, Scholar abilities can be found anywhere. They can still be learned as normal from enemies.
     """
     display_name = "Include Scholar Abilities in the item pool"
+
+"""Bonus Fun"""
+class RandomizeMusic(DefaultOnToggle):
+    """
+    When enabled, music will be randomized upon connecting to the AP World.
+    """
+    display_name = "Randomize Music"
 
 @dataclass
 class CrystalProjectOptions(PerGameCommonOptions):
     start_inventory_from_pool: StartInventoryPool
     goal: Goal
+    newWorldStoneJobQuantity: NewWorldStoneJobQuantity
     clamshellsQuantity: ClamshellsQuantity
     clamshellsInPool: ClamshellsInPool
     randomizeJobs: RandomizeJobs
     randomizeStartingJobs: RandomizeStartingJobs
-    newWorldStoneJobQuantity: NewWorldStoneJobQuantity
-    easyLeveling: EasyLeveling
-    randomizeMusic: RandomizeMusic
-    startWithTreasureFinder: StartWithTreasureFinder
-    startWithMaps: StartWithMaps
+    killBossesMode: KillBossesMode
     includedRegions: IncludedRegions
+    easyLeveling: EasyLeveling
     progressiveEquipmentMode: ProgressiveEquipmentMode
     keyMode: KeyMode
-    killBossesMode: KillBossesMode
+    startWithTreasureFinder: StartWithTreasureFinder
+    startWithMaps: StartWithMaps
     includeSummonAbilities: IncludeSummonAbilities
     includeScholarAbilities: IncludeScholarAbilities
+    randomizeMusic: RandomizeMusic
