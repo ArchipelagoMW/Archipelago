5267c2c600cc34369bde8a28e308a7adcb427605
massimilianodelliubaldini <8584296+massimilianodelliubaldini@users.noreply.github.com>
2025-05-10 23:08:20 -0400

    use f words

--- WebHostLib/generate.py
+++ WebHostLib/generate.py
@@ -107,7 +107,7 @@ def start_generation(options: dict[str, dict | str], meta: dict[str, Any]):
             meta["error"] = f"{e.__class__.__name__}: {e}"
             if e.__cause__:
                 cause = e.__cause__
-                meta["source"] = cause.__class__.__name__ + ": " + str(cause)
+                meta["source"] = f"{cause.__class__.__name__}: {str(cause)}"
             details = json.dumps(meta, indent=4).strip()
             return render_template("seedError.html", seed_error=meta["error"], details=details)
 
@@ -185,7 +185,7 @@ def gen_game(gen_options: dict, meta: dict[str, Any] | None = None, owner=None, 
                             e.__class__.__name__ + ": " + str(e))
                     if e.__cause__:
                         cause = e.__cause__
-                        meta["source"] = cause.__class__.__name__ + ": " + str(cause)
+                        meta["source"] = f"{cause.__class__.__name__}: {str(cause)}"
                     gen.meta = json.dumps(meta)
                     commit()
     except BaseException as e:
@@ -195,10 +195,10 @@ def gen_game(gen_options: dict, meta: dict[str, Any] | None = None, owner=None, 
                 if gen is not None:
                     gen.state = STATE_ERROR
                     meta = json.loads(gen.meta)
-                    meta["error"] = (e.__class__.__name__ + ": " + str(e))
+                    meta["error"] = f"{e.__class__.__name__}: {str(e)}"
                     if e.__cause__:
                         cause = e.__cause__
-                        meta["source"] = cause.__class__.__name__ + ": " + str(cause)
+                        meta["source"] = f"{cause.__class__.__name__}: {str(cause)}"
                     gen.meta = json.dumps(meta)
                     commit()
         raise
