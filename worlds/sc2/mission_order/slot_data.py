7aca512da39e93f386b5579b1f237b55c10422e9
Ziktofel <ziktofel@gmail.com>
2025-05-21 00:31:00 +0200

    Use Protocol for inheritance

--- worlds/sc2/mission_order/slot_data.py
+++ worlds/sc2/mission_order/slot_data.py
@@ -4,13 +4,12 @@ Creating these is handled by the nodes they represent in .nodes.py.
 """
 
 from __future__ import annotations
-from typing import List
+from typing import List, Protocol
 from dataclasses import dataclass
 
 from .entry_rules import SubRuleRuleData
 
-@dataclass
-class MissionOrderObjectSlotData:
+class MissionOrderObjectSlotData(Protocol):
     entry_rule: SubRuleRuleData
 
 @dataclass
@@ -20,12 +19,6 @@ class CampaignSlotData(MissionOrderObjectSlotData):
     exits: List[int]
     layouts: List[LayoutSlotData]
 
-    def __init__(self, name, entry_rule, exits, layouts):
-        self.name = name
-        self.entry_rule = entry_rule
-        self.exits = exits
-        self.layouts = layouts
-
     @staticmethod
     def legacy(name: str, layouts: List[LayoutSlotData]) -> CampaignSlotData:
         return CampaignSlotData(name, SubRuleRuleData.empty(), [], layouts)
@@ -34,14 +27,9 @@ class CampaignSlotData(MissionOrderObjectSlotData):
 @dataclass
 class LayoutSlotData(MissionOrderObjectSlotData):
     name: str
+    entry_rule: SubRuleRuleData
     exits: List[int]
     missions: List[List[MissionSlotData]]
-
-    def __init__(self, name, entry_rule, exits, missions):
-        self.name = name
-        self.entry_rule = entry_rule
-        self.exits = exits
-        self.missions = missions
 
     @staticmethod
     def legacy(name: str, missions: List[List[MissionSlotData]]) -> LayoutSlotData:
@@ -52,13 +40,8 @@ class LayoutSlotData(MissionOrderObjectSlotData):
 class MissionSlotData(MissionOrderObjectSlotData):
     mission_id: int
     prev_mission_ids: List[int]
+    entry_rule: SubRuleRuleData
     victory_cache_size: int = 0
-
-    def __init__(self, mission_id, entry_rule, prev_mission_ids, victory_cache_size = 0):
-        self.mission_id = mission_id
-        self.entry_rule = entry_rule
-        self.prev_mission_ids = prev_mission_ids
-        self.victory_cache_size = victory_cache_size
 
     @staticmethod
     def empty() -> MissionSlotData:
