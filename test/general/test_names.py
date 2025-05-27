c9d4c3b30f65f4a80d11ddc5db06720596314f29
Thomas Barlow <github@mysterymayhem.co.uk>
2025-05-27 21:43:48 +0100

    Also test item/location names in item/location groups

--- test/general/test_names.py
+++ test/general/test_names.py
@@ -4,11 +4,15 @@ from . import setup_solo_multiworld
 
 
 class TestNames(unittest.TestCase):
-    def assertIsStr(self, v: str):
+    def assertIsStr(self, v: str, extra_msg: str | None = None):
         """Helper for asserting the type of a value is exactly str, with a more helpful failure message."""
         # The default message would only print the type of the value, but printing the value itself can be helpful in
         # tracking down where the non-str type has come from.
-        self.assertIs(type(v), str, f"Expected plain str type, but got '{v}' of type '{type(v)}'")
+        msg = f"Expected plain str type, but got '{v}' of type '{type(v)}'"
+        # Add an extra message to the end if one is provided.
+        if extra_msg:
+            msg += f" - {extra_msg}"
+        self.assertIs(type(v), str, msg)
 
     def test_item_names_format(self) -> None:
         """Item names must not be all numeric in order to differentiate between ID and name in !hint"""
@@ -34,11 +38,15 @@ class TestNames(unittest.TestCase):
                     for location_name in world_type.location_name_to_id:
                         self.assertIsStr(location_name)
                 with self.subTest("location_name_groups"):
-                    for location_group_name in world_type.location_name_groups:
+                    for location_group_name, names_in_group in world_type.location_name_groups.items():
                         self.assertIsStr(location_group_name)
+                        for name in names_in_group:
+                            self.assertIsStr(name, f"Location name within location group '{location_group_name}'")
                 with self.subTest("item_name_groups"):
-                    for item_group_name in world_type.item_name_groups:
+                    for item_group_name, names_in_group in world_type.item_name_groups.items():
                         self.assertIsStr(item_group_name)
+                        for name in names_in_group:
+                            self.assertIsStr(name, f"Item name within item group '{item_group_name}'")
 
                 multiworld = setup_solo_multiworld(world_type)
 
