f0ccd6a217743599d73110dc294fe9520fd2da86
BootsinSoots <theron.swanson@gmail.com>
2025-06-02 21:02:17 -0400

    Minor docstring typo

--- worlds/luigismansion/LuigiOptions.py
+++ worlds/luigismansion/LuigiOptions.py
@@ -206,13 +206,14 @@ class Furnisanity(OptionSet):
     Adds interactable objects, such as dressers, paintings, candles, and light fixtures, to the location pool.
 
     Different sets of locations can be added within the list. Valid strings are:
+
     "Hangables" includes items on walls such as paintings and other decor
 
     "Decor" includes items such as instruments and suits of armor
 
     "Ceiling" includes ceiling fans and lights attached to the ceiling
 
-    "Candles" includes any candles tat can be interacted with
+    "Candles" includes any candles that can be interacted with
 
     "Seating" includes chairs, stools and other typs of seating
 
