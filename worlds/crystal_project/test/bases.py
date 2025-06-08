ae8a3c34ad0d88872629d4547c0d62cae7197d7e
Emerassi <78173681+Emerassi@users.noreply.github.com>
2025-06-08 15:37:28 -0700

    create assert_region_entrances function to allow testing all the exits from a region behave as expected.

--- worlds/crystal_project/test/bases.py
+++ worlds/crystal_project/test/bases.py
@@ -1,5 +1,27 @@
 from test.bases import WorldTestBase
+from ..constants.regions import *
 
 
 class CrystalProjectTestBase(WorldTestBase):
     game = "Crystal Project"
+
+    def assert_region_entrances(self, region: str, reachable_regions: tuple[str, ...] | None=None, unreachable_regions: tuple[str, ...] | None=None):
+        menu_entrance: str = MENU + " -> " + region
+        menu_entrances = self.multiworld.get_entrances(self.world.player)
+        menu_entrance_exists: bool = False
+        for entrance in menu_entrances:
+            if entrance.name == menu_entrance:
+                menu_entrance_exists = True
+
+        if not menu_entrance_exists:
+            self.multiworld.get_region(MENU, self.world.player).add_exits([region])
+
+        if isinstance(reachable_regions, tuple):
+            for reachable in reachable_regions:
+                with self.subTest(msg="Test Can Reach Entrance", region=region, reachable_region=reachable):
+                    self.assertTrue(self.can_reach_entrance(region + " -> " + reachable))
+
+        if isinstance(unreachable_regions, tuple):
+            for unreachable in unreachable_regions:
+                with self.subTest(msg="Test Cannot Reach Entrance", region=region, unreachable_region=unreachable):
+                    self.assertFalse(self.can_reach_entrance(region + " -> " + unreachable))
--- worlds/crystal_project/test/test_level_options.py
+++ worlds/crystal_project/test/test_level_options.py
@@ -1,7 +1,7 @@
+from .bases import CrystalProjectTestBase
 from ..constants.key_items import *
 from ..constants.keys import *
 from ..constants.regions import *
-from .bases import CrystalProjectTestBase
 
 
 class TestLevelGatingOff(CrystalProjectTestBase):
@@ -29,3 +29,10 @@ class TestLevelGatingOn(CrystalProjectTestBase):
         self.assertFalse(self.can_reach_region(ANCIENT_RESERVOIR))
         self.collect_by_name([PROGRESSIVE_LEVEL_CAP, PROGRESSIVE_LEVEL_CAP])
         self.assertTrue(self.can_reach_region(ANCIENT_RESERVOIR))
+
+    # leaving this on temporarily so you can see an example with things like how to make a tuple with only one element etc, delete once we have more tests using assert_region_entrances
+    def test_new_function_test(self):
+        self.assert_region_entrances(IBEK_CAVE, unreachable_regions=(SARA_SARA_BEACH,))
+        self.collect_by_name("Item - Ibek Bell")
+        self.collect_by_name([PROGRESSIVE_LEVEL_CAP, PROGRESSIVE_LEVEL_CAP])
+        self.assert_region_entrances(IBEK_CAVE, reachable_regions=(SARA_SARA_BEACH,))
