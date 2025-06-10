f9d94601dc631bc102cbf1ab5c90c56b13ba0841
Emerassi <78173681+Emerassi@users.noreply.github.com>
2025-06-10 14:29:48 -0700

    added helper function to test base for collect all level caps, mounts, or both.

--- worlds/crystal_project/test/bases.py
+++ worlds/crystal_project/test/bases.py
@@ -1,5 +1,7 @@
 from test.bases import WorldTestBase
+from ..constants.mounts import *
 from ..constants.regions import *
+from ..constants.key_items import *
 
 
 class CrystalProjectTestBase(WorldTestBase):
@@ -25,3 +27,18 @@ class CrystalProjectTestBase(WorldTestBase):
             for unreachable in unreachable_regions:
                 with self.subTest(msg="Test Cannot Reach Entrance", region=region, unreachable_region=unreachable):
                     self.assertFalse(self.can_reach_entrance(region + " -> " + unreachable))
+
+    def collect_mounts(self):
+        self.collect_by_name(
+            [PROGRESSIVE_SALMON_VIOLA, PROGRESSIVE_SALMON_VIOLA,
+             PROGRESSIVE_QUINTAR_WOODWIND, PROGRESSIVE_QUINTAR_WOODWIND, PROGRESSIVE_QUINTAR_WOODWIND,
+             IBEK_BELL,
+             OWL_DRUM])
+
+    def collect_level_caps(self):
+        self.collect_by_name(
+            [PROGRESSIVE_LEVEL_CAP, PROGRESSIVE_LEVEL_CAP, PROGRESSIVE_LEVEL_CAP, PROGRESSIVE_LEVEL_CAP, PROGRESSIVE_LEVEL_CAP])
+
+    def collect_mounts_and_level_caps(self):
+        self.collect_mounts()
+        self.collect_level_caps()
