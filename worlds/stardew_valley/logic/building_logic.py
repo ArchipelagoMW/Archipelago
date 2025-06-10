81c9e8081120b71f1604d5e4333826af92aab8c4
Alex Gilbert <alexgilbert@yahoo.com>
2025-06-10 12:24:41 -0400

    - Add wizard building rules

--- worlds/stardew_valley/logic/building_logic.py
+++ worlds/stardew_valley/logic/building_logic.py
@@ -2,8 +2,13 @@ from functools import cached_property
 
 from Utils import cache_self1
 from .base_logic import BaseLogic, BaseLogicMixin
-from ..stardew_rule import StardewRule, true_
-from ..strings.building_names import Building
+from ..stardew_rule import StardewRule, true_, false_
+from ..strings.building_names import Building, WizardBuilding
+from ..strings.crop_names import Fruit
+from ..strings.fish_names import Fish, WaterItem
+from ..strings.forageable_names import Forageable
+from ..strings.material_names import Material
+from ..strings.metal_names import MetalBar, Mineral
 from ..strings.region_names import Region
 
 AUTO_BUILDING_BUILDINGS = {Building.shipping_bin, Building.pet_bowl, Building.farm_house}
@@ -50,3 +55,19 @@ class BuildingLogic(BaseLogic):
     @cached_property
     def can_construct_buildings(self) -> StardewRule:
         return self.logic.region.can_reach(Region.carpenter)
+
+    def can_purchase_wizard_blueprint(self, building_name: str) -> StardewRule:
+        rule = self.logic.region.can_reach(Region.wizard_tower) & self.logic.received("Magic Ink")
+        if building_name == WizardBuilding.earth_obelisk:
+            return rule & self.logic.money.can_spend(500_000) & self.logic.has_all(MetalBar.iridium, Mineral.earth_crystal)
+        if building_name == WizardBuilding.water_obelisk:
+            return rule & self.logic.money.can_spend(500_000) & self.logic.has_all(MetalBar.iridium, Fish.clam, WaterItem.coral)
+        if building_name == WizardBuilding.desert_obelisk:
+            return rule & self.logic.money.can_spend(1_000_000) & self.logic.has_all(MetalBar.iridium, Forageable.coconut, Forageable.cactus_fruit)
+        if building_name == WizardBuilding.desert_obelisk:
+            return rule & self.logic.money.can_spend(1_000_000) & self.logic.has_all(MetalBar.iridium, Forageable.dragon_tooth, Fruit.banana)
+        if building_name == WizardBuilding.junimo_hut:
+            return rule & self.logic.money.can_spend(20_000) & self.logic.has_all(MetalBar.iridium, Material.stone, Fruit.starfruit, Material.fiber)
+        if building_name == WizardBuilding.gold_clock:
+            return rule & self.logic.money.can_spend(10_000_000)
+        return false_
