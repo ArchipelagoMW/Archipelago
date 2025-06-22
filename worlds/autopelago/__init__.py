5c8f67d0cd7eb088bfae9f9c655dabadfc6dba76
Joe Amenta <airbreather@linux.com>
2025-06-22 08:00:59 -0400

    Fix an oversight in the previous fix that would cause the Easter egg item replacements to fail for games that didn't have at least one replacement for each category.

--- worlds/autopelago/__init__.py
+++ worlds/autopelago/__init__.py
@@ -180,7 +180,7 @@ class AutopelagoWorld(World):
         nonprogression_item_table = {c: [item_name for item_name in items if item_name not in excluded_names]
                                      for c, items in generic_nonprogression_item_table.items()}
         for category, items in nonprogression_item_table.items():
-            dlc_games = set(game_specific_nonprogression_items)
+            dlc_games = {g for g, categories in game_specific_nonprogression_items.items() if category in categories}
             self.multiworld.random.shuffle(items)
             replacements_made = 0
             for game_name in self.multiworld.game.values():
