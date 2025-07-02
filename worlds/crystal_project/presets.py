a3c8b2310e0b9d4304c7b324dc2d104d4cdc4534
dragonsbutalsorabbits <kathrynibarber@gmail.com>
2025-07-01 19:32:08 -0700

    updated explorer mode option preset settings

--- worlds/crystal_project/presets.py
+++ worlds/crystal_project/presets.py
@@ -7,15 +7,16 @@ from .constants.key_items import *
 explorer_mode_options = {
     "progression_balancing":         ProgressionBalancing.default,
     "goal":                          Goal.option_clamshells,
-    "clamshellGoalQuantity":         40,
+    "clamshellGoalQuantity":         50,
     "extraClamshellsInPool":         10,
     "shopsanity":                    Shopsanity.option_disabled,
-    "progressiveMountMode":          ProgressiveMountMode.option_false,
+    "regionsanity":                  Regionsanity.option_true,
+    "progressiveMountMode":          ProgressiveMountMode.option_true,
     "levelGating":                   LevelGating.option_level_catch_up,
     "progressiveLevelSize":          10,
     "maxLevel":                      99,
     "keyMode":                       KeyMode.option_vanilla_skelefree,
-    "start_inventory_from_pool":     {PROGRESSIVE_LEVEL: 5},
+    "start_inventory_from_pool":     {PROGRESSIVE_LEVEL: 9},
 }
 
 crystal_project_options_presets: Dict[str, Dict[str, Any]] = {
