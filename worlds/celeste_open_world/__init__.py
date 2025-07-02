343fc7aeee823b65368027bdbd52b9e9e9329c11
PoryGone <porygoneplays@gmail.com>
2025-07-02 02:45:50 -0400

    Increment APWorld Version

--- worlds/celeste_open_world/__init__.py
+++ worlds/celeste_open_world/__init__.py
@@ -251,7 +251,7 @@ class CelesteOpenWorld(World):
 
     def fill_slot_data(self):
         return {
-            "apworld_version": 10003,
+            "apworld_version": 10004,
             "min_mod_version": 10000,
 
             "death_link": self.options.death_link.value,
