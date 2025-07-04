7ffadb0fee9c33dd2837c893e16b867b3a57fce4
Matthew <matthew.marinets@gmail.com>
2025-07-04 11:28:39 -0700

    sc2: Updating exclude_overpowered_items comment again to be clear the override is per-item

--- worlds/sc2/options.py
+++ worlds/sc2/options.py
@@ -467,7 +467,8 @@ class ExcludeOverpoweredItems(Toggle):
     If turned on, a curated list of very strong items are excluded.
     These items were selected for promoting repetitive strategies, or for providing a lot of power in a boring way.
     Recommended off for players looking for a challenge or for repeat playthroughs.
-    Excluded Items overrides this option rather than add to it. Locked items will override this option.
+    Excluding an OP item overrides the exclusion from this item rather than add to it.
+    OP items may be unexcluded or locked with Unexcluded Items or Locked Items options.
     """
     display_name = "Exclude Overpowered Items"
 
