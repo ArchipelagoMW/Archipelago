879ae1fc9d0b6da6b8f9947f8bba3e6d6b766bc1
mobby45 <68152858+mobby45@users.noreply.github.com>
2025-07-17 20:05:01 +0200

    Update worlds/tww/__init__.py
    Co-authored-by: Jonathan Tan <tanjo3@users.noreply.github.com>

--- worlds/tww/__init__.py
+++ worlds/tww/__init__.py
@@ -83,7 +83,7 @@ class TWWWeb(WebWorld):
     The web interface includes the setup guide and the options page for generating YAMLs.
     """
 
-        tutorials = [
+    tutorials = [
         Tutorial(
             "Multiworld Setup Guide",
             "A guide to setting up the Archipelago The Wind Waker software on your computer.",
