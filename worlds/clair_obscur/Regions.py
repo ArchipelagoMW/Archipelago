0a67a5d9b129a0ecdb18a460a21cc4d78503ae6e
Jostar <joshgmac@gmail.com>
2025-07-19 14:01:27 -0300

    Preliminary logic added
    World Map split into regions by esquie abilities, esquie items replaced with 5 progressive items, regions connected and access rules for items added to the entrances.

--- worlds/clair_obscur/Regions.py
+++ worlds/clair_obscur/Regions.py
@@ -3,30 +3,24 @@ from typing import Dict, Tuple, List
 from BaseClasses import Region
 from worlds.clair_obscur import ClairObscurWorld
 from worlds.clair_obscur.Data import data
+from worlds.generic.Rules import set_rule
 
 
 def create_regions(world: ClairObscurWorld) -> dict[str, Region]:
     regions: Dict[str, Region] = {}
-
+    regions["Menu"] = Region("Menu", world.player, world.multiworld)
+    world.multiworld.regions.append(regions["Menu"])
 
     for region_name, region_data in data.regions.items():
         new_region = Region(region_name, world.player, world.multiworld)
 
         regions[region_name] = new_region
         world.multiworld.regions.append(new_region)
-
-
-    for region_name in regions:
-        region_data = regions[region_name]
-        if region_name == "World Map":
-            continue
-        region_data.connect(regions["World Map"], f"{region_name} -> World Map")
-        regions["World Map"].connect(region_data, f"World Map -> {region_name}")
-
-
-    regions["Menu"] = Region("Menu", world.player, world.multiworld)
-    world.multiworld.regions.append(regions["Menu"])
-    regions["Menu"].connect(regions["World Map"])
-    regions["World Map"].connect(regions["Menu"])
+        parent = world.multiworld.get_region(region_data.parent_map, world.player)
+        entrance = parent.connect(new_region)
+        if region_data.condition:
+            for cond in region_data.condition.keys():
+                amount = region_data.condition[cond]
+                set_rule(entrance, lambda state: state.has(cond, world.player, amount))
 
     return regions
