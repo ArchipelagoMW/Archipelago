8a305929358ad6d83df994e66d5d272cfa5c581a
Jouramie <16137441+Jouramie@users.noreply.github.com>
2025-07-20 14:13:59 -0400

    fix test post classification

--- worlds/stardew_valley/test/test_options/TestToolOptions.py
+++ worlds/stardew_valley/test/test_options/TestToolOptions.py
@@ -27,11 +27,16 @@ class TestToolProgression(SVTestBase):
 
     def test_given_progressive_when_generate_then_last_trash_can_is_classified_differently(self):
         trash_cans = self.get_items_by_name(APTool.trash_can)
-        # If we run fill tests, then it should have the post_fill classification.
-        # If not, it should have the pre-fill classification.
-        expected_classification = ItemClassification.useful if self.skip_default_tests else ItemClassification.progression_skip_balancing
         progressive_count = sum([1 for item in trash_cans if item.classification == ItemClassification.progression])
-        special_count = sum([1 for item in trash_cans if item.classification == expected_classification])
+        special_count = sum([1 for item in trash_cans if item.classification == ItemClassification.useful])
 
         self.assertEqual(3, progressive_count)
         self.assertEqual(1, special_count)
+
+    def test_given_progressive_when_generate_then_last_trash_can_changes_classification_post_fill(self):
+        trash_can, post_fill_classification = next((classification_to_update
+                                                    for classification_to_update in self.world.classifications_to_override_post_fill
+                                                    if classification_to_update[0].name == APTool.trash_can),
+                                                   (None, None))
+
+        self.assertEqual(post_fill_classification, ItemClassification.progression_skip_balancing)
