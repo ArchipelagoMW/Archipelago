2d00e2369afc961541018e41ef1ddfa4b4c37e6b
SomeJakeGuy <somejakeguy@gmail.com>
2025-07-20 20:08:42 -0400

    Changes LM Client Vacuum speed increase based on variable vacuum available in received items

--- worlds/luigismansion/LMClient.py
+++ worlds/luigismansion/LMClient.py
@@ -580,10 +580,10 @@ class LMContext(CommonContext):
             return
 
         # Always adjust the Vacuum speed as saving and quitting or going to E. Gadds lab could reset it back to normal.
-        if any([netItem.item for netItem in self.items_received if netItem.item == 8064]): # TODO update for variable amounts of this item
+        vac_count = len(list(netItem.item for netItem in self.items_received if netItem.item == 8064))
+        if vac_count >= 2:
             vac_speed = "3800000F"
-            vac_item = next(netItem.item for netItem in self.items_received if netItem.item == 8064)
-            lm_item_name = self.item_names.lookup_in_game(vac_item)
+            lm_item_name = self.item_names.lookup_in_game(8064)
             lm_item = ALL_ITEMS_TABLE[lm_item_name]
             for addr_to_update in lm_item.update_ram_addr:
                 dme.write_bytes(addr_to_update.ram_addr, bytes.fromhex(vac_speed))
