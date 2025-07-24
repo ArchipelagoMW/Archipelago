f1b784b7e7ab65a6a484311f259264e511e41fdc
Jostar <joshgmac@gmail.com>
2025-07-23 21:41:20 -0300

    Logic update
    Logic now properly considers esquie and barrier breaker. Painted power included as well, for any flight-required areas with combat. Manor and Mini Level chests split into appropriate regions. Conditions can be added to specific locations through locations.json.

--- worlds/clair_obscur/Regions.py
+++ worlds/clair_obscur/Regions.py
@@ -3,24 +3,34 @@ from typing import Dict, Tuple, List
 from BaseClasses import Region
 from worlds.clair_obscur import ClairObscurWorld
 from worlds.clair_obscur.Data import data
-from worlds.generic.Rules import set_rule
+from worlds.generic.Rules import set_rule, add_rule
 
 
 def create_regions(world: ClairObscurWorld) -> dict[str, Region]:
     regions: Dict[str, Region] = {}
-    regions["Menu"] = Region("Menu", world.player, world.multiworld)
-    world.multiworld.regions.append(regions["Menu"])
 
+    #Create menu region
+    menu = Region("Menu", world.player, world.multiworld)
+    world.multiworld.regions.append(menu)
+
+    #Create regions and add to multiworld
     for region_name, region_data in data.regions.items():
         new_region = Region(region_name, world.player, world.multiworld)
 
         regions[region_name] = new_region
+        # parent = world.multiworld.get_region(region_data.parent_map, world.player)
+        # parent.connect(new_region)
+        # parent.create_exit(region_name)
         world.multiworld.regions.append(new_region)
-        parent = world.multiworld.get_region(region_data.parent_map, world.player)
-        entrance = parent.connect(new_region)
+
+    #After all regions are added, connect them
+    for region_name, region_data in data.regions.items():
+        from_reg = world.multiworld.get_region(region_data.parent_map, world.player)
+        to_reg = world.multiworld.get_region(region_name, world.player)
+        connection = from_reg.connect(to_reg)
         if region_data.condition:
             for cond in region_data.condition.keys():
                 amount = region_data.condition[cond]
-                set_rule(entrance, lambda state: state.has(cond, world.player, amount))
+                add_rule(connection, lambda state, con=cond, pl=world.player, am=amount: state.has(con, pl, am))
 
     return regions
