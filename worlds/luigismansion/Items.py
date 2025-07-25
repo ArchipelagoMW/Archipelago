fe7f2205a58cbd1cf1267c76e7b1c7fa90b982cf
BootsinSoots <theron.swanson@gmail.com>
2025-07-25 17:36:03 -0400

    Fix trap addresses

--- worlds/luigismansion/Items.py
+++ worlds/luigismansion/Items.py
@@ -254,28 +254,28 @@ other_filler_items: Dict[str, LMItemData] = {
 
 trap_filler_items: Dict[str, LMItemData] = {
     "Possession Trap": LMItemData("Trap", 141, IC.trap,
-        update_ram_addr=[LMRamData(0x804DDE7C, ram_byte_size=4, item_count=5)]),
+        update_ram_addr=[LMRamData(0x804DDF18, ram_byte_size=4, item_count=5)]),
     "Bonk Trap": LMItemData("Trap", 142, IC.trap,
-        update_ram_addr=[LMRamData(0x804DDE7C, ram_byte_size=4, item_count=4)]),
+        update_ram_addr=[LMRamData(0x804DDF18, ram_byte_size=4, item_count=4)]),
     "Bomb": LMItemData("Trap", 130, IC.trap,
-        update_ram_addr=[LMRamData(0x804DDE7C, ram_byte_size=4, item_count=1)]), # value of 1
+        update_ram_addr=[LMRamData(0x804DDF18, ram_byte_size=4, item_count=1)]), # value of 1
     "Ice Trap": LMItemData("Trap", 131, IC.trap,
-        update_ram_addr=[LMRamData(0x804DDE7C, ram_byte_size=4, item_count=3)]), # value of 3
+        update_ram_addr=[LMRamData(0x804DDF18, ram_byte_size=4, item_count=3)]), # value of 3
     "Banana Trap": LMItemData("Trap", 132, IC.trap,
-        update_ram_addr=[LMRamData(0x804DDE7C, ram_byte_size=4, item_count=2)]), # value of 2
+        update_ram_addr=[LMRamData(0x804DDF18, ram_byte_size=4, item_count=2)]), # value of 2
     "Poison Mushroom": LMItemData("Trap", 125, IC.trap,
-        update_ram_addr=[LMRamData(0x804DDE7C, ram_byte_size=4, item_count=6)]), # value of 6
+        update_ram_addr=[LMRamData(0x804DDF18, ram_byte_size=4, item_count=6)]), # value of 6
     "Ghost": LMItemData("Trap", 126, IC.trap,
         update_ram_addr=[LMRamData(0x804DDA50, ram_byte_size=4, item_count=1)]),
     "Fear Trap": LMItemData("Trap", 143, IC.trap,
-        update_ram_addr=[LMRamData(0x804DDE7C, ram_byte_size=4, item_count=8)]), #Crawling
+        update_ram_addr=[LMRamData(0x804DDF18, ram_byte_size=4, item_count=8)]), #Crawling
     # "Drag Trap": LMItemData("Trap", 144, IC.trap, update_ram_addr=),
     "Spooky Time": LMItemData("Trap", 145, IC.trap,
-        update_ram_addr=[LMRamData(0x804de478, ram_byte_size=4, item_count=150)]),
+        update_ram_addr=[LMRamData(0x804de470, ram_byte_size=4, item_count=150)]),
     "Squash Trap": LMItemData("Trap", 146, IC.trap,
-        update_ram_addr=[LMRamData(0x804DDE7C, ram_byte_size=4, item_count=7)]),
+        update_ram_addr=[LMRamData(0x804DDF18, ram_byte_size=4, item_count=7)]),
     "No Vac Trap": LMItemData("Trap", 147, IC.trap,
-        update_ram_addr=[LMRamData(0x804DDE7C, ram_byte_size=4, item_count=9)])
+        update_ram_addr=[LMRamData(0x804DDF18, ram_byte_size=4, item_count=9)])
 }
 
 filler_items = {**other_filler_items,
