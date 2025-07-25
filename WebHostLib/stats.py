4e8f4bf14fc463104d8318397ea003146289969a
Fabian Dill <fabian.dill@web.de>
2025-07-26 01:18:57 +0200

    WebHost: list unrecognized games as Other in stats

--- WebHostLib/stats.py
+++ WebHostLib/stats.py
@@ -26,9 +26,11 @@ def get_db_data(known_games: typing.Set[str]) -> typing.Tuple[typing.Counter[str
     room: Room
     for room in select(room for room in Room if room.creation_time >= cutoff):
         for slot in room.seed.slots:
-            if slot.game in known_games:
-                total_games[slot.game] += 1
-                games_played[room.creation_time.date()][slot.game] += 1
+            current_game = slot.game
+            if slot.game not in known_games:
+                current_game = "Other"
+            total_games[current_game] += 1
+            games_played[room.creation_time.date()][current_game] += 1
     return total_games, games_played
 
 
