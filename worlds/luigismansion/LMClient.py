3065158111750f7bd465bcd355b538c2fc1d8ed2
SomeJakeGuy <somejakeguy@gmail.com>
2025-07-28 01:31:02 -0400

    Fixed dolphin misspelling

--- worlds/luigismansion/LMClient.py
+++ worlds/luigismansion/LMClient.py
@@ -27,7 +27,7 @@ CONNECTION_LOST_STATUS = "Dolphin connection was lost. Please restart your emula
 NO_SLOT_NAME_STATUS = "No slot name was detected. Ensure a randomized ROM is loaded. Retrying in 5 seconds..."
 CONNECTION_VERIFY_SERVER = "Dolphin was confirmed to be opened and ready, Connect to the server when ready..."
 CONNECTION_INITIAL_STATUS = "Dolphin emulator was not detected to be running. Retrying in 5 seconds..."
-CONNECTION_CONNECTED_STATUS = "Dolpin is connected, AP is connected, Ready to play LM!"
+CONNECTION_CONNECTED_STATUS = "Dolphin is connected, AP is connected, Ready to play LM!"
 
 # This is the address that holds the player's slot name.
 # This way, the player does not have to manually authenticate their slot name.
