a0b13e41606e061896b976c1634a7338d4c26388
Jostar <joshgmac@gmail.com>
2025-08-06 10:50:38 -0300

    Updated to handle location display names
    Some conditions needed to be removed from locations. Generates again. Underwater items added to location pool and proper region.

--- worlds/clair_obscur/Options.py
+++ worlds/clair_obscur/Options.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from Options import Choice, PerGameCommonOptions, StartInventory, OptionGroup
+from Options import Choice, PerGameCommonOptions, StartInventory, OptionGroup, Toggle
 
 
 class Goal(Choice):
@@ -15,6 +15,28 @@ class Goal(Choice):
     option_simon = 3
 
     default = 1
+
+class ExcludeLocations(Choice):
+    """
+    This will remove all Endless Tower locations from the pool.
+    None: No locations excluded.
+    Endless Tower:
+    """
+    internal_name = "exclude_endgame_locations"
+    display_name = "Exclude Endgame Locations"
+    option_none = 0
+    option_endless = 1
+    option_drafts = 2
+    option_both = 3
+    default = 3
+
+class ShuffleLostGestrals:
+    internal_name = "shuffle_lost_gestrals"
+    display_name = "Shuffle Lost Gestrals"
+    option_false = 0
+    option_gestrals = 1
+    option_rewards = 2
+    option_both = 3
 
 class ClairObscurStartInventory(StartInventory):
     """
