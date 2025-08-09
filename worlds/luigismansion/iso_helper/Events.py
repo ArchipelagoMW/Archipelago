8c34bec18b3f24f6ceb3f7fa6b035c41dd4bd15b
SomeJakeGuy <somejakeguy@gmail.com>
2025-08-09 13:07:33 -0400

    Added a small fix for clairvoya to stop disappearing unexpectedly.

--- worlds/luigismansion/iso_helper/Events.py
+++ worlds/luigismansion/iso_helper/Events.py
@@ -99,8 +99,8 @@ def update_blackout_event(gcm: GCM) -> GCM:
 
 # Updates all common events
 def update_common_events(gcm: GCM, randomize_mice: bool) -> GCM:
-    list_custom_events = ["03", "10", "22", "23", "24", "28", "29", "33", "35", "38", "50", "61", "64", "65",
-     "66", "67", "68", "71", "72", "74", "75", "82", "86", "87", "88", "89", "90"]
+    list_custom_events = ["03", "10", "22", "23", "24", "28", "29", "33", "35", "37", "38", "50", "61", "64",
+        "65", "66", "67", "68", "71", "72", "74", "75", "82", "86", "87", "88", "89", "90"]
     if randomize_mice:
         list_custom_events += ["95", "97", "98", "99", "100"]
 
