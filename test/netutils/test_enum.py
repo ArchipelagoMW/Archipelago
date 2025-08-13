3fddfbd804b0786b3ed393e1a179ab9b4ed66152
black-sliver <59490463+black-sliver@users.noreply.github.com>
2025-08-13 02:06:05 +0200

    Test: add test for unpickling NetUtils enums
    This verifies that Utils.ByValue either works or is not required,
    and once we drop ByValue, this validates that future Python
    versions do not break our Enums again.

---
+++ test/netutils/test_enum.py
@@ -0,0 +1,36 @@
+"""Verify that NetUtils' enums work correctly with all supported Python versions."""
+
+import unittest
+from enum import Enum
+from typing import Type
+
+from NetUtils import ClientStatus, HintStatus, SlotType
+from Utils import restricted_dumps, restricted_loads
+
+
+class Base:
+    class DataEnumTest(unittest.TestCase):
+        type: Type[Enum]
+        value: Enum
+
+        def test_unpickle(self) -> None:
+            """Tests that enums used in multidata or multisave can be pickled and unpickled."""
+            pickled = restricted_dumps(self.value)
+            unpickled = restricted_loads(pickled)
+            self.assertEqual(unpickled, self.value)
+            self.assertIsInstance(unpickled, self.type)
+
+
+class HintStatusTest(Base.DataEnumTest):
+    type = HintStatus
+    value = HintStatus.HINT_AVOID
+
+
+class ClientStatusTest(Base.DataEnumTest):
+    type = ClientStatus
+    value = ClientStatus.CLIENT_GOAL
+
+
+class SlotTypeTest(Base.DataEnumTest):
+    type = SlotType
+    value = SlotType.player
