476049c5bd715700648598202af17a4768988831
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2025-08-15 01:00:16 +0200

    launcher component

--- worlds/apquest/__init__.py
+++ worlds/apquest/__init__.py
@@ -1,1 +1,18 @@
 from .world import APQuestWorld as APQuestWorld
+from worlds.LauncherComponents import components, Component, launch_subprocess, Type, SuffixIdentifier
+
+
+def run_client(*args) -> None:
+    print("Running Civ6 Client")
+    from .client.ap_quest_client import main  # lazy import
+
+    launch_subprocess(main, name="APQuest Client")
+
+
+components.append(
+    Component(
+        "APQuest Client",
+        func=run_client,
+        component_type=Type.CLIENT,
+    )
+)
