a5814c938037dc2d77c7ddd5dabe6f4ee722f63d
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2025-08-15 18:31:01 +0200

    ruff made my comment look unhinged

--- worlds/apquest/world.py
+++ worlds/apquest/world.py
@@ -1,7 +1,7 @@
-# Imports of base Archipelago modules must be absolute.
 from collections.abc import Mapping
 from typing import Any
 
+# Imports of base Archipelago modules must be absolute.
 from worlds.AutoWorld import World
 
 # Imports of your world's files must be relative.
