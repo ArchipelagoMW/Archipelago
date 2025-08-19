5d45b032bbf4ca595fb25d9371de17dc81142f10
Alex Gilbert <alexgilbert@yahoo.com>
2025-08-19 14:12:28 -0400

    - Sorted entries

--- worlds/stardew_valley/strings/metal_names.py
+++ worlds/stardew_valley/strings/metal_names.py
@@ -30,9 +30,6 @@ class MetalBar:
 
 
 class Mineral:
-    lemon_stone = "Lemon Stone"
-    kyanite = "Kyanite"
-    ghost_crystal = "Ghost Crystal"
     amethyst = "Amethyst"
     any_gem = "Any Gem"
     aquamarine = "Aquamarine"
@@ -41,9 +38,14 @@ class Mineral:
     emerald = "Emerald"
     fire_quartz = "Fire Quartz"
     frozen_tear = "Frozen Tear"
+    ghost_crystal = "Ghost Crystal"
     jade = "Jade"
     jamborite = "Jamborite"
+    kyanite = "Kyanite"
+    lemon_stone = "Lemon Stone"
+    limestone = "Limestone"
     marble = "Marble"
+    mudstone = "Mudstone"
     obsidian = "Obsidian"
     opal = "Opal"
     petrified_slime = "Petrified Slime"
@@ -53,40 +55,38 @@ class Mineral:
     thunder_egg = "Thunder Egg"
     tigerseye = "Tigerseye"
     topaz = "Topaz"
-    mudstone = "Mudstone"
-    limestone = "Limestone"
 
 
 class Artifact:
-    prehistoric_handaxe = artifact("Prehistoric Handaxe")
-    dwarf_gadget = artifact("Dwarf Gadget")
-    ancient_seed = artifact("Ancient Seed")
-    glass_shards = artifact("Glass Shards")
-    rusty_cog = artifact("Rusty Cog")
-    rare_disc = artifact("Rare Disc")
+    anchor = artifact("Anchor")
     ancient_doll = artifact("Ancient Doll")
     ancient_drum = artifact("Ancient Drum")
+    ancient_seed = artifact("Ancient Seed")
     ancient_sword = artifact("Ancient Sword")
     arrowhead = artifact("Arrowhead")
     bone_flute = artifact("Bone Flute")
     chewing_stick = artifact("Chewing Stick")
     chicken_statue = artifact("Chicken Statue")
-    anchor = artifact("Anchor")
     chipped_amphora = artifact("Chipped Amphora")
+    dwarf_gadget = artifact("Dwarf Gadget")
     dwarf_scroll_i = artifact("Dwarf Scroll I")
     dwarf_scroll_ii = artifact("Dwarf Scroll II")
     dwarf_scroll_iii = artifact("Dwarf Scroll III")
     dwarf_scroll_iv = artifact("Dwarf Scroll IV")
     dwarvish_helm = artifact("Dwarvish Helm")
     elvish_jewelry = artifact("Elvish Jewelry")
+    glass_shards = artifact("Glass Shards")
     golden_mask = artifact("Golden Mask")
     golden_relic = artifact("Golden Relic")
     ornamental_fan = artifact("Ornamental Fan")
+    prehistoric_handaxe = artifact("Prehistoric Handaxe")
     prehistoric_tool = artifact("Prehistoric Tool")
+    rare_disc = artifact("Rare Disc")
+    rusty_cog = artifact("Rusty Cog")
     rusty_spoon = artifact("Rusty Spoon")
     rusty_spur = artifact("Rusty Spur")
-    strange_doll_green = artifact("Strange Doll (Green)")
     strange_doll = artifact("Strange Doll")
+    strange_doll_green = artifact("Strange Doll (Green)")
 
 
 class Fossil:
@@ -116,37 +116,37 @@ class Fossil:
 
 
 class ModArtifact:
-    ancient_hilt = "Ancient Hilt"
     ancient_blade = "Ancient Blade"
+    ancient_doll_body = "Ancient Doll Body"
+    ancient_doll_legs = "Ancient Doll Legs"
+    ancient_hilt = "Ancient Hilt"
+    chipped_amphora_piece_1 = "Chipped Amphora Piece 1"
+    chipped_amphora_piece_2 = "Chipped Amphora Piece 2"
     mask_piece_1 = "Mask Piece 1"
     mask_piece_2 = "Mask Piece 2"
     mask_piece_3 = "Mask Piece 3"
-    ancient_doll_body = "Ancient Doll Body"
-    ancient_doll_legs = "Ancient Doll Legs"
     prismatic_shard_piece_1 = "Prismatic Shard Piece 1"
     prismatic_shard_piece_2 = "Prismatic Shard Piece 2"
     prismatic_shard_piece_3 = "Prismatic Shard Piece 3"
     prismatic_shard_piece_4 = "Prismatic Shard Piece 4"
-    chipped_amphora_piece_1 = "Chipped Amphora Piece 1"
-    chipped_amphora_piece_2 = "Chipped Amphora Piece 2"
 
 
 class ModFossil:
-    neanderthal_limb_bones = "Neanderthal Limb Bones"
-    neanderthal_ribs = "Neanderthal Ribs"
-    neanderthal_skull = "Neanderthal Skull"
-    neanderthal_pelvis = "Neanderthal Pelvis"
-    dinosaur_tooth = "Dinosaur Tooth"
-    dinosaur_skull = "Dinosaur Skull"
     dinosaur_claw = "Dinosaur Claw"
     dinosaur_femur = "Dinosaur Femur"
-    dinosaur_ribs = "Dinosaur Ribs"
     dinosaur_pelvis = "Dinosaur Pelvis"
+    dinosaur_ribs = "Dinosaur Ribs"
+    dinosaur_skull = "Dinosaur Skull"
+    dinosaur_tooth = "Dinosaur Tooth"
     dinosaur_vertebra = "Dinosaur Vertebra"
-    pterodactyl_ribs = "Pterodactyl Ribs"
-    pterodactyl_skull = "Pterodactyl Skull"
-    pterodactyl_r_wing_bone = "Pterodactyl R Wing Bone"
+    neanderthal_limb_bones = "Neanderthal Limb Bones"
+    neanderthal_pelvis = "Neanderthal Pelvis"
+    neanderthal_ribs = "Neanderthal Ribs"
+    neanderthal_skull = "Neanderthal Skull"
+    pterodactyl_claw = "Pterodactyl Claw"
     pterodactyl_l_wing_bone = "Pterodactyl L Wing Bone"
     pterodactyl_phalange = "Pterodactyl Phalange"
+    pterodactyl_r_wing_bone = "Pterodactyl R Wing Bone"
+    pterodactyl_ribs = "Pterodactyl Ribs"
+    pterodactyl_skull = "Pterodactyl Skull"
     pterodactyl_vertebra = "Pterodactyl Vertebra"
-    pterodactyl_claw = "Pterodactyl Claw"
