79d34d1f88d394c8f1d3050dd0d5ba6c50f324e8
Natan Maia <natanmaiaplays@outlook.com>
2025-08-19 22:36:02 -0300

    removed comments

--- worlds/silverdaze/__init__.py
+++ worlds/silverdaze/__init__.py
@@ -27,7 +27,7 @@ class SDWorld(World):
     # include events, but don't have to since events will be placed manually.
     
     # IDs in this dict are numeric and totally arbitrary, as long as they're unique
-    # from each other. For normal items, we can use their database IDs.
+    # from each other. For normal items, we can use their database IDs from the game itself.
     item_name_to_id = {
         name: data.code for name, data in item_table.items()
     }
@@ -44,13 +44,13 @@ class SDWorld(World):
         item = SDItem(name, item_data.classification, item_data.code, self.player)
         return item
 
-    # CORRECT ORDER:
+    # CORRECT ORDER OF EXECUTION:
     # stage_assert_generate(cls, multiworld: MultiWorld)
     # generate_early(self)                                                     unused?
     # create_regions(self)                                                          OK
     # create_items(self)                                                            OK       
-    # set_rules(self) 
-    # connect_entrances(self)
+    # set_rules(self)                                                               OK
+    # connect_entrances(self)                                     unused in undertale?
     # generate_basic(self) 
     # pre_fill(self), fill_hook(self) and post_fill(self) 
     # generate_output(self, output_directory: str) 
@@ -108,68 +108,7 @@ class SDWorld(World):
         
         self.multiworld.itempool += itempool
 
+    def set_rules(self):
+        set_rules(self)
+        set_completion_rules(self)
 
-
-
-# class SDWorld(World):
-#     """
-#     This will describe Silver Daze eventually.
-#     """
-#     game = "Silver Daze"
-#     required_client_version = (0, 5, 4)
-#     options_dataclass = SilverDazeOptions
-
-#     item_name_to_id = {name: item_table.code.name for name, data in item_table.code}
-#     location_name_to_id = {name: SDLocationData.id for name, data in SDLocationData.id}
-
-#     def get_filler_item_name(self):
-#         return "Heal Token"
-
-#     def create_items(self):
-#         itempool = []
-#         for name, max_quantity in item_table:
-#             itempool += [name] * max_quantity
-
-#         #Starting Party Member given at game start
-#         starter_member = "Pinn"
-#         if (Options.StartingPartyMember == "option_geo"):
-#             starter_member = "Geo"
-#         if (Options.StartingPartyMember == "option_kani"):
-#             starter_member = "Kani"
-#         if (Options.StartingPartyMember == "option_random"):
-#             import random
-#             #Sawyer: Make sure this has all party members in the final game.
-#             member = random.randint(1,3)
-#             if (member == 1): starter_member = "Pinn"
-#             elif (member == 2): starter_member = "Geo"
-#             elif (member == 3): starter_member = "Kani"
-#         itempool.remove(starter_member)
-#         self.multiworld.push_precollected(self.create_item(starter_member))
-
-#         #Create Items
-#         itempool = [item for item in map(lambda name: self.create_item(name), itempool)]
-#         # Fill remaining items with randomly generated junk
-#         while len(itempool) < len(self.multiworld.get_unfilled_locations(self.player)):
-#             itempool.append(self.create_filler())
-
-#         self.multiworld.itempool += itempool
-
-#     def set_rules(self):
-#         set_rules(self)
-#         set_completion_rules(self)
-
-#     def create_regions(self):
-#         def SDRegion(region_name: str, exits=[]):
-#             ret = Region(region_name, self.player, self.multiworld)
-#             ret.locations += [SDLocationData(ret, id)
-#                               ]
-#             for exit in exits:
-#                 ret.exits.append(Entrance(self.player, exit, ret))
-#             return ret
-
-#         self.multiworld.regions += [SDRegion(*r) for r in sd_regions]
-
-#     def create_item(self, name: str) -> Item:
-#         item_data = item_table[name]
-#         item = SDItem(name, item_data.classification, item_data.code, self.player)
-#         return item
