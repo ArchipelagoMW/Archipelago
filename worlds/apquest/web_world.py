01f4390a264a8c6284acb9a08a7e065a414b931b
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2025-08-24 01:59:59 +0200

    Write WebWorld and setup guides

--- worlds/apquest/web_world.py
+++ worlds/apquest/web_world.py
@@ -4,12 +4,23 @@ from worlds.AutoWorld import WebWorld
 
 from .options import option_groups, option_presets
 
-
-# TODO: Explain what a webworld is
+# For our game to display correctly on the archipelago.gg website, we need to define a WebWorld subclass.
 class APQuestWebWorld(WebWorld):
+    # We need to override the "game" field of the WebWorld superclass.
+    # This must be the same string as the regular World class.
     game = "APQuest"
 
-    setup = Tutorial(
+    # Your game pages will have a visual theme (affecting e.g. the background image).
+    # You can choose between dirt, grass, grassFlowers, ice, jungle, ocean, partyTime, and stone.
+    theme = "grassFlowers"
+
+    # A WebWorld can have any number of tutorials, but should always have at least an English setup guide.
+    # Many WebWorlds just have one setup guide, but some have multiple, e.g. for different languages.
+    # We need to create a Tutorial object for every setup guide.
+    # In order, we need to provide a title, a description, a language, a filepath, a link, and authors.
+    # The filepath is relative to a "/docs/" directory in the root folder of your apworld.
+    # The "link" parameter is unused, but we still need to provide it.
+    setup_en = Tutorial(
         "Multiworld Setup Guide",
         "A guide to setting up APQuest for MultiWorld.",
         "English",
@@ -17,8 +28,19 @@ class APQuestWebWorld(WebWorld):
         "setup/en",
         ["NewSoupVi"],
     )
+    # Let's have our setup guide in German as well.
+    setup_de = Tutorial(
+        "Multiworld Setup Guide",
+        "A guide to setting up APQuest for MultiWorld.",
+        "German",
+        "setup_de.md",
+        "setup/de",
+        ["NewSoupVi"]
+    )
 
-    tutorials = [setup]  # noqa: RUF012
+    # We add these tutorials to our WebWorld by overriding the "tutorials" field.
+    tutorials = [setup_en, setup_de]  # noqa: RUF012
 
+    # If we have option groups and/or option presets, we need to specify these here as well.
     option_groups = option_groups
     options_presets = option_presets
