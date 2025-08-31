bba83107dfaecb57e8371e2ee9acd9247d3f0247
PinkSwitch <blabidyfa@gmail.com>
2025-08-31 17:17:57 -0500

    Fix epilogue skip being offset

--- worlds/earthbound/Rom.py
+++ worlds/earthbound/Rom.py
@@ -155,7 +155,7 @@ def patch_rom(world, rom, player: int) -> None:
     rom.write_bytes(0x01FE8B, bytearray(starting_area_coordinates[world.start_location][2:4]))  # Respawn position
 
     if world.options.skip_epilogue:
-        rom.write_bytes(0x2EA3C6, struct.pack("I", 0xEEA432))
+        rom.write_bytes(0x2EA42F, struct.pack("I", 0xEEA432))
 
     if world.starting_character == "Poo":
         rom.write_bytes(starting_levels[world.starting_character], bytearray([0x06]))
