bc3588651b3d38f604575bd1aedae7f6bbca2de4
Silvris <58583688+Silvris@users.noreply.github.com>
2025-08-30 21:32:07 -0500

    move goal kdl3

--- worlds/kdl3/__init__.py
+++ worlds/kdl3/__init__.py
@@ -303,9 +303,6 @@ class KDL3World(World):
 
     def generate_basic(self) -> None:
         self.stage_shuffle_enabled = self.options.stage_shuffle > 0
-        goal = self.options.goal.value
-        goal_location = self.multiworld.get_location(location_name.goals[goal], self.player)
-        goal_location.place_locked_item(KDL3Item("Love-Love Rod", ItemClassification.progression, None, self.player))
         for level in range(1, 6):
             self.multiworld.get_location(f"Level {level} Boss - Defeated", self.player) \
                 .place_locked_item(
