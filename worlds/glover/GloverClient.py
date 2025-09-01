96a5c6bbab4ac4d9556fa92900a650ce4818e630
jjjj12212 <mjackson12212@hotmail.com>
2025-09-01 11:17:17 -0600

    remove dpad shortcuts

--- worlds/glover/GloverClient.py
+++ worlds/glover/GloverClient.py
@@ -66,7 +66,7 @@ loc_name_to_id = network_data_package["games"]["Glover"]["location_name_to_id"]
 itm_name_to_id = network_data_package["games"]["Glover"]["item_name_to_id"]
 script_version: int = 1
 version: str = "V0.1"
-patch_md5: str = "19ebd89a4b9907b02947467125dcdf29"
+patch_md5: str = "34bb78448e2ed9c4236fad0f79f64448"
 gvr_options = settings.get_settings().glover_options
 program = None
 
