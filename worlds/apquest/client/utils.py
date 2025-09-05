fffc30cdf93e00847a0d1706fc3f9c3b97b4fa58
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2025-09-05 19:27:30 +0200

    Fix getting of the user path

--- worlds/apquest/client/utils.py
+++ worlds/apquest/client/utils.py
@@ -1,10 +1,14 @@
 from pathlib import Path
 
+from Utils import user_path
+
 
 def make_data_directory(dir_name: str) -> Path:
-    data_directory = Path() / "data"
-    if not data_directory.exists():
-        raise FileNotFoundError(f"Unable to find data directory {data_directory}")
+    root_directory = Path(user_path())
+    if not root_directory.exists():
+        raise FileNotFoundError(f"Unable to find AP directory {root_directory.absolute()}.")
+
+    data_directory = root_directory / "data"
 
     specific_data_directory = data_directory / "apquest" / dir_name
     specific_data_directory.mkdir(parents=True, exist_ok=True)
