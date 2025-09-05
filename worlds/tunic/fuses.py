a0c311dd54f97ccbe60efe68c98b60d754c56a7d
Scipio Wright <scipiowright@gmail.com>
2025-09-04 21:18:26 -0400

    oh right that's why it was there, circular imports

--- worlds/tunic/fuses.py
+++ worlds/tunic/fuses.py
@@ -4,7 +4,7 @@ from BaseClasses import CollectionState
 from worlds.generic.Rules import set_rule
 
 from .constants import *
-from .logic_helpers import has_ability, has_sword
+from .logic_helpers import has_ability, has_sword, fuse_activation_reqs
 
 if TYPE_CHECKING:
     from . import TunicWorld
@@ -39,35 +39,6 @@ fuse_location_table: dict[str, TunicLocationData] = {
     "Quarry - Activate Fuse": TunicLocationData("Quarry", "Quarry Entry"),
     "Rooted Ziggurat Lower - [Miniboss] Activate Fuse": TunicLocationData("Rooted Ziggurat Lower", "Rooted Ziggurat Lower Miniboss Platform"),
     "Rooted Ziggurat Lower - [Before Boss] Activate Fuse": TunicLocationData("Rooted Ziggurat Lower", "Rooted Ziggurat Lower Back"),
-}
-
-# for fuse locations and reusing event names to simplify er_rules
-fuse_activation_reqs: dict[str, list[str]] = {
-    swamp_fuse_2: [swamp_fuse_1],
-    swamp_fuse_3: [swamp_fuse_1, swamp_fuse_2],
-    fortress_exterior_fuse_2: [fortress_exterior_fuse_1],
-    beneath_the_vault_fuse: [fortress_exterior_fuse_1, fortress_exterior_fuse_2],
-    fortress_candles_fuse: [fortress_exterior_fuse_1, fortress_exterior_fuse_2, beneath_the_vault_fuse],
-    fortress_door_left_fuse: [fortress_exterior_fuse_1, fortress_exterior_fuse_2, beneath_the_vault_fuse,
-                              fortress_candles_fuse],
-    fortress_courtyard_upper_fuse: [fortress_exterior_fuse_1],
-    fortress_courtyard_lower_fuse: [fortress_exterior_fuse_1, fortress_courtyard_upper_fuse],
-    fortress_door_right_fuse: [fortress_exterior_fuse_1, fortress_courtyard_upper_fuse, fortress_courtyard_lower_fuse],
-    quarry_fuse_2: [quarry_fuse_1],
-    "Activate Furnace Fuse": [west_furnace_fuse],
-    "Activate South and West Fortress Exterior Fuses": [fortress_exterior_fuse_1, fortress_exterior_fuse_2],
-    "Activate Upper and Central Fortress Exterior Fuses": [fortress_exterior_fuse_1, fortress_courtyard_upper_fuse,
-                                                           fortress_courtyard_lower_fuse],
-    "Activate Beneath the Vault Fuse": [fortress_exterior_fuse_1, fortress_exterior_fuse_2, beneath_the_vault_fuse],
-    "Activate Eastern Vault West Fuses": [fortress_exterior_fuse_1, fortress_exterior_fuse_2, beneath_the_vault_fuse,
-                                          fortress_candles_fuse, fortress_door_left_fuse],
-    "Activate Eastern Vault East Fuse": [fortress_exterior_fuse_1, fortress_courtyard_upper_fuse,
-                                         fortress_courtyard_lower_fuse, fortress_door_right_fuse],
-    "Activate Quarry Connector Fuse": [quarry_fuse_1],
-    "Activate Quarry Fuse": [quarry_fuse_1, quarry_fuse_2],
-    "Activate Ziggurat Fuse": [ziggurat_teleporter_fuse],
-    "Activate West Garden Fuse": [west_garden_fuse],
-    "Activate Library Fuse": [library_lab_fuse],
 }
 
 fuse_location_base_id = base_id + 10000
--- worlds/tunic/logic_helpers.py
+++ worlds/tunic/logic_helpers.py
@@ -3,7 +3,6 @@ from typing import TYPE_CHECKING
 from BaseClasses import CollectionState
 
 from .constants import *
-from .fuses import fuse_activation_reqs
 from .options import HexagonQuestAbilityUnlockType, IceGrappling
 
 if TYPE_CHECKING:
@@ -87,6 +86,36 @@ def can_shop(state: CollectionState, world: "TunicWorld") -> bool:
 def can_get_past_bushes(state: CollectionState, world: "TunicWorld") -> bool:
     # add in glass cannon + stick for grass rando
     return has_sword(state, world.player) or state.has_any((fire_wand, laurels, gun), world.player)
+
+
+# for fuse locations and reusing event names to simplify er_rules
+fuse_activation_reqs: dict[str, list[str]] = {
+    swamp_fuse_2: [swamp_fuse_1],
+    swamp_fuse_3: [swamp_fuse_1, swamp_fuse_2],
+    fortress_exterior_fuse_2: [fortress_exterior_fuse_1],
+    beneath_the_vault_fuse: [fortress_exterior_fuse_1, fortress_exterior_fuse_2],
+    fortress_candles_fuse: [fortress_exterior_fuse_1, fortress_exterior_fuse_2, beneath_the_vault_fuse],
+    fortress_door_left_fuse: [fortress_exterior_fuse_1, fortress_exterior_fuse_2, beneath_the_vault_fuse,
+                              fortress_candles_fuse],
+    fortress_courtyard_upper_fuse: [fortress_exterior_fuse_1],
+    fortress_courtyard_lower_fuse: [fortress_exterior_fuse_1, fortress_courtyard_upper_fuse],
+    fortress_door_right_fuse: [fortress_exterior_fuse_1, fortress_courtyard_upper_fuse, fortress_courtyard_lower_fuse],
+    quarry_fuse_2: [quarry_fuse_1],
+    "Activate Furnace Fuse": [west_furnace_fuse],
+    "Activate South and West Fortress Exterior Fuses": [fortress_exterior_fuse_1, fortress_exterior_fuse_2],
+    "Activate Upper and Central Fortress Exterior Fuses": [fortress_exterior_fuse_1, fortress_courtyard_upper_fuse,
+                                                           fortress_courtyard_lower_fuse],
+    "Activate Beneath the Vault Fuse": [fortress_exterior_fuse_1, fortress_exterior_fuse_2, beneath_the_vault_fuse],
+    "Activate Eastern Vault West Fuses": [fortress_exterior_fuse_1, fortress_exterior_fuse_2, beneath_the_vault_fuse,
+                                          fortress_candles_fuse, fortress_door_left_fuse],
+    "Activate Eastern Vault East Fuse": [fortress_exterior_fuse_1, fortress_courtyard_upper_fuse,
+                                         fortress_courtyard_lower_fuse, fortress_door_right_fuse],
+    "Activate Quarry Connector Fuse": [quarry_fuse_1],
+    "Activate Quarry Fuse": [quarry_fuse_1, quarry_fuse_2],
+    "Activate Ziggurat Fuse": [ziggurat_teleporter_fuse],
+    "Activate West Garden Fuse": [west_garden_fuse],
+    "Activate Library Fuse": [library_lab_fuse],
+}
 
 
 def has_fuses(fuse_event: str, state: CollectionState, world: "TunicWorld") -> bool:
