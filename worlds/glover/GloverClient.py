3e34f6f9334b97b3aae2f5ddf3df975d1e3c99b8
jjjj12212 <mjackson12212@hotmail.com>
2025-09-08 13:58:35 -0600

    Potion fix

--- worlds/glover/GloverClient.py
+++ worlds/glover/GloverClient.py
@@ -66,7 +66,7 @@ loc_name_to_id = network_data_package["games"]["Glover"]["location_name_to_id"]
 itm_name_to_id = network_data_package["games"]["Glover"]["item_name_to_id"]
 script_version: int = 1
 version: str = "V0.1"
-patch_md5: str = "31dac75ad5e65e2ec64b69548f09b1ec"
+patch_md5: str = "d622a238e69329c650c3137bf7e7158c"
 gvr_options = settings.get_settings().glover_options
 program = None
 
