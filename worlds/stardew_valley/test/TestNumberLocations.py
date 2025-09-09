2dac6328713e8a46aa58b110f1e8c22cd58b6370
Alex Gilbert <alexgilbert@yahoo.com>
2025-09-09 13:10:11 -0400

    - Updated location counts

--- worlds/stardew_valley/test/TestNumberLocations.py
+++ worlds/stardew_valley/test/TestNumberLocations.py
@@ -89,7 +89,7 @@ class TestAllSanityWithModsSettingsHasAllExpectedLocations(SVTestBase):
     options = allsanity_mods_7_x_x_exclude_disabled()
 
     def test_allsanity_with_mods_has_at_least_locations(self):
-        expected_locations = 3473
+        expected_locations = 3189  # It was before disabling SVE 3473
         real_locations = self.get_real_locations()
         number_locations = len(real_locations)
         print(f"Stardew Valley - Allsanity Locations with all mods: {number_locations}")
