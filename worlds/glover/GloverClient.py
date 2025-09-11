aa0ee83b8273cd6a303af7443e35a22ee594eae3
jjjj12212 <mjackson12212@hotmail.com>
2025-09-11 08:12:41 -0600

    Pirate 1 enemies

--- worlds/glover/GloverClient.py
+++ worlds/glover/GloverClient.py
@@ -66,7 +66,7 @@ loc_name_to_id = network_data_package["games"]["Glover"]["location_name_to_id"]
 itm_name_to_id = network_data_package["games"]["Glover"]["item_name_to_id"]
 script_version: int = 1
 version: str = "V0.1"
-patch_md5: str = "ed2fa044810a9129210fd9e2b18b1c7e"
+patch_md5: str = "22a707fa3b78ce45c068bc82eb897c1c"
 gvr_options = settings.get_settings().glover_options
 program = None
 
