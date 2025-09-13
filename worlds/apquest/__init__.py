5ee5d620f7650834789fb8cb77f58e04a7fc323d
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2025-09-13 21:49:59 +0200

    Move comment to where it belongs

--- worlds/apquest/__init__.py
+++ worlds/apquest/__init__.py
@@ -1,7 +1,7 @@
-# In our __init__.py, we just import our world class from our world.py to initialize it.
-# Obviously, this world class needs to exist first. For this, read world.py.
 from worlds.LauncherComponents import Component, Type, components, launch
 
+# In our __init__.py, we just import our world class from our world.py to initialize it.
+# Obviously, this world class needs to exist first. For this, read world.py.
 from .world import APQuestWorld as APQuestWorld
 
 
