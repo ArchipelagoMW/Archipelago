f72b3eb09901ba226fdf73f9f13b40dab2c1d5c0
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2025-09-13 22:03:48 +0200

    Clear up what CommonClient is

--- worlds/apquest/__init__.py
+++ worlds/apquest/__init__.py
@@ -11,7 +11,7 @@ from .world import APQuestWorld as APQuestWorld
 # (Note: Some worlds distribute their clients as separate, standalone programs,
 #  while others include them in the apworld itself. Standalone clients are not an apworld component,
 #  although you could make a component that e.g. auto-installs and launches the standalone client for the user.)
-# APQuest has a CommonClient-derived client inside that apworld that contains the entire game. This is a component.
+# APQuest has a Python client inside the apworld that contains the entire game. This is a component.
 # APQuest will not teach you how to make a client or any other type of component.
 # However, let's quickly talk about how you register a component to be launchable from the Archipelago Launcher.
 # First, you'll need a function that takes a list of args (e.g. from the command line) that launches your component.
@@ -23,7 +23,7 @@ def run_client(*args: str) -> None:
     # you should use LauncherComponents.launch_subprosses to launch it.
     # Specifically for components that support a gui mode using kivy, but can also be run without gui,
     # you should use the LauncherComponents.launch helper (which itself calls launch_subprocess).
-    # This mainly applies to CommonClient-derived clients.
+    # This mainly applies to clients derived from APs generic "CommonClient" class.
     launch(launch_ap_quest_client, name="APQuest Client", args=args)
 
 
