6e762e909660d5f9aca133452b221fd10c55fdfd
jjjj12212 <mjackson12212@hotmail.com>
2025-09-17 14:59:51 -0600

    Prehistoric worlds

--- worlds/glover/GloverClient.py
+++ worlds/glover/GloverClient.py
@@ -66,7 +66,7 @@ loc_name_to_id = network_data_package["games"]["Glover"]["location_name_to_id"]
 itm_name_to_id = network_data_package["games"]["Glover"]["item_name_to_id"]
 script_version: int = 1
 version: str = "V0.1"
-patch_md5: str = "e860076ef2e40e6a432a75d8ceb46e5d"
+patch_md5: str = "3d7dbee239ef2caa0a176e03be9854c4"
 gvr_options = settings.get_settings().glover_options
 program = None
 
