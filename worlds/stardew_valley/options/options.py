00d7ce34711fd8680883190fadfc0e6aa4fc826a
Alex Gilbert <alexgilbert@yahoo.com>
2025-09-16 22:22:49 -0400

    - Fixed a documentation error

--- worlds/stardew_valley/options/options.py
+++ worlds/stardew_valley/options/options.py
@@ -544,7 +544,7 @@ class Shipsanity(Choice):
     """Locations for shipping items?
     None: There are no checks for shipping items
     Crops: Every crop and forageable being shipped is a check
-    Fish: Every fish being shipped is a check except legendaries
+    Fish: Every fish being shipped is a check
     Full Shipment: Every item in the Collections page is a check
     Full Shipment With Fish: Every item in the Collections page and every fish is a check
     Everything: Every item in the game that can be shipped is a check
