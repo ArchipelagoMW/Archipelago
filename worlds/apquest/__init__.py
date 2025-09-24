6063e2fa92c4e052c4afbe40cbdc0f79fea5879c
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2025-09-25 01:07:43 +0200

    ignore instead of skip

--- worlds/apquest/__init__.py
+++ worlds/apquest/__init__.py
@@ -4,7 +4,7 @@
 
 # Apart from the regular apworld code that allows generating multiworld seeds with your game,
 # your apworld might have other "components" that should be launchable from the Archipelago Launcher.
-# You can skip this for now. If you are specifically interested in components, you can read components.py.
+# You can ignore this for now. If you are specifically interested in components, you can read components.py.
 from . import components as components
 
 # The main thing we do in our __init__.py is importing our world class from our world.py to initialize it.
