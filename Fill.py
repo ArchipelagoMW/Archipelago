165ba26370d90ee42e1e27e22ec806a8d26f0297
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2025-09-25 02:24:03 +0200

    iMportant, the new apple device

--- Fill.py
+++ Fill.py
@@ -584,7 +584,7 @@ def distribute_items_restrictive(multiworld: MultiWorld,
     multiworld.random.shuffle(itempool)
 
     fill_locations = sorted(multiworld.get_unfilled_locations())
-    # All fill steps for i"mportant" items (early, priority, progression) should be balanced by the balancing factor.
+    # All fill steps for "important" items (early, priority, progression) should be balanced by the balancing factor.
     fill_locations = balanced_shuffle(multiworld, fill_locations, itempool)
 
     fill_locations, itempool = distribute_early_items(multiworld, fill_locations, itempool)
