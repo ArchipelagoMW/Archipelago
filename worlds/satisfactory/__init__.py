dfbf406bd4fccb70ab55125b22709b348c4f9ca7
Jarno Westhof <jarnowesthof@gmail.com>
2025-09-26 21:21:30 +0200

    Fix hint creation

--- worlds/satisfactory/__init__.py
+++ worlds/satisfactory/__init__.py
@@ -1,4 +1,5 @@
 from typing import TextIO, ClassVar, Any
+from collections.abc import Iterable
 from BaseClasses import Item, ItemClassification, CollectionState
 from NetUtils import Hint
 from .GameLogic import GameLogic
@@ -229,22 +230,29 @@ class SatisfactoryWorld(World):
         else:
             return Items.create_item_uninitialized(name, self.player)
 
-    def modify_multidata(self, multidata: dict[str, Any]) -> None:
-        locations_visible_from_start: list[int] = list(range(1338000, 1338099))  # ids of Hub 1-1,1 to 2-5,10
+    def extend_hint_information(self, _: dict[int, dict[int, str]]):
+        """
+        Normally used for adding entrance information, 
+        but in this case we want to create hints for locations that hold usefull items.
+        Since we only know item placements after generation is completed it was either this 
+            or fill_slot_data or modify_multidata, and this method seemed the best fit
+        """
+
+        locations_visible_from_start: set[int] = set(range(1338000, 1338099))  # ids of Hub 1-1,1 to 2-5,10
 
         if "Building: AWESOME Shop" in self.options.start_inventory \
                 or "Building: AWESOME Shop" in self.options.start_inventory_from_pool \
-                or 1338622 in multidata["precollected_items"][self.player]:  # id of Building: AWESOME Shop
-            locations_visible_from_start.extend(range(1338700, 1338709))  # ids of shop locations 1 to 10
+                or self.options.awesome_logic_placement.value == Placement.starting_inventory:
+            locations_visible_from_start.update(range(1338700, 1338709))  # ids of shop locations 1 to 10
 
-        for location_id in locations_visible_from_start:
-            if location_id in multidata["locations"][self.player]:
-                item_id, player_id, flags = multidata["locations"][self.player][location_id]
+            location_names_with_useful_items: Iterable[str] = [
+                location.name
+                for location in self.get_locations()
+                if location.address in locations_visible_from_start and location.item \
+                        and location.item.flags & (ItemClassification.progression | ItemClassification.useful) > 0
+            ]
 
-                if player_id != self.player and flags & (
-                        ItemClassification.progression | ItemClassification.useful) > 0:
-                    hint = Hint(player_id, self.player, location_id, item_id, False, item_flags=flags)
-                    multidata["precollected_hints"][self.player].add(hint)
+            self.options.start_location_hints.value.update(location_names_with_useful_items)
 
     def push_precollected_by_name(self, item_name: str) -> None:
         item = self.create_item(item_name)
