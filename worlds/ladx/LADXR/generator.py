f26fcc0edab7c63f684104e46322e9732084e134
threeandthreee <alex@3and3.dev>
2025-09-30 12:47:17 -0400

    LADX: use generic slot name for slots 101+ (#5208)
    * init
    
    * we already had the generic name, just use it
    
    * cap hints at 101
    
    * nevermind, the name is just baked in here

--- worlds/ladx/LADXR/generator.py
+++ worlds/ladx/LADXR/generator.py
@@ -271,9 +271,9 @@ def generateRom(base_rom: bytes, args, patch_data: Dict):
         mw = None
         if spot.item_owner != spot.location_owner:
             mw = spot.item_owner
-            if mw > 100:
+            if mw > 101:
                 # There are only 101 player name slots (99 + "The Server" + "another world"), so don't use more than that
-                mw = 100
+                mw = 101
         spot.patch(rom, spot.item, multiworld=mw)
     patches.enemies.changeBosses(rom, patch_data["world_setup"]["boss_mapping"])
     patches.enemies.changeMiniBosses(rom, patch_data["world_setup"]["miniboss_mapping"])
