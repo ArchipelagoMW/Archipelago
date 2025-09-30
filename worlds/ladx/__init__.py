5cec3f45f593e7a006fa18a6c6ffd756acdda776
threeandthreee <alex@3and3.dev>
2025-09-30 12:39:53 -0400

    LADX: reorganize options page (#4851)
    * init
    
    * merge upstream/main
    
    * improve option tooltips, clean up file a bit
    
    * ladx feels like more of an ocean game
    
    * one more
    
    * more cleanup
    
    * some reorg
    
    * Apply suggestions from code review
    
    Co-authored-by: Scipio Wright <scipiowright@gmail.com>
    
    * clean up accidental newlines
    
    * rewording
    
    * dont do the ohko alias
    
    ---------
    
    Co-authored-by: Scipio Wright <scipiowright@gmail.com>

--- worlds/ladx/__init__.py
+++ worlds/ladx/__init__.py
@@ -71,7 +71,7 @@ class LinksAwakeningWebWorld(WebWorld):
         "setup/en",
         ["zig"]
     )]
-    theme = "dirt"
+    theme = "ocean"
     option_groups = ladx_option_groups
     options_presets: typing.Dict[str, typing.Dict[str, typing.Any]] = {
         "Keysanity": {
