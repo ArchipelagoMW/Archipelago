00c94202c4f370b2ad970743a9231ed8f72529aa
mattman107 <mathoman@ymail.com>
2025-10-05 14:41:04 -0400

    Requested changes 2

--- worlds/oot_soh/location_access/overworld/haunted_wasteland.py
+++ worlds/oot_soh/location_access/overworld/haunted_wasteland.py
@@ -11,10 +11,6 @@ class EventLocations(str, Enum):
     HAUNTED_WATELAND_NUT_POT = "Haunted Wasteland Nut Pot"
     HAUNTED_WATELAND_CARPET_MERCHANT = "Haunted Wasteland Carpet Merchant"
 
-
-class LocalEvents(str, Enum):
-    CAN_BUY_FROM_CARPET_MERCHANT = "Can Buy From Carpet Merchant"
-
 def set_region_rules(world: "SohWorld") -> None:
     player = world.player
     
@@ -34,12 +30,12 @@ def set_region_rules(world: "SohWorld") -> None:
     add_events(Regions.HAUNTED_WASTELAND, world, [
         (EventLocations.HAUNTED_WATELAND_FAIRY_POT, Events.CAN_ACCESS_FAIRIES, lambda bundle: True),
         (EventLocations.HAUNTED_WATELAND_NUT_POT, Events.CAN_FARM_NUTS, lambda bundle: True),
-        (EventLocations.HAUNTED_WATELAND_CARPET_MERCHANT, LocalEvents.CAN_BUY_FROM_CARPET_MERCHANT, lambda bundle: has_item(Items.ADULT_WALLET, bundle) and True and (can_jump_slash(bundle) or can_use(Items.HOVER_BOOTS, bundle))) # TODO replace true with CanBuyAnother() equivilent
+        (EventLocations.HAUNTED_WATELAND_CARPET_MERCHANT, Events.CARPET_MERCHANT, lambda bundle: has_item(Items.ADULT_WALLET, bundle) and (can_jump_slash(bundle) or can_use(Items.HOVER_BOOTS, bundle))) # TODO replace true with CanBuyAnother() equivilent
     ])
     # Locations
     add_locations(Regions.HAUNTED_WASTELAND, world, [
         (Locations.WASTELAND_CHEST, lambda bundle: has_fire_source(bundle)),
-        (Locations.WASTELAND_CARPET_SALESMAN, lambda bundle: can_jump_slash(bundle) or can_use(Items.HOVER_BOOTS, bundle)),
+        (Locations.WASTELAND_CARPET_SALESMAN, lambda bundle: (can_jump_slash(bundle) or can_use(Items.HOVER_BOOTS, bundle)) and has_item(Items.ADULT_WALLET, bundle)),
         (Locations.WASTELAND_GS, lambda bundle: hookshot_or_boomerang(bundle)),
         (Locations.WASTELAND_NEAR_GS_POT1, lambda bundle: can_break_pots(bundle)),
         (Locations.WASTELAND_NEAR_GS_POT2, lambda bundle: can_break_pots(bundle)),
@@ -63,5 +59,5 @@ def set_region_rules(world: "SohWorld") -> None:
     # Connections
     connect_regions(Regions.WASTELAND_NEAR_COLOSSUS, world, [
         (Regions.DESERT_COLOSSUS, lambda bundle: True),
-        (Regions.HAUNTED_WASTELAND, lambda bundle: can_do_trick(Tricks.HW_REVERSE, bundle) or False)
+        (Regions.HAUNTED_WASTELAND, lambda bundle: can_do_trick(Tricks.HW_REVERSE, bundle))
     ])
