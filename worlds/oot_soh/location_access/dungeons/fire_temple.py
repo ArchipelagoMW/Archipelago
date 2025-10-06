08e8e32c90980d99b62c8c41aa0626ec710cb5a9
Fredrik Andréasson <medumee@gmail.com>
2025-10-07 00:20:54 +0200

    Fire Temple start

--- worlds/oot_soh/location_access/dungeons/fire_temple.py
+++ worlds/oot_soh/location_access/dungeons/fire_temple.py
@@ -5,5 +5,79 @@ if TYPE_CHECKING:
 
 
 def set_region_rules(world: "SohWorld") -> None:
-    player = world.player
+    ## Fire Temple Entryway
+    
+    ## Fire Temple First Room
+    
+    ## Fire Temple Near Boss Room
+    
+    ## Fire Temple Loop Enemies
+    
+    ## Fire Temple Loop Tiles
+    
+    ## Fire Temple Loop Flare Dancer
+    
+    ## Fire Temple Loop Hammer Switch
+    
+    ## Fire Temple Loop Goron Room
+    
+    ## Fire Temple Loop Exit
+    
+    ## Fire Temple Big Lava Room
+    
+    ## Fire Temple Big Lava Room North Goron
+    
+    ## Fire Temple Big Lava Room North Tiles
+    
+    ## Fire Temple Big Lava Room South Goron
+    
+    ## Fire Temple Fire Pillar Room
+    
+    ## Fire Temple Shortcut Room
+    
+    ## Fire Temple Shortcut Climb
+    
+    ## Fire Temple Boulder Maze Lower
+    
+    ## Fire Temple Boulder Maze Lower Side Room
+    
+    ## Fire Temple East Central Room
+    
+    ## Fire Temple Fire Wall Chase
+    
+    ## Fire Temple Map Region
+    
+    ## Fire Temple Boulder Maze Upper
+    
+    ## Fire Temple Scarecrow Room
+    
+    ## Fire Temple East Peak
+    
+    ## Fire Temple Corridor
+    
+    ## Fire Temple Fire Maze Room
+    
+    ## Fire Temple Fire Maze Upper
+    
+    ## Fire Temple Fire Maze Side Room
+    
+    ## Fire Temple West Central Lower
+    
+    ## Fire Temple West Central Upper
+    
+    ## Fire Temple Late Fire Maze
+    
+    ## Fire Temple Upper Flare Dancer
+    
+    ## Fire Temple West Climb
+    
+    ## Fire Temple West Peak
+    
+    ## Fire Temple Hammer Return Path
+    
+    ## Fire Temple Above Fire Maze
+    
+    ## Fire Temple Boss Entryway
+    
+    ## Fire Temple Boss Room
     
