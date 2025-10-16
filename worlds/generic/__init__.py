c09a098b39c9ebbd81fd2e217d7254df7b87d7e4
duckboycool <duckboycool101@gmail.com>
2025-10-16 08:26:22 -0600

    Deprecate old PlandoItem

--- worlds/generic/__init__.py
+++ worlds/generic/__init__.py
@@ -5,6 +5,7 @@ from BaseClasses import Item, Tutorial, ItemClassification
 
 from ..AutoWorld import InvalidItemError, World, WebWorld
 from NetUtils import SlotType
+from Utils import deprecate
 
 
 class GenericWeb(WebWorld):
@@ -49,22 +50,27 @@ class GenericWorld(World):
             return Item(name, ItemClassification.filler, -1, self.player)
         raise InvalidItemError(name)
 
+if __debug__:
+    class PlandoItem:
+        def __init__(self, *args, **kwargs):
+            deprecate("worlds.generic.PlandoItem is deprecated and will be removed in the next version. "
+                      "Use Options.PlandoItem(s) instead.")
+else:
+    class PlandoItem(NamedTuple):
+        item: str
+        location: str
+        world: Union[bool, str] = False  # False -> own world, True -> not own world
+        from_pool: bool = True  # if item should be removed from item pool
+        force: str = 'silent'  # false -> warns if item not successfully placed. true -> errors out on failure to place item.
 
-class PlandoItem(NamedTuple):
-    item: str
-    location: str
-    world: Union[bool, str] = False  # False -> own world, True -> not own world
-    from_pool: bool = True  # if item should be removed from item pool
-    force: str = 'silent'  # false -> warns if item not successfully placed. true -> errors out on failure to place item.
+        def warn(self, warning: str):
+            if self.force in ['true', 'fail', 'failure', 'none', 'false', 'warn', 'warning']:
+                logging.warning(f'{warning}')
+            else:
+                logging.debug(f'{warning}')
 
-    def warn(self, warning: str):
-        if self.force in ['true', 'fail', 'failure', 'none', 'false', 'warn', 'warning']:
-            logging.warning(f'{warning}')
-        else:
-            logging.debug(f'{warning}')
-
-    def failed(self, warning: str, exception=Exception):
-        if self.force in ['true', 'fail', 'failure']:
-            raise exception(warning)
-        else:
-            self.warn(warning)
+        def failed(self, warning: str, exception=Exception):
+            if self.force in ['true', 'fail', 'failure']:
+                raise exception(warning)
+            else:
+                self.warn(warning)
