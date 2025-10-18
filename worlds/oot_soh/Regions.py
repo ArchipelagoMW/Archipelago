86cdf7dce848d2623980189fc5804da0b2b55e5f
Scipio Wright <scipiowright@gmail.com>
2025-10-17 20:22:06 -0400

    Fix some stuff that should've been changed to enums

--- worlds/oot_soh/Regions.py
+++ worlds/oot_soh/Regions.py
@@ -226,7 +226,7 @@ def create_regions_and_locations(world: "SohWorld") -> None:
             world.included_locations.update(carpenters_location_table)
 
         if world.options.fortress_carpenters == "fast":
-            for location_name in ("GF Gerudo Membership Card", "TH 1 Torch Carpenter"):
+            for location_name in (Locations.GF_GERUDO_MEMBERSHIP_CARD, Locations.TH_1_TORCH_CARPENTER):
                 world.included_locations[location_name] = carpenters_location_table[location_name]
 
         if world.options.shuffle_100_gs_reward:
