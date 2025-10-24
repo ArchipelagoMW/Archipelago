73cc418cafe653a3a449675fffaa49147baea28b
jjjj12212 <mjackson12212@hotmail.com>
2025-10-24 08:23:03 -0600

    MD5 change for latest Fear-1 fixes

--- worlds/glover/GloverClient.py
+++ worlds/glover/GloverClient.py
@@ -66,7 +66,7 @@ loc_name_to_id = network_data_package["games"]["Glover"]["location_name_to_id"]
 itm_name_to_id = network_data_package["games"]["Glover"]["item_name_to_id"]
 script_version: int = 1
 version: str = "V0.1"
-patch_md5: str = "b1ce4888356f29891d29059210e01371"
+patch_md5: str = "94074335dca9f4c1a537cbae3cbf8d12"
 gvr_options = settings.get_settings().glover_options
 program = None
 
