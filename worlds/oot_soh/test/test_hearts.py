730ba88dd8de4cc33ef92e410f86f9850dbc6b8e
Scipio Wright <scipiowright@gmail.com>
2025-10-28 15:26:27 -0400

    Remove pieces of heart from heart test

--- worlds/oot_soh/test/test_hearts.py
+++ worlds/oot_soh/test/test_hearts.py
@@ -7,25 +7,12 @@ class TestHearts(SohTestBase):
     world: SohWorld
 
     def test_heart_counts(self):
+        # pieces of heart are not progression, so we do not need to test them here
         self.assertTrue(self.multiworld.state.soh_heart_count[self.player] == 3)  # type: ignore
-
-        self.collect(self.world.create_item(Items.HEART_CONTAINER))
-        self.assertTrue(self.multiworld.state.soh_heart_count[self.player] == 4)  # type: ignore
-
-        for _ in range(3):
-            self.collect(self.world.create_item(Items.PIECE_OF_HEART))
-        self.assertTrue(self.multiworld.state.soh_heart_count[self.player] == 4)  # type: ignore
 
         heart = self.world.create_item(Items.HEART_CONTAINER)
         self.collect(heart)
-        self.assertTrue(self.multiworld.state.soh_heart_count[self.player] == 5)  # type: ignore
-
-        heart_piece = self.world.create_item(Items.PIECE_OF_HEART)
-        self.collect(heart_piece)
-        self.assertTrue(self.multiworld.state.soh_heart_count[self.player] == 6)  # type: ignore
-
-        self.remove(heart_piece)
-        self.assertTrue(self.multiworld.state.soh_heart_count[self.player] == 5)  # type: ignore
+        self.assertTrue(self.multiworld.state.soh_heart_count[self.player] == 4)  # type: ignore
 
         self.remove(heart)
-        self.assertTrue(self.multiworld.state.soh_heart_count[self.player] == 4)  # type: ignore
+        self.assertTrue(self.multiworld.state.soh_heart_count[self.player] == 3)  # type: ignore
