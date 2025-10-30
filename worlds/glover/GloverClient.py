9c296e805efeded353ecf8efa0f9cbc597625467
jjjj12212 <mjackson12212@hotmail.com>
2025-10-30 11:32:07 -0600

    Space3 Events

--- worlds/glover/GloverClient.py
+++ worlds/glover/GloverClient.py
@@ -66,7 +66,7 @@ loc_name_to_id = network_data_package["games"]["Glover"]["location_name_to_id"]
 itm_name_to_id = network_data_package["games"]["Glover"]["item_name_to_id"]
 script_version: int = 1
 version: str = "V0.1"
-patch_md5: str = "d5434c5180d39ebc459506ed57904f2c"
+patch_md5: str = "c479f1186103d019b1eb1d2366bc441a"
 gvr_options = settings.get_settings().glover_options
 program = None
 
