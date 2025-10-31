acd27a7dd2d95015645f8184c94074ad9313815a
Smg065 <tails2tails@gmail.com>
2025-10-31 13:59:19 -0600

    Powerball Never Opening Rule

--- worlds/glover/__init__.py
+++ worlds/glover/__init__.py
@@ -831,7 +831,12 @@ class GloverWorld(World):
         garibs_location.place_locked_item(all_garibs_item)
 
     def connect_entrances(self):
-        visualize_regions(self.multiworld.get_region("Menu", self.player), "Glover.puml", regions_to_highlight=self.multiworld.get_all_state().reachable_regions[self.player])
+        reachable_regions = self.multiworld.get_all_state().reachable_regions[self.player]
+        unreachable_regions = []
+        for each_region in self.multiworld.regions:
+            if not each_region in reachable_regions:
+                unreachable_regions.append(each_region)
+        visualize_regions(self.multiworld.get_region("Menu", self.player), "Glover.puml", regions_to_highlight=unreachable_regions)
         return super().connect_entrances()
 
     def build_options(self):
