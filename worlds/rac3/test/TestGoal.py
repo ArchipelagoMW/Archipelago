8167744460907dd0c0a102a02e6b25e0bd810eaa
Myth197 <myth197.rando@gmail.com>
2025-11-10 18:01:08 +0000

    RAC3: Update Unittests

--- worlds/rac3/test/TestGoal.py
+++ worlds/rac3/test/TestGoal.py
@@ -1,4 +1,4 @@
-from Rac3Addresses import RAC3ITEM, RAC3REGION
+from Rac3Addresses import RAC3ITEM, RAC3LOCATION, RAC3REGION
 from worlds.rac3.test import RAC3TestBase
 
 
@@ -11,22 +11,26 @@ class TestBiobliterator(RAC3TestBase):
         self.assertFalse(self.can_reach_region(RAC3REGION.STARSHIP_PHOENIX),
                          "Starship Phoenix reachable without coordinates")
         self.assertFalse(self.can_reach_region(RAC3REGION.COMMAND_CENTER), "Command Center reachable from Veldin")
-        self.assertFalse(self.can_reach_location("Command Center: Biobliterator Defeated!"),
-                         "Goal reachable from Veldin")
+        self.assertFalse(self.can_reach_location(RAC3LOCATION.COMMAND_CENTER_BIOBLITERATOR),
+                         "Goal location reachable from Veldin")
+        self.assertBeatable(False)
 
         state.sweep_for_advancements()
         self.assertTrue(self.can_reach_region(RAC3REGION.FLORANA), "Can't reach Florana from Veldin")
         self.assertTrue(self.can_reach_region(RAC3REGION.STARSHIP_PHOENIX), "Can't reach Starship Phoenix from Veldin")
         self.assertFalse(self.can_reach_region(RAC3REGION.COMMAND_CENTER), "Command Center reachable from Florana")
-        self.assertFalse(self.can_reach_location("Command Center: Biobliterator Defeated!"),
-                         "Goal reachable from Florana")
+        self.assertFalse(self.can_reach_location(RAC3LOCATION.COMMAND_CENTER_BIOBLITERATOR),
+                         "Goal location reachable from Florana")
+        self.assertBeatable(False)
 
         self.collect_by_name(RAC3ITEM.COMMAND_CENTER)
         self.assertTrue(self.can_reach_region(RAC3REGION.COMMAND_CENTER), "Can't reach Command Center with coordinates")
-        self.assertFalse(self.can_reach_location("Command Center: Biobliterator Defeated!"),
-                         "Goal reachable with no items")
+        self.assertFalse(self.can_reach_location(RAC3LOCATION.COMMAND_CENTER_BIOBLITERATOR),
+                         "Goal location reachable with no items")
+        self.assertBeatable(False)
 
         self.collect_by_name([RAC3ITEM.HYPERSHOT, RAC3ITEM.GRAV_BOOTS, RAC3ITEM.TYHRRA_GUISE, RAC3ITEM.HACKER,
                               RAC3ITEM.REFRACTOR])
-        self.assertTrue(self.can_reach_location("Command Center: Biobliterator Defeated!"),
-                        "Goal not reachable with items")
+        self.assertTrue(self.can_reach_location(RAC3LOCATION.COMMAND_CENTER_BIOBLITERATOR),
+                        "Goal location not reachable with items")
+        self.assertBeatable(True)
