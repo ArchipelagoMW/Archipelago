e402278114a73cf426c5d75296e97447ecac8aff
jjjj12212 <mjackson12212@hotmail.com>
2025-11-28 08:18:09 -0700

    wayroom ball fix

--- worlds/glover/GloverClient.py
+++ worlds/glover/GloverClient.py
@@ -66,7 +66,7 @@ loc_name_to_id = network_data_package["games"]["Glover"]["location_name_to_id"]
 itm_name_to_id = network_data_package["games"]["Glover"]["item_name_to_id"]
 script_version: int = 1
 version: str = "V0.1"
-patch_md5: str = "e479b7883fe883b378c51bf328dd355b"
+patch_md5: str = "1cc1ae4dfe7f18dd4e0cf2c1cc20fd4f"
 gvr_options = settings.get_settings().glover_options
 program = None
 
