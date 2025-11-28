37eeb32b5ff2945da3a6b0e27703eb911eb2f54e
Ixrec <ericrhitchcock@gmail.com>
2025-11-28 23:29:57 +0000

    actually let's just go to 0.6.4 so we can use world_version

--- worlds/nine_sols/__init__.py
+++ worlds/nine_sols/__init__.py
@@ -119,10 +119,7 @@ class NineSolsWorld(World):
         # more client/mod features, these are only in the apworld because we want them fixed per-slot/at gen time
         slot_data["jade_costs"] = self.jade_costs
         # APWorld versions are not (yet?) exposed by AP servers, so the client/mod needs us to put it in slot_data
-        apworld_manifest = orjson.loads(pkgutil.get_data(__name__, "archipelago.json").decode("utf-8"))
-        slot_data["apworld_version"] = apworld_manifest["world_version"]
-        # The version is stored on Worlds, so when we're ready to bump our min AP version to 0.6.4, we can do this:
-        # slot_data["apworld_version"] = self.world_version.to_simple_string()
+        slot_data["apworld_version"] = self.world_version.as_simple_string()
         return slot_data
 
     def write_spoiler(self, spoiler_handle: TextIO) -> None:
