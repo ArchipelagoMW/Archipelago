8aded002d9b9f57e7cb03efb053851bbc69bd916
Silvris <58583688+Silvris@users.noreply.github.com>
2025-11-28 14:16:24 -0600

    remove 0.6.3 compat

--- worlds/mm3/__init__.py
+++ worlds/mm3/__init__.py
@@ -94,8 +94,6 @@ class MM3World(World):
     location_name_groups = location_groups
     web = MM3WebWorld()
     rom_name: bytearray
-    if version_tuple < (0, 6, 4):
-        world_version: tuple[int, int, int] = (0, 1, 6)
 
     def __init__(self, world: MultiWorld, player: int):
         self.rom_name = bytearray()
