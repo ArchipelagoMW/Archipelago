7b680e15f311ca4c0f92a1422f53ee83730907f1
GlassToadstool <jacob.les.tabitha@gmail.com>
2025-12-01 19:51:11 +0000

    Started logic refactoring and updating

--- worlds/portal2/Items.py
+++ worlds/portal2/Items.py
@@ -1,6 +1,6 @@
 from enum import Flag, auto
 from BaseClasses import Item, ItemClassification
-from typing import NamedTuple, Optional
+from ItemNames import *
 
 class ItemTag(Flag):
     # Item type
@@ -12,61 +12,67 @@ class ItemTag(Flag):
     DELETE = auto()
     DISABLE = auto()
     ALTER = auto()
+
+portal_2_base_id = 98275000
+offset_index = 0
     
-class Portal2ItemData(NamedTuple):
-    in_game_name: str
-    variant: Optional[str]
-    tags: ItemTag
-    classification: ItemClassification
-    id_offset: int
+class Portal2ItemData:
+    def __init__(self, in_game_name: str, variant: str = None, tags: ItemTag = None, classification: ItemClassification = ItemClassification.progression):
+        self.in_game_name = in_game_name
+        self.variant = variant
+        self.tags = tags
+        self.classification = classification
+        
+        global portal_2_base_id, offset_index
+        self.id = portal_2_base_id + offset_index
+        offset_index += 1
 
 class Portal2Item(Item):
     game: str = "Portal 2"
 
 item_table: dict[str, Portal2ItemData] = {
     # Portal Guns
-    "Portal Gun": Portal2ItemData("weapon_portalgun", "CanFirePortal1", ItemTag.WEAPON | ItemTag.DISABLE, ItemClassification.progression, 0), # May remove
-    "Upgraded Portal Gun": Portal2ItemData("weapon_portalgun", "CanFirePortal2", ItemTag.WEAPON | ItemTag.DISABLE, ItemClassification.progression, 1),
-    "PotatOS": Portal2ItemData("weapon_portalgun", "potato", ItemTag.WEAPON | ItemTag.DISABLE, ItemClassification.progression, 2),
+    portal_gun_1: Portal2ItemData("weapon_portalgun", "CanFirePortal1", ItemTag.WEAPON | ItemTag.DISABLE, ItemClassification.progression and ItemClassification.useful), # May remove
+    portal_gun_2: Portal2ItemData("weapon_portalgun", "CanFirePortal2", ItemTag.WEAPON | ItemTag.DISABLE, ItemClassification.progression and ItemClassification.useful),
+    potatos: Portal2ItemData("weapon_portalgun", "potato", ItemTag.WEAPON | ItemTag.DISABLE, ItemClassification.progression), # Currently no logic set for this in game or in generation
 
     # Cubes (GetModelName())
-    "Weighted Cube": Portal2ItemData("prop_weighted_cube", "models/props/metal_box.mdl", ItemTag.ENTITY | ItemTag.DELETE | ItemTag.CUBE, ItemClassification.progression, 3),
-    "Reflection Cube": Portal2ItemData("prop_weighted_cube", "models/props/reflection_cube.mdl", ItemTag.ENTITY | ItemTag.DELETE | ItemTag.CUBE, ItemClassification.progression, 4),
-    "Spherical Cube": Portal2ItemData("prop_weighted_cube", "models/props_gameplay/mp_ball.mdl", ItemTag.ENTITY | ItemTag.DELETE | ItemTag.CUBE, ItemClassification.progression, 5),
+    weighted_cube: Portal2ItemData("prop_weighted_cube", "models/props/metal_box.mdl", ItemTag.ENTITY | ItemTag.DELETE | ItemTag.CUBE, ItemClassification.progression),
+    reflection_cube: Portal2ItemData("prop_weighted_cube", "models/props/reflection_cube.mdl", ItemTag.ENTITY | ItemTag.DELETE | ItemTag.CUBE, ItemClassification.progression),
+    spherical_cube: Portal2ItemData("prop_weighted_cube", "models/props_gameplay/mp_ball.mdl", ItemTag.ENTITY | ItemTag.DELETE | ItemTag.CUBE, ItemClassification.progression),
+    antique_cube: Portal2ItemData("prop_weighted_cube", "models/props_underground_underground_weighted_cube.mdl", ItemTag.ENTITY | ItemTag.DELETE | ItemTag.CUBE, ItemClassification.progression),
 
     # Buttons
-    "Button": Portal2ItemData("prop_button", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 6),
-    "Old Button": Portal2ItemData("prop_under_button", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 7),
-    "Floor Button": Portal2ItemData("prop_floor_button", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 8),
-    "Old Floor Button": Portal2ItemData("prop_under_floor_button", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 9),
-    "Cube Button": Portal2ItemData("prop_floor_cube_button", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 10),
-    "Ball Button": Portal2ItemData("prop_floor_ball_button", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 11),
+    button: Portal2ItemData("prop_button", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression),
+    old_button: Portal2ItemData("prop_under_button", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression),
+    floor_button: Portal2ItemData("prop_floor_button", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression),
+    old_floor_button: Portal2ItemData("prop_under_floor_button", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression),
+    cube_button: Portal2ItemData("prop_floor_cube_button", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression),
+    ball_button: Portal2ItemData("prop_floor_ball_button", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression),
 
     # Puzzle Elements
-    "Fizzler": Portal2ItemData("trigger_portal_cleanser", None, ItemTag.ENTITY | ItemTag.ALTER, ItemClassification.progression, 12),
-    "Frankenturret": Portal2ItemData("prop_monster_box", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 13),
-    "Paint": Portal2ItemData("info_paint_sprayer", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 14),
-    "Laser": Portal2ItemData("env_portal_laser", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 15),
-    "Faith Plate": Portal2ItemData("trigger_catapult", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 16),
-    "Funnel": Portal2ItemData("prop_tractor_beam", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 17),
-    "Bridge": Portal2ItemData("prop_wall_projector", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 18),
-    "Funnel": Portal2ItemData("prop_tractor_beam", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 19),
-    "Laser Relays": Portal2ItemData("prop_laser_relay", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 20),
-    "Laser Catcher": Portal2ItemData("prop_laser_catcher", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 21),
+    frankenturret: Portal2ItemData("prop_monster_box", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression),
+    paint: Portal2ItemData("info_paint_sprayer", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression), # Not yet implemented in game
+    laser: Portal2ItemData("env_portal_laser", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression),
+    faith_plate: Portal2ItemData("trigger_catapult", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression),
+    funnel: Portal2ItemData("prop_tractor_beam", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression),
+    bridge: Portal2ItemData("prop_wall_projector", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression),
+    laser_relays: Portal2ItemData("prop_laser_relay", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression),
+    laser_catcher: Portal2ItemData("prop_laser_catcher", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression),
     
     # Hazards
-    "Turrets": Portal2ItemData("npc_portal_turret_floor", None, ItemTag.ENTITY | ItemTag.DISABLE, ItemClassification.progression, 22),
+    turrets: Portal2ItemData("npc_portal_turret_floor", None, ItemTag.ENTITY | ItemTag.DISABLE, ItemClassification.progression),
 
     # Goal Items
-    "Adventure Core": Portal2ItemData("npc_personality_core", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 23),
-    "Space Core": Portal2ItemData("npc_personality_core", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 24),
-    "Fact Core": Portal2ItemData("npc_personality_core", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression, 25),
+    adventure_core: Portal2ItemData("npc_personality_core", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression),
+    space_core: Portal2ItemData("npc_personality_core", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression),
+    fact_core: Portal2ItemData("npc_personality_core", None, ItemTag.ENTITY | ItemTag.DELETE, ItemClassification.progression),
 
-    "Moon Dust": Portal2ItemData("", None, None, ItemClassification.filler, 26),
-    "Lemon": Portal2ItemData("", None, None, ItemClassification.filler, 27),
-    "Slice of Cake": Portal2ItemData("", None, None, ItemClassification.filler, 28),
+    moon_dust: Portal2ItemData("", None, None, ItemClassification.filler),
+    lemon: Portal2ItemData("", None, None, ItemClassification.filler),
+    slice_of_cake: Portal2ItemData("", None, None, ItemClassification.filler),
     
 }
 
 # Junk items
-junk_items = ["Moon Dust", "Lemon", "Slice of Cake"]
+junk_items = [moon_dust, lemon, slice_of_cake]
--- worlds/portal2/__init__.py
+++ worlds/portal2/__init__.py
@@ -1,4 +1,3 @@
-import random
 from BaseClasses import ItemClassification, MultiWorld, Region
 from .Options import Portal2Options
 from .Items import Portal2Item, Portal2ItemData, item_table, junk_items
@@ -27,6 +26,8 @@ class Portal2World(World):
     for key, value in all_locations_table.items():
         location_name_to_id[key] = value.id
     
+    # Helper Functions
+
     def create_item(self, name: str):
         self.item_count += 1
         return Portal2Item(name, item_table[name].classification, self.item_name_to_id[name], self.player)
@@ -35,32 +36,49 @@ class Portal2World(World):
         return_location = Portal2Location(self.player, name, id, parent)
         return return_location
     
+    def get_filler_item_name(self):
+        return self.multiworld.random(junk_items)
+    
+    def create_chapter_region(self, chapter_number: int) -> Region:
+        # Get chapters
+            chapter_map_dict = {name: info.id for name, info in map_complete_table.items()
+                            if name.split()[1].startswith(str(chapter_number))}
+            # Count locations
+            self.location_count += len(chapter_map_dict)
+            # Create Region
+            region = Region(f"Chapter{chapter_number}", self.player, self.multiworld)
+
+            # For non randomized locations
+            #region.add_locations(chapter_list, Portal2Location)
+
+            # For randomization
+            # Create a region for each level and place the location in that region (This handles the in order map completion logic for each chapter)
+            chapter_map_list = list(chapter_map_dict.keys())
+            self.multiworld.random.shuffle(chapter_map_list)
+
+            for c in chapter_map_list:
+                
+
+            return region
+    
+    # Overridden methods called by Main.py in execution order
+
     def create_regions(self) -> None:
         menu_region = Region("Menu", self.player, self.multiworld)
         self.multiworld.regions.append(menu_region)
 
         # Add chapters to those regions
-        for i in range(1, 10):
-            # Get chapters
-            chapter_list = {name: info.id for name, info in map_complete_table.items()
-                            if name.split()[1].startswith(str(i))}
-            # Count locations
-            self.location_count += len(chapter_list)
-            # Create Region
-            region = Region(f"Chapter{i}", self.player, self.multiworld)
-            region.add_locations(chapter_list, Portal2Location)
+        for i in range(1, 8):
+            region = self.create_chapter_region(i)
             menu_region.connect(region, f"Chapter {i} Entrance")
 
-        victory_loc = Portal2Location(self.player, "Beat Final Level", None, "Chapter 9")
+        # For chapter 9
+        chapter_9_region = self.create_chapter_region(9)
+        menu_region.connect(chapter_9_region, f"Chapter 9 Entrance")
+
+        victory_loc = Portal2Location(self.player, "Beat Final Level", None, chapter_9_region)
         victory_loc.place_locked_item(Portal2Item("Victory", ItemClassification.progression, None, self.player))
         self.multiworld.completion_condition[self.player] = lambda state: state.has("Victory", self.player)
-
-    def set_rules(self):
-        # Cannot access chapter 9 until goal items collected
-        set_rule(self.multiworld.get_entrance("Chapter 9 Entrance", self.player),
-             lambda state: state.has("Adventure Core", self.player) and
-                           state.has("Space Core", self.player) and
-                           state.has("Fact Core", self.player))
 
     def create_items(self):
         for item in item_table.keys():
@@ -69,6 +87,15 @@ class Portal2World(World):
         filler_name = self.get_filler_item_name()
         while self.item_count < self.location_count:
             self.multiworld.itempool.append(self.create_item(filler_name))
-    
-    def get_filler_item_name(self):
-        return random.choice(junk_items)
+
+    def set_rules(self):
+        # Cannot access chapter 9 until goal items collected
+        set_rule(self.multiworld.get_entrance("Chapter 9 Entrance", self.player),
+             lambda state: state.has("Adventure Core", self.player) and
+                           state.has("Space Core", self.player) and
+                           state.has("Fact Core", self.player))
+        
+    def fill_slot_data(self):
+        # Return the chapter map orders e.g. {chapter1: ['sp_a1_intro2', 'sp_a1_intro5', ...], chapter2: [...], ...}
+        # This is for generating and updating the Extras menu (level select screen) in portal 2 at the start and when checks are made
+        return {}
