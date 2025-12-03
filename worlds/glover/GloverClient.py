4214b28f2bc62922232978f1be3ff0ff389d568f
jjjj12212 <mjackson12212@hotmail.com>
2025-12-03 09:20:36 -0700

    debug ROM

--- worlds/glover/GloverClient.py
+++ worlds/glover/GloverClient.py
@@ -66,7 +66,7 @@ loc_name_to_id = network_data_package["games"]["Glover"]["location_name_to_id"]
 itm_name_to_id = network_data_package["games"]["Glover"]["item_name_to_id"]
 script_version: int = 1
 version: str = "V0.1"
-patch_md5: str = "27945bbef330784a6828dc82ea9ba4f7"
+patch_md5: str = "57fcaf468b783c4e85b078197716790c"
 gvr_options = settings.get_settings().glover_options
 program = None
 
