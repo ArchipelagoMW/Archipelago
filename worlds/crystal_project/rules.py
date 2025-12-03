c5cf03a0a244b41997bda8e087dc58e28e01ef1e
dragonsbutalsorabbits <kathrynibarber@gmail.com>
2025-12-03 11:44:14 -0800

    remembered to add progressive mount 5 to new fish race rule

--- worlds/crystal_project/rules.py
+++ worlds/crystal_project/rules.py
@@ -88,7 +88,7 @@ class CrystalProjectLogic:
         if is_rental_salmon_enough:
             return state.has(SALMON_RIVER_PASS, self.player) or self.options.regionsanity.value == self.options.regionsanity.option_disabled
         else:
-            return state.has(PROGRESSIVE_SALMON_VIOLA, self.player) and (state.has(SALMON_RIVER_PASS, self.player) or self.options.regionsanity.value == self.options.regionsanity.option_disabled)
+            return (state.has(PROGRESSIVE_SALMON_VIOLA, self.player) or state.has(PROGRESSIVE_MOUNT, self.player, 5)) and (state.has(SALMON_RIVER_PASS, self.player) or self.options.regionsanity.value == self.options.regionsanity.option_disabled)
 
     def has_golden_quintar(self, state: CollectionState) -> bool:
         return state.has(PROGRESSIVE_QUINTAR_WOODWIND, self.player, 3) or state.has(PROGRESSIVE_MOUNT, self.player, 7)
