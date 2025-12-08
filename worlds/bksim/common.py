129dfb39666e08537c549f384d76ace330ce46be
Emily Venezia <emilygamergirl99@gmail.com>
2025-12-07 19:08:13 -0500

    BKSim: fix StrEnum instead of str for item creation - v0.1.1

--- worlds/bksim/common.py
+++ worlds/bksim/common.py
@@ -2,14 +2,16 @@ from enum import StrEnum
 
 game_name = 'BKSimulator'
 
+
 class RID(StrEnum):
     HOME = 'Home'
     SUNNY = 'Sunny'
     RAINY = 'Rainy'
     SNOWY = 'Snowy'
+
+
 class ITEM(StrEnum):
     SHOES = 'Better Shoes'
     BOOTS = 'Snow Boots'
     NEWLOC = 'New Location'
     TOY = 'Plastic Toy'
-
