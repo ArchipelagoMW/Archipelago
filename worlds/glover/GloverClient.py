52db8dd72d84fb9d19b617d8360cebc38a9a7792
jjjj12212 <mjackson12212@hotmail.com>
2025-12-17 08:15:21 -0700

    Fix crash with Text, Default Checkpoint

--- worlds/glover/GloverClient.py
+++ worlds/glover/GloverClient.py
@@ -66,7 +66,7 @@ loc_name_to_id = network_data_package["games"]["Glover"]["location_name_to_id"]
 itm_name_to_id = network_data_package["games"]["Glover"]["item_name_to_id"]
 script_version: int = 1
 version: str = "V1.0"
-patch_md5: str = "72353615206548f76e6416b5d02b29ec"
+patch_md5: str = "b20ad7065b89568354d1427d0ada819b"
 gvr_options = settings.get_settings().glover_options
 program = None
 
