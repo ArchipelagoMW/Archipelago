2147fcc3925d64b51ff57581e606796e402ef3d8
DrAwesome4333 <22409322+DrAwesome4333@users.noreply.github.com>
2025-12-20 14:13:14 -0700

    Added CORS headers to API Endpoints

--- WebHostLib/api/__init__.py
+++ WebHostLib/api/__init__.py
@@ -2,10 +2,12 @@
 from typing import List, Tuple
 
 from flask import Blueprint
+from flask_cors import CORS
 
 from ..models import Seed, Slot
 
 api_endpoints = Blueprint('api', __name__, url_prefix="/api")
+cors = CORS(api_endpoints, resources={r"/api/*": {"origins": "*"}})
 
 
 def get_players(seed: Seed) -> List[Tuple[str, str]]:
