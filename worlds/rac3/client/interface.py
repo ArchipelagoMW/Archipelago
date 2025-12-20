83a9aed5f8f662f6aee5e38256d1008be4b4c56a
Taoshi <nicolaikh@live.dk>
2025-12-20 23:17:50 +0100

    RAC3: Add softlock prevention status to dump info

--- worlds/rac3/client/interface.py
+++ worlds/rac3/client/interface.py
@@ -231,6 +231,7 @@ class Rac3Interface(GameInterface):
     notification_time: float | None = None
     notification_merge_count: int = 1
     message_display: bool = False
+    ship_slot_limit: int = 0
 
     # Called at once when client started
     def init(self):
@@ -769,6 +770,7 @@ class Rac3Interface(GameInterface):
                 f'Planet{count}: {PLANET_NAME_FROM_ID[self._read8(RAC3_REGION_DATA_TABLE[name].SLOT_ADDRESS)]}')
             count += 1
         logger.info(f'Current planet Tracked: {self.planet}')
+        logger.info(f'Softlock Prevention:\nHolostar Studios - {self.softlock_prevention_check(RAC3REGION.HOLOSTAR_STUDIOS)}\nQwarks Hideout - {self.softlock_prevention_check(RAC3REGION.QWARKS_HIDEOUT)}')
         logger.info(f'Slot Data: {slot_data}')
 
     def multiplier_cycler(self):
@@ -1027,3 +1029,14 @@ class Rac3Interface(GameInterface):
         self._write32(self._read32(RAC3MESSAGEBOX.EDGE_COLOR_POINTER), default_theme.BOX)
         self._write32(self._read32(RAC3MESSAGEBOX.CENTER_COLOR_POINTER), default_theme.BOX)
         self._write32(self._read32(RAC3MESSAGEBOX.TEXT_COLOR_POINTER), default_theme.TEXT)
+    
+    def softlock_prevention_check(self, planet: str) -> bool:
+        match planet:
+            case RAC3REGION.QWARKS_HIDEOUT:
+                return (self.UnlockItem[RAC3ITEM.QWARKS_HIDEOUT].status > 0 and 
+                        self.UnlockItem[RAC3ITEM.REFRACTOR].status == 0)
+            case RAC3REGION.HOLOSTAR_STUDIOS:
+                return (self.UnlockItem[RAC3ITEM.HOLOSTAR_STUDIOS].status > 0 and
+                        self.UnlockItem[RAC3ITEM.HACKER].status == 0 and
+                        self.UnlockItem[RAC3ITEM.HYPERSHOT].status == 0)
+        return False
