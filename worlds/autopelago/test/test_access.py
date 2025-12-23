e0c4f545ab9154ea5a8b8073fc0ca9b8a91b4fee
Joe Amenta <airbreather@linux.com>
2025-12-23 18:17:34 -0500

    tests are important too

--- worlds/autopelago/test/test_access.py
+++ worlds/autopelago/test/test_access.py
@@ -1,9 +1,10 @@
-﻿from typing import TypedDict, Iterable, TypeVar
-from typing_extensions import Unpack
+﻿from collections.abc import Iterable
+from typing import TypedDict, TypeVar
 
 from test.bases import WorldTestBase
+from typing_extensions import Unpack
 
-T = TypeVar('T')
+T = TypeVar("T")
 
 
 def all_pairwise_combinations(*possible_items: T) -> Iterable[tuple[T, T]]:
@@ -19,116 +20,157 @@ class CollectKwargs(TypedDict):
 
 
 class AccessTest(WorldTestBase):
-    game = 'Autopelago'
+    game = "Autopelago"
 
     def setUp(self):
-        super(AccessTest, self).setUp()
-        self.pack_rat = self.world.create_item('Pack Rat')
-        self.rat_pack = self.world.create_item('Rat Pack')
+        super().setUp()
+        self.pack_rat = self.world.create_item("Pack Rat")
+        self.rat_pack = self.world.create_item("Rat Pack")
 
-    def collectRats(self, rat_count: int):
-        self.collect((self.pack_rat for _ in range(rat_count)))
+    def collect_rats(self, rat_count: int):
+        self.collect(self.pack_rat for _ in range(rat_count))
 
-    def removeRats(self, rat_count: int):
-        self.remove((self.pack_rat for _ in range(rat_count)))
+    def remove_rats(self, rat_count: int):
+        self.remove(self.pack_rat for _ in range(rat_count))
 
-    def assertAndCollectAccessDependencyFromHere(self, location: str, *possible_items: str | list[str],
-                                                 **kwargs: Unpack[CollectKwargs]):
-        for item_list in possible_items:
-            if item_list is str:
-                item_list = (item_list,)
-
+    def assert_and_collect_access_dependency_from_here(self, location: str, *possible_items: str | list[str],
+                                                       **kwargs: Unpack[CollectKwargs]):
+        for original_item_list in possible_items:
+            item_list = (original_item_list,) if original_item_list is str else original_item_list
             self.assertFalse(self.can_reach_location(location))
             self.collect_by_name(item_list)
             self.assertTrue(self.can_reach_location(location))
             self.remove_by_name(item_list)
-        if ('collect' not in kwargs) or kwargs['collect']:
+        if ("collect" not in kwargs) or kwargs["collect"]:
             self.collect_by_name(possible_items[0])
 
-    def assertAndCollectAdditionalRatsFromHere(self, location: str, rat_count: int, collect=True):
-        self.collectRats(max(0, rat_count - 5))
+    def assert_and_collect_additional_rats_from_here(self, location: str, rat_count: int, collect=True):
+        self.collect_rats(max(0, rat_count - 5))
         self.assertFalse(self.can_reach_location(location))
         self.collect(self.rat_pack)
         self.assertTrue(self.can_reach_location(location))
         self.remove(self.rat_pack)
-        self.collectRats(min(rat_count, 5))
+        self.collect_rats(min(rat_count, 5))
         self.assertTrue(self.can_reach_location(location))
         if not collect:
-            self.removeRats(rat_count)
+            self.remove_rats(rat_count)
 
-    def testTopBranches(self) -> None:
+    def test_top_branches(self) -> None:
         # World 1
-        self.assertAndCollectAdditionalRatsFromHere('Basketball', 5)
-        self.assertAndCollectAccessDependencyFromHere('Prawn Stars', 'Premium Can of Prawn Food', 'Priceless Antique')
-        self.assertAndCollectAccessDependencyFromHere('Angry Turtles', 'Pizza Rat', 'Ninja Rat', collect=False)
-        self.assertAndCollectAccessDependencyFromHere('Pirate Bake Sale', 'Pie Rat')
-        self.assertAndCollectAdditionalRatsFromHere('Bowling Ball Door', 4)
-        self.assertAndCollectAccessDependencyFromHere('Captured Goldfish', 'Giant Novelty Scissors')
+        self.assert_and_collect_additional_rats_from_here(
+            "Basketball", 5)
+        self.assert_and_collect_access_dependency_from_here(
+            "Prawn Stars", "Premium Can of Prawn Food", "Priceless Antique")
+        self.assert_and_collect_access_dependency_from_here(
+            "Angry Turtles", "Pizza Rat", "Ninja Rat", collect=False)
+        self.assert_and_collect_access_dependency_from_here(
+            "Pirate Bake Sale", "Pie Rat")
+        self.assert_and_collect_additional_rats_from_here(
+            "Bowling Ball Door", 4)
+        self.assert_and_collect_access_dependency_from_here(
+            "Captured Goldfish", "Giant Novelty Scissors")
 
         # World 2
-        self.assertAndCollectAccessDependencyFromHere('Computer Interface', 'Computer Rat')
-        self.assertAndCollectAccessDependencyFromHere('Kart Races', 'Blue Turtle Shell', 'Banana Peel')
-        self.assertAndCollectAccessDependencyFromHere('Daring Adventurer', 'Masterful Longsword', 'MacGuffin',
-                                                      collect=False)
-        self.assertAndCollectAdditionalRatsFromHere('Broken-Down Bus', 14)
-        self.assertAndCollectAccessDependencyFromHere('Copyright Mouse', 'Fake Mouse Ears', 'Legally Binding Contract')
-        self.assertAndCollectAdditionalRatsFromHere('Room Full of Typewriters', 12, collect=False)
-        self.assertAndCollectAccessDependencyFromHere('Binary Tree', 'Child\'s First Hand Axe')
-        self.assertAndCollectAccessDependencyFromHere('Rat Rap Battle', 'Fifty Cents', 'Notorious R.A.T.')
-        self.assertAndCollectAccessDependencyFromHere('Secret Cache', 'Virtual Key', 'Map of the Entire Internet')
+        self.assert_and_collect_access_dependency_from_here(
+            "Computer Interface", "Computer Rat")
+        self.assert_and_collect_access_dependency_from_here(
+            "Kart Races", "Blue Turtle Shell", "Banana Peel")
+        self.assert_and_collect_access_dependency_from_here(
+            "Daring Adventurer", "Masterful Longsword", "MacGuffin", collect=False)
+        self.assert_and_collect_additional_rats_from_here(
+            "Broken-Down Bus", 14)
+        self.assert_and_collect_access_dependency_from_here(
+            "Copyright Mouse", "Fake Mouse Ears", "Legally Binding Contract")
+        self.assert_and_collect_additional_rats_from_here(
+            "Room Full of Typewriters", 12, collect=False)
+        self.assert_and_collect_access_dependency_from_here(
+            "Binary Tree", "Child's First Hand Axe")
+        self.assert_and_collect_access_dependency_from_here(
+            "Rat Rap Battle", "Fifty Cents", "Notorious R.A.T.")
+        self.assert_and_collect_access_dependency_from_here(
+            "Secret Cache", "Virtual Key", "Map of the Entire Internet")
 
         # World 3
-        self.assertAndCollectAccessDependencyFromHere('Makeshift Rocket Ship', *all_pairwise_combinations(
-            'Energy Drink that is Pure Rocket Fuel', 'Pile of Scrap Metal in the Shape of a Rocket Ship',
-            'Ratstronaut', 'Turbo Encabulator'))
-        self.assertAndCollectAccessDependencyFromHere('Robo-Clop: The Robot War Horse', 'Quantum Sugar Cube')
-        self.assertAndCollectAccessDependencyFromHere('Homeless Mummy', 'Pharaoh-Not Anti-Mummy Spray', 'Ziggu Rat',
-                                                      collect=False)
-        self.assertAndCollectAdditionalRatsFromHere('Stalled Rocket', 15)
-        self.assertAndCollectAccessDependencyFromHere('Seal of Fortune', 'Constellation Prize', 'Free Vowel')
-        self.assertAndCollectAdditionalRatsFromHere('Space Opera', 9)
-        self.assertAndCollectAccessDependencyFromHere('Minotaur Labyrinth', 'Red Matador\'s Cape', 'Lab Rat',
-                                                      collect=False)
+        self.assert_and_collect_access_dependency_from_here(
+            "Makeshift Rocket Ship", *all_pairwise_combinations(
+            "Energy Drink that is Pure Rocket Fuel", "Pile of Scrap Metal in the Shape of a Rocket Ship",
+            "Ratstronaut", "Turbo Encabulator"))
+        self.assert_and_collect_access_dependency_from_here(
+            "Robo-Clop: The Robot War Horse", "Quantum Sugar Cube")
+        self.assert_and_collect_access_dependency_from_here(
+            "Homeless Mummy", "Pharaoh-Not Anti-Mummy Spray", "Ziggu Rat", collect=False)
+        self.assert_and_collect_additional_rats_from_here(
+            "Stalled Rocket", 15)
+        self.assert_and_collect_access_dependency_from_here(
+            "Seal of Fortune", "Constellation Prize", "Free Vowel")
+        self.assert_and_collect_additional_rats_from_here(
+            "Space Opera", 9)
+        self.assert_and_collect_access_dependency_from_here(
+            "Minotaur Labyrinth", "Red Matador's Cape", "Lab Rat", collect=False)
         self.assertBeatable(False)
-        self.assertAndCollectAccessDependencyFromHere('Snakes on a Planet', 'Mongoose in a Combat Spacecraft')
+        self.assert_and_collect_access_dependency_from_here(
+            "Snakes on a Planet", "Mongoose in a Combat Spacecraft")
         self.assertBeatable(True)
 
-    def testBottomBranches(self) -> None:
+    def test_bottom_branches(self) -> None:
         # World 1
-        self.assertAndCollectAdditionalRatsFromHere('Basketball', 5)
-        self.assertAndCollectAccessDependencyFromHere('Angry Turtles', 'Pizza Rat', 'Ninja Rat')
-        self.assertAndCollectAccessDependencyFromHere('Prawn Stars', 'Premium Can of Prawn Food', 'Priceless Antique',
-                                                      collect=False)
-        self.assertAndCollectAccessDependencyFromHere('Restaurant', 'Chef Rat')
-        self.assertAndCollectAdditionalRatsFromHere('Bowling Ball Door', 3)
-        self.assertAndCollectAccessDependencyFromHere('Captured Goldfish', 'Giant Novelty Scissors')
+        self.assert_and_collect_additional_rats_from_here(
+            "Basketball", 5)
+        self.assert_and_collect_access_dependency_from_here(
+            "Angry Turtles", "Pizza Rat", "Ninja Rat")
+        self.assert_and_collect_access_dependency_from_here(
+            "Prawn Stars", "Premium Can of Prawn Food", "Priceless Antique", collect=False)
+        self.assert_and_collect_access_dependency_from_here(
+            "Restaurant", "Chef Rat")
+        self.assert_and_collect_additional_rats_from_here(
+            "Bowling Ball Door", 3)
+        self.assert_and_collect_access_dependency_from_here(
+            "Captured Goldfish", "Giant Novelty Scissors")
 
         # World 2
-        self.assertAndCollectAccessDependencyFromHere('Computer Interface', 'Computer Rat')
-        self.assertAndCollectAccessDependencyFromHere('Daring Adventurer', 'Masterful Longsword', 'MacGuffin')
-        self.assertAndCollectAccessDependencyFromHere('Kart Races', 'Blue Turtle Shell', 'Banana Peel', collect=False)
-        self.assertAndCollectAdditionalRatsFromHere('Overweight Boulder', 14)
-        self.assertAndCollectAccessDependencyFromHere('Blue-Colored Screen Interface', 'Lost CTRL Key',
-                                                      'Hammer of Problem-Solving')
-        self.assertAndCollectAccessDependencyFromHere('Trapeze', 'Acro Rat', collect=False)
-        self.assertAndCollectAccessDependencyFromHere('Computer Ram', 'Artificial Grass')
-        self.assertAndCollectAccessDependencyFromHere('Stack of Crates', 'Forklift Certification', 'Gym Rat')
-        self.assertAndCollectAccessDependencyFromHere('Secret Cache', 'Virtual Key', 'Map of the Entire Internet')
+        self.assert_and_collect_access_dependency_from_here(
+            "Computer Interface", "Computer Rat")
+        self.assert_and_collect_access_dependency_from_here(
+            "Daring Adventurer", "Masterful Longsword", "MacGuffin")
+        self.assert_and_collect_access_dependency_from_here(
+            "Kart Races", "Blue Turtle Shell", "Banana Peel", collect=False)
+        self.assert_and_collect_additional_rats_from_here(
+            "Overweight Boulder", 14)
+        self.assert_and_collect_access_dependency_from_here(
+            "Blue-Colored Screen Interface", "Lost CTRL Key", "Hammer of Problem-Solving")
+        self.assert_and_collect_access_dependency_from_here(
+            "Trapeze", "Acro Rat", collect=False)
+        self.assert_and_collect_access_dependency_from_here(
+            "Computer Ram", "Artificial Grass")
+        self.assert_and_collect_access_dependency_from_here(
+            "Stack of Crates", "Forklift Certification", "Gym Rat")
+        self.assert_and_collect_access_dependency_from_here(
+            "Secret Cache", "Virtual Key", "Map of the Entire Internet")
 
         # World 3
-        self.assertAndCollectAccessDependencyFromHere('Makeshift Rocket Ship', *all_pairwise_combinations(
-            'Energy Drink that is Pure Rocket Fuel', 'Pile of Scrap Metal in the Shape of a Rocket Ship',
-            'Ratstronaut', 'Turbo Encabulator'))
-        self.assertAndCollectAccessDependencyFromHere('Robo-Clop: The Robot War Horse', 'Quantum Sugar Cube')
-        self.assertAndCollectAdditionalRatsFromHere('Stalled Rocket', 15, collect=False)
-        self.assertAndCollectAccessDependencyFromHere('Homeless Mummy', 'Pharaoh-Not Anti-Mummy Spray', 'Ziggu Rat')
-        self.assertAndCollectAccessDependencyFromHere('Frozen Assets', 'Playing with Fire For Dummies', collect=False)
-        self.assertAndCollectAccessDependencyFromHere('Alien Vending Machine', 'Foreign Coin', collect=False)
-        self.assertAndCollectAccessDependencyFromHere('Seal of Fortune', 'Constellation Prize', 'Free Vowel')
-        self.assertAndCollectAccessDependencyFromHere('Minotaur Labyrinth', 'Red Matador\'s Cape', 'Lab Rat')
-        self.assertAndCollectAdditionalRatsFromHere('Space Opera', 24, collect=False)
-        self.assertAndCollectAccessDependencyFromHere('Asteroid with Pants', 'Asteroid Belt', 'Moon Shaped Like a Butt',
-                                                      collect=False)
+        self.assert_and_collect_access_dependency_from_here(
+            "Makeshift Rocket Ship", *all_pairwise_combinations(
+            "Energy Drink that is Pure Rocket Fuel", "Pile of Scrap Metal in the Shape of a Rocket Ship",
+            "Ratstronaut", "Turbo Encabulator"))
+        self.assert_and_collect_access_dependency_from_here(
+            "Robo-Clop: The Robot War Horse", "Quantum Sugar Cube")
+        self.assert_and_collect_additional_rats_from_here(
+            "Stalled Rocket", 15, collect=False)
+        self.assert_and_collect_access_dependency_from_here(
+            "Homeless Mummy", "Pharaoh-Not Anti-Mummy Spray", "Ziggu Rat")
+        self.assert_and_collect_access_dependency_from_here(
+            "Frozen Assets", "Playing with Fire For Dummies", collect=False)
+        self.assert_and_collect_access_dependency_from_here(
+            "Alien Vending Machine", "Foreign Coin", collect=False)
+        self.assert_and_collect_access_dependency_from_here(
+            "Seal of Fortune", "Constellation Prize", "Free Vowel")
+        self.assert_and_collect_access_dependency_from_here(
+            "Minotaur Labyrinth", "Red Matador's Cape", "Lab Rat")
+        self.assert_and_collect_additional_rats_from_here(
+            "Space Opera", 24, collect=False)
+        self.assert_and_collect_access_dependency_from_here(
+            "Asteroid with Pants", "Asteroid Belt", "Moon Shaped Like a Butt", collect=False)
         self.assertBeatable(False)
-        self.assertAndCollectAccessDependencyFromHere('Snakes on a Planet', 'Mongoose in a Combat Spacecraft')
+        self.assert_and_collect_access_dependency_from_here(
+            "Snakes on a Planet", "Mongoose in a Combat Spacecraft")
         self.assertBeatable(True)
