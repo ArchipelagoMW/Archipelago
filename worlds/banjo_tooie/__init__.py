9ff5efee044a85b87315e4c69ba0ab9efb3605d3
g0goTAS <g0gosigchr@gmail.com>
2025-12-29 00:23:46 -0500

    indent refix

--- worlds/banjo_tooie/__init__.py
+++ worlds/banjo_tooie/__init__.py
@@ -592,8 +592,8 @@ class BanjoTooieWorld(World):
         if not self.options.randomize_notes.value \
                 and not self.options.randomize_signposts.value and not self.options.nestsanity.value \
                 and self.options.randomize_bk_moves.value != RandomizeBKMoveList.option_none:
-                raise OptionError("Your options are too restrictive for Randomize BK Moves."
-                    "Try randomizing notes, signs or nestsanity")
+            raise OptionError("Your options are too restrictive for Randomize BK Moves."
+                "Try randomizing notes, signs or nestsanity")
         if self.options.victory_condition.value == VictoryCondition.option_token_hunt:
             if self.options.token_hunt_length.value > self.options.tokens_in_pool.value:
                 self.options.token_hunt_length.value = self.options.tokens_in_pool.value
