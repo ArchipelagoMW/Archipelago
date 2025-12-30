459504b56b39516bbfce30e6ce756aabc416eca3
AmondoVagyok <solokapitany10@gmail.com>
2025-12-30 20:59:09 +0100

    RAC3: Rangers yaml option description changes

--- worlds/rac3/options/rangers_options.py
+++ worlds/rac3/options/rangers_options.py
@@ -6,11 +6,11 @@ class Rangers(Choice):
     """
     Determines whether Ranger Missions and anything that is located in or behind them is a location.
     None: Removes anything that is located in or behind a Ranger Mission from being a location.
-    Story Missions: Story related Ranger Missions, and anything directly locked behind them, are added as locations,
-    these are the following: all Marcadia, Blackwater City, Aridia mission.
+    Story Missions: Story related Ranger Missions, and anything directly locked behind them, are added as locations.
+    Story missions include: Marcadia, Blackwater City, and Aridia missions.
     Optional Missions: Ranger Missions that are optional in the main game, and anything directly locked behind them,
-    are added as locations, these are the following:
-    Tyhrranosis: Operation ISLAND STRIKE and Metropolis: Operation URBAN STORM missions
+    are added as locations.
+    Optional missions include: Tyhrranosis: Operation ISLAND STRIKE and Metropolis: Operation URBAN STORM missions
     All: Ranger Missions, and anything directly locked behind them, are added as locations.
     Any Skill Points or Titanium Bolts are added if their respective setting is enabled.
     """
