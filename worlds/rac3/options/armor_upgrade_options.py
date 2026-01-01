afc1237500e0bd77532345db8ecf2cca14bbf17f
AmondoVagyok <solokapitany10@gmail.com>
2026-01-01 20:32:17 +0100

    RAC3: Fixed typo

--- worlds/rac3/options/armor_upgrade_options.py
+++ worlds/rac3/options/armor_upgrade_options.py
@@ -8,7 +8,7 @@ class ArmorUpgrade(Range):
     If you wish to play without armor upgrades set this to 0
     If you wish to play with all vanilla armor upgrades in the pool set this to 4
     Anything above 4 will add extra copies of this item so the player has more chance to get an armor upgrade.
-    Collecting more than 4 Progressive Armor item will do nothing.
+    Collecting more than 4 Progressive Armor items will do nothing.
     """
     display_name = RAC3OPTION.ARMOR_UPGRADE
     range_start = 0
