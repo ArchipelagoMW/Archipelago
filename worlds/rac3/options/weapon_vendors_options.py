6ce03e8689bdb2551fe47512219e5cd48993714c
AmondoVagyok <solokapitany10@gmail.com>
2026-01-01 19:41:11 +0100

    RAC3: Changed weapon vendors option default to disabled, added warning into description, removed weapons from default excluded locations

--- worlds/rac3/options/weapon_vendors_options.py
+++ worlds/rac3/options/weapon_vendors_options.py
@@ -4,6 +4,9 @@ from worlds.rac3 import RAC3OPTION
 
 class WeaponVendors(Choice):
     """
+    Warning: Receiving weapons currently send out their associated vendor checks as well, it is recommended to keep
+    this option disabled until it is fixed.
+
     Determines whether weapon vendors are locations in the world.
     Disabled: No weapon vendors are locations.
     Enabled: Weapon vendors are added as locations.
@@ -11,4 +14,4 @@ class WeaponVendors(Choice):
     display_name = RAC3OPTION.WEAPON_VENDORS
     option_disabled = 0
     option_enabled = 1
-    default = 1
+    default = 0
