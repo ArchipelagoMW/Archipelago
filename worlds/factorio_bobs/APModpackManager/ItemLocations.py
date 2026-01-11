ec4a6b241c482f51ed8c11b4feea902d400049e1
Osiris32-and-a-half <64905829+osiris32-and-a-half@users.noreply.github.com>
2026-01-11 21:55:27 +0000

    Correctly bound item/location id's

--- worlds/factorio_bobs/APModpackManager/ItemLocations.py
+++ worlds/factorio_bobs/APModpackManager/ItemLocations.py
@@ -30,7 +30,7 @@ def add_item(name: str, item_id: int | None = None, bump = 0, groups: set[str] |
     TODO report which modpacks caused collision
     """
     if item_id is None:
-        item_id = int(hashlib.sha256(name.encode("utf-8")).hexdigest(), 16) + bump
+        item_id = (int(hashlib.sha256(name.encode("utf-8")).hexdigest(), 16) + bump) % (2**31-2) + 1
     if item_id in id_to_items and id_to_items[item_id] != name:
         raise ValueError(f"Item {id_to_items[item_id]} and {name} has had item collision with id: {item_id}",
                          "discover which packs implement them and report the error to their creators ")
@@ -69,7 +69,7 @@ def add_location(name: str, location_id: int | None = None, bump = 0, groups: se
     TODO report which modpacks caused collision
     """
     if location_id is None:
-        location_id = int(hashlib.sha256(name.encode("utf-8")).hexdigest(), 16) + bump
+        location_id = (int(hashlib.sha256(name.encode("utf-8")).hexdigest(), 16) + bump) % (2**31-2) + 1
     if location_id in id_to_locations and id_to_locations[location_id] != name:
         raise ValueError(f"location {id_to_locations[location_id]} and {name} has had location collision with id: {location_id}",
                          "discover which packs implement them and report the error to their creators ")
