0a338805aa14e1c37c977dfae47e7f23cf13c203
AmondoVagyok <solokapitany10@gmail.com>
2026-01-13 23:17:07 +0100

    RAC3: Handle Aquatos Base and Holostar first visit with Clank already collected

--- worlds/rac3/client/interface.py
+++ worlds/rac3/client/interface.py
@@ -1191,7 +1191,11 @@ class Rac3Interface(GameInterface):
                         self.UnlockItem[RAC3ITEM.HYPERSHOT].status == 0))
         return False
     def clank_cycler(self):
-        if self.UnlockItem[RAC3ITEM.CLANK].status:
+        if self.planet == RAC3REGION.HOLOSTAR_STUDIOS and self._read8(0x00142713) == 0:
+            self._write16(RAC3STATUS.NO_CLANK, 1)
+        elif self.planet == RAC3REGION.AQUATOS_BASE:
+            self._write16(RAC3STATUS.NO_CLANK, 1)
+        elif self.UnlockItem[RAC3ITEM.CLANK].status:
             if self.UnlockItem[RAC3ITEM.CLANK].unlock_delay:
                 self._write16(RAC3STATUS.NO_CLANK, 0)
                 self.UnlockItem[RAC3ITEM.CLANK].unlock_delay = 0
