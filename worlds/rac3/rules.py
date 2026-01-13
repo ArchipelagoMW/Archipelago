5480f1f368ca0ea282f5807f946f9cf733be545f
AmondoVagyok <solokapitany10@gmail.com>
2026-01-13 23:50:38 +0100

    RAC3: Clank logic start

--- worlds/rac3/rules.py
+++ worlds/rac3/rules.py
@@ -111,10 +111,13 @@ def set_rules(world: "RaC3World"):
         # RAC3VENDOR.FLORANA_WHIP
         # RAC3VENDOR.FLORANA_N60
         # RAC3TBOLT.FLORANA_BELOW_VENDOR
-        # RAC3TROPHY.FLORANA_RATCHET
-        # RAC3TBOLT.FLORANA_PATH_OF_DEATH
-        # RAC3SKILLPOINT.FLORANA_PATH
-        # RAC3LOCATION.FLORANA_DEFEAT_QWARK
+        RAC3TROPHY.FLORANA_RATCHET: lambda state: state.can_reach_location(
+            RAC3LOCATION.FLORANA_DEFEAT_QWARK, player=world.player),
+        RAC3TBOLT.FLORANA_PATH_OF_DEATH: lambda state: state.can_reach_location(
+            RAC3LOCATION.FLORANA_DEFEAT_QWARK, player=world.player),
+        RAC3SKILLPOINT.FLORANA_PATH: lambda state: state.can_reach_location(
+            RAC3LOCATION.FLORANA_DEFEAT_QWARK, player=world.player),
+        RAC3LOCATION.FLORANA_DEFEAT_QWARK: lambda state: state.has(RAC3ITEM.CLANK, world.player),
 
         # RAC3VENDOR.PHOENIX_SUCK
         # RAC3VENDOR.PHOENIX_INFECTOR
@@ -432,7 +435,7 @@ def set_rules(world: "RaC3World"):
         # RAC3LOCATION.DAXX_CHARGE_BOOTS
         # RAC3TROPHY.DAXX_PLUMBER
         RAC3LOCATION.DAXX_GUNSHIP: lambda state: state.has(RAC3ITEM.HYPERSHOT, world.player),
-        RAC3TBOLT.DAXX_TAXI: lambda state: state.has(RAC3ITEM.HYPERSHOT, world.player),
+        RAC3TBOLT.DAXX_TAXI: lambda state: state.has_all([RAC3ITEM.HYPERSHOT,RAC3ITEM.CLANK], world.player),
         RAC3TBOLT.DAXX_DOOR:
             lambda state: state.has_all([RAC3ITEM.HYPERSHOT, RAC3ITEM.HACKER, RAC3ITEM.CHARGE_BOOTS], world.player),
         RAC3LOCATION.DAXX_FACILITY: lambda state: state.has_all([RAC3ITEM.HYPERSHOT, RAC3ITEM.HACKER], world.player),
@@ -461,11 +464,12 @@ def set_rules(world: "RaC3World"):
 
         # RAC3LOCATION.SKIDD_CAPTURED
 
-        RAC3LOCATION.DRACO_COURTNEY: lambda state: state.has(RAC3ITEM.GRAV_BOOTS, world.player),
+        RAC3LOCATION.DRACO_COURTNEY: lambda state: state.has_all([RAC3ITEM.GRAV_BOOTS, RAC3ITEM.CLANK], world.player),
 
-        # RAC3TBOLT.ZELDRIN_STARPORT_1
-        RAC3TBOLT.ZELDRIN_STARPORT_2: lambda state: state.has(RAC3ITEM.HYPERSHOT, world.player),
-        RAC3LOCATION.ZELDRIN_STARPORT_ITEM: lambda state: state.has(RAC3ITEM.HYPERSHOT, world.player),
+        #RAC3TBOLT.ZELDRIN_STARPORT_1
+        RAC3TBOLT.ZELDRIN_STARPORT_2: lambda state: state.has_all([RAC3ITEM.HYPERSHOT, RAC3ITEM.CLANK], world.player),
+        RAC3LOCATION.ZELDRIN_STARPORT_ITEM:
+            lambda state: state.has_all([RAC3ITEM.HYPERSHOT, RAC3ITEM.CLANK], world.player),
         # RAC3LOCATION.ZELDRIN_STARPORT_SHIP
 
         RAC3SKILLPOINT.METROPOLIS_GOOD_YEAR: lambda state: state.has_any(
@@ -502,7 +506,8 @@ def set_rules(world: "RaC3World"):
             lambda state: state.has_any([RAC3ITEM.FLUX_RIFLE, RAC3ITEM.PROGRESSIVE_FLUX_RIFLE], world.player),
         RAC3LOCATION.CRASH_SITE_NANO_PAK:
             lambda state: state.has_all([RAC3ITEM.GRAV_BOOTS, RAC3ITEM.HYPERSHOT], world.player),
-        # RAC3LOCATION.CRASH_SITE_ESCAPE_POD
+        RAC3LOCATION.CRASH_SITE_ESCAPE_POD:
+            lambda state: state.has_any([RAC3ITEM.HYPERSHOT, RAC3ITEM.CLANK], world.player),
         RAC3LOCATION.CRASH_SITE_INFOBOT_ARIDIA: lambda state: state.has(RAC3ITEM.GRAV_BOOTS, world.player),
 
         # RAC3VENDOR.ARIDIA_QWACK_O_RAY
@@ -529,8 +534,10 @@ def set_rules(world: "RaC3World"):
         RAC3TBOLT.HIDEOUT: lambda state: state.has(RAC3ITEM.GRAV_BOOTS, world.player),
         RAC3LOCATION.HIDEOUT_PDA: lambda state: state.has(RAC3ITEM.GRAV_BOOTS, world.player),
         RAC3SKILLPOINT.HIDEOUT_DAN: lambda state: state.has_all([RAC3ITEM.WARP_PAD, RAC3ITEM.HYPERSHOT], world.player),
-        RAC3TROPHY.HIDEOUT_QWARK: lambda state: state.has_all([RAC3ITEM.WARP_PAD, RAC3ITEM.HYPERSHOT], world.player),
-        RAC3LOCATION.HIDEOUT_QWARK: lambda state: state.has_all([RAC3ITEM.WARP_PAD, RAC3ITEM.HYPERSHOT], world.player),
+        RAC3TROPHY.HIDEOUT_QWARK:
+            lambda state: state.has_all([RAC3ITEM.WARP_PAD, RAC3ITEM.HYPERSHOT, RAC3ITEM.CLANK], world.player),
+        RAC3LOCATION.HIDEOUT_QWARK:
+            lambda state: state.has_all([RAC3ITEM.WARP_PAD, RAC3ITEM.HYPERSHOT, RAC3ITEM.CLANK], world.player),
 
         # RAC3TROPHY.KOROS_COURTNEY
         # RAC3TBOLT.KOROS_FENCE
