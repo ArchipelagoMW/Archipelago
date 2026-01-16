c48a0226cd3adfa9d9a312657b53e5198a9d1bcc
Myth197 <myth197.rando@gmail.com>
2026-01-16 15:31:27 -0600

    RAC3: Set Phoenix as the home warp

--- worlds/rac3/client/interface.py
+++ worlds/rac3/client/interface.py
@@ -1160,7 +1160,7 @@ class Rac3Interface(GameInterface):
             logger.error(f'Aborting homewarp, Unknown Planet: {self.planet}')
             return
         planet_to_load = RAC3_REGION_DATA_TABLE[self.planet].PLANET_TO_LOAD
-        home_id = RAC3_REGION_DATA_TABLE[self.planet].ID
+        home_id = RAC3_REGION_DATA_TABLE[RAC3REGION.STARSHIP_PHOENIX].ID
         if planet_to_load:
             self._write8(planet_to_load, home_id)
             self._write8(RAC3STATUS.PLANET_LOAD, 1)
