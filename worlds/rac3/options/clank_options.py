ba7acba80a06f7d486c2af4dffb89fbb72471859
AmondoVagyok <solokapitany10@gmail.com>
2026-01-18 12:01:35 +0100

    RAC3: Added warning message to Clank Options yaml option

--- worlds/rac3/options/clank_options.py
+++ worlds/rac3/options/clank_options.py
@@ -7,6 +7,7 @@ class ClankOptions(Choice):
     Determines how Clank and his packs appear in the world.
     Vanilla: You start with Clank on Veldin.
     Clank item: Clank himself is randomized in the pool, findig him will give access to both packs.
+    Warning: Using this option while starting on Veldin may result in a restrictive start for solo seeds!
     """
     display_name = RAC3OPTION.CLANK_OPTIONS
     option_vanilla = 0
