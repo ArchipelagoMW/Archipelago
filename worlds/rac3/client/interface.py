5cbdebcdb4e20c4f484ee6e76d5cf799d94cbac6
AmondoVagyok <solokapitany10@gmail.com>
2026-01-18 15:22:20 +0100

    RAC3: Implement gadget hotfix

--- worlds/rac3/client/interface.py
+++ worlds/rac3/client/interface.py
@@ -704,7 +704,7 @@ class Rac3Interface(GameInterface):
                 # happening
                 or self._read16(RAC3STATUS.FALL_TIMER) == 1):
             return False
-        if self.planet != RAC3REGION.QWARKS_HIDEOUT or self.distance_to_moby(self.pda_vendor) < 12.0:
+        if self.planet == RAC3REGION.QWARKS_HIDEOUT and self.distance_to_moby(self.pda_vendor) < 12.0:
             return False
         return True
 
