6a3688ffb991e87d978210723d65bd9036858efe
Taoshi <nicolaikh@live.dk>
2026-01-20 19:24:27 +0100

    RAC3: Fix multiple messages getting queued for a single check

--- worlds/rac3/client/callbacks.py
+++ worlds/rac3/client/callbacks.py
@@ -106,7 +106,7 @@ async def handle_checked_locations(ctx: 'Context') -> None:
     if new_checks:
         real_checks = list(await ctx.check_locations(new_checks))
         ctx.locations_checked.update(real_checks)
-        for location in real_checks:
+        for location in set(real_checks):
             net_item = ctx.locations_info.get(location, None)
             if net_item is not None and net_item.player != ctx.slot:
                 item_to_player_names = get_rich_item_name(ctx, net_item, True)
