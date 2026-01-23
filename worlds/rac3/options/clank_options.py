ff8e43d5a477bb3a846e0b353e29967c3448f0ba
AmondoVagyok <solokapitany10@gmail.com>
2026-01-23 17:22:27 +0100

    RAC3: Add Clank Packs as a new yaml option.

--- worlds/rac3/options/clank_options.py
+++ worlds/rac3/options/clank_options.py
@@ -5,11 +5,13 @@ from worlds.rac3 import RAC3OPTION
 class ClankOptions(Choice):
     """
     Determines how Clank and his packs appear in the world.
-    Vanilla: You start with Clank on Veldin.
+    Vanilla: You start with Clank and all of his packs.
     Clank item: Clank himself is randomized in the pool, findig him will give access to both packs.
+    Clank Packs: The heli-pack and thruster-pack are randomized as separate items in the pool.
     Warning: Using this option while starting on Veldin may result in a restrictive start for solo seeds!
     """
     display_name = RAC3OPTION.CLANK_OPTIONS
     option_vanilla = 0
     option_clank_item = 1
+    option_clank_packs = 2
     default = 0
