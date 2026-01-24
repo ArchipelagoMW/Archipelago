6b1dd74bbaa1edb53158eeb73841acb2d55f8aad
Taoshi <nicolaikh@live.dk>
2026-01-24 01:59:51 +0100

    RAC3: Add armor vendor info to skin option

--- worlds/rac3/options/ratchet_skins_options.py
+++ worlds/rac3/options/ratchet_skins_options.py
@@ -7,6 +7,7 @@ class RatchetSkin(Choice):
     Cosmetic:
     What Skin should Ratchet have?
     Remember to reload your save file after starting a new game to apply the skin!
+    Alternatively, go to the Armor Vendor to reload the skin.
     """
     display_name = RAC3OPTION.SKIN
     option_default = 0
