99554775066ea0b435f9cf811284795aa7a1dcc9
Myth197 <myth197.rando@gmail.com>
2026-01-24 12:25:30 -0600

    RAC3: RaC3World import fix

--- worlds/rac3/__init__.py
+++ worlds/rac3/__init__.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 from worlds.LauncherComponents import Component, components, icon_paths, launch_subprocess, SuffixIdentifier, Type
 from worlds.rac3.constants.options import RAC3OPTION
-from worlds.rac3.world import World as Rac3World
+from worlds.rac3.world import RaC3World
 
 
 def run_client(_url: Optional[str] = None):
