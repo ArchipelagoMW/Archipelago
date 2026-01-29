3b5d7098305a5b45873ec00d48915907bd352be0
Taoshi <nicolaikh@live.dk>
2026-01-29 16:44:11 +0100

    RAC3: Basic Vendor cycling

--- worlds/rac3/constants/vendors/vendor.py
+++ worlds/rac3/constants/vendors/vendor.py
@@ -5,14 +5,27 @@ from worlds.rac3.constants.status import RAC3STATUS
 
 
 class RAC3VENDOR:
-    """Struct for the Vendor data, containing address offsets for where data can be read"""
+    """Base struct for Vendor data, containing common address offsets"""
     CURSOR_OFFSET: int = -0xC0
     SUBMENU_OFFSET: int = -0xBC
     MODEL_UPDATE_OFFSET: int = -0xB0
     SLOT_COUNT_OFFSET: int = 0x600
     VENDOR_TYPE_OFFSET: int = -0xF0
-    VENDOR_WEAPON_TYPE_OFFSET: int = 0x604  # 0 = Normal, 1 = Slim Cognito
 
+    @staticmethod
+    def get_vendor_property_address(planet, vendor_prop):
+        """Provides the vendor property address for reading data"""
+        return RAC3STATUS.VENDOR_BASE + PLANET_VENDOR_OFFSET[planet] + vendor_prop
+    
+    @classmethod
+    def get_vendor_item_property_address(cls, planet: str, slot: int, item_prop_offset: int):
+        """Provides the item property address for reading vendor item data, using the correct slot size for the vendor type."""
+        return cls.get_vendor_property_address(planet, 0) + (slot * cls.SLOT_SIZE) + item_prop_offset
+
+
+class RAC3WEAPONVENDOR(RAC3VENDOR):
+    """Struct for Weapon Vendor data, with weapon-specific slot size and offsets"""
+    VENDOR_WEAPON_TYPE_OFFSET: int = 0x604  # 0 = Normal, 1 = Slim Cognito
     SLOT_SIZE: int = 0x14
 
     ITEM_ID_OFFSET: int = 0x00  # 4 Bytes
@@ -24,12 +37,10 @@ class RAC3VENDOR:
     ITEM_ALL_AMMO_OFFSET: int = 0x12  # 1 Byte, 0 = Normal, 1 = All Ammo
     ITEM_MEMCARD_OFFSET: int = 0x13  # 1 Byte, 0 = Normal, 1 = Memory Card Check
 
-    @staticmethod
-    def get_vendor_item_property_address(planet: str, slot: int, item_prop_offset: int):
-        """Provides the item property address for reading data"""
-        return RAC3VENDOR.get_vendor_property_address(planet, 0) + (slot * RAC3VENDOR.SLOT_SIZE) + item_prop_offset
+class RAC3ARMORVENDOR(RAC3VENDOR):
+    """Struct for Armor Vendor data, with armor-specific slot size and offsets"""
+    SLOT_SIZE: int = 0x10
 
-    @staticmethod
-    def get_vendor_property_address(planet, vendor_prop):
-        """Provides the vendor property address for reading data"""
-        return RAC3STATUS.VENDOR_BASE + PLANET_VENDOR_OFFSET[planet] + vendor_prop
+    ITEM_ICON_OFFSET: int = 0x00  # 4 Bytes
+    ITEM_COST_OFFSET: int = 0x04  # 4 Bytes
+    ITEM_LEVEL_OFFSET: int = 0x08  # 4 Bytes
