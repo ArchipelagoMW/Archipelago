f180a2d449cc057bf0496ac96bc69b5f20ad6b38
Taoshi <nicolaikh@live.dk>
2026-02-01 02:15:26 +0100

    RAC3: Exclude the PDA from vendor update

--- worlds/rac3/constants/vendors/vendor.py
+++ worlds/rac3/constants/vendors/vendor.py
@@ -12,6 +12,7 @@ class RAC3VENDOR:
     MODEL_UPDATE_OFFSET: int = -0xB0
     SLOT_COUNT_OFFSET: int = 0x600
     VENDOR_TYPE_OFFSET: int = -0xF0
+    IS_PDA_OFFSET: int = -0xE4
     SLOT_SIZE: int = 0
 
     VENDORTYPE_TO_SLOT_SIZE: dict[int, int] = {
