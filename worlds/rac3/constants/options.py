61fd1722e4ac69dd19132aa79c4271dae6477e93
AmondoVagyok <solokapitany10@gmail.com>
2026-02-02 00:19:35 +0100

    RAC3: Refactor game name

--- worlds/rac3/constants/options.py
+++ worlds/rac3/constants/options.py
@@ -5,7 +5,7 @@ class RAC3OPTION:
     """String constants for RAC3 options"""
     OPTIONS = "Options"
     GAME_TITLE = "Rac3"
-    GAME_TITLE_FULL = "Ratchet & Clank 3"
+    GAME_TITLE_FULL = "Ratchet and Clank 3"
     VERSION = "Version"
     VERSION_NUMBER = "0.1.0"  # This is automatically updated by the GitHub actions workflow
     START_INVENTORY_FROM_POOL = "Start Inventory From Pool"
