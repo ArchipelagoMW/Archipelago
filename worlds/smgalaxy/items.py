8f1ca7b13a12589394f2cbe382397efc224bad88
Joethepic <60947591+Joethepic@users.noreply.github.com>
2026-02-06 10:06:39 -0600

    Update items.py

--- worlds/smgalaxy/items.py
+++ worlds/smgalaxy/items.py
@@ -8,9 +8,10 @@ class SMGItem(Item):
 # This is all the items that are used by the game we define them here so the they can be used.
 
 item_table: Dict[str, SMGLocationData] = {
-  "Power Star": 170000004, # rom address  0x007ACCA0F2FF8760
+  "Power Star": 170000004, # rom address  0x007ACCA0F2FF8760 don't remeber how i found this or if it's acurate so could use double check.
   "Progressive Grand Star": 170000005,
   "Green Star": 170000006,
   "Nothing": 170000007,
   "Peach": 170000008
 }
+
