55bc7dbf2ae8bea852a91aa32c82ede168c8494f
Emily Venezia <emilygamergirl99@gmail.com>
2026-02-08 13:22:40 -0500

    BKSim: Upgrade rules to RuleBuilder

--- worlds/bksim/rules.py
+++ worlds/bksim/rules.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from math import floor
-from ..generic.Rules import set_rule
+from rule_builder.rules import Has, True_
 from .common import *
 from .locations import BKSim_Location
 import typing
@@ -15,31 +15,30 @@ def set_rules(world: BKSimWorld) -> None:
     player = world.player
     options = world.options
 
-    world.get_region(RID.HOME).connect(connecting_region=world.get_region(RID.SUNNY))
-    world.get_region(RID.HOME).connect(connecting_region=world.get_region(RID.RAINY))
-    world.get_region(RID.HOME).connect(connecting_region=world.get_region(RID.SNOWY), rule=lambda state: state.has(ITEM.BOOTS, player))
+    world.create_entrance(world.get_region(RID.HOME), world.get_region(RID.SUNNY))
+    world.create_entrance(world.get_region(RID.HOME), world.get_region(RID.RAINY))
+    world.create_entrance(world.get_region(RID.HOME), world.get_region(RID.SNOWY), Has(ITEM.BOOTS))
 
     locs_list: typing.Iterable[BKSim_Location] = typing.cast(typing.Iterable[BKSim_Location], multiworld.get_locations(player))
     loc_count = options.locs_per_weather.value
-    max_rules = []
+    max_rule = True_()
     for loc in locs_list:
         if loc.info.region_id == RID.SUNNY:
             if loc.info.index == 0:
                 continue
-            tmp_rule = lambda state, idx=loc.info.index: state.has(ITEM.SHOES, player, floor(idx / 2))
-            if loc.info.index == loc_count - 1:
-                max_rules.append(tmp_rule)
-            set_rule(loc, tmp_rule)
+            tmp_rule = Has(ITEM.SHOES, floor(loc.info.index / 2))
+            if loc.info.index == loc_count - 1:  # Append the strictest requirement to a 'max rule'
+                max_rule &= tmp_rule
+            world.set_rule(loc, tmp_rule)
         elif loc.info.region_id == RID.RAINY:
-            tmp_rule = lambda state, idx=loc.info.index: (state.has(ITEM.SHOES, player, floor(idx / 2) + 1) and state.has(
-                ITEM.NEWLOC, player)) or state.has(ITEM.SHOES, player, idx)
-            if loc.info.index == loc_count - 1:
-                max_rules.append(tmp_rule)
-            set_rule(loc, tmp_rule)
+            tmp_rule = Has(ITEM.SHOES, loc.info.index) | (Has(ITEM.NEWLOC) & Has(ITEM.SHOES, floor(loc.info.index / 2) + 1))
+            if loc.info.index == loc_count - 1:  # Append the strictest requirement to a 'max rule'
+                max_rule &= tmp_rule
+            world.set_rule(loc, tmp_rule)
         elif loc.info.region_id == RID.SNOWY:
-            tmp_rule = lambda state, idx=loc.info.index: state.has(ITEM.BOOTS, player, floor(idx / 2) + 1)
-            if loc.info.index == loc_count - 1:
-                max_rules.append(tmp_rule)
-            set_rule(loc, tmp_rule)
+            tmp_rule = Has(ITEM.BOOTS, floor(loc.info.index / 2) + 1)
+            if loc.info.index == loc_count - 1:  # Append the strictest requirement to a 'max rule'
+                max_rule &= tmp_rule
+            world.set_rule(loc, tmp_rule)
 
-    multiworld.completion_condition[player] = lambda state: all(rule(state) for rule in max_rules)
+    world.set_completion_rule(max_rule)  # Require all the strictest requirements, as goal requires completing all locations.
