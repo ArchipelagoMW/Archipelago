e8987383d19bc757dcc0a2581b851d0de517cf8b
Nikkilite <Niko6697n@gmail.com>
2026-02-10 05:39:09 +0100

    Create world completion logic more streamlined

--- worlds/cat_quest/rules.py
+++ worlds/cat_quest/rules.py
@@ -48,9 +48,25 @@ def set_all_location_rules(world: CatQuestWorld) -> None:
             
 
 def set_completion_condition(world: CatQuestWorld) -> None:
-    world.multiworld.completion_condition[world.player] = lambda state: (
-        state.has_all(("Royal Art of Water Walking", "Royal Art of Flight"), world.player) and 
-        state.has_any(("Flamepurr", "Lightnyan", "Freezepaw", "Cattrap", "Astropaw",
-                       "Progressive Flamepurr", "Progressive Lightnyan", "Progressive Freezepaw", "Progressive Cattrap", "Progressive Astropaw"
-                       ), world.player)
-    )
+    if world.options.goal != "max_level":
+        if world.options.goal == "spellmastery":
+            if world.options.skill_upgrade == "progressive_skills":
+                world.multiworld.completion_condition[world.player] = lambda state: ( 
+                    state.has_all_counts({skill["name"]: 10 for skill in prog_skills}, world.player)
+                )
+            else:
+                world.multiworld.completion_condition[world.player] = lambda state: (
+                    state.has_all(("Flamepurr", "Lightnyan", "Freezepaw", "Cattrap", "Astropaw", "Healing Paw", "Purrserk"), world.player) and 
+                    (
+                        world.options.skill_upgrade == "coins" or 
+                        state.has_all_counts({upgrade["name"]: 9 for upgrade in prog_skill_uprades}, world.player) or
+                        state.has("Progressive Magic Level", world.player, 9)
+                    )              
+                )
+        else:
+            world.multiworld.completion_condition[world.player] = lambda state: (
+                state.has_all(("Royal Art of Water Walking", "Royal Art of Flight"), world.player) and 
+                state.has_any(("Flamepurr", "Lightnyan", "Freezepaw", "Cattrap", "Astropaw",
+                            "Progressive Flamepurr", "Progressive Lightnyan", "Progressive Freezepaw", "Progressive Cattrap", "Progressive Astropaw"
+                            ), world.player)
+            )
