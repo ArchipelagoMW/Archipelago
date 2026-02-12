cb3687e03888a3a84899b96c524597d1f6f005f8
Emily Venezia <emilygamergirl99@gmail.com>
2026-02-12 16:56:01 -0500

    ' -> "

--- worlds/apsudoku/__init__.py
+++ worlds/apsudoku/__init__.py
@@ -8,18 +8,18 @@ class AP_SudokuWebWorld(WebWorld):
     """
     Play a little Sudoku while you're in BK mode to maybe get some useful hints
     """
-    game = 'Sudoku'
+    game = "Sudoku"
     world_type = WorldType.HINT_GAME
     options_page = False
-    theme = 'partyTime'
+    theme = "partyTime"
 
     setup_en = Tutorial(
-        tutorial_name='Setup Guide',
-        description='A guide to playing APSudoku',
-        language='English',
-        file_name='setup_en.md',
-        link='setup/en',
-        authors=['EmilyV']
+        tutorial_name="Setup Guide",
+        description="A guide to playing APSudoku",
+        language="English",
+        file_name="setup_en.md",
+        link="setup/en",
+        authors=["EmilyV"]
     )
     
     tutorials = [setup_en]
