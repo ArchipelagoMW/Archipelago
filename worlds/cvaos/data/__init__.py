543a99cc8e516cc11f94796b89b74cfc393e317a
Dawn McKnight <belmont95@live.com>
2026-02-16 21:18:09 -0700

    refactor(data/__init__.py): adds the AbilityCombo class from routing_info to the module's exports

--- worlds/cvaos/data/__init__.py
+++ worlds/cvaos/data/__init__.py
@@ -4,7 +4,7 @@ from .entrance_info import EntranceInfo, entrance_info_collection
 from .item_info import ItemInfo, item_info_collection
 from .pickup_info import PickupInfo, pickup_info_collection
 from .room_info import RoomInfo, room_info_collection
-from .routing_info import RoutingInfo, entrance_to_entrance_info_collection
+from .routing_info import RoutingInfo, entrance_to_entrance_info_collection, AbilityCombo
 
 __all__ = [
     "EntranceInfo",
@@ -12,6 +12,7 @@ __all__ = [
     "PickupInfo",
     "RoomInfo",
     "RoutingInfo",
+    "AbilityCombo",
     "entrance_info_collection",
     "item_info_collection",
     "pickup_info_collection",
