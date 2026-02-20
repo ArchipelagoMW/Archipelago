9460b2c274d1f2b53ffc61eae0278abdbf8a689a
Ixrec <ericrhitchcock@gmail.com>
2026-02-20 01:48:34 +0000

    stop asserting on slot data keys; this never catches bugs, only makes bug fixes more annoying

--- worlds/nine_sols/test/test_basic.py
+++ worlds/nine_sols/test/test_basic.py
@@ -7,25 +7,7 @@ class TestDefaultWorld(NineSolsTestBase):
     def test_default_world(self):
         self.assertEqual(self.getLocationCount(), 324)  # 318 default locations + 6 events
 
-        # we don't assert on the whole slot_data dict because e.g. apworld_version would be tautological
-        # instead we assert on the set of keys to make sure we haven't forgotten to test a new one
         slot_data = self.world.fill_slot_data()
-        self.assertSetEqual(set(slot_data.keys()), {
-            'apworld_version',
-            'skip_soulscape_platforming',
-            'seals_for_eigong',
-            'seals_for_ethereal',
-            'seals_for_prison',
-            'jade_costs',
-            'first_root_node_name',
-            'logic_difficulty',
-            'shop_unlocks',
-            'kuafu_shop_unlock_sol_seals',
-            'chiyou_shop_unlock_sol_seals',
-            'kuafu_extra_inventory_unlock_sol_seals',
-            'prevent_weakened_prison_state',
-        })
-        # now for the "real" slot_data tests on our default world:
         self.assertEqual(slot_data['skip_soulscape_platforming'], 0)
         self.assertEqual(slot_data['seals_for_eigong'], 8)
         self.assertEqual(slot_data['seals_for_ethereal'], 4)
