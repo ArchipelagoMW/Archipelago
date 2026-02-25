d64c1ddc8e9b991e07cf2937f55f50e0e80c45f6
patrick.doan <pado879@gmail.com>
2026-02-25 13:16:45 -0500

    Updating Encounter Types

--- worlds/pokemon_emerald/regions.py
+++ worlds/pokemon_emerald/regions.py
@@ -43,7 +43,7 @@ def create_regions(world: "PokemonEmeraldWorld") -> Dict[str, Region]:
         """
         # For each of land, water, and fishing, connect the region if indicated by include_slots
         for i, (encounter_type, subcategories) in enumerate(encounter_categories.items()):
-            #if encounter_type in world.options
+            if encounter_type in world.options.dexsanity_encounter_types:
                 if include_slots[i]:
                     region_name = f"{map_name}_{encounter_type.value}_ENCOUNTERS"
 
