07e2381cbb20517247168661d9076b2d49c647e4
Uriel <imurx@proton.me>
2026-03-08 21:01:19 -0300

    try to prevent busy-looping on create random port socket when doing test

--- test/webhost/test_port_allocation.py
+++ test/webhost/test_port_allocation.py
@@ -84,7 +84,7 @@ class TestPortAllocating(unittest.TestCase):
             s.close()
 
         sockets.clear()
-        for _ in range(30_000):
+        for _ in range(30_000 - (len(get_used_ports()) + 100)):
             socket = create_random_port_socket(("30000-65535",), "127.0.0.1")
             sockets.append(socket)
             _, port = socket.getsockname()
