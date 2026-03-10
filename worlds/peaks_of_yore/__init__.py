1bd0115bd8fbdc4b9d4bbcab57334008c3ec52a0
EliottDup <eliott.duparc@gmail.com>
2026-03-10 13:38:07 +0100

    implement slot settings version

--- worlds/peaks_of_yore/__init__.py
+++ worlds/peaks_of_yore/__init__.py
@@ -232,5 +232,8 @@ class PeaksOfWorld(World):
                 state.can_reach_location(loc.name, self.player) for loc in self.multiworld.get_locations(self.player))
 
     def fill_slot_data(self) -> dict[str, Any]:
-        return self.options.as_dict("death_link", "goal", "rope_unlock_mode", "death_link_traps",
-                                    "game_mode", "disable_solemn_tempest", "include_free_solo", "include_time_attack", casing="camel")
+        data_dict = self.options.as_dict("death_link", "goal", "rope_unlock_mode", "death_link_traps",
+                                    "game_mode", "disable_solemn_tempest", "include_free_solo", "include_time_attack",
+                                    casing="camel")
+        data_dict.update({"settingsVer": 1})
+        return data_dict
