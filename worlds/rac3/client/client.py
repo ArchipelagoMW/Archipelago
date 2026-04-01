bc6337bc6501898aa05175621cc6d6783b1d6aa9
Taoshi <nicolaikh@live.dk>
2026-04-01 22:15:54 +0200

    RAC3: Fix other player's name not showing up in game

--- worlds/rac3/client/client.py
+++ worlds/rac3/client/client.py
@@ -299,8 +299,9 @@ class Rac3Context(CommonContext):
                     self.item_names.lookup_in_slot(net_item.item, net_item.player),
                     net_item.flags
                 )
-                format_color = RAC3TEXTFORMATSTRING.NORMAL if net_item.player == self.slot else RAC3TEXTFORMATSTRING.GREEN 
-                player_name = self.player_names.get(net_item.player, "???")
+                receiving_player = args.get("receiving", None)
+                format_color = RAC3TEXTFORMATSTRING.NORMAL if receiving_player == self.slot else RAC3TEXTFORMATSTRING.GREEN 
+                player_name = self.player_names.get(receiving_player, "???")
                 hint_text = f'{RAC3TEXTFORMATSTRING.WHITE}Hint: {format_color}{player_name}{RAC3TEXTFORMATSTRING.WHITE}\'s {item_name}{RAC3TEXTFORMATSTRING.WHITE} is at\\n{RAC3TEXTFORMATSTRING.GREEN}{location_name}'
                 if net_item.player != self.slot:
                     hint_text += f"\\n{RAC3TEXTFORMATSTRING.WHITE}in {format_color}{player_name}{RAC3TEXTFORMATSTRING.WHITE}'s world."
