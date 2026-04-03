59cdca8b93fb4f6a673e7c12438a59605d5512d9
Evereliquest <evereliquest@gmail.com>
2026-04-02 22:13:42 -0400

    Cleaning up and fixing the APWorld, currently four levels don't work

--- worlds/teardown/regions.py
+++ worlds/teardown/regions.py
@@ -7,64 +7,42 @@ from BaseClasses import Entrance, Region
 if TYPE_CHECKING:
     from .world import TeardownWorld
 
-# A region is a container for locations ("checks"), which connects to other regions via "Entrance" objects.
-# Many games will model their Levels after physical in-game places, but you can also have more abstract regions.
-# For a location to be in logic, its containing region must be reachable.
-# The Entrances connecting regions can have rules - more on that in rules.py.
-# This makes regions especially useful for traversal logic ("Can the player reach this part of the map?")
 
-# Every location must be inside a region, and you must have at least one region.
-# This is why we create regions first, and then later we create the locations (in locations.py).
 
 
 def create_and_connect_regions(world: TeardownWorld) -> None:
     create_all_regions(world)
     connect_regions(world)
 
-
+# Creates all regions
 def create_all_regions(world: TeardownWorld) -> None:
-    # Creating a region is as simple as calling the constructor of the Level class.
     level_1 = Region("Level 1", world.player, world.multiworld)
     level_2 = Region("Level 2", world.player, world.multiworld)
     level_3 = Region("Level 3", world.player, world.multiworld)
-    #level_4 = Region("Level 4", world.player, world.multiworld)
-    #level_5 = Region("Level 5", world.player, world.multiworld)
 
-    # Let's put all these regions in a list.
+# Lists all regions
     regions = [level_1, level_2, level_3]
 
-    # Some regions may only exist if the player enables certain options.
-    # In our case, the Hammer locks the top middle chest in its own room if the hammer option is enabled.
+# Creates region if option is enabled
     if world.options.Bonus_Level:
         bonus_level_4 = Region("Bonus Level 4", world.player, world.multiworld)
         regions.append(bonus_level_4)
 
-    # We now need to add these regions to multiworld.regions so that AP knows about their existence.
+# Adds all regions to list
     world.multiworld.regions += regions
 
-
+# Renames the objects we lost creating them
 def connect_regions(world: TeardownWorld) -> None:
-    # We have regions now, but still need to connect them to each other.
-    # But wait, we no longer have access to the region variables we created in create_all_regions()!
-    # Luckily, once you've submitted your regions to multiworld.regions,
-    # you can get them at any time using world.get_region(...).
     level_1 = world.get_region("Level 1")
     level_2 = world.get_region("Level 2")
     level_3 = world.get_region("Level 3")
-   # level_4 = world.get_region("Level 4")
-   # level_5 = world.get_region("Level 5")
 
-
-    # An even easier way is to use the region.connect helper.
+# Connects the regions
     level_1.connect(level_2, "Level 1 to Level 2")
     level_2.connect(level_3, "Level 2 to Level 3")
-   # level_3.connect(level_4, "Level 3 to Level 4")
-   #level_4.connect(level_5, "Level 4 to Level 5")
 
 
-    # Some Entrances may only exist if the player enables certain options.
-    # In our case, the Hammer locks the top middle chest in its own room if the hammer option is enabled.
-    # In this case, we previously created an extra "Level Extra 1" region that we now need to connect to Level 1.
+# Connects the region if option is enabled
     if world.options.Bonus_Level:
         bonus_level_4 = world.get_region("Bonus Level 4")
         level_3.connect(bonus_level_4, "Level 3 to Bonus Level 4")
--- worlds/teardown/web_world.py
+++ worlds/teardown/web_world.py
@@ -4,18 +4,14 @@ from worlds.AutoWorld import WebWorld
 from .options import option_groups, option_presets
 
 
-# For our game to display correctly on the website, we need to define a WebWorld subclass.
+# This is probably useless, should look into it
 class TeardownWebWorld(WebWorld):
-    # We need to override the "game" field of the WebWorld superclass.
-    # This must be the same string as the regular World class.
     game = "Teardown"
 
-    # Your game pages will have a visual theme (affecting e.g. the background image).
-    # You can choose between dirt, grass, grassFlowers, ice, jungle, ocean, partyTime, and stone.
+# You can choose between dirt, grass, grassFlowers, ice, jungle, ocean, partyTime, and stone.
     theme = "dirt"
 
     # A WebWorld can have any number of tutorials, but should always have at least an English setup guide.
-    # Many WebWorlds just have one setup guide, but some have multiple, e.g. for different languages.
     # We need to create a Tutorial object for every setup guide.
     # In order, we need to provide a title, a description, a language, a filepath, a link, and authors.
     # The filepath is relative to a "/docs/" directory in the root folder of your apworld.
@@ -28,18 +24,6 @@ class TeardownWebWorld(WebWorld):
         "setup/en",
         ["Evereliquest"],
     )
-    # Let's have our setup guide in German as well.
-    # Do not translate the title and description!
-    # WebHost needs them to be the same to identify that it is the same tutorial.
-    # This lets it display the tutorials more compactly.
-    #setup_de = Tutorial(
-    #    "Multiworld Setup Guide",
-    #    "A guide to setting up Teardown for MultiWorld.",
-    #    "German",
-    #    "setup_de.md",
-    #    "setup/de",
-    #    ["NewSoupVi"],
-    #)
 
     # We add these tutorials to our WebWorld by overriding the "tutorials" field.
     tutorials = [setup_en]
