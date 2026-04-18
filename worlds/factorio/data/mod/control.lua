3c5647fa21de82f23d294e8a9d287317607ff22f
Enderdraak <Endedraak@gmail.com>
2026-04-18 21:10:33 +0200

    changed the load order.
    This way the trigger techs that are unlocked by the starting technologies show up.

--- worlds/factorio/data/mod/control.lua
+++ worlds/factorio/data/mod/control.lua
@@ -3,5 +3,5 @@
 local handler = require("event_handler")
 
 -- used the build in event handler in the core to assigned the events.
-handler.add_lib(require("scripts/tech-obscurity.lua"))
 handler.add_lib(require("scripts/main.lua"))
+handler.add_lib(require("scripts/tech-obscurity.lua"))
