064269fbef9be92553611011eee47db3ffe42239
Taoshi <nicolaikh@live.dk>
2026-04-27 12:24:59 +0200

    RAC3: Add Hacker Skip yaml option

--- worlds/rac3/constants/options.py
+++ worlds/rac3/constants/options.py
@@ -42,4 +42,4 @@ class RAC3OPTION:
     CLANK_OPTIONS = "Clank Pack Options"
     SHIP_VENDOR = "Ship Vendor"
     ARMOR_VENDOR = "Armor Vendor"
-    SCOUT_VENDORS = "Scout Vendors"
+    HACKER_SKIP = "Hacker Skip"
