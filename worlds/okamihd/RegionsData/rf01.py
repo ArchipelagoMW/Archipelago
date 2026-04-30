bb0bb82c4ac907b7fe8d4fe9815b380ef8259efa
Ragmoa <ryckaert.guillaume@neuf.fr>
2026-04-30 23:45:32 +0200

    Move Shinshu Merchant to healed Shinshu File

--- worlds/okamihd/RegionsData/rf01.py
+++ worlds/okamihd/RegionsData/rf01.py
@@ -22,21 +22,3 @@ events = {
 locations = {
 }
 
-# Shop locations (shopId=18): 300000 + 18*1000 + slot = 318000 + slot
-# These are added separately and conditionally created based on RandomizeShops option
-shop_locations = {
-    RegionNames.SHINSHU_FIELD: {
-        "Shinshu Field - Shop Slot 1": LocData(318000, type=LocationType.SHOP),
-        "Shinshu Field - Shop Slot 2": LocData(318001, type=LocationType.SHOP),
-        "Shinshu Field - Shop Slot 3": LocData(318002, type=LocationType.SHOP),
-        "Shinshu Field - Shop Slot 4": LocData(318003, type=LocationType.SHOP),
-        "Shinshu Field - Shop Slot 5": LocData(318004, type=LocationType.SHOP),
-        "Shinshu Field - Shop Slot 6": LocData(318005, type=LocationType.SHOP),
-        "Shinshu Field - Shop Slot 7": LocData(318006, type=LocationType.SHOP),
-        "Shinshu Field - Shop Slot 8": LocData(318007, type=LocationType.SHOP),
-        "Shinshu Field - Shop Slot 9": LocData(318008, type=LocationType.SHOP),
-        "Shinshu Field - Shop Slot 10": LocData(318009, type=LocationType.SHOP),
-        "Shinshu Field - Shop Slot 11": LocData(318010, type=LocationType.SHOP),
-        "Shinshu Field - Shop Slot 12": LocData(318011, type=LocationType.SHOP),
-    }
-}
