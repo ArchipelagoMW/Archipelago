164eb98e73b3884e369b68351ce7053740adb0e7
lepideble <147614625+lepideble@users.noreply.github.com>
2026-05-03 21:02:43 +0200

    Set technologies as essential if they contain an advancement item

--- worlds/factorio/data/mod_template/data-final-fixes.lua
+++ worlds/factorio/data/mod_template/data-final-fixes.lua
@@ -178,6 +178,9 @@ copy_factorio_icon(new_tree_copy, "{{ progressive_technology_table[item.name][0]
 {#- use default AP icon if no Factorio graphics exist -#}
 {% if item.advancement or not tech_tree_information %}set_ap_icon(new_tree_copy){% else %}set_ap_unimportant_icon(new_tree_copy){% endif %}
 {%- endif -%}
+{%- if tech_tree_information %}
+new_tree_copy.essential = {{ variable_to_lua(item.advancement) }}
+{%- endif -%}
 {#- connect Technology  #}
 {%- if location in tech_tree_layout_prerequisites %}
 {%- for prerequisite in tech_tree_layout_prerequisites[location] %}
