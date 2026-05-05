0c3b82fd729fc11524c0ac1d316c951acadc1baf
Evereliquest <evereliquest@gmail.com>
2026-05-05 01:20:15 -0400

    It Works!! Basic tests are included

---
+++ worlds/teardown/test/testforty.py
@@ -0,0 +1,19 @@
+from .bases import TeardownTestBase
+
+
+# When writing a test, you'll first need to subclass unittest.TestCase.
+# In our case, we'll subclass the APQuestTestBase we defined in bases.py.
+class BasicTestLogic(TeardownTestBase):
+    # Our test base is a subclass of WorldTestBase.
+    # WorldTestBase takes a dict of options and sets up a multiworld for you with a single world of your game.
+    # The world will have the options you specified.
+    options = {
+        "Mission Amount": 40,
+        "Randomize Starting Tools": False,
+        "Randomize Starting Level": True,
+        #"Valuable Sanity": False,
+
+        # Options you don't specify will use their default values.
+        # It is good practice to specify every option that has an impact on your test, even when it's the default value.
+        # As such, we'll spell out that hard_mode is meant to be False.
+    }
