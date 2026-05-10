40cc3ed49dd0b85518ba26d4b12c5fb0441e402c
Ragmoa <ryckaert.guillaume@neuf.fr>
2026-05-10 16:55:52 +0200

    Again forgot to comment debug again

--- worlds/okamihd/__init__.py
+++ worlds/okamihd/__init__.py
@@ -49,7 +49,7 @@ class OkamiWorld(World):
 
         create_regions(self)
         # DEBUG
-        visualize_regions(self.multiworld.get_region("Menu", self.player),"G:\projets\OkamiAP\worlds\okamihd\docs\OkamiHD.puml")
+        # visualize_regions(self.multiworld.get_region("Menu", self.player),"G:\projets\OkamiAP\worlds\okamihd\docs\OkamiHD.puml")
 
     def create_items(self):
         self.multiworld.itempool += self.create_itempool()
