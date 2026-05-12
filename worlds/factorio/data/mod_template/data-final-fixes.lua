98afd8c46f650f0f3afe651e7608644abcd06008
Enderdraak <Endedraak@gmail.com>
2026-05-12 22:41:44 +0200

    Found more text
    removed old commented out code.
    Removed extra changes from other branches
    Found more text to be put in the locale.

--- worlds/factorio/data/mod_template/data-final-fixes.lua
+++ worlds/factorio/data/mod_template/data-final-fixes.lua
@@ -154,7 +154,7 @@ technologies["{{ original_tech_name }}"].hidden_in_factoriopedia = true
 {#- the tech researched by the local player #}
 new_tree_copy = table.deepcopy(template_tech)
 new_tree_copy.name = "ap-{{ location.address }}-"{# use AP ID #}
-{%- if (location.revealed or tech_tree_information == 2) %}
+{%- if location.revealed %}
 new_tree_copy.localised_name = {"technology-name.ap-technology-full", "{{ player_names[item.player] }}", "{{ item.name }}", "{{ location.name }}"}
 new_tree_copy.localised_description  = {"technology-description.ap-technology-full", "{{ item.name }}", "{{ player_names[item.player] }}", {% if item.advancement %}{"technology-description.ap-technology-item-advancement"}{% elif item.useful %}{"technology-description.ap-technology-item-useful"}{% elif item.trap %}{"technology-description.ap-technology-item-trap"}{% else %}""{% endif %}}
 {%- else  %}
@@ -171,7 +171,7 @@ new_tree_copy.unit = nil
 new_tree_copy.unit.count = {{ location.count }}
 new_tree_copy.unit.ingredients = {{ variable_to_lua(location.factorio_ingredients) }}
 {% endif %}
-{%- if (location.revealed or tech_tree_information == 2) and item.name in base_tech_table -%}
+{%- if location.revealed and item.name in base_tech_table -%}
 {#- copy Factorio Technology Icon #}
 copy_factorio_icon(new_tree_copy, "{{ item.name }}")
 {%- if item.name == "rocket-silo" and item.player == location.player %}
@@ -179,7 +179,7 @@ copy_factorio_icon(new_tree_copy, "{{ item.name }}")
 table.insert(new_tree_copy.effects, {type = "nothing", effect_description = "Ingredient {{ loop.index }}: {{ ingredient }}"})
 {% endfor -%}
 {% endif -%}
-{%- elif (location.revealed or tech_tree_information == 2) and item.name in progressive_technology_table -%}
+{%- elif location.revealed and item.name in progressive_technology_table -%}
 copy_factorio_icon(new_tree_copy, "{{ progressive_technology_table[item.name][0] }}")
 {%- else -%}
 {#- use default AP icon if no Factorio graphics exist -#}
