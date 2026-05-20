2a8c03bc54b8966e925d3f9c92a4841f3d1c0525
qwint <qwint.42@gmail.com>
2026-05-19 21:38:39 -0500

    refactor to use a class

--- utils/curses_utils.py
+++ utils/curses_utils.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import curses
 import curses.textpad
 import curses.ascii
@@ -6,6 +5,168 @@ import curses.ascii
 
 class CursesCancel(Exception):
     pass
+
+
+class SelectBox:
+    ret: str | None
+    window: curses.window
+    data: list[str]
+
+    # static per instance layout info
+    MAX_BOX_HEIGHT: int
+    MAX_BOX_LENGTH: int
+    MAX_POINTER: int
+    POINTER_COL: int
+    DATA_COL: int
+    TOP_OF_SCREEN: int
+
+    # current selection / screen position
+    pointer: int
+    scroll_offset: int
+
+    # set by resize
+    computed_box_height: int
+    computed_box_length: int
+    max_scroll_offset: int
+
+    def __init__(self, data: list[str], max_height=30, max_length=30):
+        self.ret = None
+        self.data = data
+
+        self.MAX_BOX_HEIGHT = max_height
+        self.MAX_BOX_LENGTH = max_length
+        self.MAX_POINTER = len(data) - 1
+        self.POINTER_COL = 1
+        self.DATA_COL = self.POINTER_COL + 1
+        self.TOP_OF_SCREEN = 1
+
+        pointer = 0
+        scroll_offset = 0
+
+    def run(self, stdscr):
+        """
+        Entry point for running curses on a new window
+        Will run until a value is picked or is cancelled by the user
+        """
+        self.window = stdscr
+        self.resize()
+        while self.ret is None:
+            self.ret = self.poll()
+            pass
+
+    @property
+    def scrolling_enabled(self):
+        return self.MAX_POINTER > self.computed_box_height
+
+    def resize(self):
+        """Calculates the max usable area and redraws the selection box and its contents"""
+        self.pointer = self.scroll_offset = 0
+        y, x = self.window.getmaxyx()
+
+        self.computed_box_height = min(min(self.MAX_BOX_HEIGHT, y - 2), self.MAX_POINTER)
+        self.computed_box_length = min(self.MAX_BOX_LENGTH, x - 3)
+        # these seem to cause trouble with single digit resized terminals but i'm ok with that for now
+
+        self.max_scroll_offset = self.MAX_POINTER + 1 - self.computed_box_height
+        curses.textpad.rectangle(self.window, 0, 0, self.computed_box_height + 1, self.computed_box_length + 1)
+        self.write_data()
+        self.set_pointer()
+        self.window.refresh()
+
+    def addstr(self, text: str, line: int):
+        """Clears the given window line and replaces it with the given text"""
+        self.window.addstr(self.TOP_OF_SCREEN + line, self.DATA_COL, " " * (self.computed_box_length - 1))
+        self.window.addstr(self.TOP_OF_SCREEN + line, self.DATA_COL, text)
+
+    def write_data(self):
+        """writes all text from data to the appropriate scrolled position in the window"""
+        for index, text in enumerate(self.data):  # todo: switch to enumerating offset by slicing data first
+            offset_index = index - self.scroll_offset
+            if 0 <= offset_index < self.computed_box_height:
+                self.addstr(text, offset_index)
+
+    def move_pointer(self, down: bool):
+        """Shifts the pointer if we can, and scrolls the screen if we shift offscreen"""
+        if self.pointer <= 0 and not down:
+            return
+        if self.pointer >= self.MAX_POINTER and down:
+            return
+        previous = self.pointer
+        self.pointer += int(down or -1)
+        if self.scrolling_enabled and previous == self.scroll_offset and not down:
+            self.scroll_offset -= 1
+            self.write_data()
+        elif self.scrolling_enabled and previous - self.scroll_offset == self.computed_box_height - 1 and down:
+            self.scroll_offset += 1
+            self.write_data()
+
+        self.set_pointer()
+
+    def set_pointer(self):
+        """Refreshes the pointer column to clear highlighting and pointer text and reset on the appropriate row"""
+        for i in range(self.computed_box_height):
+            v_index = self.TOP_OF_SCREEN + i
+            self.window.addch(v_index, self.POINTER_COL, " ", curses.A_NORMAL)
+            self.window.chgat(v_index, self.POINTER_COL, self.computed_box_length, curses.A_NORMAL)
+        v_index = self.TOP_OF_SCREEN + self.pointer - self.scroll_offset
+        self.window.addch(v_index, self.POINTER_COL, "*", curses.A_STANDOUT)
+        self.window.chgat(v_index, self.POINTER_COL, self.computed_box_length, curses.A_STANDOUT)
+
+    def page(self, down: bool):
+        """Dynamically scrolls the view down a page, keeping the cursor constant unless we are already at the extreme"""
+        # get the index for blindly paging
+        new_pointer = self.pointer + (int(down or -1) * self.computed_box_height)
+
+        # if the blind paging goes too far off the valid values
+        if new_pointer >= self.MAX_POINTER:
+            # and we are already scrolled as far down
+            if self.scroll_offset >= self.max_scroll_offset:
+                # jump self.pointer to the bottom of the list
+                self.pointer = self.MAX_POINTER
+            # else we can scroll to the bottom and offset the pointer by the scrolled amount
+            else:
+                self.pointer -= (self.scroll_offset - self.max_scroll_offset)
+                self.scroll_offset -= (self.scroll_offset - self.max_scroll_offset)
+        # elif the blind paging goes too far up
+        elif new_pointer <= 0:
+            # and we are already scrolled as far up
+            if self.scroll_offset <= 0:
+                # jump pointer to the top of the list
+                self.pointer = 0
+            # else we can scroll to the top and offset the pointer by the scrolled amount
+            else:
+                self.pointer -= self.scroll_offset
+                self.scroll_offset -= self.scroll_offset
+        # else apply the blind paging
+        else:
+            self.pointer = new_pointer
+            self.scroll_offset += (int(down or -1) * self.computed_box_height)
+            # but if the scroll offset goes out of bounds, bind it while offsetting pointer to real scrolled amount
+            if self.scroll_offset < 0:
+                self.pointer -= self.scroll_offset
+                self.scroll_offset -= self.scroll_offset
+            elif self.scroll_offset > self.max_scroll_offset:
+                self.pointer -= (self.scroll_offset - self.max_scroll_offset)
+                self.scroll_offset -= (self.scroll_offset - self.max_scroll_offset)
+        self.write_data()
+        self.set_pointer()
+
+    def poll(self) -> int | None:
+        key = self.window.getch()  # implicit .refresh()
+        if key == curses.KEY_UP:
+            self.move_pointer(down=False)
+        elif key == curses.KEY_DOWN:
+            self.move_pointer(down=True)
+        elif key == curses.KEY_RIGHT:
+            self.page(down=True)
+        elif key == curses.KEY_LEFT:
+            self.page(down=False)
+        elif key == curses.ascii.ESC:
+            raise CursesCancel("User Cancelled")
+        elif key == curses.KEY_RESIZE:
+            self.resize()
+        else:
+            return self.pointer
 
 
 def curses_select(data: list[str]) -> str | None:
@@ -17,148 +178,13 @@ def curses_select(data: list[str]) -> str | None:
 
     :return: None if cancelled, else the chosen string.
     """
-    def select_box(stdscr):
-        nonlocal ret
-        POINTER_COL = 1
-        DATA_COL = POINTER_COL + 1
-        MAX_POINTER = len(data) - 1
-        VERTICAL_OFFSET = 1
-        BASE_BOX_HEIGHT = 30
-        BASE_BOX_LENGTH = 40
-        MAX_BOX_HEIGHT: int = 0
-        MAX_BOX_LENGTH: int = 0
-        SCROLLING_ENABLED: bool = False
-        BOX_HEIGHT: int = 0
-        MAX_SCROLL_OFFSET: int = 0
 
-        # shared attrs for the select
-        pointer = 0
-        scroll_offset = 0
+    window = SelectBox(data)
 
-        def resize(self):
-            nonlocal MAX_BOX_HEIGHT
-            nonlocal MAX_BOX_LENGTH
-            nonlocal SCROLLING_ENABLED
-            nonlocal BOX_HEIGHT
-            nonlocal MAX_SCROLL_OFFSET
-            nonlocal pointer
-            nonlocal scroll_offset
-            pointer = scroll_offset = 0
-            _maxyx = self.getmaxyx()
-            MAX_BOX_HEIGHT = min(BASE_BOX_HEIGHT, _maxyx[0] - 2)
-            MAX_BOX_LENGTH = min(BASE_BOX_LENGTH, _maxyx[1] - 3)
-            # these seem to cause trouble with single digit resized terminals but i'm ok with that for now
-
-            SCROLLING_ENABLED = MAX_POINTER > MAX_BOX_HEIGHT
-            BOX_HEIGHT = min(MAX_BOX_HEIGHT, MAX_POINTER)
-            MAX_SCROLL_OFFSET = MAX_POINTER + 1 - BOX_HEIGHT
-            curses.textpad.rectangle(self, 0, 0, BOX_HEIGHT + 1, MAX_BOX_LENGTH + 1)
-            write_data(self)
-            set_pointer(self, pointer)
-            self.refresh()
-
-        def write_data(self):
-            for index, line in enumerate(data):
-                offset_index = index - scroll_offset
-                if 0 <= offset_index < BOX_HEIGHT:
-                    self.addstr(VERTICAL_OFFSET + offset_index, DATA_COL, line + " " * (MAX_BOX_LENGTH - 1 - len(line)))
-
-        def move_pointer(self, down: bool):
-            nonlocal pointer
-            nonlocal scroll_offset
-            if pointer <= 0 and not down:
-                return
-            if pointer >= MAX_POINTER and down:
-                return
-            previous = pointer
-            pointer += int(down or -1)
-            if SCROLLING_ENABLED and previous == scroll_offset and not down:
-                scroll_offset -= 1
-                write_data(self)
-            elif SCROLLING_ENABLED and previous - scroll_offset == BOX_HEIGHT - 1 and down:
-                scroll_offset += 1
-                write_data(self)
-
-            set_pointer(self, pointer)
-
-        def set_pointer(self, new):
-            for i in range(BOX_HEIGHT):
-                self.addch(VERTICAL_OFFSET + i, POINTER_COL, " ", curses.A_NORMAL)
-                self.chgat(VERTICAL_OFFSET + i, POINTER_COL, MAX_BOX_LENGTH, curses.A_NORMAL)
-            self.addch(VERTICAL_OFFSET + new - scroll_offset, POINTER_COL, "*", curses.A_STANDOUT)
-            self.chgat(VERTICAL_OFFSET + new - scroll_offset, POINTER_COL, MAX_BOX_LENGTH, curses.A_STANDOUT)
-
-        def page(self, down: bool):
-            nonlocal pointer
-            nonlocal scroll_offset
-
-            # get the index for blindly paging
-            new_pointer = pointer + (int(down or -1) * BOX_HEIGHT)
-
-            # if the blind paging goes too far off the valid values
-            if new_pointer >= MAX_POINTER:
-                # and we are already scrolled as far down
-                if scroll_offset >= MAX_SCROLL_OFFSET:
-                    # jump pointer to the bottom of the list
-                    pointer = MAX_POINTER
-                # else we can scroll to the bottom and offset the pointer by the scrolled amount
-                else:
-                    pointer -= (scroll_offset - MAX_SCROLL_OFFSET)
-                    scroll_offset -= (scroll_offset - MAX_SCROLL_OFFSET)
-            # elif the blind paging goes too far up
-            elif new_pointer <= 0:
-                # and we are already scrolled as far up
-                if scroll_offset <= 0:
-                    # jump pointer to the top of the list
-                    pointer = 0
-                # else we can scroll to the top and offset the pointer by the scrolled amount
-                else:
-                    pointer -= scroll_offset
-                    scroll_offset -= scroll_offset
-            # else apply the blind paging
-            else:
-                pointer = new_pointer
-                scroll_offset += (int(down or -1) * BOX_HEIGHT)
-                # but if the scroll offset goes out of bounds, bind it while offsetting pointer to real scrolled amount
-                if scroll_offset < 0:
-                    pointer -= scroll_offset
-                    scroll_offset -= scroll_offset
-                elif scroll_offset > MAX_SCROLL_OFFSET:
-                    pointer -= (scroll_offset - MAX_SCROLL_OFFSET)
-                    scroll_offset -= (scroll_offset - MAX_SCROLL_OFFSET)
-            write_data(self)
-            set_pointer(self, pointer)
-
-        def poll(self) -> int | None:
-            key = self.getch()  # implicit .refresh()
-            if key == curses.KEY_UP:
-                move_pointer(self, down=False)
-            elif key == curses.KEY_DOWN:
-                move_pointer(self, down=True)
-            elif key == curses.KEY_RIGHT:
-                page(self, down=True)
-            elif key == curses.KEY_LEFT:
-                page(self, down=False)
-            elif key == curses.ascii.ESC:
-                raise CursesCancel("User Cancelled")
-            elif key == curses.KEY_RESIZE:
-                resize(self)
-            else:
-                return pointer
-
-        resize(stdscr)
-        while ret is None:
-            ret = poll(stdscr)
-            pass
-
-    ret = None
     try:
-        curses.wrapper(select_box)
+        curses.wrapper(window.run)
     except CursesCancel:
         return None
-    assert ret < len(data), f"attempted to return {ret} - {curses.ascii.ascii(ret)} - {curses.ascii.ctrl(ret)} - {curses.ascii.alt(ret)} - {curses.ascii.unctrl(ret)}"
-    return data[ret]
-
-# example usage
-# my_data = ["hello", "there", "angel", "from", "my", "nightmare", "shadow", "in", "the", "background", "of", "the", "morgue", "the", "unspecting", "victim", "of", "darkness", "in", "the", "valley", "we", "can", "live", "like", "jack", "and", "sally", "if", "you", "want"]
-# print(curses_select(my_data))
+    if window.ret is None:
+        return None
+    return data[window.ret]
