d9ad5de5afbfab65592e05efaa2d64984978fd22
Ragmoa <ryckaert.guillaume@neuf.fr>
2026-05-28 17:29:29 +0200

    Take into account advenced powers in chest rules

--- worlds/okamihd/Rules.py
+++ worlds/okamihd/Rules.py
@@ -115,7 +115,7 @@ def apply_event_or_location_rules(loc: Location, name: str, data: LocData | Even
         case LocationType.TREASURE_BUD:
             required_techinques += [BrushTechniques.GREENSPROUT_BLOOM]
         case LocationType.BURIED_UNDER_LEAF_PILE:
-            required_techinques += [BrushTechniques.GALESTORM]
+            rules.append(HasAny(BrushTechniques.GALESTORM,BrushTechniques.WHIRLWIND))
         case LocationType.BURIED_CHEST:
             if world.options.NightTimeChecksRequireCrescent:
                 required_techinques += [BrushTechniques.CRESCENT]
@@ -124,9 +124,9 @@ def apply_event_or_location_rules(loc: Location, name: str, data: LocData | Even
             if world.options.NightTimeChecksRequireCrescent:
                 required_techinques += [BrushTechniques.CRESCENT]
         case LocationType.BURNING_CHEST:
-            rules.append(HasAny(BrushTechniques.GALESTORM, BrushTechniques.WATERSPOUT))
+            rules.append(HasAny(BrushTechniques.GALESTORM, BrushTechniques.WATERSPOUT,BrushTechniques.WHIRLWIND,BrushTechniques.DELUGE))
         case LocationType.BURNING_CHEST_NO_WATER:
-            required_techinques += [BrushTechniques.GALESTORM]
+            rules.append(HasAny(BrushTechniques.GALESTORM,BrushTechniques.WHIRLWIND))
         case LocationType.UNDERWATER_CHEST:
             required_power_slash_level = max(required_power_slash_level, 1)
         case LocationType.UNDERWATER_CHEST_SHALLOW:
@@ -141,11 +141,11 @@ def apply_event_or_location_rules(loc: Location, name: str, data: LocData | Even
             required_techinques += [BrushTechniques.GREENSPROUT_BLOOM, BrushTechniques.WATERSPOUT,
                                     BrushTechniques.GALESTORM]
         case LocationType.FROZEN_CHEST:
-            required_techinques += [BrushTechniques.INFERNO]
+            rules.append(HasAny(BrushTechniques.INFERNO,BrushTechniques.FIREBURST))
         case LocationType.FISHING_MINIGAME:
             required_power_slash_level = max(required_power_slash_level, 1)
         case LocationType.THUNDER_CHEST:
-            required_techinques += [BrushTechniques.THUNDERBOLT]
+           rules.append(HasAny(BrushTechniques.THUNDERBOLT,BrushTechniques.THUNDERSTORM))
 
         case _:
             required_techinques += []
