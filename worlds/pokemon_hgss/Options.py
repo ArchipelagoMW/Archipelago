2e4c02e6d79376602c01bddf5dc1a668fb671d40
EyeballSweat <72865536+EyeballSweat@users.noreply.github.com>
2026-06-03 18:08:26 +0100

    Add basic HGSS world options

--- worlds/pokemon_hgss/Options.py
+++ worlds/pokemon_hgss/Options.py
@@ -1,8 +1,35 @@
 from dataclasses import dataclass
 
-from Options import PerGameCommonOptions
+from Options import Choice, DefaultOnToggle, PerGameCommonOptions
+
+
+class Goal(Choice):
+    """
+    Determines the main completion goal.
+
+    Currently only Lance is supported. More goals can be added later,
+    such as Red or all 16 badges.
+    """
+
+    display_name = "Goal"
+
+    option_lance = 0
+
+    default = 0
+
+
+class HMBadgeRequirements(DefaultOnToggle):
+    """
+    Determines whether HMs require their matching badge in logic.
+
+    When enabled, Surf requires both HM03 Surf and Fog Badge.
+    When disabled, Surf only requires HM03 Surf.
+    """
+
+    display_name = "HM Badge Requirements"
 
 
 @dataclass
 class PokemonHGSSOptions(PerGameCommonOptions):
-    pass
+    goal: Goal
+    hm_badge_requirements: HMBadgeRequirements
