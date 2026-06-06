e32970626f5a062e9f65a44faf83444be1d06e0e
Taoshi <nicolaikh@live.dk>
2026-06-06 18:41:45 +0200

    RAC3: Improve NGPlusItems yaml description

--- worlds/rac3/options/ngplus_item_options.py
+++ worlds/rac3/options/ngplus_item_options.py
@@ -9,6 +9,9 @@ class NGPlusItems(Choice):
     Determines if the RY3NO should be an item in the item pool or not.
     **Disable**: It is available.
     **Enable**: It is not available.
+
+    This option when enabled also puts mega versions of weapons in the vendor if you have a V5.
+    For progressive weapons, it will instead add 3 more upgrades to the pool, up to V8.
     """
     display_name = RAC3OPTION.NGPLUS_ITEMS
     option_disable = 0
