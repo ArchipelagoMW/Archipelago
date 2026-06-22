098f6896f12b2bc2e11ee8b889775a49dfad31db
AmondoVagyok <solokapitany10@gmail.com>
2026-06-22 18:16:28 +0200

    RAC3: Update NG+ items yaml description and add Weapon Level before weapon level locations

--- worlds/rac3/options/ngplus_item_options.py
+++ worlds/rac3/options/ngplus_item_options.py
@@ -6,12 +6,15 @@ from worlds.rac3.constants.options import RAC3OPTION
 
 class NGPlusItems(Choice):
     """
-    Determines if the RY3NO should be an item in the item pool or not.
-    **Disable**: It is available.
-    **Enable**: It is not available.
-
-    This option when enabled also puts mega versions of weapons in the vendor if you have a V5.
+    Determines how New Game Plus items appear in the multiworld.
+    This option when enabled puts the RY3N0 into the item pool and the mega versions of weapons in the vendor
+    if you have a V5 weapon.
     For progressive weapons, it will instead add 3 more upgrades to the pool, up to V8.
+    **Disable**: The RY3N0 will not appear in the multiworld, the mega versions of weapons are not purchasable.
+    **Enable**: The RY3N0 will appear in the multiworld, the mega versions of weapons are purchasable.
+
+    This option when enabled will not grant checks for the mega weapon vendor purchases, it'll provide the mega version
+    of the weapon only.
     """
     display_name = RAC3OPTION.NGPLUS_ITEMS
     option_disable = 0
