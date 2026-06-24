9cbd4c9e7eeacfc6943796e36b1263dac4f768ba
Taoshi <nicolaikh@live.dk>
2026-06-24 10:47:48 +0200

    RAC3: Make good exp function setting independent

--- worlds/rac3/rules.py
+++ worlds/rac3/rules.py
@@ -62,11 +62,16 @@ def can_earn_good_exp(state: CollectionState, world: "RaC3World") -> bool:
     """Determine if the player can earn good experience based on the planets they can access"""
     if state.has_any(GOOD_EXP_PLANETS, world.player):
         return True
-    if world.options.trophies.value:
-        if state.can_reach_location(RAC3TROPHY.COMMAND_LAWRENCE, world.player):
-            return True
-        if state.can_reach_location(RAC3TROPHY.HIDEOUT_QWARK, world.player):
-            return True
+    if (state.can_reach_region(RAC3REGION.COMMAND_CENTER)
+        and state.has_all([RAC3ITEM.HYPERSHOT, RAC3ITEM.GRAV_BOOTS, RAC3ITEM.TYHRRA_GUISE], world.player)
+        and state.has_any([RAC3ITEM.HELI_PACK, RAC3ITEM.THRUSTER_PACK, RAC3ITEM.CLANK, RAC3ITEM.PROGRESSIVE_PACK],
+                              world.player)):
+        return True
+    if (state.can_reach_region(RAC3REGION.QWARKS_HIDEOUT)
+        and state.has_all([RAC3ITEM.WARP_PAD, RAC3ITEM.HYPERSHOT], world.player)
+        and state.has_any([RAC3ITEM.HELI_PACK, RAC3ITEM.CLANK, RAC3ITEM.PROGRESSIVE_PACK, RAC3ITEM.CHARGE_BOOTS],
+                              world.player)):
+        return True
     if world.options.ngplus_start.value:
         return True
     return False
