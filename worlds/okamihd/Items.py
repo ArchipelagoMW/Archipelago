f04faee6feefa3e8f0cb4271d24ee938524d1f22
Ragmoa <ryckaert.guillaume@neuf.fr>
2026-06-30 13:52:05 +0200

    Update item counts

--- worlds/okamihd/Items.py
+++ worlds/okamihd/Items.py
@@ -152,15 +152,15 @@ bitable_items = {
 }
 useful_items = {
     # Useful items - Counts here are invalid, it's intended, to not fille the item pool with these
-    "Sun Fragment": ItemData(0x05, ItemClassification.useful,count_in_pool=6), # Should be 12
+    "Sun Fragment": ItemData(0x05, ItemClassification.useful,count_in_pool=9), # Should be 12
     "Astral Pouch": ItemData(0x06, ItemClassification.useful,count_in_pool=0),# Intended
     "Stray Bead": ItemData(0xCC, ItemClassification.useful,count_in_pool=0),# Should be 99, set to 0 for now cause they mess with container collection states
     # probably will have to be changed to progession_skip balancing once DF shops get randomized
     "Demon Fang": ItemData(0x1F, ItemClassification.useful, count_in_pool=0),  # to see when DF shops get randomized
     # Technically a filler item, but useful feels more appropriate. Warping with those without Fountain will probably be out of logic.
     "Mermaid Coin": ItemData(0x0e, ItemClassification.useful,count_in_pool=5),#Accurate count, kept it since it isn't too much
-    "Golden Peach": ItemData(0x0f, ItemClassification.useful,count_in_pool=7), # 14 in total... Probably not useful to have THAT many?,
-    "Gold Dust": ItemData(0x9e, ItemClassification.useful,count_in_pool=7) # 15 if we count the ones sold by merchants, which we may randomize, only 1 in a chest if we don't count those...
+    "Golden Peach": ItemData(0x0f, ItemClassification.useful,count_in_pool=10), # 14 in total... Probably not useful to have THAT many?,
+    "Gold Dust": ItemData(0x9e, ItemClassification.useful,count_in_pool=11) # 15 if we count the ones sold by merchants, which we may randomize, only 1 in a chest if we don't count those...
 }
 
 filler_items = {
