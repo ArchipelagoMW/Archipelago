a969d776611cb8b418bf396ea25a841514e588a1
Taoshi <nicolaikh@live.dk>
2026-07-04 10:49:09 +0200

    RAC3: Add stored fillers to server side save data

--- worlds/rac3/client/callbacks.py
+++ worlds/rac3/client/callbacks.py
@@ -165,7 +165,6 @@ async def _handle_game_ready(ctx: "Context") -> None:
             logger.debug(f"Data Package: {ctx.stored_data.get(RAC3OPTION.PROCESSED_LOCATIONS, 'Empty')}")
             logger.info(f"Items Received: {len(ctx.items_received)}")
             items_to_process = ctx.stored_data.get(RAC3OPTION.PROCESSED_LOCATIONS, len(ctx.items_received))
-            ctx.game_interface.init_stored_fillers()
             ctx.game_interface.initial_fillers.clear()
             counter = 0
             for count, item in enumerate(ctx.items_received):
@@ -188,6 +187,7 @@ async def _handle_game_ready(ctx: "Context") -> None:
             logger.info(f"Locations collected: {counter}")
             logger.info("Updating save data...")
             ctx.game_interface.load_save(ctx.save_data)
+            ctx.game_interface.init_stored_fillers()
             ctx.game_interface.process_offline_fillers(ctx.data_received)
             ctx.game_interface.reset_death_count()
             ctx.game_interface.setup_challenge_mode()
