509c048d7e3b69d7ed66daa8344f1421dcbd73e9
Alex Alfonso Trigo <119543558+plex4451@users.noreply.github.com>
2026-07-04 17:58:16 +0200

    Bugfix: Changed Utils.local_path to Utils.user_path
    Co-authored-by: qwint <qwint.42@gmail.com>

--- Launcher.py
+++ Launcher.py
@@ -464,7 +464,7 @@ def run_gui(launch_components: list[Component], args: Any) -> None:
             # Test if persistent storage exists (first time running the Launcher)
             # Depending on the result, either show the setup screen for the first time
             # Otherwise, show the main screen with all the components
-            if os.path.exists(Utils.local_path("_persistent_storage.yaml")):
+            if os.path.exists(Utils.user_path("_persistent_storage.yaml")):
                 self.top_screen.ids.all.trigger_action(duration=0)
             else:
                 self.top_screen.ids.setup.trigger_action(duration=0)
