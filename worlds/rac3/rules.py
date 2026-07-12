a3b399e4f56a42cef4d5ad93cdde4a8e455c512b
Taoshi <nicolaikh@live.dk>
2026-07-12 18:32:56 +0200

    RAC3: Adjust Aridia Mission 3 logic for long range weapons

--- worlds/rac3/rules.py
+++ worlds/rac3/rules.py
@@ -673,13 +673,16 @@ def set_rules(world: "RaC3World"):
             lambda state: state.can_reach_location(RAC3LOCATION.ARIDIA_RANGERS_2, world.player),
         RAC3LOCATION.ARIDIA_RANGERS_3:
             lambda state: (state.has_any([RAC3ITEM.GRAV_BOOTS, RAC3ITEM.RIFT_INDUCER,
-                                          RAC3ITEM.FLUX_RIFLE, RAC3ITEM.PROGRESSIVE_FLUX_RIFLE,
-                                          RAC3ITEM.ANNIHILATOR, RAC3ITEM.PROGRESSIVE_ANNIHILATOR,
-                                          RAC3ITEM.RY3N0, RAC3ITEM.PROGRESSIVE_RY3N0,
-                                          RAC3ITEM.SUCK_CANNON, RAC3ITEM.PROGRESSIVE_SUCK_CANNON,
-                                          RAC3ITEM.DISC_BLADE, RAC3ITEM.PROGRESSIVE_DISC_BLADE,
-                                          RAC3ITEM.PLASMA_COIL, RAC3ITEM.PROGRESSIVE_PLASMA_COIL], world.player)
-                           or state.has(RAC3ITEM.PROGRESSIVE_RIFT_INDUCER, world.player, 2))
+                                          RAC3ITEM.FLUX_RIFLE, RAC3ITEM.ANNIHILATOR,
+                                          RAC3ITEM.RY3N0, RAC3ITEM.SUCK_CANNON,
+                                          RAC3ITEM.DISC_BLADE, RAC3ITEM.PLASMA_COIL], world.player)
+                           or state.has(RAC3ITEM.PROGRESSIVE_RIFT_INDUCER, world.player, 2 if progressive_requirement == 1 else progressive_requirement)
+                           or state.has(RAC3ITEM.PROGRESSIVE_FLUX_RIFLE, world.player, progressive_requirement)
+                           or state.has(RAC3ITEM.PROGRESSIVE_ANNIHILATOR, world.player, progressive_requirement)
+                           or state.has(RAC3ITEM.PROGRESSIVE_RY3N0, world.player, progressive_requirement)
+                           or state.has(RAC3ITEM.PROGRESSIVE_SUCK_CANNON, world.player, progressive_requirement)
+                           or state.has(RAC3ITEM.PROGRESSIVE_DISC_BLADE, world.player, progressive_requirement)
+                           or state.has(RAC3ITEM.PROGRESSIVE_PLASMA_COIL, world.player, progressive_requirement))
                           and state.can_reach_location(RAC3LOCATION.ARIDIA_RANGERS_2, world.player),
         RAC3LOCATION.ARIDIA_RANGERS_4:
             lambda state: state.can_reach_location(RAC3LOCATION.ARIDIA_RANGERS_3, world.player),
