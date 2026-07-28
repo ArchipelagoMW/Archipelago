663240454d452a90deb771e7f30bd79f23f372cf
orizach01 <orizach01@gmail.com>
2026-07-29 00:21:37 +0300

    stuff

--- worlds/mksm/options.py
+++ worlds/mksm/options.py
@@ -21,14 +21,14 @@ class Character(Choice):
 
 class RedKoinPercent(Range):
     """
-    The randomizer fills the item pool with Red Koin items which are then randomized around the multiworld.
-    The number of Red Koins in the pool depends on your character (some characters have more locations than others).
+    The randomizer tries to fill the item pool with 60 Red Koin items which are then randomized around the multiworld.
+    There may be less than 60 if there aren't enough locations in the world.
     To complete the goal you need to both beat the boss goal and have a set number of Red Koins.
     This option determines what % of all the available Red Koins in the pool is needed.
     0 means the goal will be beating the boss goal only.
-    100 means requiring to find ALL Red Koins from locations AND beating the boss goal.
-    80 (default) means you need to find at least 80% of all Red Koins AND beat the boss goal to win.
-    There is a tracker in the pause menu that shows: current amount / need for goal / total in the multiworld.
+    100 means requiring to get ALL 60 Red Koins AND beating the boss goal.
+    80 (default) means you need to get at least 80% of all Red Koins AND beat the boss goal to win.
+    There is a tracker in the pause menu that shows: current amount / needed for goal / total in the multiworld.
     """
     display_name = "Red Koin goal percent"
     range_start = 0
@@ -47,7 +47,7 @@ class BossGoal(Choice):
     display_name = "Boss Goal"
 
     option_main_bosses = 0
-    option_main_and_secret_bosses = 2
+    option_main_and_secret_bosses = 1
 
     default = option_main_bosses
 
