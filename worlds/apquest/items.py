728048288b8c6bdc658433d2e4622109e32b5c64
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2026-08-17 21:42:57 +0200

    more accurate?

--- worlds/apquest/items.py
+++ worlds/apquest/items.py
@@ -48,7 +48,7 @@ def get_random_filler_item_name(world: APQuestWorld) -> str:
     # For this purpose, we need to use a random generator.
 
     # IMPORTANT: Whenever you need to use a random generator, you must use world.random.
-    # world.random is an instance of Python's random.random class, but seeded using the multiworld seed as a base.
+    # world.random is an instance of Python's random.random class, but seeded deterministically by the multiworld seed.
     # This ensures that generating with the same multiworld seed twice yields the same output.
     # Do not create your own "bare" instance of random.
     if world.random.randint(0, 99) < world.options.trap_chance:
