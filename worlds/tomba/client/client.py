4dd8bc96f2b22e6776fb0020fdb12383e14dd83a
T4g1 <thomasvangysegem@gmail.com>
2026-09-14 16:32:31 +0200

    [fix] Faster synchronisation for sections updates

--- worlds/tomba/client/client.py
+++ worlds/tomba/client/client.py
@@ -92,7 +92,6 @@ class TombaContext(CommonContext):
         self.periodic_handlers: list[Handler] = [
             Handler(self.tomba.keep_alive, interval_ms=KEEP_ALIVE_INTERVAL),
             Handler(self.tomba.patch_game, interval_ms=1000),
-            Handler(self.tomba.update_section, interval_ms=2000),
             Handler(self.tomba.update_events, interval_ms=250),
             Handler(self.tomba.update_inventory, interval_ms=750),
             Handler(self.check_handler.update_locations, interval_ms=500),
@@ -263,7 +262,7 @@ class TombaContext(CommonContext):
 
                         await self.process_items_received()
 
-                        await self.found_handler.update_found_items()
+                        await self.tomba.update_section()
 
                     now = time.time()
                     tick_duration = now - last_tick
--- worlds/tomba/client/handlers/transition.py
+++ worlds/tomba/client/handlers/transition.py
@@ -99,7 +99,7 @@ class TransitionHandler(AbstractHandler):
 
             await self.tomba.playstation.write_memory(entrance_address + 5, data)
 
-            logger.info(
+            logger.debug(
                 f"Update transition 0x{entrance_address:08X} "
                 f"0x{int(entrance_id):02X} "
                 f"to 0x{target_area:02X}-0x{target_section:02X} "
