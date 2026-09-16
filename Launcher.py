8b928e3685cf3ba220829eb4597271da9b9e3527
qwint <qwint.42@gmail.com>
2026-09-15 23:40:41 -0500

    skip loading worlds if they are already loaded due to a component miss

--- Launcher.py
+++ Launcher.py
@@ -300,9 +300,11 @@ def run_gui(launch_components: list["Component"], args: Any) -> None:
             import importlib
             import time
             start = time.perf_counter()
-            assert "worlds" not in sys.modules, "worlds module already loaded."
-            importlib.import_module("worlds")
-            logging.error(f"Worlds module loaded in {time.perf_counter() - start:.2f} seconds")
+            if "worlds" in sys.modules:
+                logging.warning("worlds module already loaded.")
+            else:
+                importlib.import_module("worlds")
+                logging.error(f"Worlds module loaded in {time.perf_counter() - start:.2f} seconds")
 
             global refresh_components
             logger = logging.getLogger("Worlds")
